b5524fb79d1fd14970024ff56dbd23a7dd36122d
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2010-05-12 18:33:46 -0500

    (hyrangea)Removed Stanford Webauth stuff (defaulting back to Blacklight's Authlogic) and updated tests, controllers, helpers, etc. accordingly

--- app/helpers/hydra_access_controls_helper.rb
+++ app/helpers/hydra_access_controls_helper.rb
@@ -1,14 +1,31 @@
 module HydraAccessControlsHelper
   
+  # Incomplete.  Currently returns true if user is logged in (regardless of permission level)
+  def test_permission(permission_type)
+    if !current_user.nil?
+      user = current_user.login
+      result = case permission_type
+        when :edit
+          logger.debug("Checking edit permissions. user perms: #{Array(RoleMapper.roles(user)).flatten.inspect}, current session user #{user}")
+          RoleMapper.roles(user).include?("donor") || RoleMapper.roles(user).include?("archivist")
+        when :read
+          logger.debug("Checking read permissions. user perms: #{Array(RoleMapper.roles(user)).flatten.inspect}, current session user #{user}")
+          RoleMapper.roles(user).include?("donor") || RoleMapper.roles(user).include?("archivist") || RoleMapper.roles(user).include?("researcher") || RoleMapper.roles(user).include?("patron")
+        else
+          false     
+      end 
+      return result
+    else
+      return false
+    end
+  end
+  
   def editor?
-    user = session[:user]
-    RoleMapper.roles(user).include?("donor") || RoleMapper.roles(user).include?("archivist")
+    test_permission(:edit)
   end
   
   def reader?
-    user = session[:user]
-    logger.debug("Checking read permissions. user perms: #{Array(RoleMapper.roles(user)).flatten.inspect}, current session user #{session[:user]}")
-    RoleMapper.roles(user).include?("donor") || RoleMapper.roles(user).include?("archivist") || RoleMapper.roles(user).include?("researcher") || RoleMapper.roles(user).include?("patron")
+   test_permission(:read)
   end
 
 end
