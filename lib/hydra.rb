739dd98c84b4a4f3a13097eb24d8cdae3f23b952
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2010-05-12 18:47:07 -0500

    (hyrangea)moved access controls enforcement into mixin in hydra_repository plugin

--- lib/hydra.rb
+++ lib/hydra.rb
@@ -1,6 +1,7 @@
 # Hydra libraries
 module Hydra; end
 require 'hydra/repository_controller'
+require 'hydra/access_controls_enforcement'
 require 'hydra/testing_server'
 
 require 'mediashelf/active_fedora_helper'
---
+++ lib/hydra/access_controls_enforcement.rb
@@ -0,0 +1,45 @@
+module Hydra::AccessControlsEnforcement
+
+  private
+
+  # If someone hits the show action while their session's viewing_context is in edit mode, 
+  # this will redirect them to the edit action.
+  # If they do not have sufficient privileges to edit documents, it will silently switch their session to browse mode.
+  def enforce_viewing_context_for_show_requests    
+    if params[:viewing_context] == "browse"
+      session[:viewing_context] = params[:viewing_context]
+    elsif session[:viewing_context] == "edit"
+      if editor?
+        redirect_to :action=>:edit
+      else
+        session[:viewing_context] = "browse"
+      end
+    end
+  end
+
+  def enforce_search_permissions
+    if !reader? 
+      @extra_controller_params[:qt] = Blacklight.config[:public_qt]
+      return @extra_controller_params
+    end
+  end
+  def enforce_read_permissions
+    unless @document['access_t'] && (@document['access_t'].first == "public" || @document['access_t'].first == "Public")
+    # case @document['access_t'].first
+    # when /private/
+      unless reader?
+        flash[:notice]= "You do not have sufficient access privileges to read this document, which has been marked private."
+        redirect_to(:action => 'index', :q => nil , :f => nil) and return false
+      end
+    end
+  end
+
+  def enforce_edit_permissions
+    if !editor?
+      session[:viewing_context] = "browse"
+      flash[:notice] = "You do not have sufficient privileges to edit this document. You have been redirected to the read-only view."
+      redirect_to :action=>:show
+    end
+  end
+
+end
