f14c58b719986eb96a224c46dc5cd4466848185c
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2010-05-13 00:27:36 -0500

    (hyrangea)adding FileAssets views and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 ActionController::Routing::Routes.draw do |map|
-     map.resources :file_assets, :assets
+     map.resources :file_assets
+     
+     # This creates routes named asset_file_assets, new_asset_file_asset, etc.
+     # The routes map to the :file_assets controller with the param :container_id 
+     # which is then used within the controller to deal with parent - child relationships 
+     # between Assets and the FileAssets inside them.
+     map.resources :file_assets, :path_prefix => '/assets/:container_id', :name_prefix => "asset_"
+     
+     map.resources :assets do |assets|
+       assets.resources :downloads, :only=>[:index]
+     end
 end
--- lib/hydra.rb
+++ lib/hydra.rb
@@ -1,9 +1,10 @@
 # Hydra libraries
 module Hydra; end
+require 'mediashelf/active_fedora_helper'
+
 require 'hydra/repository_controller'
 require 'hydra/access_controls_enforcement'
 require 'hydra/testing_server'
 require 'hydra/assets_controller_helper'
 require 'hydra/file_assets_helper'
 
-require 'mediashelf/active_fedora_helper'
