0e6a77d24aaf6857e60f0a098463673eabe3cf8f
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2010-06-24 00:18:21 -0500

    (hyrangea)working on file asset list display (and setting dc:extent to filesize on upload)

--- app/models/file_asset.rb
+++ app/models/file_asset.rb
@@ -1,5 +1,18 @@
 class FileAsset < ActiveFedora::Base
   
+  # @num file size in bits
+  # Returns a human readable filesize appropriate for the given number of bytes (ie. automatically chooses 'bytes','KB','MB','GB','TB')
+  # Based on a bit of python code posted here: http://blogmag.net/blog/read/38/Print_human_readable_file_size
+  def bits_to_human_readable(num)
+      ['bytes','KB','MB','GB','TB'].each do |x|
+        if num < 1024.0
+          return "#{num.to_i} #{x}"
+        else
+          num = num/1024.0
+        end
+      end
+  end
+  
   has_metadata :name => "DC", :type => ActiveFedora::QualifiedDublinCoreDatastream do |m|
   end
       
@@ -7,6 +20,11 @@ class FileAsset < ActiveFedora::Base
     super
     datastreams_in_memory["DC"].title_values = label
   end    
+  
+  def add_file_datastream(file, opts={})
+    super
+    datastreams_in_memory["DC"].extent_values = bits_to_human_readable(file.size)
+  end
   
   def save
     super
