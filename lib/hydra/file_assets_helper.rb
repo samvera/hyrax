8d669ef80ceaa66be576d8ed7aad110570c86977
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2010-10-17 21:31:49 -0500

    (hyrangea)documentation

--- lib/hydra/file_assets_helper.rb
+++ lib/hydra/file_assets_helper.rb
@@ -1,5 +1,8 @@
 module Hydra::FileAssetsHelper
   
+  # Creates a File Asset, adding the posted blob to the File Asset's datastreams and saves the File Asset
+  #
+  # @return [FileAsset] the File Asset  
   def create_and_save_file_asset_from_params
     if params.has_key?(:Filedata)
       @file_asset = create_asset_from_params
@@ -11,18 +14,28 @@ module Hydra::FileAssetsHelper
     end
   end
   
-  def add_posted_blob_to_asset
-    @file_asset.add_file_datastream(params[:Filedata], :label=>params[:Filename])
+  # Puts the contents of params[:Filedata] (posted blob) into a datastream within the given @asset
+  #
+  # @param [FileAsset] the File Asset to add the blob to
+  # @return [FileAsset] the File Asset  
+  def add_posted_blob_to_asset(asset=@file_asset)
+    asset.add_file_datastream(params[:Filedata], :label=>params[:Filename])
   end
   
+  # Creates a File Asset and sets its label from params[:Filename]
+  #
+  # @return [FileAsset] the File Asset
   def create_asset_from_params    
-    # file_asset = asset_class_from_params.new
     file_asset = FileAsset.new
     file_asset.label = params[:Filename]
     
     return file_asset
   end
   
+  # This is pre-Hydra code that created an AudioAsset, VideoAsset or ImageAsset based on the
+  # current params in the controller.
+  #
+  # @return [Constant] the recommended Asset class
   def asset_class_from_params
     if params.has_key?(:type)
       chosen_type = case params[:type]
@@ -48,6 +61,15 @@ module Hydra::FileAssetsHelper
     choose_model_by_filename_extension( File.extname(filename) )
   end
   
+  # Rudimentary method to choose an Asset class based on a filename extension
+  #
+  # @param [String] extension
+  # @return [Constant] the recommended Asset class.  Default: FileAsset
+  #
+  # Recognized extensions: 
+  # AudioAsset => ".wav", ".mp3", ".aiff"
+  # VideoAsset => ".mov", ".flv", ".mp4"
+  # ImageAsset => ".jpeg", ".jpg", ".gif", ".png"
   def choose_model_by_filename_extension(extension)
     case extension
     when ".wav", ".mp3", ".aiff"
