775e8a61a5e12709767087fec7481b27132200c4
rickjohnson <rick.johnson@nd.edu>
2011-03-15 16:54:51 -0400

    (hyrangea)Fix for HYDRA-406 where metadata was indexed twice for ImageAsset, AudioAsset, and VideoAsset

--- app/models/audio_asset.rb
+++ app/models/audio_asset.rb
@@ -1,6 +1,13 @@
 class AudioAsset < FileAsset
-  def initialize
-    super
+  def initialize(attrs = {})
+    super(attrs)
     add_relationship(:has_model, "info:fedora/afmodel:FileAsset")
+  end
+
+  # Override ActiveFedora::Base.to_solr to...
+  # For now just return solr_doc but will need to be changed to 
+  # include datastreams in AudioAsset if added later
+  def to_solr(solr_doc = Solr::Document.new, opts={})
+    solr_doc
   end
 end
