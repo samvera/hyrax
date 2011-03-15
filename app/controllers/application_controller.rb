d460a3b372e888ad33186bd5e5757bb31cc5a212
Jessie Keck <jessie.keck@gmail.com>
2011-03-14 17:14:43 -0700

    (Hydrus) Initial move of css/js to plugin.  Also invoking css/js load from ApplicationController so that it's available to all other controllers when necessary down the road.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,20 +20,20 @@ class ApplicationController
   # Uncomment the :secret if you're not using the cookie session store
   # protect_from_forgery # :secret => '200c1e5f25e610288439b479ef176bbd'
   
-  
+  # This really shouldn't be an override.  What we SHOULD be doing is invoking another method in a before filter that will modify the javascript_includes and stylesheet_links arrays.
+  # I don't know if there was a particular reason to do it like this so I'm not going to muck w/ it.  The way it is now, if BL adds something, or a file name changes (e.g. jQuery version) this will break.
   def default_html_head
     # when working offline, comment out the above uncomment the next line:
     #javascript_includes << ['jquery-1.4.2.min.js', 'jquery-ui-1.8.1.custom.min.js', { :plugin=>:blacklight } ]
-  
     javascript_includes << ['http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js', 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.1/jquery-ui.min.js']
     javascript_includes << ['application', {:plugin=>:hydra_repository, :media=>'all'}]
     
-    javascript_includes << ['blacklight', 'application', 'accordion', { :plugin=>:blacklight } ]
+    javascript_includes << ['blacklight', 'application', { :plugin=>:blacklight } ]
     
     stylesheet_links << ['yui', 'jquery/ui-lightness/jquery-ui-1.8.1.custom.css', 'application', {:plugin=>:blacklight, :media=>'all'}]
     stylesheet_links << ['redmond/jquery-ui-1.8.5.custom.css', {:plugin=>:hydra_repository, :media=>'all'}]      
-    stylesheet_links << ['styles', 'hydrangea', "hydrangea-split-button.css", {:plugin=>:hydra_repository,:media=>'all'}]
-  end 
+    stylesheet_links << ['styles', 'hydrangea', "hydrangea-split-button.css", {:plugin=>:hydra_repository, :media=>'all'}]
+  end
       
   def current_user
     return @current_user if defined?(@current_user)
--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -6,18 +6,7 @@ class CatalogController
   include Hydra::RepositoryController
   include Hydra::AccessControlsEnforcement
   before_filter :require_solr, :require_fedora, :only=>[:show, :edit, :index]
-  
-  def default_html_head
-    javascript_includes << ['http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js', 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.1/jquery-ui.min.js']
-    javascript_includes << ['application']
     
-    javascript_includes << ['blacklight', 'application', { :plugin=>:blacklight } ]
-    
-    stylesheet_links << ['yui', 'jquery/ui-lightness/jquery-ui-1.8.1.custom.css', 'application', {:plugin=>:blacklight, :media=>'all'}]
-    stylesheet_links << ['redmond/jquery-ui-1.8.5.custom.css', {:media=>'all'}]      
-    stylesheet_links << ['styles', 'hydrangea', "hydrangea-split-button.css", {:media=>'all'}]
-  end
-  
   def edit
     af_base = ActiveFedora::Base.load_instance(params[:id])
     the_model = ActiveFedora::ContentModel.known_models_for( af_base ).first
