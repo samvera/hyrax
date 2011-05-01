samvera/hydra-head

(Hydrus) adding t.identifer for HYDRUS-8

lib/hydra/mods_article.rb
@@ -6,44 +6,51 @@ module Hydra
 
     set_terminology do |t|
       t.root(:path=>"mods", :xmlns=>"http://www.loc.gov/mods/v3", :schema=>"http://www.loc.gov/standards/mods/v3/mods-3-2.xsd")
-
-      t.title_info(:path=>"titleInfo") {
-        t.main_title(:index_as=>[:facetable],:path=>"title", :label=>"title")
-        t.language(:index_as=>[:facetable],:path=>{:attribute=>"lang"})
-      } 
-      t.language{
-        t.lang_code(:index_as=>[:facetable], :path=>"languageTerm", :attributes=>{:type=>"code"})
-      }
-      t.abstract   
-      t.subject {
-        t.topic(:index_as=>[:facetable])
-      }      
-      t.topic_tag(:proxy=>[:subject, :topic])    
-      # t.topic_tag(:index_as=>[:facetable],:path=>"subject", :default_content_path=>"topic")
-      # This is a mods:name.  The underscore is purely to avoid namespace conflicts.
-      t.name_ {
-        # this is a namepart
-        t.namePart(:type=>:string, :label=>"generic name")
-        # affiliations are great
-        t.affiliation
-        t.institution(:path=>"affiliation", :index_as=>[:facetable], :label=>"organization")
-        t.displayForm
-        t.role(:ref=>[:role])
-        t.description(:index_as=>[:facetable])
-        t.date(:path=>"namePart", :attributes=>{:type=>"date"})
-        t.last_name(:path=>"namePart", :attributes=>{:type=>"family"})
-        t.first_name(:path=>"namePart", :attributes=>{:type=>"given"}, :label=>"first name")
-        t.terms_of_address(:path=>"namePart", :attributes=>{:type=>"termsOfAddress"})
-        t.computing_id
-      }
-      # lookup :person, :first_name        
-      t.person(:ref=>:name, :attributes=>{:type=>"personal"}, :index_as=>[:facetable])
-      t.department(:proxy=>[:person,:description],:index_as=>[:facetable])
-      t.organization(:ref=>:name, :attributes=>{:type=>"corporate"}, :index_as=>[:facetable])
-      t.conference(:ref=>:name, :attributes=>{:type=>"conference"}, :index_as=>[:facetable])
-      t.role {
-        t.text(:path=>"roleTerm",:attributes=>{:type=>"text"})
-        t.code(:path=>"roleTerm",:attributes=>{:type=>"code"})
+    t.title_info(:path=>"titleInfo") {
+      t.main_title(:path=>"title", :label=>"title")
+      t.language(:index_as=>[:facetable],:path=>{:attribute=>"lang"})
+    } 
+    t.title(:proxy=>[:title_info, :main_title]) 
+    
+    t.language{
+      t.lang_code(:index_as=>[:facetable], :path=>"languageTerm", :attributes=>{:type=>"code"})
+    }
+    t.abstract   
+    t.subject {
+      t.topic
+    }     
+    t.topic_tag(:index_as=>[:facetable],:path=>"subject", :default_content_path=>"topic")
+    t.identifier {
+      t.type_(:path=>{:attribute=>"type"})
+    }
+    # This is a mods:name.  The underscore is purely to avoid namespace conflicts.
+    t.name_ {
+      # this is a namepart
+      t.namePart(:type=>:string, :label=>"generic name")
+      # affiliations are great
+      t.affiliation
+      t.institution(:path=>"affiliation", :index_as=>[:facetable], :label=>"organization")
+      t.displayForm
+      t.role(:ref=>[:role])
+      t.description
+      t.date(:path=>"namePart", :attributes=>{:type=>"date"})
+      t.last_name(:path=>"namePart", :attributes=>{:type=>"family"})
+      t.first_name(:path=>"namePart", :attributes=>{:type=>"given"}, :label=>"first name")
+      t.terms_of_address(:path=>"namePart", :attributes=>{:type=>"termsOfAddress"})
+    }
+    # lookup :person, :first_name        
+    t.person(:ref=>:name, :attributes=>{:type=>"personal"}, :index_as=>[:facetable])
+    t.organization(:ref=>:name, :attributes=>{:type=>"corporate"}, :index_as=>[:facetable])
+    t.conference(:ref=>:name, :attributes=>{:type=>"conference"}, :index_as=>[:facetable])
+    t.role {
+      t.text(:path=>"roleTerm",:attributes=>{:type=>"text"})
+      t.code(:path=>"roleTerm",:attributes=>{:type=>"code"})
+    }
+    t.journal(:path=>'relatedItem', :attributes=>{:type=>"host"}) {
+      t.title_info(:index_as=>[:facetable],:ref=>[:title_info])
+      t.origin_info(:path=>"originInfo") {
+        t.publisher
+        t.date_issued(:path=>"dateIssued")
       }
       t.journal(:path=>'relatedItem', :attributes=>{:type=>"host"}) {
         t.title_info(:index_as=>[:facetable],:ref=>[:title_info])
@@ -103,6 +110,7 @@ module Hydra
              xml.subject {
                xml.topic
              }
+             xml.identifer
              xml.relatedItem(:type=>"host") {
                xml.titleInfo {
                  xml.title

samvera/hyrax

lib/hydra/mods_article.rb
@@ -7,6 +7,7 @@ module Hydra
 
     set_terminology do |t|
       t.root(:path=>"mods", :xmlns=>"http://www.loc.gov/mods/v3", :schema=>"http://www.loc.gov/standards/mods/v3/mods-3-2.xsd")
+<<<<<<< HEAD
     t.title_info(:path=>"titleInfo") {
       t.main_title(:path=>"title", :label=>"title")
       t.language(:index_as=>[:facetable],:path=>{:attribute=>"lang"})
@@ -52,6 +53,49 @@ module Hydra
       t.origin_info(:path=>"originInfo") {
         t.publisher
         t.date_issued(:path=>"dateIssued")
+=======
+
+      t.title_info(:path=>"titleInfo") {
+        t.main_title(:index_as=>[:facetable],:path=>"title", :label=>"title")
+        t.language(:index_as=>[:facetable],:path=>{:attribute=>"lang"})
+      } 
+      t.language{
+        t.lang_code(:index_as=>[:facetable], :path=>"languageTerm", :attributes=>{:type=>"code"})
+      }
+      t.abstract 
+      t.identifier {
+        t.type_(:path=>{:attribute=>"type"})
+      }  
+      t.subject {
+        t.topic(:index_as=>[:facetable])
+      }      
+      t.topic_tag(:proxy=>[:subject, :topic])    
+      # t.topic_tag(:index_as=>[:facetable],:path=>"subject", :default_content_path=>"topic")
+      # This is a mods:name.  The underscore is purely to avoid namespace conflicts.
+      t.name_ {
+        # this is a namepart
+        t.namePart(:type=>:string, :label=>"generic name")
+        # affiliations are great
+        t.affiliation
+        t.institution(:path=>"affiliation", :index_as=>[:facetable], :label=>"organization")
+        t.displayForm
+        t.role(:ref=>[:role])
+        t.description(:index_as=>[:facetable])
+        t.date(:path=>"namePart", :attributes=>{:type=>"date"})
+        t.last_name(:path=>"namePart", :attributes=>{:type=>"family"})
+        t.first_name(:path=>"namePart", :attributes=>{:type=>"given"}, :label=>"first name")
+        t.terms_of_address(:path=>"namePart", :attributes=>{:type=>"termsOfAddress"})
+        t.computing_id
+      }
+      # lookup :person, :first_name        
+      t.person(:ref=>:name, :attributes=>{:type=>"personal"}, :index_as=>[:facetable])
+      t.department(:proxy=>[:person,:description],:index_as=>[:facetable])
+      t.organization(:ref=>:name, :attributes=>{:type=>"corporate"}, :index_as=>[:facetable])
+      t.conference(:ref=>:name, :attributes=>{:type=>"conference"}, :index_as=>[:facetable])
+      t.role {
+        t.text(:path=>"roleTerm",:attributes=>{:type=>"text"})
+        t.code(:path=>"roleTerm",:attributes=>{:type=>"code"})
+>>>>>>> adding t.identifer for HYDRUS-8
       }
       t.journal(:path=>'relatedItem', :attributes=>{:type=>"host"}) {
         t.title_info(:index_as=>[:facetable],:ref=>[:title_info])