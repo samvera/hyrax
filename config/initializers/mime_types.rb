03e5ac4a35a18a202046c3e4208b5474f0948c6c
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2011-05-19 16:36:20 -0500

    hydra mime_types initializers

---
+++ config/initializers/mime_types.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Add new mime types for use in respond_to blocks:
+# Mime::Type.register "text/richtext", :rtf
+# Mime::Type.register_alias "text/html", :iphone
+
+Mime::Type.register "image/jpg", :jpg
+Mime::Type.register "application/pdf", :pdf
+Mime::Type.register "image/jpeg2000", :jp2
+Mime::Type.register "text/html", :html
+Mime::Type.register_alias "text/html", :textile
+Mime::Type.register_alias "text/html", :inline
