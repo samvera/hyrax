3de453fdd2a0c49fb14c2e0503753e8bf0ab4abf
John Scofield <john.scofield@yourmediashelf.com>
2011-06-26 04:30:15 -0400

    tidying a bit

--- lib/hydra-head.rb
+++ lib/hydra-head.rb
@@ -1,4 +1,10 @@
 module HydraHead 
   require 'engine' if defined?(Rails) && Rails::VERSION::MAJOR == 3
   require 'application_controller'
+
+  require 'hydra-head/version'
+
+  def self.version
+    HydraHead::VERSION
+  end
 end
