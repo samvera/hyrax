3c402686f10f3714c2da04baf23f0c6a78845d19
Jessie Keck <jessie.keck@gmail.com>
2011-06-28 16:41:16 -0700

    Loading CSS a new way.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController
   helper :generic_content_objects, :personalization #, :hydrangea_datasets
   
   # helper_method [:request_is_for_user_resource?]#, :user_logged_in?]
-  before_filter [:store_bounce, :set_x_ua_compat]
+  before_filter [:store_bounce, :set_x_ua_compat, :add_custom_css]
   
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
@@ -23,19 +23,19 @@ class ApplicationController
   
   # This really shouldn't be an override.  What we SHOULD be doing is invoking another method in a before filter that will modify the javascript_includes and stylesheet_links arrays.
   # I don't know if there was a particular reason to do it like this so I'm not going to muck w/ it.  The way it is now, if BL adds something, or a file name changes (e.g. jQuery version) this will break.
-  def default_html_head
-    # when working offline, comment out the above uncomment the next line:
-    #javascript_includes << ['jquery-1.4.2.min.js', 'jquery-ui-1.8.1.custom.min.js', { :plugin=>:blacklight } ]
-    javascript_includes << ['http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js', 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.1/jquery-ui.min.js']
-    javascript_includes << ['application', {:plugin=>"hydra-head"}]
-    
-    javascript_includes << ['application', { :plugin=>:blacklight } ]
-    
-    stylesheet_links << ['yui', 'jquery/ui-lightness/jquery-ui-1.8.1.custom.css', 'application', {:plugin=>:blacklight, :media=>'all'}]
-    stylesheet_links << ['redmond/jquery-ui-1.8.5.custom.css', {:plugin=>"hydra-head", :media=>'all'}]      
-    stylesheet_links << ['styles', 'hydrangea', "hydrangea-split-button.css", {:plugin=>"hydra-head", :media=>'all'}]
-    stylesheet_links << ['hydra/styles.css', {:plugin=>"hydra-head", :media=>'all'}]
-  end
+  # def default_html_head
+  #   # when working offline, comment out the above uncomment the next line:
+  #   #javascript_includes << ['jquery-1.4.2.min.js', 'jquery-ui-1.8.1.custom.min.js', { :plugin=>:blacklight } ]
+  #   javascript_includes << ['http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js', 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.1/jquery-ui.min.js']
+  #   javascript_includes << ['application', {:plugin=>"hydra-head"}]
+  #   
+  #   javascript_includes << ['application', { :plugin=>:blacklight } ]
+  #   
+  #   stylesheet_links << ['yui', 'jquery/ui-lightness/jquery-ui-1.8.1.custom.css', 'application', {:plugin=>:blacklight, :media=>'all'}]
+  #   stylesheet_links << ['redmond/jquery-ui-1.8.5.custom.css', {:plugin=>"hydra-head", :media=>'all'}]      
+  #   stylesheet_links << ['styles', 'hydrangea', "hydrangea-split-button.css", {:plugin=>"hydra-head", :media=>'all'}]
+  #   stylesheet_links << ['hydra/styles.css', {:plugin=>"hydra-head", :media=>'all'}]
+  # end
       
   def current_user
     return @current_user if defined?(@current_user)
@@ -52,4 +52,9 @@ class ApplicationController
     # Always force latest IE rendering engine & Chrome Frame through header information.
     response.headers["X-UA-Compatible"] = "IE=edge,chrome=1"
   end
+  
+  def add_custom_css
+    stylesheet_links << ["html_refactor", {:plugin=>"hydra-head", :media=>"all"}]
+  end
+  
 end
