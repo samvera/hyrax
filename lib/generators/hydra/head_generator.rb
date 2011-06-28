1b0d0999f771ef75ad45bec27507ed49b7e8a799
John Scofield <john.scofield@yourmediashelf.com>
2011-06-28 00:20:11 -0400

    tweaking next_migration_number

--- lib/generators/hydra/head_generator.rb
+++ lib/generators/hydra/head_generator.rb
@@ -68,11 +68,16 @@ EOF
   # Implement the required interface for Rails::Generators::Migration.
   # taken from http://github.com/rails/rails/blob/master/activerecord/lib/generators/active_record.rb
   def self.next_migration_number(dirname)
-    if ActiveRecord::Base.timestamped_migrations
-      Time.now.utc.strftime("%Y%m%d%H%M%S")
+    unless @previous_migration_nr
+      if ActiveRecord::Base.timestamped_migrations
+        @previous_migration_nr = Time.now.utc.strftime("%Y%m%d%H%M%S").to_i
+      else
+        @previous_migration_nr = "%.3d" % (current_migration_number(dirname) + 1).to_i
+      end
     else
-      "%.3d" % (current_migration_number(dirname) + 1)
+      @previous_migration_nr +=1 
     end
+    @previous_migration_nr.to_s
   end
 
   def create_migration_file
