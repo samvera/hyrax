ff71ede419907a2862ed91c53f666a35eee8079a
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2011-06-30 18:50:26 -0500

    HYDRA-548: another placeholder for generator tests

--- test_support/spec/generators/hydra-head_generator_spec.rb
+++ test_support/spec/generators/hydra-head_generator_spec.rb
@@ -6,6 +6,9 @@ require "generators/blacklight/blacklight_generator"
 describe Hydra::HeadGenerator do
 
   it "should be tested, but it's not clear how..."
+  it "should inject Hydra behaviors into User model by adding include Hydra::User" do
+    pending "Not sure how to test this..."
+  end
 
 end
 
