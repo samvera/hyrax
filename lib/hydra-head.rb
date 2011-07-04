6ead36b1fb5c4762ce89f1cb4dcbbc6213dab96c
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2011-07-04 13:39:44 -0500

    HYDRA-532: added catalog routes; set up HydraHead.add_routes & set generator to add it to routes.rb

--- lib/hydra-head.rb
+++ lib/hydra-head.rb
@@ -3,13 +3,27 @@ module HydraHead
   require 'application_controller'
 
   require 'hydra-head/version'
-
+  require 'hydra-head/routes'
+  
   def self.version
     HydraHead::VERSION
   end
 
   def self.root
     @root ||= File.expand_path(File.dirname(File.dirname(__FILE__)))
+  end
+  
+  # If you put this in your application's routes.rb, it will add the Hydra Head routes to the app.
+  # The hydra:head generator puts this in routes.rb for you by default.
+  # See {HydraHead::Routes} for information about how to modify which routes are generated.
+  # @example 
+  #   # in config/routes.rb
+  #   MyAppName::Application.routes.draw do
+  #     Blacklight.add_routes(self)
+  #     HydraHead.add_routes(self)
+  #   end
+  def self.add_routes(router, options = {})
+    HydraHead::Routes.new(router, options).draw
   end
   
 end
---
+++ lib/hydra-head/routes.rb
@@ -0,0 +1,47 @@
+# -*- encoding : utf-8 -*-
+module HydraHead
+  class Routes
+
+    def initialize(router, options)
+      @router = router
+      @options = options
+    end
+
+    def draw
+      route_sets.each do |r|
+        self.send(r)
+      end
+    end
+
+    protected
+
+    def add_routes &blk
+      @router.instance_exec(@options, &blk)
+    end
+
+    def route_sets
+      (@options[:only] || default_route_sets) - (@options[:except] || [])
+    end
+
+    def default_route_sets
+      [:catalog, :assets]
+    end
+    
+    module RouteSets
+      def catalog
+        add_routes do |options|
+          resources :catalog, :only => [:edit, :destroy]
+        end
+      end
+      
+      def assets
+        add_routes do |options|
+          resources :assets
+        end
+      end
+      
+    end
+    include RouteSets
+    
+  end
+end
