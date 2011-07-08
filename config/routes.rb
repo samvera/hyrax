9fb8d9dfba0ef5d7f8455c98512fe2383a55aacc
John Scofield <john.scofield@yourmediashelf.com>
2011-07-08 09:33:51 -0400

    HYDRA-558: moving routes to lib/hydra-head/routes.rb
    Adds functionality for configuring routes in the host
    application.
    
    https://jira.duraspace.org/browse/HYDRA-558

--- config/routes.rb
+++ config/routes.rb
@@ -1,50 +1,12 @@
-Rails.application.routes.draw do |map|
+Rails.application.routes.draw do
 
-  mount_at = HydraHead::Engine.config.mount_at
-
-  match mount_at => 'hydra_head/catalog#index'
-
-  resources :catalog, :only => [ :index, :show ],
-                          :controller => "hydra_head/catalog",
-                          :path_prefix => mount_at,
-                          :as => "hydra_head_"
-
-  resources :file_assets
-          
-   # This creates routes named asset_file_assets, new_asset_file_asset, etc.
-   # The routes map to the :file_assets controller with the param :container_id 
-   # which is then used within the controller to deal with parent - child relationships 
-   # between Assets and the FileAssets inside them.
-   
-
-# resources :file_assets, :path_prefix => '/assets/:container_id', :name_prefix => "asset_"
-   
-  resources :assets do
-    resources :downloads, :only=>[:index]
-    resources :contributors, :only=>[:new,:create]
-    resources :grants, :only=>[:new,:create]       
-    resources :permissions
-    resources :file_assets
-  end
-
-  match 'catalog/:id/edit', :to => 'catalog#edit', :as => 'edit_catalog'
-  match 'catalog/:id/delete', :to => 'catalog#delete', :as => 'delete_catalog'
-  match 'about', :to => 'catalog#about', :as => 'about'
-  
-  resources :user_sessions
-  match 'logged_out', :to => 'user_sessions#logged_out', :as => 'logged_out'
-  match 'superuser', :to => 'user_sessions#superuser', :as => 'superuser'
-  
-  resources :get, :only=>:show 
-  
-  # this is to remove documents from SOLR but not from Fedora.
-  match "withdraw", :to => "assets#withdraw", :as => "withdraw" 
-  
-  match 'assets/:asset_id/contributors/:contributor_type/:index', :to => 'contributors#show', :as => 'asset_contributor', :conditions => { :method => :get }
-  match 'assets/:asset_id/contributors/:contributor_type/:index', :to => 'contributors#destroy', :as => 'connect',  :conditions => { :method => :delete }
-  
-  # Allow updates to assets/:asset_id/permissions (no :id necessary)
-  match 'assets/:asset_id/permissions', :to => 'permissions#update', :as => 'update_group_permissions'
-  
-  match 'generic_contents_object/content/:container_id', :to => 'generic_content_objects#create', :as => 'generic_content_object', :conditions => {:method => :post}
+  # Routes for the HydraHead application are defined in HydraHead::Routes
+  #
+  # These routes can be injected into your Rails application by adding
+  #     HydraHead.add_routes(self)
+  # to the application's ./config/routes.rb. The injected routes can be 
+  # customized as well, e.g.:
+  #     HydraHead.add_routes(self, :only => [:file_assets]) # will only load file_asset routes
+  #     HydraHead.add_routes(self, :except => [:catalog]) # will not load catalog routes
 end
+
