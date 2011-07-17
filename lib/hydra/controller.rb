2ac5803558132848bbe327d205b528f6dd49e2f5
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2011-07-17 17:03:10 -0500

    more cucumber coverage; tweak to view helper loading

--- lib/hydra/controller.rb
+++ lib/hydra/controller.rb
@@ -1,5 +1,7 @@
+# Adds behaviors that Hydra needs all controllers to have. (mostly view helpers)
 module Hydra::Controller
   def self.included(base)
     base.helper :hydra_assets
+    base.helper :hydra_fedora_metadata
   end
 end
