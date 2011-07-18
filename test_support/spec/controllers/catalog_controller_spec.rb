cc0ef857d05fac1297b5178f5be1faad931b7a30
Justin Coyne <jcoyne@justincoyne.com>
2011-07-18 15:37:35 -0500

    Added pending items

--- test_support/spec/controllers/catalog_controller_spec.rb
+++ test_support/spec/controllers/catalog_controller_spec.rb
@@ -40,9 +40,20 @@ describe CatalogController do
   end
   
   it "should not choke on objects with periods in ids (ie Fedora system objects)" do    
-    pending "this would require a patch to all routes that allows periods in ids. for now, use rake solrizer:fedora:forget_system_objects"
+     pending "Need to override blacklight routes"	
+
+	## We could do something like this to remove the catalog/show route and replace it with a route that allows dots (e.g. resources :catalog, :id=> /.+/)
+	  # def add_route
+	  #   new_route = ActionController::Routing::Routes.builder.build(name, route_options)
+	  #   ActionController::Routing::Routes.routes.insert(0, new_route)
+	  # end
+
+	  # def remove_route
+	  #   ActionController::Routing::Routes.routes.reject! { |r| r.instance_variable_get(:@requirements)[:slug_id] == id }
+	  # end
+
     catalog_path("fedora-system:FedoraObject-3.0").should == '/catalog/fedora-system:FedoraObject-3.0'
-    route_for(:controller=>"catalog", :action=>"show", :id=>"fedora-system:FedoraObject-3.0").should == '/catalog/fedora-system:FedoraObject-3.0'
+    { :get => "/catalog/fedora-system:FedoraObject-3.0" }.should route_to(:controller => 'catalog', :action => 'show', :id=>'fedora-system:FedoraObject-3.0')
   end
   
   describe "index" do
@@ -54,19 +65,16 @@ describe CatalogController do
       end
 
       it "should only return public documents if role does not have permissions" do
-        pending("FIXME")
-        request.env["WEBAUTH_USER"]="Mr. Notallowed"
+        controller.stubs(:current_user).returns(nil)
         get :index
         assigns("response").docs.count.should == @public_only_results.docs.count
       end
       it "should return all documents if role does have permissions" do
-        pending("adjusted for superuser, but assertions aren't working as with test above")
-        mock_user = mock("User", :login=>"BigWig")
+	User.any_instance.stubs(:login).returns("BigWig")
+        mock_user =  User.new
         session[:superuser_mode] = true
         controller.stubs(:current_user).returns(mock_user)
         get :index
-        # assigns["response"].docs.should include(@public_only_results.first)
-        # assigns["response"].docs.should include(@private_only_results.first)
         assigns["response"].docs.count.should > @public_only_results.docs.count
       end
     end
