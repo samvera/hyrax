b69f7e4daa76d41effdaff4033933079c88a5947
Jessie Keck <jessie.keck@gmail.com>
2011-08-12 17:54:37 -0700

    Copying some part of partials to new partials that are more in line w/ how the workflow steps are divided.

--- lib/hydra/submission_workflow.rb
+++ lib/hydra/submission_workflow.rb
@@ -92,7 +92,7 @@ module Hydra::SubmissionWorkflow
       :mods_assets =>      [{:name => "contributor",     :edit_partial => "contributors/contributor_form",    :show_partial => "mods_assets/show_contributors"},
                             {:name => "publication",     :edit_partial => "mods_assets/publication_form",     :show_partial => "mods_assets/show_publication"},
                             {:name => "additional_info", :edit_partial => "mods_assets/additional_info_form", :show_partial => "mods_assets/show_additional_info"},
-                            {:name => "files",           :edit_partial => "file_assets/file_assets_form",     :show_partial => "files_assets/index"},
+                            {:name => "files",           :edit_partial => "file_assets/file_assets_form",     :show_partial => "mods_assets/show_file_assets"},
                             {:name => "permissions",     :edit_partial => "permissions/permissions_form",     :show_partial => "mods_assets/show_permissions"}
                            ],
       :generic_contents => [{:name => "description", :edit_partial => "generic_content_objects/description_form", :show_partial => "generic_contents/show_description"},
