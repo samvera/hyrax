817184bd7196e7d272b4793167b02ff24e3681b7
Jessie Keck <jessie.keck@gmail.com>
2011-08-14 15:15:37 -0700

    Adding in some comments above methods.

--- lib/hydra/submission_workflow.rb
+++ lib/hydra/submission_workflow.rb
@@ -1,8 +1,11 @@
 module Hydra::SubmissionWorkflow
+  
+  # When in this module is incuded in a controller (e.g. responds to :before_filter) add the validate_worflow_step method to the before filter chain.
   def self.included(base)
     base.before_filter :validate_workflow_step if base.respond_to?(:before_filter)
   end
   
+  # To be used in a before_filter.  This will call a method with the #{af_model}_#{action_param}_validation.  If that method returns false then we will redirect back.  The controller method doing the validation should set an appropriate flash message.
   def validate_workflow_step
     unless model_config.nil?
       # we may want to use the workflow name instead of or in addition to the action in the validation naming convention.
@@ -12,7 +15,8 @@ module Hydra::SubmissionWorkflow
       end
     end
   end
-  
+
+  # Returns the name field for the next step in the configuration for the current model given the current step.
   def next_step_in_workflow(current_step)
     unless model_config.nil?
       if current_step.blank?
@@ -27,18 +31,22 @@ module Hydra::SubmissionWorkflow
     nil
   end
   
+  # Convenience method to return the first step of a models workflow.
   def first_step_in_workflow
     model_config.first[:name]
   end
   
+  # Convenience method to return the partial for any given step by name.
   def workflow_partial_for_step(step)
     find_workflow_step_by_name(step)[:edit_partial]
   end
   
+  # Convenience method to return an entire workflow step by name.
   def find_workflow_step_by_name(name)
     model_config.find{|config| config[:name] == name.to_s} unless model_config.nil?
   end
   
+  # Returns an array of display partials for steps previous to the given step.
   def previous_show_partials(current_step)
     previous_partials = []
     # if there is no step then we are on the first step of the workflow and don't need to display anything.
@@ -52,6 +60,9 @@ module Hydra::SubmissionWorkflow
     previous_partials
   end
   
+  # Will return the entire workflow configuration for the current model.
+  # We determing model first by seeing the @document object is a SolrDocument.  If it is we will determing from the has_model_s field.
+  # Otherwise we will attemtp to determine by the parameters (content_type directly passed or the id of an object).
   def model_config
     # If we  can get it directly from solr get it there.
     if !@document.nil? and @document.is_a?(SolrDocument)
@@ -68,6 +79,7 @@ module Hydra::SubmissionWorkflow
     nil
   end
   
+  # Reutrns a symbolized model name determined by parameters.
   def get_af_model_from_params
     if params.has_key?(:content_type)
       return params[:content_type].pluralize.to_sym
@@ -81,12 +93,12 @@ module Hydra::SubmissionWorkflow
     end
   end
   
+  # Convenience method to return the model from the @document objects has_model_s field.
   def get_af_model_from_solr
     @document[:has_model_s].first.gsub("info:fedora/afmodel:","").underscore.pluralize.to_sym
   end
 
-
-
+  # The configuration hash.  This should probably live somewhere else and get read in so it can be properly configured at the application level.  But for now it's here.
   def workflow_config
     {
       :mods_assets =>      [{:name => "contributor",     :edit_partial => "contributors/contributor_form",    :show_partial => "mods_assets/show_contributors"},
