8deab769e53ac3b44baf5b2c7ff6e762c28947e7
Michael J. Giarlo <leftwing@alumni.rutgers.edu>
2011-12-13 17:06:39 -0500

    refs #525: adds empty rails app, initialized by rails generator

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hylion::Application.config.secret_token = '5da0d4ecae27adcb446fc023f6fac682de592172e833f43ae4cea53dc20ac1d438a80b9e8bd654e2a0613a0305b62d217020d95a6d2db1417982c3702f927bd3'
