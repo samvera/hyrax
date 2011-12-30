a69bf7f2f11805a7fbca1b79a45621a7e4ab7c8a
Michael J. Giarlo <leftwing@alumni.rutgers.edu>
2011-12-29 22:56:59 -0500

    adds bootstrap to css in the BL way, changes app name in the BL way, wipes out unused application.html.erb, adds BL helper for overrides

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,28 @@
 class ApplicationController < ActionController::Base
   # Adds a few additional behaviors into the application controller 
-   include Blacklight::Controller  
-# Adds Hydra behaviors into the application controller 
+  include Blacklight::Controller  
+  # Adds Hydra behaviors into the application controller 
   include Hydra::Controller
+
   def layout_name
    'hydra-head'
+  end
+
+  ApplicationController.before_filter do |controller|
+    # remove default jquery-ui theme.
+    # controller.stylesheet_links.each do |args|
+    #   args.delete_if {|a| a =~
+    #   /^|\/jquery-ui-[\d.]+\.custom\.css$/ }
+    # end
+ 
+    # add in a different jquery-ui theme, or any other css or what
+    #     have you
+    controller.stylesheet_links << 'bootstrap.min.css'
+
+    #     controller.javascript_includes << "my_local_behaviors.js"
+
+    #     controller.extra_head_content << '<link rel="something"
+    #     href="something">'
   end
 
   # Please be sure to impelement current_user and user_session. Blacklight depends on 
