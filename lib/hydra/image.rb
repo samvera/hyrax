4b79c09ab393201cf37ab74bdc819e09bfc557eb
Justin Coyne <justin.coyne@yourmediashelf.com>
2012-01-09 14:00:20 -0600

    Deprecate image. Bump version

--- lib/hydra/image.rb
+++ lib/hydra/image.rb
@@ -28,8 +28,6 @@ require "httparty"
 
 module Hydra
 class Image < ActiveFedora::Base
-=begin
-  TODO This doesn't seem to be used anywhere. Commenting out until I hear back. Justin 2011-12-21
   include Hydra::ModelMethods
   include HTTParty
 
@@ -176,6 +174,5 @@ class Image < ActiveFedora::Base
     "#{admin_site}fedora/objects/#{pid}/datastreams/#{ds_name}/content"
   end
 
-=end
 end
 end
