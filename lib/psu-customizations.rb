e1631ea448013f23f26826c46c3f5c2e2d25e839
Michael J. Giarlo <leftwing@alumni.rutgers.edu>
2012-01-25 17:01:31 -0500

    refs #555, tweaks af monkeypatch to bring it in line with latest from dor-services

--- lib/psu-customizations.rb
+++ lib/psu-customizations.rb
@@ -5,14 +5,9 @@ module ActiveFedora
   class Base
     def initialize(attrs={})
       unless attrs[:pid]
-        attrs = attrs.merge!({:pid => PSU::IdService.mint})
-        @new_object = true
-      else
-        @new_object = not attrs[:new_object] == false
+        attrs = attrs.merge!({:pid => PSU::IdService.mint, :new_object => true})
       end
-      @inner_object = Fedora::FedoraObject.new(attrs)
-      @datastreams = {}
-      configure_defined_datastreams
+      super
     end  
   end
 end
