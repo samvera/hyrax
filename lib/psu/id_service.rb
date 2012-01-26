56757b8cb8bd0f1c45edea01de9cc3c4b4a8c715
Michael J. Giarlo <leftwing@alumni.rutgers.edu>
2012-01-26 16:13:16 -0500

    moves identifier validation out of spec test and into identifier service

--- lib/psu/id_service.rb
+++ lib/psu/id_service.rb
@@ -1,5 +1,9 @@
 module PSU
   class IdService    
+    @@xdigits = Noid::XDIGIT.join
+    def self.valid?(identifier)
+      identifier =~ /^id:[#@@xdigits]{2}\d{2}[#@@xdigits]{2}\d{2}[#@@xdigits]$/
+    end
     def self.mint
       minter = Noid::Minter.new(:template => '.reeddeeddk')
       return "id:#{minter.mint}"
