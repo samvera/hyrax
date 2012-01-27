c63a12d1610edd51ed9372f5c9fa48aa4f4e95da
Michael J. Giarlo <leftwing@alumni.rutgers.edu>
2012-01-27 13:18:01 -0500

    refs #585, implements rdf/nt-based dublin core datastreams for generic file, tweaks test

--- app/models/generic_file.rb
+++ app/models/generic_file.rb
@@ -5,12 +5,22 @@ class GenericFile < ActiveFedora::Base
   include Hydra::ModelMethods
 
   has_metadata :name => "characterization", :type => FitsDatastream
-  has_metadata :name => "descMetadata", :type => Psu::DcDatastream
+  has_metadata :name => "descMetadata", :type => ActiveFedora::DCRDFDatastream
   has_file_datastream :type => FileContentDatastream
 
   delegate :contributor, :to => :descMetadata
   delegate :creator, :to => :descMetadata
   delegate :title, :to => :descMetadata
+  delegate :description, :to => :descMetadata
+  delegate :publisher, :to => :descMetadata
+  delegate :created, :to => :descMetadata
+  delegate :subject, :to => :descMetadata
+  delegate :language, :to => :descMetadata
+  delegate :date, :to => :descMetadata
+  delegate :rights, :to => :descMetadata
+  delegate :type, :to => :descMetadata
+  delegate :format, :to => :descMetadata
+  delegate :identifier, :to => :descMetadata
 
   before_save :characterize
 
