9764dc6a754bfe00c591137e2be62d4f3edfb7f5
Dan Coughlin <danny@psu.edu>
2012-01-27 12:59:18 -0500

    working on getting multiple file uploads working.  currently unable to get rights assignement properly saved.

--- app/controllers/generic_files_controller.rb
+++ app/controllers/generic_files_controller.rb
@@ -2,6 +2,7 @@ class GenericFilesController < ApplicationController
   
   include Hydra::Controller
   include Hydra::AssetsControllerHelper  # This is to get apply_depositor_metadata method
+  include Hydra::FileAssetsHelper
 
   before_filter :authenticate_user!
   before_filter :enforce_access_controls, :only=>[:edit, :update]
@@ -11,16 +12,14 @@ class GenericFilesController < ApplicationController
   end
 
   def create
-    @generic_file = GenericFile.new(params[:generic_file].reject {|k,v| k=="Filedata" || k=="Filename"})
-    apply_depositor_metadata(@generic_file)
     
-    if (@generic_file.save)
-      flash[:success] = "You saved #{@generic_file.title}"
-      redirect_to :action=>"edit", :id=>@generic_file.pid
-    else 
-      flash[:error] = "Unable to save."
-      render :action=>"new"
+    @generic_files = create_and_save_generic_files_from_params
+    notice = []
+    @generic_files.each do |gf|
+      notice << render_to_string(:partial=>'generic_files/asset_saved_flash', :locals => { :generic_file => gf })
     end
+    flash[:notice] = notice.join("<br/>".html_safe) unless notice.blank?
+    redirect_to catalog_index_path
   end
   
   def edit
@@ -37,6 +36,26 @@ class GenericFilesController < ApplicationController
   end
   
   protected
+  # takes form file inputs and assigns meta data individually 
+  # to each generic file asset and saves generic file assets # @param [Hash] of form fields
+  def create_and_save_generic_files_from_params
+    
+    if params.has_key?(:Filedata)
+      @generic_files = []
+      params[:Filedata].each do |file|
+        generic_file = GenericFile.new(params[:generic_file].reject {|k,v| k=="Filedata" || k=="Filename"})
+        
+        add_posted_blob_to_asset(generic_file,file)
+        generic_file.label = file.original_filename
+        generic_file.save
+        @generic_files << generic_file
+      end
+      return @generic_files
+    else
+      render :text => "400 Bad Request", :status => 400
+    end
+  end
+
   def process_files
     @file_assets = create_and_save_file_assets_from_params
     notice = []
