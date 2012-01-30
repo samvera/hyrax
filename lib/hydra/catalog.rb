8a5500f772c588a9ca2705cd4d549eac33ee1056
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2012-01-30 15:12:24 -0600

    HYDRA-738 removing stuff from Hydra::Catalog that was moved to Hydra::Controller

--- lib/hydra/catalog.rb
+++ lib/hydra/catalog.rb
@@ -17,20 +17,15 @@ module Hydra::Catalog
   
   def self.included(klass)
     # Other modules to auto-include
-    klass.send(:include, Hydra::AccessControlsEnforcement)
-    klass.send(:include, MediaShelf::ActiveFedoraHelper)
-    klass.send(:include, Hydra::RepositoryController)
+    klass.send(:include, Hydra::Controller)
     klass.send(:include, Hydra::UI::Controller)
     
     
     # Controller filters
     # Also see the generator (or generated CatalogController) to see more before_filters in action
-    klass.before_filter :require_solr, :check_scripts
     klass.before_filter :load_fedora_document, :only=>[:show,:edit]
     
     # View Helpers
-    klass.helper :hydra
-    klass.helper :hydra_assets
     klass.helper :hydra_uploader
     klass.helper :article_metadata
   end
@@ -59,9 +54,5 @@ module Hydra::Catalog
     @document_fedora = af_base.adapt_to(the_model)
     @file_assets = @document_fedora.file_objects(:response_format=>:solr)
   end
-  
-  
-  def check_scripts
-    session[:scripts] ||= (params[:combined] and params[:combined] == "true")
-  end
+
 end
