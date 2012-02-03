4df97204b22162741c1d6d8cad082b89c86f42c9
Michael J. Giarlo <leftwing@alumni.rutgers.edu>
2012-02-03 13:19:12 -0500

    refs #592, sets up preconditions for FITS tests

--- spec/models/fits_datastream_spec.rb
+++ spec/models/fits_datastream_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 
 describe FitsDatastream do
-    it "should read the fits meta data fields" 
-
+  before do
+    @subject = FitsDatastream.new(nil, 'characterization')
+    @subject.stubs(:pid=>'my_pid')
+    @subject.stubs(:dsVersionID=>'characterization.3')
+  end
+  it "should read the fits meta data fields"
 end
