a2284692b2b1949a04e7b354463dcbccbbd76b5e
Justin Coyne <justin.coyne@yourmediashelf.com>
2012-02-07 13:35:26 -0600

    Tests running -- but failing

--- lib/application_controller.rb
+++ lib/application_controller.rb
@@ -8,18 +8,17 @@ module HydraHead
       before_filter :method_for_before_filtering
     end
 
-    module InstanceMethods
-      def method_for_before_filtering
-        #puts "Filtering before" 
-      end
+    def method_for_before_filtering
+      #puts "Filtering before" 
+    end
 
-      def method_not_a_filter
-        puts "not used as a filter"
-      end
+    def method_not_a_filter
+      puts "not used as a filter"
     end
   end
 end
 
+#TODO this seems bad
 ::ActionController::Base.send :include, HydraHead::Controller
 
 
