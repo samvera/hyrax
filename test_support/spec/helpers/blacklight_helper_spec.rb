e9a1f11abc74ce2d4d947f45364bf817c1fe9591
Chris Beer <chris_beer@wgbh.org>
2012-02-14 17:35:21 -0500

    defer to blacklight link_to_document method (from blacklight master\!\!)

--- test_support/spec/helpers/blacklight_helper_spec.rb
+++ test_support/spec/helpers/blacklight_helper_spec.rb
@@ -37,18 +37,6 @@ describe BlacklightHelper do
         helper.render_head_content.should == "Something extraMy added content"
       end
     end
-    describe "link_to_document" do
-      before(:each)do
-        @mock_doc = mock('mock doc')
-        @mock_doc.expects(:[]).with(:id).returns("123456")
-      end
-      it "passes on the title attribute to the link_to_with_data method" do
-        helper.link_to_document(@mock_doc,:label=>"Some crazy long label...",:title=>"Some crazy longer label").should match(/title=\"Some crazy longer label\"/)
-      end
-      it "doesn't add an erroneous title attribute if one isn't provided" do
-        helper.link_to_document(@mock_doc,:label=>"Some crazy long label...").should_not match(/title=/)
-      end
-    end
     
     describe "link back to catalog" do
       it "should return the view parameter in the link back to catalog method if there is one in the users previous search session" do
