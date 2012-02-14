0c8b89ce4e3833126ae8ca24200cfc6c61e8c566
Justin Coyne <justin.coyne@yourmediashelf.com>
2012-02-14 16:05:52 -0600

    Fixed so tests compile

--- test_support/spec/controllers/contributors_controller_spec.rb
+++ test_support/spec/controllers/contributors_controller_spec.rb
@@ -8,7 +8,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 # or
 # rake cucumber
 
-describe ContributorsController do
+describe Hydra::ContributorsController do
   
   describe "create" do
     it "should support adding new person / contributor / organization nodes" do
