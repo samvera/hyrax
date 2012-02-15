f6ac2fbbbce48909d17a0a76b20d6905b30b67aa
Chris Beer <chris_beer@wgbh.org>
2012-02-15 13:05:29 -0500

    update blacklight helper specs

--- test_support/spec/helpers/blacklight_helper_spec.rb
+++ test_support/spec/helpers/blacklight_helper_spec.rb
@@ -13,7 +13,12 @@ describe BlacklightHelper do
   
   describe "Overridden blacklight methods" do
     describe "document_partial_name" do
+
       it "Should lop off everything before the first colin after the slash" do
+    @config = Blacklight::Configuration.new.configure do |config|
+      config.show.display_type = 'has_model_s'
+    end
+        helper.stubs(:blacklight_config).returns(@config)
         helper.document_partial_name('has_model_s' => ["info:fedora/afmodel:Presentation"]).should == "presentations"
         helper.document_partial_name('has_model_s' => ["info:fedora/hull-cModel:genericContent"]).should == "generic_contents" 
       end
@@ -23,7 +28,7 @@ describe BlacklightHelper do
         item = stub("item", :value=>'two', :hits=>9)
 
         ret_val = helper.render_selected_facet_value("one", item)
-        ret_val.should == "<span class=\"selected\">two (9)</span>"
+        ret_val.should == "<span class=\"selected label\">two <span class=\"count\">(9)</span></span>"
         ret_val.should be_html_safe
       end
     end
