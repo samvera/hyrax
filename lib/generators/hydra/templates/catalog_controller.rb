1e2c4b70c81da5a050614931feb8a66b992196ce
Justin Coyne <justin.coyne@yourmediashelf.com>
2012-02-16 22:37:34 -0600

    Added correct field for type facet

--- lib/generators/hydra/templates/catalog_controller.rb
+++ lib/generators/hydra/templates/catalog_controller.rb
@@ -39,7 +39,7 @@ class CatalogController < ApplicationController
     #
     # :show may be set to false if you don't want the facet to be drawn in the 
     # facet bar
-    config.add_facet_field 'format', :label => 'Format' 
+    config.add_facet_field 'object_type_facet', :label => 'Format' 
     config.add_facet_field 'pub_date', :label => 'Publication Year' 
     config.add_facet_field 'subject_topic_facet', :label => 'Topic', :limit => 20 
     config.add_facet_field 'language_facet', :label => 'Language', :limit => true 
