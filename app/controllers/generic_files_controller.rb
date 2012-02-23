dfe25cfc477e5fbd619149b2f34742716d2d0509
Dan Coughlin <danny@psu.edu>
2012-02-23 16:56:11 -0500

    got first four cucumber scenarios working and interface for those four scenarios refs #594

--- app/controllers/generic_files_controller.rb
+++ app/controllers/generic_files_controller.rb
@@ -11,19 +11,25 @@ class GenericFilesController < ApplicationController
     @generic_file = GenericFile.new 
   end
 
+  def edit
+    @generic_file = GenericFile.find(params[:id])
+  end
+
   def create
     
     @generic_files = create_and_save_generic_files_from_params
-    notice = []
-    @generic_files.each do |gf|
-      notice << render_to_string(:partial=>'generic_files/asset_saved_flash', :locals => { :generic_file => gf })
+    if @generic_files.empty? 
+      flash[:notice] = "You must specify a file to upload" 
+      redirect_params = {:controller => "generic_files", :action => "new"} 
+    else
+      notice = []
+      @generic_files.each do |gf|
+        notice << render_to_string(:partial=>'generic_files/asset_saved_flash', :locals => { :generic_file => gf })
+      end
+      flash[:notice] = notice.join("<br/>".html_safe) unless notice.blank?
+      redirect_params = {:controller => "catalog", :action => "index"} 
     end
-    flash[:notice] = notice.join("<br/>".html_safe) unless notice.blank?
-    redirect_to catalog_index_path
-  end
-  
-  def edit
-    @generic_file = GenericFile.find(params[:id])
+    redirect_to redirect_params 
   end
 
   def show
@@ -44,28 +50,27 @@ class GenericFilesController < ApplicationController
     end 
     render :edit 
   end
-  
+
   def edit
     @generic_file = GenericFile.find(params[:id])
   end
 
   def show
-    @generic_file = GenericFile.find(params[:id])
+    @generic_file = GenericFile.find(params[:id]) 
   end
 
   def audit
     @generic_file = GenericFile.find(params[:id])
     render :json=>@generic_file.content.audit
   end
- 
 
 
   protected
   # takes form file inputs and assigns meta data individually 
   # to each generic file asset and saves generic file assets # @param [Hash] of form fields
   def create_and_save_generic_files_from_params
+    @generic_files = []
     if params.has_key?(:Filedata)
-      @generic_files = []
       params[:Filedata].each do |file|
         generic_file = GenericFile.new(params[:generic_file].reject {|k,v| k=="Filedata" || k=="Filename"})
         
@@ -73,14 +78,11 @@ class GenericFilesController < ApplicationController
         apply_depositor_metadata(generic_file)
         generic_file.label = file.original_filename
         # Delete this next line when GenericFile.label no longer wipes out the title
-        if params[:generic_file].has_key?(:title) then generic_file.title = params[:generic_file][:title] end
+        generic_file.title = params[:generic_file][:title] if params[:generic_file].has_key?(:title) 
         generic_file.save
         @generic_files << generic_file
       end
-      return @generic_files
-    else
-      render :text => "400 Bad Request", :status => 400
     end
+    @generic_files
   end
-
 end
--- features/ingest_upload_files.feature
+++ features/ingest_upload_files.feature
@@ -15,15 +15,34 @@ Feature: Uploading files via web form
   Scenario: Upload a file, no metadata
     Given I am logged in as "contentauthor@psu.edu"
     When I am on the "ingest" page 
-    And I attach the file "test_support/fixtures/image.jp2" to "generic_file_Filedata[]"
+    And I attach the file "test_support/fixtures/image.jp2" to "Filedata[]"
     And I press "Upload"
-#    Then I should see "The file world.png has been saved"
-#
-#  Scenario: Upload a file, with metadata
-#  Scenario: Submit metadata, no file
+    Then I should see "The file image.jp2 has been saved"
+
+  Scenario: Upload a file, with metadata
+    Given I am logged in as "contentauthor@psu.edu"
+    When I am on the "ingest" page 
+    And I attach the file "test_support/fixtures/image.jp2" to "Filedata[]"
+    And I fill in "Mike Motorcycle" for "generic_file_contributor" 
+    And I fill in "Dan Ran" for "generic_file_creator"
+    And I fill in "Dan's Book" for "generic_file_title"
+    #And I select "" from 
+    And I press "Upload"
+    Then I should see "The file image.jp2 has been saved"
+
+  Scenario: Submit metadata, no file
+    Given I am logged in as "contentauthor@psu.edu"
+    When I am on the "ingest" page 
+    And I fill in "Mike Motorcycle" for "generic_file_contributor" 
+    And I fill in "Dan Ran" for "generic_file_creator"
+    And I fill in "My Title!" for "generic_file_title"
+    And I press "Upload"
+    Then I should see "You must specify a file to upload"
+   
+#  difference between add and upload is that add
+#  just tests the button to display more fields
+#  upload tests the submission of the data 
 #  Scenario: Add more files
 #  Scenario: Upload multiple files 
-#  Scenario: After upload I see
-#  Scenario: Fill in metadata
 #  Scenario: Add More metadata
-#  Scenario: Click selectbox on metadata fields.
+#  Scenario: Upload More metadata
