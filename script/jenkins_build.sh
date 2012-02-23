88265964991376e093dd620f140b62fb3a26e3d6
Jeff Minelli <jgm106@psu.edu>
2012-02-23 13:39:17 -0500

    missed a line..

--- script/jenkins_build.sh
+++ script/jenkins_build.sh
@@ -9,7 +9,8 @@
 echo "=-=-=-=-= $0 begin"
 cd ${JENKINS_HOME}/jobs/gamma/workspace
 
-echo "=-=-=-=-= $0 source .rvmrc"
+echo "=-=-=-=-= $0 source rvm.sh/.rvmrc"
+source /etc/profile.d/rvm.sh
 source .rvmrc
 
 echo "=-=-=-=-= $0 bundle install"
