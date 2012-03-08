99d63eda12f3e3bce28c833c16b0a44dbbb6b5db
Michael J. Giarlo <leftwing@alumni.rutgers.edu>
2012-03-08 09:04:00 -0500

    refs #594, adjusts file paths in upload feature to be relative, modifies step definition to handle relative paths more gracefully

--- features/step_definitions/gamma.rb
+++ features/step_definitions/gamma.rb
@@ -1,3 +1,3 @@
 When /^I attach a file "([^"]*)" to the dynamically created "([^"]*)"$/ do |path, field|
-  find(field).native.send_keys(File.expand_path(path, __FILE__))
+  find(field).native.send_keys(File.expand_path(path, Rails.root))
 end
