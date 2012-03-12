9765ff9b0f82774394974f57e2f8c21efa49e3de
Chris Beer <chris_beer@wgbh.org>
2012-03-12 17:35:00 -0400

    move monkeypatch to spec/support

---
+++ spec/support/uploaded_file_monkeypatch.rb
@@ -0,0 +1,6 @@
+#Monkey patch UploadedFile so that it responds to read (same as ActionDispatch::Http::UploadedFile). Required by RestClient when posting to fedora.
+class Rack::Test::UploadedFile
+  def read(*args)
+    @tempfile.read(*args)
+  end
+end
