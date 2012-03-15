12273c608fbb28ed49ff7dab3e055e8c9d9bcdbf
Michael J. Giarlo <leftwing@alumni.rutgers.edu>
2012-03-15 14:56:09 -0400

    refs #632, moves headless cucumber stuff into its own file since env.rb is system-generated, wraps headless mode in a block that tests the value of HEADLESS env var, adds HEADLESS env var to invocation of gamma:ci task in jenkins build script

---
+++ features/support/headless.rb
@@ -0,0 +1,12 @@
+# Headless for Jenkins CI builds
+Before("@javascript") do
+  if ENV['HEADLESS'] == 'true'
+    Capybara.current_driver = :selenium
+    require 'headless'   
+    headless = Headless.new
+    headless.start
+    at_exit do
+      headless.destroy
+    end
+  end
+end
--- script/jenkins_build.sh
+++ script/jenkins_build.sh
@@ -19,11 +19,8 @@ source .rvmrc
 echo "=-=-=-=-= $0 bundle install"
 bundle install
 
-echo "=-=-=-=-= $0 rake db:migrate"
-rake db:migrate
-
 echo "=-=-=-=-= $0 rake --trace gamma:ci"
-rake --trace gamma:ci
+HEADLESS=true rake --trace gamma:ci
 retval=$?
 
 echo "=-=-=-=-= $0 finished $retval"
