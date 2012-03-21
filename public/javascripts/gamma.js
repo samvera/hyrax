b1346d5bdcfd7caa577b655c77c05d5aed4aa2d5
Dan Coughlin <danny@psu.edu>
2012-03-21 08:27:40 -0400

    added geocities autocomplete for based_near metadata

--- public/javascripts/gamma.js
+++ public/javascripts/gamma.js
@@ -98,5 +98,29 @@ $(function() {
              return false;
             }*/
         });
+    $( "#generic_file_based_near" ).autocomplete({
+      source: function( request, response ) {
+        $.ajax({
+          url: "http://ws.geonames.org/searchJSON",
+          dataType: "jsonp",
+          data: {
+            featureClass: "P",
+            style: "full",
+            maxRows: 12,
+            name_startsWith: request.term
+          },
+          success: function( data ) {
+            response( $.map( data.geonames, function( item ) {
+              return {
+                label: item.name + (item.adminName1 ? ", " + item.adminName1 : "") + ", " + item.countryName,
+                value: item.name
+              }
+            }));
+          }
+        });
+      },
+      minLength: 2,
+    });
+
 
 });
