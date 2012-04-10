3cfe3c19f9eb85403ebc4b43430f08b30dacc54f
Justin Coyne <justin.coyne@yourmediashelf.com>
2012-04-10 08:50:27 -0500

    Remove old deprecated code

--- lib/hydra/access_controls_enforcement.rb
+++ lib/hydra/access_controls_enforcement.rb
@@ -143,12 +143,8 @@ module Hydra::AccessControlsEnforcement
   # Currently does nothing, instead relies on 
   # @param [Hash] opts (optional, not currently used)
   def enforce_index_permissions(opts={})
-    return true
     # Do nothing. Relies on enforce_search_permissions being included in the Controller's solr_search_params_logic
-    # apply_gated_discovery
-    # if !reader? 
-    #   solr_parameters[:qt] = Blacklight.config[:public_qt]
-    # end
+    return true
   end
   
   #
@@ -189,7 +185,7 @@ module Hydra::AccessControlsEnforcement
     # Grant access based on user id & role
     unless current_user.nil?
       # for roles
-      RoleMapper.roles(user_key).each_with_index do |role, i|
+      ::RoleMapper.roles(user_key).each_with_index do |role, i|
         permission_types.each do |type|
           user_access_filters << "#{type}_access_group_t:#{role}"
         end
@@ -234,57 +230,4 @@ module Hydra::AccessControlsEnforcement
     solr_parameters[:fq] ||= []
     solr_parameters[:fq] << "-has_model_s:\"info:fedora/afmodel:FileAsset\""
   end
-
-  # Build the lucene query that performs gated discovery based on Hydra rightsMetadata information in Solr
-  # @param [String] user_query the user's original query request that will be wrapped in access controls
-  def build_lucene_query(user_query)
-    logger.warn("DEPRECATED: build_lucene_query has been deprecated.  Recommended convention is to use blacklight's dismax search requestHandler (not lucene) and filter queries with :fq solr parameters.  See Hydra::AccessControlsEnforcement#apply_gated_discovery and Hydra::AccessControlsEnforcement#exclude_unwanted_models")
-    q = ""
-    # start query of with user supplied query term
-      q << "_query_:\"{!dismax qf=$qf_dismax pf=$pf_dismax}#{user_query}\" AND " if user_query
-
-
-    # Append the exclusion of FileAssets
-      q << "NOT _query_:\"info\\\\:fedora/afmodel\\\\:FileAsset\""
-
-    # Append the query responsible for adding the users discovery level
-      permission_types = ["edit","discover","read"]
-      field_queries = []
-      embargo_query = ""
-      permission_types.each do |type|
-        field_queries << "_query_:\"#{type}_access_group_t:public\""
-      end
-
-      unless current_user.nil?
-        # for roles
-        RoleMapper.roles(user_key).each do |role|
-          permission_types.each do |type|
-            field_queries << "_query_:\"#{type}_access_group_t:#{role}\""
-          end
-        end
-        # for individual person access
-        permission_types.each do |type|
-          field_queries << "_query_:\"#{type}_access_person_t:#{user_key}\""
-        end
-        if current_user.is_being_superuser?(session)
-          permission_types.each do |type|
-            field_queries << "_query_:\"#{type}_access_person_t:[* TO *]\""
-          end
-        end
-
-        # if it is the depositor and it is under embargo, that is ok
-        # otherwise if it not the depositor and it is under embargo, don't show it
-        embargo_query = " OR  ((_query_:\"embargo_release_date_dt:[NOW TO *]\" AND  _query_:\"depositor_t:#{user_key}\") AND NOT (NOT _query_:\"depositor_t:#{user_key}\" AND _query_:\"embargo_release_date_dt:[NOW TO *]\"))"
-      end
-      
-      # remove anything with an embargo release date in the future  
-#embargo_query = " AND NOT _query_:\"embargo_release_date_dt:[NOW TO *]\"" if embargo_query.blank?
-      field_queries << " NOT _query_:\"embargo_release_date_dt:[NOW TO *]\"" if embargo_query.blank?
-      
-      q << " AND (#{field_queries.join(" OR ")})"
-      q << embargo_query 
-    return q
-  end
-
-
 end
--- test_support/spec/helpers/access_controls_enforcement_spec.rb
+++ test_support/spec/helpers/access_controls_enforcement_spec.rb
@@ -89,65 +89,6 @@ describe Hydra::AccessControlsEnforcement do
       @solr_parameters[:fq].should include("-has_model_s:\"info:fedora/afmodel:FileAsset\"")  
     end
   end
-  
- describe "build_lucene_query" do
-
-   it "should return fields for all roles the user is a member of checking against the discover, access, read fields" do
-     stub_user = User.new :email=>'archivist1@example.com'
-     stub_user.stubs(:is_being_superuser?).returns false
-     helper.stubs(:current_user).returns(stub_user)
-     # This example assumes that archivist1 is in the archivist and researcher groups.
-     # Tried stubbing RoleMapper.roles instead, but that broke 26 other tests because mocha fails to release the expectation.
-     # RoleMapper.stubs(:roles).with(stub_user.email).returns(["archivist", "researcher"])
-     query = helper.send(:build_lucene_query, "query_string")
-     # RoleMapper.stubs(:roles).with(stub_user.email).returns(["archivist", "researcher"])
-     # query = helper.send(:build_lucene_query, "string")
-     
-     ["discover","edit","read"].each do |type|
-       query.should match(/_query_\:\"#{type}_access_group_t\:archivist/) and
-       query.should match(/_query_\:\"#{type}_access_group_t\:researcher/)
-     end
-     query.should match /^_query_:"\{!dismax qf=\$qf_dismax pf=\$pf_dismax\}query_string" AND NOT _query_:"info\\\\:fedora\/afmodel\\\\:FileAsset"/
-   end
-   it "should not have dismax clause if no user_query is suplied" do
-     stub_user = User.new
-     stub_user.stubs(:is_being_superuser?).returns false
-     helper.stubs(:current_user).returns(stub_user)
-     query = helper.send(:build_lucene_query, nil)
-     query.should match /^NOT _query_:"info\\\\:fedora\/afmodel\\\\:FileAsset"/
-   end
-
-   it "should return fields for all the person specific discover, access, read fields" do
-     stub_user = User.new
-     stub_user.stubs(:is_being_superuser?).returns false
-     helper.stubs(:current_user).returns(stub_user)
-     query = helper.send(:build_lucene_query, "string")
-     ["discover","edit","read"].each do |type|
-       query.should match(/_query_\:\"#{type}_access_person_t\:#{stub_user.email}/)
-     end
-   end
-   describe "for superusers" do
-     it "should return superuser access level" do
-       stub_user = User.new
-       stub_user.stubs(:is_being_superuser?).returns true
-       helper.stubs(:current_user).returns(stub_user)
-       query = helper.send(:build_lucene_query, "string")
-       ["discover","edit","read"].each do |type|         
-         query.should match(/_query_\:\"#{type}_access_person_t\:\[\* TO \*\]/)
-       end
-     end
-     it "should not return superuser access to non-superusers" do
-       stub_user = User.new
-       stub_user.stubs(:is_being_superuser?).returns false
-       helper.stubs(:current_user).returns(stub_user)
-       query = helper.send(:build_lucene_query, "string")
-       ["discover","edit","read"].each do |type|
-         query.should_not match(/_query_\:\"#{type}_access_person_t\:\[\* TO \*\]/)
-       end
-     end
-   end
-
- end
 end
 
 
