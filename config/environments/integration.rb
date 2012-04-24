5d75a33731d5ccaaf5a3fdcc82ae8cc317b30ea2
Jeff Minelli <jgm106@psu.edu>
2012-04-24 12:17:09 -0400

    added logging entry back in

--- config/environments/integration.rb
+++ config/environments/integration.rb
@@ -22,5 +22,8 @@ ScholarSphere::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
+
+  config.log_level = :debug
+
 end
 
