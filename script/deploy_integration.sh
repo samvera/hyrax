a68c73b4b8231a01c2d5c755b684804e0250d99c
Jeff Minelli <jgm106@psu.edu>
2012-04-30 17:05:41 -0400

    deployment script to integration on successful build in jenkins

---
+++ script/deploy_integration.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#
+# Currently a stub for jenkins as called from
+# https://gamma-ci.dlt.psu.edu/jenkins/job/scholarsphere/configure
+#	Build -> Execute Shell Command == 
+#	test -x /opt/heracles/deploy_integration.sh && /opt/heracles/deploy_integration.sh 
+# to run CI testing.
+
+HHOME=/opt/heracles
+WORKSPACE=/opt/heracles/scholarsphere/scholarsphere-integration
+
+echo hello ss-integration
+echo "=-=-=-=-= $0 export RAILS_ENV=integration"
+export RAILS_ENV=integration
+
+echo "=-=-=-=-= $0 source ${HHOME}/.bashrc"
+source ${HHOME}/.bashrc
+
+echo "=-=-=-=-= $0 source /etc/profile.d/rvm.sh"
+source /etc/profile.d/rvm.sh
+
+echo "=-=-=-=-= $0 cd ${WORKSPACE}"
+cd ${WORKSPACE}
+
+echo "=-=-=-=-= $0 source ${WORKSPACE}/.rvmrc"
+source ${WORKSPACE}/.rvmrc
+
+echo "=-=-=-=-= $0 cp -f ${HHOME}/config/{database,fedora,solr}.yml ${WORKSPACE}/config"
+cp -f ${HHOME}/config/{database,fedora,solr}.yml ${WORKSPACE}/config
+
+echo "=-=-=-=-= $0 bundle install"
+bundle install
+
+echo "=-=-=-=-= $0 passenger-install-apache2-module -a"
+passenger-install-apache2-module -a
+
+#echo "=-=-=-=-= $0 rake db:migrate"
+#RAILS_ENV=integration rake db:migrate
+retval=$?
+
+echo "=-=-=-=-= $0 finished $retval"
+exit $retval
+
+#
+# end
