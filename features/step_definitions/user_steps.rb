eb9fd3c148abc1416dbc3c67460cc156ae7f39ab
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2012-05-02 12:02:06 -0400

    fixes #745 Now cucumber sets up a RackTest driver with the headers you need, as necessary

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -2,21 +2,20 @@
 #   I log in as "archivist1@example.com"
 # @example
 #   I am logged in as "archivist1@example.com"
-Given /^I (?:am )?log(?:ged)? in as "([^\"]*)"$/ do |email|
-  # Given %{a User exists with a Login of "#{login}"}
-  user = User.create(:email => email, :password => "password", :password_confirmation => "password")
-  User.find_by_email(email).should_not be_nil
-  visit destroy_user_session_path
-  visit new_user_session_path
-  fill_in "Email", :with => email 
-  fill_in "Password", :with => "password"
-  click_button "Sign in"
-  #step %{I should see a link to "my account info" with label "#{email}"} 
-  step %{And I click the anchor "#{email}"} 
-  step %{I should see a link to "my account info"}
+Given /^I (?:am )?log(?:ged)? in as "([^\"]*)"$/ do |login|
+  driver_name = "rack_test_authenticated_header_#{login}".to_s
+  Capybara.register_driver(driver_name) do |app|
+    Capybara::RackTest::Driver.new(app, headers: { 'REMOTE_USER' => login })
+  end
+  user = User.create(:login => login)
+  User.find_by_login(login).should_not be_nil
+  Capybara.current_driver = driver_name
+  
+  visit "/"
+  step %{And I click the anchor "#{login}"} 
   step %{I should see a link to "ingest" with label "upload"}
   step %{I should see a link to "dashboard" with label "my dashboard"}
-  step %{I should see a link to "logout"} 
+  # step %{I should see a link to "logout"} 
 end
 
 Given /^I am logged in as "([^\"]*)" with "([^\"]*)" permissions$/ do |login,permission_group|
@@ -36,5 +35,6 @@ Given /^I am not logged in$/ do
 end
 
 Given /^I log out$/ do
-  visit destroy_user_session_path
+  Capybara.use_default_driver
+  visit "/"
 end
