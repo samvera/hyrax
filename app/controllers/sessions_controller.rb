028068536a0df95291b873abfa3d68d1dbc543d3
Justin Coyne <justin.coyne@yourmediashelf.com>
2012-05-03 11:31:46 -0500

    hardcode cookie name to see if this helps the logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   def destroy
-    cookies.delete(ENV['COSIGN_SERVICE']) if ENV['COSIGN_SERVICE']
+    cookies.delete('cosign-gamma-ci.dlt.psu.edu')# if ENV['COSIGN_SERVICE']
     redirect_to "https://webaccess.psu.edu/cgi-bin/logout"
   end
 end
