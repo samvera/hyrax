6e15f2c0db83c56b4372cd7355f266a006051796
Michael J. Giarlo <leftwing@alumni.rutgers.edu>
2012-05-04 10:20:44 -0400

    batches are not exposed in the UI, so no need to overrride that hydra-head method

--- lib/hydra/access_controls_enforcement.rb
+++
@@ -1,6 +0,0 @@
-module Hydra::AccessControlsEnforcement
-  def exclude_unwanted_models(solr_parameters, user_parameters)
-    solr_parameters[:fq] ||= []
-    solr_parameters[:fq] << "-has_model_s:\"info:fedora/afmodel:Batch\""
-  end
-end
