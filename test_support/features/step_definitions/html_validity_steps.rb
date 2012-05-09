65382625edb96d59de1ea6dcf7137bc2e5393825
Chris Beer <chris@cbeer.info>
2012-05-09 11:04:41 -0700

    update html validity check to hit validator.nu directly

--- test_support/features/step_definitions/html_validity_steps.rb
+++ test_support/features/step_definitions/html_validity_steps.rb
@@ -6,16 +6,51 @@ Then /^the page should be HTML5 valid$/ do
   response_is_html_valid?(page.source).should be_true
 end
 
+class Html5Validator
+  BASE_URI = 'http://html5.validator.nu'
+  HEADERS = { 'Content-Type' => 'text/html; charset=utf-8', 'Content-Encoding' => 'UTF-8' }
+
+  def initialize(text)
+    @text = text
+
+  end
+
+  def valid?
+    errors.empty?
+  end
+
+  def errors
+    validation['messages'].select { |msg| msg['type'] == 'error' }
+  end
+
+  def validation
+    @validation ||= validate!
+  end
+
+  def inspect
+    str = "\n#{validator.errors.length} HTML5 Validation Error(s):\n"
+    errors.each_with_index do |error,i|
+      str << "  #{i+1}) #{ error['message'] }\n"
+    end
+
+    str
+  end
+
+  private
+  def validate!
+    response = RestClient.post "#{BASE_URI}/?out=json", @text, HEADERS
+    JSON.parse(response.body)
+  end
+end
+
 private
 
 def response_is_html_valid?(resp)
   begin
     # http://validator.w3.org/docs/api.html says to sleep for at least 1 second between requests.  Just making sure we don't get throttled.
     sleep(1.0)
-    validator_response = RestClient.post("http://validator.w3.org/check",
-                                         :output => "soap12",
-                                         :doctype => "HTML5",
-                                         :fragment => resp)
+    validator = Html5Validator.new(resp)
+    return true if validator.valid?
   rescue SocketError, RestClient::BadGateway #meaning we're either not connected to the internet or we were throttled by the validator.
     puts "WARNING: No connection to W3C validator.  Page may not be HTML5 valid!"
     return true
@@ -23,45 +58,7 @@ def response_is_html_valid?(resp)
     puts "WARNING: Timeout connecting to W3C validator.  Page may not be HTML5 valid!"
     return true
   end
-  
-  xml = Nokogiri::XML(validator_response)
 
-  #removing namespaces because we really don't care
-  xml.remove_namespaces!
-
-  fault = xml.xpath("//Fault/Code/Subcode/Value").text
-  if fault.present?
-    puts "WARNING: There was an error with the W3C validator.  Page may not be HTML5 valid!"
-    puts "FAULT: #{fault}"
-    return true
-  end
-
-  # "true" or "false"
-  valid = xml.xpath("//validity").text
-  error_list = xml.xpath("//errors/errorlist/error")
-  warning_list = xml.xpath("//warnings/warninglist/warning")
-
-  error_count = xml.xpath("//errors/errorcount").text
-  warning_count = xml.xpath("//warnings/warningcount").text
-
-  errors = []
-  warnings = []
-  error_list.each do |err|
-    errors << {:message => err.xpath("./message").text, :source => err.xpath("./source").text, :line => err.xpath("./line").text, :col => err.xpath("./col").text}
-  end
-
-  warning_list.each do |warn|
-    warnings << warn.xpath("./message").text
-  end
-  
-  if valid == "true"
-    return true
-  else
-    text = "\n#{error_count} HTML5 Validation Error(s):\n"
-    errors.each_with_index do |error,i|
-      text << "  #{i+1}) Line #{error[:line]}, Column #{error[:col]}: #{error[:message]}\n"
-    end
-    puts text unless errors == []
-    return false
-  end
+  puts validator.inspect
+  return false
 end
