0911e67aaa9ce03f1b00b3065aedc6054545db73
Carolyn Cole <cam156@psu.edu>
2012-05-11 08:24:12 -0400

    added test for getting only user doc on dashboard fixes #771

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -3,16 +3,22 @@ require 'spec_helper'
 describe DashboardController do
 
   describe "logged in user" do
-    before do
+    before (:each) do
       @user = FactoryGirl.find_or_create(:archivist)
       sign_in @user
       controller.stubs(:clear_session_user) ## Don't clear out the authenticated session
     end
     describe "#index" do
-      it "should return an array of documents" do
+      before (:each) do
         xhr :get, :index
+      end
+      it "should be a success" do
         response.should be_success
         response.should render_template('dashboard/index')        
+      end
+      it "should return an array of documents I can edit" do
+        @user_results = Blacklight.solr.find Hash[:fq=>["edit_access_group_t:public OR edit_access_person_t:#{@user.login}"]]
+        assigns(:document_list).count.should eql(@user_results.docs.count)
       end
     end
   end
