9ff81fb5d66188a913cfcdfb97294585eb1502f9
Justin Coyne <justin.coyne@yourmediashelf.com>
2012-05-17 10:26:34 -0500

    fixed test so it doesn't care which port fedora is running on

--- test_support/spec/helpers/generic_content_objects_helper_spec.rb
+++ test_support/spec/helpers/generic_content_objects_helper_spec.rb
@@ -2,7 +2,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe GenericContentObjectsHelper do
   it "should have a url for a pid" do
-    helper.datastream_disseminator_url('hydra:123', 'sampleDs').should == 'http://127.0.0.1:8983/fedora-test/get/hydra:123/sampleDs'
+    helper.datastream_disseminator_url('hydra:123', 'sampleDs').should match /http:\/\/127.0.0.1:898\d\/fedora-test\/get\/hydra:123\/sampleDs/
   end
 
 end
