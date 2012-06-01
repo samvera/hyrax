87ac9824a871c365160291ea3a5f786c94f5d39f
Carolyn Cole <cam156@psu.edu>
2012-06-01 10:09:33 -0400

    Added tests for recent files. Fixes #828

---
+++ spec/controllers/catalog_controller_spec.rb
@@ -0,0 +1,52 @@
+require 'spec_helper'
+
+describe CatalogController do
+ 
+  before do
+    @user = FactoryGirl.find_or_create(:user)
+    sign_in @user
+    controller.stubs(:clear_session_user) ## Don't clear out the authenticated session
+  end
+  after do
+    @user.delete
+  end
+  
+  describe "#recent" do
+    before do
+        @gf1 = GenericFile.create(title:'Generic File 1', contributor:'contributor 1', resource_type:'type 1', discover_groups:['public'])
+        sleep 1 # make sure next file is not at the same time compare        
+        @gf2 = GenericFile.create(title:'Generic File 2', contributor:'contributor 2', resource_type:'type 2', discover_groups:['public'])
+        sleep 1 # make sure next file is not at the same time compare        
+        @gf3 = GenericFile.create(title:'Generic File 3', contributor:'contributor 3', resource_type:'type 3', discover_groups:['public'])
+        sleep 1 # make sure next file is not at the same time compare        
+        @gf4 = GenericFile.create(title:'Generic File 4', contributor:'contributor 4', resource_type:'type 4', discover_groups:['public'])
+        xhr :get, :recent        
+    end
+    
+    after do
+      @gf1.delete
+      @gf2.delete
+      @gf3.delete
+      @gf4.delete
+    end 
+    
+    it "should find my 4 files" do
+      response.should be_success
+      response.should render_template('catalog/recent')        
+      assigns(:recent_documents).count.should eql(4)
+      # the order is reversed since the first in should be the last out in descending time order
+      assigns(:recent_documents).each {|doc| logger.info doc.fetch(:generic_file__title_t)[0]}
+      lgf4 = assigns(:recent_documents)[0]
+      lgf3 = assigns(:recent_documents)[1]
+      lgf2 = assigns(:recent_documents)[2]
+      lgf1 = assigns(:recent_documents)[3]
+      lgf4.fetch(:generic_file__title_t)[0].should eql(@gf4.title[0])
+      lgf4.fetch(:generic_file__contributor_t)[0].should eql(@gf4.contributor[0])
+      lgf4.fetch(:generic_file__resource_type_t)[0].should eql(@gf4.resource_type[0])
+      lgf1.fetch(:generic_file__title_t)[0].should eql(@gf1.title[0])
+      lgf1.fetch(:generic_file__contributor_t)[0].should eql(@gf1.contributor[0])
+      lgf1.fetch(:generic_file__resource_type_t)[0].should eql(@gf1.resource_type[0])
+    end
+
+  end
+end
