df7ecd9bea264914c7f81872ffb605a37684951c
Michael J. Giarlo <leftwing@alumni.rutgers.edu>
2012-06-13 14:46:42 -0400

    fixes #890: makes FK constraints env-specific which was breaking db:migrate on mysql1test, preventing setup of the staging environment, also cleans up some of the formatting of the migration getting rid of trailing whitespace and sticking to the consistent two-space ruby style convention

--- db/migrate/20120521121725_create_mailboxer.mailboxer_engine.rb
+++ db/migrate/20120521121725_create_mailboxer.mailboxer_engine.rb
@@ -1,14 +1,14 @@
 # This migration comes from mailboxer_engine (originally 20110511145103)
 class CreateMailboxer < ActiveRecord::Migration
-  def self.up    
-  #Tables
-  	#Conversations
+  def self.up
+    #Tables
+    #Conversations
     create_table :conversations do |t|
       t.column :subject, :string, :default => ""
       t.column :created_at, :datetime, :null => false
       t.column :updated_at, :datetime, :null => false
-    end    
-  	#Receipts
+    end
+    #Receipts
     create_table :receipts do |t|
       t.references :receiver, :polymorphic => true
       t.column :notification_id, :integer, :null => false
@@ -18,8 +18,8 @@ class CreateMailboxer < ActiveRecord::Migration
       t.column :mailbox_type, :string, :limit => 25
       t.column :created_at, :datetime, :null => false
       t.column :updated_at, :datetime, :null => false
-    end    
-  	#Notifications and Messages
+    end
+    #Notifications and Messages
     create_table :notifications do |t|
       t.column :type, :string
       t.column :body, :text
@@ -30,31 +30,29 @@ class CreateMailboxer < ActiveRecord::Migration
       t.column :draft, :boolean, :default => false
       t.column :updated_at, :datetime, :null => false
       t.column :created_at, :datetime, :null => false
-    end    
-    
-    
-  #Indexes
-  	#Conversations
-  	#Receipts
-  	add_index "receipts","notification_id"
+    end
 
-  	#Messages  
-  	add_index "notifications","conversation_id"
-  
-  #Foreign keys    
-  	#Conversations
-  	#Receipts
-  	add_foreign_key "receipts", "notifications", :name => "receipts_on_notification_id"
-  	#Messages  
-  	add_foreign_key "notifications", "conversations", :name => "notifications_on_conversation_id"
+    #Indexes
+    #Conversations
+    #Receipts
+    add_index "receipts","notification_id"
+    #Messages
+    add_index "notifications","conversation_id"
+
+    #Foreign keys
+    #Conversations
+    #Receipts
+    add_foreign_key "receipts", "notifications", :name => "receipts_on_notification_id_#{Rails.env}"
+    #Messages  
+    add_foreign_key "notifications", "conversations", :name => "notifications_on_conversation_id_#{Rails.env}"
   end
-  
+
   def self.down
-  #Tables  	
-  	remove_foreign_key "receipts", :name => "receipts_on_notification_id"
-  	remove_foreign_key "notifications", :name => "notifications_on_conversation_id"
-  	
-  #Indexes
+    #Tables
+    remove_foreign_key "receipts", :name => "receipts_on_notification_id_#{Rails.env}"
+    remove_foreign_key "notifications", :name => "notifications_on_conversation_id_#{Rails.env}"
+
+    #Indexes
     drop_table :receipts
     drop_table :conversations
     drop_table :notifications
