8c0055ba7df2db652ce2f3b25b13744f15069c2a
Jeff Minelli <jgm106@psu.edu>
2012-06-21 09:04:21 -0400

    adding timestamps to logs. fixes #925

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,11 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+class Logger
+  def format_message(severity, timestamp, progname, msg)
+    "#{timestamp} (#{$$}) #{msg}\n"
+  end
+end
+
 # Initialize the rails application
 ScholarSphere::Application.initialize!
