d434f960760a152b19c23d6339dd129e54e00eb1
Dan Coughlin <danny@psu.edu>
2012-06-26 10:13:55 -0400

    added new deploy script that can be used for scholarsphere-test. This uses the production rails environment for now.

---
+++ script/deploy_test.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+# deploy script for scholarsphere-test
+
+HHOME=/opt/heracles
+WORKSPACE=${HHOME}/scholarsphere/scholarsphere-test
+
+echo "=-=-=-=-= $0 source ${HHOME}/.bashrc"
+source ${HHOME}/.bashrc
+
+echo "=-=-=-=-= $0 source /etc/profile.d/rvm.sh"
+source /etc/profile.d/rvm.sh
+
+echo "=-=-=-=-= $0 cd ${WORKSPACE}"
+cd ${WORKSPACE}
+
+echo "=-=-=-=-= $0 source ${WORKSPACE}/.rvmrc"
+source ${WORKSPACE}/.rvmrc
+
+echo "=-=-=-=-= $0 bundle install"
+bundle install
+
+echo "=-=-=-=-= $0 passenger-install-apache2-module -a"
+passenger-install-apache2-module -a
+
+echo "=-=-=-=-= $0 rake db:migrate"
+RAILS_ENV=production rake db:migrate
+
+echo "=-=-=-=-= $0 script/delayed_job stop/start"
+RAILS_ENV=production script/delayed_job stop 
+RAILS_ENV=production script/delayed_job -n 10 start
+
+retval=$?
+
+echo "=-=-=-=-= $0 finished $retval"
+exit $retval
+
+#
+# end
