8481c7019a3be5437ee5444e60cd476bb869c03d
Carolyn Cole <cam156@psu.edu>
2012-06-26 13:17:04 -0400

    used defined to avoid name error. refs #854

--- config/environment.rb
+++ config/environment.rb
@@ -9,4 +9,4 @@ end
 
 # Initialize the rails application
 ScholarSphere::Application.initialize!
-ActiveRecord::Base.connection.execute("SET AUTOCOMMIT=1") if ActiveRecord::ConnectionAdapters::Mysql2Adapter and (ActiveRecord::Base.connection.instance_of? ActiveRecord::ConnectionAdapters::Mysql2Adapter)
+ActiveRecord::Base.connection.execute("SET AUTOCOMMIT=1") if defined?(ActiveRecord::ConnectionAdapters::Mysql2Adapter) and (ActiveRecord::Base.connection.instance_of? ActiveRecord::ConnectionAdapters::Mysql2Adapter)
