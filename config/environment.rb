21ca45a62100ad358dded8ef30b4a664564985f7
Carolyn Cole <cam156@psu.edu>
2012-06-26 12:54:26 -0400

    changed to only do if mysql. refs #854

--- config/environment.rb
+++ config/environment.rb
@@ -9,4 +9,5 @@ end
 
 # Initialize the rails application
 ScholarSphere::Application.initialize!
-ActiveRecord::Base.connection.execute("SET AUTOCOMMIT=1")
+ActiveRecord::Base.connection.execute("SET AUTOCOMMIT=1") if ActiveRecord::Base.connection.instance_of? ActiveRecord::ConnectionAdapters::Mysql2Adapter 
+#ActiveRecord::Base.connection.instance_of? ActiveRecord::ConnectionAdapters::Mysql2Adapter ActiveRecord::Base.connection.execute("SET AUTOCOMMIT=1") 
