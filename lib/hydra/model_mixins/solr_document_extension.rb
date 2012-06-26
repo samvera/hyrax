55c1ffcc1b9442da4ed0a8e7117d3e19d5b6417c
Chris Beer <cabeer@stanford.edu>
2012-06-26 13:50:46 -0700

    move get_person_from_role

--- lib/hydra/model_mixins/solr_document_extension.rb
+++ lib/hydra/model_mixins/solr_document_extension.rb
@@ -7,6 +7,17 @@ module Hydra::ModelMixins
 
       type.first.to_s.gsub("info:fedora/afmodel:","").gsub("Hydrangea","").gsub(/^Generic/,"")
     end
+
+    def get_person_from_role(role, opts={})
+      i = 0
+      while i < 10
+        persons_roles = self["person_#{i}_role_t"].map{|w|w.strip.downcase} unless self["person_#{i}_role_t"].nil?
+        if persons_roles and persons_roles.include?(role.downcase)
+          return {:first=>self["person_#{i}_first_name_t"], :last=>self["person_#{i}_last_name_t"]}
+        end
+        i += 1
+      end
+    end
   end
 end
 
