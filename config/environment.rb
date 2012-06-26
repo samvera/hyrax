ac588a2e1f6c2a60184f17af66f17967805635c1
Carolyn Cole <cam156@psu.edu>
2012-06-26 13:09:30 -0400

    changed to only do if mysql available. refs #854

--- config/environment.rb
+++ config/environment.rb
@@ -9,5 +9,4 @@ end
 
 # Initialize the rails application
 ScholarSphere::Application.initialize!
-ActiveRecord::Base.connection.execute("SET AUTOCOMMIT=1") if ActiveRecord::Base.connection.instance_of? ActiveRecord::ConnectionAdapters::Mysql2Adapter 
-#ActiveRecord::Base.connection.instance_of? ActiveRecord::ConnectionAdapters::Mysql2Adapter ActiveRecord::Base.connection.execute("SET AUTOCOMMIT=1") 
+ActiveRecord::Base.connection.execute("SET AUTOCOMMIT=1") if ActiveRecord::ConnectionAdapters::Mysql2Adapter and (ActiveRecord::Base.connection.instance_of? ActiveRecord::ConnectionAdapters::Mysql2Adapter)
