9a2994d859f84416e30fb7269e2a29702bd708df
Michael J. Giarlo <leftwing@alumni.rutgers.edu>
2012-06-28 15:52:54 -0400

    refs #792: removing rack middleware customization for graceful MySQL error handling.  punting that off to another ticket, as it's a potentially huge or at least complicated chunk of work.

--- config/initializers/database_checker.rb
+++
@@ -1,7 +0,0 @@
-class DatabaseChecker
-  def call(env)
-    ActiveRecord::Base.connection_pool.with_connection { |con| con.active? }
-  rescue => error
-    ApplicationController::render_500(error)
-  end
-end
