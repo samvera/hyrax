d20086bfc1874cbc77b5174554145eb0e2974b7d
Michael J. Giarlo <leftwing@alumni.rutgers.edu>
2012-07-02 12:56:29 -0400

    whoops, I think I reset this in debugging MySQL UI errors -- consider_all_requests_local should be TRUE in dev mode

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,7 +11,7 @@ ScholarSphere::Application.configure do
   config.whiny_nils = true
 
   # Show full error reports and disable caching
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local       = true
   #config.action_view.debug_rjs             = false
   config.action_controller.perform_caching = false
 
