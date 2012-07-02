0ef1687437db495e0aa23d3fe4de90d796cb1621
Michael J. Giarlo <leftwing@alumni.rutgers.edu>
2012-07-02 12:55:16 -0400

    refs #950: pulls over some blacklight js and css to handle and style clicking of 'more' facets links in dashboard and catalog; not yet pretty but getting there.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,6 @@
 //= require jquery-1.7.2.min
-//= require jquery_ujs.js 
+//= require jquery_ujs.js
 //= require jquery-ui-1.8.16.custom.min
-//= require general 
+//= require blacklight
+//= require general
 //= require scholarsphere
