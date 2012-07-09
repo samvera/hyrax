228595c5790351d0492a556b033b2e1baaede017
Justin Coyne <justin.coyne@yourmediashelf.com>
2012-07-09 14:31:40 -0500

    Don't expect hydraUiController to include hydra_fedora_metadata helper

--- hydra-file-access/spec/controllers/hydra_ui_controller_spec.rb
+++ hydra-file-access/spec/controllers/hydra_ui_controller_spec.rb
@@ -10,7 +10,6 @@ describe Hydra::Controller do
   end
 
   it "should add the necessary helpers to classes that include it" do
-    HydraUiControllerTest.should_receive(:helper).with(:hydra_fedora_metadata)
     HydraUiControllerTest.should_receive(:helper).with(:generic_content_objects)
     HydraUiControllerTest.should_receive(:helper).with(:hydra_uploader)
     HydraUiControllerTest.should_receive(:helper).with(:article_metadata)
