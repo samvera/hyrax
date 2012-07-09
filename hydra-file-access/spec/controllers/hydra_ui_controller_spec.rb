a86f9ca5a38c893861f7eb560d45a87715205b10
Justin Coyne <justin.coyne@yourmediashelf.com>
2012-07-09 16:53:18 -0500

    Remove article_metadata_helper

--- hydra-file-access/spec/controllers/hydra_ui_controller_spec.rb
+++ hydra-file-access/spec/controllers/hydra_ui_controller_spec.rb
@@ -12,7 +12,6 @@ describe Hydra::Controller do
   it "should add the necessary helpers to classes that include it" do
     HydraUiControllerTest.should_receive(:helper).with(:generic_content_objects)
     HydraUiControllerTest.should_receive(:helper).with(:hydra_uploader)
-    HydraUiControllerTest.should_receive(:helper).with(:article_metadata)
     HydraUiControllerTest.stub(:before_filter)
     HydraUiControllerTest.send(:include, Hydra::UI::Controller)
   end
