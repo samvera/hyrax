be4a7901939d770f76516fd3cc7592d8bc6c1c0f
Justin Coyne <justin.coyne@yourmediashelf.com>
2012-07-09 17:30:44 -0500

    Don't checkin internal

--- hydra-file-access/spec/internal/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Internal::Application.config.secret_token = 'e93672bb08f9755f42ed40f9440a0f37f88984a8d3ae969f5159cba95dc01ca022f227a9e9f8de4e91d63a727af8b1e0279b4ad8a1c7d395f63cb37bf4c59e96'
