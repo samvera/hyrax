58ab432e754075921c85f1463c3b88d216ceae39
Justin Coyne <justin.coyne@yourmediashelf.com>
2012-07-10 15:33:39 -0500

    Removed deprecated active_fedora_helper

--- hydra-core/lib/hydra.rb
+++ hydra-core/lib/hydra.rb
@@ -28,7 +28,6 @@ require 'hydra/rights_metadata'
 require 'hydra/mods'
 require 'hydra/model_methods'
 require 'hydra/models/file_asset'
-require 'mediashelf/active_fedora_helper' #deprecated
 
 SolrDocument.use_extension Hydra::ModelMixins::SolrDocumentExtension if defined? SolrDocument
 
