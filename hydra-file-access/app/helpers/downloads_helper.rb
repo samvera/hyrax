440894e539ea0992093c1a959ac05465b9949bcd
Justin Coyne <justin.coyne@yourmediashelf.com>
2012-07-11 16:16:59 -0500

    Removed deprecated downloads helper

--- hydra-file-access/app/helpers/downloads_helper.rb
+++
@@ -1,4 +0,0 @@
-module DownloadsHelper
-  include Hydra::DownloadsHelperBehavior
-end
-
--- hydra-file-access/app/helpers/hydra/facets_helper_behavior.rb
+++ hydra-file-access/app/helpers/hydra/facets_helper_behavior.rb
@@ -1,21 +1,14 @@
-# WILL BE REMOVED IN HYDRA-HEAD 5.x
-require 'deprecation'
 module Hydra
   module FacetsHelperBehavior
     include Blacklight::FacetsHelperBehavior
-    extend Deprecation
-    self.deprecation_horizon = 'hydra-head 5.x'
-
 
     # Removing the [remove] link and label class from the default selected facet display
-    # NOT DEPRECATED BECAUSE THIS IS AN OVERRIDE OF A BLACKLIGHT METHOD
     def render_selected_facet_value(facet_solr_field, item)
       content_tag(:span, render_facet_value(facet_solr_field, item, :suppress_link => true), :class => "selected")
     end
 
     # Override to remove the label class (easier integration with bootstrap)
     # and handles arrays
-    # NOT DEPRECATED BECAUSE THIS IS AN OVERRIDE OF A BLACKLIGHT METHOD
     def render_facet_value(facet_solr_field, item, options ={})    
       if item.is_a? Array
         render_array_facet_value(facet_solr_field, item, options)
@@ -25,8 +18,6 @@ module Hydra
     end
 
     def render_array_facet_value(facet_solr_field, item, options)
-      ActiveSupport::Deprecation.warn("render_array_facet_value is deprecated and will be removed in the next release")
-
       (link_to_unless(options[:suppress_link], item[0], add_facet_params_and_redirect(facet_solr_field, item[0]), :class=>"facet_select") + " (" + format_num(item[1]) + ")").html_safe 
     end
   end
