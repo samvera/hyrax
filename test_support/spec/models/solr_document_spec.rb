7f5ef5ae1046bdc264e6cebde4feeb16ffaf61e8
Chris Beer <cabeer@stanford.edu>
2012-07-12 13:03:42 +0100

    mocha-ize the expectation

--- test_support/spec/models/solr_document_spec.rb
+++ test_support/spec/models/solr_document_spec.rb
@@ -2,10 +2,16 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe SolrDocument do
   describe "#to_model" do
+
+    before do
+      class SolrDocumentWithHydraOverride < SolrDocument
+         include Hydra::Solr::Document
+      end
+    end
     # this isn't a great test, but...
     it "should try to cast the SolrDocument to the Fedora object" do
-      ActiveFedora::Base.should_receive(:load_instance_from_solr).with('asdf', an_instance_of(SolrDocument))
-      SolrDocument.new(:id => 'asdfg').to_model
+      ActiveFedora::Base.expects(:load_instance_from_solr).with('asdfg', kind_of(SolrDocument))
+      SolrDocumentWithHydraOverride.new(:id => 'asdfg').to_model
     end
   end
 end
