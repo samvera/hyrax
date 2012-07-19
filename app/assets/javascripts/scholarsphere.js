583b88def88fc9b754a58b35850610f9e8ff8910
Dan Coughlin <danny@psu.edu>
2012-07-19 17:19:00 -0400

    took the heading elements off the facets for accesibility, also added text to 'more' links for accessibility as well. removing links that only say 'more' is a big thing to remain and continue to add. fixes #1042

--- app/assets/javascripts/scholarsphere.js
+++ app/assets/javascripts/scholarsphere.js
@@ -281,19 +281,10 @@ $(function() {
     $('#permissions_submit').show();
   });
 
-	$(".loggedin").click(function(){
-		$(this).next(".dropdown").slideToggle(400);
-	});
-	
-	$(".itemexpand").click(function(){
-		$(this).parent().parent().next("tr.hidden").toggle();
-		$(this).toggleClass("itemexpand itemhide");
-		return false;
-	});
-	
-	$("h3.expandable").click(function(){
+  // facets lists
+	$("li.expandable").click(function(){
 		$(this).next("ul").slideToggle();
-		$(this).toggleClass("open");
+    $(this).find('i').toggleClass("icon-chevron-down");
 	}); 
 
 	$('#generic_file_permissions_new_group_name').change(function (){
