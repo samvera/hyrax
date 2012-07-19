2677aa480ef07f6ff2dd6a85cced1e742b31d0ae
Carolyn Cole <cam156@psu.edu>
2012-07-19 15:20:45 -0400

    Fixed public and PSU permission edit.  Also fixed bug when ldap groups contained no entries. fixes #1041

--- lib/scholarsphere/ldap.rb
+++ lib/scholarsphere/ldap.rb
@@ -8,6 +8,6 @@ module ScholarSphere::LDAP
   def self.result_groups_for_user(result)
     # Northwestern - result.map{|r| r[:cn].first}
     # only get umg and strip off the cn and the ,dc=psu,dc=edu
-    return result.first[:psmemberof].select{ |y| y.starts_with? 'cn=umg/' }.map{ |x| x.sub(/^cn=/, '').sub(/,dc=psu,dc=edu/, '') }
+    return result.blank? ? [] : result.first[:psmemberof].select{ |y| y.starts_with? 'cn=umg/' }.map{ |x| x.sub(/^cn=/, '').sub(/,dc=psu,dc=edu/, '') } 
   end 
 end
