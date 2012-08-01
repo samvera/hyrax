e46b4704f1282b846fa046b1f95afb8f7e8b3c41
Michael J. Giarlo <leftwing@alumni.rutgers.edu>
2012-07-31 23:31:10 -0400

    refs #1018, #1019, #1021: fixes gf path in links in event job messages; fixes signin/signout icon messup

--- app/models/content_new_version_event_job.rb
+++ app/models/content_new_version_event_job.rb
@@ -3,7 +3,7 @@ class ContentNewVersionEventJob < EventJob
 
   def initialize(generic_file_id, depositor_id)
     gf = GenericFile.find(generic_file_id)
-    message = "User #{link_to depositor_id, profile_path(depositor_id)} has added a new version of #{link_to gf.title.first, url_for(gf.noid)}"
+    message = "User #{link_to depositor_id, profile_path(depositor_id)} has added a new version of #{link_to gf.title.first, generic_file_path(gf.noid)}"
     timestamp = Time.now.to_i
     depositor = User.find_by_login(depositor_id)
     # Log the event to the depositor's stream
