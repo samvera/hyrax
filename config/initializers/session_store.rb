3870e4fa75d0108cabaaf2eee6245ef5210262fa
Michael J. Giarlo <leftwing@alumni.rutgers.edu>
2012-08-10 15:15:16 -0400

    refs #1219: invalidates old cookies by changing key

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-ScholarSphere::Application.config.session_store :cookie_store, :key => '_scholarsphere_session'
+ScholarSphere::Application.config.session_store :cookie_store, :key => '_scholarsphere_secure_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
