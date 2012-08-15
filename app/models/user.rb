5a4a2800388e88e977052fd501fe846bd8d99d64
Dan Coughlin <dan.coughlin@gmail.com>
2012-08-15 13:19:30 -0400

    quick changes for some inconsistency on production.

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,7 +25,7 @@ class User < ActiveRecord::Base
   attr_accessible :email, :login, :display_name, :address, :admin_area, :department, :title, :office, :chat_id, :website, :affiliation, :telephone, :avatar
 
   # Add user avatar (via paperclip library)
-  has_attached_file :avatar, :styles => { medium: "300x300>", thumb: "100x100>" }, :default_url => ActionController::Base.helpers.asset_path('missing_:style.png')
+  has_attached_file :avatar, :styles => { medium: "300x300>", thumb: "100x100>" }, :default_url => '/assets/missing_:style.png'
   validates :avatar, :attachment_content_type => { :content_type => /^image\/(jpg|jpeg|pjpeg|png|x-png|gif)$/ }, :if => Proc.new { |p| p.avatar.file? }
   validates :avatar, :attachment_size => { :less_than => 500.kilobytes }, :if => Proc.new { |p| p.avatar.file? }
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,6 +68,7 @@ ScholarSphere::Application.configure do
   
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += %w( generic_files.js dashboard.js video.js audio.min.js jquery.zclip.min.js bootstrap-tooltip.js bootstrap-popover.js video-js.css generic_files.css jquery-ui-1.8.1.custom.css bootstrap.min.css )
+  config.assets.precompile += %w( *.jpg *.png *.gif *.ico )
 
   config.logout_url = "https://webaccess.psu.edu/cgi-bin/logout?#{get_vhost_by_host[1]}"
   config.login_url = "https://webaccess.psu.edu?cosign-#{get_vhost_by_host[0]}&#{get_vhost_by_host[1]}"
