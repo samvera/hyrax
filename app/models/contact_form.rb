6bd8f4f7d6196d7c62e7fd01af8ebaf96f7185e7
Dan Coughlin <dan.coughlin@gmail.com>
2012-08-21 12:16:57 -0400

    added some changes to auto generate ticket categories in redmine. fixes #1253

--- app/models/contact_form.rb
+++ app/models/contact_form.rb
@@ -7,17 +7,18 @@ class ContactForm < MailForm::Base
       ["General inquiry or request", "General inquiry or request"]
     ] 
   attribute :contact_method,  :captcha  => true
+  attribute :category,    :validate => true
   attribute :name,        :validate => true
   attribute :email,       :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attribute :subject,     :validate => true
   attribute :message,     :validate => true
-  attribute :issue_type,  :validate => true
   # - can't use this without ActiveRecord::Base validates_inclusion_of :issue_type, :in => ISSUE_TYPES
 
   # Declare the e-mail headers. It accepts anything the mail method
   # in ActionMailer accepts.
   def headers
     {
-      :subject => "ScholarSphere Contact Form",
+      :subject => "SS-Form:#{subject}",
       :to => ScholarSphere::Application.config.contact_email, 
       :from => %("#{name}" <#{email}>)
     }
