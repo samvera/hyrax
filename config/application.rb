b3870562eec71b5852d402c5a970f6ce37dfe368
Dan Coughlin <dan.coughlin@gmail.com>
2012-08-27 14:21:43 -0400

    updated the metadata help and other textual changes.  created new url for depositor agreement to be used so terms can still be used when we implement the TOS. fixes #1275

--- config/application.rb
+++ config/application.rb
@@ -110,20 +110,20 @@ module ScholarSphere
     # help text to display for form metadata elements, these will need to be updated to
     # reflect a field name change (should one happen) in the generic file datastream
     config.metadata_help = {
-      "generic_file_resource_type" => "The type of resource this file is in ScholarSphere.",
-      "generic_file_title" => "The title of the object being uploaded to ScholarSphere.",
-      "generic_file_tag" => "Keywords, or tags, that describe the object.",
-      "generic_file_subject" => "If you would like to align the description of your object with an existing vocabulary of subject terms, enter those here. ScholarSphere supports Library of Congress Subject Headings (LCSH). Begin typing a subject term and ScholarSphere will present you with a list of matching terms.",
-      "generic_file_creator" => "The name of a person or group primarily responsible for the creation of the object being uploaded.",
-      "generic_file_related_url" => "A URL about the object or the context in which it was created. Example: a link to the research project from which a data set was derived.",
-      "generic_file_based_near" => "A location that is relevant to where this file was created.",
-      "generic_file_contributor" => "A person or group that contributed to the object's existence, including schools, colleges, or institutes at Penn State, and funding agencies responsible for funding the research that produced the object.",
-      "generic_file_date_created" => "Date on which the object was created in its present form.",
-      "generic_file_description" => "A brief description, an abstract, or notes about relationships between this object and other objects to which it is related, providing extra context about the object.",
-      "generic_file_identifier" => "A unique handle identifying the object in an external context, e.g. if it has an ISBN, OCLC number, or similar identifying numbers or names.",
-      "generic_file_language" => "A language in which the object is expressed.",
-      "generic_file_publisher" => "An entity responsible for dissemination of the object.",
-      "generic_file_rights" => "A statement about the rights assigned to the object or a content license such as those provided by Creative Commons.  Note that these are not automatically enforced by ScholarSphere, which considers rights, licensing, and access levels separate."
+      "generic_file_resource_type" => "Pre-defined categories to describe the type of file content being uploaded, such as \"article\" or \"dataset.\"  More than one type may be selected.",
+      "generic_file_title" => "A name for the file to aid in identifying it. Defaults to the file name, though a more descriptive title is encouraged. <em>This is a required field</em>.", 
+      "generic_file_tag" => "Words or phrases you select to describe what the file is about. These are used to search for content. <em>This is a required field</em>.",
+      "generic_file_subject" => "Headings or index terms describing what the file is about; these <em>do</em> need to conform to an existing vocabulary. Currently ScholarSphere supports Library of Congress Subject Headings.",
+      "generic_file_creator" => "The person or group responsible for the file being uploaded. Usually this is the author of the content. Personal names should be entered with the last name first, e.g. \"Smith, John.\" <em>This is a required field</em>.",
+      "generic_file_related_url" => "A link to a website or other specific content (audio, video, PDF document) related to the file. An example is the URL of a research project from which the file was derived.", 
+      "generic_file_based_near" => "A place name related to the file, such as its site of publication, or the city, state, or country the file's contents are about. Calls upon the GeoNames web service (<a href=\"http://www.geonames.org\">http://www.geonames.org</a>).",
+      "generic_file_contributor" => "A person or group you want to recognize for playing a role in the creation of the file, but not the primary role. If there is a specific role you would like noted, include it in parentheses, e.g. \"Jones, Mary (advisor).\"",
+      "generic_file_date_created" => "The date on which the file was generated. Dates are accepted in the form YYYY-MM-DD, e.g. 1776-07-04.",
+      "generic_file_description" => "Free-text notes about the file itself. Examples include abstracts of a paper, citation information for a journal article, or a tag indicating a larger collection to which the file belongs.",
+      "generic_file_identifier" => "A unique handle describing the file. An example would be a DOI for a journal article, or an ISBN or OCLC number for a book.",
+      "generic_file_language" => " The language of the file content.",
+      "generic_file_publisher" => "The person or group making the file available. Generally this is Penn State or the Penn State University Libraries.",
+      "generic_file_rights" => "Licensing and distribution information governing access to the file. Select from the provided drop-down list. <em>This is a required field</em>."
     }
 
     # Custom directories with classes and modules you want to be autoloadable.
