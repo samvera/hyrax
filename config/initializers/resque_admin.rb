3d2fe6a52fb915ade7f2cf9e7a966060f96b972c
Michael J. Giarlo <leftwing@alumni.rutgers.edu>
2012-09-09 19:15:50 -0400

    restoring resque admin config to make sure we don't miss that on merge (paranoid, I know)

--- config/initializers/resque_admin.rb
+++ config/initializers/resque_admin.rb
@@ -1,14 +1,7 @@
 class ResqueAdmin
   def self.matches?(request)
     current_user = request.env['warden'].user
-    puts "----------------------------------"
-    puts current_user
-    puts "----------------------------------"
     return false if current_user.blank?
-    puts current_user.groups
-    puts "----------------------------------"
-    #current_user.groups.include? 'umg/up.dlt.scholarsphere-admin'
-    #current_user.groups.include? 'umg/up.its.voipusers'
-    return true
+    current_user.groups.include? 'umg/up.dlt.scholarsphere-admin'
   end
 end
