98f3b1a7346445c5a24931386715dd174db985e0
Dan Coughlin <dan.coughlin@gmail.com>
2012-09-08 23:30:19 -0400

    batch update now works as a resque job.  Need to merge it into release branch now.

--- app/models/batch_update_job.rb
+++ app/models/batch_update_job.rb
@@ -3,31 +3,15 @@ class BatchUpdateJob < MetaSaveJob
     params.symbolize_keys!
     batch = Batch.find_or_create(params[:id])
     user = User.find_by_login(login)
-    logger.error "---------------------"
-    logger.error "params: #{params.inspect}"
-    logger.error "---------------------"
-    logger.error "user: #{user.inspect}"
 
     saved = []
     denied = []
     batch.generic_files.each do |gf|
-      logger.error "---------------------"
-      logger.error "pid: #{gf.pid}"
-      logger.error "---------------------"
-      if user.can? :read, get_permissions_solr_response_for_doc_id(gf.pid)
-        logger.error "if is true???"
-      else
-        logger.error "not true???"
-      end
-
-      unless user.can? :read, get_permissions_solr_response_for_doc_id(gf.pid)
+      unless user.can? :edit, get_permissions_solr_response_for_doc_id(gf.pid)[1]
         logger.error "DEEEENIED!"
         denied << gf
         next
       end 
-      logger.error "going through--"
-      logger.error "p title#{params[:title]}"
-      logger.error "p gfile#{params[:generic_file]}"
       gf.title = params[:title][gf.pid] if params[:title][gf.pid] rescue gf.label 
       gf.update_attributes(params[:generic_file])
       gf.set_visibility(params)
