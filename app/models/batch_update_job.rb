95e46558ea3b75ea06a8920c86ae2c8d359a62ed
Michael J. Giarlo <leftwing@alumni.rutgers.edu>
2012-09-09 19:13:49 -0400

    just a few minor tweaks to reduce some abstraction and remove old code that's not relevant anymore

--- app/models/batch_update_job.rb
+++ app/models/batch_update_job.rb
@@ -1,28 +1,42 @@
-class BatchUpdateJob < MetaSaveJob
+class BatchUpdateJob
+  include Rails.application.routes.url_helpers
+  include ActionView::Helpers
+  include ActionView::Helpers::DateHelper
+  include Hydra::AccessControlsEnforcement
+  include ApplicationHelper
+
+  def self.queue
+    :batch_update
+  end
+
+  def self.perform(*args)
+    new(*args)
+  end
+
   def initialize(login, params)
     params.symbolize_keys!
     batch = Batch.find_or_create(params[:id])
     user = User.find_by_login(login)
 
-    saved = []
-    denied = []
+    # TODO: Determine if we need to use these two arrays. Commented out all lines for now.
+    #saved = []
+    #denied = []
     batch.generic_files.each do |gf|
       unless user.can? :edit, get_permissions_solr_response_for_doc_id(gf.pid)[1]
-        logger.error "DEEEENIED!"
-        denied << gf
+        logger.error "User #{user.login} DEEEENIED access to #{gf.pid}!"
+        #denied << gf
         next
-      end 
-      gf.title = params[:title][gf.pid] if params[:title][gf.pid] rescue gf.label 
+      end
+      gf.title = params[:title][gf.pid] if params[:title][gf.pid] rescue gf.label
       gf.update_attributes(params[:generic_file])
       gf.set_visibility(params)
-      gf.save
 
-      save_tries = 0 
+      save_tries = 0
       begin
         gf.save
       rescue RSolr::Error::Http => error
-        logger.warn "GenericFilesController::create_and_save_generic_file Caught RSOLR error #{error.inspect}"
-        save_tries++
+        save_tries += 1
+        logger.warn "BatchUpdateJob caught RSOLR error on #{gf.pid}: #{error.inspect}"
         # fail for good if the tries is greater than 3
         rescue_action_without_handler(error) if save_tries >=3
         sleep 0.01
@@ -33,9 +47,8 @@ class BatchUpdateJob < MetaSaveJob
         Resque.enqueue(ContentUpdateEventJob, gf.pid, login)
       rescue Redis::CannotConnectError
         logger.error "Redis is down!"
-      end 
-      saved << gf
-    end 
+      end
+      #saved << gf
+    end
   end
-
 end
--- app/models/meta_save_job.rb
+++ app/models/meta_save_job.rb
@@ -1,15 +1,2 @@
 class MetaSaveJob
-  include Rails.application.routes.url_helpers
-  include ActionView::Helpers
-  include ActionView::Helpers::DateHelper
-  include Hydra::AccessControlsEnforcement
-  include ApplicationHelper
-
-  def self.queue
-    :batch_update
-  end
-
-  def self.perform(*args)
-    new(*args)
-  end
 end
