c9e002f88fc6cec1da67721439affdb4a92e906d
Carolyn Cole <cam156@psu.edu>
2012-09-21 09:41:55 -0400

    Changed Description to Abstract or Summary.  fixes #1450

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -205,6 +205,7 @@ class CatalogController < ApplicationController
     end
 
     config.add_search_field('description') do |field|
+      field.label = "Abstract or Summary"
       field.solr_parameters = {
         :"spellcheck.dictionary" => "description"
       }
