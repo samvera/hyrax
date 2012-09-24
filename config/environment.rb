1b838af678ba268f40d443dac9be2d3cef16c533
Dan Coughlin <dan.coughlin@gmail.com>
2012-09-24 11:04:56 -0400

    moved redis.yml out of git tracking and added a new block to the redis queue for activity tracker. fixes #1407

--- config/environment.rb
+++ config/environment.rb
@@ -12,6 +12,7 @@ if defined?(PhusionPassenger)
       # The important two lines
       $redis.client.disconnect if $redis 
       $redis = Redis.new(host: config[:host], port: config[:port], thread_safe: true) rescue nil
+      Resque.redis.client.reconnect if Resque.redis
     end
   end
 else
--- config/initializers/resque_config.rb
+++ config/initializers/resque_config.rb
@@ -1,5 +1,8 @@
 rails_root = ENV['RAILS_ROOT'] || "#{File.dirname(__FILE__)}/../.."
 rails_env = ENV['RAILS_ENV'] || 'development'
 
+config = YAML::load(ERB.new(IO.read(File.join(Rails.root, 'config', 'redis.yml'))).result)['resque'].with_indifferent_access
+Resque.redis = Redis.new(host: config[:host], port: config[:port], thread_safe: true)
+
 Resque.inline = rails_env == 'test'
 Resque.redis.namespace = "scholarsphere:#{rails_env}"
