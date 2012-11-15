dde1336a011885167b4247b1208a7f884d1e0783
Justin Coyne <justin.coyne@yourmediashelf.com>
2012-11-15 14:41:03 -0600

    Removed deprecated methods from Hydra::User

--- hydra-access-controls/lib/hydra/user.rb
+++ hydra-access-controls/lib/hydra/user.rb
@@ -1,9 +1,7 @@
 # Injects behaviors into User model so that it will work with Hydra Access Controls
 # By default, this module assumes you are using the User model created by Blacklight, which uses Devise.
 # To integrate your own User implementation into Hydra, override this Module or define your own User model in app/models/user.rb within your Hydra head.
-require 'deprecation'
 module Hydra::User
-  extend Deprecation
   
   def self.included(klass)
     # Other modules to auto-include
@@ -24,19 +22,4 @@ module Hydra::User
       self.send("find_by_#{Devise.authentication_keys.first}".to_sym, key)
     end
   end
-
-  # This method should display the unique identifier for this user
-  # the unique identifier is what access controls will be enforced against. 
-  def unique_id
-    return to_s
-  end
-  deprecation_deprecate :unique_id
-
-
-  # For backwards compatibility with the Rails2 User models in Hydra/Blacklight
-  def login
-    return unique_id
-  end
-  deprecation_deprecate :login
-  
 end
