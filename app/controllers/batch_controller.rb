91cc0f0d24ce140310bee59eca811f5cf89b508b
Justin Coyne <justin.coyne@yourmediashelf.com>
2012-11-15 23:03:10 -0600

    Use engine routes

--- app/controllers/batch_controller.rb
+++ app/controllers/batch_controller.rb
@@ -40,6 +40,6 @@ class BatchController < ApplicationController
     @batch.save
     Resque.enqueue(BatchUpdateJob, current_user.login, params, params[:generic_file])
     flash[:notice] = 'Your files are being processed by ScholarSphere in the background. The metadata and access controls you specified are being applied. Files will be marked <span class="label label-important" title="Private">Private</span> until this process is complete (shouldn\'t take too long, hang in there!). You may need to refresh your dashboard to see these updates.'
-    redirect_to dashboard_path
+    redirect_to sufia.dashboard_path
   end
 end
