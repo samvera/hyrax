dde929d1cd8c2f97d3975cbccf58e688640a66db
Justin Coyne <justin.coyne@yourmediashelf.com>
2012-11-16 13:29:13 -0600

    restore fits_to_desc_mapping

--- lib/generators/sufia/templates/config/sufia.rb
+++ lib/generators/sufia/templates/config/sufia.rb
@@ -1,7 +1,11 @@
 Sufia.config do |config|
   config.id_namespace = "sufia"
   config.fits_path = "fits.sh"
-  config.fits_to_desc_mapping= {}
+  config.fits_to_desc_mapping= {
+      :file_title => :title,
+      :file_author => :creator
+    }
+
 
   # TODO move these to an HttpAuth initializer
   config.login_url =""
