bdd41941a227a390612f6038f87464dc25257122
Justin Coyne <justin.coyne@yourmediashelf.com>
2013-01-08 16:16:05 -0600

    We don't need to have a file for this test

--- spec/models/single_use_link_spec.rb
+++ spec/models/single_use_link_spec.rb
@@ -2,9 +2,7 @@ require 'spec_helper'
 
 describe SingleUseLink do
   before (:all) do
-    GenericFile.any_instance.stub(:terms_of_service).and_return('1')
     @file = GenericFile.new
-    @file.add_file_datastream(File.new(fixture_path + '/world.png'), :dsid=>'content')
     @file.apply_depositor_metadata('mjg36')
     @file.save
   end
