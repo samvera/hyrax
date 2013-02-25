e2222f803ef33a5a86c92f424ee23ca9e4b9ba37
Justin Coyne <justin@curationexperts.com>
2013-02-25 10:54:56 -0600

    The config generator should use string fields

--- hydra-core/lib/generators/hydra/templates/config/initializers/hydra_config.rb
+++ hydra-core/lib/generators/hydra/templates/config/initializers/hydra_config.rb
@@ -15,7 +15,7 @@ if Hydra.respond_to?(:configure)
     # This specifies the solr field names of permissions-related fields.
     # You only need to change these values if you've indexed permissions by some means other than the Hydra's built-in tooling.
     # If you change these, you must also update the permissions request handler in your solrconfig.xml to return those values
-    indexer = Solrizer::Descriptor.new(:text, :stored, :indexed, :multivalued)
+    indexer = Solrizer::Descriptor.new(:string, :stored, :indexed, :multivalued)
     config[:permissions] = {
       :discover => {:group =>ActiveFedora::SolrService.solr_name("discover_access_group", indexer), :individual=>ActiveFedora::SolrService.solr_name("discover_access_person", indexer)},
       :read => {:group =>ActiveFedora::SolrService.solr_name("read_access_group", indexer), :individual=>ActiveFedora::SolrService.solr_name("read_access_person", indexer)},
