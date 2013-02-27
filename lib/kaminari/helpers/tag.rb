71ac3b6280eb6eb106f9e63f7951610ebbea21e7
Justin Coyne <justin@curationexperts.com>
2013-02-27 12:45:31 -0600

    deactivate kaminari patch

--- lib/kaminari/helpers/tag.rb
+++ lib/kaminari/helpers/tag.rb
@@ -2,10 +2,11 @@
 module Kaminari
   module Helpers
     class Tag
-      def page_url_for(page)
-        #@template.url_for @params.merge(@param_name => (page <= 1 ? nil : page)).symbolize_keys
-        Sufia::Engine.routes.url_helpers.url_for @params.merge(@param_name => (page <= 1 ? nil : page), :only_path=>true).symbolize_keys
-      end
+      # def page_url_for(page)
+      #   #@template.url_for @params.merge(@param_name => (page <= 1 ? nil : page)).symbolize_keys
+      #   #Sufia::Engine.routes.url_helpers.url_for @params.merge(@param_name => (page <= 1 ? nil : page), :only_path=>true).symbolize_keys
+      #   (@route_set || @template).url_for @params.merge(@param_name => (page <= 1 ? nil : page))
+      # end
     end
   end
 end
