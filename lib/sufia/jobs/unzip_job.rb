3f67a0bd63b0eb01f191b034f6bfab6c99bf8c78
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-05-14 11:44:25 -0400

    Refactoring via extract method
    The unzip job has duplicate/common behavior with
    Sufia::GenericFile::Actions.create_metadata; This is a first step for
    consolidating this.

--- lib/sufia/jobs/unzip_job.rb
+++ lib/sufia/jobs/unzip_job.rb
@@ -24,22 +24,31 @@ class UnzipJob
   end
 
   def run
-    zip_file = GenericFile.find(pid)
     Zip::Archive.open_buffer(zip_file.content.content) do |archive|
       archive.each do |f|
-        @generic_file = GenericFile.new
-        @generic_file.batch_id = zip_file.batch.pid
-        file_name = f.name
-        mime_types = MIME::Types.of(file_name)
-        mime_type = mime_types.empty? ? "application/octet-stream" : mime_types.first.content_type
-        options = {:label=>file_name, :dsid=>'content', :mimeType=>mime_type}
-        @generic_file.add_file_datastream(f.read, options)
-        @generic_file.set_title_and_label( file_name, :only_if_blank=>true )
-        @generic_file.apply_depositor_metadata(zip_file.edit_users.first)
-        @generic_file.date_uploaded = Time.now.ctime
-        @generic_file.date_modified = Time.now.ctime
-        @generic_file.save
+        create_file(f)
       end
     end
+  end
+
+  protected
+
+  def create_file(f)
+    @generic_file = GenericFile.new
+    @generic_file.batch_id = zip_file.batch.pid
+    file_name = f.name
+    mime_types = MIME::Types.of(file_name)
+    mime_type = mime_types.empty? ? "application/octet-stream" : mime_types.first.content_type
+    options = {:label=>file_name, :dsid=>'content', :mimeType=>mime_type}
+    @generic_file.add_file_datastream(f.read, options)
+    @generic_file.set_title_and_label( file_name, :only_if_blank=>true )
+    @generic_file.apply_depositor_metadata(zip_file.edit_users.first)
+    @generic_file.date_uploaded = Time.now.ctime
+    @generic_file.date_modified = Time.now.ctime
+    @generic_file.save
+  end
+
+  def zip_file
+    @zip_file ||= GenericFile.find(pid)
   end
 end
