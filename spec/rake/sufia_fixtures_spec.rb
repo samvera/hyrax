41a169cd595b0c6c479486720fe6b0815085f1c5
Justin Coyne <justin@curationexperts.com>
2013-07-05 12:00:29 -0500

    Fix path to fixtures.rake

--- spec/rake/sufia_fixtures_spec.rb
+++ spec/rake/sufia_fixtures_spec.rb
@@ -1,17 +1,3 @@
-# Copyright © 2012 The Pennsylvania State University
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 # Used to test the sufia-fixtures rake task
 #
 require "spec_helper"
@@ -57,7 +43,7 @@ describe "sufia:fixtures" do
     @rake = Rake::Application.new 
     Rake.application = @rake
     Rake.application.rake_require("sufia-fixtures", ["#{Sufia::Engine.root}/tasks/"], loaded_files_excluding_current_rake_file)
-    Rake.application.rake_require("lib/tasks/fixtures", ["."], loaded_files_excluding_current_rake_file)
+    Rake.application.rake_require("fixtures", ["#{Sufia::Engine.root}/lib/tasks/"], loaded_files_excluding_current_rake_file)
     Rake.application.rake_require("lib/tasks/active_fedora", [activefedora_path], loaded_files_excluding_current_rake_file)      
     Rake::Task.define_task(:environment)
   end
