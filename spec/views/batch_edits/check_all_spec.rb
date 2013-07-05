c203059119b89db5c73b415a99f0308c08d5f81e
Justin Coyne <justin@curationexperts.com>
2013-07-05 11:25:20 -0500

    check all view, should supply the controller

--- spec/views/batch_edits/check_all_spec.rb
+++ spec/views/batch_edits/check_all_spec.rb
@@ -8,12 +8,14 @@ describe 'Check All' do
   end
 
   it 'should render batch edits actions' do
+    controller.stub(:controller_name).and_return('batch_edits')
     controller.prepend_view_path "app/views/batch_edits"
     html = render partial: 'batch_edits/check_all'
     html.should have_selector("li[data-behavior='batch-edit-select-abc']")
   end
 
   it 'should render dashboard actions' do
+    controller.stub(:controller_name).and_return('dashboard')
     controller.prepend_view_path "app/views/dashboard"
     html = render partial: 'batch_edits/check_all'
     html.should have_selector("li[data-behavior='batch-edit-select-none']")
