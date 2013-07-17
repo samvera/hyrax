79f5d55328fc73fc77502d06b42f0871a2ca4c5d
Justin Coyne <justin@curationexperts.com>
2013-07-17 11:06:00 -0500

    render_selected_facet_value ought to use facet_display_value

--- hydra-core/spec/helpers/facets_helper_spec.rb
+++ hydra-core/spec/helpers/facets_helper_spec.rb
@@ -10,11 +10,16 @@ describe FacetsHelper do
   
   describe "render_selected_facet_value" do
     it "should be html_safe and not have the remove link" do
-      item = stub("item", :value=>'two', :hits=>9)
-
+      item = double("item", :value=>'two', :hits=>9)
       ret_val = helper.render_selected_facet_value("one", item)
       ret_val.should == "<span class=\"selected\">two <span class=\"count\">9</span></span>"
       ret_val.should be_html_safe
+    end
+    it "should use facet_display_value" do
+      item = double("item", :value=>'two', :hits=>9)
+      helper.stub(:facet_display_value).and_return('four')
+      ret_val = helper.render_selected_facet_value("one", item)
+      ret_val.should == "<span class=\"selected\">four <span class=\"count\">9</span></span>"
     end
   end
 end
