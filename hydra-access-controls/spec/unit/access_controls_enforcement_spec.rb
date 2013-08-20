30e2ec2684280d7b0e3745fd2d8ef6d8a4c963b1
Chris Colvard <cjcolvar@indiana.edu>
2013-08-20 15:28:23 -0400

    Escape spaces in group names in solr query conditions

--- hydra-access-controls/spec/unit/access_controls_enforcement_spec.rb
+++ hydra-access-controls/spec/unit/access_controls_enforcement_spec.rb
@@ -140,6 +140,14 @@ describe Hydra::AccessControlsEnforcement do
         @solr_parameters[:fq].first.should match(/#{type}_access_group_ssim\:cde\\\/567/)        
       end
     end
+    it "should escape spaces in the group names" do
+      RoleMapper.stub(:roles).with(@stub_user.user_key).and_return(["abc 123","cd/e 567"])
+      subject.send(:apply_gated_discovery, @solr_parameters, @user_parameters)
+      ["discover","edit","read"].each do |type|
+        @solr_parameters[:fq].first.should match(/#{type}_access_group_ssim\:abc\\ 123/)
+        @solr_parameters[:fq].first.should match(/#{type}_access_group_ssim\:cd\\\/e\\ 567/)
+      end
+    end
   end
   
   describe "exclude_unwanted_models" do
