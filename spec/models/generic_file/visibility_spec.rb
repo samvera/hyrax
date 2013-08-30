11cc9a3a726746e966aabc07bd61aa9b53f3fae2
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-08-30 16:21:09 -0400

    Removing ActiveModel::Dirty
    This was causing grief in inheritance cases.

---
+++ spec/models/generic_file/visibility_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe Sufia::GenericFile::Visibility do
+  module VisibilityOverride
+    extend ActiveSupport::Concern
+    include Sufia::GenericFile::Permissions
+    def visibility; super; end
+    def visibility=(value); super(value); end
+  end
+  class MockParent < ActiveFedora::Base
+    include VisibilityOverride
+  end
+
+  it 'allows for overrides of visibility' do
+    expect{
+      MockParent.new(visibility: Sufia::Models::AccessRight::VISIBILITY_TEXT_VALUE_PRIVATE)
+    }.to_not raise_error
+  end
+end
