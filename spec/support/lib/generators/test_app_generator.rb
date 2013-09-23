5ea8163d785b670b2b431f4bc12cd68ef5ce0fa9
Justin Coyne <justin@curationexperts.com>
2013-09-23 17:44:54 -0500

    Don't run blacklight and hydra generator twice

--- spec/support/lib/generators/test_app_generator.rb
+++ spec/support/lib/generators/test_app_generator.rb
@@ -3,18 +3,6 @@ require 'rails/generators'
 class TestAppGenerator < Rails::Generators::Base
   source_root File.expand_path("../../../../support", __FILE__)
 
-  def run_blacklight_generator
-    say_status("warning", "GENERATING BL", :yellow)       
-
-    generate 'blacklight', '--devise'
-  end
-
-  def run_hydra_head_generator
-    say_status("warning", "GENERATING HH", :yellow)       
-
-    generate 'hydra:head', '-f'
-  end
-
   def run_sufia_generator
     say_status("warning", "GENERATING SUFIA", :yellow)       
 
