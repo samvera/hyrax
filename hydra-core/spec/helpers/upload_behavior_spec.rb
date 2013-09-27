3fcb8e1574f5522632829f5f43f9f03d1ee8861c
Justin Coyne <justin@curationexperts.com>
2013-09-27 08:52:23 -0500

    Substituted double for the now deprecated mock() and stub()

--- hydra-core/spec/helpers/upload_behavior_spec.rb
+++ hydra-core/spec/helpers/upload_behavior_spec.rb
@@ -8,28 +8,28 @@ describe Hydra::Controller::UploadBehavior do
 
   describe "add_posted_blob_to_asset" do
     it "should set object title and label, relying on datastream_id to set dsId" do
-      mock_file = mock("File")
+      mock_file = double("File")
       file_name = "Posted Filename.foo"
       helper.stub(:params).and_return( :Filedata=>[mock_file], :Filename=>file_name, "container_id"=>"hydrangea:2973" )      
-      mock_fa = mock("file asset")
+      mock_fa = double("file asset")
       helper.stub(:datastream_id).and_return('bar')
       mock_fa.should_receive(:add_file).with(mock_file, 'bar', file_name)
       helper.add_posted_blob_to_asset(mock_fa,mock_file, file_name) # this is the deprecated 2 argument method
     end
     
     it "should support submissions from swfupload" do
-      mock_file = mock("File")
+      mock_file = double("File")
       file_name = "Posted Filename.foo"
       helper.stub(:params).and_return( :Filedata=>[mock_file], :Filename=>file_name, "container_id"=>"hydrangea:2973" )      
-      mock_fa = mock("file asset")
+      mock_fa = double("file asset")
       mock_fa.should_receive(:add_file).with(mock_file, 'content', file_name)
       helper.add_posted_blob_to_asset(mock_fa,mock_file, file_name)
     end
     it "should support submissions from single-file uploader, defaulting to dsId of content" do
-      mock_file = mock("File")
+      mock_file = double("File")
       file_name = "Posted Filename.foo"
       helper.stub(:params).and_return( :Filedata=>[mock_file], :container_id=>"hydrangea:2973" )      
-      mock_fa = mock("file asset")
+      mock_fa = double("file asset")
       mock_fa.should_receive(:add_file).with(mock_file, 'content', file_name)
       helper.add_posted_blob_to_asset(mock_fa,mock_file, file_name)
     end
