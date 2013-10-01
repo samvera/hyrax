54cac89c1dca8934f5e601e50b637a5f25997b26
Justin Coyne <justin@curationexperts.com>
2013-10-01 11:53:11 -0500

    Deprecate Hydra::ModelMixins::SolrDocumentExtension

--- hydra-core/lib/hydra/model_mixins/solr_document_extension.rb
+++ hydra-core/lib/hydra/model_mixins/solr_document_extension.rb
@@ -1,5 +1,8 @@
 module Hydra::ModelMixins
   module SolrDocumentExtension
+    extend Deprecation
+    self.deprecation_horizon = 'hydra-head 7.0.0'
+
     def document_type display_type = CatalogController.blacklight_config.show.display_type
       type = self.fetch(:medium_t, nil)
 
@@ -7,6 +10,7 @@ module Hydra::ModelMixins
 
       type.first.to_s.gsub("info:fedora/afmodel:","").gsub("Hydrangea","").gsub(/^Generic/,"")
     end
+    deprecation_deprecate :document_type
 
     def get_person_from_role(role, opts={})
       i = 0
@@ -18,9 +22,12 @@ module Hydra::ModelMixins
         i += 1
       end
     end
+    deprecation_deprecate :get_person_from_role
+
     def get_file_asset_count()
       ActiveFedora::Base.count(:conditions=>"is_part_of_t:#{ActiveFedora::Base.quote_for_solr(id)}")
     end
+    deprecation_deprecate :get_file_asset_count
   end
 end
 
