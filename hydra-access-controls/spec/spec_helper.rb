b6edba1bc2baceeea3d7d3988dad5a32d00a8427
dchandekstark <dchandekstark@gmail.com>
2013-11-15 20:14:12 -0500

    Make Hydra::Datastream::RightsMetadata#to_solr use Hydra config to get Solr field names. Make Hydra::Datastream::InheritableRightsMetadata#to_solr use Hydra config to get Solr field names. Added permissions and inherited permissions field names to stub Hydra config in hydra-access-controls spec_helper.

--- hydra-access-controls/spec/spec_helper.rb
+++ hydra-access-controls/spec/spec_helper.rb
@@ -5,12 +5,45 @@ require 'rspec/autorun'
 require 'hydra-access-controls'
 
 module Hydra
-  # Stubbing Hydra.config[:policy_aware] so Hydra::PolicyAwareAbility will be loaded for tests.
   def self.config
-    {:permissions=>{:policy_aware => true}}
+    indexer = Solrizer::Descriptor.new(:string, :stored, :indexed, :multivalued)
+    {
+      :permissions => {
+        # Stubbing Hydra.config[:policy_aware] so Hydra::PolicyAwareAbility will be loaded for tests.
+        :policy_aware => true,
+        :discover => {
+          :group => ActiveFedora::SolrService.solr_name("discover_access_group", indexer), 
+          :individual => ActiveFedora::SolrService.solr_name("discover_access_person", indexer)
+        },
+        :read => {
+          :group => ActiveFedora::SolrService.solr_name("read_access_group", indexer), 
+          :individual => ActiveFedora::SolrService.solr_name("read_access_person", indexer)
+        },
+        :edit => {
+          :group => ActiveFedora::SolrService.solr_name("edit_access_group", indexer), 
+          :individual => ActiveFedora::SolrService.solr_name("edit_access_person", indexer)
+        },
+        :embargo_release_date => ActiveFedora::SolrService.solr_name("embargo_release_date", Solrizer::Descriptor.new(:date, :stored, :indexed)),
+        
+        :inheritable => {
+          :discover => {
+            :group => ActiveFedora::SolrService.solr_name("inheritable_discover_access_group", indexer), 
+            :individual => ActiveFedora::SolrService.solr_name("inheritable_discover_access_person", indexer)
+          },
+          :read => {
+            :group => ActiveFedora::SolrService.solr_name("inheritable_read_access_group", indexer), 
+            :individual => ActiveFedora::SolrService.solr_name("inheritable_read_access_person", indexer)
+          },
+          :edit => {
+            :group => ActiveFedora::SolrService.solr_name("inheritable_edit_access_group", indexer), 
+            :individual => ActiveFedora::SolrService.solr_name("inheritable_edit_access_person", indexer)
+          },
+          :embargo_release_date => ActiveFedora::SolrService.solr_name("inheritable_embargo_release_date", Solrizer::Descriptor.new(:date, :stored, :indexed))
+        } # inheritable
+      }
+    }
   end
 end
-
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
