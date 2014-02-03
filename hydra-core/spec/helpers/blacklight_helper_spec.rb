d681e0b1a62ac4f40060dcc3c81c1917a9c96316
Justin Coyne <justin@curationexperts.com>
2014-02-03 16:55:57 -0600

    Remove deprecated configuration parameters. Fixes #144

--- hydra-core/spec/helpers/blacklight_helper_spec.rb
+++ hydra-core/spec/helpers/blacklight_helper_spec.rb
@@ -4,7 +4,7 @@ describe BlacklightHelper do
   describe "document_partial_name" do
     it "should lop off everything before the first colin after the slash" do
       @config = Blacklight::Configuration.new.configure do |config|
-        config.show.display_type = 'has_model_s'
+        config.show.display_type_field = 'has_model_s'
       end
       helper.stub(:blacklight_config).and_return(@config)
       helper.document_partial_name('has_model_s' => ["info:fedora/afmodel:Presentation"]).should == "presentation"
@@ -12,7 +12,7 @@ describe BlacklightHelper do
     end
     it "should support single valued fields" do
       @config = Blacklight::Configuration.new.configure do |config|
-        config.show.display_type = 'active_fedora_model_ssi'
+        config.show.display_type_field = 'active_fedora_model_ssi'
       end
       helper.stub(:blacklight_config).and_return(@config)
       helper.document_partial_name('active_fedora_model_ssi' => "Chicken").should == "chicken" 
@@ -20,7 +20,7 @@ describe BlacklightHelper do
 
     it "should handle periods" do
       @config = Blacklight::Configuration.new.configure do |config|
-        config.show.display_type = 'has_model_s'
+        config.show.display_type_field = 'has_model_s'
       end
       helper.stub(:blacklight_config).and_return(@config)
       helper.document_partial_name('has_model_s' => ["info:fedora/afmodel:text.PDF"]).should == "text_pdf"
