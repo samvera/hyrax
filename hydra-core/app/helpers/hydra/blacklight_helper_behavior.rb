8e37672b072ee4ebfcdae330fadab8cf1cfb3554
Justin Coyne <justin@curationexperts.com>
2014-03-17 10:22:49 -0500

    Remove override of BlacklightHelper#add_facet_params_and_redirect It's not relevant as of Blacklight 5.

--- hydra-core/app/helpers/hydra/blacklight_helper_behavior.rb
+++ hydra-core/app/helpers/hydra/blacklight_helper_behavior.rb
@@ -18,23 +18,5 @@ module Hydra
 
       Array(display_type).first.gsub(/^[^\/]+\/[^:]+:/,"").gsub(/\./, '_').underscore
     end    
-
-  #   COPIED from vendor/plugins/blacklight/app/helpers/application_helper.rb
-    # Used in catalog/facet action, facets.rb view, for a click
-    # on a facet value. Add on the facet params to existing
-    # search constraints. Remove any paginator-specific request
-    # params, or other request params that should be removed
-    # for a 'fresh' display. 
-    # Change the action to 'index' to send them back to
-    # catalog/index with their new facet choice. 
-    def add_facet_params_and_redirect(field, value)
-      new_params = super
-
-      # Delete :qt, if needed - added to resolve NPE errors
-      new_params.delete(:qt)
-
-      new_params
-    end
-
   end
 end
