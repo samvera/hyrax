f0c84a1ee8252a8cecd98fb79c2bec8d08f32716
Justin Coyne <justin@curationexperts.com>
2014-03-20 21:22:21 -0500

    Upgrade to blacklight 5.3 so that we only need to override the smallest bit of code to produce the correct document partials

--- hydra-core/app/helpers/hydra/blacklight_helper_behavior.rb
+++ hydra-core/app/helpers/hydra/blacklight_helper_behavior.rb
@@ -2,21 +2,20 @@ module Hydra
   module BlacklightHelperBehavior
     include Blacklight::BlacklightHelperBehavior
     
+    ##
     # Given a Fedora uri, generate a reasonable partial name
     # Rails thinks that periods indicate a filename, so escape them with slashes.
-    # @param [Hash] document the solr document (hash of fields & values)
-    # @return [String] the name for the display partial
+    #
+    # @param [SolrDocument] document
+    # @param [String, Array] display_type a value suggestive of a partial
+    # @return [String] the name of the partial to render
     # @example
-    #   document_partial_name('has_model_s' => ["info:fedora/hull-cModel:genericContent"])
-    #    => "generic_content"
-    #   document_partial_name('has_model_s' => ["info:fedora/hull-cModel:text.pdf"])
-    #    => "text_pdf"
-    def document_partial_name(document)
-      display_type = document[blacklight_config.show.display_type_field]
-
-      return 'default' unless display_type 
-
-      Array(display_type).first.gsub(/^[^\/]+\/[^:]+:/,"").gsub(/\./, '_').underscore
-    end    
+    #   type_field_to_partial_name(["info:fedora/hull-cModel:genericContent"])
+    #   => 'generic_content'
+    #   type_field_to_partial_name(["info:fedora/hull-cModel:text.pdf"])
+    #   => 'text_pdf'
+    def type_field_to_partial_name(document, display_type)
+      Array(display_type).first.gsub(/^[^\/]+\/[^:]+:/,"").gsub("-","_").underscore.parameterize("_")
+    end
   end
 end
