75953388166fe44514bdd61ae30ebd41d1c72f75
Justin Coyne <justin@curationexperts.com>
2014-04-01 20:57:52 -0500

    Update to hydra-head 7.0.1

--- lib/sufia/downloads_controller_behavior.rb
+++ lib/sufia/downloads_controller_behavior.rb
@@ -9,17 +9,6 @@ module Sufia
 
       # moved check into the routine so we can handle the user with no access
       prepend_before_filter :normalize_identifier
-
-      # Catch permission errors
-      # todo: Remove this once Hydra Head 7.0.1 comes out since this should be fixed
-      #       in hydra-head
-      rescue_from CanCan::AccessDenied do |exception|
-        if current_user and current_user.persisted?
-          redirect_to root_url, :alert => exception.message
-        else
-          redirect_to new_user_session_url, :alert => exception.message
-        end
-      end
     end
 
     def datastream_name
--- sufia-models/sufia-models.gemspec
+++ sufia-models/sufia-models.gemspec
@@ -29,9 +29,7 @@ Gem::Specification.new do |spec|
   spec.add_dependency 'rails', '> 4.0.0', '< 5.0'
   spec.add_dependency 'activeresource', "~>4.0.0" # No longer a dependency of rails 4.0
 
-  spec.add_dependency "blacklight", "~> 5.0"
-  spec.add_dependency "hydra-head", "~> 7.0.0"
-  spec.add_dependency "rubydora", "~> 1.7.0"
+  spec.add_dependency "hydra-head", "~> 7.0.1"
   spec.add_dependency 'nest', '~> 1.1.1'
   spec.add_dependency 'resque', '~> 1.23'
   spec.add_dependency 'resque-pool', '0.3.0'
