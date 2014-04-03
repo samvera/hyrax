eb1660c2fb18853968175b1f1f8730cc12961d59
Matt Zumwalt <matt@curationexperts.com>
2014-04-03 15:53:30 -0500

    only render tag cloud section on homepage if a blacklight query has been run. (ie don't render on login page)

--- app/helpers/tag_cloud_helper.rb
+++ app/helpers/tag_cloud_helper.rb
@@ -7,7 +7,6 @@ module TagCloudHelper
       options = options.dup
       options[:granularity] ||= 15.0
       options[:partial] ||= "catalog/tag_cloud"
-      # options[:layout] ||= "facet_layout" unless options.has_key?(:layout)
       options[:locals] ||= {}
       options[:locals][:solr_field] ||= display_facet.name 
       options[:locals][:facet_field] ||= facet_configuration_for_field(display_facet.name)
@@ -28,8 +27,6 @@ module TagCloudHelper
   # Returns the appropriate scale factor for achieving the desired granularity with a given set of items
   def calculate_scale_factor(items, granularity)
     hit_counts = items.map {|i| i.hits}
-    # len = hit_counts.length
-    # median = len % 2 == 1 ? hit_counts[len/2] : (hit_counts[len/2 - 1] + hit_counts[len/2]).to_f / 2
     if hit_counts.max.nil?
       1
     else
