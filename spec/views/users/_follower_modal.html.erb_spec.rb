a992b9b6bfe7ada98ad7f7f9fa96aba9ca57759b
Justin Coyne <justin@curationexperts.com>
2014-04-10 14:07:16 -0500

    Use the User.to_param to craft paths. Fixes #367

---
+++ spec/views/users/_follower_modal.html.erb_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe 'users/_follower_modal.html.erb' do
+  let(:user) { FactoryGirl.create(:user, display_name: "Frank") }
+
+  before do
+    assign :followers, [user]
+  end
+
+  it "should draw user list" do
+    render
+    page = Capybara::Node::Simple.new(rendered)
+    expect(page).to have_link "Frank", href: "/users/#{user.to_param}" 
+  end
+
+end
+
+
