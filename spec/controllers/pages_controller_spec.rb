ee24d72cdf6728053a22a87641a814cf47acf33a
Carolyn Cole <cam156@psu.edu>
2014-04-22 18:07:31 -0400

    Modify how the pages controller and helper work so that you do not have to be logged in before viewing the page.

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,13 +1,25 @@
 require 'spec_helper'
 
 describe PagesController do
-  describe "GET #show" do
-    let(:page) { ContentBlock.create!(name: "about_page", value: "foo bar") }
+  let (:page_name) {"about_page"}
+  context "content exists" do
+    describe "GET #show" do
+      let(:page) { ContentBlock.create!(name: page_name, value: "foo bar") }
 
-    it "should update the node" do
-      get :show, id: page.name
-      expect(response).to be_successful
-      expect(assigns[:page]).to eq page
+      it "should update the node" do
+        get :show, id: page.name
+        expect(response).to be_successful
+        expect(assigns[:page]).to eq page
+      end
+    end
+  end
+  context "content does not exist" do
+    describe "GET #show" do
+      it "should create the node" do
+        get :show, id: "about_page"
+        expect(response).to be_successful
+        expect(assigns[:page]).not_to be_nil
+      end
     end
   end
 end
