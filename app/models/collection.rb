4df0803e2fa106eadebda741e173c5b75cb4b35d
Justin Coyne <justin@curationexperts.com>
2014-05-13 13:33:00 -0500

    Use the local properties datastream to avoid a deprecation warning

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,5 +1,7 @@
 class Collection < ActiveFedora::Base
   include Hydra::Collection
+  # override the default Hydra properties so we don't get a prefix deprecation warning.
+  has_metadata "properties", type: Worthwhile::PropertiesDatastream
   include CurationConcern::CollectionModel
   include Hydra::Collections::Collectible
 
