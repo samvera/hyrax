50701b407badb2d5e88acdbde8c52d58baa87fdd
Justin Coyne <justin@curationexperts.com>
2014-05-13 22:43:24 -0500

    Encapsulate browse_everything upload in a module

---
+++ app/controllers/concerns/sufia/files_controller/browse_everything.rb
@@ -0,0 +1,35 @@
+module Sufia::FilesController
+  module BrowseEverything
+    include ActiveSupport::Concern
+
+    def create 
+      if params[:selected_files].present?
+        create_from_browse_everything(params)
+      else
+        super
+      end
+    end
+
+    protected
+
+      def create_from_browse_everything(params)
+        params[:selected_files].each_pair do |index, file_info| 
+          next if file_info.blank? || file_info["url"].blank?
+          create_file_from_url(file_info["url"])
+        end
+        redirect_to self.class.upload_complete_path( params[:batch_id])
+      end
+      
+      # Generic utility for creating GenericFile from a URL
+      # Used in to import files using URLs from a file picker like browse_everything 
+      def create_file_from_url(url, batch_id=nil)
+        @generic_file = ::GenericFile.new
+        @generic_file.import_url = url
+        @generic_file.label = File.basename(url)
+        create_metadata(@generic_file)
+        Sufia.queue.push(ImportUrlJob.new(@generic_file.pid))
+        @generic_file
+      end
+
+  end
+end
