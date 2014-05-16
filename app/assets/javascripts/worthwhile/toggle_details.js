8e6e1212cc532f463eebb853cfcfa99ace153f4b
Justin Coyne <justin@curationexperts.com>
2014-05-16 14:59:29 -0500

    Make expand details javascript turbolinks safe. Fixes curationexperts/absolute#85

--- app/assets/javascripts/worthwhile/toggle_details.js
+++ app/assets/javascripts/worthwhile/toggle_details.js
@@ -1,5 +1,5 @@
-$(function(){
-  //NOTE: This function is tightly coupled with the catalog index markup
+Blacklight.onLoad(function() {
+//NOTE: This function is tightly coupled with the catalog index markup
   $('.show-details').on('click', function(event){
     event.preventDefault();
     $('.extended-attributes', $(this).parents('li'))
--- app/assets/javascripts/worthwhile/worthwhile.js
+++ app/assets/javascripts/worthwhile/worthwhile.js
@@ -1,32 +1,5 @@
-// This is a manifest file that'll be compiled into worthwhile.js, which will include all the files
-// listed below.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// the compiled file.
-//
-// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
-// GO AFTER THE REQUIRES BELOW.
-//
-//
-//= require jquery-ui-1.9.2/jquery.ui.widget
-//= require jquery-ui-1.9.2/jquery.ui.core
-//= require jquery-ui-1.9.2/jquery.ui.menu
-//= require jquery-ui-1.9.2/jquery.ui.position
-//= require jquery-ui-1.9.2/jquery.ui.autocomplete
-//
-//= require blacklight/blacklight
-//
-//= require bootstrap/dropdown
-//= require bootstrap/button
-//= require bootstrap/collapse
-//= require bootstrap/tooltip
-//= require bootstrap/popover
-//=# require bootstrap-datepicker
-//
-//= require manage_repeating_fields
-//= require toggle_details
-//= require help_modal
-//= require jquery.tokeninput
+//= require worthwhile/manage_repeating_fields
+//= require worthwhile/help_modal
 //= require worthwhile/select_works
 //= require worthwhile/link_users
 //= require worthwhile/link_groups
@@ -34,10 +7,7 @@
 //= require worthwhile/facet_mine
 //= require worthwhile/accept_contributor_agreement
 //= require worthwhile/proxy_submission
-//= require handlebars
-//=# require browse_everything
-//=# require worthwhile/browse_everything_implement
-
+//= require worthwhile/toggle_details
 
 
 // Initialize plugins and Bootstrap dropdowns on jQuery's ready event as well as
