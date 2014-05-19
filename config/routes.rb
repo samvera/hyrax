95e60cf340bdd6ea8a924c90336b241241472986
Matt Zumwalt <matt@curationexperts.com>
2014-05-19 11:58:08 -0500

    Fix permissions routes. refs #78

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,4 @@
 Worthwhile::Engine.routes.draw do
   # mount BrowseEverything::Engine => '/remote_files/browse'
   resources :classify_concerns, only: [:new, :create]
-  namespace :curation_concern, path: :concern do
-    # Worthwhile.configuration.registered_curation_concern_types.map(&:tableize).each do |curation_concern_name|
-    #   namespaced_resources curation_concern_name, except: [:index]
-    # end
-    resources( :permissions, only:[]) do
-      member do
-        get :confirm
-        post :copy
-      end
-    end
-  end
 end
