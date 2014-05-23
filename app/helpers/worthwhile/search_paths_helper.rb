d5fbb0d8c21c2e95a36472ccf923051def75c336
Matt Zumwalt <matt@curationexperts.com>
2014-05-23 17:10:31 -0500

    fixing grid layout in collection show and edit pages refs #134

--- app/helpers/worthwhile/search_paths_helper.rb
+++ app/helpers/worthwhile/search_paths_helper.rb
@@ -1,11 +1,13 @@
 module Worthwhile::SearchPathsHelper
 
-  def search_path_for_my_works
-    main_app.catalog_index_path(:'f[generic_type_sim][]' => 'Work', works: 'mine')
+  def search_path_for_my_works(opts={})
+    params_for_my_works = {:'f[generic_type_sim][]'=>'Work', works: 'mine'}
+    main_app.catalog_index_path(params_for_my_works.merge(opts))
   end
 
-  def search_path_for_my_collections
-    main_app.catalog_index_path(:'f[generic_type_sim][]' => 'Collection', works: 'mine')
+  def search_path_for_my_collections(opts={})
+    params_for_my_collections = {:'f[generic_type_sim][]'=>'Collection', works: 'mine'}
+    main_app.catalog_index_path(params_for_my_collections.merge(opts))
   end
 
 end
