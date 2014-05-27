f2d16f8a779bed9d17585f301a8b5f7aaf46cf3b
Matt Zumwalt <matt@curationexperts.com>
2014-05-27 16:51:16 -0500

    Makes Collections rely on Hydra::Collections refs curationexperts/absolute#134

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,17 +1,6 @@
 class Collection < ActiveFedora::Base
-  include Hydra::Collection
+  include CurationConcern::CollectionModel
+
   # override the default Hydra properties so we don't get a prefix deprecation warning.
   has_metadata "properties", type: Worthwhile::PropertiesDatastream
-  include CurationConcern::CollectionModel
-  include Hydra::Collections::Collectible
-
-  def can_be_member_of_collection?(collection)
-    collection == self ? false : true
-  end
-
-  def to_solr(solr_doc={}, opts={})
-    super(solr_doc, opts).tap do |solr_doc|
-      index_collection_pids(solr_doc)
-    end
-  end
 end
