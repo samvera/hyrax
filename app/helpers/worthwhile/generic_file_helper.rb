4422bbf88b45ed53e2ba86ef9525769f18a6d95d
val99erie <val99erie@gmail.com>
2014-06-17 16:17:01 -0500

    View changes for representative of a collection.  Story curationexperts/absolute#162

--- app/helpers/worthwhile/generic_file_helper.rb
+++ app/helpers/worthwhile/generic_file_helper.rb
@@ -1,11 +1,19 @@
 module Worthwhile::GenericFileHelper
 
-    def generic_file_title(gf)
-      can?(:read, gf) ? gf.to_s : "File"
-    end
+  def generic_file_title(gf)
+    can?(:read, gf) ? gf.to_s : "File"
+  end
 
-    def generic_file_link_name(gf)
-      can?(:read, gf) ? gf.filename : "File"
+  def generic_file_link_name(gf)
+    can?(:read, gf) ? gf.filename : "File"
+  end
+
+  def parent_path(parent)
+    if parent.is_a?(Collection)
+      collection_path(parent)
+    else
+      polymorphic_path([:curation_concern, parent])
     end
+  end
     
 end
