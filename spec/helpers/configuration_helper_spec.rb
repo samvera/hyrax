5ff786b8ab9dfbeec25e72f17caaf4325cb8a79e
Justin Coyne <justin@curationexperts.com>
2014-06-24 09:29:24 -0500

    reorder the navbar so that "All" is first

---
+++ spec/helpers/configuration_helper_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe Blacklight::ConfigurationHelperBehavior do
+
+  describe "creator facet" do
+    before do
+      allow(helper).to receive(:blacklight_config).and_return(CatalogController.blacklight_config)
+    end
+    let(:field) { 'desc_metadata__creator_sim' }
+    subject { helper.facet_field_label field }
+    it { should eq 'Creator' }
+  end
+end
