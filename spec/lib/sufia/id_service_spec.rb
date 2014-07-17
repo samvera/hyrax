4b5a4dd936fb3cee223fa1d1d0d642050e4e1e98
Justin Coyne <justin@curationexperts.com>
2014-07-16 22:41:01 -0500

    Override assign_pid rather than monkeypatch ActiveFedora

--- spec/lib/sufia/id_service_spec.rb
+++ spec/lib/sufia/id_service_spec.rb
@@ -2,15 +2,33 @@ require 'spec_helper'
 
 describe Sufia::IdService do
   describe "mint" do
-    before(:all) do
-      @id = Sufia::IdService.mint
-    end
-    it "should create a unique id" do
-      @id.should_not be_empty
-    end
+    subject { Sufia::IdService.mint }
+
+    it { should_not be_empty }
+
     it "should not mint the same id twice in a row" do
-      other_id = Sufia::IdService.mint
-      other_id.should_not == @id
+      expect(Sufia::IdService.mint).to_not eq subject
+    end
+
+    it "should be valid" do    
+      expect(Sufia::IdService.valid?(subject)).to be true
+    end
+
+    context "when the pid already exists in Fedora" do
+      let(:mock_pid) { 'scholarsphere:ef12ef12f' }
+      let(:unique_pid) {  'scholarsphere:bb22bb22b' }
+
+      before do
+        allow(Sufia::IdService).to receive(:next_id).and_return(mock_pid, unique_pid)
+        expect(ActiveFedora::Base).to receive(:exists?).with(mock_pid).and_return(true)
+        expect(ActiveFedora::Base).to receive(:exists?).with(unique_pid).and_return(false)
+      end
+
+      it "should not assign that pid again" do
+        expect(subject).to eq unique_pid
+      end
     end
   end
+
+
 end
