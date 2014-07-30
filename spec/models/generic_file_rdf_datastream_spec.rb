4c81a2d538ba1b81f999d4bb9489829b8bcdd425
Justin Coyne <justin@curationexperts.com>
2014-07-30 10:02:00 -0500

    Fixed GenericFileRdfDatastream spec

--- spec/models/generic_file_rdf_datastream_spec.rb
+++ spec/models/generic_file_rdf_datastream_spec.rb
@@ -1,10 +1,13 @@
 require 'spec_helper'
 
-describe GenericFileRdfDatastream, :type => :model do
+describe GenericFileRdfDatastream do
+  subject { GenericFileRdfDatastream.new(double('base object', uri: '/test/foo', new_record?: true), 'descMetadata') }
+
   it "should have bibliographicCitation" do
     subject.bibliographic_citation = "foo"
     expect(subject.bibliographic_citation).to eq ["foo"]
   end
+
   it "should have source" do
     subject.source = "foo"
     expect(subject.source).to eq ["foo"]
