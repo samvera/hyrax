4527a3121519601189c303f5c99bc4705ef58a0d
Justin Coyne <justin@curationexperts.com>
2014-07-30 08:25:23 -0500

    Removed Persistence module from spec directory

--- spec/active_fedora/persistence.rb
+++
@@ -1,9 +0,0 @@
-module ActiveFedora
-  # = Active Fedora Persistence
-  module Persistence
-    #class UnsavedDigitalObject
-    def assign_pid
-      @pid ||= Sufia::IdService.mint
-    end
-  end
-end
