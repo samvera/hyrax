be3bfea8c07aef59bedc65b504c54a4e21dd5cac
Carolyn Cole <cam156@psu.edu>
2014-08-01 14:57:10 -0400

    Adding a check to make sure the check all box exists before checking to see if it has been checked.

--- app/assets/javascripts/sufia/dashboard_actions.js
+++ app/assets/javascripts/sufia/dashboard_actions.js
@@ -2,7 +2,7 @@ Blacklight.onLoad(function() {
   // toggle button on or off based on boxes being clicked
   $(".batch_document_selector, .batch_document_selector_all").bind('click', function(e) {
     var n = $(".batch_document_selector:checked").length;
-    if (n>0 || $('input#check_all')[0].checked) {
+    if (n>0 || ($('input#check_all').length && $('input#check_all')[0].checked)) {
       $('.sort-toggle').hide();
     } else {
       $('.sort-toggle').show();
