dfc84212f9ee91fba0a40a550d02e9ec79847ba5
Justin Coyne <justin@curationexperts.com>
2014-08-07 20:59:19 -0500

    Shared tests should assert that description should be multivalued

--- spec/support/shared/shared_examples_has_dc_metadata.rb
+++ spec/support/shared/shared_examples_has_dc_metadata.rb
@@ -2,12 +2,12 @@ shared_examples 'has_dc_metadata' do
 
   # Single-valued fields
   it { should have_unique_field(:created) }
-  it { should have_unique_field(:description) }
   it { should have_unique_field(:date_uploaded) }
   it { should have_unique_field(:date_modified) }
 
   # Multivalued fields
   it { should have_multivalue_field(:content_format) }
+  it { should have_multivalue_field(:description) }
   it { should have_multivalue_field(:identifier) }
   it { should have_multivalue_field(:publisher) }
   it { should have_multivalue_field(:source) }
