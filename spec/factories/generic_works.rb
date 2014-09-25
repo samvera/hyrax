9cb4d7cc419d3963263800571afd73f8a97277f9
Justin Coyne <justin@curationexperts.com>
2014-09-25 13:42:25 -0500

    Give a confirmation message about embargo if the object is under embargo. Ref curationexperts/absolute#231

--- spec/factories/generic_works.rb
+++ spec/factories/generic_works.rb
@@ -2,6 +2,7 @@ FactoryGirl.define do
   factory :work, aliases: [:generic_work, :private_generic_work], class: GenericWork do
     ignore do
       user { FactoryGirl.create(:user) }
+      embargo_date { Date.tomorrow.to_s }
     end
 
     title ["Test title"]
@@ -21,17 +22,17 @@ FactoryGirl.define do
     end
 
     factory :embargoed_work do
-      after(:build) { |work, _| work.apply_embargo(Date.tomorrow.to_s, Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PRIVATE, Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC) }
+      after(:build) { |work, evaluator| work.apply_embargo(evaluator.embargo_date, Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PRIVATE, Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC) }
     end
 
     factory :embargoed_work_with_files do
       after(:build) { |work, _| 2.times { work.generic_files << FactoryGirl.build(:generic_file) }}
-      after(:build) { |work, _| work.apply_embargo(Date.tomorrow.to_s, Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PRIVATE, Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC) }
+      after(:build) { |work, evaluator| work.apply_embargo(evaluator.embargo_date, Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PRIVATE, Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC) }
     end
 
     factory :leased_work_with_files do
       after(:build) { |work, _| 2.times { work.generic_files << FactoryGirl.build(:generic_file) }}
-      after(:build) { |work, _| work.apply_lease(Date.tomorrow.to_s, Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC, Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PRIVATE) }
+      after(:build) { |work, evaluator| work.apply_lease(evaluator.embargo_date, Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC, Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PRIVATE) }
     end
   end
 end
