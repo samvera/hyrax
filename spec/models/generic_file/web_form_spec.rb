ff1fdc674bed9b6ded82e13626bc9b13e479839c
Justin Coyne <justin@curationexperts.com>
2014-11-04 08:39:13 -0600

    Update test for accessible attributes to include permissions_attributes

--- spec/models/generic_file/web_form_spec.rb
+++ spec/models/generic_file/web_form_spec.rb
@@ -23,7 +23,7 @@ describe GenericFile, :type => :model do
   describe "accessible_attributes" do
     it "should have a list" do
       expect(subject.accessible_attributes).to include(:part_of, :resource_type, :title, :creator, :contributor, :description,
-        :tag, :rights, :publisher, :date_created, :subject, :language, :identifier, :based_near, :related_url, :permissions)
+        :tag, :rights, :publisher, :date_created, :subject, :language, :identifier, :based_near, :related_url, :permissions_attributes)
     end
 
     it "should sanitize them" do
