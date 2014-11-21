0e6be95f571fdbf460d4610754cad1ffe76f4c74
Carolyn Cole <cam156@psu.edu>
2014-11-21 12:59:56 -0500

    Making sure the modal is actually sorted numerically and allow some files to be on the second page for sure by adding the key in more than one time for the first page.

--- spec/features/browse_files_spec.rb
+++ spec/features/browse_files_spec.rb
@@ -7,9 +7,14 @@ describe "Browse files", :type => :feature do
     @fixtures = find_or_create_file_fixtures
     @fixtures[0].tag = ["key"]
     (1..25).each do |i|
-      @fixtures[0].tag << i
+      @fixtures[0].tag << "key_#{i}"
     end
+    @fixtures[1].tag = ["key"]
     @fixtures[0].save
+    (1..20).each do |i|
+      @fixtures[1].tag << "key_#{i}"
+    end
+    @fixtures[1].save
   end
 
   after :all do
@@ -23,21 +28,31 @@ describe "Browse files", :type => :feature do
     click_button "search-submit-header"
     click_link "Keyword"
     click_link "more Keywords»"
+    expect(page).to have_css "h3", text: "Keyword"
   end
 
   describe "when not logged in" do
     it "should let us browse some of the fixtures" do
       click_link "18"
       expect(page).to have_content "Search Results"
+      expect(page).to have_css "a", text: @fixtures[0].title[0]
       click_link @fixtures[0].title[0]
       expect(page).to have_content "Download"
       expect(page).not_to have_content "Edit"
     end
     it "should allow you to click next" do
-      click_link 'Next »'
+      expect(page).to have_content "Numerical Sort"
+      expect(page).to have_css "a.sort_change", text:"Numerical Sort"
+      click_link "Numerical Sort"
       within(".modal-body") do
-        expect(page).to have_content "5"
-        expect(page).not_to have_content "11"
+        expect(page).to have_content "key_1 "
+        expect(page).not_to have_content "key_25 "
+      end
+      click_link 'Next »'
+      expect(page).to have_css "a.btn-link", text:"« Previous", wait: Capybara.default_wait_time*4
+      within(".modal-body") do
+        expect(page).to have_content "key_25 "
+        expect(page).not_to have_content "key_1 "
       end
     end
   end
