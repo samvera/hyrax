917ffe2c636f66fcc5b681f3c2129a37d921f7bf
Justin Coyne <justin@curationexperts.com>
2014-12-03 15:08:30 -0600

    Update rspec syntax

--- hydra-access-controls/spec/unit/role_mapper_spec.rb
+++ hydra-access-controls/spec/unit/role_mapper_spec.rb
@@ -2,27 +2,27 @@ require 'spec_helper'
 
 describe RoleMapper do
   it "should define the 4 roles" do
-    RoleMapper.role_names.sort.should == %w(admin_policy_object_editor archivist donor patron researcher) 
+    expect(RoleMapper.role_names.sort).to eq %w(admin_policy_object_editor archivist donor patron researcher) 
   end
   it "should quer[iy]able for roles for a given user" do
-    RoleMapper.roles('leland_himself@example.com').sort.should == ['archivist', 'donor', 'patron']
-    RoleMapper.roles('archivist2@example.com').should == ['archivist']
+    expect(RoleMapper.roles('leland_himself@example.com').sort).to eq ['archivist', 'donor', 'patron']
+    expect(RoleMapper.roles('archivist2@example.com')).to eq ['archivist']
   end
 
   it "should not change it's response when it's called repeatedly" do
     u = User.new(:uid=>'leland_himself@example.com')
-    u.stub(:new_record?).and_return(false)
-    RoleMapper.roles(u).sort.should == ['archivist', 'donor', 'patron', "registered"]
-    RoleMapper.roles(u).sort.should == ['archivist', 'donor', 'patron', "registered"]
+    allow(u).to receive(:new_record?).and_return(false)
+    expect(RoleMapper.roles(u).sort).to eq ['archivist', 'donor', 'patron', "registered"]
+    expect(RoleMapper.roles(u).sort).to eq ['archivist', 'donor', 'patron', "registered"]
   end
 
   it "should return an empty array if there are no roles" do
-    RoleMapper.roles('zeus@olympus.mt').empty?.should == true
+    expect(RoleMapper.roles('zeus@olympus.mt')).to be_empty
   end
   it "should know who is what" do
-    RoleMapper.whois('archivist').sort.should == %w(archivist1@example.com archivist2@example.com leland_himself@example.com)
-    RoleMapper.whois('salesman').empty?.should == true
-    RoleMapper.whois('admin_policy_object_editor').sort.should == %w(archivist1@example.com)
+    expect(RoleMapper.whois('archivist').sort).to eq %w(archivist1@example.com archivist2@example.com leland_himself@example.com)
+    expect(RoleMapper.whois('salesman')).to be_empty
+    expect(RoleMapper.whois('admin_policy_object_editor').sort).to eq %w(archivist1@example.com)
   end
 
 end
