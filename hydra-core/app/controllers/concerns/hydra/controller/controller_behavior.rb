c6d4207e678031cf150c77a975f56529fce10ae6
Justin Coyne <justin@curationexperts.com>
2015-01-16 08:15:56 -0600

    Refactor how access denied errors are handled This enables applications to override the behavior.

--- hydra-core/app/controllers/concerns/hydra/controller/controller_behavior.rb
+++ hydra-core/app/controllers/concerns/hydra/controller/controller_behavior.rb
@@ -1,10 +1,10 @@
 # Include this module into any of your Controller classes to add Hydra functionality
 #
-# The primary function of this module is to mix in a number of other Hydra Modules, including 
+# The primary function of this module is to mix in a number of other Hydra Modules, including
 #   Hydra::AccessControlsEnforcement
 #
-# @example 
-#  class CustomHydraController < ApplicationController  
+# @example
+#  class CustomHydraController < ApplicationController
 #    include Hydra::Controller::ControllerBehavior
 #  end
 #
@@ -14,25 +14,27 @@ module Hydra::Controller::ControllerBehavior
   included do
     # Other modules to auto-include
     include Hydra::AccessControlsEnforcement
-  
+
     # Catch permission errors
-    rescue_from CanCan::AccessDenied do |exception|
-      if (exception.action == :edit)
-        redirect_to({:action=>'show'}, :alert => exception.message)
-      elsif current_user and current_user.persisted?
-        redirect_to root_path, :alert => exception.message
-      else
-        session["user_return_to"] = request.url
-        redirect_to new_user_session_path, :alert => exception.message
-      end
-    end
+    rescue_from CanCan::AccessDenied, with: :deny_access
   end
-  
-  
+
   # get the currently configured user identifier.  Can be overridden to return whatever (ie. login, email, etc)
   # defaults to using whatever you have set as the Devise authentication_key
   def user_key
     current_user.user_key if current_user
+  end
+
+  # Override this method if you wish to customize the way access is denied
+  def deny_access(exception)
+    if exception.action == :edit
+      redirect_to({ action: 'show' }, alert: exception.message)
+    elsif current_user and current_user.persisted?
+      redirect_to root_path, alert: exception.message
+    else
+      session['user_return_to'.freeze] = request.url
+      redirect_to new_user_session_path, alert: exception.message
+    end
   end
 
   module ClassMethods
