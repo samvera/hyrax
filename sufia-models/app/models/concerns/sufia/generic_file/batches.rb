bfc7a273cabe7fa38fa09b745e40d44508e5cfaf
Justin Coyne <justin@curationexperts.com>
2015-01-21 15:40:51 -0600

    Refactor batch methods

--- sufia-models/app/models/concerns/sufia/generic_file/batches.rb
+++ sufia-models/app/models/concerns/sufia/generic_file/batches.rb
@@ -11,16 +11,15 @@ module Sufia
       # to each file when it is done uploading.  The Batch object is not created
       # until all objects are done uploading and the user is redirected to
       # BatchController#edit.  Therefore, we must handle the case where
-      # self.batch_id is set but self.batch returns nil.
+      # batch_id is set but batch returns nil.
       def related_files
-        return [] if batch.nil?
+        return [] unless batch
         batch.generic_files.reject { |sibling| sibling.id == id }
       end
 
       # Is this file in the middle of being processed by a batch?
       def processing?
-         return false if self.batch.blank? || !self.batch.methods.include?(:status)
-         (!self.batch.status.empty?) && (self.batch.status.count == 1) && (self.batch.status[0] == "processing")
+         try(:batch).try(:status) == ['processing'.freeze]
       end
 
     end
