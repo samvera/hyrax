d3efe8f5d4708abf2f538a54da0e99e1c45c5822
Carolyn Cole <cam156@psu.edu>
2015-02-16 14:51:52 -0500

    Adding a rescue for a TimeoutError as we are seeing those in production

--- sufia-models/lib/sufia/models/resque.rb
+++ sufia-models/lib/sufia/models/resque.rb
@@ -10,11 +10,19 @@ module Sufia
       end
 
       def push(job)
+        push_tries = 0
         queue = job.respond_to?(:queue_name) ? job.queue_name : default_queue_name
         begin
           ::Resque.enqueue_to queue, MarshaledJob, Base64.encode64(Marshal.dump(job))
         rescue Redis::CannotConnectError
           ActiveFedora::Base.logger.error "Redis is down!"
+        rescue Redis::TimeoutError => error
+          ActiveFedora::Base.logger.warn "Redis Timed out.  Trying again! #{job.inspect}"
+          push_tries+=1
+          # fail for good if the tries is greater than 3
+          raise error if push_tries >=3
+          sleep 0.01
+          retry
         end
       end
     end
