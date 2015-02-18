282880a6a3ebe0a44943ef0bed1c3d406b2263c3
Weiwei Shi <weiwei.shi@ualberta.ca>
2015-02-18 00:56:38 -0700

    add class accessors for presenter and form class for batch form and single use link view - follow up to #906

--- app/controllers/concerns/sufia/batch_controller_behavior.rb
+++ app/controllers/concerns/sufia/batch_controller_behavior.rb
@@ -8,6 +8,8 @@ module Sufia
 
       before_filter :has_access?
       ActiveSupport::Deprecation.deprecate_methods(BatchController, :initialize_fields)
+      class_attribute :edit_form_class
+      self.edit_form_class = Sufia::Forms::BatchEditForm 
     end
 
     def edit
@@ -20,7 +22,7 @@ module Sufia
       @batch = Batch.find_or_create(params[:id])
       @batch.status = ["processing"]
       @batch.save
-      file_attributes = Sufia::Forms::BatchEditForm.model_attributes(params[:generic_file])
+      file_attributes = edit_form_class.model_attributes(params[:generic_file])
       Sufia.queue.push(BatchUpdateJob.new(current_user.user_key, params[:id], params[:title], file_attributes, params[:visibility]))
       flash[:notice] = 'Your files are being processed by ' + t('sufia.product_name') + ' in the background. The metadata and access controls you specified are being applied. Files will be marked <span class="label label-danger" title="Private">Private</span> until this process is complete (shouldn\'t take too long, hang in there!). You may need to refresh your dashboard to see these updates.'
       if uploading_on_behalf_of? @batch
@@ -34,7 +36,7 @@ module Sufia
 
     def edit_form
       generic_file = ::GenericFile.new(creator: [current_user.name], title: @batch.generic_files.map(&:label))
-      Sufia::Forms::BatchEditForm.new(generic_file)
+      edit_form_class.new(generic_file)
     end
 
     # override this method if you need to initialize more complex RDF assertions (b-nodes)
