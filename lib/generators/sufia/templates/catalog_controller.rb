071f7159489b9dabdfb3cc291128a7a03a48a2a7
Adam Wead <amsterdamos@gmail.com>
2015-02-25 15:12:18 -0500

    Remove arrow in relevance sorting label
    Fixes issue reporting in #914 by updating the catalog_controller
    generator to match the current one in Blacklight.

--- lib/generators/sufia/templates/catalog_controller.rb
+++ lib/generators/sufia/templates/catalog_controller.rb
@@ -304,7 +304,7 @@ class CatalogController < ApplicationController
     # whether the sort is ascending or descending (it must be asc or desc
     # except in the relevancy case).
     # label is key, solr field is value
-    config.add_sort_field "score desc, #{uploaded_field} desc", label: "relevance \u25BC"
+    config.add_sort_field "score desc, #{uploaded_field} desc", label: "relevance"
     config.add_sort_field "#{uploaded_field} desc", label: "date uploaded \u25BC"
     config.add_sort_field "#{uploaded_field} asc", label: "date uploaded \u25B2"
     config.add_sort_field "#{modified_field} desc", label: "date modified \u25BC"
