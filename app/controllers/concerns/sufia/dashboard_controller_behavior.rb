159f2c9534275da65c493f7f3709bbe2a4ab1c6e
Michael J. Giarlo <leftwing@alumni.rutgers.edu>
2015-03-10 09:10:15 -0700

    Dashboard controller should display incoming and outgoing transfers.

--- app/controllers/concerns/sufia/dashboard_controller_behavior.rb
+++ app/controllers/concerns/sufia/dashboard_controller_behavior.rb
@@ -1,7 +1,7 @@
 module Sufia
   module DashboardControllerBehavior
     extend ActiveSupport::Concern
-    
+
     included do
       include ActionView::Helpers::DateHelper
 
@@ -34,6 +34,8 @@ module Sufia
       @user = current_user
       @activity = current_user.get_all_user_activity(params[:since].blank? ? DateTime.now.to_i - Sufia.config.activity_to_show_default_seconds_since_now : params[:since].to_i)
       @notifications = current_user.mailbox.inbox
+      @incoming = ProxyDepositRequest.where(receiving_user_id: current_user.id).reject &:deleted_file?
+      @outgoing = ProxyDepositRequest.where(sending_user_id: current_user.id)
     end
 
     # Formats the user's activities into human-readable strings used for rendering JSON
