486d5f2aa100e39c0b34c8f490908933ae18bc58
Adam Wead <amsterdamos@gmail.com>
2015-03-26 10:55:31 -0400

    Updating view test, fixes #991

--- spec/views/homepage/_home_header.html.erb_spec.rb
+++ spec/views/homepage/_home_header.html.erb_spec.rb
@@ -1,13 +1,13 @@
 require 'spec_helper'
 
-RSpec.describe "homepage/_home_header.html.erb" do
+describe "homepage/_home_header.html.erb" do
   let(:groups) { [] }
   let(:ability) { instance_double("Ability") }
   describe "share your work button" do
     before do
       allow(controller).to receive(:current_ability).and_return(ability)
       allow(ability).to receive(:can?).with(:view_share_work, GenericFile).and_return(can_view_share_work)
-      stub_template "homepage/_marketing" => "marketing"
+      stub_template "homepage/_marketing.html.erb" => "marketing"
       render
     end
     context "when the user can view" do
