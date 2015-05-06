d7599560bcc13c86dfd3cc8c7caa3a4c3d5e48a5
Yinlin Chen <ylchen@vt.edu>
2015-05-06 14:44:11 -0400

    Fixes #993
    remove Sufia::Works::CurationConcern::WithBasicMetadata

--- spec/models/works/generic_work_spec.rb
+++ spec/models/works/generic_work_spec.rb
@@ -41,5 +41,29 @@ describe Sufia::Works::GenericWork do
       expect { work.destroy }.to change{ GenericFile.count }.by(-2)
     end
   end
+
+  describe "metadata" do
+    it "should have descriptive metadata" do
+      expect(subject).to respond_to(:relative_path)
+      expect(subject).to respond_to(:depositor)
+      expect(subject).to respond_to(:related_url)
+      expect(subject).to respond_to(:based_near)
+      expect(subject).to respond_to(:part_of)
+      expect(subject).to respond_to(:contributor)
+      expect(subject).to respond_to(:creator)
+      expect(subject).to respond_to(:title)
+      expect(subject).to respond_to(:description)
+      expect(subject).to respond_to(:publisher)
+      expect(subject).to respond_to(:date_created)
+      expect(subject).to respond_to(:date_uploaded)
+      expect(subject).to respond_to(:date_modified)
+      expect(subject).to respond_to(:subject)
+      expect(subject).to respond_to(:language)
+      expect(subject).to respond_to(:rights)
+      expect(subject).to respond_to(:resource_type)
+      expect(subject).to respond_to(:identifier)
+    end
+  end
+
 end
 
--- sufia-models/app/models/sufia/works/generic_work.rb
+++ sufia-models/app/models/sufia/works/generic_work.rb
@@ -1,7 +1,7 @@
 # a very simple type of work with DC metadata
 module Sufia::Works
   class GenericWork < Work
-    include Sufia::Works::CurationConcern::WithBasicMetadata
     include Sufia::GenericFile::Permissions
+    include Sufia::GenericFile::Metadata
   end
 end
