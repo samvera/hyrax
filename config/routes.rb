little9/sufia

Access Rights Should be copied to all the files in the work. Fixes #1001

config/routes.rb
@@ -112,6 +112,16 @@ Sufia::Engine.routes.draw do
   post 'contact' => 'contact_form#create', as: :contact_form_index
   get 'contact' => 'contact_form#new'
 
+  # Permissions routes
+  namespace :curation_concern, path: :concern do
+    resources( :permissions, only:[]) do
+      member do
+        get :confirm
+        post :copy_visibility
+      end
+    end
+  end
+  
   mount Hydra::Collections::Engine => '/'
 
   # Resque monitoring routes. Don't bother with this route unless Sufia::ResqueAdmin

spec/controllers/curation_concern/permissions_controller_spec.rb
@@ -1,0 +1,28 @@
+require 'spec_helper'
+
+describe CurationConcern::PermissionsController do
+  let(:user) { FactoryGirl.create(:user) }
+  before { sign_in user }
+
+  describe "#confirm" do
+    let(:generic_work) { FactoryGirl.create(:generic_work, user: user) }
+
+    it "should draw the page" do
+      get :confirm, id: generic_work
+      expect(response).to be_success
+    end
+  end
+
+  describe "#copy_visibility" do
+    let(:generic_work) { FactoryGirl.create(:generic_work, user: user) }
+    it "should add a worker to the queue" do
+      worker = double
+      CopyVisibilityJob.should_receive(:new).with(generic_work.id).and_return(worker)
+      Sufia.queue.should_receive(:push).with(worker)
+      post :copy_visibility, id: generic_work
+      expect(response).to redirect_to Sufia::Engine.routes.url_helpers.generic_work_path(generic_work)
+      expect(flash[:notice]).to eq 'Updating file permissions. This may take a few minutes. You may want to refresh your browser or return to this record later to see the updated file permissions.'
+    end
+  end
+
+end

samvera/hyrax

config/routes.rb
@@ -112,6 +112,16 @@ Sufia::Engine.routes.draw do
   post 'contact' => 'contact_form#create', as: :contact_form_index
   get 'contact' => 'contact_form#new'
 
+  # Permissions routes
+  namespace :curation_concern, path: :concern do
+    resources( :permissions, only:[]) do
+      member do
+        get :confirm
+        post :copy
+      end
+    end
+  end
+  
   mount Hydra::Collections::Engine => '/'
 
   # Resque monitoring routes. Don't bother with this route unless Sufia::ResqueAdmin

spec/controllers/curation_concern/permissions_controller_spec.rb
@@ -1,0 +1,28 @@
+require 'spec_helper'
+
+describe CurationConcern::PermissionsController do
+  let(:user) { FactoryGirl.create(:user) }
+  before { sign_in user }
+  render_views
+
+  describe "#confirm" do
+    let(:generic_work) { FactoryGirl.create(:generic_work, user: user) }
+    it "should draw the page" do
+      get :confirm, id: generic_work
+      expect(response).to be_success
+    end
+  end
+
+  describe "#copy_visibility" do
+    let(:generic_work) { FactoryGirl.create(:generic_work, user: user) }
+    it "should add a worker to the queue" do
+      worker = double
+      CopyPermissionsJob.should_receive(:new).with(generic_work.id).and_return(worker)
+      Sufia.queue.should_receive(:push).with(worker)
+      post :copy, id: generic_work
+      expect(response).to redirect_to Sufia::Engine.routes.url_helpers.generic_work_path(generic_work)
+      expect(flash[:notice]).to eq 'Updating file permissions. This may take a few minutes. You may want to refresh your browser or return to this record later to see the updated file permissions.'
+    end
+  end
+
+end