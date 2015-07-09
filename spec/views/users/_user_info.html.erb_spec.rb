52fbb52f8e29ae2b6b7c9706254b6bc09e94aa69
Michael J. Giarlo <leftwing@alumni.rutgers.edu>
2015-07-09 10:31:49 -0700

    Check to make sure Zotero integration is enabled before asking user instances if they have a Zotero user ID. Fixes #1235

---
+++ spec/views/users/_user_info.html.erb_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe 'users/_user_info.html.erb', type: :view do
+  let(:user) { stub_model(User, user_key: 'jdoe42') }
+
+  context 'with Zotero disabled' do
+    before do
+      allow(Sufia.config).to receive(:arkivo_api) { false }
+      allow(user).to receive(:zotero_userid).and_raise(NoMethodError)
+      render "users/user_info", user: user
+    end
+
+    it 'does not display a Zotero profile link' do
+      expect(rendered).not_to match(/Zotero Profile/)
+    end
+  end
+
+  context 'with Zotero enabled' do
+    before do
+      allow(Sufia.config).to receive(:arkivo_api) { true }
+      allow(user).to receive(:zotero_userid) { 'jdoe42zotero' }
+      render "users/user_info", user: user
+    end
+
+    it 'displays a Zotero profile link' do
+      expect(rendered).to match(/Zotero Profile/)
+    end
+  end
+end
