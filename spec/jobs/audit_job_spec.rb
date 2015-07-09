6144e9e88610459ab00c8dc6f42adf230f8f7030
Eliot Jordan <eliotj@princeton.edu>
2015-07-09 15:50:16 -0400

    update and fix audit job specs

--- spec/jobs/audit_job_spec.rb
+++ spec/jobs/audit_job_spec.rb
@@ -4,74 +4,49 @@ describe AuditJob do
   let(:user) { FactoryGirl.create(:user) }
 
   let(:file) do
-    GenericFile.create do |file|
-      file.add_file(File.open(fixture_path + '/world.png'), path: 'content', original_name: 'world.png')
+    generic_file = GenericFile.create do |file|
       file.apply_depositor_metadata(user)
+      Hydra::Works::AddFileToGenericFile.call(file, fixture_file_path('world.png'), :original_file, versioning: true)
     end
   end
 
   let(:job) { AuditJob.new(file.id, 'content', uri) }
 
   describe "audit on content" do
-    let(:uri) { file.content.uri }
-    xit "should pass" do
+    let(:uri) { file.original_file.uri }
+    it "should pass" do
       expect(job.run).to eq(true)
     end
   end
 
   describe "audit on a version of the content" do
-    let(:uri) { file.content.versions.first.uri }
-    xit "should pass" do
+    let(:uri) { CurationConcerns::VersioningService.latest_version_of(file.original_file).uri }
+    it "should pass" do
       expect(job.run).to eq(true)
     end
   end
 
   describe "audit on an invalid version of the content" do
-    let(:uri) { file.content.versions.first.uri + "bogus" }
-    xit "should fail" do
+    let(:uri) { CurationConcerns::VersioningService.latest_version_of(file.original_file).uri + "bogus" }
+    it "should fail" do
       expect(job.run).to eq(false)
     end
   end
 
-  ### TODO - Needs Refactoring after Sufia is broken up
-  # describe "sending mail" do
-  #   let(:uri) { file.content.uri }
-  #   let(:inbox) { user.mailbox.inbox }
-  #
-  #   before do
-  #     allow_any_instance_of(ActiveFedora::FixityService).to receive(:check).and_return(result)
-  #     job.run
-  #   end
-  #
-  #   context "when the audit passes" do
-  #     let(:result) { true }
-  #     xit "should not send mail" do
-  #       expect(inbox.count).to eq(0)
-  #     end
-  #   end
-  #   context "when the audit fails" do
-  #     let(:result) { false }
-  #     xit "should send failing mail" do
-  #       expect(inbox.count).to eq(1)
-  #       inbox.each { |msg| expect(msg.last_message.subject).to eq(AuditJob::FAIL) }
-  #     end
-  #   end
-  # end
-  #
-  # describe "run_audit" do
-  #   let(:uri) { file.content.versions.first.uri }
-  #   let!(:old) { ChecksumAuditLog.create(generic_file_id: file.id, dsid: 'content', version: uri, pass: 1, created_at: 2.minutes.ago) }
-  #   let!(:new) { ChecksumAuditLog.create(generic_file_id: file.id, dsid: 'content', version: uri, pass: 0) }
-  #   let(:mock_service) { double('mock fixity check service') }
-  #
-  #   before do
-  #     allow(ActiveFedora::FixityService).to receive(:new).and_return(mock_service)
-  #     allow(mock_service).to receive(:check).and_return(true, false, false, true, false)
-  #   end
-  #
-  #   xit "should not prune failed audits" do
-  #     5.times { job.send(:run_audit) }
-  #     expect(ChecksumAuditLog.logs_for(file.id, 'content').map(&:pass)).to eq [0, 1, 0, 0, 1, 0, 1]
-  #   end
-  # end
+  describe "run_audit" do
+    let(:uri) { CurationConcerns::VersioningService.latest_version_of(file.original_file).uri }
+    let!(:old) { ChecksumAuditLog.create(generic_file_id: file.id, dsid: 'content', version: uri, pass: 1, created_at: 2.minutes.ago) }
+    let!(:new) { ChecksumAuditLog.create(generic_file_id: file.id, dsid: 'content', version: uri, pass: 0) }
+    let(:mock_service) { double('mock fixity check service') }
+  
+    before do
+      allow(ActiveFedora::FixityService).to receive(:new).and_return(mock_service)
+      allow(mock_service).to receive(:check).and_return(true, false, false, true, false)
+    end
+  
+    it "should not prune failed audits" do
+      5.times { job.send(:run_audit) }
+      expect(ChecksumAuditLog.logs_for(file.id, 'content').map(&:pass)).to eq [0, 1, 0, 0, 1, 0, 1]
+    end
+  end
 end
