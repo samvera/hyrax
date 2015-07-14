41034eb5039de38bec24d4cf04914000cb0e5474
Justin Coyne <justin@curationexperts.com>
2015-07-14 09:55:02 -0500

    Include CurationConcern helper to provide all_type_tab method

--- app/controllers/concerns/sufia/catalog.rb
+++ app/controllers/concerns/sufia/catalog.rb
@@ -3,6 +3,9 @@ module Sufia
     extend ActiveSupport::Concern
     included do
       self.search_params_logic += [:only_works_and_collections, :show_works_or_works_that_contain_files]
+
+      # include the all_type_tab view helper method
+      helper CurationConcerns::CatalogHelper
     end
   end
 end
