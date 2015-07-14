61ae05a7d2a8716fd7646dcc10a0304273b78ea9
Justin Coyne <justin@curationexperts.com>
2015-07-14 13:32:20 -0500

    Remove unused factories

--- spec/factories/generic_works.rb
+++ spec/factories/generic_works.rb
@@ -17,12 +17,5 @@ FactoryGirl.define do
       visibility Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC
     end
 
-    factory :work_with_one_file do
-      after(:create) { |work, evaluator| Hydra::Works::AddGenericFileToGenericWork.call(work, FactoryGirl.create(:generic_file, user: evaluator.user, title:['A Contained Generic File'], filename:['filename.pdf'])) }
-    end
-
-    factory :work_with_files do
-      after(:create) { |work, evaluator| 2.times { Hydra::Works::AddGenericFileToGenericWork.call(work, FactoryGirl.create(:generic_file, user: evaluator.user)) } }
-    end
   end
 end
