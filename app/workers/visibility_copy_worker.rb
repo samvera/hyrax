1631e81c68392d7df1e6f5437fe3e9779a3041ad
Matt Zumwalt <matt@curationexperts.com>
2015-07-15 12:11:14 -0400

    VisibilityCopyWorker only builds embargoes and leases when necessary. fixes #92

--- app/workers/visibility_copy_worker.rb
+++ app/workers/visibility_copy_worker.rb
@@ -13,10 +13,16 @@ class VisibilityCopyWorker
     work = ActiveFedora::Base.find(pid)
     work.generic_files.each do |file|
       file.visibility = work.visibility  # visibility must come first, because it can clear an embargo/lease
-      file.lease.attributes = work.lease.attributes.except("id")
-      file.lease.save
-      file.embargo.attributes = work.embargo.attributes.except("id")
-      file.embargo.save
+      if work.lease
+        file.build_lease unless file.lease
+        file.lease.attributes = work.lease.attributes.except("id")
+        file.lease.save
+      end
+      if work.embargo
+        file.build_embargo unless file.embargo
+        file.embargo.attributes = work.embargo.attributes.except("id")
+        file.embargo.save
+      end
       file.save!
     end
   end
