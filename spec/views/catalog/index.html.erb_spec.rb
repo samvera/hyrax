e8a5899610d7cd7a431b1f01ccad948d68281649
Matt Zumwalt <matt@curationexperts.com>
2015-07-16 12:36:31 -0400

    fixes tests for views/catalog/index.html.erb -- relies on projecthydra-labs/curation_concerns#100 or projecthydra-labs/curation_concerns#35

--- spec/views/catalog/index.html.erb_spec.rb
+++ spec/views/catalog/index.html.erb_spec.rb
@@ -1,11 +1,12 @@
 require 'spec_helper'
 
 describe 'catalog/index.html.erb' do
-
   let(:collection) { stub_model(Collection, title: 'collection1', id: 'abc123') }
   let(:doc) { SolrDocument.new(collection.to_solr) }
 
   before do
+    view.extend CurationConcerns::CollectionsHelper
+    allow(view).to receive(:current_users_collections).and_return([])
     allow(view).to receive(:blacklight_config).and_return(CatalogController.blacklight_config)
     stub_template 'catalog/_search_sidebar.html.erb' => ''
     stub_template 'catalog/_search_header.html.erb' => ''
@@ -14,6 +15,7 @@ describe 'catalog/index.html.erb' do
     allow(view).to receive(:search_session).and_return({})
     allow(view).to receive(:current_search_session).and_return(nil)
     allow(view).to receive(:document_counter_with_offset).and_return(5)
+
     params[:view] = 'gallery'
 
     resp = []
@@ -24,15 +26,29 @@ describe 'catalog/index.html.erb' do
     allow(resp).to receive(:empty?).and_return(false)
 
     # This stubs out the SolrDocument#to_model
-    allow(ActiveFedora::Base).to receive(:load_instance_from_solr).with('abc123').and_return(collection)
+    allow(ActiveFedora::Base).to receive(:load_instance_from_solr).with('abc123', doc).and_return(collection)
+
     assign(:document_list, [doc])
   end
 
-  it 'appears on page without error' do
-    render
-    expect(rendered).to include(collection.title)
-    page = Capybara::Node::Simple.new(rendered)
-    expect(page).to have_selector("span.glyphicon.glyphicon-th.collection-icon-search")
+  context "when user does not have permissions" do
+    before  { allow(view).to receive(:can?).and_return(false) }
+    it 'appears on page without error' do
+      render
+      expect(rendered).to include(collection.title)
+      page = Capybara::Node::Simple.new(rendered)
+      expect(page).to have_selector("span.glyphicon.glyphicon-th.collection-icon-search")
+    end
   end
+  context "when user has all the permissions" do
+    before  { allow(view).to receive(:can?).and_return(true) }
+    it 'appears on page without error' do
+      render
+      expect(rendered).to include(collection.title)
+      page = Capybara::Node::Simple.new(rendered)
+      expect(page).to have_selector("span.glyphicon.glyphicon-th.collection-icon-search")
+    end
+  end
+
 
 end
