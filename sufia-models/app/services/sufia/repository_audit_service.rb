94514b91a7353394a509a8ba3792a4a3d4e0523f
Carolyn Cole <cam156@psu.edu>
2015-07-17 08:14:14 -0400

    Fixing audit service since audit no longer exists on the GenericFile

--- sufia-models/app/services/sufia/repository_audit_service.rb
+++ sufia-models/app/services/sufia/repository_audit_service.rb
@@ -1,7 +1,9 @@
 module Sufia
   class RepositoryAuditService
     def self.audit_everything
-      ::FileSet.find_each(&:audit)
+      ::FileSet.find_each do |gf|
+        CurationConcerns::FileSetAuditService.new(gf).audit
+      end
     end
   end
 end
