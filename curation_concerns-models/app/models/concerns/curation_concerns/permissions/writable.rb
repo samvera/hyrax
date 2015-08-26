0e9be3acd28c40514ca4d9dc8fa6bf8e4ef650c8
Justin Coyne <jcoyne@justincoyne.com>
2015-08-26 12:14:33 -0500

    Remove unused methods

--- curation_concerns-models/app/models/concerns/curation_concerns/permissions/writable.rb
+++ curation_concerns-models/app/models/concerns/curation_concerns/permissions/writable.rb
@@ -29,44 +29,6 @@ module CurationConcerns
           { key: :edit_groups, message: 'Registered cannot have edit access', condition: ->(obj) { obj.edit_groups.include?('registered') } }
         ]
       end
-
-      def clear_permissions!
-        self.permissions = []
-      end
-
-      ## Updates those permissions that are provided to it. Does not replace any permissions unless they are provided
-      # def permissions=(params)
-      #   raise "Fixme #{params}"
-      #   perm_hash = permission_hash
-      #   params[:new_user_name].each { |name, access| perm_hash['person'][name] = access } if params[:new_user_name].present?
-      #   params[:new_group_name].each { |name, access| perm_hash['group'][name] = access } if params[:new_group_name].present?
-
-      #   params[:user].each { |name, access| perm_hash['person'][name] = access} if params[:user]
-      #   params[:group].each { |name, access| perm_hash['group'][name] = access if ['read', 'edit'].include?(access)} if params[:group]
-
-      #   # rightsMetadata.update_permissions(perm_hash)
-      # end
-
-      # def permissions
-      #   raise "Fixme "
-      #   perms = super
-      #   perms.map {|p| { name: p.name, access: p.access, type:p.type } }
-      # end
-
-      private
-
-        def permission_hash
-          old_perms = permissions
-          user_perms = {}
-          old_perms.select { |r| r[:type] == 'user' }.each do |r|
-            user_perms[r[:name]] = r[:access]
-          end
-          group_perms = {}
-          old_perms.select { |r| r[:type] == 'group' }.each do |r|
-            group_perms[r[:name]] = r[:access]
-          end
-          { 'person' => user_perms, 'group' => group_perms }
-        end
     end
   end
 end
