233cb359afabfaee2769dbc74359fe76203e5716
Justin Coyne <jcoyne@justincoyne.com>
2015-08-26 13:15:12 -0500

    Update deprecated capybara wait configuration
    Capybara.default_wait_time -> Capybara.default_max_wait_time

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,7 +16,7 @@ require 'rspec/rails'
 require 'rspec/active_model/mocks'
 require 'capybara/poltergeist'
 Capybara.javascript_driver = :poltergeist
-Capybara.default_wait_time = ENV['TRAVIS'] ? 30 : 15
+Capybara.default_max_wait_time = ENV['TRAVIS'] ? 30 : 15
 require 'capybara/rspec'
 require 'capybara/rails'
 
