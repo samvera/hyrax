5ad0548bf7da0bfb5eb69be0cb9dc2593d8b0bd7
Esmé Cowles <escowles@ucsd.edu>
2015-08-27 09:02:56 -0400

    Removing duplicate model filtering, adding test coverage

---
+++ spec/search_builders/curation_concerns/search_builder_spec.rb
@@ -0,0 +1,58 @@
+require 'spec_helper'
+
+describe CurationConcerns::SearchBuilder do
+  let(:processor_chain) { [:filter_models] }
+  let(:ability) { double('ability') }
+  let(:context) { double('context') }
+  let(:user) { double('user') }
+  let(:solr_params) { { fq: [] } }
+
+  subject { described_class.new(processor_chain, context) }
+
+  describe '#only_generic_files' do
+    before { subject.only_generic_files(solr_params) }
+
+    it 'adds GenericFile to query' do
+      expect(solr_params[:fq].first).to include('{!raw f=has_model_ssim}GenericFile')
+    end
+  end
+
+  describe '#find_one' do
+    before do
+      allow(subject).to receive(:blacklight_params).and_return(id: '12345')
+      subject.find_one(solr_params)
+    end
+
+    it 'adds id to query' do
+      expect(solr_params[:fq].first).to include('{!raw f=id}12345')
+    end
+  end
+
+  describe '#gated_discovery_filters' do
+    before do
+      allow(subject).to receive(:current_ability).and_return(ability)
+      allow(ability).to receive(:current_user).and_return(user)
+      allow(user).to receive(:groups).and_return(['admin'])
+    end
+
+    it 'does not filter results for admin users' do
+      expect(subject.gated_discovery_filters).to eq([])
+    end
+  end
+
+  describe '#discovery_permissions' do
+    before { allow(subject).to receive(:blacklight_params).and_return(works: 'mine') }
+
+    it 'limits query to edit permissions' do
+      expect(subject.discovery_permissions).to eq(['edit'])
+    end
+  end
+
+  describe '#filter_models' do
+    before { subject.filter_models(solr_params) }
+
+    it 'limits query to collection and generic work' do
+      expect(solr_params[:fq].first).to eq('(_query_:"{!raw f=has_model_ssim}GenericWork" OR _query_:"{!raw f=has_model_ssim}Collection")')
+    end
+  end
+end
