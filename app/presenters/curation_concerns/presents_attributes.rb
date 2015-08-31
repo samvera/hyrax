2e835430e0b18df4d267b07f5c5b8522eb26aa0e
Justin Coyne <jcoyne@justincoyne.com>
2015-08-31 15:19:15 -0500

    Extract AttributeRenderer
    This reduces complexity and enables translations of attributes

--- app/presenters/curation_concerns/presents_attributes.rb
+++ app/presenters/curation_concerns/presents_attributes.rb
@@ -1,48 +1,18 @@
 module CurationConcerns
   module PresentsAttributes
-    include ActionView::Helpers::UrlHelper
-    # If options[:catalog_search_link] is false,
-    # it will return the attribute value as text.
-    # If options[:catalog_search_link] is true,
-    # it will return a link to a catalog search for that text.
+    ##
+    # Present the attribute as an HTML table row.
     #
-    # If the method_name of the attribute is different than
-    # how the attribute name should appear on the search URL,
-    # you can explicitly set the URL's search field name using
-    # options[:search_field].
-    # rubocop:disable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
-    def attribute_to_html(method_name, label = nil, options = {})
-      return unless respond_to?(method_name)
-      markup = ''
-      label ||= derived_label_for(method_name)
-
-      subject = send(method_name)
-      return markup if !subject.present? && !options[:include_empty]
-      markup << %(<tr><th>#{label}</th>\n<td><ul class='tabular'>)
-      [subject].flatten.compact.each do |value|
-        if method_name == :rights
-          # Special treatment for license/rights.  A URL from the Sufia gem's config/sufia.rb is stored in the descMetadata of the
-          # curation_concern.  If that URL is valid in form, then it is used as a link.  If it is not valid, it is used as plain text.
-          begin
-            parsed_uri = URI.parse(value)
-          rescue
-            nil
-          end
-          if parsed_uri.nil?
-            markup << %(<li class="attribute #{method_name}">#{ERB::Util.h(value)}</li>\n)
-          else
-            markup << %(<li class="attribute #{method_name}"><a href=#{ERB::Util.h(value)} target="_blank"> #{ERB::Util.h(CurationConcerns.config.cc_licenses_reverse[value])}</a></li>\n)
-          end
-        else
-          search_field = options[:search_field] || method_name
-          li_value = link_to_if(options[:catalog_search_link], ERB::Util.h(value), Rails.application.routes.url_helpers.catalog_index_path(search_field: search_field, q: ERB::Util.h(value)))
-          markup << %(<li class="attribute #{method_name}">#{li_value}</li>\n)
-        end
-      end
-      markup << %(</ul></td></tr>)
-      markup.html_safe
+    # @param [Hash] options
+    # @option options [true, false] :catalog_search_link return a link to a catalog search for that text if true
+    # @option options [String] :search_field If the method_name of the attribute is different than
+    #   how the attribute name should appear on the search URL,
+    #   you can explicitly set the URL's search field name
+    # @option options [String] :label The default label for the field if no translation is found
+    def attribute_to_html(field, options = {})
+      return unless respond_to?(field)
+      AttributeRenderer.new(field, send(field), options).render
     end
-    # rubocop:enable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
 
     def permission_badge
       dom_label_class, link_title = extract_dom_label_class_and_link_title(solr_document)
---
+++ spec/renderers/attribute_renderer_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe AttributeRenderer do
+  let(:renderer) { described_class.new(:name, ['Bob', 'Jessica']) }
+
+  describe "#attribute_to_html" do
+    subject { renderer.render }
+
+    it { is_expected.to eq "<tr><th>Name</th>\n" \
+       "<td><ul class='tabular'><li class=\"attribute name\">Bob</li>\n" \
+       "<li class=\"attribute name\">Jessica</li>\n" \
+       "</ul></td></tr>" }
+  end
+end
