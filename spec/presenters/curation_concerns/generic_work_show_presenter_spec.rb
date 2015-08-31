8c3c5d1c8270183ae4d017745d43f218c3aea0cc
Justin Coyne <jcoyne@justincoyne.com>
2015-08-31 13:10:48 -0500

    Translate the registered status
    Fixes #239

--- spec/presenters/curation_concerns/generic_work_show_presenter_spec.rb
+++ spec/presenters/curation_concerns/generic_work_show_presenter_spec.rb
@@ -1,9 +1,13 @@
 require 'spec_helper'
 
 describe CurationConcerns::GenericWorkShowPresenter do
-  let(:solr_document) { SolrDocument.new("title_tesim" => ["foo bar"],
-                                         "human_readable_type_tesim" => ["Generic Work"],
-                                         "has_model_ssim" => ["GenericWork"]) }
+  let(:solr_document) { SolrDocument.new(attributes) }
+  let(:attributes) do
+    { "title_tesim" => ["foo bar"],
+      "human_readable_type_tesim" => ["Generic Work"],
+      "has_model_ssim" => ["GenericWork"] }
+  end
+
   let(:ability) { nil }
   let(:presenter) { described_class.new(solr_document, ability) }
 
@@ -20,5 +24,24 @@ describe CurationConcerns::GenericWorkShowPresenter do
   describe "#model_name" do
     subject { presenter.model_name }
     it { is_expected.to be_kind_of ActiveModel::Name }
+  end
+
+  describe "#permission_badge" do
+    subject { presenter.permission_badge }
+
+    context "for a private work" do
+      it { is_expected.to eq '<span class="label label-danger" title="Private">Private</span>' }
+    end
+
+    context "for a work that is restricted to registered users" do
+      let(:attributes) do
+        { "title_tesim" => ["foo bar"],
+          "human_readable_type_tesim" => ["Generic Work"],
+          Hydra.config.permissions.read.group => ['registered'],
+          "has_model_ssim" => ["GenericWork"] }
+      end
+
+      it { is_expected.to eq '<span class="label label-info" title="Institution Name">Institution Name</span>' }
+    end
   end
 end
