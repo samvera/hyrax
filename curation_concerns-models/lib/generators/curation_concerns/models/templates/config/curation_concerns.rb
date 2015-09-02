0280b9655f8a2f2678cf4f2142621e74a73079b5
Justin Coyne <jcoyne@justincoyne.com>
2015-09-02 14:15:03 -0500

    Add minter_statefile to the configuration template

--- curation_concerns-models/lib/generators/curation_concerns/models/templates/config/curation_concerns.rb
+++ curation_concerns-models/lib/generators/curation_concerns/models/templates/config/curation_concerns.rb
@@ -106,6 +106,9 @@ CurationConcerns.configure do |config|
   # Specify a different template for your repository's NOID IDs
   # config.noid_template = ".reeddeeddk"
 
+  # Store identifier minter's state in a file for later replayability
+  # config.minter_statefile = '/tmp/minter-state'
+
   # Specify the prefix for Redis keys:
   # config.redis_namespace = "curation_concerns"
 
