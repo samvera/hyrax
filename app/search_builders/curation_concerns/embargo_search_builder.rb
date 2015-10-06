f259cbce340fc18e4bb2d214e6ee225aeb3b2ed9
Justin Coyne <jcoyne@justincoyne.com>
2015-10-06 11:51:00 -0500

    Sort leases and embargoes

--- app/search_builders/curation_concerns/embargo_search_builder.rb
+++ app/search_builders/curation_concerns/embargo_search_builder.rb
@@ -1,14 +1,18 @@
 module CurationConcerns
   # Finds embargoed objects
   class EmbargoSearchBuilder < Blacklight::SearchBuilder
-    self.default_processor_chain = [:add_paging_to_solr, :only_active_embargoes]
+    self.default_processor_chain = [:with_pagination, :with_sorting, :only_active_embargoes]
     def initialize(scope)
       super(true, scope)
     end
 
     # TODO: add more complex pagination
-    def add_paging_to_solr(solr_params)
+    def with_pagination(solr_params)
       solr_params[:rows] = 1000
+    end
+
+    def with_sorting(solr_params)
+      solr_params[:sort] = 'embargo_release_date_dtsi desc'
     end
 
     def only_active_embargoes(solr_params)
--- app/search_builders/curation_concerns/lease_search_builder.rb
+++ app/search_builders/curation_concerns/lease_search_builder.rb
@@ -1,14 +1,18 @@
 module CurationConcerns
   # Finds objects under lease
   class LeaseSearchBuilder < Blacklight::SearchBuilder
-    self.default_processor_chain = [:add_paging_to_solr, :only_active_leases]
+    self.default_processor_chain = [:with_pagination, :with_sorting, :only_active_leases]
     def initialize(scope)
       super(true, scope)
     end
 
     # TODO: add more complex pagination
-    def add_paging_to_solr(solr_params)
+    def with_pagination(solr_params)
       solr_params[:rows] = 1000
+    end
+
+    def with_sorting(solr_params)
+      solr_params[:sort] = 'lease_expiration_date_dtsi desc'
     end
 
     def only_active_leases(solr_params)
