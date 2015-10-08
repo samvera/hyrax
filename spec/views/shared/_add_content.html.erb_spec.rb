b5e299a279c3efbad61daae24a3cc4b7840cc104
Justin Coyne <jcoyne@justincoyne.com>
2015-10-08 13:16:11 -0500

    Extract add_works partial from add_content

--- spec/views/shared/_add_content.html.erb_spec.rb
+++ spec/views/shared/_add_content.html.erb_spec.rb
@@ -1,23 +1,34 @@
 require 'spec_helper'
 
 describe 'shared/_add_content.html.erb' do
+  before do
+    allow(view).to receive(:current_user).and_return(user)
+  end
+
   context 'for people who can create Works and Collections' do
-    it 'has links to create works and collections' do
-      allow(view).to receive(:current_user).and_return(FactoryGirl.create(:admin))
+    let(:user) { create(:admin) }
+    before do
       allow(view).to receive(:can?).and_return(true)
-      render partial: 'shared/add_content'
+      render
+    end
+
+    it 'has links to create works and collections' do
       CurationConcerns.config.curation_concerns.each do |curation_concern_type|
         expect(rendered).to have_link("New #{curation_concern_type.human_readable_type}", href: new_polymorphic_path([:curation_concerns, curation_concern_type]))
       end
       expect(rendered).to have_link('Add a Collection', href: collections.new_collection_path)
     end
   end
+
   context 'for people who can only create Collections' do
-    it 'has links to add collections but not to add works' do
-      allow(view).to receive(:current_user).and_return(FactoryGirl.create(:admin))
+    let(:user) { create(:admin) }
+    before do
       allow(view).to receive(:can?).and_return(true)
       allow(view).to receive(:can_ever_create_works?).and_return(false)
-      render partial: 'shared/add_content'
+      render
+    end
+
+    it 'has links to add collections but not to add works' do
       CurationConcerns.config.curation_concerns.each do |curation_concern_type|
         expect(rendered).not_to have_link("New #{curation_concern_type.human_readable_type}", href: new_polymorphic_path([:curation_concerns, curation_concern_type]))
       end
@@ -26,10 +37,13 @@ describe 'shared/_add_content.html.erb' do
   end
 
   context 'for people who cannot create anything' do
-    it 'does not have links to add works or collections' do
-      allow(view).to receive(:current_user).and_return(FactoryGirl.create(:user))
+    let(:user) { create(:user) }
+    before do
       allow(view).to receive(:can?).and_return(false)
-      render partial: 'shared/add_content'
+      render
+    end
+
+    it 'does not have links to add works or collections' do
       expect(rendered).not_to have_text('Add')
       expect(rendered).not_to have_text('Admin')
       CurationConcerns.config.curation_concerns.each do |curation_concern_type|
