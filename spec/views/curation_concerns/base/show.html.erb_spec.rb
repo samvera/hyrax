a05f4e682a93fa1a357b1199a419d532bc4ce986
Justin Coyne <jcoyne@justincoyne.com>
2015-10-13 15:52:23 -0500

    Cleanup view spec

--- spec/views/curation_concerns/base/show.html.erb_spec.rb
+++ spec/views/curation_concerns/base/show.html.erb_spec.rb
@@ -4,12 +4,18 @@ describe 'curation_concerns/base/show.html.erb' do
   let(:object_profile) { ["{\"id\":\"999\"}"] }
   let(:contributor) { 'Frodo' }
   let(:creator)     { 'Bilbo' }
-  let(:solr_document) { SolrDocument.new(id: '999',
-                                         object_profile_ssm: object_profile,
-                                         has_model_ssim: ['GenericWork'],
-                                         human_readable_type_tesim: ['Generic Work'],
-                                         contributor_tesim: contributor,
-                                         creator_tesim: creator) }
+  let(:solr_document) {
+    SolrDocument.new(
+      id: '999',
+      object_profile_ssm: object_profile,
+      has_model_ssim: ['GenericWork'],
+      human_readable_type_tesim: ['Generic Work'],
+      contributor_tesim: contributor,
+      creator_tesim: creator,
+      rights_tesim: ['http://creativecommons.org/licenses/by/3.0/us/']
+    )
+  }
+
   let(:ability) { nil }
   let(:presenter) do
     CurationConcerns::WorkShowPresenter.new(solr_document, ability)
@@ -23,10 +29,9 @@ describe 'curation_concerns/base/show.html.erb' do
       render
     end
 
-    it 'has links to edit' do # and add to collections' do
-      expect(rendered).to have_link('Edit This Generic Work', href: edit_polymorphic_path([:curation_concerns, presenter]))
-      # expect(rendered).to have_selector "a[data-toggle='modal'][data-target='##{curation_concern.to_param}-modal']", text: "Add to a Collection"
-      # expect(rendered).to have_selector("div.modal##{curation_concern.to_param}-modal form[action='#{collections.collections_path}'] input[value='Add to collection']")
+    it 'draws the page' do
+      expect(rendered).to have_link 'Attribution 3.0 United States', href: 'http://creativecommons.org/licenses/by/3.0/us/'
+      expect(rendered).to have_link 'Edit This Generic Work', href: edit_polymorphic_path([:curation_concerns, presenter])
     end
   end
 
@@ -37,11 +42,8 @@ describe 'curation_concerns/base/show.html.erb' do
       allow(view).to receive(:can?).with(:collect, String).and_return(false)
       render
     end
-    it 'does not have links to edit' do # , but has add to collection' do
+    it 'does not have links to edit' do
       expect(rendered).not_to have_content('Edit this Generic Work')
-
-      # expect(rendered).to have_selector "a[data-toggle='modal'][data-target='##{curation_concern.to_param}-modal']", text: "Add to a Collection"
-      # expect(rendered).to have_selector("div.modal##{curation_concern.to_param}-modal form[action='#{collections.collections_path}'] input[value='Add to collection']")
     end
   end
 end
