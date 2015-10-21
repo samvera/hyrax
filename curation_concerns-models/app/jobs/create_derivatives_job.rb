6bf4d17f3cfe1172303c6ae272794ae25b7ad9a8
val99erie <val99erie@gmail.com>
2015-10-21 13:21:08 -0500

    Update the solr index of the parent work when thumbnail is created. Story curationexperts/goldenseal#213

--- curation_concerns-models/app/jobs/create_derivatives_job.rb
+++ curation_concerns-models/app/jobs/create_derivatives_job.rb
@@ -8,5 +8,13 @@ class CreateDerivativesJob < ActiveFedoraIdBasedJob
     file_set.create_derivatives(file_name)
     # The thumbnail is indexed in the solr document, so reindex
     file_set.update_index
+    file_set.parent.update_index if parent_needs_reindex?
+  end
+
+  # If this file_set is the thumbnail for the parent work,
+  # then the parent also needs to be reindexed.
+  def parent_needs_reindex?
+    return false unless file_set.parent
+    file_set.parent.thumbnail_id == file_set.id
   end
 end
--- spec/jobs/create_derivatives_job_spec.rb
+++ spec/jobs/create_derivatives_job_spec.rb
@@ -1,12 +1,14 @@
 require 'spec_helper'
 
 describe CurationConcerns::CreateDerivativesJob do
+  let(:id) { '123' }
+
   before do
     @ffmpeg_enabled = CurationConcerns.config.enable_ffmpeg
     CurationConcerns.config.enable_ffmpeg = true
-    allow(ActiveFedora::Base).to receive(:find).with('123').and_return(file_set)
+    allow(ActiveFedora::Base).to receive(:find).with(id).and_return(file_set)
     allow(file_set).to receive(:mime_type).and_return('audio/x-wav')
-    allow(file_set).to receive(:id).and_return('123')
+    allow(file_set).to receive(:id).and_return(id)
   end
 
   let(:file_set) { FileSet.new }
@@ -19,7 +21,33 @@ describe CurationConcerns::CreateDerivativesJob do
     it 'calls create_derivatives and save on a file set' do
       expect(Hydra::Derivatives::AudioDerivatives).to receive(:create)
       expect(file_set).to receive(:update_index)
-      CreateDerivativesJob.perform_now('123', 'spec/fixtures/piano_note.wav')
+      CreateDerivativesJob.perform_now(id, 'spec/fixtures/piano_note.wav')
+    end
+  end
+
+  context 'with a parent object' do
+    before do
+      allow(file_set).to receive(:parent).and_return(parent)
+      # Stub out the actual derivative creation
+      expect(file_set).to receive(:create_derivatives)
+    end
+
+    context 'when the file_set is the thumbnail of the parent' do
+      let(:parent) { GenericWork.new(thumbnail_id: id) }
+
+      it 'updates the index of the parent object' do
+        expect(parent).to receive(:update_index)
+        CreateDerivativesJob.perform_now(id, 'spec/fixtures/piano_note.wav')
+      end
+    end
+
+    context "when the file_set isn't the parent's thumbnail" do
+      let(:parent) { GenericWork.new }
+
+      it "doesn't update the parent's index" do
+        expect(parent).to_not receive(:update_index)
+        CreateDerivativesJob.perform_now(id, 'spec/fixtures/piano_note.wav')
+      end
     end
   end
 end
