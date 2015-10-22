464eee563ff6cd5797d8745b576d0d57262f9421
Esmé Cowles <escowles@ticklefish.org>
2015-10-22 16:48:02 -0400

    Indexing member ids in member_ids_ssim instead of file_set_ids_ssim

--- curation_concerns-models/app/indexers/curation_concerns/work_indexing_service.rb
+++ curation_concerns-models/app/indexers/curation_concerns/work_indexing_service.rb
@@ -7,7 +7,7 @@ module CurationConcerns
         # member_ids which requires fewer Fedora API calls than file_set_ids.
         # file_set_ids requires loading all the members from Fedora but member_ids
         # looks just at solr
-        solr_doc[Solrizer.solr_name('file_set_ids', :symbol)] = object.member_ids
+        solr_doc[Solrizer.solr_name('member_ids', :symbol)] = object.member_ids
         Solrizer.set_field(solr_doc, 'generic_type', 'Work', :facetable)
         solr_doc['thumbnail_path_ss'] = thumbnail_path
       end
