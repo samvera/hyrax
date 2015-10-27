6cb1bbf97c759e23dbdfdfe8cd42caacf84a5e4c
Colin Gross <grosscol@umich.edu>
2015-10-27 10:18:18 -0400

    Depend on CurationConcerns 0.2 Update tasks path for CC-models.   Becaus CC-models not found inside CC. Remove specific commit refs Gemfile.extra that were conflicting. Use config callback infrastructure: blocks instead of lambdas.

--- config/initializers/curation_concern_events.rb
+++ config/initializers/curation_concern_events.rb
@@ -1,40 +1,40 @@
 # These events are triggered by actions within CurationConcerns Actors
-CurationConcerns.config.after_create_content = lambda { |generic_file, user|
+CurationConcerns.config.callback.set(:after_create_content) { |generic_file, user|
   CurationConcerns.queue.push(ContentDepositEventJob.new(generic_file.id, user.user_key))
 }
 
-CurationConcerns.config.after_revert_content = lambda { |generic_file, user, revision|
+CurationConcerns.config.callback.set(:after_revert_content) { |generic_file, user, revision|
   CurationConcerns.queue.push(ContentRestoredVersionEventJob.new(generic_file.id, user.user_key, revision))
 }
 
-CurationConcerns.config.after_update_content = lambda { |generic_file, user|
+CurationConcerns.config.callback.set(:after_update_content) { |generic_file, user|
   CurationConcerns.queue.push(ContentNewVersionEventJob.new(generic_file.id, user.user_key))
 }
 
-CurationConcerns.config.after_update_metadata = lambda { |generic_file, user|
+CurationConcerns.config.callback.set(:after_update_metadata) { |generic_file, user|
   CurationConcerns.queue.push(ContentUpdateEventJob.new(generic_file.id, user.user_key))
 }
 
-CurationConcerns.config.after_destroy = lambda { |id, user|
+CurationConcerns.config.callback.set(:after_destroy) { |id, user|
   CurationConcerns.queue.push(ContentDeleteEventJob.new(id, user.user_key))
 }
 
-CurationConcerns.config.after_audit_failure = lambda { |generic_file, user, log_date|
+CurationConcerns.config.callback.set(:after_audit_failure) { |generic_file, user, log_date|
   Sufia::AuditFailureService.new(generic_file, user, log_date).call
 }
 
-CurationConcerns.config.after_import_url_success = lambda { |generic_file, user|
+CurationConcerns.config.callback.set(:after_import_url_success) { |generic_file, user|
   Sufia::ImportUrlSuccessService.new(generic_file, user).call
 }
 
-CurationConcerns.config.after_import_url_failure = lambda { |generic_file, user|
+CurationConcerns.config.callback.set(:after_import_url_failure) { |generic_file, user|
   Sufia::ImportUrlFailureService.new(generic_file, user).call
 }
 
-CurationConcerns.config.after_import_local_file_success = lambda { |generic_file, user, filename|
+CurationConcerns.config.callback.set(:after_import_local_file_success) { |generic_file, user, filename|
   Sufia::ImportLocalFileSuccessService.new(generic_file, user, filename).call
 }
 
-CurationConcerns.config.after_import_local_file_failure = lambda { |generic_file, user, filename|
+CurationConcerns.config.callback.set(:after_import_local_file_failure) { |generic_file, user, filename|
   Sufia::ImportLocalFileFailureService.new(generic_file, user, filename).call
 }
