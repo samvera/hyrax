e7f674ab422dcb6f467950fa7c462fc4761183dc
Justin Coyne <jcoyne@justincoyne.com>
2015-11-10 14:30:30 -0600

    Don't show embargo/lease terms on the batch edit form

--- app/forms/sufia/forms/batch_edit_form.rb
+++ app/forms/sufia/forms/batch_edit_form.rb
@@ -1,6 +1,9 @@
 module Sufia
   module Forms
     class BatchEditForm < CurationConcerns::Forms::FileSetEditForm
+      self.terms = [:resource_type, :title, :creator, :contributor, :description,
+              :tag, :rights, :publisher, :date_created, :subject, :language,
+              :identifier, :based_near, :related_url]
     end
   end
 end
