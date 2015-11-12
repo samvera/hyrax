0ba21a3d45adc45ea329d8bdd09c4913cf8d7a5b
Justin Coyne <jcoyne@justincoyne.com>
2015-11-12 12:15:50 -0600

    Remove unused class

--- sufia-models/app/models/concerns/sufia/works/generic_work.rb
+++
@@ -1,8 +0,0 @@
-# a very simple type of work with DC metadata
-module Sufia::Works
-  module GenericWork
-    extend ActiveSupport::Concern
-
-    include Sufia::Works::GenericWork::ProxyDeposit
-  end
-end
