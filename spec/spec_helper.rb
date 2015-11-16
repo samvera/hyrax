0a90f4ec98659607bebfab5b48e06f083fadb365
Justin Coyne <jcoyne@justincoyne.com>
2015-11-16 13:23:44 -0600

    Remove test that duplicates test on line 45

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -43,7 +43,7 @@ if $in_travis
   # Monkey-patches the FITS runner to return the PDF FITS fixture
   module Hydra::Works
     class CharacterizationService
-      def self.run(_)
+      def self.run(_, _)
         raise "FITS!!!"
         # return unless file_set.original_file.has_content?
         # filename = ::File.expand_path("../fixtures/pdf_fits.xml", __FILE__)
