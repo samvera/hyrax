0cf023623235185c93968e09d413559d5472ad7d
Hector Correa <hector@hectorcorrea.com>
2015-11-19 15:30:45 -0500

    Uses the proper presenter in GenericWorksControllerBehavior

--- lib/generators/sufia/upgrade700_generator.rb
+++ lib/generators/sufia/upgrade700_generator.rb
@@ -27,7 +27,7 @@ This generator for upgrading sufia from 6.0.0 to 7.0 makes the following changes
   end
 
   def inject_sufia_work_controller_behavior
-    file_path = "app/controller/curation_concerns/generic_works_controller.rb"
+    file_path = "app/controllers/curation_concerns/generic_works_controller.rb"
     if File.exist?(file_path)
       inject_into_file file_path, after: /include CurationConcerns::CurationConcernController/ do
         "\n  # Adds Sufia behaviors to the controller.\n" \
