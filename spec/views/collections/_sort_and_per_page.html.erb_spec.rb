bc1c8ce538d3b8ea976b6636f6a7ded78ea39d2c
Justin Coyne <jcoyne@justincoyne.com>
2015-11-19 09:51:09 -0600

    Allow sort_and_per_page to be used for edit
    CurationConcerns currently only uses sort_and_per_page on the
    Collection's show view. Sufia also uses it on the edit view. On the edit
    view we use `@form` rather than `@presenter`, so replace the ivars with
    local variables.

---
+++ spec/views/collections/_sort_and_per_page.html.erb_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+describe 'collections/_sort_and_per_page.html.erb' do
+  let(:collection) { double }
+  let(:response) { double(response: { 'numFound' => 3 }) }
+
+  before do
+    allow(view).to receive(:sort_fields).and_return(['title_sort', 'date_sort'])
+    allow(view).to receive(:document_index_views).and_return(list: Blacklight::Configuration::ViewConfig.new)
+    assign(:response, response)
+  end
+
+  context "when the action is edit" do
+    before do
+      controller.action_name = "edit"
+    end
+    it "renders the form with a button" do
+      expect(view).to receive(:button_for_remove_selected_from_collection).with(collection)
+      render 'collections/sort_and_per_page', collection: collection
+    end
+  end
+
+  context "when the action is show" do
+    before do
+      controller.action_name = "show"
+    end
+    it "renders the form without a button" do
+      expect(view).not_to receive(:button_for_remove_selected_from_collection)
+      render 'collections/sort_and_per_page', collection: collection
+    end
+  end
+end
