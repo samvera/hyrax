cbe170649f940fb14127c441ffaacadf50780ea0
Justin Coyne <jcoyne@justincoyne.com>
2015-11-20 12:52:27 -0600

    Use callbacks with the correct signature. Fixes #282

--- app/services/sufia/upload_set_update_success_service.rb
+++ app/services/sufia/upload_set_update_success_service.rb
@@ -1,14 +1,14 @@
 module Sufia
   class UploadSetUpdateSuccessService < MessageUserService
-    attr_reader :upload_set_id
+    attr_reader :upload_set
 
-    def initialize(file_set, user, upload_set_id)
-      @upload_set_id = upload_set_id
-      super(file_set, user)
+    def initialize(user, upload_set)
+      @upload_set = upload_set
+      @user = user
     end
 
     def message
-      "The upload set update for  #{upload_set_id} passed."
+      "The upload set update for #{upload_set.id} passed."
     end
 
     def subject
