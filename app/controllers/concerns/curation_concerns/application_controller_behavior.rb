9fbcb17716c5e12cd902d1f2c540caad68cf5548
Justin Coyne <jcoyne@justincoyne.com>
2015-11-20 10:04:45 -0600

    Simplify error handling in JSON

--- app/controllers/concerns/curation_concerns/application_controller_behavior.rb
+++ app/controllers/concerns/curation_concerns/application_controller_behavior.rb
@@ -7,16 +7,20 @@ module CurationConcerns
     included do
       helper CurationConcerns::MainAppHelpers
 
-      rescue_from ActiveFedora::ObjectNotFoundError do |exception|
+      rescue_from ActiveFedora::ObjectNotFoundError do |_exception|
         respond_to do |wants|
           wants.html { render file: "#{Rails.root}/public/404", format: :html, status: :not_found, layout: false }
-          wants.json { render_json_response(response_type: :not_found, message: exception.message) }
+          wants.json { render_json_response(response_type: :not_found) }
         end
       end
     end
 
     # Called by Hydra::Controller::ControllerBehavior when CanCan::AccessDenied is caught
+    # @param [CanCan::AccessDenied] exception error to handle
     def deny_access(exception)
+      # For the JSON message, we don't want to display the default CanCan messages,
+      # just custom Hydra messages such as "This item is under embargo.", etc.
+      json_message = exception.message if exception.is_a? Hydra::AccessDenied
       if current_user && current_user.persisted?
         respond_to do |wants|
           wants.html do
@@ -26,13 +30,13 @@ module CurationConcerns
               redirect_to main_app.root_url, alert: exception.message
             end
           end
-          wants.json { render_json_response(response_type: :forbidden, message: exception.message) }
+          wants.json { render_json_response(response_type: :forbidden, message: json_message) }
         end
       else
         session['user_return_to'.freeze] = request.url
         respond_to do |wants|
           wants.html { redirect_to main_app.new_user_session_path, alert: exception.message }
-          wants.json { render_json_response(response_type: :unauthorized, message: exception.message) }
+          wants.json { render_json_response(response_type: :unauthorized, message: json_message) }
         end
       end
     end
