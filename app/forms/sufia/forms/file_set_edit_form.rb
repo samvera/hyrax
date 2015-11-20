89c212ac358b102c62dc086cc7442195a845ff1b
Justin Coyne <jcoyne@justincoyne.com>
2015-11-20 17:37:06 -0600

    FileSetEditForm delegates permissions and depositor

--- app/forms/sufia/forms/file_set_edit_form.rb
+++ app/forms/sufia/forms/file_set_edit_form.rb
@@ -1,5 +1,7 @@
 module Sufia::Forms
   class FileSetEditForm < CurationConcerns::Forms::FileSetEditForm
     include HydraEditor::Form::Permissions
+
+    delegate :depositor, :permissions,  to: :model
   end
 end
