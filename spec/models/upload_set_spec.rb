7b36d45b0ebdc4dd6e0e9ad6c4a9fc4fba86ac54
Hector Correa <hector@hectorcorrea.com>
2015-11-20 09:58:45 -0500

    Removes creator from UploadSet metadata

--- spec/models/upload_set_spec.rb
+++ spec/models/upload_set_spec.rb
@@ -2,11 +2,10 @@ require 'spec_helper'
 
 describe UploadSet do
   let(:user) { FactoryGirl.find_or_create(:jill) }
-  let(:upload_set) { described_class.create(title: ["test collection"], creator: [user.user_key]) }
+  let(:upload_set) { described_class.create(title: ["test collection"]) }
   subject { upload_set }
 
   it "has dc metadata" do
-    expect(subject.creator).to eq [user.user_key]
     expect(subject.title).to eq ["test collection"]
   end
 
@@ -17,12 +16,11 @@ describe UploadSet do
   it "supports to_solr" do
     expect(subject.to_solr).to_not be_nil
     expect(subject.to_solr["upload_set__title_t"]).to be_nil
-    expect(subject.to_solr["upload_set__creator_t"]).to be_nil
   end
 
   describe "find_or_create" do
     describe "when the object exists" do
-      let!(:upload_set) { described_class.create(title: ["test collection"], creator: [user.user_key]) }
+      let!(:upload_set) { described_class.create(title: ["test collection"]) }
       it "finds upload_set instead of creating" do
         expect(described_class).to_not receive(:create)
         described_class.find_or_create(subject.id)
