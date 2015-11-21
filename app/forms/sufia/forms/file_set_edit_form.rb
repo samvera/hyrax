103356ae597336d228858f5ab3c869d9e3d9a401
Justin Coyne <jcoyne@justincoyne.com>
2015-11-20 19:22:35 -0600

    Fix rubocop error

--- app/forms/sufia/forms/file_set_edit_form.rb
+++ app/forms/sufia/forms/file_set_edit_form.rb
@@ -2,6 +2,6 @@ module Sufia::Forms
   class FileSetEditForm < CurationConcerns::Forms::FileSetEditForm
     include HydraEditor::Form::Permissions
 
-    delegate :depositor, :permissions,  to: :model
+    delegate :depositor, :permissions, to: :model
   end
 end
