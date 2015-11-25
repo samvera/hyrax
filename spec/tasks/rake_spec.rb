samvera-deprecated/sufia

Adding user email list rake task.  fixes #1060

spec/tasks/rake_spec.rb
@@ -5,23 +5,21 @@ describe "Rake tasks" do
 
   describe "sufia:empty_batches" do
     before do
-      load File.expand_path("../../../sufia-models/lib/tasks/batch_cleanup.rake", __FILE__)
-      Rake::Task.define_task(:environment)
+      load_rake_environment [File.expand_path("../../../sufia-models/lib/tasks/batch_cleanup.rake", __FILE__)]
     end
-    after { Rake::Task["sufia:empty_batches"].reenable }
-    subject { capture_stdout { Rake::Task["sufia:empty_batches"].invoke } }
+    subject { run_task "sufia:empty_batches" }
     
     context "without an empty batch" do
-      it { is_expected.to be_empty }
+      it { is_expected.to eq "Output: \n Errors:" }
     end
     
     context "with an empty batch" do
       before { Batch.create("empty-batch") }
-      it { is_expected.to start_with("empty-batch contains no files - to delete, rerun with the remove option") }
+      it { is_expected.to start_with("Output: empty-batch contains no files - to delete, rerun with the remove option") }
       
       describe "removing the empty batch" do
-        subject { capture_stdout { Rake::Task["sufia:empty_batches"].invoke("remove") } }
-        it { is_expected.to start_with("empty-batch contains no files - deleted") }
+        subject { run_task "sufia:empty_batches", "remove" }
+        it { is_expected.to start_with("Output: empty-batch contains no files - deleted") }
       end
     end
   end
@@ -30,8 +28,7 @@ describe "Rake tasks" do
     let(:namespaced_id) { "sufia:123" }
     let(:corrected_id)  { "123" }
     before do
-      load File.expand_path("../../../sufia-models/lib/tasks/migrate.rake", __FILE__)
-      Rake::Task.define_task(:environment)
+      load_rake_environment [File.expand_path("../../../sufia-models/lib/tasks/migrate.rake", __FILE__)]
     end
 
     describe "deleting the namespace from ProxyDepositRequest#generic_file_id" do
@@ -39,7 +36,7 @@ describe "Rake tasks" do
       let(:receiver) { FactoryGirl.find_or_create(:archivist) }
       before do
         ProxyDepositRequest.create(generic_file_id: namespaced_id, sending_user: sender, receiving_user: receiver, sender_comment: "please take this")
-        Rake::Task["sufia:migrate:proxy_deposits"].invoke
+        run_task "sufia:migrate:proxy_deposits"
       end
       subject { ProxyDepositRequest.first.generic_file_id }
       it { is_expected.to eql corrected_id }
@@ -48,10 +45,34 @@ describe "Rake tasks" do
     describe "deleting the namespace from ChecksumAuditLog#generic_file_id" do
       before do
         ChecksumAuditLog.create(generic_file_id: namespaced_id)
-        Rake::Task["sufia:migrate:audit_logs"].invoke
+        run_task "sufia:migrate:audit_logs"
       end
       subject { ChecksumAuditLog.first.generic_file_id }
       it { is_expected.to eql corrected_id }
+    end
+  end
+
+  describe "sufia:user:list_emails" do
+    let!(:user1) { FactoryGirl.create(:user) }
+    let!(:user2) { FactoryGirl.create(:user) }
+
+    before do
+      load_rake_environment [File.expand_path("../../../tasks/sufia-user.rake", __FILE__)]
+    end
+
+    it "creates a file" do
+      run_task "sufia:user:list_emails"
+      expect(File.exist?("user_emails.txt")).to be_truthy
+      expect(IO.read("user_emails.txt")).to include(user1.email, user2.email)
+      File.delete("user_emails.txt")
+    end
+
+    it "creates a file I give it" do
+      run_task "sufia:user:list_emails", "abc123.txt"
+      expect(File.exist?("user_emails.txt")).not_to be_truthy
+      expect(File.exist?("abc123.txt")).to be_truthy
+      expect(IO.read("abc123.txt")).to include(user1.email, user2.email)
+      File.delete("abc123.txt")
     end
   end
 

samvera/hyrax

spec/tasks/rake_spec.rb
@@ -4,23 +4,21 @@ require 'rake'
 describe "Rake tasks" do
   describe "sufia:empty_upload_sets" do
     before do
-      load File.expand_path("../../../sufia-models/lib/tasks/upload_set_cleanup.rake", __FILE__)
-      Rake::Task.define_task(:environment)
+      load_rake_environment [File.expand_path("../../../sufia-models/lib/tasks/upload_set_cleanup.rake", __FILE__)]
     end
-    after { Rake::Task["sufia:empty_upload_sets"].reenable }
-    subject { capture_stdout { Rake::Task["sufia:empty_upload_sets"].invoke } }
+    subject { run_task "sufia:empty_upload_sets" }
 
     context "without an empty upload_set" do
-      it { is_expected.to be_empty }
+      it { is_expected.to eq "Output: \n Errors:" }
     end
 
     context "with an empty upload_set" do
       before { UploadSet.create("empty-upload_set") }
-      it { is_expected.to start_with("empty-upload_set contains no files - to delete, rerun with the remove option") }
+      it { is_expected.to start_with("Output: empty-upload_set contains no files - to delete, rerun with the remove option") }
 
       describe "removing the empty upload_set" do
-        subject { capture_stdout { Rake::Task["sufia:empty_upload_sets"].invoke("remove") } }
-        it { is_expected.to start_with("empty-upload_set contains no files - deleted") }
+        subject { run_task "sufia:empty_upload_sets", "remove" }
+        it { is_expected.to start_with("Output: empty-upload_set contains no files - deleted") }
       end
     end
   end
@@ -29,8 +27,7 @@ describe "Rake tasks" do
     let(:namespaced_id) { "sufia:123" }
     let(:corrected_id)  { "123" }
     before do
-      load File.expand_path("../../../sufia-models/lib/tasks/migrate.rake", __FILE__)
-      Rake::Task.define_task(:environment)
+      load_rake_environment [File.expand_path("../../../sufia-models/lib/tasks/migrate.rake", __FILE__)]
     end
 
     describe "deleting the namespace from ProxyDepositRequest#generic_work_id" do
@@ -38,7 +35,7 @@ describe "Rake tasks" do
       let(:receiver) { FactoryGirl.find_or_create(:archivist) }
       before do
         ProxyDepositRequest.create(generic_work_id: namespaced_id, sending_user: sender, receiving_user: receiver, sender_comment: "please take this")
-        Rake::Task["sufia:migrate:proxy_deposits"].invoke
+        run_task "sufia:migrate:proxy_deposits"
       end
       subject { ProxyDepositRequest.first.generic_work_id }
       it { is_expected.to eql corrected_id }
@@ -47,10 +44,34 @@ describe "Rake tasks" do
     describe "deleting the namespace from ChecksumAuditLog#file_set_id" do
       before do
         ChecksumAuditLog.create(file_set_id: namespaced_id)
-        Rake::Task["sufia:migrate:audit_logs"].invoke
+        run_task "sufia:migrate:audit_logs"
       end
       subject { ChecksumAuditLog.first.file_set_id }
       it { is_expected.to eql corrected_id }
+    end
+  end
+
+  describe "sufia:user:list_emails" do
+    let!(:user1) { FactoryGirl.create(:user) }
+    let!(:user2) { FactoryGirl.create(:user) }
+
+    before do
+      load_rake_environment [File.expand_path("../../../tasks/sufia_user.rake", __FILE__)]
+    end
+
+    it "creates a file" do
+      run_task "sufia:user:list_emails"
+      expect(File.exist?("user_emails.txt")).to be_truthy
+      expect(IO.read("user_emails.txt")).to include(user1.email, user2.email)
+      File.delete("user_emails.txt")
+    end
+
+    it "creates a file I give it" do
+      run_task "sufia:user:list_emails", "abc123.txt"
+      expect(File.exist?("user_emails.txt")).not_to be_truthy
+      expect(File.exist?("abc123.txt")).to be_truthy
+      expect(IO.read("abc123.txt")).to include(user1.email, user2.email)
+      File.delete("abc123.txt")
     end
   end
 end