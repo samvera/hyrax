samvera-deprecated/sufia

Adding recent users query end date so a range can be queried. fixes #1187
Additionally moved the recent_user query to the User model so it could be tested more efficiently

spec/controllers/admin_stats_controller_spec.rb
@@ -23,14 +23,24 @@ describe Admin::StatsController, type: :controller do
     end
 
     describe "querying user_stats" do
+      let(:one_day_ago_date) { 1.days.ago.to_datetime }
+      let(:two_days_ago_date) { 2.days.ago.to_datetime.end_of_day }
+      let(:one_day_ago) { one_day_ago_date.strftime("%Y-%m-%d") }
+      let(:two_days_ago) { two_days_ago_date.strftime("%Y-%m-%d") }
+
       it "defaults to latest 5 users" do
         get :index
         expect(assigns[:recent_users]).to eq(User.order('created_at DESC').limit(5))
       end
-      it "allows queries against user_stats" do
+      it "allows queries against user_stats without an end date " do
         expect(User).to receive(:where).with('id' => user1.id).once.and_return([user1])
-        expect(User).to receive(:where).with('created_at >= ?',  1.days.ago.strftime("%Y-%m-%d")).and_return([user2])
-        get :index, users_stats: {start_date:1.days.ago.strftime("%Y-%m-%d")}
+        expect(User).to receive(:recent_users).with(one_day_ago_date, nil).and_return([user2])
+        get :index, users_stats: { start_date: one_day_ago }
+        expect(assigns[:recent_users]).to eq([user2])
+      end
+      it "allows queries against user_stats with an end date" do
+        expect(User).to receive(:recent_users).with(two_days_ago_date, one_day_ago_date).and_return([user2])
+        get :index, users_stats: { start_date: two_days_ago, end_date: one_day_ago }
         expect(assigns[:recent_users]).to eq([user2])
       end
     end

spec/models/user_spec.rb
@@ -172,4 +172,31 @@ describe User, :type => :model do
       expect(another_user.can_receive_deposits_from.to_a).to eq [@subject]
     end
   end
+  describe "class methods" do
+    describe "recent_users" do
+      let(:new_users) { User.all.order(created_at: :desc) }
+
+      before do
+        (1..3).each {|i| User.create(email: "abc#{i}@blah.frg", password: "blarg1234", created_at:DateTime.now - i.days) }
+      end
+
+      context "when has a start date" do
+        subject { User.recent_users(Date.today - 2.days) }
+        it "returns valid data" do
+          expect(subject.count).to eq 2
+          is_expected.to include(new_users[0],new_users[1])
+          is_expected.not_to include(new_users[2])
+        end
+      end
+
+      context "when has start and end date" do
+        subject { User.recent_users(Date.today - 2.days, Date.today - 1.days) }
+        it "returns valid data" do
+          expect(subject.count).to eq 1
+          is_expected.to include(new_users[1])
+          is_expected.not_to include(new_users[2], new_users[0])
+        end
+      end
+    end
+  end
 end

samvera/hyrax

spec/controllers/admin_stats_controller_spec.rb
@@ -23,14 +23,26 @@ describe Admin::StatsController, type: :controller do
     end
 
     describe "querying user_stats" do
+      let(:one_day_ago_date) { 1.day.ago.to_datetime }
+      let(:two_days_ago_date) { 2.days.ago.to_datetime.end_of_day }
+      let(:one_day_ago) { one_day_ago_date.strftime("%Y-%m-%d") }
+      let(:two_days_ago) { two_days_ago_date.strftime("%Y-%m-%d") }
+
       it "defaults to latest 5 users" do
         get :index
         expect(assigns[:recent_users]).to eq(User.order('created_at DESC').limit(5))
       end
-      it "allows queries against user_stats" do
+
+      it "allows queries against user_stats without an end date " do
         expect(User).to receive(:where).with('id' => user1.id).once.and_return([user1])
-        expect(User).to receive(:where).with('created_at >= ?', 1.day.ago.strftime("%Y-%m-%d")).and_return([user2])
-        get :index, users_stats: { start_date: 1.day.ago.strftime("%Y-%m-%d") }
+        expect(User).to receive(:recent_users).with(one_day_ago_date, nil).and_return([user2])
+        get :index, users_stats: { start_date: one_day_ago }
+        expect(assigns[:recent_users]).to eq([user2])
+      end
+
+      it "allows queries against user_stats with an end date" do
+        expect(User).to receive(:recent_users).with(two_days_ago_date, one_day_ago_date).and_return([user2])
+        get :index, users_stats: { start_date: two_days_ago, end_date: one_day_ago }
         expect(assigns[:recent_users]).to eq([user2])
       end
     end

spec/models/user_spec.rb
@@ -171,4 +171,31 @@ describe User, type: :model do
       expect(another_user.can_receive_deposits_from.to_a).to eq [@subject]
     end
   end
+  describe "class methods" do
+    describe "recent_users" do
+      let(:new_users) { described_class.all.order(created_at: :desc) }
+
+      before do
+        (1..3).each { |i| described_class.create(email: "abc#{i}@blah.frg", password: "blarg1234", created_at: DateTime.now - i.days) }
+      end
+
+      context "when has a start date" do
+        subject { described_class.recent_users(Date.today - 2.days) }
+        it "returns valid data" do
+          expect(subject.count).to eq 2
+          is_expected.to include(new_users[0], new_users[1])
+          is_expected.not_to include(new_users[2])
+        end
+      end
+
+      context "when has start and end date" do
+        subject { described_class.recent_users(Date.today - 2.days, Date.today - 1.day) }
+        it "returns valid data" do
+          expect(subject.count).to eq 1
+          is_expected.to include(new_users[1])
+          is_expected.not_to include(new_users[2], new_users[0])
+        end
+      end
+    end
+  end
 end