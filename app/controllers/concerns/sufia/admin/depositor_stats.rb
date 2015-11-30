samvera-deprecated/sufia

Show deposits between a date range by users.  fixes #1188

app/controllers/concerns/sufia/admin/depositor_stats.rb
@@ -1,0 +1,42 @@
+#  Module to gather information about the deposits in the system
+#
+module Sufia::Admin::DepositorStats
+  extend ActiveSupport::Concern
+
+  # we are using blacklight facet queries for some of our searches so we have to include them when we are included
+  included do
+    include Blacklight::Base
+    self.copy_blacklight_config_from(CatalogController)
+  end
+
+  # Gather information about the depositors in the system
+  #
+  # @param [Hash] deposit_stats
+  # @option deposit_stats [String] :start_date optional string to specify the start date to gather the stats from
+  # @option deposit_stats [String] :end_date optional string to specify the end date to gather the stats from
+  #
+  def depositors(deposit_stats)
+    start_datetime = DateTime.parse(deposit_stats[:start_date]) unless deposit_stats[:start_date].blank?
+    end_datetime = DateTime.parse(deposit_stats[:end_date]).end_of_day unless deposit_stats[:end_date].blank?
+
+    query = GenericFile.build_date_query(start_datetime, end_datetime) unless start_datetime.blank?
+    sb = DepositSearchBuilder.new([:include_depositor_facet], self)
+    facet_results = repository.search(sb.merge(q: query).query)
+    facets = facet_results["facet_counts"]["facet_fields"]["depositor_ssim"]
+    depositors = []
+
+    # facet results come back in an array where the first item is the user and the second item is the count
+    # [ abc123, 55, ccczzz, 205 ]
+    # in the loop we are stepping through the array by twos to get the entire pair
+    # The item at i is the key and the item at i+1 is the number of files
+    (0...facets.length).step(2).each do |i|
+      depositor = {}
+      depositor[:key] = facets[i]
+      depositor[:deposits] = facets[i+1]
+      depositor[:user] = User.find_by_user_key(depositor[:key])
+      depositors << depositor
+    end
+    depositors
+  end
+
+end

spec/controllers/admin_stats_controller_spec.rb
@@ -63,9 +63,9 @@ describe Admin::StatsController, type: :controller do
     describe "counts" do
       context "when date range not set" do
         before do
-          FactoryGirl.create(:generic_file, depositor: user1)
-          FactoryGirl.create(:public_file, depositor: user1)
-          FactoryGirl.create(:registered_file, depositor: user1)
+          FactoryGirl.build(:generic_file, depositor: user1, id: "abc1223").update_index
+          FactoryGirl.build(:public_file, depositor: user1, id: "bbb1223").update_index
+          FactoryGirl.build(:registered_file, depositor: user1, id: "ccc1223").update_index
           Collection.create(title: "test") do |c|
             c.apply_depositor_metadata(user1.user_key)
           end
@@ -98,5 +98,32 @@ describe Admin::StatsController, type: :controller do
       end
     end
 
+    describe "depositor counts" do
+      before do
+        GenericFile.new(id: "abc123") do |gf|
+          gf.apply_depositor_metadata(user1)
+          gf.update_index
+        end
+        GenericFile.new(id: "def123") do |gf|
+          gf.apply_depositor_metadata(user2)
+          gf.update_index
+        end
+        GenericFile.new(id: "zzz123") do |gf|
+          gf.create_date = [2.days.ago]
+          gf.apply_depositor_metadata(user1)
+          gf.update_index
+        end
+      end
+
+      it "gathers user deposits" do
+        get :index
+        expect(assigns[:depositors]).to include({ key: user1.user_key, deposits: 2, user: user1 }, { key: user2.user_key, deposits: 1, user: user2 })
+      end
+
+      it "gathers user deposits during a date range" do
+        get :index, deposit_stats: {start_date: 1.days.ago.strftime("%Y-%m-%d"), end_date: 0.days.ago.strftime("%Y-%m-%d")}
+        expect(assigns[:depositors]).to include({ key: user1.user_key, deposits: 1, user: user1}, { key: user2.user_key, deposits: 1, user: user2 })
+      end
+    end
   end
 end

samvera/hyrax

app/controllers/concerns/sufia/admin/depositor_stats.rb
@@ -1,0 +1,41 @@
+#  Module to gather information about the deposits in the system
+#
+module Sufia::Admin::DepositorStats
+  extend ActiveSupport::Concern
+
+  # we are using blacklight facet queries for some of our searches so we have to include them when we are included
+  included do
+    include Blacklight::Base
+    copy_blacklight_config_from(CatalogController)
+  end
+
+  # Gather information about the depositors in the system
+  #
+  # @param [Hash] deposit_stats
+  # @option deposit_stats [String] :start_date optional string to specify the start date to gather the stats from
+  # @option deposit_stats [String] :end_date optional string to specify the end date to gather the stats from
+  #
+  def depositors(deposit_stats)
+    start_datetime = DateTime.parse(deposit_stats[:start_date]) unless deposit_stats[:start_date].blank?
+    end_datetime = DateTime.parse(deposit_stats[:end_date]).end_of_day unless deposit_stats[:end_date].blank?
+
+    query = GenericWork.build_date_query(start_datetime, end_datetime) unless start_datetime.blank?
+    sb = DepositSearchBuilder.new([:include_depositor_facet], self)
+    facet_results = repository.search(sb.merge(q: query).query)
+    facets = facet_results["facet_counts"]["facet_fields"]["depositor_ssim"]
+    depositors = []
+
+    # facet results come back in an array where the first item is the user and the second item is the count
+    # [ abc123, 55, ccczzz, 205 ]
+    # in the loop we are stepping through the array by twos to get the entire pair
+    # The item at i is the key and the item at i+1 is the number of files
+    (0...facets.length).step(2).each do |i|
+      depositor = {}
+      depositor[:key] = facets[i]
+      depositor[:deposits] = facets[i + 1]
+      depositor[:user] = User.find_by_user_key(depositor[:key])
+      depositors << depositor
+    end
+    depositors
+  end
+end

spec/controllers/admin_stats_controller_spec.rb
@@ -63,9 +63,9 @@ describe Admin::StatsController, type: :controller do
     describe "counts" do
       context "when date range not set" do
         before do
-          create(:generic_work, user: user1)
-          create(:public_generic_work, user: user1)
-          create(:registered_generic_work, user: user1)
+          build(:generic_work, user: user1, id: "abc1223").update_index
+          build(:public_generic_work, user: user1, id: "bbb1223").update_index
+          build(:registered_generic_work, user: user1, id: "ccc1223").update_index
           Collection.create(title: "test") do |c|
             c.apply_depositor_metadata(user1.user_key)
           end
@@ -96,6 +96,34 @@ describe Admin::StatsController, type: :controller do
           expect(GenericWork).to receive(:where_registered).and_call_original
           get :index, users_stats: { file_start_date: 1.day.ago.strftime("%Y-%m-%d"), file_end_date: 0.days.ago.strftime("%Y-%m-%d") }
         end
+      end
+    end
+
+    describe "depositor counts" do
+      before do
+        GenericWork.new(id: "abc123") do |gf|
+          gf.apply_depositor_metadata(user1)
+          gf.update_index
+        end
+        GenericWork.new(id: "def123") do |gf|
+          gf.apply_depositor_metadata(user2)
+          gf.update_index
+        end
+        GenericWork.new(id: "zzz123") do |gf|
+          gf.create_date = [2.days.ago]
+          gf.apply_depositor_metadata(user1)
+          gf.update_index
+        end
+      end
+
+      it "gathers user deposits" do
+        get :index
+        expect(assigns[:depositors]).to include({ key: user1.user_key, deposits: 2, user: user1 }, key: user2.user_key, deposits: 1, user: user2)
+      end
+
+      it "gathers user deposits during a date range" do
+        get :index, deposit_stats: { start_date: 1.day.ago.strftime("%Y-%m-%d"), end_date: 0.days.ago.strftime("%Y-%m-%d") }
+        expect(assigns[:depositors]).to include({ key: user1.user_key, deposits: 1, user: user1 }, key: user2.user_key, deposits: 1, user: user2)
       end
     end
   end