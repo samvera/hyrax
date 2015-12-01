samvera-deprecated/sufia

extracting a presenter from the admin stats controller behavior

spec/controllers/admin_stats_controller_spec.rb
@@ -14,6 +14,10 @@ describe Admin::StatsController, type: :controller do
     before do
       sign_in user1
     end
+    it "counts the users" do
+      get :index
+      expect(assigns[:presenter].users_count).to eq 2
+    end
 
     it 'allows an authorized user to view the page' do
       get :index
@@ -30,18 +34,18 @@ describe Admin::StatsController, type: :controller do
 
       it "defaults to latest 5 users" do
         get :index
-        expect(assigns[:recent_users]).to eq(User.order('created_at DESC').limit(5))
+        expect(assigns[:presenter].recent_users).to eq(User.order('created_at DESC').limit(5))
       end
       it "allows queries against user_stats without an end date " do
         expect(User).to receive(:where).with('id' => user1.id).once.and_return([user1])
         expect(User).to receive(:recent_users).with(one_day_ago_date, nil).and_return([user2])
         get :index, users_stats: { start_date: one_day_ago }
-        expect(assigns[:recent_users]).to eq([user2])
+        expect(assigns[:presenter].recent_users).to eq([user2])
       end
       it "allows queries against user_stats with an end date" do
         expect(User).to receive(:recent_users).with(two_days_ago_date, one_day_ago_date).and_return([user2])
         get :index, users_stats: { start_date: two_days_ago, end_date: one_day_ago }
-        expect(assigns[:recent_users]).to eq([user2])
+        expect(assigns[:presenter].recent_users).to eq([user2])
       end
     end
 
@@ -56,7 +60,7 @@ describe Admin::StatsController, type: :controller do
       end
       it "provides accurate files_count, ensuring that solr deletes have been expunged first" do
         get :index
-        expect(assigns[:files_count][:total]).to eq(original_files_count - 1)
+        expect(assigns[:presenter].files_count[:total]).to eq(original_files_count - 1)
       end
     end
 
@@ -72,10 +76,10 @@ describe Admin::StatsController, type: :controller do
         end
         it "includes files but not collections" do
           get :index
-          expect(assigns[:files_count][:total]).to eq(3)
-          expect(assigns[:files_count][:public]).to eq(1)
-          expect(assigns[:files_count][:registered]).to eq(1)
-          expect(assigns[:files_count][:private]).to eq(1)
+          expect(assigns[:presenter].files_count[:total]).to eq(3)
+          expect(assigns[:presenter].files_count[:public]).to eq(1)
+          expect(assigns[:presenter].files_count[:registered]).to eq(1)
+          expect(assigns[:presenter].files_count[:private]).to eq(1)
         end
       end
 
@@ -121,12 +125,13 @@ describe Admin::StatsController, type: :controller do
 
       it "gathers user deposits" do
         get :index
-        expect(assigns[:depositors]).to include({ key: user1.user_key, deposits: 2, user: user1 }, key: user2.user_key, deposits: 1, user: user2)
+        expect(assigns[:presenter].depositors).to include({ key: user1.user_key, deposits: 2, user: user1 }, key: user2.user_key, deposits: 1, user: user2)
+        expect(assigns[:presenter].active_users).to eq("example.com" => 4, user1.user_key.split('@')[0] => 3, user2.user_key.split('@')[0] => 1)
       end
 
       it "gathers user deposits during a date range" do
         get :index, deposit_stats: { start_date: 1.days.ago.strftime("%Y-%m-%d"), end_date: 0.days.ago.strftime("%Y-%m-%d") }
-        expect(assigns[:depositors]).to include({ key: user1.user_key, deposits: 1, user: user1 }, key: user2.user_key, deposits: 1, user: user2)
+        expect(assigns[:presenter].depositors).to include({ key: user1.user_key, deposits: 1, user: user1 }, key: user2.user_key, deposits: 1, user: user2)
       end
 
       context "more than 10 users" do
@@ -144,9 +149,34 @@ describe Admin::StatsController, type: :controller do
 
         it "gathers user deposits" do
           get :index
-          expect(assigns[:depositors]).to include({ key: user1.user_key, deposits: 2, user: user1 }, key: user2.user_key, deposits: 1, user: user2)
-          users.each { |user| expect(assigns[:depositors]).to include(key: user.user_key, deposits: 1, user: user) }
+          expect(assigns[:presenter].depositors).to include({ key: user1.user_key, deposits: 2, user: user1 }, key: user2.user_key, deposits: 1, user: user2)
+          users.each { |user| expect(assigns[:presenter].depositors).to include(key: user.user_key, deposits: 1, user: user) }
         end
+      end
+    end
+    describe "top formats" do
+      before do
+        GenericFile.new(id: "abc123") do |gf|
+          gf.apply_depositor_metadata(user1)
+          gf.mime_type = 'image/png'
+          gf.update_index
+        end
+        GenericFile.new(id: "def123") do |gf|
+          gf.apply_depositor_metadata(user2)
+          gf.mime_type = 'image/png'
+          gf.update_index
+        end
+        GenericFile.new(id: "zzz123") do |gf|
+          gf.create_date = [2.days.ago]
+          gf.apply_depositor_metadata(user1)
+          gf.mime_type = 'image/jpeg'
+          gf.update_index
+        end
+      end
+
+      it "gathers formats" do
+        get :index
+        expect(assigns[:presenter].top_formats).to eq("png" => 2, "jpeg" => 1)
       end
     end
   end

samvera/hyrax

spec/controllers/admin_stats_controller_spec.rb
@@ -14,6 +14,10 @@ describe Admin::StatsController, type: :controller do
     before do
       sign_in user1
     end
+    it "counts the users" do
+      get :index
+      expect(assigns[:presenter].users_count).to eq 2
+    end
 
     it 'allows an authorized user to view the page' do
       get :index
@@ -30,20 +34,20 @@ describe Admin::StatsController, type: :controller do
 
       it "defaults to latest 5 users" do
         get :index
-        expect(assigns[:recent_users]).to eq(User.order('created_at DESC').limit(5))
+        expect(assigns[:presenter].recent_users).to eq(User.order('created_at DESC').limit(5))
       end
 
       it "allows queries against user_stats without an end date " do
         expect(User).to receive(:where).with('id' => user1.id).once.and_return([user1])
         expect(User).to receive(:recent_users).with(one_day_ago_date, nil).and_return([user2])
         get :index, users_stats: { start_date: one_day_ago }
-        expect(assigns[:recent_users]).to eq([user2])
+        expect(assigns[:presenter].recent_users).to eq([user2])
       end
 
       it "allows queries against user_stats with an end date" do
         expect(User).to receive(:recent_users).with(two_days_ago_date, one_day_ago_date).and_return([user2])
         get :index, users_stats: { start_date: two_days_ago, end_date: one_day_ago }
-        expect(assigns[:recent_users]).to eq([user2])
+        expect(assigns[:presenter].recent_users).to eq([user2])
       end
     end
 
@@ -56,7 +60,7 @@ describe Admin::StatsController, type: :controller do
       end
       it "provides accurate files_count, ensuring that solr deletes have been expunged first" do
         get :index
-        expect(assigns[:files_count][:total]).to eq(original_files_count - 1)
+        expect(assigns[:presenter].files_count[:total]).to eq(original_files_count - 1)
       end
     end
 
@@ -73,10 +77,10 @@ describe Admin::StatsController, type: :controller do
 
         it "includes files but not collections" do
           get :index
-          expect(assigns[:files_count][:total]).to eq(3)
-          expect(assigns[:files_count][:public]).to eq(1)
-          expect(assigns[:files_count][:registered]).to eq(1)
-          expect(assigns[:files_count][:private]).to eq(1)
+          expect(assigns[:presenter].files_count[:total]).to eq(3)
+          expect(assigns[:presenter].files_count[:public]).to eq(1)
+          expect(assigns[:presenter].files_count[:registered]).to eq(1)
+          expect(assigns[:presenter].files_count[:private]).to eq(1)
         end
       end
 
@@ -122,12 +126,13 @@ describe Admin::StatsController, type: :controller do
 
       it "gathers user deposits" do
         get :index
-        expect(assigns[:depositors]).to include({ key: user1.user_key, deposits: 2, user: user1 }, key: user2.user_key, deposits: 1, user: user2)
+        expect(assigns[:presenter].depositors).to include({ key: user1.user_key, deposits: 2, user: user1 }, key: user2.user_key, deposits: 1, user: user2)
+        expect(assigns[:presenter].active_users).to eq("example.com" => 4, user1.user_key.split('@')[0] => 3, user2.user_key.split('@')[0] => 1)
       end
 
       it "gathers user deposits during a date range" do
         get :index, deposit_stats: { start_date: 1.day.ago.strftime("%Y-%m-%d"), end_date: 0.days.ago.strftime("%Y-%m-%d") }
-        expect(assigns[:depositors]).to include({ key: user1.user_key, deposits: 1, user: user1 }, key: user2.user_key, deposits: 1, user: user2)
+        expect(assigns[:presenter].depositors).to include({ key: user1.user_key, deposits: 1, user: user1 }, key: user2.user_key, deposits: 1, user: user2)
       end
 
       context "more than 10 users" do
@@ -145,9 +150,34 @@ describe Admin::StatsController, type: :controller do
 
         it "gathers user deposits" do
           get :index
-          expect(assigns[:depositors]).to include({ key: user1.user_key, deposits: 2, user: user1 }, key: user2.user_key, deposits: 1, user: user2)
-          users.each { |user| expect(assigns[:depositors]).to include(key: user.user_key, deposits: 1, user: user) }
+          expect(assigns[:presenter].depositors).to include({ key: user1.user_key, deposits: 2, user: user1 }, key: user2.user_key, deposits: 1, user: user2)
+          users.each { |user| expect(assigns[:presenter].depositors).to include(key: user.user_key, deposits: 1, user: user) }
         end
+      end
+    end
+    describe "top formats" do
+      before do
+        FileSet.new(id: "abc123") do |gf|
+          gf.apply_depositor_metadata(user1)
+          gf.mime_type = 'image/png'
+          gf.update_index
+        end
+        FileSet.new(id: "def123") do |gf|
+          gf.apply_depositor_metadata(user2)
+          gf.mime_type = 'image/png'
+          gf.update_index
+        end
+        FileSet.new(id: "zzz123") do |gf|
+          gf.create_date = [2.days.ago]
+          gf.apply_depositor_metadata(user1)
+          gf.mime_type = 'image/jpeg'
+          gf.update_index
+        end
+      end
+
+      it "gathers formats" do
+        get :index
+        expect(assigns[:presenter].top_formats).to eq("png" => 2, "jpeg" => 1)
       end
     end
   end