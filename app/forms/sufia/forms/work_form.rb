a4d0a48c19205dec295ae92659c8360bd03e5ea2
Adam Wead <amsterdamos@gmail.com>
2015-12-01 12:38:10 -0500

    Specify a different set of terms for GenericWork form, fixes #337

--- app/forms/sufia/forms/work_form.rb
+++ app/forms/sufia/forms/work_form.rb
@@ -1,5 +1,9 @@
 module Sufia::Forms
   class WorkForm < CurationConcerns::Forms::WorkForm
     delegate :depositor, :permissions, to: :model
+
+    def rendered_terms
+      terms - [:visibility]
+    end
   end
 end
