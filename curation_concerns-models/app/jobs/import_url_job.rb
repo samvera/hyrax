samvera-deprecated/sufia

Reload the object in the imprt url job after loading in the file incase another job has modified the object in the while the data was being copied

sufia-models/app/jobs/import_url_job.rb
@@ -13,6 +13,10 @@ class ImportUrlJob < ActiveFedoraIdBasedJob
 
     Tempfile.open(id.gsub('/', '_')) do |f|
       path, mime_type = copy_remote_file(generic_file.import_url, f)
+
+      # reload the generic file once the data is copied since this is a long running task
+      generic_file.reload
+
       # attach downloaded file to generic file stubbed out
       if Sufia::GenericFile::Actor.new(generic_file, user).create_content(f, path, 'content', mime_type)
         # add message to user for downloaded file

samvera/hyrax

curation_concerns-models/app/jobs/import_url_job.rb
@@ -11,6 +11,10 @@ class ImportUrlJob < ActiveFedoraIdBasedJob
 
     Tempfile.open(id.tr('/', '_')) do |f|
       copy_remote_file(file_set.import_url, f)
+
+      # reload the generic file once the data is copied since this is a long running task
+      file_set.reload
+
       # attach downloaded file to generic file stubbed out
       if CurationConcerns::FileSetActor.new(file_set, user).create_content(f)
         # send message to user on download success