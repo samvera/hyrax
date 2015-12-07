604df7deb16fc92db433c98fbb2a0880b68fab76
Justin Coyne <jcoyne@justincoyne.com>
2015-12-07 08:52:31 -0600

    Prune away dead code

--- app/controllers/concerns/curation_concerns/parent_container.rb
+++ app/controllers/concerns/curation_concerns/parent_container.rb
@@ -3,7 +3,6 @@ module CurationConcerns::ParentContainer
 
   included do
     helper_method :parent
-    # before_filter :authorize_edit_parent_rights!, except: [:show]  # Not sure we actually want this enforced any more (was originally in worthwhile), especially since FileSets and GenericWorks (which are PCDM::Objects) can belong to multiple parents
   end
 
   # TODO: this is slow, refactor to return a Presenter (fetch from solr)
@@ -35,15 +34,5 @@ module CurationConcerns::ParentContainer
 
     def new_or_create?
       %w(create new).include? action_name
-    end
-
-    def namespaced_parent_id
-      # Sufia::Noid.namespaceize(params[:parent_id])
-    end
-
-    # restricts edit access so that you can only edit a record if you can also edit its parent.
-
-    def authorize_edit_parent_rights!
-      authorize! :edit, parent_id
     end
 end
--- app/controllers/sessions_controller.rb
+++
@@ -1,4 +0,0 @@
-class SessionsController < Devise::SessionsController
-  include CurationConcerns::ThemedLayoutController
-  with_themed_layout '1_column'
-end
