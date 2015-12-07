8cdf984baf72ce4cc69770fc640da3845faff457
Justin Coyne <jcoyne@justincoyne.com>
2015-12-07 16:13:46 -0600

    Removed code that is duplicated by curation_concerns

--- sufia-models/app/models/concerns/sufia/collection_behavior.rb
+++ sufia-models/app/models/concerns/sufia/collection_behavior.rb
@@ -1,9 +1,6 @@
 module Sufia
   module CollectionBehavior
     extend ActiveSupport::Concern
-    include Hydra::Collection
-    include Sufia::ModelMethods
-    include Sufia::Noid
 
     included do
       before_save :update_permissions
@@ -12,12 +9,6 @@ module Sufia
 
     def update_permissions
       self.visibility = "open"
-    end
-
-    # Compute the sum of each file in the collection
-    # Return an integer of the result
-    def bytes
-      members.reduce(0) { |sum, fs| sum + fs.content.size.to_i }
     end
   end
 end
