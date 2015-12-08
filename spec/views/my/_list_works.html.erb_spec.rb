87b55d984774398039b37267bd366a479df08e75
Justin Coyne <jcoyne@justincoyne.com>
2015-12-08 15:41:33 -0600

    Draw the visibility badge on the work list. Fixes #1405

--- spec/views/my/_list_works.html.erb_spec.rb
+++ spec/views/my/_list_works.html.erb_spec.rb
@@ -23,6 +23,7 @@ describe 'my/_index_partials/_list_works.html.erb' do
     expect(rendered).to have_link work_title, href: curation_concerns_generic_work_path(work)
     expect(rendered).to have_link 'Edit Work', href: edit_curation_concerns_generic_work_path(work)
     expect(rendered).to have_link 'Delete Work', href: curation_concerns_generic_work_path(work)
+    expect(rendered).to have_css 'a.visibility-link', text: 'Private'
     expect(rendered).to have_link coll_title, href: collections.collection_path(collection)
   end
 end
