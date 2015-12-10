b66d9245a786fca41cfdc73a4c1acccf9a9564ef
Justin Coyne <jcoyne@justincoyne.com>
2015-12-10 08:17:19 -0600

    Use the correct parameters for the upload set form
    Fixes projecthydra/sufia#1434

--- spec/controllers/upload_sets_controller_spec.rb
+++ spec/controllers/upload_sets_controller_spec.rb
@@ -14,7 +14,7 @@ describe UploadSetsController do
       it "enqueues a upload_set job and redirects to generic_works list with a flash message" do
         expect(UploadSetUpdateJob).to receive(:perform_later).with(user.user_key, upload_set.id, { '1' => 'foo' },
                                                                    { tag: [] }, 'open').once
-        post :update, id: upload_set.id, title: { '1' => 'foo' }, visibility: 'open', work: { tag: [""] }
+        post :update, id: upload_set.id, title: { '1' => 'foo' }, visibility: 'open', upload_set: { tag: [""] }
         expect(response).to redirect_to routes.url_helpers.curation_concerns_generic_works_path
         expect(flash[:notice]).to include("Your files are being processed")
       end
@@ -25,12 +25,12 @@ describe UploadSetsController do
       let!(:work) { create(:generic_work, user: user, upload_set: upload_set) }
 
       it "they can set public read access" do
-        post :update, id: upload_set, visibility: "open", work: { tag: [""] }
+        post :update, id: upload_set, visibility: "open", upload_set: { tag: [""] }
         expect(work.reload.read_groups).to eq ['public']
       end
 
       it "they can set metadata like title" do
-        post :update, id: upload_set, work: { tag: ["footag", "bartag"] }, title: { work.id => ["New Title"] }
+        post :update, id: upload_set, upload_set: { tag: ["footag", "bartag"] }, title: { work.id => ["New Title"] }
         work.reload
         expect(work.title).to eq ["New Title"]
         # TODO: is order important?
@@ -38,7 +38,7 @@ describe UploadSetsController do
       end
 
       it "they cannot set any tags" do
-        post :update, id: upload_set, work: { tag: [""] }
+        post :update, id: upload_set, upload_set: { tag: [""] }
         expect(work.reload.tag).to be_empty
       end
     end
@@ -48,8 +48,8 @@ describe UploadSetsController do
       let!(:work) { create(:generic_work, title: ['Original Title'], upload_set: upload_set) }
 
       it "they cannot modify the object" do
-        post :update, id: upload_set, "work" => { "tag" => [""] },
-                      "title" => { work.id => "Title Won't Change" }
+        post :update, id: upload_set, upload_set: { "tag" => [""] },
+                      title: { work.id => "Title Won't Change" }
         work.reload
         expect(work.title).to eq ["Original Title"]
       end
