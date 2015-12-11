2ca5f36e58b545ef7ccf9871d828a9da7cc58a16
Adam Wead <amsterdamos@gmail.com>
2015-12-11 14:52:20 -0500

    Export endnote format from work, not file set, fixes #1420

--- app/controllers/concerns/sufia/works_controller_behavior.rb
+++ app/controllers/concerns/sufia/works_controller_behavior.rb
@@ -24,5 +24,18 @@ module Sufia
       def show_presenter
         Sufia::WorkShowPresenter
       end
+
+      # Called by CurationConcerns::FileSetsControllerBehavior#show
+      def additional_response_formats(format)
+        # TODO: This duplicates the same process in CurationConcerns::CurationConcernController.show
+        # for assigning a presenter. It could be extracted into a common method.
+        format.endnote do
+          _, document_list = search_results(params, CatalogController.search_params_logic + [:find_one])
+          curation_concern = document_list.first
+          raise CanCan::AccessDenied.new(nil, :show) unless curation_concern
+          presenter = show_presenter.new(curation_concern, current_ability)
+          render text: presenter.solr_document.export_as_endnote
+        end
+      end
   end
 end
