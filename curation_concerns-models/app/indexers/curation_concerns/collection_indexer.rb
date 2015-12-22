661f9a8d3d7f868a48ed75e19e78bf6dfd517b16
Justin Coyne <jcoyne@justincoyne.com>
2015-12-22 15:21:41 -0600

    Collections should have their thumbnails indexed

--- curation_concerns-models/app/indexers/curation_concerns/collection_indexer.rb
+++ curation_concerns-models/app/indexers/curation_concerns/collection_indexer.rb
@@ -1,12 +1,15 @@
 module CurationConcerns
   class CollectionIndexer < Hydra::PCDM::CollectionIndexer
+    include IndexesThumbnails
     STORED_INTEGER = Solrizer::Descriptor.new(:integer, :stored)
+
     def generate_solr_document
       super.tap do |solr_doc|
         # Makes Collections show under the "Collections" tab
         Solrizer.set_field(solr_doc, 'generic_type', 'Collection', :facetable)
         # Index the size of the collection in bytes
         solr_doc[Solrizer.solr_name(:bytes, STORED_INTEGER)] = object.bytes
+        solr_doc['thumbnail_path_ss'] = thumbnail_path
       end
     end
   end
--- spec/indexers/collection_indexer_spec.rb
+++ spec/indexers/collection_indexer_spec.rb
@@ -4,16 +4,17 @@ describe CurationConcerns::CollectionIndexer do
   let(:indexer) { described_class.new(collection) }
   let(:collection) { build(:collection) }
 
-  before { allow(collection).to receive(:bytes).and_return(1000) }
   describe "#generate_solr_document" do
+    before do
+      allow(collection).to receive(:bytes).and_return(1000)
+      allow(CurationConcerns::ThumbnailPathService).to receive(:call).and_return("/downloads/1234?file=thumbnail")
+    end
     subject { indexer.generate_solr_document }
 
-    it "has generic type" do
+    it "has required fields" do
       expect(subject.fetch('generic_type_sim')).to eq ["Collection"]
-    end
-
-    it "has bytes" do
       expect(subject.fetch('bytes_is')).to eq(1000)
+      expect(subject.fetch('thumbnail_path_ss')).to eq "/downloads/1234?file=thumbnail"
     end
   end
 end
