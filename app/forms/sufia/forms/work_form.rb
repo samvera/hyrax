351b3bcd7cd23e97419abd23967d192950f282f2
Nabeela Jaffer <njaffer@umich.edu>
2016-02-09 14:22:55 -0500

    remove form fields

--- app/forms/sufia/forms/work_form.rb
+++ app/forms/sufia/forms/work_form.rb
@@ -5,7 +5,7 @@ module Sufia::Forms
     def rendered_terms
       terms - [:files, :visibility_during_embargo, :embargo_release_date,
                :visibility_after_embargo, :visibility_during_lease,
-               :lease_expiration_date, :visibility_after_lease, :visibility]
+               :lease_expiration_date, :visibility_after_lease, :visibility, :thumbnail_id, :representative_id]
     end
   end
 end
