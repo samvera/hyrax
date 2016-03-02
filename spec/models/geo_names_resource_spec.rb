68ff6a9f995d849c1ba00869a8d7f72bc49a3fa9
Chris Beer <cabeer@stanford.edu>
2016-03-02 14:49:38 -0800

    Add a maximum line length

--- spec/models/geo_names_resource_spec.rb
+++ spec/models/geo_names_resource_spec.rb
@@ -4,7 +4,21 @@ describe GeoNamesResource, type: :model do
   before do
     state = {
       "totalResultsCount" => 1, "geonames" => [{
-        "countryId" => "1327865", "adminCode1" => "11", "countryName" => "Myanmar [Burma]", "fclName" => "country, state, region,...", "countryCode" => "MM", "lng" => "98", "fcodeName" => "first-order administrative division", "toponymName" => "Shan State", "fcl" => "A", "name" => "Shan State", "fcode" => "ADM1", "geonameId" => 1_297_099, "lat" => "22", "adminName1" => "Shan", "population" => 5_815_384
+        "countryId" => "1327865",
+        "adminCode1" => "11",
+        "countryName" => "Myanmar [Burma]",
+        "fclName" => "country, state, region,...",
+        "countryCode" => "MM",
+        "lng" => "98",
+        "fcodeName" => "first-order administrative division",
+        "toponymName" => "Shan State",
+        "fcl" => "A",
+        "name" => "Shan State",
+        "fcode" => "ADM1",
+        "geonameId" => 1_297_099,
+        "lat" => "22",
+        "adminName1" => "Shan",
+        "population" => 5_815_384
       }]
     }
     ActiveResource::HttpMock.respond_to do |mock|
