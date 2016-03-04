216d2e58fb531b82b1e4fcd4502f6404b5a4a434
Adam Wead <amsterdamos@gmail.com>
2016-03-04 16:10:26 -0500

    Add test coverage for RedisEventStore and resque approriately, fixes #1629

--- lib/sufia/redis_event_store.rb
+++ lib/sufia/redis_event_store.rb
@@ -1,5 +1,29 @@
 module Sufia
   class RedisEventStore
+    class << self
+      def for(key)
+        new(key)
+      end
+
+      # @return [Fixnum] the id of the event
+      def create(action, timestamp)
+        event_id = Redis.current.incr("events:latest_id")
+        Redis.current.hmset("events:#{event_id}", "action", action, "timestamp", timestamp)
+        event_id
+      rescue Redis::CommandError => e
+        logger.error("unable to create event: #{e}")
+        nil
+      end
+
+      def logger
+        Rails.logger || CurationConcerns::NullLogger.new
+      end
+    end
+
+    def initialize(key)
+      @key = key
+    end
+
     def fetch(size)
       Redis.current.lrange(@key, 0, size).map do |event_id|
         {
@@ -7,32 +31,14 @@ module Sufia
           timestamp: Redis.current.hget("events:#{event_id}", "timestamp")
         }
       end
-    rescue
+    rescue Redis::CommandError
       []
     end
 
     # Adds a value to the end of a list identified by key
     def push(value)
       Redis.current.lpush(@key, value)
-    rescue
-      nil
-    end
-
-    def initialize(key)
-      @key = key
-    end
-
-    def self.for(key)
-      new(key)
-    end
-
-    # @return [Fixnum] the id of the event
-    def self.create(action, timestamp)
-      event_id = Redis.current.incr("events:latest_id")
-      Redis.current.hmset("events:#{event_id}", "action", action, "timestamp", timestamp)
-      event_id
-    rescue => e
-      logger.error("unable to create event: #{e}") if logger
+    rescue Redis::CommandError
       nil
     end
   end
