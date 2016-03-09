66b632bc4eee3b9b4f644f2fb567f3bfa75b0bc4
Justin Coyne <jcoyne@justincoyne.com>
2016-03-09 17:06:21 -0500

    Use kaminari_route_prefix instead of a branch of kaminari

--- lib/curation_concerns.rb
+++ lib/curation_concerns.rb
@@ -2,6 +2,7 @@ require 'curation_concerns/version'
 require 'curation_concerns/engine'
 require 'curation_concerns/configuration'
 require 'blacklight_advanced_search'
+require 'kaminari_route_prefix'
 
 module CurationConcerns
 end
