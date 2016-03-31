5711558e8f3d95eabdb62a5f1b1e4295055f7f23
Justin Coyne <jcoyne@justincoyne.com>
2016-03-31 10:55:59 -0500

    Handle unauthorized when curation_concern is not set
    https://gist.github.com/val99erie/1db9036382148768c1ecdcc5408c9c00

---
+++ spec/views/curation_concerns/base/unauthorized.html.erb_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe 'curation_concerns/base/unauthorized.html.erb' do
+  context "when it responds to curation_concern" do
+    let(:concern) { double(human_readable_type: 'Book', id: '777') }
+    before do
+      allow(view).to receive(:curation_concern).and_return(concern)
+      render
+    end
+    it "shows a message to the user" do
+      expect(rendered).to have_content "Unauthorized The book you have tried to access is private ID: 777"
+    end
+  end
+
+  context "when it doesn't respond to curation_concern" do
+    before do
+      render
+    end
+    it "shows a message to the user" do
+      expect(rendered).to have_content "Unauthorized The page you have tried to access is private"
+    end
+  end
+end
