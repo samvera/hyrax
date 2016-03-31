113dd4b3e184077ca9b8be78bafd2e89e8a64901
Justin Coyne <jcoyne@justincoyne.com>
2016-03-31 08:20:47 -0500

    Avoid using deprecated Hydra::SearchBuilder

--- app/search_builders/curation_concerns/file_set_search_builder.rb
+++ app/search_builders/curation_concerns/file_set_search_builder.rb
@@ -1,9 +1,9 @@
 module CurationConcerns
-  class FileSetSearchBuilder < Hydra::SearchBuilder
+  class FileSetSearchBuilder < ::SearchBuilder
     include CurationConcerns::SingleResult
-    self.default_processor_chain += [:only_file_sets]
 
-    def only_file_sets(solr_parameters)
+    # This overrides the filter_models in FilterByType
+    def filter_models(solr_parameters)
       solr_parameters[:fq] << ActiveFedora::SolrQueryBuilder.construct_query_for_rel(has_model: ::FileSet.to_class_uri)
     end
   end
