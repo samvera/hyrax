88635fbc1d2fd2421537cc222fa70f1f7f58f154
Justin Coyne <jcoyne@justincoyne.com>
2016-04-06 17:46:44 -0500

    Separate the batch upload form from the base form
    Fixes #1777
    Fixes #1774

---
+++ spec/views/sufia/batch_uploads/_form.html.erb_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe 'sufia/batch_uploads/_form.html.erb', :no_clean do
+  let(:work) { GenericWork.new }
+  let(:ability) { double }
+  let(:form) { Sufia::BatchUploadForm.new(work, ability) }
+
+  before do
+    view.lookup_context.view_paths.push 'app/views/curation_concerns/base'
+    allow(view).to receive(:curation_concern).and_return(work)
+    allow(controller).to receive(:current_user).and_return(stub_model(User))
+    assign(:form, form)
+  end
+
+  let(:page) do
+    render
+    Capybara::Node::Simple.new(rendered)
+  end
+
+  it "draws the page" do
+    expect(page).to have_selector("form[action='/batch_uploads']")
+    expect(page).to have_selector("input#generic_work_title")
+    expect(page).to have_link("New Work", "/concern/generic_works/new")
+    expect(page).to have_link("Cancel", "/")
+  end
+end
