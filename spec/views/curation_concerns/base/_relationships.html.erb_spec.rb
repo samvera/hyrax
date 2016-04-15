46c514502840b478842b18e44d902ab16de15687
Justin Coyne <jcoyne@justincoyne.com>
2016-04-15 14:01:11 -0500

    Show collections on the work show page.
    Fixes #1480

---
+++ spec/views/curation_concerns/base/_relationships.html.erb_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe 'curation_concerns/base/relationships' do
+  let(:ability) { double }
+  let(:solr_doc) { double(id: '123', human_readable_type: 'Work') }
+  let(:presenter) { Sufia::WorkShowPresenter.new(solr_doc, ability) }
+
+  context "when collections are not present" do
+    before do
+      render 'curation_concerns/base/relationships', presenter: presenter
+    end
+    it "shows the message" do
+      expect(rendered).to match %r{This Work is not currently in any collections\.}
+    end
+  end
+
+  context "when collections are present" do
+    let(:collection_presenters) { [double(id: '456', title: 'Containing collection')] }
+    let(:page) { Capybara::Node::Simple.new(rendered) }
+    before do
+      allow(presenter).to receive(:collection_presenters).and_return(collection_presenters)
+      render 'curation_concerns/base/relationships', presenter: presenter
+    end
+    it "links to collections" do
+      expect(page).to have_link 'Containing collection'
+    end
+  end
+end
