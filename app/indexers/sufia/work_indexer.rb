1ad11b52f509162da0646cb3a8fbce5607da93d9
mtribone <mtribone@psu.edu>
2016-04-28 10:49:30 -0400

    Created a new service and override CC thumbnail path service

--- app/indexers/sufia/work_indexer.rb
+++ app/indexers/sufia/work_indexer.rb
@@ -1,5 +1,6 @@
 module Sufia
   class WorkIndexer < CurationConcerns::WorkIndexer
+    self.thumbnail_path_service = Sufia::WorkThumbnailPathService
     def generate_solr_document
       super.tap do |solr_doc|
         # This enables us to return a Work when we have a FileSet that matches
