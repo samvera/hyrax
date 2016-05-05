2e60b6d954c60cf25236ad22f31177f18d33a54d
Colin Gross <grosscol@gmail.com>
2016-05-05 10:06:58 -0400

    Add active attribute to rights source yml. Make rights service utilize active attribute of terms. Add fixture file of dummy rights for testing. Update rights tests to use fixture file.

--- app/services/rights_service.rb
+++ app/services/rights_service.rb
@@ -3,12 +3,14 @@ module RightsService
   self.authority = Qa::Authorities::Local.subauthority_for('rights')
 
   def self.select_options
-    authority.all.map do |element|
-      [element[:label], element[:id]]
-    end
+    active_elements.map { |e| [e[:label], e[:id]] }
   end
 
   def self.label(id)
     authority.find(id).fetch('term')
+  end
+
+  def self.active_elements
+    authority.all.select { |e| authority.find(e[:id])[:active] }
   end
 end
