3bb73e84a65e453156c3f52baf684e7fd0ec6675
Carrick Rogers <carrickr@iu.edu>
2016-05-05 14:07:17 -0400

    Addressing @grosscol's comment regarding repetitive documentation. Related to #766 [skip ci]

--- app/actors/curation_concerns/actors.rb
+++ app/actors/curation_concerns/actors.rb
@@ -5,11 +5,10 @@ module CurationConcerns
   # (often via user http request to a controller).  It
   # performs one or more steps in the business process of a
   # create, update, or destroy command issued by a user.
-  # An Actor is invoked by a controller or jobs as the result
-  # of command line runner.  You may a have a stack of multiple
-  # actors that each perform one specific task.  Actors should
-  # have a specific task and be chained together to execute
-  # more complex business purposes.
+  # You may a have a stack of multiple actors that each
+  # perform one action.  Actors should have a specific task
+  # and be chained together to execute more complex business
+  # purposes.
   # @example A series of actors responsible for creating an object:
   #   Metadata Writing Actor, Rights Assignment Actor,
   #   Indexing Actor
