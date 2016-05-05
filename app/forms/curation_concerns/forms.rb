af663e48f884f1d1cfadb1c6dfecc7d08a2e82fe
Gordon Leacock <gordonl@umich.edu>
2016-05-05 11:53:51 -0400

    add form documentation see #766 [skip ci]
    Reformat indentations to comply with Rubocop

---
+++ app/forms/curation_concerns/forms.rb
@@ -0,0 +1,13 @@
+module CurationConcerns
+ # This will be the module for containing forms
+ # @since 0.14.0
+ # Forms are a type of data structures that include validation that leverage hydra editor
+ # capabilities. Their purpose is to allow users to edit and save objects based on models.
+ # The impetus for their creation was to remove validation from models. A form must define
+ # the model it uses and attributes it manipulates. It may include default values and define
+ # required fields.
+ # They are ruby objects that are typically instantiated by the controller to facilitate
+ # user creation, editing, or deleting of an object.
+  module Forms
+  end
+end
