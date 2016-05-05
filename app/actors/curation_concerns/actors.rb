7b0b13ecced64ae7f350fb25629e77995edb48dc
Carrick Rogers <carrickr@iu.edu>
2016-05-05 11:05:28 -0400

    Creation of an Actors module as a future home for all current Actors in the directory.  Adds documention for the Actors noun concept. Related to #766 [skip ci]
    Soft wrap

---
+++ app/actors/curation_concerns/actors.rb
@@ -0,0 +1,19 @@
+module CurationConcerns
+  # Module for containing Actors
+  # @since 0.14.0
+  # An Actor coordinates the response to a user command
+  # (often via user http request to a controller).  It
+  # performs one or more steps in the business process of a
+  # create, update, or destroy command issued by a user.
+  # An Actor is invoked by a controller or jobs as the result
+  # of command line runner.  You may a have a stack of multiple
+  # actors that each perform one specific task.  Actors should
+  # have a specific task and be chained together to execute
+  # more complex business purposes.
+  # @example A series of actors responsible for creating an object:
+  #   Metadata Writing Actor, Rights Assignment Actor,
+  #   Indexing Actor
+  # @see CurationConcerns::AbstractActor for primitive interface definition
+  module Actors
+  end
+end
