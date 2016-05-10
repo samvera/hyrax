6fcee43551c272ac522021f99d546f82f3c238a2
Michael J. Giarlo <mjgiarlo@stanford.edu>
2016-05-10 11:06:32 -0700

    Dump unused Redis config value.
    The `config.redis_namespace` value is largely vestigial in CC at this point; it is used only by a migrate task that turns ChecksumAuditLog entries about GenericFiles into ones about FileSets. CC has no legacy data with GenericFiles, unless we are supporting a Curate- or Worthwhile-to-CC path, for which I am seeing a lack of tooling and documentation. This provides a cleaner boundary between Sufia, which cares about Redis for activity streams where namespacing is relevant, and CC, which uses Redis only as a locking mechanism.

--- lib/generators/curation_concerns/templates/config/redis_config.rb
+++ lib/generators/curation_concerns/templates/config/redis_config.rb
@@ -1,26 +1,3 @@
-if defined?(PhusionPassenger)
-  PhusionPassenger.on_event(:starting_worker_process) do |forked|
-    # We're in smart spawning mode.
-    if forked
-      # Re-establish redis connection
-      require 'redis'
-      config = YAML.load(ERB.new(IO.read(File.join(Rails.root, 'config', 'redis.yml'))).result)[Rails.env].with_indifferent_access
-
-      # The important two lines
-      Redis.current.disconnect!
-      Redis.current = begin
-                        Redis.new(config.merge(thread_safe: true))
-                      rescue
-                        nil
-                      end
-    end
-  end
-else
-  config = YAML.load(ERB.new(IO.read(File.join(Rails.root, 'config', 'redis.yml'))).result)[Rails.env].with_indifferent_access
-  require 'redis'
-  Redis.current = begin
-             Redis.new(config.merge(thread_safe: true))
-           rescue
-             nil
-           end
-end
+require 'redis'
+config = YAML.load(ERB.new(IO.read(File.join(Rails.root, 'config', 'redis.yml'))).result)[Rails.env].with_indifferent_access
+Redis.current = Redis.new(config.merge(thread_safe: true))
