532a6a87e7dedd44b239cf42b08055bcd30aceaf
Justin Coyne <jcoyne@justincoyne.com>
2016-06-01 13:15:31 -0500

    Add reporting for works created over time

--- app/services/sufia/statistics/works.rb
+++ app/services/sufia/statistics/works.rb
@@ -1,11 +1,11 @@
 module Sufia
   module Statistics
-    class Collections < OverTime
+    class Works < OverTime
       private
 
         def point(min, max)
           query = QueryService.new.build_date_query(min, max)
-          Collection.where(query).count
+          GenericWork.where(query).count
         end
     end
   end
