1228c0c833fb7128952c0bf038b3038ad54bade7
Justin Coyne <jcoyne@justincoyne.com>
2016-06-08 08:45:56 -0500

    Shares controller should show works
    Previously it was attempting to show
    `ActiveFedora::Aggregation::ListSource`s.
    
    Fixes #2162
    
    Also refactors search builders to move methods to the most useful
    concrete class.

--- app/search_builders/sufia/my_collections_search_builder.rb
+++ app/search_builders/sufia/my_collections_search_builder.rb
@@ -6,4 +6,11 @@ class Sufia::MyCollectionsSearchBuilder < Sufia::SearchBuilder
     :show_only_resources_deposited_by_current_user,
     :show_only_collections
   ]
+
+  def show_only_collections(solr_parameters)
+    solr_parameters[:fq] ||= []
+    solr_parameters[:fq] += [
+      ActiveFedora::SolrQueryBuilder.construct_query_for_rel(has_model: Collection.to_class_uri)
+    ]
+  end
 end
--- app/search_builders/sufia/search_builder.rb
+++ app/search_builders/sufia/search_builder.rb
@@ -4,13 +4,6 @@ class Sufia::SearchBuilder < Blacklight::SearchBuilder
   include Hydra::AccessControlsEnforcement
   include CurationConcerns::SearchFilters
 
-  def show_only_collections(solr_parameters)
-    solr_parameters[:fq] ||= []
-    solr_parameters[:fq] += [
-      ActiveFedora::SolrQueryBuilder.construct_query_for_rel(has_model: Collection.to_class_uri)
-    ]
-  end
-
   def show_only_resources_deposited_by_current_user(solr_parameters)
     solr_parameters[:fq] ||= []
     solr_parameters[:fq] += [
@@ -24,56 +17,4 @@ class Sufia::SearchBuilder < Blacklight::SearchBuilder
       ActiveFedora::SolrQueryBuilder.construct_query_for_rel(has_model: ::GenericWork.to_class_uri)
     ]
   end
-
-  def show_only_shared_files(solr_parameters)
-    solr_parameters[:fq] ||= []
-    solr_parameters[:fq] += [
-      "-" + ActiveFedora::SolrQueryBuilder.construct_query_for_rel(depositor: scope.current_user.user_key)
-    ]
-  end
-
-  def show_only_highlighted_works(solr_parameters)
-    ids = scope.current_user.trophies.pluck(:work_id)
-    solr_parameters[:fq] ||= []
-    solr_parameters[:fq] += [
-      ActiveFedora::SolrQueryBuilder.construct_query_for_ids(ids)
-    ]
-  end
-
-  # Limits search results just to GenericWorks and collections
-  # @param solr_parameters the current solr parameters
-  # @param user_parameters the current user-submitted parameters
-  def only_works_and_collections(solr_parameters)
-    solr_parameters[:fq] ||= []
-    solr_parameters[:fq] << "#{Solrizer.solr_name('has_model', :symbol)}:(\"GenericWork\" \"Collection\")"
-  end
-
-  # show both works that match the query and works that contain files that match the query
-  def show_works_or_works_that_contain_files(solr_parameters)
-    return if solr_parameters[:q].blank?
-    solr_parameters[:user_query] = solr_parameters[:q]
-    solr_parameters[:q] = new_query
-  end
-
-  protected
-
-    # the {!lucene} gives us the OR syntax
-    def new_query
-      "{!lucene}#{interal_query(dismax_query)} #{interal_query(join_for_works_from_files)}"
-    end
-
-    # the _query_ allows for another parser (aka dismax)
-    def interal_query(query_value)
-      "_query_:\"#{query_value}\""
-    end
-
-    # the {!dismax} causes the query to go against the query fields
-    def dismax_query
-      "{!dismax v=$user_query}"
-    end
-
-    # join from file id to work relationship solrized file_set_ids_ssim
-    def join_for_works_from_files
-      "{!join from=#{ActiveFedora.id_field} to=file_set_ids_ssim}#{dismax_query}"
-    end
 end
