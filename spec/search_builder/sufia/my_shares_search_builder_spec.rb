fe4fab16ee5b79f1b2b3e5925d0154ccb61325ba
Justin Coyne <jcoyne@justincoyne.com>
2016-06-08 10:24:04 -0500

    Update the curation-concerns dependency to beta8

--- spec/search_builder/sufia/my_shares_search_builder_spec.rb
+++ spec/search_builder/sufia/my_shares_search_builder_spec.rb
@@ -4,18 +4,15 @@ describe Sufia::MySharesSearchBuilder do
   let(:scope) { double('The scope',
                        blacklight_config: config,
                        params: {},
+                       current_ability: Ability.new(me),
                        current_user: me) }
   let(:builder) { described_class.new(scope) }
-
-  before do
-    builder.current_ability = Ability.new(me)
-  end
 
   subject { builder.to_hash['fq'] }
 
   it "filters things we have access to in which we are not the depositor" do
     expect(subject).to eq ["edit_access_group_ssim:public OR edit_access_group_ssim:registered OR edit_access_person_ssim:#{me.user_key}",
-                           "(_query_:\"{!field f=has_model_ssim}GenericWork\" OR _query_:\"{!field f=has_model_ssim}Collection\")",
+                           "{!terms f=has_model_ssim}GenericWork,Collection",
                            "-_query_:\"{!field f=depositor_ssim}#{me.user_key}\""]
   end
 end
