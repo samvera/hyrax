3a220bf88b2d4d54cf93e7a61000493a3e6aae1a
Justin Coyne <jcoyne@justincoyne.com>
2016-06-09 11:59:17 -0500

    Give media_display a presenter
    Media display only takes a presenter in the latest version of
    curation_concerns. Previously we were sending a FileSet.

--- spec/features/edit_file_spec.rb
+++ spec/features/edit_file_spec.rb
@@ -2,17 +2,19 @@ describe "Editing a file:", type: :feature do
   let(:user) { create(:user) }
   let(:file_title) { 'Some kind of title' }
   let(:work) { build(:work, user: user) }
-  let(:file) { work.members.first }
+  let(:file_set) { create(:file_set, user: user, title: [file_title]) }
+  let(:file) { File.open(fixture_path + '/world.png') }
 
   before do
     sign_in user
-    work.ordered_members << create(:file_set, user: user, title: [file_title])
+    Hydra::Works::AddFileToFileSet.call(file_set, file, :original_file)
+    work.ordered_members << file_set
     work.save!
   end
 
   context 'when the user tries to update file content, but forgets to select a file:' do
     it 'shows the edit page again' do
-      visit edit_curation_concerns_file_set_path(file)
+      visit edit_curation_concerns_file_set_path(file_set)
       click_link 'Versions'
       click_button 'Upload New Version'
       expect(page).to have_content "Edit #{file_title}"
