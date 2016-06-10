2a40c97cf1a8da53c2c6e4502dd5e75ab8a6c6a7
Justin Coyne <jcoyne@justincoyne.com>
2016-06-10 13:35:16 -0500

    Leverage CurationConcerns::FilterByType
    Fixes #2171

--- app/search_builders/sufia/my_collections_search_builder.rb
+++ app/search_builders/sufia/my_collections_search_builder.rb
@@ -1,16 +1,11 @@
 # Added to allow for the My controller to show only things I have edit access to
 class Sufia::MyCollectionsSearchBuilder < Sufia::SearchBuilder
   include Sufia::MySearchBuilderBehavior
+  include CurationConcerns::FilterByType
 
-  self.default_processor_chain += [
-    :show_only_resources_deposited_by_current_user,
-    :show_only_collections
-  ]
+  self.default_processor_chain += [:show_only_resources_deposited_by_current_user]
 
-  def show_only_collections(solr_parameters)
-    solr_parameters[:fq] ||= []
-    solr_parameters[:fq] += [
-      ActiveFedora::SolrQueryBuilder.construct_query_for_rel(has_model: Collection.to_class_uri)
-    ]
+  def only_collections?
+    true
   end
 end
--- app/search_builders/sufia/my_works_search_builder.rb
+++ app/search_builders/sufia/my_works_search_builder.rb
@@ -1,10 +1,11 @@
 # Added to allow for the My controller to show only things I have edit access to
 class Sufia::MyWorksSearchBuilder < Sufia::SearchBuilder
   include Sufia::MySearchBuilderBehavior
+  include CurationConcerns::FilterByType
 
-  self.default_processor_chain = [:add_advanced_search_to_solr] + default_processor_chain
-  self.default_processor_chain += [
-    :show_only_resources_deposited_by_current_user,
-    :show_only_generic_works
-  ]
+  self.default_processor_chain += [:add_advanced_search_to_solr, :show_only_resources_deposited_by_current_user]
+
+  def only_works?
+    true
+  end
 end
