62e414ff107665191b4944be4e6b547b0a713a1a
Justin Coyne <jcoyne@justincoyne.com>
2016-06-10 11:33:40 -0500

    Remove instances of `curation_concerns_generic_work_path`
    fixes #2194

--- spec/views/collections/_show_document_list_menu.html.erb_spec.rb
+++ spec/views/collections/_show_document_list_menu.html.erb_spec.rb
@@ -2,13 +2,13 @@
 describe 'collections/_show_document_list_menu.html.erb', type: :view do
   context 'when user logged in displaying the collections of current user' do
     let(:user) { create :user }
-    let(:work) { build(:public_work, user: user, id: "1234") }
-    let(:collection) { build(:work, user: user) }
-
-    before { collection.members << work }
+    let(:document) { SolrDocument.new(id: '1234') }
+    before do
+      allow(document).to receive(:to_model).and_return(stub_model(GenericWork))
+    end
 
     it "displays the action list in individual work drop down" do
-      render(partial: 'collections/show_document_list_menu.html.erb', locals: { id: work.id, current_user: user })
+      render('collections/show_document_list_menu.html.erb', document: document, current_user: user)
       expect(rendered).to have_content 'Select an action'
       expect(rendered).not_to have_content 'Single-Use Link to File'
       expect(rendered).to have_content 'Edit'
