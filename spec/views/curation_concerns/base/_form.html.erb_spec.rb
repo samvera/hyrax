e1bf1482fac0020b8b6650ff6cee55d2afbe951f
Justin Coyne <jcoyne@justincoyne.com>
2016-06-13 11:20:25 -0500

    Use the non-deprecated mocking syntax
    Also combine tests to optimize the suite for speed

--- spec/views/curation_concerns/base/_form.html.erb_spec.rb
+++ spec/views/curation_concerns/base/_form.html.erb_spec.rb
@@ -16,8 +16,8 @@ describe 'curation_concerns/base/_form.html.erb' do
     allow(view).to receive(:curation_concern).and_return(work)
     allow(controller).to receive(:current_user).and_return(stub_model(User))
     assign(:form, form)
-    controller.stub(:controller_name).and_return('batch_uploads')
-    controller.stub(:action_name).and_return('new')
+    allow(controller).to receive(:controller_name).and_return('batch_uploads')
+    allow(controller).to receive(:action_name).and_return('new')
   end
 
   let(:page) do
@@ -27,19 +27,14 @@ describe 'curation_concerns/base/_form.html.erb' do
 
   context "for a new object" do
     let(:work) { GenericWork.new }
-    it "routes to the GenericWorkController" do
-      expect(page).to have_selector("form[action='/concern/generic_works']")
-    end
-
-    it "has a switch to Batch Upload link" do
-      expect(page).to have_link('Batch upload')
-    end
-
     context 'with browse-everything disabled (default)' do
       before do
         allow(Sufia.config).to receive(:browse_everything) { nil }
       end
-      it 'does not render the BE upload widget' do
+      it "draws the page" do
+        expect(page).to have_selector("form[action='/concern/generic_works']")
+        expect(page).to have_link('Batch upload')
+        # does not render the BE upload widget
         expect(page).not_to have_selector('button#browse-btn')
       end
     end
@@ -70,27 +65,12 @@ describe 'curation_concerns/base/_form.html.erb' do
   end
 
   context "for a persisted object" do
-    it "routes to the GenericWorkController" do
+    it "draws the page" do
       expect(page).to have_selector("form[action='/concern/generic_works/456']")
-    end
-
-    describe 'when the work has two or more resource types' do
-      it "only draws one resource_type multiselect" do
-        expect(page).to have_selector("select#generic_work_resource_type", count: 1)
-      end
-      it "allows to change the thumbnail" do
-        expect(page).to have_selector("select#generic_work_thumbnail_id", count: 1)
-      end
-      it "allows to change the representative media" do
-        expect(page).to have_selector("select#generic_work_representative_id", count: 1)
-      end
-    end
-
-    it "doesn't have switch to Batch Upload link" do
+      expect(page).to have_selector("select#generic_work_resource_type", count: 1)
+      expect(page).to have_selector("select#generic_work_thumbnail_id", count: 1)
+      expect(page).to have_selector("select#generic_work_representative_id", count: 1)
       expect(page).not_to have_link('Batch upload', href: '/batch_uploads')
-    end
-
-    it "renders the link for the Cancel button" do
       expect(page).to have_link("Cancel", href: "/dashboard")
     end
   end
