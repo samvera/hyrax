99c720fe1d3be57bce8fca1df04ba52678f7ab03
Justin Coyne <jcoyne@justincoyne.com>
2016-06-15 10:21:59 -0500

    Pass replacment function to CleanGraphRepository
    This allows us to use a more dyanmic replacement function that has
    inputs such as the request context

--- hydra-core/app/models/hydra/content_negotiation/clean_graph_repository.rb
+++ hydra-core/app/models/hydra/content_negotiation/clean_graph_repository.rb
@@ -2,14 +2,18 @@ module Hydra::ContentNegotiation
   # CleanGraphRepository has a #find interface which returns a graph for use
   # with content negotiation.
   class CleanGraphRepository
-    attr_reader :connection
-    def initialize(connection)
+    attr_reader :connection, :replacer
+    # @param [#get] connection the connection to fedora
+    # @param [#call] replacer a function that is called with id and graph and returns a string representation of the new uri.
+    def initialize(connection, replacer = Hydra.config.id_to_resource_uri)
       @connection = connection
+      @replacer = replacer
     end
 
     def find(id)
       ReplacingGraphFinder.new(
-        GraphFinder.new(connection, id)
+        GraphFinder.new(connection, id),
+        replacer
       ).graph
     end
   end
--- hydra-core/app/models/hydra/content_negotiation/replacing_graph_finder.rb
+++ hydra-core/app/models/hydra/content_negotiation/replacing_graph_finder.rb
@@ -2,6 +2,13 @@ module Hydra::ContentNegotiation
   # Decorator for Finder which replaces Fedora subjects in the graph with a 
   # configured URI
   class ReplacingGraphFinder < SimpleDelegator
+
+    attr_reader :replacer
+    def initialize(graph_finder, replacer)
+      super(graph_finder)
+      @replacer = replacer
+    end
+
     def graph
       graph_replacer.run
     end
@@ -9,7 +16,9 @@ module Hydra::ContentNegotiation
     private
 
     def graph_replacer
-      ::Hydra::ContentNegotiation::FedoraUriReplacer.new(base_uri, __getobj__.graph)
+      ::Hydra::ContentNegotiation::FedoraUriReplacer.new(base_uri,
+                                                         __getobj__.graph,
+                                                         replacer)
     end
 
     def base_uri
