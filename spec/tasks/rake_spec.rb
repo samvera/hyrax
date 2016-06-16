cbff59e60ed2fdf93daed2dacb2a388fd17b92f3
Justin Coyne <jcoyne@justincoyne.com>
2016-06-15 22:05:13 -0500

    Remove the sufia:migrate task
    This was intended to migrate Sufia 5 to Sufia 6.  It is not necessary in
    Sufia 7

--- spec/tasks/rake_spec.rb
+++ spec/tasks/rake_spec.rb
@@ -1,34 +1,6 @@
 require 'rake'
 
 describe "Rake tasks" do
-  describe "sufia:migrate" do
-    let(:namespaced_id) { "sufia:123" }
-    let(:corrected_id)  { "123" }
-    before do
-      load_rake_environment [File.expand_path("../../../tasks/migrate.rake", __FILE__)]
-    end
-
-    describe "deleting the namespace from ProxyDepositRequest#work_id" do
-      let(:sender) { create(:user) }
-      let(:receiver) { create(:user) }
-      before do
-        ProxyDepositRequest.create(work_id: namespaced_id, sending_user: sender, receiving_user: receiver, sender_comment: "please take this")
-        run_task "sufia:migrate:proxy_deposits"
-      end
-      subject { ProxyDepositRequest.first.work_id }
-      it { is_expected.to eql corrected_id }
-    end
-
-    describe "deleting the namespace from ChecksumAuditLog#file_set_id" do
-      before do
-        ChecksumAuditLog.create(file_set_id: namespaced_id)
-        run_task "sufia:migrate:audit_logs"
-      end
-      subject { ChecksumAuditLog.first.file_set_id }
-      it { is_expected.to eql corrected_id }
-    end
-  end
-
   describe "sufia:user:list_emails" do
     let!(:user1) { FactoryGirl.create(:user) }
     let!(:user2) { FactoryGirl.create(:user) }
