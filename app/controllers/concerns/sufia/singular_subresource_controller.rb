6c07a934a1672569215a43a1f8b65e0224f9ef86
Justin Coyne <jcoyne@justincoyne.com>
2016-06-21 16:46:52 -0700

    Load works using CurationConcerns::WorkRelation

--- app/controllers/concerns/sufia/singular_subresource_controller.rb
+++ app/controllers/concerns/sufia/singular_subresource_controller.rb
@@ -2,8 +2,9 @@ module Sufia::SingularSubresourceController
   extend ActiveSupport::Concern
 
   included do
+    before_action :find_work, only: :work
+    load_and_authorize_resource :work, only: :work
     load_and_authorize_resource :file, class: 'FileSet', only: :file, id_param: :id
-    load_and_authorize_resource :work, class: 'GenericWork', only: :work, id_param: :id
   end
 
   # Overriding the default behavior from Hydra::Core::ControllerBehavior
@@ -14,5 +15,9 @@ module Sufia::SingularSubresourceController
       session['user_return_to'.freeze] = request.url
       redirect_to new_user_session_path, alert: exception.message
     end
+  end
+
+  def find_work
+    @work = CurationConcerns::WorkRelation.new.find(params[:id])
   end
 end
