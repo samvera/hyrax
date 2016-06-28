bb9cfdab7cd14d1069cfc18ee3d2293423accd1d
Justin Coyne <jcoyne@justincoyne.com>
2016-06-28 12:53:03 -0500

    Don't hardcode the param_key in the javascript

--- spec/views/batch_edits/edit.html.erb_spec.rb
+++ spec/views/batch_edits/edit.html.erb_spec.rb
@@ -1,8 +1,13 @@
 describe 'batch_edits/edit.html.erb', type: :view do
-  let(:generic_work) { stub_model(GenericWork, id: nil, depositor: 'bob', rights: ['']) }
-  let(:form) { double }
+  let(:generic_work) { stub_model(GenericWork, id: '999', depositor: 'bob', rights: ['']) }
+  let(:batch) { ['999'] }
+  let(:form) { Sufia::Forms::BatchEditForm.new(generic_work, nil, batch) }
 
   before do
+    allow(GenericWork).to receive(:load_instance_from_solr).and_return(generic_work)
+
+    # this prevents AF from hitting Fedora (permissions is a related object)
+    allow(generic_work).to receive(:permissions_attributes=)
     allow(controller).to receive(:current_user).and_return(stub_model(User))
     allow(form).to receive(:model).and_return(generic_work)
     allow(form).to receive(:names).and_return(['title 1', 'title 2'])
@@ -13,6 +18,7 @@ describe 'batch_edits/edit.html.erb', type: :view do
   end
 
   it "draws help for description" do
+    expect(rendered).to have_selector 'form[data-model="generic_work"]'
     expect(rendered).to have_selector ".generic_work_description p.help-block"
   end
 end
