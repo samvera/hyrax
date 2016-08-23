b2ca8afe510a4c79d4ab54856a6f3aa310ea56a5
Anna Headley <anna3LC@gmail.com>
2016-08-22 21:06:44 -0400

    Only show link to file manager when there is more than one child member

--- spec/views/curation_concerns/base/_show_actions.html.erb_spec.rb
+++ spec/views/curation_concerns/base/_show_actions.html.erb_spec.rb
@@ -1,27 +1,19 @@
 require 'spec_helper'
 
 describe 'curation_concerns/base/_show_actions.html.erb', type: :view do
+  let(:presenter) { Sufia::WorkShowPresenter.new(solr_document, ability) }
+  let(:solr_document) { SolrDocument.new(attributes) }
+  let(:attributes) { work.to_solr }
   let(:ability) { double }
-  let(:presenter) do
-    Sufia::WorkShowPresenter.new(
-      SolrDocument.new(
-        id: "123",
-        title_tesim: "Parent",
-        has_model_ssim: ["GenericWork"]
-      ),
-      ability
-    )
-  end
-  let(:member) do
-    Sufia::WorkShowPresenter.new(
-      SolrDocument.new(
-        id: "work",
-        title_tesim: "Child Work",
-        has_model_ssim: ["GenericWork"]
-      ),
-      ability
-    )
-  end
+  let(:work) { create(:work, id: "123", title: ["Parent"]) }
+  let(:member) { Sufia::WorkShowPresenter.new(member_document, ability) }
+  let(:member_document) { SolrDocument.new(member_attributes) }
+  let(:member_attributes) { member_work.to_solr }
+  let(:member_work) { create(:generic_work, id: "work", title: ["Child Work"]) }
+  let(:file_member) { Sufia::FileSetPresenter.new(file_document, ability) }
+  let(:file_document) { SolrDocument.new(file_attributes) }
+  let(:file_attributes) { file.to_solr }
+  let(:file) { create(:file_set, id: 'file') }
 
   before do
     allow(ability).to receive(:can?).with(:create, FeaturedWork).and_return(false)
@@ -55,9 +47,18 @@ describe 'curation_concerns/base/_show_actions.html.erb', type: :view do
         expect(rendered).to have_link 'Delete'
       end
     end
-    context "when the work contains children" do
+    context "when the work contains 1 child" do
       before do
         allow(presenter).to receive(:member_presenters).and_return([member])
+        render 'curation_concerns/base/show_actions.html.erb', presenter: presenter
+      end
+      it "does not show file manager link" do
+        expect(rendered).not_to have_link 'File Manager'
+      end
+    end
+    context "when the work contains 2 children" do
+      before do
+        allow(presenter).to receive(:member_presenters).and_return([member, file_member])
         render 'curation_concerns/base/show_actions.html.erb', presenter: presenter
       end
       it "shows file manager link" do
