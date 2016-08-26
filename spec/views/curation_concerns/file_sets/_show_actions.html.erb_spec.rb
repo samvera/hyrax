b2a771bf458e38b2cd23abbdc017e2cdba026125
Adam Wead <amsterdamos@gmail.com>
2016-08-26 09:36:17 -0400

    Restore translation for single-use-link button

--- spec/views/curation_concerns/file_sets/_show_actions.html.erb_spec.rb
+++ spec/views/curation_concerns/file_sets/_show_actions.html.erb_spec.rb
@@ -20,8 +20,8 @@ describe 'curation_concerns/file_sets/_show_actions.html.erb', type: :view do
   let(:presenter) do
     Sufia::WorkShowPresenter.new(solr_document, ability)
   end
+  let(:page) { Capybara::Node::Simple.new(rendered) }
   describe 'citations' do
-    let(:page) { Capybara::Node::Simple.new(rendered) }
     before do
       Sufia.config.citations = citations
       allow(controller).to receive(:can?).with(:edit, presenter).and_return(false)
@@ -44,6 +44,21 @@ describe 'curation_concerns/file_sets/_show_actions.html.erb', type: :view do
       it 'does not appear on page' do
         expect(page).to have_no_selector('a#citations')
       end
+    end
+  end
+
+  describe 'editor' do
+    before do
+      allow(presenter).to receive(:editor?).and_return(true)
+      assign(:presenter, presenter)
+      view.lookup_context.view_paths.push 'app/views/curation_concerns/base'
+      render
+    end
+
+    it 'renders actions for the user' do
+      expect(page).to have_link("Edit This File")
+      expect(page).to have_link("Delete This File")
+      expect(page).to have_link("Single-Use Link to File")
     end
   end
 end
