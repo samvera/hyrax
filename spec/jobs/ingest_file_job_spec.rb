26e17fb7a655f90dcb1db23687e3f878ce9e85cd
Adam Wead <amsterdamos@gmail.com>
2016-08-30 13:58:49 -0400

    Refactor ingest workflow to use current working file
    If the current working file used by the initial ingest is already available locally, it should pass that file to successive jobs. Each subsequent job
    can verify the file still exists locally and continue to use it; otherwise, it will download a new local working copy.

--- spec/jobs/ingest_file_job_spec.rb
+++ spec/jobs/ingest_file_job_spec.rb
@@ -20,16 +20,16 @@ describe IngestFileJob do
       Object.send(:remove_const, :FileSetWithExtras)
     end
     it 'uses the provided relationship' do
-      expect(CharacterizeJob).to receive(:perform_later).with(file_set, String)
-      described_class.perform_now(file_set, filename, 'image/png', 'bob', 'remastered')
+      expect(CharacterizeJob).to receive(:perform_later).with(file_set, String, filename)
+      described_class.perform_now(file_set, filename, user, mime_type: 'image/png', relation: 'remastered')
       expect(file_set.reload.remastered.mime_type).to eq 'image/png'
     end
   end
 
   context 'when given a mime_type' do
     it 'uses the provided mime_type' do
-      expect(CharacterizeJob).to receive(:perform_later).with(file_set, String)
-      described_class.perform_now(file_set, filename, 'image/png', 'bob')
+      expect(CharacterizeJob).to receive(:perform_later).with(file_set, String, filename)
+      described_class.perform_now(file_set, filename, user, mime_type: 'image/png')
       expect(file_set.reload.original_file.mime_type).to eq 'image/png'
     end
   end
@@ -40,8 +40,8 @@ describe IngestFileJob do
       # The parameter versioning: false instructs the machinery in Hydra::Works NOT to do versioning. So it can be handled later on.
       allow(CurationConcerns::VersioningService).to receive(:create)
       expect(Hydra::Works::AddFileToFileSet).to receive(:call).with(file_set, instance_of(::File), :original_file, versioning: false).and_call_original
-      expect(CharacterizeJob).to receive(:perform_later).with(file_set, String)
-      described_class.perform_now(file_set, filename, nil, 'bob')
+      expect(CharacterizeJob).to receive(:perform_later).with(file_set, String, filename)
+      described_class.perform_now(file_set, filename, user)
     end
   end
 
@@ -53,8 +53,8 @@ describe IngestFileJob do
 
     before do
       allow(Hydra::Works::CharacterizationService).to receive(:run).with(any_args)
-      described_class.perform_now(file_set, file1, 'image/png', user.user_key)
-      described_class.perform_now(file_set, file2, 'text/plain', user2.user_key)
+      described_class.perform_now(file_set, file1, user.user_key, mime_type: 'image/png')
+      described_class.perform_now(file_set, file2, user2.user_key, mime_type: 'text/plain')
     end
 
     it 'has two versions' do
