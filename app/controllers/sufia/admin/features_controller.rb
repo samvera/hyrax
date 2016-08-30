da414acb074f8493855d9e7a3e065a200f2884ed
Justin Coyne <jcoyne@justincoyne.com>
2016-08-30 10:12:20 -0500

    Add feature flipper to the admin menu

--- app/controllers/sufia/admin/features_controller.rb
+++ app/controllers/sufia/admin/features_controller.rb
@@ -1,8 +1,12 @@
 module Sufia
   module Admin
     class FeaturesController < Flip::FeaturesController
+      layout 'admin'
       before_action do
         authorize! :manage, Sufia::Feature
+        add_breadcrumb  'Home', root_path
+        add_breadcrumb  'Repository Dashboard', sufia.admin_path
+        add_breadcrumb  'Settings', sufia.admin_features_path
       end
     end
   end
