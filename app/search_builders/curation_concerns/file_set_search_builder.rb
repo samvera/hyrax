5f9f674fc8889e6abe766a5487278d0eed87a632
Justin Coyne <jcoyne@justincoyne.com>
2016-09-08 13:05:40 -0500

    Refactor filtering by type

--- app/search_builders/curation_concerns/file_set_search_builder.rb
+++ app/search_builders/curation_concerns/file_set_search_builder.rb
@@ -2,9 +2,9 @@ module CurationConcerns
   class FileSetSearchBuilder < ::SearchBuilder
     include CurationConcerns::SingleResult
 
-    # This overrides the filter_models in FilterByType
-    def filter_models(solr_parameters)
-      solr_parameters[:fq] << ActiveFedora::SolrQueryBuilder.construct_query_for_rel(has_model: ::FileSet.to_class_uri)
+    # This overrides the models in FilterByType
+    def models
+      [::FileSet.to_class_uri]
     end
   end
 end
