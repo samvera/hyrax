b7c01fd2c6a04961caf9f789fda188639d1b7dab
Andy Smith <andjsmit@iupui.edu>
2016-09-09 17:05:41 -0400

    Adds total item count for admin sets.
    Added view test and fixed rubocop issues.
    
    Revisions per jcoyne comments.
    
    Another rubocop fix.
    
    Change requests.

--- app/presenters/sufia/admin_set_presenter.rb
+++ app/presenters/sufia/admin_set_presenter.rb
@@ -1,4 +1,7 @@
 module Sufia
   class AdminSetPresenter < CollectionPresenter
+    def total_items
+      ActiveFedora::SolrService.count("{!field f=isPartOf_ssim}#{id}", fl: id)
+    end
   end
 end
