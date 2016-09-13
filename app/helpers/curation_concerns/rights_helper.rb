d003622354ac941bd290fbb9afd9973bd77b1c8b
Justin Coyne <jcoyne@justincoyne.com>
2016-09-13 15:54:49 -0500

    Move include_current_value to the LicenseService

--- app/helpers/curation_concerns/rights_helper.rb
+++
@@ -1,9 +0,0 @@
-module CurationConcerns::RightsHelper
-  def include_current_value(value, _index, render_options, html_options)
-    unless value.blank? || RightsService.active?(value)
-      html_options[:class] << ' force-select'
-      render_options += [[RightsService.label(value), value]]
-    end
-    [render_options, html_options]
-  end
-end
