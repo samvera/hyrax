b1e62d81983e9de4ec5c7e440cbe95457e1d43aa
Justin Coyne <jcoyne@justincoyne.com>
2016-09-20 09:29:43 -0400

    Fix test for Sipity::Entity

--- spec/models/sipity/entity_spec.rb
+++ spec/models/sipity/entity_spec.rb
@@ -1,8 +1,9 @@
+require 'spec_helper'
+
 module Sipity
   RSpec.describe Entity, type: :model do
     subject { described_class }
-    its(:column_names) { is_expected.to include("proxy_for_id") }
-    its(:column_names) { is_expected.to include("proxy_for_type") }
+    its(:column_names) { is_expected.to include("proxy_for") }
     its(:column_names) { is_expected.to include("workflow_id") }
     its(:column_names) { is_expected.to include("workflow_state_id") }
 
