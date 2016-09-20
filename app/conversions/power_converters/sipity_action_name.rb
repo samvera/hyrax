af611025c6f1fe20876d4148eb0dd85b612c6227
Justin Coyne <jcoyne@justincoyne.com>
2016-09-20 10:48:06 -0400

    Add sipity_action_name converter

---
+++ app/conversions/power_converters/sipity_action_name.rb
@@ -0,0 +1,9 @@
+require 'power_converter'
+PowerConverter.define_conversion_for(:sipity_action_name) do |input|
+  case input
+  when String, Symbol
+    input.to_s.sub(/[\?\!]\Z/, '')
+  when Sipity::WorkflowAction
+    input.name
+  end
+end
