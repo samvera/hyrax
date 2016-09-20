2a2074784c29afe366f4f8f95d3f3a23e350e9be
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2016-09-20 09:44:53 -0400

    Adding PowerConverter#convert_to_sipity_role
    Addresses #994

---
+++ app/conversions/power_converters/sipity_role.rb
@@ -0,0 +1,9 @@
+require 'power_converter'
+PowerConverter.define_conversion_for(:sipity_role) do |input|
+  case input
+  when Sipity::Role
+    input
+  when String, Symbol
+    Sipity::Role.find_by(name: input)
+  end
+end
---
+++ spec/conversions/power_converters/sipity_role_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+require "#{CurationConcerns::Engine.root}/app/conversions/power_converters/sipity_role"
+
+RSpec.describe 'PowerConverter', no_clean: true do
+  context 'role' do
+    it "will convert Sipity::Role" do
+      object = Sipity::Role.new
+      expect(PowerConverter.convert(object, to: :sipity_role)).to eq(object)
+    end
+
+    it "will convert a #to_sipity_role object" do
+      object = double(to_sipity_role: Sipity::Role.new)
+      expect(PowerConverter.convert(object, to: :sipity_role)).to eq(object.to_sipity_role)
+    end
+
+    it "will convert a string to a Sipity::Role if there exists a Sipity::Role with a name equal to the string" do
+      Sipity::Role.create!(name: 'hello')
+      expect(PowerConverter.convert('hello', to: :sipity_role)).to be_a(Sipity::Role)
+    end
+
+    it "will raise an exception if given a string and no Sipity::Role exists with that name" do
+      expect { PowerConverter.convert('hello', to: :sipity_role) }.to raise_error(PowerConverter::ConversionError)
+    end
+
+    it "will convert a base object with composed attributes delegator" do
+      base_object = Sipity::Role.new
+      expect(PowerConverter.convert(base_object, to: :sipity_role)).to eq(base_object)
+    end
+
+    it 'will not convert an arbitrary object' do
+      expect { PowerConverter.convert(double, to: :sipity_role) }.to raise_error(PowerConverter::ConversionError)
+    end
+  end
+end
