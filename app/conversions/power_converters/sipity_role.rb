23fc9670bfcc0726c2b5565b3ed4d86a627f7ab2
Justin Coyne <jcoyne@justincoyne.com>
2016-09-21 09:37:37 -0400

    Create a role if none exists

--- app/conversions/power_converters/sipity_role.rb
+++ app/conversions/power_converters/sipity_role.rb
@@ -3,6 +3,6 @@ PowerConverter.define_conversion_for(:sipity_role) do |input|
   when Sipity::Role
     input
   when String, Symbol
-    Sipity::Role.find_by(name: input)
+    Sipity::Role.find_or_create_by(name: input)
   end
 end
--- spec/conversions/power_converters/sipity_role_spec.rb
+++ spec/conversions/power_converters/sipity_role_spec.rb
@@ -2,31 +2,31 @@ require 'spec_helper'
 
 RSpec.describe 'PowerConverter', no_clean: true do
   context 'role' do
-    it "will convert Sipity::Role" do
+    it "converts Sipity::Role" do
       object = Sipity::Role.new
       expect(PowerConverter.convert(object, to: :sipity_role)).to eq(object)
     end
 
-    it "will convert a #to_sipity_role object" do
+    it "converts a #to_sipity_role object" do
       object = double(to_sipity_role: Sipity::Role.new)
       expect(PowerConverter.convert(object, to: :sipity_role)).to eq(object.to_sipity_role)
     end
 
-    it "will convert a string to a Sipity::Role if there exists a Sipity::Role with a name equal to the string" do
+    it "converts a string to a Sipity::Role if there exists a Sipity::Role with a name equal to the string" do
       Sipity::Role.create!(name: 'hello')
       expect(PowerConverter.convert('hello', to: :sipity_role)).to be_a(Sipity::Role)
     end
 
-    it "will raise an exception if given a string and no Sipity::Role exists with that name" do
-      expect { PowerConverter.convert('hello', to: :sipity_role) }.to raise_error(PowerConverter::ConversionError)
+    it "creates a new role if given a string and no Sipity::Role exists with that name" do
+      expect { PowerConverter.convert('hello', to: :sipity_role) }.to change { Sipity::Role.count }.by(1)
     end
 
-    it "will convert a base object with composed attributes delegator" do
+    it "converts a base object with composed attributes delegator" do
       base_object = Sipity::Role.new
       expect(PowerConverter.convert(base_object, to: :sipity_role)).to eq(base_object)
     end
 
-    it 'will not convert an arbitrary object' do
+    it 'does not convert an arbitrary object' do
       expect { PowerConverter.convert(double, to: :sipity_role) }.to raise_error(PowerConverter::ConversionError)
     end
   end
