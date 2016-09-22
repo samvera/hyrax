44d941e0a12b941946aab4585fafda74230df232
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2016-09-22 09:29:26 -0400

    Appeasing Rubocop

--- app/models/sipity/notification.rb
+++ app/models/sipity/notification.rb
@@ -19,7 +19,7 @@ module Sipity
     NOTIFICATION_TYPE_EMAIL = 'email'.freeze
 
     # TODO: There are other types, but for now, we are assuming just emails to send.
-    enum(notification_type: { NOTIFICATION_TYPE_EMAIL => NOTIFICATION_TYPE_EMAIL } )
+    enum(notification_type: { NOTIFICATION_TYPE_EMAIL => NOTIFICATION_TYPE_EMAIL })
 
     def self.valid_notification_types
       notification_types.keys
