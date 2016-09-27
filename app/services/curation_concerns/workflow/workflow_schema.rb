070c8861b74ef7ede3859098d89565d8b3e96f28
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2016-09-27 12:41:08 -0400

    Updating documentation concerning WorkflowSchema
    [skip ci]

--- app/services/curation_concerns/workflow/workflow_schema.rb
+++ app/services/curation_concerns/workflow/workflow_schema.rb
@@ -1,18 +1,29 @@
 module CurationConcerns
   module Workflow
-    # Responsible for describing the JSON schema for a Workflow
+    # Responsible for describing the JSON schema for a Workflow.
+    #
+    # The `work_types name` defines the name of the Sipity::Workflow.
+    # The `work_types actions` defines the actions that can be taken in the given states (e.g. :from_states) by the given :roles.
+    # The `work_types actions transition_to` defines the state to which we transition when the action is taken.
+    # The `work_types actions notifications` defines the notifications that should be sent when the action is taken.
+    #
+    # @see Sipity::Workflow
+    # @see Sipity::WorkflowAction
+    # @see Sipity::WorkflowState
+    # @see Sipity::Role
+    # @see Sipity::Notification
     WorkflowSchema = Dry::Validation.Schema do
       required(:work_types).each do
-        required(:name).filled(:str?)
+        required(:name).filled(:str?) # Sipity::Workflow#name
         required(:actions).each do
-          required(:name).filled(:str?)
+          required(:name).filled(:str?) # Sipity::WorkflowAction#name
           required(:from_states).each do
-            required(:names) { array? { each(:str?) } }
-            required(:roles) { array? { each(:str?) } }
+            required(:names) { array? { each(:str?) } } # Sipity::WorkflowState#name
+            required(:roles) { array? { each(:str?) } } # Sipity::Role#name
           end
-          required(:transition_to).filled(:str?)
+          required(:transition_to).filled(:str?) # Sipity::WorkflowState#name
           optional(:notifications).each do
-            required(:name).value(format?: /\A[a-z|_]+\Z/i)
+            required(:name).value(format?: /\A[a-z|_]+\Z/i) # Sipity::Notification#name
             required(:notification_type).value(included_in?: Sipity::Notification.valid_notification_types)
             required(:to) { array? { each(:str?) } }
             optional(:cc) { array? { each(:str?) } }
