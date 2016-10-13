0f2625e232c871463a04ed688639498d3269c87f
Justin Coyne <jcoyne@justincoyne.com>
2016-10-13 11:33:35 -0500

    Add a built in deactivate function for workflow

--- app/services/curation_concerns/workflow/activate_object.rb
+++ app/services/curation_concerns/workflow/activate_object.rb
@@ -1,7 +1,6 @@
 module CurationConcerns
   module Workflow
     class ActivateObject
-      # See https://github.com/bbatsov/rubocop/issues/3130
       def self.call(entity:, **)
         entity.proxy_for.state = Vocab::FedoraResourceStatus.active
       end
