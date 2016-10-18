a1c03c40ee3c15e59974324e3ea644a8b0a244ed
Justin Coyne <jcoyne@justincoyne.com>
2016-10-18 11:15:51 -0500

    Don't allow mediated deposit to grant edit access

--- spec/forms/sufia/forms/work_form_spec.rb
+++ spec/forms/sufia/forms/work_form_spec.rb
@@ -1,4 +1,4 @@
-describe Sufia::Forms::WorkForm do
+describe Sufia::Forms::WorkForm, :no_clean do
   let(:work) { GenericWork.new }
   let(:form) { described_class.new(work, nil) }
   let(:works) { [GenericWork.new, FileSet.new, GenericWork.new] }
@@ -15,6 +15,36 @@ describe Sufia::Forms::WorkForm do
     it "expects ordered work members" do
       allow(work).to receive(:ordered_members).and_return(works)
       expect(form.ordered_work_members.size).to eq(2)
+    end
+  end
+
+  describe ".build_permitted_params" do
+    before do
+      allow(described_class).to receive(:model_class).and_return(GenericWork)
+    end
+    subject { described_class.build_permitted_params }
+    context "without mediated deposit" do
+      it { is_expected.to include(permissions_attributes: [:type, :name, :access, :id, :_destroy]) }
+    end
+  end
+
+  describe ".model_attributes" do
+    before do
+      allow(described_class).to receive(:model_class).and_return(GenericWork)
+    end
+    subject { described_class.model_attributes(ActionController::Parameters.new(attributes)) }
+    context "and a user is granted edit access" do
+      let(:attributes) { { permissions_attributes: [{ type: 'person', name: 'justin', access: 'edit' }] } }
+
+      context "without mediated deposit" do
+        it { is_expected.to eq ActionController::Parameters.new(permissions_attributes: [ActionController::Parameters.new(type: 'person', name: 'justin', access: 'edit')]).permit! }
+      end
+      context "with mediated deposit" do
+        before do
+          allow(Flipflop).to receive(:enable_mediated_deposit?).and_return(true)
+        end
+        it { is_expected.to eq ActionController::Parameters.new(permissions_attributes: []).permit! }
+      end
     end
   end
 end
