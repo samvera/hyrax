57f21560322ae05fcce95970a941180351b606cc
Justin Coyne <jcoyne@justincoyne.com>
2016-10-27 11:32:16 -0500

    i18n for form_progress partial
    Also changes "Enter required metadata" to "Describe your work" fixes

--- spec/views/sufia/batch_uploads/_form.html.erb_spec.rb
+++ spec/views/sufia/batch_uploads/_form.html.erb_spec.rb
@@ -7,16 +7,8 @@ describe 'sufia/batch_uploads/_form.html.erb', type: :view do
   let(:user) { stub_model(User) }
 
   before do
-    view.lookup_context.view_paths.push "#{CurationConcerns::Engine.root}/app/views/curation_concerns/base"
-    view.lookup_context.view_paths.push 'app/views/sufia/batch_uploads'
-    view.lookup_context.view_paths.push 'app/views/curation_concerns/base'
-    allow(view).to receive(:curation_concern).and_return(work)
+    stub_template "curation_concerns/base/_guts4form.html.erb" => "Form guts"
     assign(:form, form)
-    allow(controller).to receive_messages(current_user: user,
-                                          controller_name: 'batch_uploads',
-                                          action_name: 'new',
-                                          repository: CurationConcerns::GenericWorksController.new.repository,
-                                          blacklight_config: CurationConcerns::GenericWorksController.new.blacklight_config)
   end
 
   let(:page) do
@@ -28,8 +20,6 @@ describe 'sufia/batch_uploads/_form.html.erb', type: :view do
     expect(page).to have_selector("form[action='/batch_uploads']")
     # No title, because it's captured per file (e.g. Display label)
     expect(page).not_to have_selector("input#generic_work_title")
-    expect(page).to have_link("New Work", href: "/concern/generic_works/new")
-    expect(page).to have_link("Cancel", href: "/dashboard")
-    expect(rendered).to match(/Display label/)
+    expect(view.content_for(:files_tab)).to have_link("New Work", href: "/concern/generic_works/new")
   end
 end
