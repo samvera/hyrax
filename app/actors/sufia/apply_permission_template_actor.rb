a9f5645701defdca1f2f14c10700213cf9dceeb1
Justin Coyne <jcoyne@justincoyne.com>
2016-11-03 07:19:36 -0400

    Handle the case where the admin_set_id is a blank string

--- app/actors/sufia/apply_permission_template_actor.rb
+++ app/actors/sufia/apply_permission_template_actor.rb
@@ -8,8 +8,8 @@ module Sufia
     protected
 
       def add_edit_users(attributes)
-        return unless attributes[:admin_set_id]
-        template = Sufia::PermissionTemplate.find_by(admin_set_id: attributes[:admin_set_id])
+        return unless attributes[:admin_set_id].present?
+        template = Sufia::PermissionTemplate.find_by!(admin_set_id: attributes[:admin_set_id])
         curation_concern.edit_users = template.access_grants.where(agent_type: 'user', access: 'manage').pluck(:agent_id)
         curation_concern.edit_groups = template.access_grants.where(agent_type: 'group', access: 'manage').pluck(:agent_id)
         curation_concern.read_users = template.access_grants.where(agent_type: 'user', access: 'view').pluck(:agent_id)
