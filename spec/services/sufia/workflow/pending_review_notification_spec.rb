5ee8d28cf933173ca482b4b586ddac4240bafdea
Michael J. Giarlo <mjgiarlo@stanford.edu>
2016-11-18 14:47:12 -0800

    Notifications should be sent for workflow actions. Fixes #2627.

---
+++ spec/services/sufia/workflow/pending_review_notification_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+RSpec.describe Sufia::Workflow::PendingReviewNotification do
+  let(:depositor) { create(:user) }
+  let(:to_user) { create(:user) }
+  let(:cc_user) { create(:user) }
+  let(:workflow) { Sipity::Workflow.create(name: 'foo') }
+  let(:workflow_state) { Sipity::WorkflowState.create(name: 'bar', workflow: workflow) }
+  let(:work) { create(:generic_work, user: depositor) }
+  let(:entity) do
+    Sipity::Entity.create!(proxy_for_global_id: work.to_global_id.to_s,
+                           workflow: workflow,
+                           workflow_state: workflow_state)
+  end
+  let(:comment) { double("comment", comment: 'A pleasant read') }
+  let(:recipients) { { to: [to_user], cc: [cc_user] } }
+
+  describe ".send_notification" do
+    it 'sends a message to all users except depositor' do
+      expect(depositor).to receive(:send_message).once.and_call_original
+
+      expect { described_class.send_notification(entity: entity, user: depositor, comment: comment, recipients: recipients) }
+        .to change { depositor.mailbox.inbox.count }.by(1)
+        .and change { to_user.mailbox.inbox.count }.by(1)
+        .and change { cc_user.mailbox.inbox.count }.by(1)
+    end
+  end
+end
