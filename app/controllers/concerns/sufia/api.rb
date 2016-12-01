daeeca084b23107d60a86aef10d464cb4bd94f27
Justin Coyne <jcoyne@justincoyne.com>
2016-12-01 14:30:40 -0600

    Move API translations to sufia

--- app/controllers/concerns/sufia/api.rb
+++ app/controllers/concerns/sufia/api.rb
@@ -14,11 +14,11 @@ module Sufia
         success: {
           code: 200,
           message: "Request Succeeded",
-          description: I18n.t('curation_concerns.api.success.default')
+          description: I18n.t('sufia.api.success.default')
         },
         deleted: {
           code: 200,
-          message: I18n.t('curation_concerns.api.deleted.default')
+          message: I18n.t('sufia.api.deleted.default')
         },
         created: {
           code: 201,
@@ -27,38 +27,38 @@ module Sufia
         accepted: {
           code: 202,
           message: "Accepted",
-          description: I18n.t('curation_concerns.api.accepted.default')
+          description: I18n.t('sufia.api.accepted.default')
         },
         bad_request: {
           code: 400,
           message: "Bad Request",
-          description: I18n.t('curation_concerns.api.bad_request.default')
+          description: I18n.t('sufia.api.bad_request.default')
         },
         unauthorized: {
           code: 401,
           message: "Authentication Required",
-          description: I18n.t('curation_concerns.api.unauthorized.default')
+          description: I18n.t('sufia.api.unauthorized.default')
         },
         forbidden: {
           code: 403,
           message: "Not Authorized",
-          description: I18n.t('curation_concerns.api.forbidden.default')
+          description: I18n.t('sufia.api.forbidden.default')
         },
         not_found: {
           code: 404,
           message: "Resource not found",
-          description: I18n.t('curation_concerns.api.not_found.default')
+          description: I18n.t('sufia.api.not_found.default')
         },
         unprocessable_entity: {
           code: 422,
           message: "Unprocessable Entity",
-          description: I18n.t('curation_concerns.api.unprocessable_entity.default'),
+          description: I18n.t('sufia.api.unprocessable_entity.default'),
           errors: {}
         },
         internal_error: {
           code: 500,
           message: "Internal Server Error",
-          description: I18n.t('curation_concerns.api.internal_error.default')
+          description: I18n.t('sufia.api.internal_error.default')
         }
       }
     end
