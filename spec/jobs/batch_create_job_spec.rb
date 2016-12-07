8c988e88c886d3ead8fb3212ea11bb00dac82702
Justin Coyne <jcoyne@justincoyne.com>
2016-12-07 13:24:51 -0800

    Refactor BatchCreateJob spec
    So that it tests just the job and doesn't call the whole stack (which is
    slow). Also added CreateWorkJob spec to cover that previously untested
    class.
    
    Fixes #17

--- spec/jobs/batch_create_job_spec.rb
+++ spec/jobs/batch_create_job_spec.rb
@@ -1,18 +1,7 @@
 describe BatchCreateJob do
   let(:user) { create(:user) }
   let(:log) { create(:batch_create_operation, user: user) }
-
-  before do
-    allow(CharacterizeJob).to receive(:perform_later)
-    allow(Hyrax.config.callback).to receive(:run)
-    allow(Hyrax.config.callback).to receive(:set?)
-      .with(:after_batch_create_success)
-      .and_return(true)
-    allow(Hyrax.config.callback).to receive(:set?)
-      .with(:after_batch_create_failure)
-      .and_return(true)
-  end
-
+  let(:child_log) { double }
   describe "#perform" do
     let(:file1) { File.open(fixture_path + '/world.png') }
     let(:file2) { File.open(fixture_path + '/image.jp2') }
@@ -22,9 +11,6 @@ describe BatchCreateJob do
     let(:resource_types) { { upload1.id.to_s => 'Article', upload2.id.to_s => 'Image' } }
     let(:metadata) { { keyword: [] } }
     let(:uploaded_files) { [upload1.id.to_s, upload2.id.to_s] }
-    let(:errors) { double(full_messages: "It's broke!") }
-    let(:work) { double(errors: errors) }
-    let(:actor) { double(curation_concern: work) }
 
     subject do
       described_class.perform_later(user,
@@ -35,46 +21,32 @@ describe BatchCreateJob do
                                     log)
     end
 
-    it "updates work metadata" do
-      expect(Hyrax::CurationConcern).to receive(:actor).and_return(actor).twice
-      expect(actor).to receive(:create).with(keyword: [], title: ['File One'], resource_type: ["Article"], uploaded_files: ['1']).and_return(true)
-      expect(actor).to receive(:create).with(keyword: [], title: ['File Two'], resource_type: ["Image"], uploaded_files: ['2']).and_return(true)
-      expect(Hyrax.config.callback).to receive(:run).with(:after_batch_create_success, user)
+    before do
+      allow(Hyrax::Operation).to receive(:create!).with(user: user,
+                                                        operation_type: "Create Work",
+                                                        parent: log).and_return(child_log)
+    end
+
+    it "spawns CreateWorkJobs for each work" do
+      expect(CreateWorkJob).to receive(:perform_later).with(user,
+                                                            "GenericWork",
+                                                            {
+                                                              keyword: [],
+                                                              title: ['File One'],
+                                                              resource_type: ["Article"],
+                                                              uploaded_files: ['1']
+                                                            },
+                                                            child_log).and_return(true)
+      expect(CreateWorkJob).to receive(:perform_later).with(user,
+                                                            "GenericWork",
+                                                            {
+                                                              keyword: [],
+                                                              title: ['File Two'],
+                                                              resource_type: ["Image"],
+                                                              uploaded_files: ['2']
+                                                            },
+                                                            child_log).and_return(true)
       subject
-      expect(log.status).to eq 'pending'
-      expect(log.reload.status).to eq 'success'
-    end
-
-    context "when permissions_attributes are passed" do
-      let(:metadata) do
-        { "permissions_attributes" => [{ "type" => "group", "name" => "public", "access" => "read" }] }
-      end
-      it "sets the groups" do
-        subject
-        work = GenericWork.last
-        expect(work.read_groups).to include "public"
-      end
-    end
-
-    context "when visibility is passed" do
-      let(:metadata) do
-        { "visibility" => 'open' }
-      end
-      it "sets public read access" do
-        subject
-        work = GenericWork.last
-        expect(work.reload.read_groups).to eq ['public']
-      end
-    end
-
-    context "when user does not have permission to edit all of the works" do
-      it "sends the failure message" do
-        expect(Hyrax::CurationConcern).to receive(:actor).and_return(actor).twice
-        expect(actor).to receive(:create).and_return(true, false)
-        expect(Hyrax.config.callback).to receive(:run).with(:after_batch_create_failure, user)
-        subject
-        expect(log.reload.status).to eq 'failure'
-      end
     end
   end
 end
