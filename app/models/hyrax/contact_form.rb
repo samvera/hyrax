f1ea1a556d5b783bb82a1e41d397aa360c956826
Michael J. Giarlo <mjgiarlo@stanford.edu>
2016-12-13 15:45:08 -0800

    Add a bunch of missing translations

--- app/models/hyrax/contact_form.rb
+++ app/models/hyrax/contact_form.rb
@@ -1,19 +1,11 @@
 module Hyrax
   class ContactForm
     include ActiveModel::Model
-    ISSUE_TYPES = [
-      ["Depositing content", "Depositing content"],
-      ["Making changes to my content", "Making changes to my content"],
-      ["Browsing and searching", "Browsing and searching"],
-      ["Reporting a problem", "Reporting a problem"],
-      ["General inquiry or request", "General inquiry or request"]
-    ].freeze
-
     attr_accessor :contact_method, :category, :name, :email, :subject, :message
     validates :email, :category, :name, :subject, :message, presence: true
     validates :email, format: /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i, allow_blank: true
 
-    # - can't use this without ActiveRecord::Base validates_inclusion_of :category, in: ISSUE_TYPES
+    # - can't use this without ActiveRecord::Base validates_inclusion_of :category, in: self.class.issue_types_for_locale
 
     # They should not have filled out the `contact_method' field. That's there to prevent spam.
     def spam?
@@ -28,6 +20,16 @@ module Hyrax
         to: Hyrax.config.contact_email,
         from: email
       }
+    end
+
+    def self.issue_types_for_locale
+      [
+        I18n.t('hyrax.contact_form.issue_types.depositing'),
+        I18n.t('hyrax.contact_form.issue_types.changing'),
+        I18n.t('hyrax.contact_form.issue_types.browsing'),
+        I18n.t('hyrax.contact_form.issue_types.reporting'),
+        I18n.t('hyrax.contact_form.issue_types.general')
+      ]
     end
   end
 end
