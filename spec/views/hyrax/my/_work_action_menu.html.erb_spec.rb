f1ba83b19abc01ac44eeaa393b1686862125cd25
Justin Coyne <jcoyne@justincoyne.com>
2016-12-19 15:45:02 -0600

    Don't display the transfer link when they can't use it.
    If the transfers have been disabled, don't show the link to transfer a
    work.  Fixes https://github.com/projecthydra-labs/hyku/issues/558

---
+++ spec/views/hyrax/my/_work_action_menu.html.erb_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+describe 'hyrax/my/_work_action_menu.html.erb' do
+  let(:id) { '123' }
+  let(:document) { SolrDocument.new(id: id, has_model_ssim: 'GenericWork') }
+  let(:user) { build(:user) }
+
+  before do
+    allow(view).to receive(:current_user).and_return(user)
+    allow(view).to receive(:display_trophy_link).and_return("Highlight Work on Profile")
+  end
+
+  context "When the user can transfer works" do
+    before do
+      allow(view).to receive(:can?).with(:transfer, id).and_return(true)
+      render 'hyrax/my/work_action_menu', document: document
+    end
+
+    it "draws the page" do
+      expect(rendered).to have_link "Transfer Ownership of Work"
+      expect(rendered).to have_link 'Edit Work', href: edit_hyrax_generic_work_path(id)
+      expect(rendered).to have_link 'Delete Work', href: hyrax_generic_work_path(id)
+      expect(rendered).to have_content 'Highlight Work on Profile'
+    end
+  end
+
+  context "when the user can't transfer works" do
+    before do
+      allow(view).to receive(:can?).with(:transfer, id).and_return(false)
+      render 'hyrax/my/work_action_menu', document: document
+    end
+
+    it "draws the page" do
+      expect(rendered).not_to have_link "Transfer Ownership of Work"
+      expect(rendered).to have_link 'Edit Work', href: edit_hyrax_generic_work_path(id)
+      expect(rendered).to have_link 'Delete Work', href: hyrax_generic_work_path(id)
+      expect(rendered).to have_content 'Highlight Work on Profile'
+    end
+  end
+end
