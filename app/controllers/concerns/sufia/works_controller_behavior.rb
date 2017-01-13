98262a5b09f15a723021f7bfca0772eb0e5e3d42
Joe Atzberger <ohiocore@gmail.com>
2017-01-12 16:49:18 -0800

    Stop copy/pasting code that is already available via inheritance

--- app/controllers/concerns/sufia/works_controller_behavior.rb
+++ app/controllers/concerns/sufia/works_controller_behavior.rb
@@ -62,13 +62,8 @@ module Sufia
       def after_update_response
         if permissions_changed? && curation_concern.file_sets.present?
           redirect_to sufia.confirm_access_curation_concerns_permission_path(curation_concern)
-        elsif curation_concern.visibility_changed? && curation_concern.file_sets.present?
-          redirect_to main_app.confirm_curation_concerns_permission_path(curation_concern)
         else
-          respond_to do |wants|
-            wants.html { redirect_to [main_app, curation_concern] }
-            wants.json { render :show, status: :ok, location: polymorphic_path([main_app, curation_concern]) }
-          end
+          super
         end
       end
 
