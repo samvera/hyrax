3c8bbef55f54ad2265adb87851ef952217c18c2c
Joe Atzberger <ohiocore@gmail.com>
2017-01-13 13:45:11 -0800

    Update modal partial and spec
    No need to mock a second class now that we actually have one.
    
    Reuse to dom_class identifier previously dropped inline.
    
    Trivial note: apparently Atlas doesn't pluralize to `atlases` for the
    purpose of Rails routing.

--- spec/views/shared/select_work_type_modal.html.erb_spec.rb
+++ spec/views/shared/select_work_type_modal.html.erb_spec.rb
@@ -1,5 +1,3 @@
-require 'spec_helper'
-
 RSpec.describe 'shared/_select_work_type_modal.html.erb', type: :view do
   let(:presenter) { instance_double Sufia::SelectTypeListPresenter }
   let(:row1) do
@@ -12,25 +10,25 @@ RSpec.describe 'shared/_select_work_type_modal.html.erb', type: :view do
   let(:row2) do
     instance_double(Sufia::SelectTypePresenter,
                     icon_class: 'icon',
-                    name: 'Book',
-                    description: 'Book of things',
-                    concern: other_model)
+                    name: 'Atlas',
+                    description: 'Atlas of places',
+                    concern: Atlas)
   end
-  let(:other_model) do
-    double(persisted?: false,
-           model_name: double(singular_route_key: 'foo', param_key: 'foo'))
-  end
+  let(:results) { [GenericWork, Atlas] }
+
   before do
     allow(presenter).to receive(:each).and_yield(row1).and_yield(row2)
-    allow(view).to receive(:new_polymorphic_path).and_return('/foos/new')
     allow(view).to receive(:create_work_presenter).and_return(presenter)
     render
   end
-  let(:results) { [GenericWork, other_model] }
 
-  it "draws the modal" do
-    expect(rendered).to have_selector "#worktypes-to-create.modal"
-    expect(rendered).to have_link "Book", href: '/foos/new'
-    expect(rendered).to have_link "Generic Work"
+  it 'draws the modal' do
+    expect(rendered).to have_selector '#worktypes-to-create.modal'
+    expect(rendered).to have_content 'Generic Work'
+    expect(rendered).to have_content 'Atlas'
+    expect(rendered).to have_link 'Create Work', class: ['new-generic-work', 'select-work-single'], href: '/concern/generic_works/new'
+    expect(rendered).to have_link 'Batch Create', class: ['new-generic-work', 'select-work-batch'], href: '/batch_uploads/new?payload_concern=GenericWork'
+    expect(rendered).to have_link 'Create Work', class: ['new-atlas', 'select-work-single'], href: '/concern/atlas/new'
+    expect(rendered).to have_link 'Batch Create', class: ['new-atlas', 'select-work-batch'], href: '/batch_uploads/new?payload_concern=Atlas'
   end
 end
