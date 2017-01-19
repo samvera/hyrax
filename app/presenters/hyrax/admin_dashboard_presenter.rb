aa344c45549d84bd8c13a3fc6d23a4b6c66a7a54
Justin Coyne <jcoyne@justincoyne.com>
2017-01-19 14:33:08 -0600

    Only count registered (not guest) users. Fixes #243

--- app/presenters/hyrax/admin_dashboard_presenter.rb
+++ app/presenters/hyrax/admin_dashboard_presenter.rb
@@ -2,7 +2,7 @@ module Hyrax
   class AdminDashboardPresenter
     # @return [Fixnum] the number of currently registered users
     def user_count
-      ::User.count
+      ::User.where(guest: false).count
     end
   end
 end
