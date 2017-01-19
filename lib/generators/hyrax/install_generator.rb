78813ee33bdcbbe5d6244133024b716bb26e4fd4
Justin Coyne <jcoyne@justincoyne.com>
2017-01-19 14:06:25 -0600

    Generate a workaround for tinymce assets
    We're already doing this fix in hyku

--- lib/generators/hyrax/install_generator.rb
+++ lib/generators/hyrax/install_generator.rb
@@ -25,6 +25,7 @@ module Hyrax
   15. Installs Blacklight gallery (and removes it's scss)
   16. Runs the jquery-datatables generator
   17. Initializes the active-fedora_noid database-backed minter
+  18. Adds configuration to config/application.rb to fix tinymce asset compiling
          """
 
     def run_required_generators
@@ -150,6 +151,16 @@ module Hyrax
 
     def af_noid_database_minter_initialize
       generate 'active_fedora:noid:install'
+    end
+
+    def inject_tinymce_fix
+      file_path = "config/application.rb"
+      insert_into_file file_path, after: /Rails::Application\s*\n/ do
+        "\n      # The compile method (default in tinymce-rails 4.5.2) doesn't work when also" \
+        "\n      # using tinymce-rails-imageupload, so revert to the :copy method" \
+        "\n      # https://github.com/spohlenz/tinymce-rails/issues/183" \
+        "\n      config.tinymce.install = :copy\n"
+      end
     end
   end
 end
