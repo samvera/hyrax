03ef9f08c0d23a573b07504af56d36e6a8509d4a
Michael J. Giarlo <mjgiarlo@stanford.edu>
2017-01-30 09:20:59 -0800

    Mark flash notices as safe using view context
    Fixes projecthydra-labs/hyku#694. Follows #315, which fixed one part of the problem but not all of it.
    
    Using i18n _html-suffixed keys does not automatically mark strings as html_safe in the controller context, so use the view context to achieve the same effect. HT to @jcoyne.

--- app/controllers/concerns/hyrax/batch_uploads_controller_behavior.rb
+++ app/controllers/concerns/hyrax/batch_uploads_controller_behavior.rb
@@ -21,7 +21,8 @@ module Hyrax
       raise CanCan::AccessDenied, "Cannot create an object of class '#{unsafe_pc}'" unless safe_pc
       # authorize! :create, safe_pc
       create_update_job(safe_pc)
-      flash[:notice] = t('hyrax.works.new.after_create_html', application_name: view_context.application_name)
+      # Calling `#t` in a controller context does not mark _html keys as html_safe
+      flash[:notice] = view_context.t('hyrax.works.new.after_create_html', application_name: view_context.application_name)
       redirect_after_update
     end
 
