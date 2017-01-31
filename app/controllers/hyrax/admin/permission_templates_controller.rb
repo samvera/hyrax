65b4ccfbcf1a32ca6a1069143e5cc6fabce559c2
Justin Coyne <jcoyne@justincoyne.com>
2017-01-31 10:02:58 -0600

    Refactor Hyrax::Admin::PermissionTemplatesController

--- app/controllers/hyrax/admin/permission_templates_controller.rb
+++ app/controllers/hyrax/admin/permission_templates_controller.rb
@@ -6,13 +6,17 @@ module Hyrax
                                   class: 'Hyrax::PermissionTemplate'
 
       def update
-        Forms::PermissionTemplateForm.new(@permission_template).update(update_params)
+        form.update(update_params)
         # Ensure we redirect to currently active tab with the appropriate notice
-        current_tab = current_tab_selector
-        redirect_to hyrax.edit_admin_admin_set_path(params[:admin_set_id], anchor: current_tab), notice: I18n.t(current_tab, scope: 'hyrax.admin.admin_sets.form.permission_update_notices')
+        redirect_to edit_admin_admin_set_path(params[:admin_set_id], anchor: current_tab),
+                    notice: translate(current_tab, scope: 'hyrax.admin.admin_sets.form.permission_update_notices')
       end
 
       private
+
+        def form
+          Forms::PermissionTemplateForm.new(@permission_template)
+        end
 
         def update_params
           params.require(:permission_template)
@@ -20,10 +24,16 @@ module Hyrax
                         access_grants_attributes: [:access, :agent_id, :agent_type, :id])
         end
 
-        def current_tab_selector
-          return 'participants' if params[:permission_template][:access_grants_attributes].present?
-          return 'workflow' if params[:permission_template][:workflow_name].present?
-          'visibility'
+        # @return [String] the name of the current UI tab to show
+        def current_tab
+          pt = params[:permission_template]
+          @current_tab ||= if pt[:access_grants_attributes].present?
+                             'participants'
+                           elsif pt[:workflow_name].present?
+                             'workflow'
+                           else
+                             'visibility'
+                           end
         end
     end
   end
