5c9fb71504cc4f8e8376da9c3fc6c202e9fdd11a
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2017-01-31 16:21:15 -0500

    Enforcing AdminSet has_one PermissionTemplate
    This cleans up a bit of the extra scenarios, embracing the conceptual
    idea that each AdminSet has one and only one PermissionTemplate and
    that each PermissionTemplate must belong to one AdminSet.
    
    Related to #256

--- app/services/hyrax/admin_set_service.rb
+++ app/services/hyrax/admin_set_service.rb
@@ -56,18 +56,12 @@ module Hyrax
       # later utilized by Javascript to limit new Work options based on AdminSet selected
       def data_attributes(admin_set)
         attrs = {}
-        # Get permission template associated with this AdminSet (if any)
-        # TODO: Should this be a `find_by!` as per https://github.com/projecthydra-labs/hyrax/issues/256
-        permission_template = PermissionTemplate.find_by(admin_set_id: admin_set.id)
 
-        # Only add data attributes if permission template exists
-        if permission_template
-          # Save all PermissionTemplate release & visibility data attributes (if not blank or false)
-          attrs['data-release-date'] = permission_template.release_date unless permission_template.release_date.blank?
-          attrs['data-release-before-date'] = true if permission_template.release_before_date?
-          attrs['data-visibility'] = permission_template.visibility unless permission_template.visibility.blank?
-        end
-
+        permission_template = PermissionTemplate.find_by!(admin_set_id: admin_set.id)
+        # Leverage all PermissionTemplate release & visibility data attributes (if not blank or false)
+        attrs['data-release-date'] = permission_template.release_date unless permission_template.release_date.blank?
+        attrs['data-release-before-date'] = true if permission_template.release_before_date?
+        attrs['data-visibility'] = permission_template.visibility unless permission_template.visibility.blank?
         attrs
       end
   end
