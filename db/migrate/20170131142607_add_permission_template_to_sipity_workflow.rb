a81df61c6f4b411733d646ced2736c7f6211d823
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2017-02-20 11:04:25 -0500

    Ensuring permission template admin_set_id unique
    * Each AdminSet has one and only one PermissionTemplate
    * Each PermissionTemplate belongs to one and only one AdminSet

--- db/migrate/20170131142607_add_permission_template_to_sipity_workflow.rb
+++ db/migrate/20170131142607_add_permission_template_to_sipity_workflow.rb
@@ -3,5 +3,7 @@ class AddPermissionTemplateToSipityWorkflow < ActiveRecord::Migration
     add_column :sipity_workflows, :permission_template_id, :integer, index: true
     remove_index :sipity_workflows, :name
     add_index :sipity_workflows, [:permission_template_id, :name], name: :index_sipity_workflows_on_permission_template_and_name, unique: true
+    remove_index :permission_templates, :admin_set_id
+    add_index :permission_templates, :admin_set_id, unique: true
   end
 end
