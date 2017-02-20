bad51c3de2f40564038e8f9695dbf36dfc1f8e53
Glen Horton <hortongn@ucmail.uc.edu>
2017-02-20 20:47:29 +0000

    Fix select all/none for My Works

--- spec/helpers/batch_edits_helper_spec.rb
+++ spec/helpers/batch_edits_helper_spec.rb
@@ -1,5 +1,6 @@
 describe BatchEditsHelper, type: :helper do
   describe "#render_check_all" do
+    subject { helper.render_check_all }
     before do
       view.lookup_context.prefixes = ['my']
     end
@@ -7,28 +8,34 @@ describe BatchEditsHelper, type: :helper do
     context "with my works" do
       it "shows the check all dropdown" do
         allow(controller).to receive(:params).and_return(controller: "my/works")
-        expect(helper.render_check_all).to have_css("span.glyphicon-cog")
+        expect(subject).to have_css("span.caret")
+        expect(subject).to have_content t("sufia.dashboard.my.action.select_all")
+        expect(subject).to have_content t("sufia.dashboard.my.action.select_none")
       end
     end
 
     context "with my shares" do
       it "shows the check all dropdown" do
         allow(controller).to receive(:params).and_return(controller: "my/shares")
-        expect(helper.render_check_all).to have_css("span.glyphicon-cog")
+        expect(subject).to have_css("span.caret")
+        expect(subject).to have_content t("sufia.dashboard.my.action.select_all")
+        expect(subject).to have_content t("sufia.dashboard.my.action.select_none")
       end
     end
 
     context "with my highlights" do
       it "shows the check all dropdown" do
         allow(controller).to receive(:params).and_return(controller: "my/shares")
-        expect(helper.render_check_all).to have_css("span.glyphicon-cog")
+        expect(subject).to have_css("span.caret")
+        expect(subject).to have_content t("sufia.dashboard.my.action.select_all")
+        expect(subject).to have_content t("sufia.dashboard.my.action.select_none")
       end
     end
 
     context "with my collections" do
       it "does not show the check all dropdown" do
         allow(controller).to receive(:params).and_return(controller: "my/collections")
-        expect(helper.render_check_all).to be_nil
+        expect(subject).to be_nil
       end
     end
 
@@ -36,7 +43,7 @@ describe BatchEditsHelper, type: :helper do
       it "does not show the check all dropdown" do
         allow(helper).to receive(:params).and_return(controller: "foo")
         assign(:disable_select_all, true)
-        expect(helper.render_check_all).to have_css("input[disabled=disabled]")
+        expect(subject).to have_css("input[disabled=disabled]")
       end
     end
   end
