07b416846fc15334d8ccc25d11ba8746913e570e
Justin Coyne <jcoyne@justincoyne.com>
2017-02-21 10:07:26 -0600

    My works page should show all my deposits
    Even those I lack edit access to.  Some workflows may remove a
    depositors ability to edit their submissions once they are deposited. We
    still want these works to be displayed on the "My Works" page.
    
    Fixes #448

--- app/search_builders/hyrax/my_works_search_builder.rb
+++ app/search_builders/hyrax/my_works_search_builder.rb
@@ -1,9 +1,13 @@
 # Added to allow for the My controller to show only things I have edit access to
 class Hyrax::MyWorksSearchBuilder < Hyrax::SearchBuilder
-  include Hyrax::MySearchBuilderBehavior
   include Hyrax::FilterByType
 
   self.default_processor_chain += [:show_only_resources_deposited_by_current_user]
+
+  # We remove the access controls filter, because some of the works a user has
+  # deposited may have gone through a workflow which has removed their ability
+  # to edit the work.
+  self.default_processor_chain -= [:add_access_controls_to_solr_params]
 
   def only_works?
     true
