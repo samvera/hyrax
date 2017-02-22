70469bee1fef6a9eb6aac6a24c83f0d4e51c3f70
Justin Coyne <jcoyne@justincoyne.com>
2017-02-22 09:46:54 -0600

    Keep the menu section open when a subitem is selected
    Fixes #462

---
+++ app/presenters/hyrax/menu_presenter.rb
@@ -0,0 +1,37 @@
+module Hyrax
+  # view-model for the admin menu
+  class MenuPresenter
+    def initialize(view_context)
+      @view_context = view_context
+    end
+
+    attr_reader :view_context
+
+    delegate :controller, :controller_name, :action_name, :content_tag,
+             :current_page?, :link_to, to: :view_context
+
+    # Returns true if the current controller happens to be one of the controllers that deals
+    # with workflow.  This is used to keep the parent section on the sidebar open.
+    def workflows_section?
+      controller.instance_of? Hyrax::Admin::WorkflowRolesController
+    end
+
+    def nav_link(href, **link_options)
+      list_options = current_page?(href) ? { class: 'active' } : {}
+      content_tag(:li, list_options) do
+        link_to(href, link_options) do
+          yield
+        end
+      end
+    end
+
+    # Draw a collaspable menu section. The passed block should contain <li> items.
+    def collapsable_section(text, id:, icon_class:, open:, &block)
+      CollapsableSectionPresenter.new(view_context: view_context,
+                                      text: text,
+                                      id: id,
+                                      icon_class: icon_class,
+                                      open: open).render(&block)
+    end
+  end
+end
