8d854a2ba547c25b3d3bf700523b6b80dfd9e0ea
Justin Coyne <jcoyne@justincoyne.com>
2017-02-23 11:19:38 -0600

    Update flipflop. We can stop overriding now

--- app/controllers/hyrax/admin/features_controller.rb
+++ app/controllers/hyrax/admin/features_controller.rb
@@ -7,13 +7,11 @@ module Hyrax
         authorize! :manage, Hyrax::Feature
       end
 
-      # overriding so we can have a layout https://github.com/voormedia/flipflop/issues/18
       def index
         add_breadcrumb t(:'hyrax.controls.home'), root_path
         add_breadcrumb t(:'hyrax.toolbar.admin.menu'), hyrax.admin_path
         add_breadcrumb t(:'hyrax.admin.sidebar.settings'), hyrax.admin_features_path
-        @feature_set = Flipflop::FeaturesController::FeaturesPresenter.new(Flipflop::FeatureSet.current)
-        render
+        super
       end
     end
   end
