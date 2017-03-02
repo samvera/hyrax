b7c4139ba64537665bff45bfac4f85a25ae70208
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2017-03-02 13:16:03 -0500

    Adding specs for Hyrax::Noid
    As part of removing the deprecation notice, I wanted to test the
    behavior of the Hyrax::Noid module.

--- app/services/hyrax/noid.rb
+++ app/services/hyrax/noid.rb
@@ -7,9 +7,10 @@ module Hyrax
     ## This overrides the default behavior, which is to ask Fedora for an id
     # @see ActiveFedora::Persistence.assign_id
     def assign_id
-      service.mint if Hyrax.config.enable_noids
+      service.mint if Hyrax.config.enable_noids?
     end
 
+    # @todo Do we need this here? I'm a bit surprised to see it here [JNF]
     def to_param
       id
     end
