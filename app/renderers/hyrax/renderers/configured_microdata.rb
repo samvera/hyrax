3fef081fff3f40c8acfa1c646142281f7573e3bb
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2017-03-02 13:25:43 -0500

    Consolidating duplicate knowledge for microdata
    Instead of several different paths for translation, opt for a single
    path. Also renamed the 'key' to 'context' as it provides a bit of
    disambiguation (we lookup translations by a full key, and by using key
    it implies that might be the only part of translation we use).

--- app/renderers/hyrax/renderers/configured_microdata.rb
+++ app/renderers/hyrax/renderers/configured_microdata.rb
@@ -5,14 +5,12 @@ module Hyrax
 
       def microdata?(field)
         return false unless Hyrax.config.display_microdata?
-        key = "#{PREFIX}.#{field}.property"
-        t(key, default: false)
+        translate_microdata(field: field, field_context: 'property', default: false)
       end
 
       def microdata_object?(field)
         return false unless Hyrax.config.display_microdata?
-        key = "#{PREFIX}.#{field}.type"
-        t(key, default: false)
+        translate_microdata(field: field, field_context: 'type', default: false)
       end
 
       def microdata_object_attributes(field)
@@ -24,21 +22,27 @@ module Hyrax
       end
 
       def microdata_property(field)
-        t("#{PREFIX}.#{field}.property")
+        translate_microdata(field: field, field_context: 'property')
       end
 
       def microdata_type(field)
-        t("#{PREFIX}.#{field}.type")
+        translate_microdata(field: field, field_context: 'type')
       end
 
       def microdata_value_attributes(field)
         if microdata?(field)
-          key = microdata_object?(field) ? :value : :property
-          { itemprop: t("#{PREFIX}.#{field}.#{key}") }
+          field_context = microdata_object?(field) ? :value : :property
+          { itemprop: translate_microdata(field: field, field_context: field_context) }
         else
           {}
         end
       end
+
+      private
+
+        def translate_microdata(field:, field_context:, default: true)
+          t("#{PREFIX}.#{field}.#{field_context}", default: default)
+        end
     end
   end
 end
