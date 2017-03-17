68f26b4b0e089d98cbb491d05f325a9890a2e70b
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2017-03-17 16:00:12 -0400

    Clarifying Hyrax::Operation
    Adding documentation and additional specs to better understand these
    classes.

--- app/models/hyrax/operation.rb
+++ app/models/hyrax/operation.rb
@@ -1,4 +1,5 @@
 module Hyrax
+  # The database storage of inter-related jobs and their states.
   class Operation < ActiveRecord::Base
     PENDING = 'pending'.freeze
     PERFORMING = 'performing'.freeze
@@ -11,15 +12,29 @@ module Hyrax
     belongs_to :user, class_name: '::User'
 
     # If this is a batch job (has children), check to see if all the children are complete
+    # @see #fail! when none of the children are in a PENDING nor PERFORMING state but at least one is in a FAILURE state
+    # @see #success! none of the children are in a PENDING, PERFORMING, nor FAILURE state
     def rollup_status
       with_lock do
-        stats = children.pluck(:status)
+        # We don't need all of the status of the children, just need to see
+        # if there is at least one PENDING, PERFORMING, or FAILURE.
+        # With this change, it doesn't matter if we have 10_000 children or 1, we will only ever get
+        # back an that is no longer than the total number of possible status values. Is it necessary?
+        # No, but there is no need to instantiate an array of all of those values.
+        stats = children.select(:status).distinct.pluck(:status)
         return if stats.include?(PENDING) || stats.include?(PERFORMING)
         return fail! if stats.include?(FAILURE)
         success!
       end
     end
 
+    # Mark this operation as a SUCCESS. If this is a child operation, roll up to
+    # the parent any failures.
+    #
+    # @see Hyrax::Operation::SUCCESS
+    # @see #rollup_status
+    # @note This will run any registered :success callbacks
+    # @todo Where are these callbacks defined? Document this
     def success!
       run_callbacks :success do
         update(status: SUCCESS)
@@ -27,6 +42,14 @@ module Hyrax
       end
     end
 
+    # Mark this operation as a FAILURE. If this is a child operation, roll up to
+    # the parent any failures.
+    #
+    # @param [String, nil] message - record any failure message
+    # @see Hyrax::Operation::FAILURE
+    # @see #rollup_status
+    # @note This will run any registered :success callbacks
+    # @todo Where are these callbacks defined? Document this
     def fail!(message = nil)
       run_callbacks :failure do
         update(status: FAILURE, message: message)
@@ -34,10 +57,15 @@ module Hyrax
       end
     end
 
+    # Sets the operation status to PERFORMING
+    # @see Hyrax::Operation::PERFORMING
     def performing!
       update(status: PERFORMING)
     end
 
+    # Sets the operation status to PENDING
+    # @param [#class, #job_id] job - The job associated with this operation
+    # @see Hyrax::Operation::PENDING
     def pending_job(job)
       update(job_class: job.class.to_s, job_id: job.job_id, status: Hyrax::Operation::PENDING)
     end
