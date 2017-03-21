216033eba00bdf9b23fd5958cc814da169fc9bf3
Jamie Little <jamie@jamielittle.org>
2017-03-21 15:07:24 -0400

    Remove hardcoded autcomplete fields
    remove autocomplete options from authority select
    
    Refactor to remove $ from ac class

--- app/assets/javascripts/hyrax/app.js
+++ app/assets/javascripts/hyrax/app.js
@@ -43,19 +43,21 @@ Hyrax = {
     },
 
     autocomplete: function () {
-        var Autocomplete = require('hyrax/autocomplete');
-        var autocomplete = new Autocomplete({
-	    "autocompleteFields":
-	    ["subject","language","creator","based_near","work"]
-	});
+        var Autocomplete = require('hyrax/autocomplete')
+        var autocomplete = new Autocomplete()
+
+        $('[data-autocomplete]').each((function() {
+          var data = $(this).data()
+          autocomplete.setup({'element' : $(this), 'data': data})
+        }))
+
         $('.multi_value.form-group').manage_fields({
           add: function(e, element) {
-              autocomplete.fieldAdded(element);
+              autocomplete.setup({'element':$(element), 'data':$(element).data()})
 	      // Don't mark an added element as readonly even if previous element was
-	      $(element).attr('readonly',false);
+	      $(element).attr('readonly', false)
           }
-        });
-        autocomplete.setup();
+        })
     },
 
     editor: function () {
