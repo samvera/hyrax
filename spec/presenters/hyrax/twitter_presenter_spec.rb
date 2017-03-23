1a37b3ccdddac60a312f51b173856197032dc42b
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2017-03-23 13:23:44 -0400

    Extracting common method #tweeter
    I've been leveraging the flay gem to find duplication in code. This was
    an easy and quick fix. It consolidates duplicate logic into a module.
    
    The added benefit is that untested code in one of the presenters is now
    tested.

---
+++ spec/presenters/hyrax/twitter_presenter_spec.rb
@@ -0,0 +1,23 @@
+module Hyrax
+  RSpec.describe TwitterPresenter do
+    describe '.twitter_handle_for' do
+      let(:user_key) { 'user_key' }
+      subject { described_class.twitter_handle_for(user_key: user_key) }
+      context "with a found user that has a twitter handle" do
+        before { allow(::User).to receive(:find_by_user_key).with(user_key).and_return(user) }
+        let(:user) { instance_double(::User, twitter_handle: 'test', user_key: user_key) }
+        it { is_expected.to eq '@test' }
+      end
+
+      context "with a found user that doesn't have a twitter handle" do
+        before { allow(::User).to receive(:find_by_user_key).with(user_key).and_return(user) }
+        let(:user) { instance_double(::User, twitter_handle: '', user_key: user_key) }
+        it { is_expected.to eq '@HydraSphere' }
+      end
+
+      context "with a user that can't be found" do
+        it { is_expected.to eq '@HydraSphere' }
+      end
+    end
+  end
+end
