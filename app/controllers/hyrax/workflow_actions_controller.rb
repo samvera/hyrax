90012b333e8b00281a3ab645d47edb52c890a9fe
Josh Gum <josh.gum@oregonstate.edu>
2017-03-24 13:29:11 -0700

    add json response to workflow_actions

--- app/controllers/hyrax/workflow_actions_controller.rb
+++ app/controllers/hyrax/workflow_actions_controller.rb
@@ -3,16 +3,19 @@ module Hyrax
     before_action :authenticate_user!
 
     def update
-      work = ActiveFedora::Base.find(params[:id])
+      @curation_concern = ActiveFedora::Base.find(params[:id])
       workflow_action_form = Hyrax::Forms::WorkflowActionForm.new(
         current_ability: current_ability,
-        work: work,
+        work: @curation_concern,
         attributes: workflow_action_params
       )
       if workflow_action_form.save
-        redirect_to [main_app, work], notice: "The #{work.human_readable_type} has been updated."
+        after_update_response
       else
-        render 'hyrax/base/unauthorized', status: :unauthorized
+        respond_to do |wants|
+          wants.html { render 'hyrax/base/unauthorized', status: :unauthorized }
+          wants.json { render_json_response(response_type: :unprocessable_entity, options: { errors: @curation_concern.errors }) }
+        end
       end
     end
 
@@ -20,6 +23,13 @@ module Hyrax
 
       def workflow_action_params
         params.require(:workflow_action).permit(:name, :comment)
+      end
+
+      def after_update_response
+        respond_to do |wants|
+          wants.html { redirect_to [main_app, @curation_concern], notice: "The #{@curation_concern.human_readable_type} has been updated." }
+          wants.json { render 'hyrax/base/show', status: :ok, location: polymorphic_path([main_app, @curation_concern]) }
+        end
       end
   end
 end
