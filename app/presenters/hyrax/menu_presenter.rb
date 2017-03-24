b46120870a95458ed0fa8edfda6820d691eceff5
Justin Coyne <jcoyne@justincoyne.com>
2017-03-24 15:15:15 -0500

    Use route helpers instead of hashes.
    Fixes #653

--- app/presenters/hyrax/menu_presenter.rb
+++ app/presenters/hyrax/menu_presenter.rb
@@ -8,7 +8,7 @@ module Hyrax
     attr_reader :view_context
 
     delegate :controller, :controller_name, :action_name, :content_tag,
-             :current_page_except_defaults?, :link_to, to: :view_context
+             :current_page?, :link_to, to: :view_context
 
     # Returns true if the current controller happens to be one of the controllers that deals
     # with workflow.  This is used to keep the parent section on the sidebar open.
@@ -21,7 +21,7 @@ module Hyrax
     # @param also_active_for [Hash, String] a hash or string with alternative paths that should be 'active'
     def nav_link(options, also_active_for: nil, **link_html_options)
       active_urls = [options, also_active_for].compact
-      list_options = active_urls.any? { |url| current_page_except_defaults?(url) } ? { class: 'active' } : {}
+      list_options = active_urls.any? { |url| current_page?(url) } ? { class: 'active' } : {}
       content_tag(:li, list_options) do
         link_to(options, link_html_options) do
           yield
