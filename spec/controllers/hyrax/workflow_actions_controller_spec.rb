8d0173268ca6b883fe2d1ce0d347163b9ce22007
Josh Gum <josh.gum@oregonstate.edu>
2017-03-24 13:29:11 -0700

    add json response to workflow_actions

--- spec/controllers/hyrax/workflow_actions_controller_spec.rb
+++ spec/controllers/hyrax/workflow_actions_controller_spec.rb
@@ -33,5 +33,23 @@ RSpec.describe Hyrax::WorkflowActionsController, type: :controller do
       put :update, params: { id: generic_work.to_param, workflow_action: { name: 'advance', comment: '' } }
       expect(response).to redirect_to(main_app.hyrax_generic_work_path(generic_work, locale: 'en'))
     end
+
+    context 'when responding to json' do
+      it 'will render :ok when the form is successfully saved' do
+        expect(form).to receive(:save).and_return(true)
+        sign_in(user)
+
+        put :update, params: { id: generic_work.to_param, workflow_action: { name: 'advance', comment: '' } }, format: :json
+        expect(response.status).to eq 200
+      end
+
+      it 'will render :unprocessable_entity when the form fails to save' do
+        expect(form).to receive(:save).and_return(false)
+        sign_in(user)
+
+        put :update, params: { id: generic_work.to_param, workflow_action: { name: 'advance', comment: '' } }, format: :json
+        expect(response.status).to eq 422
+      end
+    end
   end
 end
