08ec88ecc57a9be8eb7a0d7f186cc5ba01fedb3f
Justin Coyne <jcoyne@justincoyne.com>
2017-04-06 10:50:54 -0500

    Don't delegate representative_id twice

--- app/presenters/hyrax/work_show_presenter.rb
+++ app/presenters/hyrax/work_show_presenter.rb
@@ -30,7 +30,7 @@ module Hyrax
     end
 
     # CurationConcern methods
-    delegate :stringify_keys, :human_readable_type, :collection?, :representative_id, :to_s,
+    delegate :stringify_keys, :human_readable_type, :collection?, :to_s,
              to: :solr_document
 
     # Metadata Methods
