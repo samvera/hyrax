41c01034bf6b4c6447766a4d495a61b0271a893a
Justin Coyne <jcoyne@justincoyne.com>
2017-04-10 10:14:56 -0500

    Rename AddAsMemberOfCollectionsActor to CollectionsMembershipActor
    This new name is more appropriate because the actor also removes access
    from collections.

--- app/actors/hyrax/actors/collections_membership_actor.rb
+++ app/actors/hyrax/actors/collections_membership_actor.rb
@@ -1,20 +1,21 @@
 module Hyrax
   module Actors
-    class AddAsMemberOfCollectionsActor < AbstractActor
+    # Adds membership to and removes membership from collections
+    class CollectionsMembershipActor < AbstractActor
       def create(attributes)
         collection_ids = attributes.delete(:member_of_collection_ids)
-        add_to_collections(collection_ids) && next_actor.create(attributes)
+        assign_collections(collection_ids) && next_actor.create(attributes)
       end
 
       def update(attributes)
         collection_ids = attributes.delete(:member_of_collection_ids)
-        add_to_collections(collection_ids) && next_actor.update(attributes)
+        assign_collections(collection_ids) && next_actor.update(attributes)
       end
 
       private
 
-        # Maps from collection ids to collection objects
-        def add_to_collections(collection_ids)
+        # Maps from collection ids to collection objects.
+        def assign_collections(collection_ids)
           return true unless collection_ids
           # grab/save collections this user has no edit access to
           other_collections = curation_concern.member_of_collections.select { |coll| ability.cannot?(:edit, coll) }
