c643e71aa2d09b62392b671345d7ba34cb348963
Justin Coyne <jcoyne@justincoyne.com>
2017-04-14 11:58:32 -0500

    Don't update content blocks if no data has been passed in.
    Also document the appearance form

--- spec/forms/hyrax/forms/admin/appearance_spec.rb
+++ spec/forms/hyrax/forms/admin/appearance_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 RSpec.describe Hyrax::Forms::Admin::Appearance do
-  let(:form) { described_class.new({}) }
+  let(:form) { described_class.new(arguments) }
 
   describe "update!" do
     let(:block) { instance_double(ContentBlock) }
@@ -9,10 +9,28 @@ RSpec.describe Hyrax::Forms::Admin::Appearance do
     before do
       allow(ContentBlock).to receive(:find_or_create_by).and_return(block)
     end
+    context "when arguments are passed in" do
+      let(:arguments) do
+        { header_background_color: '',
+          header_text_color: '',
+          link_color: '',
+          footer_link_color: '',
+          primary_button_background_color: '' }
+      end
 
-    it "calls update block 5 times" do
-      expect(block).to receive(:update!).exactly(5).times
-      form.update!
+      it "calls update block 5 times" do
+        expect(block).to receive(:update!).exactly(5).times
+        form.update!
+      end
+    end
+
+    context "when no arguments are passed in" do
+      let(:arguments) { {} }
+
+      it "doesn't call update block" do
+        expect(block).not_to receive(:update!)
+        form.update!
+      end
     end
   end
 end
