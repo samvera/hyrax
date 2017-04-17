7d0b0ec4174ea69eb1439f5b265d2394c5404fa2
Tahir Poduska <tap87@cornell.edu>
2017-04-17 16:45:35 -0400

    Changed after_destroy to before_destroy for cleaning derivatives Added a delete work feature spec Removed hardcoded path to my works, use hyrax.my_works_path

---
+++ spec/features/delete_work_spec.rb
@@ -0,0 +1,23 @@
+RSpec.feature 'Deleting a work', type: :feature do
+  let(:user) { create(:user) }
+  let(:work) { build(:work, user: user) }
+  let(:file_set) { create(:file_set, user: user, title: ['ABC123xyz']) }
+  let(:file) { File.open(fixture_path + '/world.png') }
+
+  before do
+    sign_in user
+    Hydra::Works::AddFileToFileSet.call(file_set, file, :original_file)
+    work.ordered_members << file_set
+    work.read_groups = []
+    work.save!
+  end
+
+  context 'After deleting a work from the work show page' do
+    it 'redirects to my dashboard' do
+      visit hyrax_generic_work_path(work)
+      click_on('Delete', match: :first)
+      expect(page).to have_current_path(hyrax.my_works_path, only_path: true)
+      expect(page).to have_content 'Deleted Test title'
+    end
+  end
+end
