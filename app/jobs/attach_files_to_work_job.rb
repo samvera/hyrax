d88af0e275a87a9db9109d900ca3b1cdfa2ab233
Joe Atzberger <ohiocore@gmail.com>
2017-04-19 17:19:30 -0700

    Move steps unaffected by loop outside loop
    Why repeatedly fetch the same user?

--- app/jobs/attach_files_to_work_job.rb
+++ app/jobs/attach_files_to_work_job.rb
@@ -5,15 +5,15 @@ class AttachFilesToWorkJob < ActiveJob::Base
   # @param [ActiveFedora::Base] work - the work object
   # @param [Array<UploadedFile>] uploaded_files - an array of files to attach
   def perform(work, uploaded_files)
+    user = User.find_by_user_key(work.depositor)
+    work_permissions = work.permissions.map(&:to_hash)
     uploaded_files.each do |uploaded_file|
       file_set = FileSet.new
-      user = User.find_by_user_key(work.depositor)
       actor = Hyrax::Actors::FileSetActor.new(file_set, user)
       actor.create_metadata(visibility: work.visibility)
       attach_content(actor, uploaded_file.file)
       actor.attach_file_to_work(work)
-      actor.file_set.permissions_attributes = work.permissions.map(&:to_hash)
-
+      actor.file_set.permissions_attributes = work_permissions
       uploaded_file.update(file_set_uri: file_set.uri)
     end
   end
