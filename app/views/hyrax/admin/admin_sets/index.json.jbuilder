d182b014acfa8a9afb31b4def840674567a1108c
Josh Gum <josh.gum@oregonstate.edu>
2017-04-21 21:41:05 -0700

    adds json serialization for admin sets listing

---
+++ app/views/hyrax/admin/admin_sets/index.json.jbuilder
@@ -0,0 +1,5 @@
+json.admin_sets @admin_sets do |admin_set|
+  json.id admin_set['id']
+  json.title admin_set['title_tesim']
+  json.description admin_set['description_tesim']
+end
---
+++ spec/views/hyrax/admin/admin_sets/index.json.erb_spec.rb
@@ -0,0 +1,29 @@
+RSpec.describe "hyrax/admin/admin_sets/index.json.jbuilder" do
+  context "when no admin sets exists" do
+    it "renders no admin sets" do
+      render
+      json = JSON.parse(rendered)
+      expect(json['admin_sets']).to eq []
+    end
+  end
+
+  context "when an admin set exists" do
+    let(:admin_set) { build(:admin_set, id: '123', title: ['Example Admin Set'], creator: ['jdoe@example.com'], description: ['Wat']) }
+    let(:solr_doc) { SolrDocument.new(admin_set.to_solr) }
+    let(:admin_sets) { [solr_doc] }
+    let(:presenter_class) { Hyrax::AdminSetPresenter }
+    let(:presenter) { instance_double(presenter_class, total_items: 99) }
+    let(:ability) { instance_double("Ability") }
+    before do
+      allow(controller).to receive(:current_ability).and_return(ability)
+      allow(controller).to receive(:presenter_class).and_return(presenter_class)
+      allow(presenter_class).to receive(:new).and_return(presenter)
+      assign(:admin_sets, admin_sets)
+    end
+    it "lists admin set" do
+      render
+      json = JSON.parse(rendered)
+      expect(json['admin_sets']).to eq [{ 'id' => '123', 'title' => ['Example Admin Set'], 'description' => ['Wat'] }]
+    end
+  end
+end
