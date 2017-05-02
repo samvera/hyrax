17df0c3c097fc780f2cc1a7e0f768605c540c5bb
Justin Coyne <jcoyne@justincoyne.com>
2017-05-02 12:45:15 -0500

    Refactor test

--- spec/services/hyrax/persist_directly_contained_output_file_service_spec.rb
+++ spec/services/hyrax/persist_directly_contained_output_file_service_spec.rb
@@ -1,14 +1,18 @@
 require 'spec_helper'
 
 RSpec.describe Hyrax::PersistDirectlyContainedOutputFileService do
-  let(:object)            { FileSet.create! { |fs| fs.apply_depositor_metadata('justin') } }
-  let(:file_path)         { File.join(fixture_path, 'test.tif') }
-  let(:file)              { File.new(file_path) }
-  let(:destination_name)  { 'the_derivative_name' }
+  # PersistDirectlyContainedOutputFileService is used by FullTextExtract.output_file_service
+  let(:object) { FileSet.create! { |fs| fs.apply_depositor_metadata('justin') } }
   let(:stream) { StringIO.new("fake file content") }
+  subject(:call) do
+    described_class.call(stream,
+                         format: 'txt',
+                         url: object.uri,
+                         container: 'extracted_text')
+  end
 
   it "persists the file to the specified destination on the given object" do
-    described_class.call(stream, format: 'txt', url: object.uri, container: 'extracted_text')
+    expect(call).to be true
     expect(object.reload.extracted_text.content).to eq("fake file content")
   end
 end
