9f22bc2453e4be730801efcfc79a390fc146ad1d
Michael J. Giarlo <mjgiarlo@stanford.edu>
2017-05-11 15:14:27 -0700

    Add view spec for rights statement

--- spec/views/hyrax/base/_attribute_rows.html.erb_spec.rb
+++ spec/views/hyrax/base/_attribute_rows.html.erb_spec.rb
@@ -2,8 +2,13 @@ require 'spec_helper'
 
 RSpec.describe 'hyrax/base/_attribute_rows.html.erb', type: :view do
   let(:url) { "http://example.com" }
+  let(:rights_statement_uri) { 'http://rightsstatements.org/vocab/InC/1.0/' }
   let(:ability) { double }
-  let(:work) { stub_model(GenericWork, related_url: [url]) }
+  let(:work) do
+    stub_model(GenericWork,
+               related_url: [url],
+               rights_statement: [rights_statement_uri])
+  end
   let(:solr_document) { SolrDocument.new(work.to_solr) }
   let(:presenter) { Hyrax::WorkShowPresenter.new(solr_document, ability) }
 
@@ -15,5 +20,9 @@ RSpec.describe 'hyrax/base/_attribute_rows.html.erb', type: :view do
   it 'shows external link with icon for related url field' do
     expect(page).to have_selector '.glyphicon-new-window'
     expect(page).to have_link(url)
+  end
+
+  it 'shows rights statement with link to statement URL' do
+    expect(page).to have_link(rights_statement_uri)
   end
 end
