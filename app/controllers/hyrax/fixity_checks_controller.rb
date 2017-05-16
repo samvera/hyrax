efc86084b73da83b468217c86d77e054d8f7bf26
Jonathan Rochkind <jrochkind@chemheritage.org>
2017-05-16 11:57:08 -0400

    FixityChecksController updated for latest architecture,
    insisting on async_jobs false so it can actually get a response,
    naming keys accurately for what they actually are.

--- app/controllers/hyrax/fixity_checks_controller.rb
+++ app/controllers/hyrax/fixity_checks_controller.rb
@@ -2,6 +2,18 @@ module Hyrax
   class FixityChecksController < ApplicationController
     before_action :authenticate_user!
 
+    # request here with param :file_set_id will trigger a fixity check if
+    # needed, and respond with a JSON hash that looks something like:
+    #
+    #     { "file_id" => [
+    #         {
+    #           "checked_uri" => "http://127.0.0.1:8986/rest/test/12/57/9s/28/12579s28n/files/3ff48171-f625-48bb-a73d-b1ba16dde530/fcr:versions/version1",
+    #           "passed" => true,
+    #           "expected_result" => "urn:sha1:03434..."
+    #           "created_at" => "2017-05-16T15:32:50.961Z"
+    #         }
+    #       ]
+    #     }
     def create
       render json: fixity_check_service.fixity_check
     end
@@ -9,8 +21,8 @@ module Hyrax
     protected
 
       def fixity_check_service
-        file_set = ::FileSet.find(params[:file_set_id])
-        FileSetFixityCheckService.new(file_set)
+        @fixity_check_service ||=
+          FileSetFixityCheckService.new(::FileSet.find(params[:file_set_id]), async_jobs: false)
       end
   end
 end
--- spec/controllers/hyrax/fixity_checks_controller_spec.rb
+++ spec/controllers/hyrax/fixity_checks_controller_spec.rb
@@ -18,11 +18,20 @@ RSpec.describe Hyrax::FixityChecksController do
         expect(response).to be_success
         json = JSON.parse(response.body)
         # json is a structure like this:
-        #   { file_id => [{ "version" => "version1", "pass" => 999 },
-        #                 { "version" => "version2", "pass" => 0 },
+        #   { file_id => [{ "checked_uri" => "...4-4d71-83ba-1bc52a5e4300/fcr:versions/version1", "passed" => true },
+        #                 { "checked_uri" => ".../version2", "passed" => false },
         #                 ...] }
-        fixity_results = json.values.flatten.collect { |result| result["pass"] }
-        expect(fixity_results.reduce(true) { |sum, value| sum && value }).to eq 999 # never been audited
+        json.each do |file_id, array_of_checks|
+          array_of_checks.each do |check_hash|
+            ["file_set_id", "file_id", "checked_uri", "passed",
+             "expected_result", "created_at"].each do |internal_key|
+              expect(check_hash).to have_key(internal_key)
+            end
+            expect(check_hash["passed"]).to be_in([true, false])
+          end
+        end
+        fixity_results = json.values.flatten.collect { |result| result["passed"] }
+        expect(fixity_results.all? { |r| r == true }).to be true
       end
     end
   end
