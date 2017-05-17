fe878adddfe431b70eff630b359c49f8c423a73b
Glen Horton <hortongn@ucmail.uc.edu>
2017-05-17 19:52:33 +0000

    Fix Browse Everything when work is being edited

--- spec/views/hyrax/base/_browse_everything.html.erb_spec.rb
+++ spec/views/hyrax/base/_browse_everything.html.erb_spec.rb
@@ -9,5 +9,6 @@ RSpec.describe 'hyrax/base/_browse_everything.html.erb', type: :view do
     render 'hyrax/base/browse_everything', f: f
     page = Capybara::Node::Simple.new(rendered)
     expect(page).to have_selector('div.alert-success', text: /Please note that if you upload a large number of files/i, count: 1)
+    expect(page).to have_selector("button[id='browse-btn'][data-target='#edit_generic_work_#{form.model.id}']")
   end
 end
