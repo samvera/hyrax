be1e03b6bc1e5909af934b33fafe4eeda336ac9a
Jonathan Rochkind <jrochkind@chemheritage.org>
2017-05-24 14:50:49 -0400

    Change data from integer to boolean in db-independent way
    previous migration from #984, postgres did not like.
    
    This way could be slow, as it needs to fetch and save each object individually,
    but I can't think of anything better.
    
    Those who already generated this hyrax app into a local app may have to manually
    copy the migration. If this hadn't been in a release yet, that wouldn't seem
    like a problem -- but seems it made it into 1.0 somehow?
    
    Fixes #1035
    
    As a bonus, it's reversible now.

--- db/migrate/20170504192714_change_checksum_audit_log.rb
+++ db/migrate/20170504192714_change_checksum_audit_log.rb
@@ -1,8 +1,18 @@
 class ChangeChecksumAuditLog < ActiveRecord::Migration[5.0]
   def change
     rename_column :checksum_audit_logs, :version, :checked_uri
-    change_column :checksum_audit_logs, :pass, :boolean
-    rename_column :checksum_audit_logs, :pass, :passed
+    add_column    :checksum_audit_logs, :pass, :passed
+
+    reversible do |dir|
+      dir.up do
+        ChecksumAuditLog.find_each { |log| log.update!(passed: log.pass ) }
+      end
+      dir.down do
+        ChecksumAuditLog.find_each { |log| log.update!(pass: log.passed ) }
+      end
+    end
+
+    remove_column :checksum_audit_log, :pass
     add_index     :checksum_audit_logs, :checked_uri
   end
 end
