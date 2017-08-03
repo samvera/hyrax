9ea6cd885ff36ed63d72c219e31c1f63bc6e7e70
Michael J. Giarlo <mjgiarlo@stanford.edu>
2017-08-03 09:45:31 -0700

    Remove incorrect TODO comment
    This is a documented service that folks can use in their own rake tasks, etc.

--- app/services/hyrax/repository_fixity_check_service.rb
+++ app/services/hyrax/repository_fixity_check_service.rb
@@ -1,6 +1,5 @@
 module Hyrax
   class RepositoryFixityCheckService
-    # TODO: this never seems to be used. Remove?
     def self.fixity_check_everything
       ::FileSet.find_each do |gf|
         Hyrax::FileSetFixityCheckService.new(gf).fixity_check
