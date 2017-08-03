26639b22a01bd764fc0af0d1ec920160dfb2fd09
Joe Atzberger <ohiocore@gmail.com>
2017-08-03 13:23:23 -0700

    Only call WorkingDirectory if filepath file unavailable
    Add a test for that.
    
    Refactored spec to do some of the common stubbing in the `let` statements.

--- app/jobs/characterize_job.rb
+++ app/jobs/characterize_job.rb
@@ -8,12 +8,12 @@ class CharacterizeJob < Hyrax::ApplicationJob
   # @param [String, NilClass] filepath the cached file within the Hyrax.config.working_path
   def perform(file_set, file_id, filepath = nil)
     raise "#{file_set.class.characterization_proxy} was not found for FileSet #{file_set.id}" unless file_set.characterization_proxy?
-    filename = Hyrax::WorkingDirectory.find_or_retrieve(file_id, file_set.id, filepath)
-    Hydra::Works::CharacterizationService.run(file_set.characterization_proxy, filename)
+    filepath = Hyrax::WorkingDirectory.find_or_retrieve(file_id, file_set.id) unless filepath && File.exist?(filepath)
+    Hydra::Works::CharacterizationService.run(file_set.characterization_proxy, filepath)
     Rails.logger.debug "Ran characterization on #{file_set.characterization_proxy.id} (#{file_set.characterization_proxy.mime_type})"
     file_set.characterization_proxy.save!
     file_set.update_index
     file_set.parent.in_collections.each(&:update_index) if file_set.parent
-    CreateDerivativesJob.perform_later(file_set, file_id, filename)
+    CreateDerivativesJob.perform_later(file_set, file_id, filepath)
   end
 end
