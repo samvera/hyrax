07940b4c6e8d2f27edac1b93b419f668d7c9e775
Michael J. Giarlo <mjgiarlo@stanford.edu>
2017-08-04 13:31:45 -0700

    Improve comment in fixity check service

--- app/services/hyrax/repository_fixity_check_service.rb
+++ app/services/hyrax/repository_fixity_check_service.rb
@@ -1,8 +1,11 @@
 module Hyrax
   class RepositoryFixityCheckService
+    # This is a service that folks can use in their own rake tasks,
+    # etc. It is not otherwise called or relied upon in Hyrax.
+    # @see https://github.com/samvera/hyrax/wiki/Hyrax-Management-Guide#fixity-checking
     def self.fixity_check_everything
-      ::FileSet.find_each do |gf|
-        Hyrax::FileSetFixityCheckService.new(gf).fixity_check
+      ::FileSet.find_each do |file_set|
+        Hyrax::FileSetFixityCheckService.new(file_set).fixity_check
       end
     end
   end
