24fb839ca431999d3df9e8683902bf03a65ee58a
Glen Horton <hortongn@ucmail.uc.edu>
2017-08-07 21:27:11 +0000

    Add new and edit views for collection types

--- spec/controllers/hyrax/admin/collection_types_controller_spec.rb
+++ spec/controllers/hyrax/admin/collection_types_controller_spec.rb
@@ -121,6 +121,23 @@ RSpec.describe Hyrax::Admin::CollectionTypesController, type: :controller do
         get :new
         expect(response).to have_http_status(:success)
       end
+
+      it 'adds breadcrumbs' do
+        expect(controller).to receive(:add_breadcrumb).with(I18n.t('hyrax.controls.home'), root_path)
+        expect(controller).to receive(:add_breadcrumb).with(I18n.t('hyrax.dashboard.breadcrumbs.admin'), dashboard_path)
+        expect(controller).to receive(:add_breadcrumb).with(I18n.t('hyrax.admin.sidebar.configuration'), '#')
+        expect(controller).to receive(:add_breadcrumb).with(I18n.t('hyrax.admin.collection_types.index.breadcrumb'), admin_collection_types_path)
+        expect(controller).to receive(:add_breadcrumb).with(I18n.t('hyrax.admin.collection_types.new.header'), new_admin_collection_type_path)
+        get :new
+        expect(response).to be_success
+        expect(response).to render_template "layouts/dashboard"
+      end
+
+      it 'defines a form' do
+        get :new
+        expect(response).to be_success
+        expect(assigns[:form]).to be_kind_of Hyrax::Forms::Admin::CollectionTypeForm
+      end
     end
 
     describe "#edit" do
@@ -141,6 +158,23 @@ RSpec.describe Hyrax::Admin::CollectionTypesController, type: :controller do
       it "returns success" do
         delete :destroy, params: { id: :id }
         expect(response).to have_http_status(:success)
+      end
+
+      it 'adds breadcrumbs' do
+        expect(controller).to receive(:add_breadcrumb).with(I18n.t('hyrax.controls.home'), root_path)
+        expect(controller).to receive(:add_breadcrumb).with(I18n.t('hyrax.dashboard.breadcrumbs.admin'), dashboard_path)
+        expect(controller).to receive(:add_breadcrumb).with(I18n.t('hyrax.admin.sidebar.configuration'), '#')
+        expect(controller).to receive(:add_breadcrumb).with(I18n.t('hyrax.admin.collection_types.index.breadcrumb'), admin_collection_types_path)
+        expect(controller).to receive(:add_breadcrumb).with(I18n.t('hyrax.admin.collection_types.edit.header'), edit_admin_collection_type_path(1))
+        get :edit, params: { id: 1 }
+        expect(response).to be_success
+        expect(response).to render_template "layouts/dashboard"
+      end
+
+      it 'defines a form' do
+        get :edit, params: { id: 1 }
+        expect(response).to be_success
+        expect(assigns[:form]).to be_kind_of Hyrax::Forms::Admin::CollectionTypeForm
       end
     end
   end
