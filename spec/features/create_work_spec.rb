7ae3921aba460c296341737777ff5a0b3595402b
Michael J. Giarlo <mjgiarlo@stanford.edu>
2017-08-08 10:54:03 -0700

    Re-enable feature spec that tests uploading files and creating works

--- spec/features/create_work_spec.rb
+++ spec/features/create_work_spec.rb
@@ -1,5 +1,4 @@
-# Skipping because this was failing intermittently on travis
-RSpec.feature 'Creating a new Work', :js, :workflow, skip: true do
+RSpec.feature 'Creating a new Work', :js, :workflow do
   let(:user) { create(:user) }
   let(:file1) { File.open(fixture_path + '/world.png') }
   let(:file2) { File.open(fixture_path + '/image.jp2') }
@@ -10,17 +9,18 @@ RSpec.feature 'Creating a new Work', :js, :workflow, skip: true do
     # Grant the user access to deposit into an admin set.
     create(:permission_template_access,
            :deposit,
-           permission_template: create(:permission_template, with_admin_set: true),
+           permission_template: create(:permission_template, with_admin_set: true, with_active_workflow: true),
            agent_type: 'user',
            agent_id: user.user_key)
     # stub out characterization. Travis doesn't have fits installed, and it's not relevant to the test.
     allow(CharacterizeJob).to receive(:perform_later)
   end
 
-  context "when the user is not a proxy", skip: "This was failing intermittently" do
+  context "when the user is not a proxy" do
     before do
       sign_in user
-      click_link "Create Work"
+      click_link 'Works'
+      click_link "Add new work"
       choose "payload_concern", option: "GenericWork"
       click_button 'Create work'
     end
@@ -29,35 +29,40 @@ RSpec.feature 'Creating a new Work', :js, :workflow, skip: true do
       click_link "Files" # switch tab
       expect(page).to have_content "Add files"
       expect(page).to have_content "Add folder"
-      # Capybara doesn't dependably upload files, so we'll stub out the results of the uploader:
-      page.execute_script("$(\"#new_generic_work\").append('<input name=\"uploaded_files[]\" value=\"#{uploaded_file1.id}\" type=\"hidden\">')" \
-        ".append('<input name=\"uploaded_files[]\" value=\"#{uploaded_file2.id}\" type=\"hidden\">');")
-      # attach_file("files[]", File.dirname(__FILE__) + "/../../spec/fixtures/image.jp2", visible: false)
-      # attach_file("files[]", File.dirname(__FILE__) + "/../../spec/fixtures/jp2_fits.xml", visible: false)
-      # click_button "Start upload"
-      click_link "Metadata" # switch tab
+      within('span#fileupload') do
+        attach_file("files[]", "#{Hyrax::Engine.root}/spec/fixtures/image.jp2", visible: false)
+        attach_file("files[]", "#{Hyrax::Engine.root}/spec/fixtures/jp2_fits.xml", visible: false)
+      end
+      click_link "Descriptions" # switch tab
       fill_in('Title', with: 'My Test Work')
-
+      fill_in('Creator', with: 'Doe, Jane')
+      fill_in('Keyword', with: 'testing')
+      select('In Copyright', from: 'Rights statement')
+      # With selenium and the chrome driver, focus remains on the
+      # select box. Click outside the box so the next line can't find
+      # its element
+      find('body').click
       choose('generic_work_visibility_open')
       expect(page).to have_content('Please note, making something visible to the world (i.e. marking this as Public) may be viewed as publishing which could impact your ability to')
-      # attach_file('Upload a file', fixture_file_path('files/image.png'))
       check('agreement')
-      puts "Required metadata: #{page.evaluate_script(%{$('#form-progress').data('save_work_control').requiredFields.areComplete})}"
-      puts "Required files: #{page.evaluate_script(%{$('#form-progress').data('save_work_control').uploads.hasFiles})}"
-      puts "Agreement : #{page.evaluate_script(%{$('#form-progress').data('save_work_control').depositAgreement.isAccepted})}"
+      # These lines are for debugging, should this test fail
+      # puts "Required metadata: #{page.evaluate_script(%{$('#form-progress').data('save_work_control').requiredFields.areComplete})}"
+      # puts "Required files: #{page.evaluate_script(%{$('#form-progress').data('save_work_control').uploads.hasFiles})}"
+      # puts "Agreement : #{page.evaluate_script(%{$('#form-progress').data('save_work_control').depositAgreement.isAccepted})}"
       click_on('Save')
       expect(page).to have_content('My Test Work')
       expect(page).to have_content "Your files are being processed by Hyrax in the background."
     end
   end
 
-  context 'when the user is a proxy', skip: "This was failing intermittently" do
+  context 'when the user is a proxy' do
     let(:second_user) { create(:user) }
 
     before do
       ProxyDepositRights.create!(grantor: second_user, grantee: user)
       sign_in user
-      click_link "Create Work"
+      click_link 'Works'
+      click_link "Add new work"
       choose "payload_concern", option: "GenericWork"
       click_button 'Create work'
     end
@@ -65,37 +70,34 @@ RSpec.feature 'Creating a new Work', :js, :workflow, skip: true do
     it "allows on-behalf-of deposit" do
       click_link "Files" # switch tab
       expect(page).to have_content "Add files"
-
-      # Capybara doesn't dependably upload files, so we'll stub out the results of the uploader:
-      page.execute_script("$(\"#new_generic_work\").append('<input name=\"uploaded_files[]\" value=\"#{uploaded_file1.id}\" type=\"hidden\">')" \
-        ".append('<input name=\"uploaded_files[]\" value=\"#{uploaded_file2.id}\" type=\"hidden\">');")
-      # attach_file("files[]", File.dirname(__FILE__) + "/../../spec/fixtures/image.jp2", visible: false)
-      # attach_file("files[]", File.dirname(__FILE__) + "/../../spec/fixtures/jp2_fits.xml", visible: false)
-      # click_button "Start upload"
-
-      click_link "Metadata" # switch tab
+      within('span#fileupload') do
+        attach_file("files[]", "#{Hyrax::Engine.root}/spec/fixtures/image.jp2", visible: false)
+        attach_file("files[]", "#{Hyrax::Engine.root}/spec/fixtures/jp2_fits.xml", visible: false)
+      end
+      click_link "Descriptions" # switch tab
       fill_in('Title', with: 'My Test Work')
-
+      fill_in('Creator', with: 'Doe, Jane')
+      fill_in('Keyword', with: 'testing')
+      select('In Copyright', from: 'Rights statement')
+      # With selenium and the chrome driver, focus remains on the
+      # select box. Click outside the box so the next line can't find
+      # its element
+      find('body').click
       choose('generic_work_visibility_open')
       expect(page).to have_content('Please note, making something visible to the world (i.e. marking this as Public) may be viewed as publishing which could impact your ability to')
       select(second_user.user_key, from: 'On behalf of')
       check('agreement')
-      puts "Required metadata: #{page.evaluate_script(%{$('#form-progress').data('save_work_control').requiredFields.areComplete})}"
-      puts "Required files: #{page.evaluate_script(%{$('#form-progress').data('save_work_control').uploads.hasFiles})}"
-      puts "Agreement : #{page.evaluate_script(%{$('#form-progress').data('save_work_control').depositAgreement.isAccepted})}"
+      # These lines are for debugging, should this test fail
+      # puts "Required metadata: #{page.evaluate_script(%{$('#form-progress').data('save_work_control').requiredFields.areComplete})}"
+      # puts "Required files: #{page.evaluate_script(%{$('#form-progress').data('save_work_control').uploads.hasFiles})}"
+      # puts "Agreement : #{page.evaluate_script(%{$('#form-progress').data('save_work_control').depositAgreement.isAccepted})}"
       click_on('Save')
-
       expect(page).to have_content('My Test Work')
       expect(page).to have_content "Your files are being processed by Hyrax in the background."
 
-      click_link('Dashboard')
-      click_link('Shares')
-
-      click_link('Works Shared with Me')
+      sign_in second_user
+      click_link 'Works'
       expect(page).to have_content "My Test Work"
-      # TODO: Show the work details (owner email)
-      # first('i.glyphicon-chevron-right').click
-      # first('.expanded-details').click_link(second_user.email)
     end
   end
 end
