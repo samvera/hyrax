7c179ffb1db57af8efecacc2dbab7ae381cb6ab8
Michael J. Giarlo <mjgiarlo@stanford.edu>
2017-08-08 12:27:41 -0700

    Add link to upstream Capybara/chromedriver issue

--- spec/support/features/confirmation.rb
+++ spec/support/features/confirmation.rb
@@ -1,6 +1,9 @@
 # spec/support/features/confirmation.rb
-# TODO: Delete this module when `#accept_confirm` supported by capybara w/ chrome
-#       https://github.com/samvera/hyrax/issues/1445
+
+# TODO: Delete this module when `#accept_confirm` supported by
+# capybara w/ chrome:
+#   https://github.com/teamcapybara/capybara/issues/1860
+#   https://github.com/samvera/hyrax/issues/1445
 module Features
   module Confirmation
     def accept_confirm
