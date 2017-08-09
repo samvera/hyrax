836b282409242151b4525403c773054e8d58e357
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2017-08-09 14:54:26 -0400

    Updating CollectionType attributes
    These changes brings the column names inline with the helper methods
    that were articulated in the associated Github issue. For short-term
    backwards compatibility, I'm including alias_attributes so other
    developers in the sprint will have an easier migration path.
    
    Closes #1452

--- spec/models/hyrax/collection_type_spec.rb
+++ spec/models/hyrax/collection_type_spec.rb
@@ -11,12 +11,12 @@ RSpec.describe Hyrax::CollectionType, type: :model do
   end
 
   it "has configuration properties with defaults" do
-    expect(collection_type.nestable).to be_truthy
-    expect(collection_type.discovery).to be_truthy
-    expect(collection_type.sharing).to be_truthy
-    expect(collection_type.multiple_membership).to be_truthy
-    expect(collection_type.require_membership).to be_falsey
-    expect(collection_type.workflow).to be_falsey
-    expect(collection_type.visibility).to be_falsey
+    expect(collection_type.nestable?).to be_truthy
+    expect(collection_type.discoverable?).to be_truthy
+    expect(collection_type.sharable?).to be_truthy
+    expect(collection_type.allow_multiple_membership?).to be_truthy
+    expect(collection_type.require_membership?).to be_falsey
+    expect(collection_type.assigns_workflow?).to be_falsey
+    expect(collection_type.assigns_visibility?).to be_falsey
   end
 end
