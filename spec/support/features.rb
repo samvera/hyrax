23e08548282d23a2e4a39eeb2bba7d92ca4ea9a6
Michael J. Giarlo <mjgiarlo@stanford.edu>
2017-08-14 09:48:06 -0700

    Use upstream implementation of `#accept_confirm`
    @twalpole (from the Capybara team) commented on #1445 that support for `#accept_confirm` for headless Chrome is already present in Capybara. I am not sure how my testing last week failed to turn this up... This commit removes the temporary workaround added in #1436 and uses the upstream implementations of both `#accept_confirm` and the headless Chrome javascript driver.
    
    Fixes #1445
    
    (Thanks a bunch for commenting over here, @twalpole, and for all the work you do on Capybara!)

--- spec/support/features.rb
+++ spec/support/features.rb
@@ -1,6 +1,5 @@
 # spec/support/features.rb
 require File.expand_path('../features/session_helpers', __FILE__)
-require File.expand_path('../features/confirmation', __FILE__)
 require File.expand_path('../features/workflow', __FILE__)
 
 require File.expand_path('../selectors', __FILE__)
@@ -9,5 +8,4 @@ require File.expand_path('../statistic_helper', __FILE__)
 
 RSpec.configure do |config|
   config.include Features::SessionHelpers, type: :feature
-  config.include Features::Confirmation, type: :feature
 end
