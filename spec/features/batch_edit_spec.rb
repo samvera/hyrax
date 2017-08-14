e3015cc895c911adeb027c076ebc1fac541e1a07
Michael J. Giarlo <mjgiarlo@stanford.edu>
2017-08-14 15:18:51 -0700

    Incorporate improvements from ScholarSphere batch edit spec changes
    Refs #815

--- spec/features/batch_edit_spec.rb
+++ spec/features/batch_edit_spec.rb
@@ -14,42 +14,13 @@ RSpec.describe 'batch', type: :feature, clean_repo: true, js: true do
   describe 'editing' do
     it 'changes the value of each field for all selected works' do
       click_on 'batch-edit'
-      batch_edit_fields.each do |f|
-        if f == "resource_type"
-          select_batch_edit_field(f, 'Book')
-        else
-          fill_in_batch_edit_field(f)
-        end
-      end
-      batch_edit_fields.each do |f|
-        fill_in_batch_edit_field_and_verify(f)
-      end
+      fill_in_batch_edit_fields_and_verify!
       work1.reload
       work2.reload
-      expect(work1.creator).to eq ['NEW creator']
-      expect(work1.contributor).to eq ['NEW contributor']
-      expect(work1.description).to eq ['NEW description']
-      expect(work1.keyword).to eq ['NEW keyword']
-      expect(work1.publisher).to eq ['NEW publisher']
-      expect(work1.date_created).to eq ['NEW date_created']
-      expect(work1.subject).to eq ['NEW subject']
-      expect(work1.language).to eq ['NEW language']
-      expect(work1.identifier).to eq ['NEW identifier']
-      # expect(work1.based_near).to eq ['NEW based_near']
-      expect(work1.related_url).to eq ['NEW related_url']
-      expect(work1.resource_type).to eq ['Book']
-      expect(work2.creator).to eq ['NEW creator']
-      expect(work2.contributor).to eq ['NEW contributor']
-      expect(work2.description).to eq ['NEW description']
-      expect(work2.keyword).to eq ['NEW keyword']
-      expect(work2.publisher).to eq ['NEW publisher']
-      expect(work2.date_created).to eq ['NEW date_created']
-      expect(work2.subject).to eq ['NEW subject']
-      expect(work2.language).to eq ['NEW language']
-      expect(work2.identifier).to eq ['NEW identifier']
-      # expect(work2.based_near).to eq ['NEW based_near']
-      expect(work2.related_url).to eq ['NEW related_url']
-      expect(work2.resource_type).to eq ['Book']
+      batch_edit_fields.each do |field|
+        expect(work1.send(field)).to match_array("NEW #{field}")
+        expect(work2.send(field)).to match_array("NEW #{field}")
+      end
 
       # Reload the form and verify
       visit '/dashboard/my/works'
@@ -78,8 +49,6 @@ RSpec.describe 'batch', type: :feature, clean_repo: true, js: true do
       # expect(page).to have_css "input#generic_work_based_near[value*='NEW based_near']"
       batch_edit_expand("related_url")
       expect(page).to have_css "input#generic_work_related_url[value*='NEW related_url']"
-      batch_edit_expand("resource_type")
-      expect(page).to have_select "generic_work_resource_type", selected: 'Book'
     end
   end
 
--- spec/support/features/batch_edit_actions.rb
+++ spec/support/features/batch_edit_actions.rb
@@ -2,36 +2,21 @@ def batch_edit_fields
   # skipping based_near because it's a select2 field, which is hard to test via capybara
   [
     "creator", "contributor", "description", "keyword", "publisher", "date_created",
-    "subject", "language", "identifier", "related_url", "resource_type"
+    "subject", "language", "identifier", "related_url"
   ]
 end
 
-def fill_in_batch_edit_field(id)
-  batch_edit_expand(id)
-  within "#form_#{id}" do
-    fill_in "generic_work_#{id}", with: "NEW #{id}"
-  end
-end
-
-def fill_in_batch_edit_field_and_verify(id)
-  within "#form_#{id}" do
-    click_button "#{id}_save"
-    # Incrementing from 5 to 15 to see if we can prevent erratic failures in Travis. These
-    # failures result in a broken build and require a restart of the specs. This restart
-    # and waiting for feedback slows the overall trajectory of iterating on Hyrax.
-    expect(page).to have_content 'Changes Saved', wait: Capybara.default_max_wait_time * 15
+def fill_in_batch_edit_fields_and_verify!
+  batch_edit_fields.each do |field_id|
+    within "#form_#{field_id}" do
+      batch_edit_expand(field_id)
+      fill_in "generic_work_#{field_id}", with: "NEW #{field_id}"
+      click_button "#{field_id}_save"
+      expect(page).to have_content 'Changes Saved'
+    end
   end
 end
 
 def batch_edit_expand(field)
-  link = find("#expand_link_#{field}")
-  while link["class"].include?("collapsed")
-    sleep 0.1
-    link.click if link["class"].include?("collapsed")
-  end
-end
-
-def select_batch_edit_field(id, option)
-  batch_edit_expand(id)
-  select(option, from: "generic_work_#{id}")
+  find("#expand_link_#{field}").click
 end
