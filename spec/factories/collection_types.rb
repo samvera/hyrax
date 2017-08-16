e8e44da3f9ef8e039b910120ae15d6a6c4d6b152
Chris Colvard <cjcolvar@indiana.edu>
2017-08-16 16:36:05 -0400

    Build out basic controller

--- spec/factories/collection_types.rb
+++ spec/factories/collection_types.rb
@@ -1,9 +1,20 @@
 FactoryGirl.define do
   factory :collection_type, class: Hyrax::CollectionType do
+    sequence(:title) { |n| "Title #{n}" }
+    description 'Collection type with all options'
+    sequence(:machine_id) { |n| "title-#{n}" }
+    nestable true
+    discoverable true
+    sharable true
+    allow_multiple_membership true
+    require_membership false
+    assigns_workflow false
+    assigns_visibility false
+
     factory :user_collection_type do
-      title ['User Collection']
-      description ['A user oriented collection type']
-      machine_id ['user_collection']
+      title 'User Collection'
+      machine_id 'user_collection'
+      description 'A user oriented collection type'
     end
   end
 end
