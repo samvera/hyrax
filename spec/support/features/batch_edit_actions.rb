08aa6e2dceb187eaca777a537ddb9dd0ad9751e2
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2017-08-20 09:56:41 -0400

    Adding auto-wrapping of all Capybara matchers
    Based on Notre Dame's experience with our [QA Tests][1], we found it
    helpful to wrap all of the Capybara find and matches with sleep
    intervals. This reduced the fragility of our feature testing by
    augmenting Capybara's semi-reliable `wait` algorithm with sleep
    intervals in cases where the `wait` was not enough.
    
    The nature of `Capybara::Maleficent` is such that most of the time
    the specs will be as fast as they can be (they're slow enough as you
    are leveraging a browser for this testing). However, when a failure
    occurs, `Capybara::Maleficent` assumes that its due to a slowness
    in the browser (and not an actual failure). From there, it sleeps for
    a bit, then tries again.
    
    [1]:https://github.com/ndlib/QA_tests/blob/3e1372377f3c019790284196d380860f32c7321a/spec/spec_support/inject_sleep.rb

--- spec/support/features/batch_edit_actions.rb
+++ spec/support/features/batch_edit_actions.rb
@@ -18,24 +18,6 @@ def fill_in_batch_edit_fields_and_verify!
 end
 
 def batch_edit_expand(field)
-  with_sleep_injector do
-    find("#expand_link_#{field}").click
-    yield if block_given?
-  end
-end
-
-# Cribbed from Notre Dame's experience with extensive Capybara testing in our QA_Tests environment.
-# https://github.com/ndlib/QA_tests/blob/4e52fa69d5c3fd774febb2bd85134a78d21cbe97/spec/spec_support/inject_sleep.rb#L8-L24
-# Capybara makes claims that it's wait will in fact wait for elements to appear. In our experience, this is
-# mostly true.
-def with_sleep_injector
-  yield
-rescue Capybara::ElementNotFound
-  sleep 3
-  begin
-    yield
-  rescue Capybara::ElementNotFound
-    sleep 10
-    yield
-  end
+  find("#expand_link_#{field}").click
+  yield if block_given?
 end
