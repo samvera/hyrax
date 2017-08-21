062af7903144d5fc9bb2fcd32170f29d69fe06e8
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2017-08-21 12:46:11 -0400

    Favoring find then fill_in instead of only fill_in
    In the QA experience at Notre Dame, we found that relying on the find
    then filling in to be more robust when dealing with JS animation.

--- spec/support/features/batch_edit_actions.rb
+++ spec/support/features/batch_edit_actions.rb
@@ -10,9 +10,10 @@ def fill_in_batch_edit_fields_and_verify!
   batch_edit_fields.each do |field_id|
     within "#form_#{field_id}" do
       batch_edit_expand(field_id)
+      page.find("#generic_work_#{field_id}") # Ensuring that the element is on the page before we fill it
       fill_in "generic_work_#{field_id}", with: "NEW #{field_id}"
-      click_button "#{field_id}_save"
 
+      find("##{field_id}_save").click
       # This was `expect(page).to have_content 'Changes Saved'`, however in debugging,
       # the `have_content` check was ignoring the `within` scoping and finding
       # "Changes Saved" for other field areas
