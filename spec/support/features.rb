451b1fcbc448f2a509801734eb028e651cddd557
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2017-08-21 12:44:19 -0400

    Consolidating method used in one location
    Instead of obfuscating its usage, consolidate the logic close to the
    place where the tests are happening.

--- spec/support/features.rb
+++ spec/support/features.rb
@@ -3,7 +3,6 @@ require File.expand_path('../features/session_helpers', __FILE__)
 require File.expand_path('../features/workflow', __FILE__)
 require File.expand_path('../features/batch_edit_actions', __FILE__)
 require File.expand_path('../selectors', __FILE__)
-require File.expand_path('../proxies', __FILE__)
 require File.expand_path('../statistic_helper', __FILE__)
 
 RSpec.configure do |config|
