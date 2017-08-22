25ac519170167a7a818e8188f8739be3fa3f3c36
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2017-08-22 07:51:05 -0400

    Working on interfaces for NestCollectionForm

--- app/forms/hyrax/forms/dashboard/nest_collection_form.rb
+++ app/forms/hyrax/forms/dashboard/nest_collection_form.rb
@@ -4,15 +4,40 @@ module Hyrax
       # Responsible for validating that both the parent and child are valid for nesting; If so, then
       # also responsible for persisting those changes.
       class NestCollectionForm
-        include ActiveModel::Model
+        module NestedCollectionQueryService
+          def self.available_child_collections(parent:)
+            raise NotImplementedError
+          end
 
-        # @param [Hyrax::Colection, NilClass] parent
-        # @param [Hyrax::Colection, NilClass] child
-        # @param [Object] query_service
-        def initialize(parent: nil, child: nil, query_service: default_query_service)
+          def self.available_parent_collections(child:)
+            raise NotImplementedError
+          end
+
+          def self.parent_and_child_can_nest?(parent:, child:)
+            raise NotImplementedError
+          end
+        end
+
+        module NestedCollectionPersistenceService
+          def self.persist_nested_collection_for(parent:, child:)
+            raise NotImplementedError
+          end
+        end
+
+        include ActiveModel::Model
+        class_attribute :default_query_service, :default_persistence_service, instance_writer: false
+        self.default_query_service = NestedCollectionQueryService
+        self.default_persistence_service = NestedCollectionPersistenceService
+
+        # @param parent [Hyrax::Colection, NilClass]
+        # @param child [Hyrax::Colection, NilClass]
+        # @param query_service [NestedCollectionQueryService]
+        # @param persistence_service [#persist_nested_collection_for] responsible for persisting the parent/child relationship
+        def initialize(parent: nil, child: nil, query_service: default_query_service, persistence_service: default_persistence_service)
           self.parent = parent
           self.child = child
           self.query_service = query_service
+          self.persistence_service = persistence_service
         end
 
         attr_accessor :parent, :child
@@ -23,12 +48,7 @@ module Hyrax
 
         def save
           return false unless valid?
-          persist!
-        end
-
-        def save!
-          raise unless valid?
-          persist!
+          persistence_service.persist_nested_collection_for(parent: parent, child: child)
         end
 
         # For the given parent, what are all of the available collections that
@@ -45,25 +65,7 @@ module Hyrax
 
         private
 
-          attr_accessor :query_service
-
-          def default_query_service
-            QueryService.new
-          end
-
-          class QueryService
-            def available_child_collections(parent:)
-              raise NotImplementedError
-            end
-
-            def available_parent_collections(child:)
-              raise NotImplementedError
-            end
-
-            def parent_and_child_can_nest?(parent:, child:)
-              raise NotImplementedError
-            end
-          end
+          attr_accessor :query_service, :persistence_service
 
           def parent_and_child_can_be_nested
             if parent.try(:nestable?) && child.try(:nestable?)
@@ -74,11 +76,6 @@ module Hyrax
               errors.add(:parent, :is_not_nestable) unless parent.try(:nestable?)
               errors.add(:child, :is_not_nestable) unless child.try(:nestable?)
             end
-          end
-
-          # Write the appropriate relationship.
-          def persist!
-            true
           end
       end
     end
--- spec/forms/hyrax/forms/dashboard/nest_collection_form_spec.rb
+++ spec/forms/hyrax/forms/dashboard/nest_collection_form_spec.rb
@@ -2,18 +2,24 @@ RSpec.describe Hyrax::Forms::Dashboard::NestCollectionForm, type: :form do
   let(:parent) { double(nestable?: true) }
   let(:child) { double(nestable?: true) }
   let(:query_service) { double('Query Service') }
-  let(:form) { described_class.new(parent: parent, child: child, query_service: query_service) }
+  let(:persistence_service) { double('Persistence Service', persist_nested_collection: true) }
+  let(:form) { described_class.new(parent: parent, child: child, query_service: query_service, persistence_service: persistence_service) }
 
   subject { form }
 
   it { is_expected.to validate_presence_of(:parent) }
   it { is_expected.to validate_presence_of(:child) }
 
-  describe '#default_query_service' do
-    subject { described_class.new.send(:default_query_service) }
+  describe '.default_query_service' do
+    subject { described_class.default_query_service }
     it { is_expected.to respond_to(:available_parent_collections) }
     it { is_expected.to respond_to(:available_child_collections) }
     it { is_expected.to respond_to(:parent_and_child_can_nest?) }
+  end
+
+  describe '#default_query_service' do
+    subject { described_class.default_persistence_service }
+    it { is_expected.to respond_to(:persist_nested_collection_for) }
   end
 
   it 'is invalid if child cannot be nested within the parent' do
@@ -50,7 +56,7 @@ RSpec.describe Hyrax::Forms::Dashboard::NestCollectionForm, type: :form do
       end
       it { is_expected.to be_falsey }
       it 'does not even attempt to persist the relationship' do
-        expect(subject).not_to receive(:persist!)
+        expect(persistence_service).not_to receive(:persist_nested_collection_for)
         subject
       end
     end
@@ -58,7 +64,10 @@ RSpec.describe Hyrax::Forms::Dashboard::NestCollectionForm, type: :form do
       before do
         expect(form).to receive(:valid?).and_return(true)
       end
-      it { is_expected.to be_truthy }
+      it "returns the result of the given persistence_service's call to persist_nested_collection_for" do
+        expect(persistence_service).to receive(:persist_nested_collection_for).with(parent: parent, child: child).and_return(:persisted)
+        subject
+      end
     end
   end
 
