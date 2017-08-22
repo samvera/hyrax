7e2561268c6534db3e511489615611be5916ab37
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2017-08-22 11:44:18 -0400

    Adding NestedCollectionPersistenceService behavior
    This is an interstitial solution with an identified discrepancy
    between the PCDM specification and the feature specs.

--- app/services/hyrax/collections/nested_collection_persistence_service.rb
+++ app/services/hyrax/collections/nested_collection_persistence_service.rb
@@ -7,8 +7,16 @@ module Hyrax
       #
       # @param parent [Collection]
       # @param child [Collection]
+      # @note There appears to be a logical disconnect between the PCDM implementation of
+      #   Collections and the validation of nesting collections:
+      #   * For PCDM the "parent has_member child" relationship is defined, and managed on the parent.
+      #   * For the Nesting collections, we require that:
+      #     * The child is EDIT-able by the user
+      #     * The parent is READ-able by the user
+      #   The Nesting collections requirement implies that the PCDM relationship is pointing in the wrong direction.
       def self.persist_nested_collection_for(parent:, child:)
-        raise NotImplementedError
+        parent.members << child
+        parent.save
       end
     end
   end
