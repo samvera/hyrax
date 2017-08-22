17123582e14a68c1baea753d14af89d9e9d78919
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2017-08-21 21:42:36 -0400

    Updating NestCollectionForm to include behaviors
    Need to work on collaborators but the general behavior is beginning to
    work.
    
    [skip ci]

--- app/forms/hyrax/forms/dashboard/nest_collection_form.rb
+++ app/forms/hyrax/forms/dashboard/nest_collection_form.rb
@@ -6,15 +6,20 @@ module Hyrax
       class NestCollectionForm
         include ActiveModel::Model
 
-        def initialize(parent: nil, child: nil)
+        # @param [Hyrax::Colection, NilClass] parent
+        # @param [Hyrax::Colection, NilClass] child
+        # @param [Object] query_service
+        def initialize(parent: nil, child: nil, query_service: default_query_service)
           self.parent = parent
           self.child = child
+          self.query_service = query_service
         end
 
         attr_accessor :parent, :child
 
         validates :parent, presence: true
         validates :child, presence: true
+        validate :parent_and_child_can_be_nested
 
         def save
           return false unless valid?
@@ -26,16 +31,38 @@ module Hyrax
           persist!
         end
 
+        # For the given parent, what are all of the available collections that
+        # can be added as sub-collection of the parent.
         def available_child_collections
-          return [] if parent.blank?
+          query_service.available_child_collections(parent: parent)
         end
 
+        # For the given child, what are all of the available collections to
+        # which the child can be added as a sub-collection.
         def available_parent_collections
-          return [] if child.blank?
+          query_service.available_parent_collections(child: child)
         end
 
         private
 
+          attr_accessor :query_service
+
+          def default_query_service
+            raise 'TODO'
+          end
+
+          def parent_and_child_can_be_nested
+            if parent.try(:nestable?) && child.try(:nestable?)
+              return true if query_service.parent_and_child_can_nest?(parent: parent, child: child)
+              errors.add(:parent, :cannot_have_child_nested)
+              errors.add(:child, :cannot_nest_in_parent)
+            else
+              errors.add(:parent, :is_not_nestable) unless parent.try(:nestable?)
+              errors.add(:child, :is_not_nestable) unless child.try(:nestable?)
+            end
+          end
+
+          # Write the appropriate relationship.
           def persist!
             true
           end
--- spec/forms/hyrax/forms/dashboard/nest_collection_form_spec.rb
+++ spec/forms/hyrax/forms/dashboard/nest_collection_form_spec.rb
@@ -1,12 +1,38 @@
 RSpec.describe Hyrax::Forms::Dashboard::NestCollectionForm, type: :form do
-  let(:parent) { double }
-  let(:child) { double }
-  let(:form) { described_class.new(parent: parent, child: child) }
+  let(:parent) { double(nestable?: true) }
+  let(:child) { double(nestable?: true) }
+  let(:query_service) { double('Query Service') }
+  let(:form) { described_class.new(parent: parent, child: child, query_service: query_service) }
 
   subject { form }
 
   it { is_expected.to validate_presence_of(:parent) }
   it { is_expected.to validate_presence_of(:child) }
+
+  it 'is invalid if child cannot be nested within the parent' do
+    expect(query_service).to receive(:parent_and_child_can_nest?).with(parent: parent, child: child).and_return(false)
+    subject.valid?
+    expect(subject.errors[:parent]).to eq(["cannot have child nested within it"])
+    expect(subject.errors[:child]).to eq(["cannot nest within parent"])
+  end
+
+  describe 'parent is not nestable' do
+    let(:parent) { double(nestable?: false) }
+
+    it 'is not valid' do
+      subject.valid?
+      expect(subject.errors[:parent]).to eq(["is not nestable"])
+    end
+  end
+
+  describe 'child is not nestable' do
+    let(:child) { double(nestable?: false) }
+
+    it 'is not valid' do
+      subject.valid?
+      expect(subject.errors[:child]).to eq(["is not nestable"])
+    end
+  end
 
   describe '#save' do
     subject { form.save }
@@ -32,27 +58,17 @@ RSpec.describe Hyrax::Forms::Dashboard::NestCollectionForm, type: :form do
   describe '#available_child_collections' do
     subject { form.available_child_collections }
 
-    describe 'when parent is not present' do
-      let(:parent) { nil }
-
-      it { is_expected.to eq([]) }
-    end
-
-    describe 'when parent is present' do
-      xit { is_expected.to eq([]) }
+    it 'delegates to the underlying query_service' do
+      expect(query_service).to receive(:available_child_collections).with(parent: parent).and_return(:results)
+      expect(subject).to eq(:results)
     end
   end
   describe '#available_parent_collections' do
     subject { form.available_parent_collections }
 
-    describe 'when parent is not present' do
-      let(:child) { nil }
-
-      it { is_expected.to eq([]) }
-    end
-
-    describe 'when parent is present' do
-      xit { is_expected.to eq([]) }
+    it 'delegates to the underlying query_service' do
+      expect(query_service).to receive(:available_parent_collections).with(child: child).and_return(:results)
+      expect(subject).to eq(:results)
     end
   end
 end
