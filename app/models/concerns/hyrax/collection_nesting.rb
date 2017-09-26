7877a06cc38b9943b24e5b5a51d3c3643c003d68
LaRita Robinson <lrobins5@nd.edu>
2017-09-26 15:11:28 -0400

    Fix nesting indexing
    Move nesting indexing from after_save to after_update_index.

--- app/models/concerns/hyrax/collection_nesting.rb
+++ app/models/concerns/hyrax/collection_nesting.rb
@@ -3,10 +3,15 @@ module Hyrax
     extend ActiveSupport::Concern
 
     included do
-      after_save :update_nested_collection_relationship_indices
+      define_model_callbacks :update_index, only: :after
+      after_update_index :update_nested_collection_relationship_indices
       def update_nested_collection_relationship_indices
         Hyrax.config.nested_relationship_reindexer.call(id: id)
       end
+    end
+
+    def update_index(*args)
+      _run_update_index_callbacks { super }
     end
   end
 end
--- spec/models/concerns/hyrax/collection_nesting_spec.rb
+++ spec/models/concerns/hyrax/collection_nesting_spec.rb
@@ -2,9 +2,14 @@ RSpec.describe Hyrax::CollectionNesting do
   describe 'including this module' do
     let(:klass) do
       Class.new do
-        def self.after_save(method_name)
-          @after_save ||= []
-          @after_save << method_name # rubocop:disable RSpec/InstanceVariable
+        def self.after_update_index(method_name)
+          @after_update_index ||= []
+          @after_update_index << method_name # rubocop:disable RSpec/InstanceVariable
+        end
+
+        def self.define_model_callbacks(method_name, only:)
+          @define_model_callbacks ||= []
+          @define_model_callbacks << [method_name, only]  # rubocop:disable RSpec/InstanceVariable
         end
 
         include Hyrax::CollectionNesting
@@ -12,8 +17,8 @@ RSpec.describe Hyrax::CollectionNesting do
       end
     end
 
-    it 'will register an after_save call to #update_nested_collection_relationship_indices' do
-      expect(klass.instance_variable_get("@after_save")).to eq([:update_nested_collection_relationship_indices])
+    it 'will register an after_update_index call to #update_nested_collection_relationship_indices' do
+      expect(klass.instance_variable_get("@after_update_index")).to eq([:update_nested_collection_relationship_indices])
     end
 
     it 'will add the instance method of #update_nested_collection_relationship_indices' do
