178f668daa4f96021ac8ed036a6fcb09b5d998a5
Jonathan Dixon <dixonj16@vbi.vt.edu>
2017-10-11 12:43:05 -0400

    Default option for admin set release settings.

--- spec/views/hyrax/admin/admin_sets/_form_visibility.html.erb_spec.rb
+++ spec/views/hyrax/admin/admin_sets/_form_visibility.html.erb_spec.rb
@@ -12,6 +12,7 @@ RSpec.describe 'hyrax/admin/admin_sets/_form_visibility.html.erb', type: :view d
   it "has the form" do
     expect(rendered).to have_selector('#visibility input[type=radio][name="permission_template[release_period]"][value=now]')
     expect(rendered).to have_selector('#visibility input[type=radio][name="permission_template[release_period]"][value=fixed]')
+    expect(rendered).to have_selector('#visibility input[type=radio][name="permission_template[release_varies]"][value=""]')
     expect(rendered).to have_selector('#visibility input[type=radio][name="permission_template[release_varies]"][value=before]')
     expect(rendered).to have_selector('#visibility input[type=radio][name="permission_template[release_varies]"][value=embargo]')
     expect(rendered).to have_selector('#visibility select[name="permission_template[release_embargo]"]')
