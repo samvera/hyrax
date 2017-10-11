4a4f782e9cdd63fda1bbb753519742cf4badb164
Justin Coyne <jcoyne@justincoyne.com>
2017-10-11 09:49:29 -0500

    Use valkyrie to query for FileSets

--- app/services/hyrax/user_stat_importer.rb
+++ app/services/hyrax/user_stat_importer.rb
@@ -48,7 +48,7 @@ module Hyrax
 
       def process_files(stats, user, start_date)
         file_ids_for_user(user).each do |file_id|
-          file = ::FileSet.find(file_id)
+          file = Queries.find_by(id: file_id)
           view_stats = extract_stats_for(object: file, from: FileViewStat, start_date: start_date, user: user)
           stats = tally_results(view_stats, :views, stats) if view_stats.present?
           delay
