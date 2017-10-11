02712479c48deb25b2da8d005426450469ebf6e4
Justin Coyne <jcoyne@justincoyne.com>
2017-10-11 17:43:36 -0500

    Change from has_model_ssim to internal_resource_ssim

--- spec/views/hyrax/dashboard/collections/_show_descriptions.html.erb_spec.rb
+++ spec/views/hyrax/dashboard/collections/_show_descriptions.html.erb_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'hyrax/dashboard/collections/_show_descriptions.html.erb', type: 
     let(:collection) do
       {
         id: '999',
-        "has_model_ssim" => ["Collection"],
+        "internal_resource_ssim" => ["Collection"],
         "title_tesim" => ["Title 1"],
         'date_created_tesim' => '2000-01-01'
       }
