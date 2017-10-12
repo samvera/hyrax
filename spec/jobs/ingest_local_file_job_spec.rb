bf13215ad12f3801961aac8a89f1a98e0293acb2
Olli Lyytinen <olli.lyytinen@durham.ac.uk>
2017-10-12 10:43:12 +0100

    Make IngestLocalFileJob not remove the ingested file.

--- spec/jobs/ingest_local_file_job_spec.rb
+++ spec/jobs/ingest_local_file_job_spec.rb
@@ -4,16 +4,13 @@ RSpec.describe IngestLocalFileJob do
   let(:file_set) { FileSet.new }
   let(:actor) { double }
 
-  let(:mock_upload_directory) { 'spec/mock_upload_directory' }
-
   before do
-    Dir.mkdir mock_upload_directory unless File.exist? mock_upload_directory
-    FileUtils.copy(File.expand_path('../../fixtures/world.png', __FILE__), mock_upload_directory)
     allow(Hyrax::Actors::FileSetActor).to receive(:new).with(file_set, user).and_return(actor)
   end
 
   it 'has attached a file' do
+    expect(FileUtils).not_to receive(:rm)
     expect(actor).to receive(:create_content).and_return(true)
-    described_class.perform_now(file_set, File.join(mock_upload_directory, 'world.png'), user)
+    described_class.perform_now(file_set, File.join(fixture_path, 'world.png'), user)
   end
 end
