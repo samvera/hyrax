64cad9fb6f04bc06d524cefe3b01b3d474c08b90
Chris Colvard <cjcolvar@indiana.edu>
2017-10-12 11:51:33 -0400

    Find all file sets using Valkyrie

--- app/services/hyrax/queries.rb
+++ app/services/hyrax/queries.rb
@@ -5,7 +5,7 @@ module Hyrax
     class_attribute :metadata_adapter
     self.metadata_adapter = Valkyrie.config.metadata_adapter
     class << self
-      delegate :find_all, :find_by, :find_members, :find_inverse_references_by, to: :default_adapter
+      delegate :find_all, :find_all_of_model, :find_by, :find_members, :find_inverse_references_by, to: :default_adapter
 
       def default_adapter
         new(metadata_adapter: metadata_adapter)
@@ -13,7 +13,7 @@ module Hyrax
     end
 
     attr_reader :metadata_adapter
-    delegate :find_all, :find_by, :find_members, :find_inverse_references_by, to: :metadata_adapter_query_service
+    delegate :find_all, :find_all_of_model, :find_by, :find_members, :find_inverse_references_by, to: :metadata_adapter_query_service
     def initialize(metadata_adapter:)
       @metadata_adapter = metadata_adapter
     end
