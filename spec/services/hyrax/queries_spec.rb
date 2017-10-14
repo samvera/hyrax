8e2a5403f6a3e2fa9626458e8dcbaabb04b3844a
Chris Colvard <cjcolvar@indiana.edu>
2017-10-13 23:42:33 -0400

    Test using valkyrie's shared specs

--- spec/services/hyrax/queries_spec.rb
+++ spec/services/hyrax/queries_spec.rb
@@ -1,7 +1,17 @@
+require 'valkyrie/specs/shared_specs/queries'
+
 RSpec.describe Hyrax::Queries do
-  let(:persister)                { Valkyrie.config.metadata_adapter.persister }
+  let(:adapter)                  { described_class.metadata_adapter }
+  let(:query_service)            { described_class.default_adapter }
+  let(:persister)                { adapter.persister }
   let(:thing_that_exists)        { persister.save(resource: GenericWork.new(id: 'i_exist')) }
   let(:thing_that_used_to_exist) { persister.delete(resource: persister.save(resource: GenericWork.new(id: 'i_used_to_exist'))) }
+
+  before do
+    described_class.metadata_adapter.persister.wipe!
+  end
+
+  it_behaves_like "a Valkyrie query provider"
 
   it 'knows the thing that exists exists' do
     expect(described_class.exists?(thing_that_exists.id)).to be true
