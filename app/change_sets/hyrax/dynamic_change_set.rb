fa3ffe3fc8079ed3a9a55f00874737ad7b7d5c0e
Justin Coyne <jcoyne@justincoyne.com>
2017-10-24 10:03:58 -0500

    Adjust the DynamicChangeSet to namespace for Hyrax classes

--- app/change_sets/hyrax/dynamic_change_set.rb
+++ app/change_sets/hyrax/dynamic_change_set.rb
@@ -3,7 +3,12 @@
 module Hyrax
   class DynamicChangeSet
     def self.new(obj, *args)
-      "#{obj.class}ChangeSet".constantize.new(obj, *args)
+      change_set_class = if obj.is_a? Collection
+                           "Hyrax::#{obj.class}ChangeSet"
+                         else
+                           "#{obj.class}ChangeSet"
+                         end
+      change_set_class.constantize.new(obj, *args)
     end
   end
 end
