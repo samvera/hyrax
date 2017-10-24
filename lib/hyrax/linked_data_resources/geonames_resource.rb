79a19df136bd85ce2b0f283718fba5bf79c6303d
Julie Allinson <julie.allinson@london.ac.uk>
2017-10-24 23:27:30 +0100

    add a new indexer for 'linked_data_attributes', ie. those whose values are RDF::URIs from which we want to fetch a label to index in solr

---
+++ lib/hyrax/linked_data_resources/geonames_resource.rb
@@ -0,0 +1,56 @@
+module Hyrax
+  module LinkedDataResources
+    # Do not Extend BaseResource For Geonames.
+    # We can get richer information from the JSON API, so let's do that instead.
+    class GeonamesResource
+      attr_reader :rdf_subject
+
+      def initialize(rdf_subject)
+        @rdf_subject = rdf_subject
+      end
+
+      # @return [String] rdf_label
+      def fetch_external
+        fetch_value
+      end
+
+      private
+
+        # Get the data from the URL by calling Faraday#get
+        def fetch_value
+          Rails.logger.info "Fetching #{rdf_subject} from the authorative source."
+          response = Faraday.get build_json_uri(rdf_subject)
+          return rdf_subject.to_s if response.status != 200
+          label(response.body)
+        rescue IOError, Faraday::ConnectionFailed, ArgumentError => e
+          # IOError could result from a 500 error on the remote server
+          # Faraday::ConnectionFailed results if there is no server to connect to
+          Rails.logger.error "Unable to fetch #{rdf_subject} from the authorative source.\n#{e.message}"
+          rdf_subject.to_s
+        end
+
+        # Construct a URI for the Geonames getJSON API
+        #
+        # @return [String] uri
+        def build_json_uri(uri)
+          "http://www.geonames.org/getJSON?geonameId=#{find_id(uri)}&username=#{Qa::Authorities::Geonames.username}"
+        end
+
+        # Extract the Geonames id from a URI in the form http://sws.geonames.org/2638077/
+        #
+        # @param uri [RDF::URI] the Geonames URI
+        # @return [String] the Geonames id
+        def find_id(uri)
+          uri.to_s.split('/')[3]
+        end
+
+        # Construct a disambiguable label from the JSON response
+        #
+        # @param json_string [String] the Faraday::Response#body
+        def label(json_string)
+          item = JSON.parse(json_string)
+          [item['name'], item['adminName1'], item['countryName']].compact.join(', ')
+        end
+    end
+  end
+end
