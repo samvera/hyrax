37a8d10521f9d4bbfa4670f7f415e2f1d2fb3e92
Chris Colvard <cjcolvar@indiana.edu>
2017-10-24 21:28:10 -0400

    Persist instead of save

--- spec/jobs/hyrax/grant_read_job_spec.rb
+++ spec/jobs/hyrax/grant_read_job_spec.rb
@@ -1,11 +1,11 @@
 RSpec.describe Hyrax::GrantReadJob do
   let(:depositor) { create(:user) }
-  let(:file_set) { build(:file_set) }
+  let(:file_set) { create_for_repository(:file_set) }
 
   it 'grants a user read access to a FileSet' do
-    expect(FileSet).to receive(:find).with(file_set.id).and_return(file_set)
-    expect(file_set).to receive(:read_users=).with(array_including(depositor.user_key))
-    expect(file_set).to receive(:save!)
+    expect(file_set.read_users).not_to include(depositor.user_key)
     described_class.perform_now(file_set.id, depositor.user_key)
+    reload = Hyrax::Queries.find_by(id: file_set.id)
+    expect(reload.read_users).to include(depositor.user_key)
   end
 end
