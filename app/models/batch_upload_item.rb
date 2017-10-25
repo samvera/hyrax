e6a59a4f1e73436350b294b50791336ea2901936
Justin Coyne <jcoyne@justincoyne.com>
2017-10-25 10:53:51 -0500

    Convert batch forms to ChangeSets

--- app/models/batch_upload_item.rb
+++ app/models/batch_upload_item.rb
@@ -1,4 +1,4 @@
-# This stands in for an object to be created from the BatchUploadForm.
+# This stands in for an object to be created from the BatchUploadChangeSet.
 # It should never actually be persisted in the repository.
 # The properties on this form should be copied to a real work type.
 class BatchUploadItem < Valkyrie::Resource
@@ -8,13 +8,4 @@ class BatchUploadItem < Valkyrie::Resource
   include ::Hyrax::BasicMetadata
 
   attr_accessor :payload_concern # a Class name: what is this a batch of?
-
-  # This mocks out the behavior of Hydra::PCDM::PcdmBehavior
-  def in_collection_ids
-    []
-  end
-
-  def create_or_update
-    raise "This is a read only record"
-  end
 end
