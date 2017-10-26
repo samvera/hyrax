2db84aaf0525253ea3910125e2cb3d818503c0d6
Justin Coyne <jcoyne@justincoyne.com>
2017-10-25 22:30:35 -0500

    Cast Valkyrie::ID to string for ActiveRecord queries

--- spec/actors/hyrax/actors/cleanup_trophies_actor_spec.rb
+++ spec/actors/hyrax/actors/cleanup_trophies_actor_spec.rb
@@ -19,7 +19,7 @@ RSpec.describe Hyrax::Actors::CleanupTrophiesActor do
     let!(:trophy) { Trophy.create(user_id: depositor.id, work_id: work.id) }
 
     it 'removes all the trophies' do
-      expect { middleware.destroy(env) }.to change { Trophy.where(work_id: work.id).count }.from(1).to(0)
+      expect { middleware.destroy(env) }.to change { Trophy.where(work_id: work.id.to_s).count }.from(1).to(0)
     end
   end
 end
