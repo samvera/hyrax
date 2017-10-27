ad7967581d2020964fce1ca58e400fec6b1b7a0c
Justin Coyne <jcoyne@justincoyne.com>
2017-10-27 10:27:25 -0500

    Make thumbnail_id optional
    The thumbnail path service was looking for nil but got an empty string
    instead.  Also updated DerivativePath to look for Valkyrie::ID

--- app/models/concerns/hyrax/collection_behavior.rb
+++ app/models/concerns/hyrax/collection_behavior.rb
@@ -13,9 +13,9 @@ module Hyrax
 
     included do
       # Points at an image file that displays this work.
-      attribute :thumbnail_id, Valkyrie::Types::SingleValuedString
+      attribute :thumbnail_id, Valkyrie::Types::SingleValuedString.optional
       # Points at a file that displays something about this work. Could be an image or a video.
-      attribute :representative_id, Valkyrie::Types::SingleValuedString
+      attribute :representative_id, Valkyrie::Types::SingleValuedString.optional
     end
 
     # Add members using the members association.
