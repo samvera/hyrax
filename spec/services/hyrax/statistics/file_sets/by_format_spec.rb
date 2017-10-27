a4877a19a1ff0fc994ba77b1ad28b8080a90dde4
Chris Colvard <cjcolvar@indiana.edu>
2017-10-27 12:26:07 -0400

    Use solr_index persister instead of update_index

--- spec/services/hyrax/statistics/file_sets/by_format_spec.rb
+++ spec/services/hyrax/statistics/file_sets/by_format_spec.rb
@@ -1,20 +1,9 @@
 RSpec.describe Hyrax::Statistics::FileSets::ByFormat, :clean_repo do
   describe ".query" do
-    let(:fs1) { build(:file_set, id: '1234567') }
-    let(:fs2) { build(:file_set, id: '2345678') }
-    let(:fs3) { build(:file_set, id: '3456789') }
-    let(:fs4) { build(:file_set, id: '4567890') }
-
-    before do
-      allow(fs1).to receive_messages(mime_type: 'text/plain', format_label: ["plain text"])
-      fs1.update_index
-      allow(fs2).to receive_messages(mime_type: 'image/jpg', format_label: ["JPEG image"])
-      fs2.update_index
-      allow(fs3).to receive_messages(mime_type: 'image/tiff', format_label: ["TIFF image"])
-      fs3.update_index
-      allow(fs4).to receive_messages(mime_type: 'image/jpg', format_label: ["JPEG image"])
-      fs4.update_index
-    end
+    let(:fs1) { create_for_repository(:file_set, mime_type: 'text/plain', format_label: ["plain text"]) }
+    let(:fs2) { create_for_repository(:file_set, mime_type: 'image/jpg', format_label: ["JPEG image"]) }
+    let(:fs3) { create_for_repository(:file_set, mime_type: 'image/tiff', format_label: ["TIFF image"]) }
+    let(:fs4) { create_for_repository(:file_set, mime_type: 'image/jpg', format_label: ["JPEG image"]) }
 
     subject { described_class.query }
 
