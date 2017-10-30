3e2fe7acd91f53b03a3112222b4c8bf1d8dd6240
Justin Coyne <jcoyne@justincoyne.com>
2017-10-30 12:38:42 -0500

    Works controller requires a change_set_persister
    Because the ResourceController delegates metadata_adapter to the
    change_set_persister.  This lead me to invert the dependency on
    Actors::Environment.

--- app/actors/hyrax/actors/environment.rb
+++ app/actors/hyrax/actors/environment.rb
@@ -1,20 +1,18 @@
 module Hyrax
   module Actors
     class Environment
-      # @param [Valkyrie::ChangeSet] curation_concern work to operate on
+      # @param [Valkyrie::ChangeSet] change_set work to operate on
+      # @param [Valkyrie::ChangeSetPersister] change_set_persister work to operate on
       # @param [Ability] current_ability the authorizations of the acting user
       # @param [ActionController::Parameters] attributes user provided form attributes
-      def initialize(change_set, current_ability, attributes)
+      def initialize(change_set, change_set_persister, current_ability, attributes)
         @change_set = change_set
         @current_ability = current_ability
         @attributes = attributes.to_h.with_indifferent_access
+        @change_set_persister = change_set_persister
       end
 
-      attr_reader :change_set, :current_ability, :attributes, :change_set
-
-      def change_set_persister
-        Hyrax::ChangeSetPersister.new(metadata_adapter: Valkyrie::MetadataAdapter.find(:indexing_persister), storage_adapter: Valkyrie.config.storage_adapter)
-      end
+      attr_reader :change_set, :current_ability, :attributes, :change_set_persister
 
       delegate :resource, to: :change_set
       alias curation_concern resource
--- app/actors/hyrax/actors/interpret_visibility_actor.rb
+++ app/actors/hyrax/actors/interpret_visibility_actor.rb
@@ -84,7 +84,7 @@ module Hyrax
       def create(env)
         intention = Intention.new(env.attributes)
         attributes = intention.sanitize_params
-        new_env = Environment.new(env.change_set, env.current_ability, attributes)
+        new_env = duplicate_env(env, attributes)
         validate(env, intention, attributes) && apply_visibility(new_env, intention) &&
           next_actor.create(new_env)
       end
@@ -94,12 +94,16 @@ module Hyrax
       def update(env)
         intention = Intention.new(env.attributes)
         attributes = intention.sanitize_params
-        new_env = Environment.new(env.change_set, env.current_ability, attributes)
+        new_env = duplicate_env(env, attributes)
         validate(env, intention, attributes) && apply_visibility(new_env, intention) &&
           next_actor.update(new_env)
       end
 
       private
+
+        def duplicate_env(env, attributes)
+          Environment.new(env.change_set, env.change_set_persister, env.current_ability, attributes)
+        end
 
         # Validate against selected AdminSet's PermissionTemplate (if any)
         def validate(env, intention, attributes)
