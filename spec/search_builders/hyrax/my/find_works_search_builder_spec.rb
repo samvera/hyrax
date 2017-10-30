50f25404694e2b14319d6b8ad11dbed483738ff3
Chris Colvard <cjcolvar@indiana.edu>
2017-10-30 16:32:17 -0400

    IDs need to be strings in some cases

--- spec/search_builders/hyrax/my/find_works_search_builder_spec.rb
+++ spec/search_builders/hyrax/my/find_works_search_builder_spec.rb
@@ -3,7 +3,7 @@ RSpec.describe Hyrax::My::FindWorksSearchBuilder do
   let(:user) { create(:user) }
   let(:ability) { instance_double(Ability, admin?: false, user_groups: [], current_user: user) }
   let(:q) { "foo" }
-  let(:params) { ActionController::Parameters.new(q: q, id: work.id, user: user.email, controller: "qa/terms", action: "search", vocab: "find_works") }
+  let(:params) { ActionController::Parameters.new(q: q, id: work.id.to_s, user: user.email, controller: "qa/terms", action: "search", vocab: "find_works") }
 
   let(:context) do
     double(current_ability: ability,
@@ -48,7 +48,7 @@ RSpec.describe Hyrax::My::FindWorksSearchBuilder do
 
     it "is successful" do
       subject
-      expect(solr_params[:fq]).to eq ["-" + ActiveFedora::SolrQueryBuilder.construct_query(member_ids_ssim: work.id)]
+      expect(solr_params[:fq]).to eq ["-" + ActiveFedora::SolrQueryBuilder.construct_query(member_ids_ssim: work.id.to_s)]
     end
   end
 
