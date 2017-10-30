2dd1529b4097285e898515e88fd67e1cdaeed151
Justin Coyne <jcoyne@justincoyne.com>
2017-10-30 12:38:42 -0500

    Works controller requires a change_set_persister
    Because the ResourceController delegates metadata_adapter to the
    change_set_persister.  This lead me to invert the dependency on
    Actors::Environment.

--- spec/actors/hyrax/actors/collections_membership_actor_spec.rb
+++ spec/actors/hyrax/actors/collections_membership_actor_spec.rb
@@ -5,7 +5,8 @@ RSpec.describe Hyrax::Actors::CollectionsMembershipActor do
   let(:attributes) { {} }
   let(:terminator) { Hyrax::Actors::Terminator.new }
   let(:change_set) { GenericWorkChangeSet.new(curation_concern) }
-  let(:env) { Hyrax::Actors::Environment.new(change_set, ability, attributes) }
+  let(:change_set_persister) { Hyrax::ChangeSetPersister.new(metadata_adapter: Valkyrie::MetadataAdapter.find(:indexing_persister), storage_adapter: Valkyrie.config.storage_adapter) }
+  let(:env) { Hyrax::Actors::Environment.new(change_set, change_set_persister, ability, attributes) }
 
   subject(:middleware) do
     stack = ActionDispatch::MiddlewareStack.new.tap do |middleware|
@@ -47,10 +48,12 @@ RSpec.describe Hyrax::Actors::CollectionsMembershipActor do
     describe "when work is in user's own collection" do
       let(:collection) { create_for_repository(:collection, user: user, title: ['A good title']) }
       let(:attributes) { { member_of_collection_ids: [] } }
+      let(:env_initial) do
+        Hyrax::Actors::Environment.new(change_set, change_set_persister, ability, member_of_collection_ids: [collection.id], title: ['test'])
+      end
 
       before do
-        subject.create(Hyrax::Actors::Environment.new(change_set, ability,
-                                                      member_of_collection_ids: [collection.id], title: ['test']))
+        subject.create(env_initial)
       end
 
       it "removes the work from that collection" do
