e669bce72665440719e97c3d9d04b5830bf0c868
Chris Colvard <cjcolvar@indiana.edu>
2017-11-01 14:29:54 -0400

    Use change set for persisting

--- app/change_sets/hyrax/file_set_change_set.rb
+++ app/change_sets/hyrax/file_set_change_set.rb
@@ -1,2 +1,8 @@
-class Hyrax::FileSetChangeSet < Valkyrie::ChangeSet
+module Hyrax
+  class FileSetChangeSet < Valkyrie::ChangeSet
+    property :read_users, multiple: true, required: false
+    property :read_groups, multiple: true, required: false
+    property :edit_users, multiple: true, required: false
+    property :edit_groups, multiple: true, required: false
+  end
 end
