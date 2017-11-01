6bffe71c245b5fdece2454843fd2fb10ce0cddf2
Justin Coyne <jcoyne@justincoyne.com>
2017-11-01 12:03:26 -0500

    Update the FileSetActor to use Valkyrie

--- app/actors/hyrax/actors/file_set_actor.rb
+++ app/actors/hyrax/actors/file_set_actor.rb
@@ -77,11 +77,12 @@ module Hyrax
       def attach_to_work(work, file_set_params = {})
         acquire_lock_for(work.id) do
           # Ensure we have an up-to-date copy of the members association, so that we append to the end of the list.
-          work.reload if work.persisted?
-          file_set.visibility = work.visibility unless assign_visibility?(file_set_params)
+          work = Hyrax::Queries.find_by(id: work.id) if work.persisted?
+          copy_visibility(work.visibility, file_set_params)
+
           work.member_ids += [file_set.id]
-          work.representative = file_set if work.representative_id.blank?
-          work.thumbnail = file_set if work.thumbnail_id.blank?
+          work.representative_id = file_set.id if work.representative_id.blank?
+          work.thumbnail_id = file_set.id if work.thumbnail_id.blank?
           # Save the work so the association between the work and the file_set is persisted (head_id)
           # NOTE: the work may not be valid, in which case this save doesn't do anything.
           persister.save(resource: work)
@@ -107,7 +108,7 @@ module Hyrax
 
       def destroy
         unlink_from_work
-        file_set.destroy
+        persister.delete(resource: file_set)
         Hyrax.config.callback.run(:after_destroy, file_set.id, user)
       end
 
@@ -115,6 +116,14 @@ module Hyrax
       self.file_actor_class = Hyrax::Actors::FileActor
 
       private
+
+        # Copy visibility to the file set and save it unless it would get set by
+        # the form parameters
+        def copy_visibility(visibility, file_set_params)
+          return if assign_visibility?(file_set_params)
+          file_set.visibility = visibility
+          persister.save(resource: file_set)
+        end
 
         def persister
           Valkyrie::MetadataAdapter.find(:indexing_persister).persister
@@ -157,12 +166,11 @@ module Hyrax
         end
 
         def unlink_from_work
-          work = file_set.parent
-          return unless work && (work.thumbnail_id == file_set.id || work.representative_id == file_set.id)
-          # Must clear the thumbnail_id and representative_id fields on the work and force it to be re-solrized.
-          # Although ActiveFedora clears the children nodes it leaves those fields in Solr populated.
-          work.thumbnail = nil if work.thumbnail_id == file_set.id
-          work.representative = nil if work.representative_id == file_set.id
+          work = Hyrax::Queries.find_parents(resource: file_set).first
+          return unless work
+          work.thumbnail_id = nil if work.thumbnail_id == file_set.id
+          work.representative_id = nil if work.representative_id == file_set.id
+          work.member_ids.delete(file_set.id)
           persister.save(resource: work)
         end
     end
