9919175bada4353057a13155bf33c63152a187e4
Blanco <blancoj@umich.edu>
2017-11-01 11:04:16 -0400

    remove permission_template and permission_template_access entries when admin_set is deleted.

--- spec/views/hyrax/admin/admin_sets/_form_participant_table.html.erb_spec.rb
+++ spec/views/hyrax/admin/admin_sets/_form_participant_table.html.erb_spec.rb
@@ -1,12 +1,13 @@
 RSpec.describe 'hyrax/admin/admin_sets/_form_participant_table.html.erb', type: :view do
   let(:template) { stub_model(Hyrax::PermissionTemplate) }
+  let(:admin) { create(:admin) }
   let(:user) { create(:user) }
-  let(:access_grant) { stub_model(Hyrax::PermissionTemplateAccess) }
+  let(:access_grants) { [stub_model(Hyrax::PermissionTemplateAccess)] }
   let(:pt_form) do
     instance_double(Hyrax::Forms::PermissionTemplateForm,
                     model_name: template.model_name,
                     to_key: template.to_key,
-                    access_grants: [access_grant])
+                    access_grants: access_grants)
   end
 
   before do
@@ -23,20 +24,27 @@ RSpec.describe 'hyrax/admin/admin_sets/_form_participant_table.html.erb', type: 
     end
 
     context "managers exist" do
-      let(:access_grant) do
-        stub_model(Hyrax::PermissionTemplateAccess,
-                   agent_type: 'user',
-                   agent_id: user.user_key,
-                   access: 'manage')
+      let(:access_grants) do
+        [stub_model(Hyrax::PermissionTemplateAccess,
+                    agent_type: 'user',
+                    agent_id: user.user_key,
+                    access: 'manage'),
+         stub_model(Hyrax::PermissionTemplateAccess,
+                    agent_type: 'group',
+                    agent_id: Ability.admin_group_name,
+                    access: 'manage')]
       end
 
       it "lists the managers in the table" do
         expect(rendered).to have_selector("h3", text: "Managers")
         expect(rendered).to have_selector("table tbody", text: user.user_key)
+        expect(rendered).to have_selector("table tbody", text: 'Repository Administrators')
+        expect(rendered).to have_button(class: 'btn-danger', disabled: true,
+                                        title: 'The repository administrators group cannot be removed')
       end
     end
     context "no managers exist" do
-      let(:access_grant) { stub_model(Hyrax::PermissionTemplateAccess) }
+      let(:access_grants) { [stub_model(Hyrax::PermissionTemplateAccess)] }
 
       it "displays a message and no table" do
         expect(rendered).to have_selector("h3", text: "Managers")
@@ -52,20 +60,27 @@ RSpec.describe 'hyrax/admin/admin_sets/_form_participant_table.html.erb', type: 
     end
 
     context "viewers exist" do
-      let(:access_grant) do
-        stub_model(Hyrax::PermissionTemplateAccess,
-                   agent_type: 'user',
-                   agent_id: user.user_key,
-                   access: 'view')
+      let(:access_grants) do
+        [stub_model(Hyrax::PermissionTemplateAccess,
+                    agent_type: 'user',
+                    agent_id: user.user_key,
+                    access: 'view'),
+         stub_model(Hyrax::PermissionTemplateAccess,
+                    agent_type: 'group',
+                    agent_id: Ability.admin_group_name,
+                    access: 'view')]
       end
 
       it "lists the viewers in the table" do
         expect(rendered).to have_selector("h3", text: "Viewers")
         expect(rendered).to have_selector("table tbody", text: user.user_key)
+        expect(rendered).to have_selector("table tbody", text: 'Repository Administrators')
+        expect(rendered).not_to have_button(class: 'btn-danger', disabled: true,
+                                            title: 'The repository administrators group cannot be removed')
       end
     end
     context "no viewers exist" do
-      let(:access_grant) { stub_model(Hyrax::PermissionTemplateAccess) }
+      let(:access_grants) { [stub_model(Hyrax::PermissionTemplateAccess)] }
 
       it "displays a message and no table" do
         expect(rendered).to have_selector("h3", text: "Viewers")
@@ -81,20 +96,27 @@ RSpec.describe 'hyrax/admin/admin_sets/_form_participant_table.html.erb', type: 
     end
 
     context "depositors exist" do
-      let(:access_grant) do
-        stub_model(Hyrax::PermissionTemplateAccess,
-                   agent_type: 'user',
-                   agent_id: user.user_key,
-                   access: 'deposit')
+      let(:access_grants) do
+        [stub_model(Hyrax::PermissionTemplateAccess,
+                    agent_type: 'user',
+                    agent_id: user.user_key,
+                    access: 'deposit'),
+         stub_model(Hyrax::PermissionTemplateAccess,
+                    agent_type: 'group',
+                    agent_id: Ability.admin_group_name,
+                    access: 'deposit')]
       end
 
       it "lists the depositors in the table" do
         expect(rendered).to have_selector("h3", text: "Depositors")
         expect(rendered).to have_selector("table tbody", text: user.user_key)
+        expect(rendered).to have_selector("table tbody", text: 'Repository Administrators')
+        expect(rendered).not_to have_button(class: 'btn-danger', disabled: true,
+                                            title: 'The repository administrators group cannot be removed')
       end
     end
     context "no depositors exist" do
-      let(:access_grant) { stub_model(Hyrax::PermissionTemplateAccess) }
+      let(:access_grants) { [stub_model(Hyrax::PermissionTemplateAccess)] }
 
       it "displays a message and no table" do
         expect(rendered).to have_selector("h3", text: "Depositors")
