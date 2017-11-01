5a9ba49b09607e308e8bcd6d36f723b1a2017a82
Justin Coyne <jcoyne@justincoyne.com>
2017-11-01 12:03:26 -0500

    Update the FileSetActor to use Valkyrie

--- app/services/hyrax/file_node_builder.rb
+++ app/services/hyrax/file_node_builder.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Hyrax
-  # Stores a file and creates an associated FileNode
+  # Stores a file and an associated FileNode
   class FileNodeBuilder
     attr_reader :storage_adapter, :persister
     def initialize(storage_adapter:, persister:)
@@ -10,13 +10,16 @@ module Hyrax
     end
 
     # @param file [ActionDispatch::Http::UploadedFile]
+    # @param node [FileNode] the metadata to represent the file
     # @return [FileNode] the persisted metadata node that represents the file
-    def create(file:)
-      node = persister.save(resource: FileNode.for(file: file))
-      stored_file = storage_adapter.upload(file: file, resource: node)
-      node.file_identifiers = node.file_identifiers + [stored_file.id]
-      node = Valkyrie::FileCharacterizationService.for(file_node: node, persister: persister).characterize(save: false)
-      persister.save(resource: node)
+    def create(file:, node:)
+      saved_node = persister.save(resource: node)
+      stored_file = storage_adapter.upload(file: file,
+                                           original_filename: node.original_filename.first,
+                                           resource: saved_node)
+      saved_node.file_identifiers = saved_node.file_identifiers + [stored_file.id]
+      saved_node = Valkyrie::FileCharacterizationService.for(file_node: node, persister: persister).characterize(save: false)
+      persister.save(resource: saved_node)
     end
   end
 end
