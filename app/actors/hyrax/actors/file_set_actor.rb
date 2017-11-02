3fd7ea847b188ef054826c91a8423897ede6ddac
Justin Coyne <jcoyne@justincoyne.com>
2017-11-01 22:54:53 -0500

    Update the file_set_controller to use ChangeSets

--- app/actors/hyrax/actors/file_set_actor.rb
+++ app/actors/hyrax/actors/file_set_actor.rb
@@ -1,4 +1,3 @@
-# rubocop:disable Metrics/ClassLength
 module Hyrax
   module Actors
     # Actions are decoupled from controller logic so that they may be called from a controller or a background job.
@@ -18,38 +17,30 @@ module Hyrax
       # @param [Hyrax::UploadedFile, File, ActionDigest::HTTP::UploadedFile] file the file uploaded by the user
       # @param [Symbol, #to_s] relation
       # @return [IngestJob, FalseClass] false on failure, otherwise the queued job
-      def create_content(file, relation = :original_file, from_url: false)
-        # If the file set doesn't have a title or label assigned, set a default.
-        file_set.label ||= label_for(file)
-        file_set.title = [file_set.label] if file_set.title.blank?
-
-        persister = Valkyrie::MetadataAdapter.find(:indexing_persister).persister
-        # Need to save to get an id
-        saved_file = persister.save(resource: file_set)
-        return unless saved_file
-
-        if from_url
-          # If ingesting from URL, don't spawn an IngestJob; instead
-          # reach into the FileActor and run the ingest with the file instance in
-          # hand. Do this because we don't have the underlying UploadedFile instance
-          file_actor = build_file_actor(relation)
-          file_actor.ingest_file(wrapper!(file: file, relation: relation))
-          # Copy visibility and permissions from parent (work) to
-          # FileSets even if they come in from BrowseEverything
-          VisibilityCopyJob.perform_later(saved_file.parent)
-          InheritPermissionsJob.perform_later(saved_file.parent)
-        else
-          IngestJob.perform_later(wrapper!(file: file, relation: relation))
-        end
-      end
-
-      # Spawns asynchronous IngestJob with user notification afterward
-      # @param [Hyrax::UploadedFile, File, ActionDigest::HTTP::UploadedFile] file the file uploaded by the user
-      # @param [Symbol, #to_s] relation
-      # @return [IngestJob] the queued job
-      def update_content(file, relation = :original_file)
-        IngestJob.perform_later(wrapper!(file: file, relation: relation), notification: true)
-      end
+      # def create_content(file, relation = :original_file, from_url: false)
+      #   # If the file set doesn't have a title or label assigned, set a default.
+      #   file_set.label ||= label_for(file)
+      #   file_set.title = [file_set.label] if file_set.title.blank?
+      #
+      #   persister = Valkyrie::MetadataAdapter.find(:indexing_persister).persister
+      #   # Need to save to get an id
+      #   saved_file = persister.save(resource: file_set)
+      #   return unless saved_file
+      #
+      #   if from_url
+      #     # If ingesting from URL, don't spawn an IngestJob; instead
+      #     # reach into the FileActor and run the ingest with the file instance in
+      #     # hand. Do this because we don't have the underlying UploadedFile instance
+      #     file_actor = build_file_actor(relation)
+      #     file_actor.ingest_file(wrapper!(file: file, relation: relation))
+      #     # Copy visibility and permissions from parent (work) to
+      #     # FileSets even if they come in from BrowseEverything
+      #     VisibilityCopyJob.perform_later(saved_file.parent)
+      #     InheritPermissionsJob.perform_later(saved_file.parent)
+      #   else
+      #     IngestJob.perform_later(wrapper!(file: file, relation: relation))
+      #   end
+      # end
 
       # @!endgroup
 
@@ -59,18 +50,18 @@ module Hyrax
       #   we have to save both the parent work and the file_set in order to record the "metadata" relationship between them.
       # @param [Hash] file_set_params specifying the visibility, lease and/or embargo of the file set.
       #   Without visibility, embargo_release_date or lease_expiration_date, visibility will be copied from the parent.
-      def create_metadata(file_set_params = {})
-        file_set.apply_depositor_metadata(user)
-        now = TimeService.time_in_utc
-        file_set.date_uploaded = now
-        file_set.date_modified = now
-        file_set.creator = [user.user_key]
-        if assign_visibility?(file_set_params)
-          env = Actors::Environment.new(file_set, nil, ability, file_set_params)
-          CurationConcern.file_set_create_actor.create(env)
-        end
-        yield(file_set) if block_given?
-      end
+      # def create_metadata(file_set_params = {})
+      #   file_set.apply_depositor_metadata(user)
+      #   now = TimeService.time_in_utc
+      #   file_set.date_uploaded = now
+      #   file_set.date_modified = now
+      #   file_set.creator = [user.user_key]
+      #   if assign_visibility?(file_set_params)
+      #     env = Actors::Environment.new(file_set, nil, ability, file_set_params)
+      #     CurationConcern.file_set_create_actor.create(env)
+      #   end
+      #   yield(file_set) if block_given?
+      # end
 
       # Adds a FileSet to the work using ore:Aggregations.
       # Locks to ensure that only one process is operating on the list at a time.
@@ -92,29 +83,6 @@ module Hyrax
       alias attach_file_to_work attach_to_work
       deprecation_deprecate attach_file_to_work: "use attach_to_work instead"
 
-      # @param [String] revision_id the revision to revert to
-      # @param [Symbol, #to_sym] relation
-      # @return [Boolean] true on success, false otherwise
-      def revert_content(revision_id, relation = :original_file)
-        return false unless build_file_actor(relation).revert_to(revision_id)
-        Hyrax.config.callback.run(:after_revert_content, file_set, user, revision_id)
-        true
-      end
-
-      def update_metadata(attributes)
-        env = Actors::Environment.new(file_set, nil, ability, attributes)
-        CurationConcern.file_set_update_actor.update(env)
-      end
-
-      def destroy
-        unlink_from_work
-        persister.delete(resource: file_set)
-        Hyrax.config.callback.run(:after_destroy, file_set.id, user)
-      end
-
-      class_attribute :file_actor_class
-      self.file_actor_class = Hyrax::Actors::FileActor
-
       private
 
         # Copy visibility to the file set and save it unless it would get set by
@@ -131,10 +99,6 @@ module Hyrax
 
         def ability
           @ability ||= ::Ability.new(user)
-        end
-
-        def build_file_actor(relation)
-          file_actor_class.new(file_set, relation, user)
         end
 
         # uses create! because object must be persisted to serialize for jobs
--- app/controllers/hyrax/file_sets_controller.rb
+++ app/controllers/hyrax/file_sets_controller.rb
@@ -3,11 +3,10 @@ module Hyrax
     include Blacklight::Base
     include Blacklight::AccessControls::Catalog
     include Hyrax::Breadcrumbs
+    include Hyrax::ResourceController
 
     before_action :authenticate_user!, except: [:show, :citation, :stats]
-    load_and_authorize_resource class: ::FileSet, except: :show
     before_action :build_breadcrumbs, only: [:show, :edit, :stats]
-
     # provides the help_text view method
     helper PermissionsHelper
 
@@ -17,7 +16,11 @@ module Hyrax
 
     class_attribute :show_presenter, :change_set_class
     self.show_presenter = Hyrax::FileSetPresenter
-    self.change_set_class = Hyrax::Forms::FileSetEditForm
+    self.resource_class = ::FileSet
+    self.change_set_persister = Hyrax::FileSetChangeSetPersister.new(
+      metadata_adapter: Valkyrie::MetadataAdapter.find(:indexing_persister),
+      storage_adapter: Valkyrie.config.storage_adapter
+    )
 
     # A little bit of explanation, CanCan(Can) sets the @file_set via the .load_and_authorize_resource
     # method. However the interface for various CurationConcern modules leverages the #curation_concern method
@@ -29,27 +32,6 @@ module Hyrax
     private :curation_concern=
     helper_method :file_set
 
-    # routed to /files/new
-    def new; end
-
-    # routed to /files/:id/edit
-    def edit
-      initialize_edit_form
-    end
-
-    # routed to /files (POST)
-    def create
-      file = params.fetch(:file_set, {}).fetch(:files, []).detect { |f| f.respond_to?(:original_filename) }
-      return render_json_response(response_type: :bad_request, options: { message: 'Error! No file uploaded', description: 'missing file' }) unless file
-      return empty_file_response(file) if empty_file?(file)
-      process_non_empty_file(file: file)
-    rescue RSolr::Error::Http => error
-      logger.error "FileSetController::create rescued #{error.class}\n\t#{error}\n #{error.backtrace.join("\n")}\n\n"
-      render_json_response(response_type: :internal_error, options: { message: 'Error occurred while creating a FileSet.' })
-    ensure
-      file.tempfile.delete if file.respond_to?(:tempfile) # remove tempfile (only if it is a temp file)
-    end
-
     # routed to /files/:id
     def show
       respond_to do |wants|
@@ -59,91 +41,83 @@ module Hyrax
       end
     end
 
-    def destroy
-      parent = curation_concern.parent
-      actor.destroy
+    def after_delete_success(_change_set, parent)
       redirect_to [main_app, parent], notice: 'The file has been deleted.'
     end
 
-    # routed to /files/:id (PUT)
-    def update
-      if attempt_update
-        after_update_response
-      else
-        after_update_failure_response
+    def destroy
+      @change_set = build_change_set(find_resource(params[:id]))
+      parent = Hyrax::Queries.find_parents(resource: @change_set.resource).first
+      authorize! :destroy, @change_set.resource
+      change_set_persister.buffer_into_index do |persist|
+        persist.delete(change_set: @change_set)
       end
-    rescue RSolr::Error::Http => error
-      flash[:error] = error.message
-      logger.error "FileSetsController::update rescued #{error.class}\n\t#{error.message}\n #{error.backtrace.join("\n")}\n\n"
-      render action: 'edit'
+      after_delete_success(@change_set, parent)
     end
 
     # routed to /files/:id/stats
     def stats
+      authorize! :stats, find_resource(params[:id])
       @stats = FileUsage.new(params[:id])
     end
 
     # routed to /files/:id/citation
-    def citation; end
+    def citation
+      authorize! :citation, find_resource(params[:id])
+    end
 
     private
 
-      def process_non_empty_file(file:)
-        # Relative path is set by the jquery uploader when uploading a directory
-        curation_concern.relative_path = params[:relative_path] if params[:relative_path]
-        actor.create_metadata(params[:file_set])
-        actor.attach_to_work(find_parent_by_id)
-        if actor.create_content(file)
-          response_for_successfully_processed_file
+      # Returns a FileUploadChangeSet if they have created or updated a file
+      def change_set_class
+        #   curation_concern.relative_path = params[:relative_path] if params[:relative_path]
+        if action_name == 'create' || (action_name == 'update' && params[:file_set] && params[:file_set].key?(:files))
+          FileUploadChangeSet
+        elsif wants_to_revert?
+          RevertFileChangeSet
         else
-          msg = curation_concern.errors.full_messages.join(', ')
-          flash[:error] = msg
-          json_error "Error creating file #{file.original_filename}: #{msg}"
+          Hyrax::FileSetChangeSet
         end
       end
 
-      def empty_file_response(file)
-        options = {
-          errors: { files: "#{file.original_filename} has no content! (Zero length file)" },
-          description: t('hyrax.api.unprocessable_entity.empty_file')
-        }
-        render_json_response(response_type: :unprocessable_entity, options: options)
+      def change_set_persister
+        if change_set_class == RevertFileChangeSet
+          return Hyrax::RevertFileChangeSetPersister.new(
+            metadata_adapter: Valkyrie::MetadataAdapter.find(:indexing_persister),
+            storage_adapter: Valkyrie.config.storage_adapter
+          )
+        end
+        self.class.change_set_persister
       end
 
-      def response_for_successfully_processed_file
+      def after_create_error(_obj, change_set)
+        if change_set.has_file?
+          render_json_response(response_type: :unprocessable_entity,
+                               options: { errors: change_set.errors.to_h,
+                                          description: t('hyrax.api.unprocessable_entity.empty_file') })
+        else
+          render_json_response(response_type: :bad_request,
+                               options: { message: change_set.errors[:files].first,
+                                          description: 'missing file' })
+        end
+      end
+
+      def after_create_success(resource, _change_set)
         respond_to do |format|
           format.html do
             if request.xhr?
               render 'jq_upload', formats: 'json', content_type: 'text/html'
             else
-              redirect_to [main_app, curation_concern.parent]
+              redirect_to [main_app, resource.parent]
             end
           end
           format.json do
-            render 'jq_upload', status: :created, location: polymorphic_path([main_app, curation_concern])
+            render 'jq_upload', status: :created, location: polymorphic_path([main_app, resource])
           end
         end
       end
 
-      # this is provided so that implementing application can override this behavior and map params to different attributes
-      def update_metadata
-        file_attributes = change_set_class.model_attributes(attributes)
-        actor.update_metadata(file_attributes)
-      end
-
-      def attempt_update
-        if wants_to_revert?
-          actor.revert_content(params[:revision])
-        elsif params.key?(:file_set)
-          if params[:file_set].key?(:files)
-            actor.update_content(params[:file_set][:files].first)
-          else
-            update_metadata
-          end
-        end
-      end
-
-      def after_update_response
+      def after_update_success(curation_concern, _change_set)
         respond_to do |wants|
           wants.html do
             redirect_to [main_app, curation_concern], notice: "The file #{view_context.link_to(curation_concern, [main_app, curation_concern])} has been updated."
@@ -155,10 +129,9 @@ module Hyrax
         end
       end
 
-      def after_update_failure_response
+      def after_update_error(curation_concern, _change_set)
         respond_to do |wants|
           wants.html do
-            initialize_edit_form
             flash[:error] = "There was a problem processing your request."
             render 'edit', status: :unprocessable_entity
           end
@@ -185,20 +158,6 @@ module Hyrax
         Hyrax::FileSetSearchBuilder
       end
 
-      def initialize_edit_form
-        original = @file_set.original_file
-        @version_list = Hyrax::VersionListPresenter.new(original ? original.versions.all : [])
-        @groups = current_user.groups
-      end
-
-      def actor
-        @actor ||= Hyrax::Actors::FileSetActor.new(@file_set, current_user)
-      end
-
-      def attributes
-        params.fetch(:file_set, {}).except(:files).permit!.dup # use a copy of the hash so that original params stays untouched when interpret_visibility modifies things
-      end
-
       def presenter
         @presenter ||= begin
           _, document_list = search_results(params)
@@ -219,10 +178,6 @@ module Hyrax
         params.require(:file_set).permit(
           :visibility_during_embargo, :embargo_release_date, :visibility_after_embargo, :visibility_during_lease, :lease_expiration_date, :visibility_after_lease, :visibility, title: []
         )
-      end
-
-      def empty_file?(file)
-        (file.respond_to?(:tempfile) && file.tempfile.size == 0) || (file.respond_to?(:size) && file.size == 0)
       end
 
       # This allows us to use the unauthorized and form_permission template in hyrax/base,
