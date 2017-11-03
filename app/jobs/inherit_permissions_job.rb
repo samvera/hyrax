6ef35474a9ec34849c920c30459a9ca75c881610
Chris Colvard <cjcolvar@indiana.edu>
2017-11-03 10:14:06 -0400

    Use FileSetChangeSets in jobs

--- app/jobs/inherit_permissions_job.rb
+++ app/jobs/inherit_permissions_job.rb
@@ -6,23 +6,32 @@ class InheritPermissionsJob < Hyrax::ApplicationJob
   # @param work containing access level and filesets
   def perform(work)
     file_sets = Hyrax::Queries.find_members(resource: work, model: ::FileSet)
-    file_sets.each do |file|
-      file_change_set = Hyrax::FileSetChangeSet.new(file)
-      file_change_set.read_users = work.read_users
-      file_change_set.read_groups = work.read_groups
-      file_change_set.edit_users = work.edit_users
-      file_change_set.edit_groups = work.edit_groups
-      file_change_set.sync
-      change_set_persister.save(change_set: file_change_set)
+    change_set_persister.buffer_into_index do |persister|
+      file_sets.each do |file_set|
+        change_set = Hyrax::FileSetChangeSet.new(file_set)
+        params = { read_users: work.read_users, read_groups: work.read_groups,
+                   edit_users: work.edit_users, edit_groups: work.edit_groups,
+                   search_context: SearchContext.new(::User.find_by_user_key(file_set.depositor)) }
+        raise "Unable to update file set. #{change_set.errors.messages}" unless change_set.validate(params)
+        change_set.sync
+        persister.save(change_set: change_set)
+      end
     end
   end
 
   private
 
     def change_set_persister
-      Hyrax::ChangeSetPersister.new(
+      Hyrax::FileSetChangeSetPersister.new(
         metadata_adapter: Valkyrie::MetadataAdapter.find(:indexing_persister),
         storage_adapter: Valkyrie.config.storage_adapter
       )
+    end
+
+    class SearchContext
+      def initialize(user = nil)
+        @user = user
+      end
+      attr_reader :user
     end
 end
