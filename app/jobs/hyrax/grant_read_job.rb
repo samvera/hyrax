341375f782db4814794cfd8bf9f86b49936ba79c
Chris Colvard <cjcolvar@indiana.edu>
2017-11-03 10:14:06 -0400

    Use FileSetChangeSets in jobs

--- app/jobs/hyrax/grant_read_job.rb
+++ app/jobs/hyrax/grant_read_job.rb
@@ -3,12 +3,32 @@ module Hyrax
   class GrantReadJob < ApplicationJob
     queue_as Hyrax.config.ingest_queue_name
 
-    # @param [String] file_set_id - the identifier of the object to grant access to
+    # @param [Valkyrie::ID] file_set_id - the identifier of the object to grant access to
     # @param [String] user_key - the user to add
     def perform(file_set_id, user_key)
-      file_set = Queries.find_by(id: file_set_id)
-      file_set.read_users += [user_key]
-      persister.save(resource: file_set)
+      file_set = Hyrax::Queries.find_file_set(id: file_set_id)
+      change_set = Hyrax::FileSetChangeSet.new(file_set)
+      params = { read_users: (file_set.read_users + [user_key]),
+                 search_context: SearchContext.new(::User.find_by_user_key(file_set.depositor)) }
+      raise "Unable to update file set. #{file_change_set.errors.messages}" unless change_set.validate(params)
+      change_set.sync
+      change_set_persister.save(change_set: change_set)
     end
+
+    private
+
+      def change_set_persister
+        Hyrax::FileSetChangeSetPersister.new(
+          metadata_adapter: Valkyrie::MetadataAdapter.find(:indexing_persister),
+          storage_adapter: Valkyrie.config.storage_adapter
+        )
+      end
+
+      class SearchContext
+        def initialize(user = nil)
+          @user = user
+        end
+        attr_reader :user
+      end
   end
 end
