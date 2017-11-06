407a00be8f8bdeeccac81e1059d5419850ae0aa3
Chris Colvard <cjcolvar@indiana.edu>
2017-11-06 17:41:38 -0500

    Use SearchBuilders instead of relations

--- app/search_builders/hyrax/exposed_models_relation.rb
+++
@@ -1,8 +0,0 @@
-module Hyrax
-  # A relation that scopes to all user visible models (e.g. works + collections + file sets)
-  class ExposedModelsRelation < AbstractTypeRelation
-    def allowable_types
-      Hyrax.config.curation_concerns + [Collection, ::FileSet]
-    end
-  end
-end
