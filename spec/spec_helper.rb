92bc905c2a5f99e67b8699da036f08bbf6d816ab
James R. Griffin III <jrgriffiniii@gmail.com>
2017-11-06 16:56:45 -0600

    Extending ImportUrlJob#copy_remote_file and CreateWithRemoteFilesActor#create_file_from_url in order to handle the auth. header exposed by the Google APIs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -72,6 +72,7 @@ Capybara.register_driver :selenium_chrome_headless_sandboxless do |app|
   browser_options.args << '--headless'
   browser_options.args << '--disable-gpu'
   browser_options.args << '--no-sandbox'
+  browser_options.args << '--window-size=1920, 1080'
   Capybara::Selenium::Driver.new(app, browser: :chrome, options: browser_options)
 end
 
