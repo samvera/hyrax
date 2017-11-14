8ec62c27c42bd9c39f556314df957060e314fb83
Julie Allinson <julie.allinson@london.ac.uk>
2017-11-14 16:17:41 +0000

    refactor controlled_vocabulary_input in line with linked_data_resources

--- spec/inputs/linked_data_resource_input_spec.rb
+++ spec/inputs/linked_data_resource_input_spec.rb
@@ -1,12 +1,12 @@
-RSpec.describe 'ControlledVocabularyInput', type: :input do
+RSpec.describe 'LinkedDataResourceInput', type: :input do
   let(:work) { GenericWork.new }
   let(:builder) { SimpleForm::FormBuilder.new(:generic_work, work, view, {}) }
-  let(:input) { ControlledVocabularyInput.new(builder, :based_near, nil, :multi_value, {}) }
+  let(:input) { LinkedDataResourceInput.new(builder, :based_near, nil, :multi_value, {}) }
 
   describe '#input' do
     before { allow(work).to receive(:[]).with(:based_near).and_return([item1, item2]) }
-    let(:item1) { double('value 1', rdf_label: ['Item 1'], rdf_subject: 'http://example.org/1', node?: false) }
-    let(:item2) { double('value 2', rdf_label: ['Item 2'], rdf_subject: 'http://example.org/2') }
+    let(:item1) { 'http://example.org/1' }
+    let(:item2) { 'http://example.org/2' }
 
     it 'renders multi-value' do
       expect(input).to receive(:build_field).with(item1, 0)
@@ -26,12 +26,13 @@ RSpec.describe 'ControlledVocabularyInput', type: :input do
   describe '#build_field' do
     subject { input.send(:build_field, value, 0) }
 
-    context 'for a resource' do
-      let(:value) { double('value 1', rdf_label: ['Item 1'], rdf_subject: 'http://example.org/1', node?: false) }
+    context 'with a resource' do
+      let(:value) { 'http://example.org/1' }
 
       it 'renders multi-value' do
         expect(subject).to have_selector('input.generic_work_based_near.multi_value')
-        expect(subject).to have_field('generic_work[based_near_attributes][0][hidden_label]', with: 'Item 1')
+        # without fetching_external (resource intensive) or retrieving existing labels via solr (not yet implemented) the label will be the rdf_subject
+        expect(subject).to have_field('generic_work[based_near_attributes][0][hidden_label]', with: 'http://example.org/1')
         expect(subject).to have_selector('input[name="generic_work[based_near_attributes][0][id]"][value="http://example.org/1"]', visible: false)
         expect(subject).to have_selector('input[name="generic_work[based_near_attributes][0][_destroy]"][value=""][data-destroy]', visible: false)
       end
