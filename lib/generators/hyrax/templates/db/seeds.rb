b50dad111cc376e7e7120a51c73924a9dcb4d74d
E. Lynette Rayle <elr37@cornell.edu>
2017-11-27 09:37:47 -0500

    use same process UI does to create objects
    * updated all objects used for QA testing
    * updated all objects used with nested collection ad hoc testing
    * DID NOT update objects used for work embargo ad hoc testing

--- lib/generators/hyrax/templates/db/seeds.rb
+++ lib/generators/hyrax/templates/db/seeds.rb
@@ -5,93 +5,144 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+#
 # To use this file, run the following command in the .internal_test_app:
 #   rails generate hyrax:sample_data
+#
+# To re-use this file, you will likely want to clean out the test app content
+#   rails console
+#     require 'active_fedora/cleaner'
+#     ActiveFedora::Cleaner.clean!
+#     exit
+#   rake db:drop db:create db:migrate
+#   bin/rails hyrax:default_admin_set:create
+#   rake db:seed
 
-puts "Creating users"
-User.create(email: 'mgr1@example.com', password: 'pppppp') # 6*p
-User.create(email: 'mgr2@example.com', password: 'pppppp')
-User.create(email: 'dep1@example.com', password: 'pppppp')
-User.create(email: 'dep2@example.com', password: 'pppppp')
-User.create(email: 'vw1@example.com', password: 'pppppp')
-User.create(email: 'vw2@example.com', password: 'pppppp')
-user = User.create(email: 'foo@example.com', password: 'foobarbaz')
-
-puts "Creating 'Nestable Collection' type"
-options = {
-  description: 'Sample collection type that allows nesting of collections.',
-  nestable: true, discoverable: true, sharable: true, allow_multiple_membership: true,
-  require_membership: false, assigns_workflow: false, assigns_visibility: false,
-  participants: [{ agent_type: Hyrax::CollectionTypeParticipant::GROUP_TYPE, agent_id: ::Ability.admin_group_name, access: Hyrax::CollectionTypeParticipant::MANAGE_ACCESS },
-                 { agent_type: Hyrax::CollectionTypeParticipant::GROUP_TYPE, agent_id: ::Ability.registered_group_name, access: Hyrax::CollectionTypeParticipant::CREATE_ACCESS }]
-}
-begin
-  coltype = Hyrax::CollectionTypes::CreateService.create_collection_type(machine_id: "nestable_collection", title: "Nestable Collection", options: options)
-rescue
-  puts "  failed to create... looking for existing"
-  coltype = Hyrax::CollectionType.find_by_machine_id!("nestable_collection")
-end
-nestable_gid = coltype.gid
-
-puts "Creating 'Non-Nestable Collection' type"
-options = {
-  description: 'Sample collection type that DOES NOT allow nesting of collections.',
-  nestable: false, discoverable: true, sharable: true, allow_multiple_membership: true,
-  require_membership: false, assigns_workflow: false, assigns_visibility: false,
-  participants: [{ agent_type: Hyrax::CollectionTypeParticipant::GROUP_TYPE, agent_id: ::Ability.admin_group_name, access: Hyrax::CollectionTypeParticipant::MANAGE_ACCESS },
-                 { agent_type: Hyrax::CollectionTypeParticipant::GROUP_TYPE, agent_id: ::Ability.registered_group_name, access: Hyrax::CollectionTypeParticipant::CREATE_ACCESS }]
-}
-begin
-  coltype = Hyrax::CollectionTypes::CreateService.create_collection_type(machine_id: "nonnestable_collection", title: "Non-Nestable Collection", options: options)
-rescue
-  puts "  failed to create... looking for existing"
-  coltype = Hyrax::CollectionType.find_by_machine_id!("nonnestable_collection")
-end
-_nonnestable_gid = coltype.gid
-
-puts "Creating collections for nesting"
-# Public User Collection
-pnc = Collection.create!(title: ["Public Nestable Collection"], read_groups: ['public'], collection_type_gid: nestable_gid) do |col|
-  col.apply_depositor_metadata(user)
+# ---------------------------------
+# methods to create various objects
+# ---------------------------------
+def create_collection_type(machine_id, options)
+  coltype = Hyrax::CollectionType.find_by_machine_id(machine_id)
+  return coltype if coltype.present?
+  default_options = {
+    nestable: false, discoverable: false, sharable: false, allow_multiple_membership: false,
+    require_membership: false, assigns_workflow: false, assigns_visibility: false,
+    participants: [{ agent_type: Hyrax::CollectionTypeParticipant::GROUP_TYPE, agent_id: ::Ability.admin_group_name, access: Hyrax::CollectionTypeParticipant::MANAGE_ACCESS },
+                   { agent_type: Hyrax::CollectionTypeParticipant::GROUP_TYPE, agent_id: ::Ability.registered_group_name, access: Hyrax::CollectionTypeParticipant::CREATE_ACCESS }]
+  }
+  final_options = default_options.merge(options.except(:title))
+  Hyrax::CollectionTypes::CreateService.create_collection_type(machine_id: machine_id, title: options[:title], options: final_options)
 end
 
-# Parent Collection
-pc = Collection.create!(title: ["A Parent Collection"], read_groups: ['public'], collection_type_gid: nestable_gid) do |parent|
-  parent.apply_depositor_metadata(user)
+def create_public_collection(user, type_gid, id, options)
+  options[:visibility] = Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC
+  create_collection(user, type_gid, id, options)
 end
 
-# Child Collection
-cc = Collection.create!(title: ["A Child Collection"], read_groups: ['public'], collection_type_gid: nestable_gid) do |child|
-  child.apply_depositor_metadata(user)
-  child.member_of_collections = [pc]
+def create_private_collection(user, type_gid, id, options)
+  options[:visibility] = Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PRIVATE
+  create_collection(user, type_gid, id, options)
 end
 
-puts "Creating works"
-# Public
+def create_collection(user, type_gid, id, options)
+  col = Collection.where(id: id)
+  return col.first if col.present?
+  col = Collection.new(id: id)
+  col.attributes = options.except(:visibility)
+  col.apply_depositor_metadata(user.user_key)
+  col.collection_type_gid = type_gid
+  col.visibility = options[:visibility]
+  col.save
+  Hyrax::Collections::PermissionsCreateService.create_default(collection: col, creating_user: user)
+  col
+end
+
+def create_public_work(user, id, options)
+  options[:visibility] = Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC
+  create_work(user, id, options)
+end
+
+def create_authenticated_work(user, id, options)
+  options[:visibility] = Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_AUTHENTICATED
+  create_work(user, id, options)
+end
+
+def create_private_work(user, id, options)
+  options[:visibility] = Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PRIVATE
+  create_work(user, id, options)
+end
+
+def create_work(user, id, options)
+  work = GenericWork.where(id: id)
+  return work.first if work.present?
+  actor = Hyrax::CurationConcern.actor
+  attributes_for_actor = options
+  work = GenericWork.new(id: id)
+  actor_environment = Hyrax::Actors::Environment.new(work, Ability.new(user), attributes_for_actor)
+  actor.create(actor_environment)
+  work
+end
+
+# ---------------------------------
+# create seeded objects for QA
+# ---------------------------------
+puts 'Create users for QA'
+User.create(Hydra.config.user_key_field => 'mgr1@example.com', email: 'mgr1@example.com', password: 'pppppp') # 6*p
+User.create(Hydra.config.user_key_field => 'mgr2@example.com', email: 'mgr2@example.com', password: 'pppppp')
+User.create(Hydra.config.user_key_field => 'dep1@example.com', email: 'dep1@example.com', password: 'pppppp')
+User.create(Hydra.config.user_key_field => 'dep2@example.com', email: 'dep2@example.com', password: 'pppppp')
+User.create(Hydra.config.user_key_field => 'vw1@example.com', email: 'vw1@example.com', password: 'pppppp')
+User.create(Hydra.config.user_key_field => 'vw2@example.com', email: 'vw2@example.com', password: 'pppppp')
+
+puts 'create collection types for QA'
+_discoverable_gid = create_collection_type('discoverable_collection_type', title: 'Discoverable', description: 'Sample collection type allowing collections to be discovered.', discoverable: true).gid
+_sharable_gid = create_collection_type('sharable_collection_type', title: 'Sharable', description: 'Sample collection type allowing collections to be shared.', sharable: true).gid
+options = { title: 'Multi-membership', description: 'Sample collection type allowing works to belong to multiple collections.', allow_multiple_membership: true }
+_multi_membership_gid = create_collection_type('multi_membership_collection_type', options)
+_nestable_1_gid = create_collection_type('nestable_1_collection_type', title: 'Nestable 1', description: 'A sample collection type allowing nesting.', nestable: true).gid
+_nestable_2_gid = create_collection_type('nestable_2_collection_type', title: 'Nestable 2', description: 'Another sample collection type allowing nesting.', nestable: true).gid
+
+# -------------------------------------------------------------
+# create seeded objects for collection nesting ad hoc testing
+# -------------------------------------------------------------
+puts 'Create users for collection nesting ad hoc testing'
+user = User.create(Hydra.config.user_key_field => 'foo@example.com', email: 'foo@example.com', password: 'foobarbaz')
+
+puts 'create collection types for collection nesting ad hoc testing'
+options = { title: 'Nestable Collection', description: 'Sample collection type that allows nesting of collections.',
+            nestable: true, discoverable: true, sharable: true, allow_multiple_membership: true }
+nestable_gid = create_collection_type('nestable_collection', options).gid
+
+options = { title: 'Non-Nestable Collection', description: 'Sample collection type that DOES NOT allow nesting of collections.',
+            nestable: false, discoverable: true, sharable: true, allow_multiple_membership: true }
+_nonnestable_gid = create_collection_type('nonnestable_collection', options).gid
+
+puts 'create collections for collection nesting ad hoc testing'
+pnc = create_public_collection(user, nestable_gid, 'pnc1', title: 'Public Nestable Collection', description: 'Public nestable collection for use in ad hoc tests.')
+pc = create_public_collection(user, nestable_gid, 'pc1', title: 'A Parent Collection', description: 'Public collection that will be a parent of another collection.')
+cc = create_public_collection(user, nestable_gid, 'cc1', title: 'A Child Collection', description: 'Public collection that will be a child of another collection.')
+cc.member_of_collections = [pc]
+
+puts 'Create works for collection nesting ad hoc testing'
 3.times do |i|
-  GenericWork.create(title: ["Public #{i}"], read_groups: ['public']) do |work|
-    work.apply_depositor_metadata(user)
-    work.member_of_collections = [pnc]
-  end
+  create_public_work(user, "pub_gw_#{i}", title: "Public #{i}",
+                                          description: "Public work #{i} being added to the Public Nested Collection",
+                                          member_of_collection_ids: [pnc.id])
 end
-
-# Authenticated
 2.times do |i|
-  GenericWork.create(title: ["Authenticated #{i}"], read_groups: ['registered']) do |work|
-    work.apply_depositor_metadata(user)
-    work.member_of_collections = [pc]
-  end
+  create_authenticated_work(user, "auth_gw_#{i}", title: "Authenticated #{i}",
+                                                  description: "Authenticated work #{i} being added to the Parent Collection",
+                                                  member_of_collection_ids: [pc.id])
 end
-
-# Private
 1.times do |i|
-  GenericWork.create(title: ["Private #{i}"]) do |work|
-    work.apply_depositor_metadata(user)
-    work.member_of_collections = [cc]
-  end
+  create_private_work(user, "priv_gw_#{i}", title: "Private #{i}",
+                                            description: "Proviate work #{i} being added to the Child Collection",
+                                            member_of_collection_ids: [cc.id])
 end
 
+# -----------------------------------------------------
+# create seeded objects for embargo ad hoc testing
+# -----------------------------------------------------
 # Active, Private Embargo
 3.times do |i|
   GenericWork.create(title: ["Active Private #{i}"]) do |work|
