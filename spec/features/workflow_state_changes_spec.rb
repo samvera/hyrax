c03bc5eb032e4d3e6becaa9e7fbb16546205e6ae
Justin Coyne <jcoyne@justincoyne.com>
2017-12-06 10:20:42 -0600

    Pass the persister to the workflow factory

--- spec/features/workflow_state_changes_spec.rb
+++ spec/features/workflow_state_changes_spec.rb
@@ -29,6 +29,7 @@ RSpec.describe "Workflow state changes", type: :feature do
   let(:workflow) { Sipity::Workflow.find_by!(name: workflow_name, permission_template: permission_template) }
   let(:work) { create_for_repository(:work, user: depositing_user, admin_set_id: admin_set.id) }
   let(:permission_template) { create(:permission_template, admin_set_id: admin_set.id) }
+  let(:persister) { Valkyrie::MetadataAdapter.find(:indexing_persister).persister }
 
   before do
     allow(::User.group_service).to receive(:byname).and_return(depositing_user.user_key => ['admin'], approving_user.user_key => ['admin'])
@@ -36,7 +37,7 @@ RSpec.describe "Workflow state changes", type: :feature do
     permission_template.available_workflows.first.update!(active: true)
     Hyrax::Workflow::PermissionGenerator.call(roles: 'approving', workflow: workflow, agents: approving_user)
     # Need to instantiate the Sipity::Entity for the given work. This is necessary as I'm not creating the work via the UI.
-    Hyrax::Workflow::WorkflowFactory.create(work, {}, depositing_user)
+    Hyrax::Workflow::WorkflowFactory.create(work, {}, depositing_user, persister: persister)
   end
 
   describe 'leaving a comment for non-state changing' do
