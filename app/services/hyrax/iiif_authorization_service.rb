2b551726a39e7fdaebefc05bd50b8ce107f6b7a5
Michael J. Giarlo <mjgiarlo@stanford.edu>
2017-12-08 12:28:35 -0800

    Clarify ignored parameter in Hyrax::IIIFAuthorizationService
    Thanks, @jeremyf!

--- app/services/hyrax/iiif_authorization_service.rb
+++ app/services/hyrax/iiif_authorization_service.rb
@@ -5,6 +5,7 @@ module Hyrax
       @controller = controller
     end
 
+    # @note we ignore the `action` param here in favor of the `:show` action for all permissions
     def can?(_action, object)
       controller.current_ability.can?(:show, file_set_id_for(object))
     end
