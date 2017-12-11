462a3c7534671d8d49e221d4a85a79adc47b7190
Justin Coyne <jcoyne@justincoyne.com>
2017-12-11 08:18:16 -0600

    Pass the persister to the WorkflowFactory

--- spec/services/hyrax/workflow/workflow_factory_spec.rb
+++ spec/services/hyrax/workflow/workflow_factory_spec.rb
@@ -8,12 +8,16 @@ RSpec.describe Hyrax::Workflow::WorkflowFactory do
     let(:attributes) { {} }
     let(:user) { create(:user) }
     let(:deposit_action) { Sipity::WorkflowAction.create!(workflow: workflow, name: 'start') }
+    let(:persister) { Valkyrie::MetadataAdapter.find(:indexing_persister).persister }
 
-    subject { described_class.create(work, attributes, user) }
+    subject { described_class.create(work, attributes, user, persister: persister) }
 
+    # rubocop:disable RSpec/ExampleLength
     it 'creates a Sipity::Entity, assign entity specific responsibility (but not to the full workflow) then runs the WorkflowActionService' do
       expect(Hyrax::Workflow::WorkflowActionService).to receive(:run).with(
-        subject: kind_of(Hyrax::WorkflowActionInfo), action: deposit_action
+        subject: kind_of(Hyrax::WorkflowActionInfo),
+        action: deposit_action,
+        persister: persister
       )
       expect do
         expect do
@@ -22,5 +26,6 @@ RSpec.describe Hyrax::Workflow::WorkflowFactory do
                                               .and change { Sipity::EntitySpecificResponsibility.count }.by(1)
       end.not_to change { Sipity::WorkflowResponsibility.count }
     end
+    # rubocop:enable RSpec/ExampleLength
   end
 end
