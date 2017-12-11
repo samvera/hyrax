f0491c026f4c88d38bb97c927b11789aba272fbd
Justin Coyne <jcoyne@justincoyne.com>
2017-12-11 09:55:54 -0600

    Update workflow actions to be aware that depositor is a multivalued field

--- spec/services/hyrax/workflow/grant_edit_to_depositor_spec.rb
+++ spec/services/hyrax/workflow/grant_edit_to_depositor_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe Hyrax::Workflow::GrantEditToDepositor do
     end
 
     context "with no additional editors" do
-      let(:work) { create_for_repository(:work_without_access, depositor: depositor.user_key) }
+      let(:work) { create_for_repository(:work_without_access, depositor: [depositor.user_key]) }
 
       it "adds edit access" do
         expect { subject }.to change { work.edit_users }.from([]).to([depositor.user_key])
@@ -25,7 +25,7 @@ RSpec.describe Hyrax::Workflow::GrantEditToDepositor do
 
     context "with an additional editor" do
       let(:editor) { create(:user) }
-      let(:work) { create_for_repository(:work_without_access, depositor: depositor.user_key, edit_users: [editor.user_key]) }
+      let(:work) { create_for_repository(:work_without_access, depositor: [depositor.user_key], edit_users: [editor.user_key]) }
 
       it "adds edit access" do
         expect { subject }.to change { work.edit_users }.from([editor.user_key]).to([editor.user_key, depositor.user_key])
