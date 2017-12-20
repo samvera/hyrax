9de06bd7e166099c57b128a4b3c34358c3d46e75
Justin Coyne <jcoyne@justincoyne.com>
2017-12-20 10:20:02 -0600

    Use create_for_repository for creating fixtures

--- spec/authorities/qa/authorities/collections_spec.rb
+++ spec/authorities/qa/authorities/collections_spec.rb
@@ -5,10 +5,10 @@ RSpec.describe Qa::Authorities::Collections, :clean_repo do
   let(:q) { "foo" }
   let(:params) { ActionController::Parameters.new(q: q, access: 'edit') }
   let(:service) { described_class.new }
-  let!(:collection1) { create(:collection, :public, title: ['foo foo'], user: user1) }
-  let!(:collection2) { create(:collection, :public, title: ['bar'], user: user1) }
-  let!(:collection3) { create(:collection, :public, title: ['another foo'], user: user1) }
-  let!(:collection4) { create(:collection, :public, title: ['foo foo foo'], user: user2) }
+  let!(:collection1) { create_for_repository(:collection, :public, title: ['foo foo'], user: user1) }
+  let!(:collection2) { create_for_repository(:collection, :public, title: ['bar'], user: user1) }
+  let!(:collection3) { create_for_repository(:collection, :public, title: ['another foo'], user: user1) }
+  let!(:collection4) { create_for_repository(:collection, :public, title: ['foo foo foo'], user: user2) }
 
   before do
     allow(controller).to receive(:params).and_return(params)
@@ -19,7 +19,7 @@ RSpec.describe Qa::Authorities::Collections, :clean_repo do
 
   describe '#search' do
     it 'displays a list of collections for the current user' do
-      expect(subject.map { |result| result[:id] }).to match_array [collection1.id, collection3.id]
+      expect(subject.map { |result| result[:id] }).to match_array [collection1.id.to_s, collection3.id.to_s]
     end
   end
 end
