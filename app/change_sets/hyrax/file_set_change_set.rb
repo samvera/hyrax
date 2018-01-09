f74455f2ad36230f15c9b2356bd8a84f2c50d50f
Justin Coyne <jcoyne@justincoyne.com>
2018-01-09 13:25:42 -0600

    Get nested permissions working with Reform on FileSetsController

--- app/change_sets/hyrax/file_set_change_set.rb
+++ app/change_sets/hyrax/file_set_change_set.rb
@@ -1,3 +1,5 @@
+# frozen_string_literals: true
+
 module Hyrax
   class FileSetChangeSet < Valkyrie::ChangeSet
     property :read_users, multiple: true, required: false
@@ -5,13 +7,9 @@ module Hyrax
     property :edit_users, multiple: true, required: false
     property :edit_groups, multiple: true, required: false
     property :depositor
-
-    # delegate :depositor, :permissions, to: :model
-    # self.required_fields = [:title, :creator, :keyword, :license]
     property :title, multiple: true, required: true
 
-    collection :permissions, virtual: true
-
+    collection :permissions, virtual: true, populator: :populate_permissions
     delegate :embargo_id, :lease_id, to: :resource
     # TODO: Figure out where to persist these fields
     property :embargo_release_date, virtual: true
@@ -24,16 +22,6 @@ module Hyrax
 
     property :user, virtual: true
 
-    def sync
-      if permissions
-        self.edit_users = permission('edit', 'person') + [user.user_key]
-        self.edit_groups = permission('edit', 'group')
-        self.read_users = permission('read', 'person')
-        self.read_groups = permission('read', 'group')
-      end
-      super
-    end
-
     class_attribute :terms
     self.terms = [:resource_type, :title, :creator, :contributor, :description,
                   :keyword, :license, :publisher, :date_created, :subject, :language,
@@ -42,29 +30,27 @@ module Hyrax
                   :visibility_during_lease, :visibility_after_lease, :lease_expiration_date,
                   :visibility]
 
-    def prepopulate!
-      prepopulate_permissions
-      super.tap do
-        @_changes = Disposable::Twin::Changed::Changes.new
-      end
-    end
-
-    def version_list
-      @version_list ||= begin
-        original = resource.original_file
-        Hyrax::VersionListPresenter.new(original ? original.versions.all : [])
-      end
-    end
-
     # We just need to respond to this method so that the rails nested form builder will work.
     def permissions_attributes=
       # nop
     end
 
-    def permission(access, type)
-      permissions_attributes
-        .select { |attr| attr['access'] == access && attr['type'] == type }
-        .map { |attr| attr['name'] }
+    # Deserializes the nested permission values from the form
+    # TODO: consider a access level change
+    def populate_permissions(options)
+      options[:doc][:permissions_attributes].each_value do |row|
+        deserialize_permission(row['agent_name'], row['type'], row['access'])
+      end
+      self.edit_users += [user.user_key]
+    end
+
+    def version_list
+      @version_list ||= begin
+        [] # TODO: remove when we have versions
+
+        # original = resource.original_file
+        # Hyrax::VersionListPresenter.new(original ? original.versions.all : [])
+      end
     end
 
     def page_title
@@ -87,17 +73,31 @@ module Hyrax
       ::SolrDocument.new(results['response']['docs'].first)
     end
 
+    # Setup default values for the form.
+    def prepopulate!
+      self.permissions = resource.edit_users.map { |key| PermissionChangeSet.new(Permission.new, agent_name: key, access: 'edit', type: 'person') } +
+                         resource.read_users.map { |key| PermissionChangeSet.new(Permission.new, agent_name: key, access: 'read', type: 'person') } +
+                         resource.edit_groups.map { |key| PermissionChangeSet.new(Permission.new, agent_name: key, access: 'edit', type: 'group') } +
+                         resource.read_groups.map { |key| PermissionChangeSet.new(Permission.new, agent_name: key, access: 'read', type: 'group') }
+      self
+    end
+
     private
+
+      def deserialize_permission(agent, type, access)
+        raise ArgumentError unless %w[group person].include?(type) &&
+                                   %w[read edit].include?(access)
+
+        field = if type == 'group'
+                  "#{access}_groups"
+                else
+                  "#{access}_users"
+                end
+        public_send(field + '=', public_send(field) + [agent])
+      end
 
       def document_model
         CatalogController.blacklight_config.document_model
-      end
-
-      def prepopulate_permissions
-        self.permissions = resource.edit_users.map { |key| PermissionChangeSet.new(Permission.new, agent_name: key, access: 'edit', type: 'person') } +
-                           resource.read_users.map { |key| PermissionChangeSet.new(Permission.new, agent_name: key, access: 'read', type: 'person') } +
-                           resource.edit_groups.map { |key| PermissionChangeSet.new(Permission.new, agent_name: key, access: 'edit', type: 'group') } +
-                           resource.read_groups.map { |key| PermissionChangeSet.new(Permission.new, agent_name: key, access: 'read', type: 'group') }
       end
   end
 end
--- app/models/hyrax/permission.rb
+++ app/models/hyrax/permission.rb
@@ -1,8 +1,14 @@
+# frozen_string_literals: true
+
 module Hyrax
   class Permission
     include ActiveModel::Model
     attr_accessor :agent_name
     attr_accessor :access
     attr_accessor :type
+
+    def id
+      [agent_name, type].join('-')
+    end
   end
 end
