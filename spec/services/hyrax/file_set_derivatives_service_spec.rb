b1a90b44c19d2072a22151bbf5ca1ac860df62cc
Justin Coyne <jcoyne@justincoyne.com>
2018-01-16 10:37:35 -0600

    Correctly mock the output of FileSet#mime_type

--- spec/services/hyrax/file_set_derivatives_service_spec.rb
+++ spec/services/hyrax/file_set_derivatives_service_spec.rb
@@ -3,7 +3,7 @@ require 'hyrax/specs/shared_specs'
 RSpec.describe Hyrax::FileSetDerivativesService do
   let(:valid_file_set) do
     FileSet.new.tap do |f|
-      allow(f).to receive(:mime_type).and_return(FileSet.image_mime_types.first)
+      allow(f).to receive(:mime_type).and_return([FileSet.image_mime_types.first])
     end
   end
 
