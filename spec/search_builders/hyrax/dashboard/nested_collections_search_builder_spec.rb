f06dceeed0569f6c35a03162302a67f688b3690f
E. Lynette Rayle <elr37@cornell.edu>
2018-01-19 14:29:02 -0500

    allow deposit collections as parent and sub-collections

--- spec/search_builders/hyrax/dashboard/nested_collections_search_builder_spec.rb
+++ spec/search_builders/hyrax/dashboard/nested_collections_search_builder_spec.rb
@@ -1,9 +1,10 @@
 RSpec.describe Hyrax::Dashboard::NestedCollectionsSearchBuilder do
   let(:collection) { double(id: '123', collection_type_gid: 'gid/abc') }
-  let(:ability) { instance_double(Ability, admin?: true) }
   let(:scope) { double(current_ability: ability, blacklight_config: CatalogController.blacklight_config) }
-  let(:access) { :read }
   let(:builder) { described_class.new(scope: scope, access: access, collection: collection) }
+  let(:access) { :read }
+  let(:user) { create(:user) }
+  let(:ability) { ::Ability.new(user) }
 
   describe '#query' do
     subject { builder.query }
@@ -26,6 +27,33 @@ RSpec.describe Hyrax::Dashboard::NestedCollectionsSearchBuilder do
     it 'will exclude the given collection' do
       subject
       expect(solr_params.fetch(:fq)).to eq(["-{!terms f=id}#{collection.id}", "_query_:\"{!field f=collection_type_gid_ssim}#{collection.collection_type_gid}\""])
+    end
+  end
+
+  describe '#gated_discovery_filters' do
+    subject { builder.gated_discovery_filters(access, ability) }
+
+    context 'when access is :deposit' do
+      let(:access) { "deposit" }
+      let!(:collection) { create(:collection, with_permission_template: attributes) }
+
+      context 'and user has access' do
+        let(:attributes) { { deposit_users: [user.user_key] } }
+
+        it { is_expected.to eq ["{!terms f=id}#{collection.id}"] }
+      end
+
+      context 'and group has access' do
+        let(:attributes) { { deposit_groups: ['registered'] } }
+
+        it { is_expected.to eq ["{!terms f=id}#{collection.id}"] }
+      end
+
+      context "and user has no access" do
+        let(:attributes) { true }
+
+        it { is_expected.to eq ["{!terms f=id}"] }
+      end
     end
   end
 end
