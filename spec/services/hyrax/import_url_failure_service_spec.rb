25ade91766ec838a0d720d32e028e7608528356d
Justin Coyne <jcoyne@justincoyne.com>
2018-01-19 13:16:58 -0600

    Don't reference errors on the import failure service
    Validations are no longer on the model instance

--- spec/services/hyrax/import_url_failure_service_spec.rb
+++ spec/services/hyrax/import_url_failure_service_spec.rb
@@ -2,17 +2,13 @@
 RSpec.describe Hyrax::ImportUrlFailureService do
   let!(:depositor) { create(:user) }
   let(:inbox) { depositor.mailbox.inbox }
-  let(:file) do
-    create_for_repository(:file_set, user: depositor)
-  end
-
-  before do
-    allow(file.errors).to receive(:full_messages).and_return(['huge mistake'])
+  let(:file_set) do
+    create_for_repository(:file_set, user: depositor, import_url: 'http://example.com/image.png')
   end
 
   describe "#call" do
     before do
-      described_class.new(file, depositor).call
+      described_class.new(file_set, depositor).call
     end
 
     it "sends failing mail" do
