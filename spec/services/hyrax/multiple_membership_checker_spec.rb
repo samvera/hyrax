d57a8d19168cd4061bde406b5fce83b865cd1994
Chris Colvard <cjcolvar@indiana.edu>
2018-02-12 22:51:41 -0500

    Test multiple single membership collections that have different collection types

--- spec/services/hyrax/multiple_membership_checker_spec.rb
+++ spec/services/hyrax/multiple_membership_checker_spec.rb
@@ -101,6 +101,50 @@ RSpec.describe Hyrax::MultipleMembershipChecker, :clean_repo do
         expect(Collection).to receive(:where).with(id: [collection2.id], collection_type_gid_ssim: [collection_type.gid]).once.and_return([collection2])
         expect(subject).to eq 'Error: You have specified more than one of the same single-membership collection types: Greedy (Foo and Bar)'
       end
+
+      context 'with multiple single membership collection types' do
+        let!(:collection_type_2) { create(:collection_type, title: 'Doc', allow_multiple_membership: false) }
+        let(:collection_types) { [collection_type.gid, collection_type_2.gid] }
+
+        it 'returns an error' do
+          expect(item).to receive(:member_of_collection_ids)
+          expect(Collection).to receive(:where).with(id: collection_ids, collection_type_gid_ssim: collection_types).once.and_call_original
+          expect(Collection).to receive(:where).with(id: [collection2.id], collection_type_gid_ssim: collection_types).once.and_call_original
+          expect(subject).to eq 'Error: You have specified more than one of the same single-membership collection types: Greedy (Foo and Bar)'
+        end
+      end
+    end
+
+    context 'when multiple single-membership collection instances are in the list, but are different collection types' do
+      let(:collection1) { create(:collection, title: ['Foo'], collection_type: collection_type) }
+      let(:collection2) { create(:collection, title: ['Bar'], collection_type: collection_type_2) }
+      let(:collections) { [collection1, collection2] }
+      let(:collection_ids) { collections.map(&:id) }
+      let(:collection_type_2) { create(:collection_type, title: 'Doc', allow_multiple_membership: false) }
+      let(:collection_types) { [collection_type.gid, collection_type_2.gid] }
+
+      it 'returns nil' do
+        expect(item).not_to receive(:member_of_collection_ids)
+        expect(checker).to receive(:single_membership_collections).with(collection_ids).once.and_call_original
+        expect(Collection).to receive(:where).with(id: collection_ids, collection_type_gid_ssim: collection_types).once.and_call_original
+        expect(subject).to be nil
+      end
+
+      context 'when including current members' do
+        let(:collections) { [collection1] }
+        let(:included) { true }
+
+        before do
+          allow(item).to receive(:member_of_collection_ids).once.and_return([collection2.id])
+        end
+
+        it 'returns nil' do
+          expect(item).to receive(:member_of_collection_ids)
+          expect(Collection).to receive(:where).with(id: collection_ids, collection_type_gid_ssim: collection_types).once.and_call_original
+          expect(Collection).to receive(:where).with(id: [collection2.id], collection_type_gid_ssim: collection_types).once.and_call_original
+          expect(subject).to be nil
+        end
+      end
     end
   end
 end
