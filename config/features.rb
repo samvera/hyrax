897d7dc8295310087289565862b196aebf54512d
Nestor Walker <naw4t@virginia.edu>
2018-02-14 15:10:46 -0500

    Add Flipflop for new analytics

--- config/features.rb
+++ config/features.rb
@@ -30,4 +30,8 @@ Flipflop.configure do
   feature :batch_upload,
           default: true,
           description: "Enable uploading batches of works"
+
+  feature :analytics_redesign,
+          default: false,
+          description: "Display new reporting features. *Very Experimental*"
 end
