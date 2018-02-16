1e5234b81d5c65c33f2b3a35d0b760c2bf34aabf
Matt Critchlow <matt.critchlow@gmail.com>
2018-02-16 10:47:20 -0800

    Initial spec coverage for Analytics::Base
    Changes proposed:
    - Initial spec coverage for Analytics::Base
    - Pending test templates for Matomo and GoogleAnalytics subclasses
    - Renamed Connection to Base
    - Renamed matomo_analytics.rb to matomo.rb
    - Add Pikiw gem to hyrax gemspec

--- app/services/hyrax/analytics/base.rb
+++ app/services/hyrax/analytics/base.rb
@@ -2,25 +2,12 @@ module Hyrax
   module Analytics
     # @abstract Base class for Analytics services that support statistics needs in Hyrax.
     # Implementing subclasses must define `#connection` `#remote_statistics` and `#to_graph`
-    class Connector
+    class Base
       # Establish connection with the analytics service
       def self.connection
         raise NotImplementedError, "#{self.class}#connection is unimplemented."
       end
 
-      # Used by Hyrax::Statistic to perform live queries against the remote service
-      # @param start_date [DateTime]
-      # @param object [ActiveFedora::Base??] probably a better type for this
-      # @param query_type
-      #
-      # return [Enumerable] of objects to cache locally in DB
-      # TODO: decide best place for query types: pageview, download, returning visitors,  new visitors,..
-      def self.remote_statistics(_start_date, _object, _query_type)
-        raise NotImplementedError, "#{self.class}#remote_statistics is unimplemented."
-      end
-
-      # OR, define explicit query methods for each query?
-      # Examples
       def self.pageviews(_start_date, _object)
         raise NotImplementedError, "#{self.class}#pageviews is unimplemented."
       end
