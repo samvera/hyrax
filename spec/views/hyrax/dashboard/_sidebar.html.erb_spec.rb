c6d2d25226803d3711e85e0af5acc049c1a8003c
Jennifer Smith <smithj15@email.unc.edu>
2018-02-19 12:43:08 -0500

    Add embargo and lease links to admin dashboard in tasks section

--- spec/views/hyrax/dashboard/_sidebar.html.erb_spec.rb
+++ spec/views/hyrax/dashboard/_sidebar.html.erb_spec.rb
@@ -44,6 +44,8 @@ RSpec.describe 'hyrax/dashboard/_sidebar.html.erb', type: :view do
     subject { rendered }
 
     it { is_expected.to have_link t('hyrax.admin.sidebar.statistics') }
+    it { is_expected.to have_link t('hyrax.embargoes.index.manage_embargoes') }
+    it { is_expected.to have_link t('hyrax.leases.index.manage_leases') }
   end
 
   context 'with a user who can review submissions' do
