a7197a15f5ce17cbffc05dabd484a7057cac2ef0
Chris Colvard <cjcolvar@indiana.edu>
2018-02-22 07:11:38 -0500

    Refactor tests

--- spec/features/collection_multi_membership_spec.rb
+++ spec/features/collection_multi_membership_spec.rb
@@ -1,137 +1,84 @@
 RSpec.describe 'Adding a work to multiple collections', type: :feature, clean_repo: true, js: true, with_nested_reindexing: true do
-  let!(:admin_user) { create(:admin, email: 'admin@example.com') }
-  let!(:single_membership_type_1) { create(:collection_type, :not_allow_multiple_membership, title: 'Single-membership 1') }
-  let!(:single_membership_type_2) { create(:collection_type, :not_allow_multiple_membership, title: 'Single-membership 2') }
-  let!(:multi_membership_type_1) { create(:collection_type, :allow_multiple_membership, title: 'Multi-membership 1') }
-  let!(:multi_membership_type_2) { create(:collection_type, :allow_multiple_membership, title: 'Multi-membership 2') }
-
-  let!(:col1_sm_1) { create(:collection_lw, id: 'col1_sm_1', title: ['Collection 1 for SM1'], user: admin_user, collection_type_gid: single_membership_type_1.gid) }
-  let!(:col2_sm_1) { create(:collection_lw, id: 'col2_sm_1', title: ['Collection 2 for SM1'], user: admin_user, collection_type_gid: single_membership_type_1.gid) }
-  let!(:col3_sm_2) { create(:collection_lw, id: 'col3_sm_2', title: ['Collection 3 for SM2'], user: admin_user, collection_type_gid: single_membership_type_2.gid) }
-  let!(:col4_mm_1) { create(:collection_lw, id: 'col4_mm_1', title: ['Collection 4 for MM1'], user: admin_user, collection_type_gid: multi_membership_type_1.gid) }
-  let!(:col5_mm_1) { create(:collection_lw, id: 'col5_mm_1', title: ['Collection 5 for MM1'], user: admin_user, collection_type_gid: multi_membership_type_1.gid) }
-  let!(:col6_mm_2) { create(:collection_lw, id: 'col6_mm_2', title: ['Collection 6 for MM2'], user: admin_user, collection_type_gid: multi_membership_type_2.gid) }
-
-  let!(:work) { create(:generic_work, user: admin_user, title: ['The highly valued work that everyone wants in their collection']) }
+  let(:admin_user) { create(:admin, email: 'admin@example.com') }
+  let(:single_membership_type_1) { create(:collection_type, :not_allow_multiple_membership, title: 'Single-membership 1') }
+  let(:single_membership_type_2) { create(:collection_type, :not_allow_multiple_membership, title: 'Single-membership 2') }
+  let(:multi_membership_type_1) { create(:collection_type, :allow_multiple_membership, title: 'Multi-membership 1') }
+  let(:multi_membership_type_2) { create(:collection_type, :allow_multiple_membership, title: 'Multi-membership 2') }
 
   before do
-    admin_user
     sign_in admin_user
-    work
   end
 
   describe 'when both collections support multiple membership' do
+    let(:old_collection) { create(:collection_lw, user: admin_user, collection_type_gid: multi_membership_type_1.gid) }
+    let!(:work) { create(:generic_work, user: admin_user, member_of_collections: [old_collection], title: ['The highly valued work that everyone wants in their collection']) }
+
     context 'and are of different types' do
-      before do
-        col4_mm_1
-        col6_mm_2
-      end
+      let!(:new_collection) { create(:collection_lw, user: admin_user, collection_type_gid: multi_membership_type_2.gid) }
 
       it 'then the work is added to both collections' do
-        # Add to the first multi-membership collection
-        visit '/dashboard/my/works'
-        check 'check_all'
-        click_button 'Add to collection' # opens the modal
-        within('div#collection-list-container') do
-          choose 'Collection 4 for MM1' # selects the collection
-          click_button "Save changes"
-        end
-
-        # forwards to collection show page
-        expect(page).to have_content(col4_mm_1.title.first)
-        expect(page).to have_content 'Works (1)'
-        expect(page).to have_content 'The highly valued work that everyone wants in their collection'
-
         # Add to second multi-membership collection of a different type
         visit '/dashboard/my/works'
         check 'check_all'
         click_button 'Add to collection' # opens the modal
         within('div#collection-list-container') do
-          choose 'Collection 6 for MM2' # selects the collection
+          choose new_collection.title.first # selects the collection
           click_button 'Save changes'
         end
+
         # forwards to collection show page
-        expect(page).to have_content(col6_mm_2.title.first)
+        expect(page).to have_content new_collection.title.first
         expect(page).to have_content 'Works (1)'
-        expect(page).to have_content 'The highly valued work that everyone wants in their collection'
+        expect(page).to have_content work.title.first
       end
     end
 
     context 'and are of the same type' do
+      let!(:new_collection) { create(:collection_lw, user: admin_user, collection_type_gid: multi_membership_type_1.gid) }
+
       it 'then the work is added to both collections' do
-        # Add to the first multi-membership collection
+        # Add to second multi-membership collection of a different type
         visit '/dashboard/my/works'
         check 'check_all'
         click_button 'Add to collection' # opens the modal
         within('div#collection-list-container') do
-          choose 'Collection 4 for MM1' # selects the collection
-          click_button "Save changes"
-        end
-
-        # forwards to collection show page
-        expect(page).to have_content(col4_mm_1.title.first)
-        expect(page).to have_content 'Works (1)'
-        expect(page).to have_content 'The highly valued work that everyone wants in their collection'
-
-        # Add to second multi-membership collection of the same type
-        visit '/dashboard/my/works'
-        check 'check_all'
-        click_button 'Add to collection' # opens the modal
-        within('div#collection-list-container') do
-          choose 'Collection 5 for MM1' # selects the collection
+          choose new_collection.title.first # selects the collection
           click_button 'Save changes'
         end
+
         # forwards to collection show page
-        expect(page).to have_content(col5_mm_1.title.first)
+        expect(page).to have_content new_collection.title.first
         expect(page).to have_content 'Works (1)'
-        expect(page).to have_content 'The highly valued work that everyone wants in their collection'
+        expect(page).to have_content work.title.first
       end
     end
   end
 
   describe 'when both collections require single membership' do
+    let(:old_collection) { create(:collection_lw, user: admin_user, collection_type_gid: single_membership_type_1.gid) }
+    let!(:work) { create(:generic_work, user: admin_user, member_of_collections: [old_collection], title: ['The highly valued work that everyone wants in their collection']) }
+
     context 'and are of different types' do
+      let!(:new_collection) { create(:collection_lw, user: admin_user, collection_type_gid: single_membership_type_2.gid) }
+
       it 'then the work is added to both collections' do
-        # Add to the first single-membership collection
-        visit '/dashboard/my/works'
-        check 'check_all'
-        click_button 'Add to collection' # opens the modal
-        within('div#collection-list-container') do
-          choose 'Collection 1 for SM1' # selects the collection
-          click_button "Save changes"
-        end
-
-        # forwards to collection show page
-        expect(page).to have_content(col1_sm_1.title.first)
-        expect(page).to have_content 'Works (1)'
-        expect(page).to have_content 'The highly valued work that everyone wants in their collection'
-
         # Add to second single-membership collection of a different type
         visit '/dashboard/my/works'
         check 'check_all'
         click_button 'Add to collection' # opens the modal
         within('div#collection-list-container') do
-          choose 'Collection 3 for SM2' # selects the collection
+          choose new_collection.title.first # selects the collection
           click_button 'Save changes'
         end
         # forwards to collection show page
-        expect(page).to have_content(col3_sm_2.title.first)
+        expect(page).to have_content new_collection.title.first
         expect(page).to have_content 'Works (1)'
-        expect(page).to have_content 'The highly valued work that everyone wants in their collection'
+        expect(page).to have_content work.title.first
       end
     end
 
     context 'and are of the same type' do
-      before do
-        # Add to the first single-membership collection
-        visit '/dashboard/my/works'
-        check 'check_all'
-        click_button 'Add to collection' # opens the modal
-        within('div#collection-list-container') do
-          choose 'Collection 1 for SM1' # selects the collection
-          click_button "Save changes"
-        end
-      end
+      let!(:new_collection) { create(:collection_lw, user: admin_user, collection_type_gid: single_membership_type_1.gid) }
 
       context 'then the work fails to add to the second collection' do
         it 'from the dashboard->works batch add to collection' do
@@ -140,7 +87,7 @@ RSpec.describe 'Adding a work to multiple collections', type: :feature, clean_re
           check 'check_all'
           click_button 'Add to collection' # opens the modal
           within('div#collection-list-container') do
-            choose 'Collection 2 for SM1' # selects the collection
+            choose new_collection.title.first # selects the collection
             click_button 'Save changes'
           end
           # forwards to work index page and shows flash message
@@ -162,14 +109,14 @@ RSpec.describe 'Adding a work to multiple collections', type: :feature, clean_re
           expect(page).to have_selector '.alert', text: 'Error: You have specified more than one of the same single-membership collection types: Single-membership 1 (Collection 1 for SM1)'
         end
 
-        it "from the collection's show page Addadd to collection" do
+        it "from the collection's show page Add to collection" do
           # Attempt to add to second single-membership collection of the same type
-          visit "/dashboard/collections/#{col2_sm_1.id}"
+          visit "/dashboard/collections/#{new_collection.id}"
           click_link 'Add existing works'
           check 'check_all'
           click_button 'Add to collection' # opens the modal
           within('div#collection-list-container') do
-            choose 'Collection 2 for SM1' # selects the collection
+            choose new_collection.title.first # selects the collection
             click_button 'Save changes'
           end
           # forwards to work index page and shows flash message
