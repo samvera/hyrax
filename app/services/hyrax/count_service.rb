2f87315e8f155246e7ac60e8e84a4f84cab7154f
lfarrell <lfarrell@email.unc.edu>
2018-02-22 14:58:39 -0500

    Set default search builder

--- app/services/hyrax/count_service.rb
+++ app/services/hyrax/count_service.rb
@@ -1,10 +1,11 @@
 module Hyrax
-  # Returns Collections that the current user has permission to use.
   class CountService
-    attr_reader :context, :search_builder
+    attr_reader :context, :search_builder, :model
+    class_attribute :default_search_builder
+    self.default_search_builder = Hyrax::AdminSetSearchBuilder
 
     # @param [#repository,#blacklight_config,#current_ability] context
-    def initialize(context, search_builder, model = ::AdminSet)
+    def initialize(context, search_builder = default_search_builder, model = ::AdminSet)
       @context = context
       @search_builder = search_builder
       @model = model
