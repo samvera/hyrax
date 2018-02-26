cd3d9058aebb94f4739900d19b5c47c03cc1de68
Conor O'Malley <conorom@umich.edu>
2018-02-25 22:31:50 -0500

    connect to GA using Google's API gem

--- config/initializers/analytics.rb
+++ config/initializers/analytics.rb
@@ -1,16 +1,17 @@
 # Reads and sets up config for the analytics platform
 # Needs to run after other initializers
 Rails.application.config.after_initialize do
+  filename = Rails.root.join('config', 'analytics.yml')
+  yaml = YAML.safe_load(File.read(filename))
+  unless yaml
+    Rails.logger.error("Unable to fetch any keys from #{filename}.")
+    return
+  end
+  config = yaml.fetch('analytics')
+
   case Hyrax.config.analytics
   when 'matomo'
     require 'piwik'
-    filename = Rails.root.join('config', 'analytics.yml')
-    yaml = YAML.safe_load(File.read(filename))
-    unless yaml
-      Rails.logger.error("Unable to fetch any keys from #{filename}.")
-      return
-    end
-    config = yaml.fetch('analytics')
     Piwik::PIWIK_URL = config['matomo_url']
     Piwik::PIWIK_TOKEN = config['matomo_token']
 
@@ -18,8 +19,10 @@ Rails.application.config.after_initialize do
     unless Hyrax::Analytics::Matomo.valid?
       Rails.logger.error("Invalid Matomo config from #{filename}.")
     end
-
-  when 'google' # rubocop:disable Lint/EmptyWhen
-    # TODO: move/refactor google yaml config to here
+  when 'google'
+    Hyrax::Analytics::GoogleAnalytics.config = config
+    unless Hyrax::Analytics::GoogleAnalytics.valid?
+      Rails.logger.error("Invalid Google Analytics config from #{filename}.")
+    end
   end
 end
