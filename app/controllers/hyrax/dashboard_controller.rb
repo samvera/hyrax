b48a86ae4421132c31a5dda3133f24661b8679f3
lfarrell <lfarrell@email.unc.edu>
2018-02-26 09:15:43 -0500

    Add collection table to dashboard page

--- app/controllers/hyrax/dashboard_controller.rb
+++ app/controllers/hyrax/dashboard_controller.rb
@@ -9,7 +9,10 @@ module Hyrax
       add_breadcrumb t(:'hyrax.dashboard.breadcrumbs.admin'), hyrax.dashboard_path
       if can? :read, :admin_dashboard
         @presenter = Hyrax::Admin::DashboardPresenter.new
-        @admin_set_rows = Hyrax::AdminSetService.new(self).search_results_with_work_count(:read)
+        @admin_set_rows = Hyrax::AdminSetService.new(self, Hyrax::AdminSetSearchBuilder).search_results_with_work_count(:read)
+
+        @collections = Hyrax::CollectionsCountService.new(self, Hyrax::AdminSetSearchBuilder, ::Collection).search_results_with_work_count(:read)
+        @collection_rows = Kaminari.paginate_array(@collections).page(params[:page]).per(10)
         render 'show_admin'
       else
         @presenter = Dashboard::UserPresenter.new(current_user, view_context, params[:since])
