41060f1286bd1a7f3f2d2361768bb02aa5a963c7
lfarrell <lfarrell@email.unc.edu>
2018-03-02 13:54:22 -0500

    Use DataTables plugin to display tables

--- app/controllers/hyrax/dashboard_controller.rb
+++ app/controllers/hyrax/dashboard_controller.rb
@@ -11,22 +11,12 @@ module Hyrax
         @presenter = Hyrax::Admin::DashboardPresenter.new
         @admin_set_rows = Hyrax::AdminSetService.new(self, Hyrax::AdminSetSearchBuilder).search_results_with_work_count(:read)
 
-        @collections = Hyrax::CollectionsCountService.new(self, Hyrax::AdminSetSearchBuilder, ::Collection).search_results_with_work_count(:read)
-        @collections.sort_by { |coll| coll.updated.to_s }.reverse!
-        @collection_rows = Kaminari.paginate_array(@collections).page(params[:page]).per(10)
+        @collection_rows = Hyrax::CollectionsCountService.new(self, Hyrax::AdminSetSearchBuilder, ::Collection).search_results_with_work_count(:read)
         render 'show_admin'
       else
         @presenter = Dashboard::UserPresenter.new(current_user, view_context, params[:since])
         render 'show_user'
       end
-    end
-
-    def update_collections
-      @collections = Hyrax::CollectionsCountService.new(self, Hyrax::AdminSetSearchBuilder, ::Collection).search_results_with_work_count(:read)
-      @collections.sort_by { |coll| coll[params[:sort_type]].to_s }.reverse!
-      @collection_rows = Kaminari.paginate_array(@collections).page(params[:page]).per(10)
-
-      render json: { rows: render_to_string('hyrax/dashboard/_analytics_collections_ajax', layout: false, locals: { collection_rows: @collection_rows}) }
     end
   end
 end
