99ffcc2a0023844c484e2bbfccb9bbb0e96e492b
Jennifer Lindner <jenlindner@gmail.com>
2018-03-05 11:10:54 -0500

    fixes situation in which embargo update receives file_set ids in its batch_document_ids

--- app/controllers/concerns/hyrax/embargoes_controller_behavior.rb
+++ app/controllers/concerns/hyrax/embargoes_controller_behavior.rb
@@ -25,7 +25,13 @@ module Hyrax
       copy_visibility = params[:embargoes].values.map { |h| h[:copy_visibility] }
       ActiveFedora::Base.find(batch).each do |curation_concern|
         Hyrax::Actors::EmbargoActor.new(curation_concern).destroy
-        curation_concern.copy_visibility_to_files if copy_visibility.include?(curation_concern.id)
+        # if the concern is a FileSet, set its visibility and skip the copy_visibility_to_files, which is built for Works
+        if curation_concern.file_set?
+          curation_concern.visibility = curation_concern.to_solr["visibility_after_embargo_ssim"]
+          curation_concern.save!
+        elsif copy_visibility.include?(curation_concern.id)
+          curation_concern.copy_visibility_to_files
+        end
       end
       redirect_to embargoes_path
     end
