b9747f57a6382401092072ff8174bbcbb0c6c231
Matt Critchlow <matt.critchlow@gmail.com>
2018-03-05 13:14:57 -0800

    Add initial finders for ResourceStat
    Changes proposed:
      * `resource_daily_stats` - daily stats query for a user/resource
    combination
      * `site_daily_stats` - daily stats query for the entire site
      * `resource_range_stats` - date range query for a user/resource
    combination
      * `site_range_stats` - date range query for the entire site

--- app/models/resource_stat.rb
+++ app/models/resource_stat.rb
@@ -5,4 +5,17 @@
 # `user_id` and a `resource_id`. But that is dependent on context and documented in the scopes and related tests.
 class ResourceStat < ApplicationRecord
   validates :date, presence: true
+
+  # Daily statistics for a given resource (Work, FileSet) associated with a User
+  scope :resource_daily_stats, ->(date, resource_id, user_id) { where('date = ? AND resource_id = ? AND user_id = ?', date, resource_id, user_id) }
+  # Daily statistics for the site
+  scope :site_daily_stats, ->(date) { where('date = ? AND resource_id IS NULL AND user_id IS NULL', date) }
+
+  # Statistics for a given resource (Work, FileSet) associated with a User, in a time range
+  scope :resource_range_stats, ->(start_date, end_date, resource_id, user_id) { where('date BETWEEN ? AND ? AND resource_id = ? AND user_id = ?', start_date, end_date, resource_id, user_id) }
+  # Statistics for the site, in a time range
+  scope :site_range_stats, ->(start_date, end_date) { where('date BETWEEN ? AND ? AND resource_id IS NULL AND user_id IS NULL', start_date, end_date) }
+
+
+
 end
