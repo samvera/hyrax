84c4c9f8c08627f3c610ee1b323b65cc8bb97b78
lfarrell <lfarrell@email.unc.edu>
2018-03-12 11:34:43 -0400

    Basic sub search for server side processed DataTables

--- app/search_builders/hyrax/analytics_works_search_builder.rb
+++ app/search_builders/hyrax/analytics_works_search_builder.rb
@@ -1,9 +1,9 @@
 module Hyrax
   # Returns all works, either active or suppressed.
   # This should only be used by an admin user
-  class WorksSearchBuilder < Hyrax::SearchBuilder
+  class AnalyticsWorksSearchBuilder < Hyrax::SearchBuilder
     include Hyrax::FilterByType
-    self.default_processor_chain += [:filter_search, :sort_ordering]
+    self.default_processor_chain += [:filter_search]
     self.default_processor_chain -= [:only_active_works]
 
     def initialize(context, params = nil)
@@ -12,18 +12,19 @@ module Hyrax
     end
 
     def filter_search(solr_parameters)
-      solr_parameters[:fq] ||= []
-
-      unless @params[:search][:value].nil?
-        solr_parameters[:fq] << "#{@params[:search][:value]}"
+      unless @params[:search][:value].nil? || @params[:search][:value] == ''
+        filter_params = "/.*#{@params[:search][:value]}.*/"
+        solr_parameters[:fq] ||= []
+        solr_parameters[:fq] << "(title_tesim:#{filter_params}
+          OR date_created_tesim:#{filter_params}
+          OR visibility_ssi:#{filter_params}
+          OR human_readable_type_tesim:#{filter_params})"
       end
     end
 
     def sort_ordering(solr_parameters)
-      solr_parameters[:sort] ||= []
-
       unless @params[:sort_column].nil? || @params[:order]['0'][:dir].nil?
-        solr_parameters[:sort] << "#{@params[:sort_column]} #{@params[:order]['0'][:dir]}"
+        solr_parameters[:sort] ||= "#{@params[:sort_column]} #{@params[:order]['0'][:dir]}"
       end
     end
 
