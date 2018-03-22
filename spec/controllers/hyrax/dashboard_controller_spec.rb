b61464001006fa3fcad5aa38ff1598cefcec46fa
lfarrell <lfarrell@email.unc.edu>
2018-03-22 12:05:51 -0400

    Add controller tests

--- spec/controllers/hyrax/dashboard_controller_spec.rb
+++ spec/controllers/hyrax/dashboard_controller_spec.rb
@@ -27,6 +27,8 @@ RSpec.describe Hyrax::DashboardController, type: :controller do
     let(:service_works) { instance_double(Hyrax::WorksCountService, search_results_with_work_count: results_works) }
     let(:results_collections) { instance_double(Array) }
     let(:results_works) { instance_double(Array) }
+    let(:repo_growth) { Hyrax::Admin::RepositoryGrowthPresenter.new(90) }
+    let(:repo_objects) { Hyrax::Admin::RepositoryObjectPresenter.new('visible') }
     let(:user) { create(:admin) }
     let(:access) { :edit }
     let(:read_admin_dashboard) { true }
@@ -35,14 +37,33 @@ RSpec.describe Hyrax::DashboardController, type: :controller do
       sign_in user
       allow(Hyrax::CollectionsCountService).to receive(:new).and_return(service_collections)
       allow(Hyrax::WorksCountService).to receive(:new).and_return(service_works)
+      allow(Hyrax::Admin::RepositoryGrowthPresenter).to receive(:new).and_return(repo_growth)
+      allow(Hyrax::Admin::RepositoryObjectPresenter).to receive(:new).and_return(repo_objects)
     end
 
     it "is successful" do
       get :show
       expect(response).to be_success
       expect(assigns[:collection_rows]).to eq results_collections
-      expect(assigns[:work_rows]).to eq results_works
       expect(response).to render_template('show_admin')
+    end
+
+    it "sends repository_growth counts" do
+      get :repository_growth
+      expect(response).to be_success
+      expect(assigns[:repo_growth]).to eq repo_growth
+    end
+
+    it "sends repository object counts" do
+      get :repository_object_counts
+      expect(response).to be_success
+      expect(assigns[:repo_objects]).to eq repo_objects
+    end
+
+    it "renders works" do
+      get :update_works_list
+      expect(response).to be_success
+      expect(assigns[:work_rows]).to eq results_works
     end
   end
 end
