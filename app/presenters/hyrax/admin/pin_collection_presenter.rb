94270d58cbb044da4f8e3de143d07a6257e7014a
lfarrell <lfarrell@email.unc.edu>
2018-03-29 16:29:48 -0400

    Add tests for pinned collections

--- app/presenters/hyrax/admin/pin_collection_presenter.rb
+++ app/presenters/hyrax/admin/pin_collection_presenter.rb
@@ -1,22 +1,17 @@
 module Hyrax
   module Admin
     class PinCollectionPresenter
-
       def initialize(params)
         @params = params
       end
 
       def pin_collection
-        pinned = PinnedCollection.find_or_initialize_by(:user_id => @params[:user_id], :collection => @params[:collection])
-        pinned.update_attributes({ :pinned => @params[:status] })
-      end
-
-      def single_pinned_collections
-        PinnedCollection.where(:user_id => @params[:user_id], :collection => @params[:collection])
+        pinned = PinnedCollection.find_or_initialize_by(user_id: @params[:user_id], collection: @params[:collection])
+        pinned.update_attributes(pinned: @params[:status])
       end
 
       def all_pinned_collections
-        PinnedCollection.where(:user_id => @params[:user_id], :pinned => 1)
+        PinnedCollection.where(user_id: @params[:user_id], pinned: 1)
       end
     end
   end
