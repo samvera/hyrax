863fc1bf5af261deab6fad00237d75cc51de8fba
Carolyn Cole <cam156@psu.edu>
2018-03-30 18:55:53 -0400

    Adding select2 clicks for passing test

--- spec/features/collection_multi_membership_spec.rb
+++ spec/features/collection_multi_membership_spec.rb
@@ -60,7 +60,8 @@ RSpec.describe 'Adding a work to multiple collections', type: :feature, clean_re
 
   describe 'when both collections require single membership' do
     let(:old_collection) { create(:collection_lw, user: admin_user, collection_type_gid: single_membership_type_1.gid) }
-    let!(:work) { create(:generic_work, user: admin_user, member_of_collections: [old_collection], title: ['The highly valued work that everyone wants in their collection']) }
+    let!(:work) { create(:generic_work, user: admin_user, member_of_collections: [old_collection], title: ['The highly valued work that everyone wants in their collection'],
+                               creator: ["Fred"], keyword: ['test'], rights_statement: ['http://rightsstatements.org/vocab/InC/1.0/']) }
 
     context 'and are of different types' do
       let!(:new_collection) { create(:collection_lw, user: admin_user, collection_type_gid: single_membership_type_2.gid) }
@@ -109,95 +110,32 @@ RSpec.describe 'Adding a work to multiple collections', type: :feature, clean_re
           # skip 'Needs additional work to find and select the second collection'
           # Attempt to add to second single-membership collection of the same type
 
-byebug
           visit edit_hyrax_generic_work_path(work)
-         click_link "Relationships"
-          #byebug
+          click_link "Relationships"
 
-         fill_in('Add to collection', with: new_collection.title.first)
-          #find('//input[@id="generic_work_member_of_collection_ids"]').click 
-          #first('//a[@data-behavior="add-relationship"]').click     
-
-         fill_in('Add to collection', with: single_collection.title.first)
-          #find('//input[@id="generic_work_member_of_collection_ids"]').click 
-          #first('//a[@data-behavior="add-relationship"]').click
-
-
-
-
-          #byebug
-
-          #element = nil
-          #all('a').each { |i| element = i if i.text == 'Add' }
-          #element.click
-
-
-          #fill_in 'input[@id="generic_work_member_of_collection_ids"]', :with => page.execute_script('/authorities/search/collections?access=deposit', new_collection.title.first)
-          #within("//label[@for='generic_work_member_of_collection_ids']") do
-          #  fill_in 'Add to collection', :with => new_collection.title.first
-          #end
-
-
-          # TODO: not sure how to find and select the correct collection
-          # select2-chosen select2-chosen-3
-          # fill_in('#select2-chosen', with: :new_collection)
-          #fill_in('Add to collection', with: new_collection.title.first)
-          #page.execute_script ('input[@id="generic_work_member_of_collection_ids"]').value(new_collection.title.first).keydown()
-          #sleep 3
-          #page.execute_script("$('.select2-chosen a:contains(\"Select a collection...\")').trigger('mouseenter').click();")
-          #element = nil
-
-          #all('a').each { |i| element = i if i.text == 'Add' }
-          # e2 = nil
-          # all('a').each { |i| e2 = i if i.text == 'Select a collection...' }
-          # e2.click
-          #element.click
-
-          # click_link 'Add'
-          # fill_in('#select2-chosen', with: :single_collection)
-          #fill_in('Add to collection', with: single_collection.title.first)
-          #page.execute_script('input[@id="generic_work_member_of_collection_ids"]').value(single_collection.title.first).keydown()
-          #page.execute_script("$('.select2-chosen a:contains(\"Select a collection...\")').trigger('mouseenter').click();")
-          #element = nil
-          # e2 = nil
-          #all('a').each { |i| element = i if i.text == 'Add' }
-          # all('a').each { |i| e2 = i if i.text == 'Select a collection...' }
-          # e2.click
-          #element.click
-          # click_link 'Add'
-          # find('body').click
-          # check required acceptance
+          first('a.select2-choice').click
+          find('.select2-input').set(new_collection.title.first)
+          expect(page).to have_css('div.select2-result-label')
+          first('div.select2-result-label').click
+          first('[data-behavior~=add-relationship]').click
+          within('[data-behavior~=collection-relationships]') do
+            within('table.table.table-striped') do
+              expect(page).to have_content(new_collection.title.first)
+            end
+          end
           check('agreement')
           sleep 3
           choose('generic_work_visibility_open')
           sleep 3
 
-byebug
           within('div#savewidget') do
            element = nil
            all('input').each { |i| element = i if i.value == 'Save changes' }
-           byebug
            element.click
           end
 
-
-          # byebug
-          #element = nil
-          #all('input').each { |i| element = i if i.value == 'Save changes' }
-          #byebug
-          #element.click
-          # click_on('Save changes')
-          # within('.panel-footer') do
-          #  click_button('Save changes')
-          # end
-
-          # forwards to work index page and shows flash message
-          # expect(page).to have_link 'All Works'
-          # expect(page).to have_link 'Your Works'
-
-          err_message = "Error: You have specified more than one of the same single-membership collection types: " \
-                        "Single-membership 1 (Collection 1 for SM1)"
-          expect(page).to have_selector '.alert', text: err_message
+          err_message = "Single collection Error: You have specified more than one of the same single-membership collection types: Single-membership 1 (Collection Title 1 and Collection Title 2)"
+          expect(page).to have_selector '.help-block', text: err_message
         end
 
         it "from the collection's show page Add to collection" do
