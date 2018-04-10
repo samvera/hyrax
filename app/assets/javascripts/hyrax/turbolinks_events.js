3f2f0344926cc442ff15d74a593bb937679d7226
Chris Colvard <cjcolvar@indiana.edu>
2018-04-10 14:18:24 -0400

    Force loading of UV by resetting flag in turbolinks:load event handler

--- app/assets/javascripts/hyrax/turbolinks_events.js
+++ app/assets/javascripts/hyrax/turbolinks_events.js
@@ -2,4 +2,7 @@
 // See https://github.com/rails/jquery-ujs/issues/456
 $(document).on('turbolinks:load', function() {
   $.rails.refreshCSRFTokens();
+  // Explicitly set flag to false to force loading of UV
+  // See https://github.com/samvera/hyrax/issues/2906
+  window.embedScriptIncluded = false;
 });
