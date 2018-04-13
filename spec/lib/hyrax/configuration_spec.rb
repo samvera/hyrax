dff48e815410120ad5999e39d1471a3e672aef2e
Bess Sadler <bess@curationexperts.com>
2018-04-13 17:18:18 -0400

    Back-port of configuration option for full-text extraction
    Allow configuration to turn off the full-text extraction
    during derivative creation.

--- spec/lib/hyrax/configuration_spec.rb
+++ spec/lib/hyrax/configuration_spec.rb
@@ -53,6 +53,7 @@ describe Hyrax::Configuration do
   it { is_expected.to respond_to(:translate_uri_to_id) }
   it { is_expected.to respond_to(:upload_path) }
   it { is_expected.to respond_to(:work_requires_files?) }
+  it { is_expected.to respond_to(:extract_full_text?) }
   it { is_expected.to respond_to(:whitelisted_ingest_dirs) }
   it { is_expected.to respond_to(:whitelisted_ingest_dirs=) }
 end
