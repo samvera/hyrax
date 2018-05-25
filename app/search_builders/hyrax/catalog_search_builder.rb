6e8fb69b337a3ec9ef1ec201feff8773a1b984a1
Chris Colvard <cjcolvar@indiana.edu>
2018-05-25 16:36:34 -0400

    Switch to using member_of_collection_ids_ssim to power collection facet and filter values based upon a whitelist of collections that the user has access to read then resolve these ids to human readable names using a helper method.
    Co-authored-by: phuongdh <phuongdh@gmail.com>

--- app/search_builders/hyrax/catalog_search_builder.rb
+++ app/search_builders/hyrax/catalog_search_builder.rb
@@ -2,7 +2,8 @@ class Hyrax::CatalogSearchBuilder < Hyrax::SearchBuilder
   self.default_processor_chain += [
     :add_access_controls_to_solr_params,
     :show_works_or_works_that_contain_files,
-    :show_only_active_records
+    :show_only_active_records,
+    :filter_collection_facet_for_access
   ]
 
   # show both works that match the query and works that contain files that match the query
@@ -17,6 +18,18 @@ class Hyrax::CatalogSearchBuilder < Hyrax::SearchBuilder
   def show_only_active_records(solr_parameters)
     solr_parameters[:fq] ||= []
     solr_parameters[:fq] << '-suppressed_bsi:true'
+  end
+
+  # only return facet counts for collections that this user has access to see
+  def filter_collection_facet_for_access(solr_parameters)
+    return if current_ability.admin?
+
+    collection_ids = Hyrax::Collections::PermissionsService.collection_ids_for_view(ability: current_ability).map { |id| "^#{id}$" }
+    solr_parameters['f.member_of_collection_ids_ssim.facet.matches'] = if collection_ids.present?
+                                                                         collection_ids.join('|')
+                                                                       else
+                                                                         "^$"
+                                                                       end
   end
 
   private
