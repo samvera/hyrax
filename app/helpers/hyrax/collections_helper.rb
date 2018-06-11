24e9529334eee060380782d1ff9af6cc58370ee1
E. Lynette Rayle <elr37@cornell.edu>
2018-06-11 08:27:04 -0400

    namespace call to route helper path collection_path

--- app/helpers/hyrax/collections_helper.rb
+++ app/helpers/hyrax/collections_helper.rb
@@ -11,7 +11,7 @@ module Hyrax
     def render_collection_links(solr_doc)
       collection_list = Hyrax::CollectionMemberService.run(solr_doc, controller.current_ability)
       return if collection_list.empty?
-      links = collection_list.map { |collection| link_to collection.title_or_label, collection_path(collection.id) }
+      links = collection_list.map { |collection| link_to collection.title_or_label, hyrax.collection_path(collection.id) }
       collection_links = []
       links.each_with_index do |link, n|
         collection_links << link
