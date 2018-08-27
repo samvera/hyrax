9b47e739123eaaf2f385abde7f05c481083f399c
Justin Coyne <jcoyne@justincoyne.com>
2018-08-27 11:45:13 -0500

    Don't show trackable fields if Devise trackable isn't installed
    Fixes #3210

--- app/presenters/hyrax/admin/users_presenter.rb
+++ app/presenters/hyrax/admin/users_presenter.rb
@@ -20,6 +20,12 @@ module Hyrax
         user.last_sign_in_at || user.created_at
       end
 
+      # return [Boolean] true if the devise trackable module is enabled.
+      def show_last_access?
+        return @show_last_access unless @show_last_access.nil?
+        @show_last_access = ::User.devise_modules.include?(:trackable)
+      end
+
       private
 
         # Returns a list of users excluding the system users and guest_users
--- spec/views/hyrax/admin/users/index.html.erb_spec.rb
+++ spec/views/hyrax/admin/users/index.html.erb_spec.rb
@@ -3,19 +3,43 @@ RSpec.describe 'hyrax/admin/users/index.html.erb', type: :view do
   let(:users) { [] }
 
   before do
-    (1..4).each { |i| users << build(:user, display_name: "user#{i}", email: "email#{i}@example.com", last_sign_in_at: Time.zone.now - 15.minutes, created_at: Time.zone.now - 3.days) }
+    (1..4).each { |i| users << build(:user, display_name: "user#{i}", email: "email#{i}@example.com", created_at: Time.zone.now - 3.days) }
     allow(presenter).to receive(:users).and_return(users)
     assign(:presenter, presenter)
+    allow(presenter).to receive(:show_last_access?).and_return(show_last_access)
   end
 
-  it "draws user list with all users" do
+  let(:page) do
     render
-    page = Capybara::Node::Simple.new(rendered)
-    expect(page).to have_content("Username")
-    expect(page).to have_content("Roles")
-    expect(page).to have_content("Last access")
-    (1..4).each do |i|
-      expect(page).to have_content("email#{i}@example.com")
+    Capybara::Node::Simple.new(rendered)
+  end
+
+  context 'when show_last_access? is true' do
+    let(:show_last_access) { true }
+    before do
+      allow(presenter).to receive(:last_accessed).and_return(Time.zone.now - 3.days)
+    end
+
+    it 'draws user list with all users' do
+      expect(page).to have_content("Username")
+      expect(page).to have_content("Roles")
+      expect(page).to have_content("Last access")
+      (1..4).each do |i|
+        expect(page).to have_content("email#{i}@example.com")
+      end
+    end
+  end
+
+  context 'when show_last_access? is false' do
+    let(:show_last_access) { false }
+
+    it 'draws user list with all users' do
+      expect(page).to have_content("Username")
+      expect(page).to have_content("Roles")
+      expect(page).not_to have_content("Last access")
+      (1..4).each do |i|
+        expect(page).to have_content("email#{i}@example.com")
+      end
     end
   end
 end
