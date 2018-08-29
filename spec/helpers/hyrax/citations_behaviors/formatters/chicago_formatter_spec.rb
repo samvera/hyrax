99b577aa7d6fed9c57611d3b153f1d02e91c19f8
Tom Johnson <tom@curationexperts.com>
2018-08-29 15:47:30 -0700

    Don't allow XSS on title in Chicago Formatter
    There's probably a larger fix related to forms here, but the chicago formatter
    needn't be so permissive.
    
    Related to #3187.

---
+++ spec/helpers/hyrax/citations_behaviors/formatters/chicago_formatter_spec.rb
@@ -0,0 +1,10 @@
+RSpec.describe Hyrax::CitationsBehaviors::Formatters::ChicagoFormatter do
+  subject(:formatter) { described_class.new(:no_context) }
+
+  let(:presenter) { Hyrax::WorkShowPresenter.new(SolrDocument.new(work.to_solr), :no_ability) }
+  let(:work)      { build(:generic_work, title: ['<ScrIPt>prompt("Confirm Password")</sCRIpt>']) }
+
+  it 'sanitizes input' do
+    expect(formatter.format(presenter)).not_to include 'prompt'
+  end
+end
