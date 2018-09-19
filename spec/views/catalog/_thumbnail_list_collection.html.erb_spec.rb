4bd961a097504c1a5cb106397905b0c0b18264c8
Huawei Weng <hweng@ucsd.edu>
2018-09-19 12:54:41 -0700

    Fixes #3164 - Collection Thumbnails don't show in search results
    Updating tests for thumbnail_list_collection.#3291

--- spec/views/catalog/_thumbnail_list_collection.html.erb_spec.rb
+++ spec/views/catalog/_thumbnail_list_collection.html.erb_spec.rb
@@ -1,10 +1,43 @@
 RSpec.describe 'catalog/_thumbnail_list_collection.html.erb', type: :view do
-  before do
-    stub_template 'catalog/_thumbnail_list_collection.html.erb' => '<div class="col-sm-3"><span class="fa fa-cubes collection-icon-search"></span></div>'
-    render
+  context "When the collection thumbnail is attached" do
+    let(:attributes) do
+      { id: "xxx",
+        "has_model_ssim": ["Collection"],
+        "title_tesim": ["Collection Title"],
+        "description_tesim": ["Collection Description"],
+        "system_modified_dtsi": 'date',
+        "thumbnail_path_ss": '/xxx/yyy?file=thumbnail' }
+    end
+    let(:doc) { SolrDocument.new(attributes) }
+    let(:current_ability) { Ability.new(build(:user)) }
+
+    before do
+      render 'catalog/thumbnail_list_collection', document: doc
+    end
+
+    it 'displays the collection thumbnail in the search results' do
+      expect(rendered).to include '/xxx/yyy?file=thumbnail'
+    end
   end
 
-  it 'displays the collection icon in the search results' do
-    expect(rendered).to match '<div class="col-sm-3"><span class="fa fa-cubes collection-icon-search"></span></div>'
+  context "When the collection thumbnail is not attached" do
+    let(:attributes) do
+      { id: "xxx",
+        "has_model_ssim": ["Collection"],
+        "title_tesim": ["Collection Title"],
+        "description_tesim": ["Collection Description"],
+        "system_modified_dtsi": 'date',
+        "thumbnail_path_ss" => Hyrax::CollectionIndexer.thumbnail_path_service.default_image }
+    end
+    let(:doc) { SolrDocument.new(attributes) }
+    let(:current_ability) { Ability.new(build(:user)) }
+
+    before do
+      render 'catalog/thumbnail_list_collection', document: doc
+    end
+
+    it 'displays the collection icon in the search results' do
+      expect(rendered).to include '/assets/collection-'
+    end
   end
 end
