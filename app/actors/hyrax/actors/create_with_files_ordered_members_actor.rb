dc1243e737383849eda847ce74aeb67bff11e39a
Julie Allinson <julie.allinson@london.ac.uk>
2018-11-05 09:04:05 +0000

    Extra documentation for the ordered members change in #fdaa8bc7bf45cfa1107296a4def908b63eff1a97

--- app/actors/hyrax/actors/create_with_files_ordered_members_actor.rb
+++ app/actors/hyrax/actors/create_with_files_ordered_members_actor.rb
@@ -1,5 +1,37 @@
 module Hyrax
   module Actors
+    # When adding member FileSets to a Work, Hyrax saves
+    #   and reloads the work for each new member FileSet.
+    #   This can significantly slow down ingest for Works
+    #   with many member FileSets. The saving and reloading
+    #   happens in FileSetActor#attach_to_work.
+    #
+    # This is a 'swappable' alternative approach. It will
+    #   be of most value to Hyrax applications dealing with
+    #   works with many filesets. Anecdotally, a work with
+    #   600 filesets can be processed in ~15 mins versus
+    #   > 3 hours with the standard approach.
+    #
+    # The tradeoff is that the ordered members are now added in a
+    #   single step after the creation of all the FileSets, thus
+    #   introducing a slight risk of orphan filesets if the upload
+    #   fails before the addition of the ordered members. This
+    #   has not been observed in practice.
+    #
+    # Swapping out the actors can be achieved thus:
+    #
+    # In `config/initializers/hyrax.rb`:
+    # ```
+    # Hyrax::CurationConcern.actor_factory.swap(Hyrax::Actors::CreateWithFilesActor,
+    #   Hyrax::Actors::CreateWithFilesOrderedMembersActor)
+    # ```
+    # Alternatively, in `config/application.rb`:
+    # ```
+    # config.to_prepare
+    #   Hyrax::CurationConcern.actor_factory.swap(Hyrax::Actors::CreateWithFilesActor,
+    #     Hyrax::Actors::CreateWithFilesOrderedMembersActor)
+    # end
+    # ```
     # Creates a work and attaches files to the work
     class CreateWithFilesOrderedMembersActor < CreateWithFilesActor
       # @return [TrueClass]
