7d789f9cf885229dd8892d1eabd1c41c698ec64c
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2019-01-30 14:50:29 -0500

    Setting Valkyrie as a development dependency
    Steps to test:
    
    * Generate a new Rails application (using defaults)
    * Add Hyrax to the Gemfile (as per README documentation)
    * Bundle new Rails application
    * Verify that `bundle exec gem list | grep valkyrie` is blank (eg.
      Valkyrie is not loaded as a dependency)
    * Spin up `rails server` on the new Rails application
    * Then review `log/development.log` and look for the info message from
      the `Hyrax::Engine.initializer` (e.g. "Hyrax::Engine.initializer did
      not load Valkyrie")
    
    In particular, we do not want to force upstream dependencies on
    Postgres.
    
    This is related to:
    
    * samvera-labs/valkyrie#640
    * samvera-labs/valkyrie#655

--- lib/wings/valkyrie_monkey_patch.rb
+++ lib/wings/valkyrie_monkey_patch.rb
@@ -1,24 +1,25 @@
-require 'valkyrie'
+if defined?(Valkyrie)
 
-# TODO: See https://github.com/samvera-labs/valkyrie/pull/654
-warning_message = "Monkey Patch Invalidated?\n\tIt appears that https://github.com/samvera-labs/valkyrie/pull/654 has been merged, look to the details of that PR to look to remove this monkey patch"
-raise(warning_message) if Valkyrie.config.respond_to?(:resource_class_resolver)
+  # TODO: See https://github.com/samvera-labs/valkyrie/pull/654
+  warning_message = "Monkey Patch Invalidated?\n\tIt appears that https://github.com/samvera-labs/valkyrie/pull/654 has been merged, look to the details of that PR to look to remove this monkey patch"
+  raise(warning_message) if Valkyrie.config.respond_to?(:resource_class_resolver)
 
-require 'wings/resource_factory'
+  require 'wings/resource_factory'
 
-module Valkyrie
-  module MonkeyPatch
-    module ResourceClassResolver
-      def resource_klass
-        Wings::ResourceFactory.convert_class_name_to_valkyrie_resource_class(internal_resource)
+  module Valkyrie
+    module MonkeyPatch
+      module ResourceClassResolver
+        def resource_klass
+          Wings::ResourceFactory.convert_class_name_to_valkyrie_resource_class(internal_resource)
+        end
       end
     end
-  end
-end
 
-class Valkyrie::Persistence::Postgres::ORMConverter
-  include Valkyrie::MonkeyPatch::ResourceClassResolver
-end
-class Valkyrie::Persistence::Solr::ORMConverter
-  include Valkyrie::MonkeyPatch::ResourceClassResolver
+    class Valkyrie::Persistence::Postgres::ORMConverter
+      include Valkyrie::MonkeyPatch::ResourceClassResolver
+    end
+    class Valkyrie::Persistence::Solr::ORMConverter
+      include Valkyrie::MonkeyPatch::ResourceClassResolver
+    end
+  end
 end
