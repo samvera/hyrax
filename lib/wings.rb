f701944afdb0748b0b6d2f34883c44362cdfaa3a
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2019-01-30 14:50:29 -0500

    Creating the Wings namespace file
    Responsible for coordinating the wings related requires.

---
+++ lib/wings.rb
@@ -0,0 +1,11 @@
+# The isolating namespace for integrating Valkyrie into Hyrax as a bridge away
+# from the hard dependency on ActiveFedora.
+#
+# @see https://wiki.duraspace.org/display/samvera/Hyrax-Valkyrie+Development+Working+Group
+#      for further context regarding the approach
+module Wings
+end
+
+require 'valkyrie'
+require 'wings/resource_factory'
+require 'wings/valkyrie_monkey_patch'
