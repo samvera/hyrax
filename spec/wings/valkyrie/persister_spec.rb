fecee743bbef20c57d6533772e8f2f9a2e68d5f0
Chris Colvard <cjcolvar@indiana.edu>
2019-02-01 11:05:06 -0500

    Persister saves

--- spec/wings/valkyrie/persister_spec.rb
+++ spec/wings/valkyrie/persister_spec.rb
@@ -4,14 +4,62 @@ require 'valkyrie/specs/shared_specs'
 require 'wings'
 
 RSpec.describe Wings::Valkyrie::Persister do
+  subject(:persister) { described_class.new(adapter: adapter) }
+  let(:adapter) { Wings::Valkyrie::MetadataAdapter.new }
+  let(:query_service) { adapter.query_service }
+  let(:af_resource_class) do
+    Class.new(ActiveFedora::Base) do
+      property :title, predicate: ::RDF::Vocab::DC.title, multiple: true
+    end
+  end
+  let(:resource_class) { Wings::ModelTransformer.to_valkyrie_resource_class(klass: af_resource_class) }
+  let(:resource) { resource_class.new(title: ['Foo']) }
+
   # it_behaves_like "a Valkyrie::Persister"
 
-  let(:adapter) { Wings::Valkyrie::MetadataAdapter.new }
-  let(:subject) { described_class.new(adapter: adapter) }
+  it { is_expected.to respond_to(:save).with_keywords(:resource) }
+  xit { is_expected.to respond_to(:save_all).with_keywords(:resources) }
+  xit { is_expected.to respond_to(:delete).with_keywords(:resource) }
 
-  it 'responds to expected methods' do
-    expect(subject).to respond_to(:save)
-    expect(subject).to respond_to(:delete)
-    expect(subject).to respond_to(:resource_factory)
+  it "can save a resource" do
+    expect(resource).not_to be_persisted
+    saved = persister.save(resource: resource)
+    expect(saved).to be_persisted
+    expect(saved.id).not_to be_blank
+  end
+
+  it "stores created_at/updated_at" do
+    book = persister.save(resource: resource)
+    book.title = "test"
+    book = persister.save(resource: book)
+    expect(book.created_at).not_to be_blank
+    expect(book.updated_at).not_to be_blank
+    expect(book.created_at).not_to be_kind_of Array
+    expect(book.updated_at).not_to be_kind_of Array
+    expect(book.updated_at > book.created_at).to eq true
+  end
+
+  it "can override default id generation with a provided id" do
+    id = SecureRandom.uuid
+    book = persister.save(resource: resource_class.new(id: id, title: ['Foo']))
+    reloaded = query_service.find_by(id: book.id)
+    expect(reloaded.id).to eq Valkyrie::ID.new(id)
+    expect(reloaded).to be_persisted
+    expect(reloaded.created_at).not_to be_blank
+    expect(reloaded.updated_at).not_to be_blank
+    expect(reloaded.created_at).not_to be_kind_of Array
+    expect(reloaded.updated_at).not_to be_kind_of Array
+  end
+
+  it "doesn't override a resource that already has an ID" do
+    book = persister.save(resource: resource_class.new(title: ['Foo']))
+    id = book.id
+    output = persister.save(resource: book)
+    expect(output.id).to eq id
+  end
+
+  it "can find that resource again" do
+    id = persister.save(resource: resource).id
+    expect(query_service.find_by(id: id)).to be_kind_of resource_class
   end
 end
