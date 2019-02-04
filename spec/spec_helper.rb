3c23dbcd6fdd7187366640726ebd05badad305e3
Tom Johnson <tomjohnson@ucsb.edu>
2019-02-04 10:26:23 -0800

    Re-enable network connections at the end of the test suite
    This avoids various errors handling Selenium sessions at the tail end of the
    test suite.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -169,13 +169,8 @@ RSpec.configure do |config|
       DatabaseCleaner.start
     end
 
-    if example.metadata[:type] == :view
-      # View tests should not hit any services. This ensures the tests are unit
-      # testing only the views and run fast.
-      WebMock.disable_net_connect!(allow_localhost: false)
-    else
-      WebMock.disable_net_connect!(allow_localhost: true)
-    end
+    WebMock.disable_net_connect!(allow_localhost: true)
+
     # using :workflow is preferable to :clean_repo, use the former if possible
     # It's important that this comes after DatabaseCleaner.start
     ensure_deposit_available_for(user) if example.metadata[:workflow]
@@ -195,7 +190,19 @@ RSpec.configure do |config|
   end
 
   config.include(ControllerLevelHelpers, type: :view)
-  config.before(:each, type: :view) { initialize_controller_helpers(view) }
+
+  config.before(:each, type: :view) do
+    # View tests should not hit any services. This ensures the tests are unit
+    # testing only the views and run fast.
+    WebMock.disable_net_connect!(allow_localhost: false)
+
+    initialize_controller_helpers(view)
+  end
+
+  config.after(:each, type: :view) do
+    # reset webmock to allow localhost connections
+    WebMock.disable_net_connect!(allow_localhost: true)
+  end
 
   config.before(:all, type: :feature) do
     # Assets take a long time to compile. This causes two problems:
