7f2562b5101ec195fc41ece1172e51ee314e8b88
joachim.dornbusch@ehess.fr <joachim.dornbusch@ehess.fr>
2019-02-19 10:00:03 +0100

    Extract and translate strings.
    Translate title in file show page.
    
    Translate "Depositor" in file show page.
    
    Translate fields in file show page.
    
    Case problem
    
    Translate actions in file show page.
    
    Translate "sort and per page" for collections show view in back office
    
    Translate "sort and per page" for collections show view in front office
    
    Translate title in "sort and per page" for collections show view in front office
    
    Page crashes in french because of missing entries in i18n file
    
    Fix test failing with "translation missing: en.hyrax.collections.sort_and_per_page.html.erb.sort_by_html"
    
    Translate "choose file" in collection branding tab
    
    Translate delete banner controls in collection branding tab
    
    Translate second "choose file" in collection branding tab
    
    Translate "link url" in collection branding tab
    
    Translate "alt text", "link url", "previous" in collection branding tab
    
    Translate search controles in collection sharing tab
    
    Translate roles in collection sharing tab
    
    Fix test failing with "translation missing: en.hyrax.collections.sort_and_per_page.html.erb.sort_by_html"
    
    German translations improvements
    
    Syntax errors in german translation
    
    Case error

--- spec/views/hyrax/dashboard/collections/_sort_and_per_page.html.erb_spec.rb
+++ spec/views/hyrax/dashboard/collections/_sort_and_per_page.html.erb_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe 'hyrax/dashboard/collections/_sort_and_per_page.html.erb', type: :view do
-  let(:subject) { 'hyrax/dashboard/collections/sort_and_per_page.html.erb' }
+  let(:subject) { 'hyrax/dashboard/collections/sort_and_per_page' }
   let(:collection) { instance_double(Collection) }
 
   before do
