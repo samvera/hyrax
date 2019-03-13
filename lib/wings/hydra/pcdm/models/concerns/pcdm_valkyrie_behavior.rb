6bf0eab3290bd4bd09bdba1fef6a3131c55a82a8
E. Lynette Rayle <elr37@cornell.edu>
2019-03-13 11:40:17 -0400

    implement #object_ids from pcdm_behavior

--- lib/wings/hydra/pcdm/models/concerns/pcdm_valkyrie_behavior.rb
+++ lib/wings/hydra/pcdm/models/concerns/pcdm_valkyrie_behavior.rb
@@ -4,12 +4,19 @@ module Wings
   module PcdmValkyrieBehavior
     ##
     # Gives the subset of #members that are PCDM objects
-    # @return [Enumerable<PCDM::ObjectBehavior>] an enumerable over the members
+    # @return [Enumerable<ActiveFedora::Base> | Enumerable<Valkyrie::Resource>] an enumerable over the members
     #   that are PCDM objects
     def objects(valkyrie: false)
       af_objects = Wings::ActiveFedoraConverter.new(resource: self).convert.objects
       return af_objects unless valkyrie
       af_objects.map(&:valkyrie_resource)
+    end
+
+    ##
+    # Gives a subset of #member_ids, where all elements are PCDM objects.
+    # @return [Enumerable<String> | Enumerable<Valkyrie::ID] the object ids
+    def object_ids(valkyrie: false)
+      objects(valkyrie: valkyrie).map(&:id)
     end
   end
 end
