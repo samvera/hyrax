7043ac251817b2dc3ebb9594570e384793d65987
Anna Headley <anna3LC@gmail.com>
2019-03-13 11:43:44 -0400

    Remove Wings::ResourceFactory
    No longer in use, and confusing
    
    Co-authored-by: LaRita Robinson <laritakr@gmail.com>

--- lib/wings/resource_factory.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-module Wings
-  # Preserving the old class for short-standing compliance
-  # with other Wings development. This can go away as part of the
-  # end of the Penn State 2019 winter developer sprint.
-  class ResourceFactory < ModelTransformer; end
-end
