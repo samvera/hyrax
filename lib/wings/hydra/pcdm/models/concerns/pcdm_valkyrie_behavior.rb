83a1e8653e2613c83a35059eb90eeda34457e842
E. Lynette Rayle <elr37@cornell.edu>
2019-03-13 15:59:07 -0400

    define member_ids as ordered
    * uses #objects method for #members and #ordered_members.  All of these will return the members in order because member_ids is ordered.
    
    TODO: The question is how to handle…
    
    ```
    obj1.members << obj2
    obj1.members += obj3
    obj1.ordered_members << obj4
    obj1.ordered_members += obj5
    ```

--- lib/wings/hydra/pcdm/models/concerns/pcdm_valkyrie_behavior.rb
+++ lib/wings/hydra/pcdm/models/concerns/pcdm_valkyrie_behavior.rb
@@ -1,17 +1,30 @@
+require 'wings/value_mapper'
 require 'wings/active_fedora_converter'
 
 module Wings
   module Pcdm
     module PcdmValkyrieBehavior
+    extend ActiveSupport::Concern
+
+      included do
+        attribute :member_ids, ::Valkyrie::Types::Array.of(::Valkyrie::Types::ID).meta(ordered: true)
+        # TODO: get/set via members and ordered_members
+        #   * get - For both, this is the same as #objects. Because the Array in Valkyrie is ordered, everything will be ordered
+        #   * set - In AF, members and ordered_members are enumerable and can be set using operators << and +=.
+        #           Since member_ids is all that keeps these in wings, how can we do that here?
+      end
+
       ##
       # Gives the subset of #members that are PCDM objects
-      # @return [Enumerable<PCDM::ObjectBehavior>] an enumerable over the members
+      # @return [Enumerable<ActiveFedora::Base> | Enumerable<Valkyrie::Resource>] an enumerable over the members
       #   that are PCDM objects
       def objects(valkyrie: false)
         af_objects = Wings::ActiveFedoraConverter.new(resource: self).convert.objects
         return af_objects unless valkyrie
         af_objects.map(&:valkyrie_resource)
       end
+      alias members objects
+      alias ordered_members objects
 
       ##
       # Gives a subset of #member_ids, where all elements are PCDM objects.
