ca89a9cd9ced83a2f4aeb99807ac490f79aa7d4d
Ryan Wick <ryanwick@gmail.com>
2019-03-27 10:54:03 -0700

    Remove second occurrence of :height

--- app/presenters/hyrax/characterization_behavior.rb
+++ app/presenters/hyrax/characterization_behavior.rb
@@ -5,7 +5,7 @@ module Hyrax
     class_methods do
       def characterization_terms
         [
-          :byte_order, :compression, :height, :width, :height, :color_space,
+          :byte_order, :compression, :height, :width, :color_space,
           :profile_name, :profile_version, :orientation, :color_map, :image_producer,
           :capture_device, :scanning_software, :gps_timestamp, :latitude, :longitude,
           :file_format, :file_title, :page_count, :duration, :sample_rate,
