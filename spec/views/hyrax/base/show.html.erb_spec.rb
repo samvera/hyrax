de959fa6056930fc487bd8a890cf4fe4eb8d84e1
Julie Allinson <julie.allinson@london.ac.uk>
2019-04-17 20:53:51 +0100

    Replace pul_uv_rails (#3696)
    * Replace pul_uv_rails with a direct install of universal viewer. pul_uv_rail is no longer maintained.
    
    Co-Authored-By: geekscruff <julie.allinson@london.ac.uk>

--- spec/views/hyrax/base/show.html.erb_spec.rb
+++ spec/views/hyrax/base/show.html.erb_spec.rb
@@ -80,7 +80,7 @@ RSpec.describe 'hyrax/base/show.html.erb', type: :view do
       let(:viewer_enabled) { true }
 
       it 'renders the UniversalViewer' do
-        expect(page).to have_selector 'div.viewer'
+        expect(page).to have_selector 'div.viewer-wrapper'
       end
     end
 
@@ -88,7 +88,7 @@ RSpec.describe 'hyrax/base/show.html.erb', type: :view do
       let(:viewer_enabled) { false }
 
       it 'omits the UniversalViewer' do
-        expect(page).not_to have_selector 'div.viewer'
+        expect(page).not_to have_selector 'div.viewer-wrapper'
       end
     end
   end
