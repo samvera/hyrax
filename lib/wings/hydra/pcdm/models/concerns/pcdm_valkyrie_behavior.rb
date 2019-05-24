bd70cdf7066bb7f718fb5cfca28e5993363bea3d
Josh Gum <revgum@gmail.com>
2019-05-24 07:16:13 -0700

    alias ordered_collection* to pcdm collections in wings adapter

--- lib/wings/hydra/pcdm/models/concerns/pcdm_valkyrie_behavior.rb
+++ lib/wings/hydra/pcdm/models/concerns/pcdm_valkyrie_behavior.rb
@@ -43,6 +43,7 @@ module Wings
 
       ##
       # @return [Enumerable<ActiveFedora::Base> | Enumerable<Valkyrie::Resource>] an enumerable over the child collections
+      # @todo There is no guarantee to collection ordering until Hyrax is fully valkyrie-native, see issue 3784
       def child_collections(valkyrie: false)
         @child_collections_navigator ||= ChildCollectionsNavigator.new(query_service: ::Valkyrie.config.metadata_adapter.query_service)
         resources = @child_collections_navigator.find_child_collections(resource: self)
@@ -51,22 +52,25 @@ module Wings
       end
       alias collections child_collections
       alias member_collections child_collections
+      alias ordered_collections child_collections
 
       ##
       # @return [Enumerable<String> | Enumerable<Valkyrie::ID] the child collection ids
+      # @todo There is no guarantee to collection ordering until Hyrax is fully valkyrie-native, see issue 3784
       def child_collection_ids(valkyrie: false)
         child_collections(valkyrie: valkyrie).map(&:id)
       end
       alias collection_ids child_collection_ids
       alias member_collection_ids child_collection_ids
+      alias ordered_collection_ids child_collection_ids
 
       ##
       # Gives the subset of #members that are PCDM objects
       # @return [Enumerable<ActiveFedora::Base> | Enumerable<Valkyrie::Resource>] an enumerable over the members
       #   that are PCDM objects
-      # NOTE: A collection could be added to members, but for Hyrax collections are stored using member_of_collection_ids.
+      # @note A collection could be added to members, but for Hyrax collections are stored using member_of_collection_ids.
       #       As long as this rule holds, then members and ordered_members are equivalent to objects/child_objects.
-      # TODO: Do we want to support members separate from child_objects (see NOTE)
+      # @todo Do we want to support members separate from child_objects (see NOTE)
       def child_objects(valkyrie: false)
         af_objects = Wings::ActiveFedoraConverter.new(resource: self).convert.objects
         return af_objects unless valkyrie
@@ -81,9 +85,9 @@ module Wings
       ##
       # Gives a subset of #member_ids, where all elements are PCDM objects.
       # @return [Enumerable<String> | Enumerable<Valkyrie::ID] the object ids
-      # NOTE: A collection could be added to member_ids, but for Hyrax collections are stored using member_of_collection_ids.
+      # @note A collection could be added to member_ids, but for Hyrax collections are stored using member_of_collection_ids.
       #       As long as this rule holds, then member_ids and ordered_member_ids are equivalent to object_ids/child_object_ids.
-      # TODO: Do we want to support member_ids separate from child_objects (see NOTE)
+      # @todo Do we want to support member_ids separate from child_objects (see NOTE)
       def child_object_ids(valkyrie: false)
         child_objects(valkyrie: valkyrie).map(&:id)
       end
@@ -96,7 +100,7 @@ module Wings
       ##
       # @return [Enumerable<ActiveFedora::Base> | Enumerable<Valkyrie::Resource>] the collections the
       #    collection or object is a member of.
-      # NOTE: For Hyrax, member_of_collection_ids is used to hold the parent collections.
+      # @note For Hyrax, member_of_collection_ids is used to hold the parent collections.
       #       members is used to hold the child works and child filesets.  This method looks
       #       in solr for the members relationship and infers a member_of relationship through
       #       a solr query by looking for all parents that have self's id in their members list.
@@ -111,7 +115,7 @@ module Wings
 
       # @return [Enumerable<ActiveFedora::Base> | Enumerable<Valkyrie::Resource>] the collections the
       #   object is a member of.
-      # TODO: This is using the member_of method which uses the members relationship.  That should
+      # @todo This is using the member_of method which uses the members relationship.  That should
       #       never return any collections since they are stored using the member_of_collection
       #       relationship.  But this method is called in characterize_job.rb and collection_indexer.rb.
       #       It could be that these are no-ops because nothing is ever returned, but more exploration
@@ -121,7 +125,7 @@ module Wings
       end
 
       # @return [Enumerable<String> | Enumerable<Valkyrie::ID] ids for collections the object is a member of
-      # TODO: This is using the member_of method which uses the members relationship.  That should
+      # @todo This is using the member_of method which uses the members relationship.  That should
       #       never return any collections since they are stored using the member_of_collection
       #       relationship.  But this method is called in base_actor.rb #destroy.
       #       It could be that these are no-ops because nothing is ever returned, but more exploration
@@ -152,7 +156,7 @@ module Wings
 
       # @param valkyrie [Boolean] Should the returned ids be for Valkyrie or AF objects?
       # @return [Enumerable<Hydra::Works::Work>] The works this work is contained in
-      # NOTE: This method avoids using the Hydra::Works version of parent_works because of Issue #361
+      # @note This method avoids using the Hydra::Works version of parent_works because of Issue #361
       def parent_works(valkyrie: false)
         af_child = Wings::ActiveFedoraConverter.new(resource: self).convert
         af_parents = af_child.member_of_works
