754485f2e271d9eff092063bededebe8de1889ce
Tom Johnson <tomjohnson@ucsb.edu>
2019-05-29 13:21:48 -0700

    [wings] Foward `Hyrax::SolrService` methods to the AF instance
    Reduces configuration overhead in `Hyrax::SolrService` until the dependency can
    be removed.

--- app/services/hyrax/solr_service.rb
+++ app/services/hyrax/solr_service.rb
@@ -4,7 +4,16 @@ module Hyrax
   #
   # This class replaces `ActiveFedora::SolrService`, which is deprecated for
   # internal use.
-  class SolrService < ActiveFedora::SolrService
+  class SolrService
+    extend Forwardable
+
+    def_delegators :@old_service, :add, :commit, :count, :delete, :get, :instance, :post, :query
+    def_delegators :instance, :conn
+
+    def initialize
+      @old_service = ActiveFedora::SolrService
+    end
+
     class << self
       ##
       # We don't implement `.select_path` instead configuring this at the Hyrax
@@ -13,6 +22,8 @@ module Hyrax
         raise NotImplementedError, 'This method is not available on this subclass.' \
                                    'Use `Hyrax.config.solr_select_path` instead'
       end
+
+      delegate :add, :commit, :count, :delete, :get, :instance, :post, :query, to: :new
     end
   end
 end
