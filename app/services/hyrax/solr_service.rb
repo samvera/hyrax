eec048eac36fb7f479785b0babb511844058ddb9
Anna Headley <anna3LC@gmail.com>
2019-06-19 14:48:43 -0400

    Inline #query, #post, and #get into solr_service from AF
    refs #3800

--- app/services/hyrax/solr_service.rb
+++ app/services/hyrax/solr_service.rb
@@ -7,7 +7,7 @@ module Hyrax
   class SolrService
     extend Forwardable
 
-    def_delegators :@old_service, :add, :commit, :count, :delete, :get, :instance, :post, :query
+    def_delegators :@old_service, :add, :commit, :count, :delete, :instance
     def_delegators :instance, :conn
 
     def initialize
@@ -25,5 +25,44 @@ module Hyrax
 
       delegate :add, :commit, :count, :delete, :get, :instance, :post, :query, to: :new
     end
+
+    # Wraps rsolr get
+    # @return [Hash] the hash straight form rsolr
+    def get(query, args = {})
+      args = args.merge(q: query, qt: 'standard')
+      SolrService.instance.conn.get(Hyrax.config.solr_select_path, params: args)
+    end
+
+    # Wraps rsolr post
+    # @return [Hash] the hash straight form rsolr
+    def post(query, args = {})
+      args = args.merge(q: query, qt: 'standard')
+      SolrService.instance.conn.post(Hyrax.config.solr_select_path, data: args)
+    end
+
+    # Wraps get by default
+    # @return [Array<SolrHit>] the response docs wrapped in SolrHit objects
+    def query(query, args = {})
+      Rails.logger.warn rows_warning unless args.key?(:rows)
+      method = args.delete(:method) || :get
+
+      result = case method
+               when :get
+                 get(query, args)
+               when :post
+                 post(query, args)
+               else
+                 raise "Unsupported HTTP method for querying SolrService (#{method.inspect})"
+               end
+      result['response']['docs'].map do |doc|
+        ::SolrHit.new(doc)
+      end
+    end
+
+    private
+
+      def rows_warning
+        "Calling Hyrax::SolrService.get without passing an explicit value for ':rows' is not recommended. You will end up with Solr's default (usually set to 10)\nCalled by #{caller[0]}"
+      end
   end
 end
