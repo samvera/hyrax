b4295f6b7287d1e51ef115d79cde3d61c9269da7
Anna Headley <anna3LC@gmail.com>
2019-06-20 16:43:01 -0400

    Use a different core for valkyrie indexing

--- spec/valkyrie/indexing/solr/indexing_adapter_spec.rb
+++ spec/valkyrie/indexing/solr/indexing_adapter_spec.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
+require 'spec_helper'
 require 'valkyrie/indexing/solr/indexing_adapter'
 
 RSpec.describe Valkyrie::Indexing::Solr::IndexingAdapter do
   let(:adapter) { described_class.new(connection: client) }
-  let(:client) { Blacklight.default_index.connection }
+  let(:client) { RSolr.connect(VALKYRIE_SOLR_TEST_URL) }
 
-  describe "#conn" do
+  describe "#connection" do
     it "returns connection" do
-      expect(adapter.conn).to eq client
+      expect(adapter.connection).to eq client
     end
   end
 end
