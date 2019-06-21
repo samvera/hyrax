1425af4944d76a1d68dfc2aa4f6560d33173c2c0
Tom Johnson <tomjohnson@ucsb.edu>
2019-06-21 13:30:18 -0700

    [wings] Setup Valkyrie Solr index
    Adds configuration for the Valkyrie Solr index. The configuration is added to
    via `config/valkyrie_index.yml`, which has defaults for
    
    This introduces new environment variables:
    
      - `VALKYRIE_SOLR_HOST`
      - `VALKYRIE_SOLR_PORT`
      - `VALKYRIE_SOLR_CORE`
    
    In keeping with the behavior of other environment-based configuration, we
    provide defaults--even in production--so the introduction of these variables
    doesn't require immediate action.
    
    We use the blacklight configured host and port if no configuration is present.

--- lib/generators/hyrax/config_generator.rb
+++ lib/generators/hyrax/config_generator.rb
@@ -11,6 +11,7 @@ class Hyrax::ConfigGenerator < Rails::Generators::Base
     * Mini-magick
     * TinyMCE
     * i18n
+    * Valkyrie index
        """
 
   source_root File.expand_path('../templates', __FILE__)
@@ -34,6 +35,10 @@ class Hyrax::ConfigGenerator < Rails::Generators::Base
   def configure_redis
     copy_file 'config/redis.yml'
     copy_file 'config/initializers/redis_config.rb'
+  end
+
+  def configure_valyrie_index
+    copy_file 'config/valkyrie_index.yml'
   end
 
   def create_initializer_config_file
--- lib/valkyrie/indexing/solr/indexing_adapter.rb
+++ lib/valkyrie/indexing/solr/indexing_adapter.rb
@@ -5,14 +5,61 @@ module Valkyrie
   module Indexing
     module Solr
       class IndexingAdapter
+        ##
+        # @!attribute [r] connection
+        #   @return [RSolr::Client]
+        # @!attribute [r] resource_indexer
+        #   @return [Class]
         attr_reader :connection, :resource_indexer
+
+        ##
         # @param connection [RSolr::Client] The RSolr connection to index to.
         # @param resource_indexer [Class, #to_solr] An indexer which is able to
         #   receive a `resource` argument and then has an instance method `#to_solr`
-        def initialize(connection:, resource_indexer: Valkyrie::Persistence::Solr::MetadataAdapter::NullIndexer)
+        def initialize(connection: default_connection, resource_indexer: default_indexer)
           @connection = connection
           @resource_indexer = resource_indexer
         end
+
+        private
+
+          ##
+          # Index configuration based on the blacklight connection.
+          def blacklight_based_config
+            begin
+              # If Blacklight raises an error, we're hopefully running a
+              # generator now (if not, the application has bigger problems
+              # than this missing configuration)
+              bl_index = Blacklight.default_index.connection.uri
+            rescue RuntimeError
+              return {}
+            end
+
+            { 'host' => bl_index.host,
+              'port' => bl_index.port,
+              'core' => 'hyrax-valkyrie' }
+          end
+
+          def connection_url
+            config = begin
+                       Rails.application.config_for(:valkyrie_index)
+                     rescue RuntimeError
+                       {}
+                     end
+
+            # if any configuration is missing, derive it from Blacklight
+            config = blacklight_based_config.merge(config)
+
+            "http://#{config['host']}:#{config['port']}/solr/#{config['core']}"
+          end
+
+          def default_connection
+            RSolr.connect(url: connection_url)
+          end
+
+          def default_indexer
+            Valkyrie::Persistence::Solr::MetadataAdapter::NullIndexer
+          end
       end
     end
   end
