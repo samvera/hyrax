76261dc1f6687cb18984784e8360c1d3c28650f0
Tom Johnson <tomjohnson@ucsb.edu>
2019-06-24 11:19:56 -0700

    [wings] Refactor AttachMembersActor specs to check changes in `env`
    Actors shouldn't guarantee side effects on objects that exist outside the stack
    environment. This refactor of the specs checks changes to the
    work (`#curation_concern`) within the `env`, instead of checking on the
    originating work itself.

--- spec/actors/hyrax/actors/attach_members_actor_spec.rb
+++ spec/actors/hyrax/actors/attach_members_actor_spec.rb
@@ -30,16 +30,16 @@ RSpec.describe Hyrax::Actors::AttachMembersActor do
 
     context "when the id already exists in the members" do
       it "does nothing" do
-        expect { subject }.not_to change { work.ordered_members.to_a }
+        expect { subject }.not_to change { env.curation_concern.ordered_members.to_a }
       end
 
       context "and the _destroy flag is set" do
         let(:attributes) { HashWithIndifferentAccess.new(work_members_attributes: { '0' => { id: id, _destroy: 'true' } }) }
 
         it "removes from the member and the ordered members" do
-          expect { subject }.to change { work.ordered_members.to_a }
-          expect(work.ordered_member_ids).not_to include(existing_child_work.id)
-          expect(work.member_ids).not_to include(existing_child_work.id)
+          expect { subject }.to change { env.curation_concern.ordered_members.to_a }
+          expect(env.curation_concern.ordered_member_ids).not_to include(existing_child_work.id)
+          expect(env.curation_concern.member_ids).not_to include(existing_child_work.id)
         end
       end
     end
@@ -49,18 +49,17 @@ RSpec.describe Hyrax::Actors::AttachMembersActor do
       let(:id) { another_work.id }
 
       context "and I can edit that object" do
-        before do
-          allow(ability).to receive(:can?).with(:edit, GenericWork).and_return(true)
-        end
+        let(:another_work) { create(:work, user: depositor) }
+
         it "is added to the ordered members" do
-          expect { subject }.to change { work.ordered_members.to_a }
-          expect(work.ordered_member_ids).to include(existing_child_work.id, another_work.id)
+          expect { subject }.to change { env.curation_concern.ordered_members.to_a }
+          expect(env.curation_concern.ordered_member_ids).to include(existing_child_work.id, another_work.id)
         end
       end
 
       context "and I can not edit that object" do
         it "does nothing" do
-          expect { subject }.not_to change { work.ordered_members.to_a }
+          expect { subject }.not_to change { env.curation_concern.ordered_members.to_a }
         end
       end
     end
