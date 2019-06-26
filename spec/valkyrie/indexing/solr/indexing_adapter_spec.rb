57450b50fd8058278be348efcefa1d61035a6d7b
Anna Headley <anna3LC@gmail.com>
2019-06-26 09:26:45 -0400

    Add save to the valkyrie solr indexing adapter
    Requires running another solr wrapper for local tests; adding another
    solr core in circleci
    
    advances #3800

--- spec/valkyrie/indexing/solr/indexing_adapter_spec.rb
+++ spec/valkyrie/indexing/solr/indexing_adapter_spec.rb
@@ -4,10 +4,29 @@ require 'valkyrie/indexing/solr/indexing_adapter'
 
 RSpec.describe Valkyrie::Indexing::Solr::IndexingAdapter do
   subject(:adapter) { described_class.new }
+  let(:persister) { Wings::Valkyrie::Persister.new(adapter: metadata_adapter) }
+  let(:metadata_adapter) { Wings::Valkyrie::MetadataAdapter.new }
+
+  before do
+    class CustomResource < Valkyrie::Resource
+      include Valkyrie::Resource::AccessControls
+      attribute :title
+      attribute :creator
+    end
+  end
+  after do
+    Object.send(:remove_const, :CustomResource)
+  end
 
   describe "#connection" do
     it "returns connection" do
       expect(adapter.connection.uri.to_s).to include 'valkyrie-test'
     end
+  end
+
+  it "can save a resource" do
+    resource = CustomResource.new(title: "Carrots", creator: "Farmer MacGregor")
+    saved = persister.save(resource: resource)
+    adapter.save(resource: saved)
   end
 end
