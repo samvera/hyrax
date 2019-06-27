573f1ce2f9eea19b32bf9905b1725db63d5be730
Anna Headley <anna3LC@gmail.com>
2019-06-27 11:02:35 -0400

    Use a Hyrax::Resource factory in solr indexing adapter specs

--- spec/valkyrie/indexing/solr/indexing_adapter_spec.rb
+++ spec/valkyrie/indexing/solr/indexing_adapter_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe Valkyrie::Indexing::Solr::IndexingAdapter do
   subject(:adapter) { described_class.new }
   let(:persister) { Wings::Valkyrie::Persister.new(adapter: metadata_adapter) }
   let(:metadata_adapter) { Wings::Valkyrie::MetadataAdapter.new }
+  let(:resource) { FactoryBot.create_for_repository(:hyrax_resource) }
+  let(:resource2) { FactoryBot.create_for_repository(:hyrax_resource) }
 
   describe "#connection" do
     it "returns connection" do
@@ -14,35 +16,25 @@ RSpec.describe Valkyrie::Indexing::Solr::IndexingAdapter do
   end
 
   it "can save a resource" do
-    resource = Hyrax::Resource.new
-    saved = persister.save(resource: resource)
-    adapter.save(resource: saved)
-    expect(Hyrax::SolrService.query("*:*", use_valkyrie: true).map(&:id)).to eq [saved.id.to_s]
+    adapter.save(resource: resource)
+    expect(Hyrax::SolrService.query("*:*", use_valkyrie: true).map(&:id)).to eq [resource.id.to_s]
   end
 
   it "can save multiple resources at once" do
-    resource = Hyrax::Resource.new
-    resource2 = Hyrax::Resource.new
-    results = persister.save_all(resources: [resource, resource2])
-    adapter.save_all(resources: results)
-
-    expect(Hyrax::SolrService.query("*:*", use_valkyrie: true).map(&:id)).to contain_exactly(*results.map(&:id).map(&:to_s))
+    adapter.save_all(resources: [resource, resource2])
+    expect(Hyrax::SolrService.query("*:*", use_valkyrie: true).map(&:id)).to contain_exactly resource.id.to_s, resource2.id.to_s
   end
 
   it "can delete an object" do
-    resource = Hyrax::Resource.new
-    saved = persister.save(resource: resource)
-    adapter.save(resource: saved)
-    adapter.delete(resource: saved)
+    adapter.save(resource: resource)
+    expect(Hyrax::SolrService.query("*:*", use_valkyrie: true).count).to eq 1
+    adapter.delete(resource: resource)
     expect(Hyrax::SolrService.query("*:*", use_valkyrie: true).count).to eq 0
   end
 
   it "can delete all objects" do
-    resource = Hyrax::Resource.new
-    resource2 = Hyrax::Resource.new
-    saved = persister.save_all(resources: [resource, resource2])
-
-    adapter.save_all(resources: saved)
+    adapter.save_all(resources: [resource, resource2])
+    expect(Hyrax::SolrService.query("*:*", use_valkyrie: true).count).to eq 2
     adapter.wipe!
     expect(Hyrax::SolrService.query("*:*", use_valkyrie: true).count).to eq 0
   end
