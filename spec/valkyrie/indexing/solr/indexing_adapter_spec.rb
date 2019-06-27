e97f6050d17c5afe901a60e36c0f5965a1280b78
Anna Headley <anna3LC@gmail.com>
2019-06-27 10:33:55 -0400

    Add save_all, delete, and wipe! to solr indexing adapter
    Completes the basic implementation.
    refs #3800

--- spec/valkyrie/indexing/solr/indexing_adapter_spec.rb
+++ spec/valkyrie/indexing/solr/indexing_adapter_spec.rb
@@ -7,17 +7,6 @@ RSpec.describe Valkyrie::Indexing::Solr::IndexingAdapter do
   let(:persister) { Wings::Valkyrie::Persister.new(adapter: metadata_adapter) }
   let(:metadata_adapter) { Wings::Valkyrie::MetadataAdapter.new }
 
-  before do
-    class CustomResource < Valkyrie::Resource
-      include Valkyrie::Resource::AccessControls
-      attribute :title
-      attribute :creator
-    end
-  end
-  after do
-    Object.send(:remove_const, :CustomResource)
-  end
-
   describe "#connection" do
     it "returns connection" do
       expect(adapter.connection.uri.to_s).to include 'valkyrie-test'
@@ -25,8 +14,36 @@ RSpec.describe Valkyrie::Indexing::Solr::IndexingAdapter do
   end
 
   it "can save a resource" do
-    resource = CustomResource.new(title: "Carrots", creator: "Farmer MacGregor")
+    resource = Hyrax::Resource.new
     saved = persister.save(resource: resource)
     adapter.save(resource: saved)
+    expect(Hyrax::SolrService.query("*:*", use_valkyrie: true).map(&:id)).to eq [saved.id.to_s]
+  end
+
+  it "can save multiple resources at once" do
+    resource = Hyrax::Resource.new
+    resource2 = Hyrax::Resource.new
+    results = persister.save_all(resources: [resource, resource2])
+    adapter.save_all(resources: results)
+
+    expect(Hyrax::SolrService.query("*:*", use_valkyrie: true).map(&:id)).to contain_exactly(*results.map(&:id).map(&:to_s))
+  end
+
+  it "can delete an object" do
+    resource = Hyrax::Resource.new
+    saved = persister.save(resource: resource)
+    adapter.save(resource: saved)
+    adapter.delete(resource: saved)
+    expect(Hyrax::SolrService.query("*:*", use_valkyrie: true).count).to eq 0
+  end
+
+  it "can delete all objects" do
+    resource = Hyrax::Resource.new
+    resource2 = Hyrax::Resource.new
+    saved = persister.save_all(resources: [resource, resource2])
+
+    adapter.save_all(resources: saved)
+    adapter.wipe!
+    expect(Hyrax::SolrService.query("*:*", use_valkyrie: true).count).to eq 0
   end
 end
