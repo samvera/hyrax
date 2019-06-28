c2681b7102eec2af6caaf9e07359385f9a40afb6
Tom Johnson <tomjohnson@ucsb.edu>
2019-06-28 08:28:08 -0700

    [wings] Only clean the valkyrie index when needed
    Only certain tests will touch the Solr index. At the moment, few of them depend
    on having the index up at all. Cleaning the index on each example introduces a
    lot of external Solr calls, and requires the Solr service to be running for
    every test no matter how isolated.
    
    Limiting this call to tests where it's explictly needed avoids these impacts. By
    taking a conservative approach now, we can end up with a tighter test suite over
    time.

--- spec/support/clean_solr.rb
+++ spec/support/clean_solr.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 RSpec.configure do |config|
-  config.before do
+  config.before(:each, clean_index: true) do
     client = Valkyrie::IndexingAdapter.find(:solr_index).connection
     client.delete_by_query("*:*", params: { softCommit: true })
   end
