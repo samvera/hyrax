a70f2a3f8c2aec97d9fd68e3bd2f69cdeeacb234
E. Lynette Rayle <elr37@cornell.edu>
2019-07-31 15:56:03 -0400

    fix match_valkyrie_ids_with_af_ids
    This matcher was incorrectly returning true in certain circumstances.  It now extends contains_exactly builtin matcher and overrides the match method to map the valkyrie ids there instead.
    
    Tests were added to confirm the matcher is operating as expected.

--- spec/support/matchers/match_valkyrie_ids_with_af_ids.rb
+++ spec/support/matchers/match_valkyrie_ids_with_af_ids.rb
@@ -1,5 +1,19 @@
-RSpec::Matchers.define :match_valkyrie_ids_with_active_fedora_ids do |_active_fedora_ids|
-  match do |valkyrie_ids|
-    contain_exactly(valkyrie_ids.map(&:id))
+module RSpec
+  module Matchers
+    module BuiltIn
+      class MatchValkyrieIdsWithActiveFedoraIds < RSpec::Matchers::BuiltIn::ContainExactly
+        def match_when_sorted?
+          values_match?(safe_sort(expected), safe_sort(actual.map(&:id)))
+        end
+      end
+    end
+  end
+end
+
+module RSpec
+  module Matchers
+    def match_valkyrie_ids_with_active_fedora_ids(items)
+      BuiltIn::MatchValkyrieIdsWithActiveFedoraIds.new(items)
+    end
   end
 end
