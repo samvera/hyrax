1eb9d28e5c62888bc9e7df0204cb46817324ef8b
VivianChu <tchu@ucsd.edu>
2019-08-06 13:45:24 -0700

    Update solr config

--- build/docker-entrypoint.sh
+++ build/docker-entrypoint.sh
@@ -8,25 +8,41 @@ if [[ -z "${SOLR_URL}" ]]; then
 fi
 
 read -r solr_host solr_port solr_core <<< "$(echo "$SOLR_URL" | sed -r 's|http://(.+):(.+)/solr/(.+)|\1 \2 \3|')"
-echo "Creating Solr core: $solr_core..."
+echo "Creating Solr core: $SOLR_CORE..."
 
-while ! nc -z "$solr_host" "$solr_port"
-do
-  echo "waiting for solr"
-  sleep 1
-done
+configDir="/hyrax_config"
+#mkdir /hyrax_config
+#cp -r "/data/.internal_test_app/solr/config" "/hyrax_config"
 
-#cd /data/.internal_test_app/solr/config
-#zip -1 -r solr_config.zip ./*
-#zip -1 -r solr_config.zip . -i /data/.internal_test_app/solr/config/*
-#zip -1 -r solr_config.zip /data/.internal_test_app/solr/config/*
-zip -r solr_config.zip /data/.internal_test_app/solr/config/*
+if [[ ! -d $coredir ]]; then
+  mkdir  -p "$configDir"
+  cp -r "/data/.internal_test_app/solr/config/" "$configDir"
+  ls -lart "$configDir"
+else
+  echo "Solr Config Directory $configDir already exists"
+  ls -lart "$configDir"
+fi
+      
+#cat /opt/solr/server/solr/mycores/hyrax-dev/conf/solrconfig.xml
+#while ! nc -z "$solr_host" "$solr_port"
+#do
+#  echo "waiting for solr"
+#  sleep 1
+#done
 
-curl -H "Content-type:application/octet-stream" --data-binary @solr_config.zip "http://$solr_host:$solr_port >>/solr/admin/configs?action=UPLOAD&name=solrconfig"
+#curl "http://$solr_host:$solr_port/solr/admin/configs?action=LIST"
 
-curl -H 'Content-type: application/json' "http://$solr_host:$solr_port" >>/api/collections/ -d '{create: {name: << "$solr_core" >>, config: solrconfig, numShards: 1}}'
+#curl "http://$solr_host:$solr_port/solr/admin/cores?action=STATUS&core=$SOLR_CORE"
 
-echo "Successfully created $solr_core"
+#(cd /data/.internal_test_app/solr/conf && zip -r - *) > myconfigset.zip
+
+#curl -X POST --header "Content-Type:application/octet-stream" --data-binary @myconfigset.zip "http://$solr_host:$solr_port/solr/admin/configs?action=UPLOAD&name=myConfigSet"
+
+#curl -X POST --header "Content-Type:application/json" "http://$solr_host:$solr_port/api/collections/" -d "{create: {name: $SOLR_CORE , config: myConfigSet, numShards: 1}}"
+
+#curl "http://$solr_host:$solr_port/solr/admin/cores?action=STATUS&core=$SOLR_CORE"
+
+echo "Successfully created $SOLR_CORE"
 
 # Then exec the container's main process
 # This is what's set as CMD in a) Dockerfile b) compose c) CI
