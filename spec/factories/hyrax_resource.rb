dd80ac50f0e4c2e775dcfbf18aeb4b4f5988fa18
Tom Johnson <tomjohnson@ucsb.edu>
2019-08-27 16:30:51 -0700

    Support `:valkyrie_create` strategy for `FactoryBot`
    The previous approach for saving valkyrie objects with FactoryBot's `#to_create`
    would save them fine, but then returned an unsaved object instance (i.e. one
    without an `#id` or create/modified dates). Using a dedicated strategy gives us
    a bit more control over what happens when we save a valkyrie object.
    
    This approach can also be extended to better support "associations" and similar
    for Valkyrie resources.
    
    See: https://github.com/thoughtbot/factory_bot/blob/master/GETTING_STARTED.md#custom-strategies

--- spec/factories/hyrax_resource.rb
+++ spec/factories/hyrax_resource.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal: true
 FactoryBot.define do
   factory :hyrax_resource, class: "Hyrax::Resource" do
-    to_create do |instance|
-      Valkyrie.config.metadata_adapter.persister.save(resource: instance)
-    end
-
     trait :under_embargo do
       association :embargo, factory: :hyrax_embargo
     end
