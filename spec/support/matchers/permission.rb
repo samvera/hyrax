8545d538f3ff51760a7a81ef2b73620a8180f42e
Tom Johnson <tomjohnson@ucsb.edu>
2019-08-27 16:34:46 -0700

    Add `.on` to permission matcher
    If we want to check what object a permission is applied to, there's now a
    matcher for that.
    
    `expect(permission).to grant_permission(:read).on(object_id).to(agent.id)`

--- spec/support/matchers/permission.rb
+++ spec/support/matchers/permission.rb
@@ -3,6 +3,7 @@
 RSpec::Matchers.define :grant_permission do |acl_type|
   match do |permission|
     access_match = permission.access == acl_type.to_s
+
     agent_match =
       if user_id
         permission.type == 'person' &&
@@ -14,9 +15,17 @@ RSpec::Matchers.define :grant_permission do |acl_type|
         true
       end
 
-    return access_match && agent_match
+    target_match =
+      if access_to_id
+        permission.access_to_id == access_to_id.to_s
+      else
+        true
+      end
+
+    return access_match && agent_match && target_match
   end
 
+  chain :on,       :access_to_id
   chain :to_user,  :user_id
   chain :to_group, :group_id
 end
