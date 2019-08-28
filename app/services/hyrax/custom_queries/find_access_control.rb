f86e28c738f3bb97c0724344de8e2ce635b8f529
Tom Johnson <tomjohnson@ucsb.edu>
2019-08-27 21:06:01 -0700

    Add a custom query for finding AccessControl objects
    Given a resource, we want to be able to find an AccessControl object for it.
    
    For a typical valkyrie adapter, this is an inverse reference lookup on the
    `AccessControl#access_to` property. An initial implementation does a broad
    reverse lookup on `#access_to`, then filters the result classes. It assumes the
    first `AccessControl` found is the correct one.
    
    For Wings, the story is a bit different. `Hydra::AccessControl` resources don't
    know what their target is. When rebuilding valkyrie resources from
    `Hydra::AccessControl`/AF objects, we inspect the permissions to assign
    `#access_to`, but we can't use this for lookup. Instead, we can rely on the
    `ActiveFedora`/`Hydra::AccessControls` pattern of looking up the
    `Hydra::AccessControl` object from its target (via its `belongs_to` reference).

---
+++ app/services/hyrax/custom_queries/find_access_control.rb
@@ -0,0 +1,23 @@
+module Hyrax
+  module CustomQueries
+    class FindAccessControl
+      def self.queries
+        [:find_access_control_for]
+      end
+
+      def initialize(query_service:)
+        @query_service = query_service
+      end
+
+      attr_reader :query_service
+      delegate :resource_factory, to: :query_service
+
+      def find_access_control_for(resource:)
+        query_service
+          .find_inverse_references_by(resource: resource, property: :access_to)
+          .find { |r| r.is_a?(Hyrax::AccessControl) } ||
+          raise(Valkyrie::Persistence::ObjectNotFoundError)
+      end
+    end
+  end
+end
