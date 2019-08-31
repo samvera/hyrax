c3ff47c68b90168945fb77761279ab4c115289f5
Tom Johnson <tomjohnson@ucsb.edu>
2019-08-30 17:00:26 -0700

    Add some documentation for Valkyrie permission handling

--- app/models/hyrax/resource.rb
+++ app/models/hyrax/resource.rb
@@ -3,6 +3,31 @@
 module Hyrax
   ##
   # The base Valkyrie model for Hyrax.
+  #
+  # @note Hyrax permissions are managed via
+  #   [Access Control List](https://en.wikipedia.org/wiki/Access-control_list)
+  #   style permissions. Legacy Hyrax models powered by `ActiveFedora` linked
+  #   the ACLs from the repository object itself (as an `acl:accessControl` link
+  #   to a container). Valkyrie models jettison that approach in favor of relying
+  #   on links back from the permissions using `access_to`. As was the case in
+  #   the past implementation, we include an object to represent the access list
+  #   itself (`Hyrax::AccessControl`). This object's `#access_to` is the way
+  #   Hyrax discovers list entries--it MUST match between the `AccessControl`
+  #   and its individual `Permissions`.
+  #
+  #   The effect of this change is that our `AccessControl` objects are detached
+  #   from `Hyrax::Resource` they can (and usually should) be edited and
+  #   persisted independently from the resource itself.
+  #
+  #   Some utilitiy methods are provided for ergonomics in transitioning from
+  #   `ActiveFedora`: the `#visibility` accessor, and the `#*_users` and
+  #   `#*_group` accessors. The main purpose of these is to provide a cached
+  #   ACL attached to a given Resource instance. However, these will likely be
+  #   deprecated in the future, and it's advisable to avoid them in favor of
+  #   `Hyrax::AccessControlList`, `Hyrax::PermissionManager` and/or
+  #   `Hyrax::VisibilityWriter` (which provide their underlying
+  #   implementations).
+  #
   class Resource < Valkyrie::Resource
     attribute :alternate_ids, ::Valkyrie::Types::Array
     attribute :embargo,       Hyrax::Embargo
