0059ccd63dc2a886f9fde4fb52b5a79f12b83555
E. Lynette Rayle <elr37@cornell.edu>
2019-09-03 07:24:39 -0400

    wings wrapper for Hydra:Works:AddFileToFileSet
    Instead of copying and modifying AddFileToFileSet in AddFileMetadataToFileSet, a wrapper that only does the round trip conversion before and after the call to the Hydra Works code was created.  This reduces the complexity of the wings code.

---
+++ spec/wings/hydra/works/services/add_file_to_file_set_spec.rb
@@ -0,0 +1,126 @@
+# frozen_string_literal: true
+require 'wings_helper'
+require 'wings/hydra/works/services/add_file_to_file_set'
+
+RSpec.describe Wings::Works::AddFileToFileSet, :clean_repo do
+  let(:af_file_set)             { create(:file_set, id: 'fileset_id') }
+  let!(:file_set)               { af_file_set.valkyrie_resource }
+
+  let(:original_file_use)  { Valkyrie::Vocab::PCDMUse.OriginalFile }
+  let(:extracted_text_use) { Valkyrie::Vocab::PCDMUse.ExtractedText }
+  let(:thumbnail_use)      { Valkyrie::Vocab::PCDMUse.Thumbnail }
+
+  let(:pdf_filename)  { 'sample-file.pdf' }
+  let(:pdf_mimetype)  { 'application/pdf' }
+  let(:pdf_file)      { File.open(File.join(fixture_path, pdf_filename)) }
+
+  let(:text_filename) { 'updated-file.txt' }
+  let(:text_mimetype) { 'text/plain' }
+  let(:text_file)     { File.open(File.join(fixture_path, text_filename)) }
+
+  let(:image_filename) { 'world.png' }
+  let(:image_mimetype) { 'image/png' }
+  let(:image_file)     { File.open(File.join(fixture_path, image_filename)) }
+
+  let(:update_existing) { true }
+
+  context 'when :use is the name of an association type' do
+    context 'and requesting original file' do
+      subject { described_class.call(file_set: file_set, file: pdf_file, type: original_file_use) }
+      it "builds and uses the association's target" do
+        ids = subject.original_file_ids
+        expect(ids.size).to eq 1
+        expect(ids.first).to be_a Valkyrie::ID
+        expect(ids.first.to_s).to start_with "#{file_set.id}/files/"
+
+        # file_metadata = Hyrax::query_service.find_by(id: ids.first) # TODO: Can't do this b/c Wings tries to get an ActiveFedora::Base object instead of FileMetadata Resource
+        # expect(file_metadata.content).to start_with('%PDF-1.3')
+        af_file_set = Hyrax.query_service.find_by_alternate_identifier(alternate_identifier: file_set.id, use_valkyrie: false)
+        expect(af_file_set.original_file.content).to start_with('%PDF-1.3')
+        expect(af_file_set.original_file.mime_type).to eq(pdf_mimetype)
+      end
+    end
+
+    context 'and requesting extracted text' do
+      subject { described_class.call(file_set: file_set, file: text_file, type: [extracted_text_use]) }
+      it "builds and uses the association's target" do
+        ids = subject.extracted_text_ids
+        expect(ids.size).to eq 1
+        expect(ids.first).to be_a Valkyrie::ID
+        expect(ids.first.to_s).to start_with "#{file_set.id}/files/"
+
+        # file_metadata = Hyrax::query_service.find_by(id: ids.first) # TODO: Can't do this b/c Wings tries to get an ActiveFedora::Base object instead of FileMetadata Resource
+        # expect(file_metadata.content).to start_with('some updated content')
+        af_file_set = Hyrax.query_service.find_by_alternate_identifier(alternate_identifier: file_set.id, use_valkyrie: false)
+        expect(af_file_set.extracted_text.content).to start_with('some updated content')
+        expect(af_file_set.extracted_text.mime_type).to eq(text_mimetype)
+      end
+    end
+
+    context 'and requesting thumbnail' do
+      subject { described_class.call(file_set: file_set, file: image_file, type: thumbnail_use) }
+      it "builds and uses the association's target" do
+        ids = subject.thumbnail_ids
+        expect(ids.size).to eq 1
+        expect(ids.first).to be_a Valkyrie::ID
+        expect(ids.first.to_s).to start_with "#{file_set.id}/files/"
+
+        # file_metadata = Hyrax::query_service.find_by(id: ids.first) # TODO: Can't do this b/c Wings tries to get an ActiveFedora::Base object instead of FileMetadata Resource
+        # expect(file_metadata.content).to start_with('some updated content')
+        af_file_set = Hyrax.query_service.find_by_alternate_identifier(alternate_identifier: file_set.id, use_valkyrie: false)
+        expect(af_file_set.thumbnail).to have_content
+        expect(af_file_set.thumbnail.mime_type).to eq(image_mimetype)
+      end
+    end
+  end
+
+  context 'when :use is NOT the name of an association type' do
+    let(:transcript_use)   { Valkyrie::Vocab::PCDMUse.Transcript }
+    let(:service_file_use) { Valkyrie::Vocab::PCDMUse.ServiceFile }
+
+    subject { described_class.call(file_set: file_set, file: text_file, type: transcript_use) }
+    before do
+      described_class.call(file_set: file_set, file: pdf_file, type: service_file_use)
+    end
+    it 'adds the given file and applies the specified RDF::URI use to it' do
+      # TODO: Valkyrie resource needs to support filter_files_by_type
+      expect(subject.filter_files_by_type(transcript_use).first.content).to start_with('some updated content')
+      expect(subject.filter_files_by_type(service_file_use).first.content).to start_with('%PDF-1.3')
+    end
+  end
+
+  context 'when :versioning => true' do
+    let(:versioning) { true }
+    subject { described_class.call(file_set: file_set, file: pdf_file, type: original_file_use, versioning: versioning) }
+    it 'updates the file and creates a version' do
+      pending 'Valkyrization of versioning for files'
+      expect(subject.original_file.versions.all.count).to eq(1)
+      expect(subject.original_file.content).to start_with('%PDF-1.3')
+    end
+
+    context 'and there are already versions' do
+      before do
+        described_class.call(file_set: file_set, file: pdf_file, type: original_file_use, versioning: versioning)
+      end
+      subject { described_class.call(file_set: file_set, file: text_file, type: original_file_use, versioning: versioning) }
+      it 'adds to the version history' do
+        pending 'Valkyrization of versioning for files'
+        expect(subject.original_file.versions.all.count).to eq(2)
+        expect(subject.original_file.content).to eq("some updated content\n")
+      end
+    end
+  end
+
+  context 'when :versioning => false' do
+    let(:versioning) { false }
+    before do
+      described_class.call(file_set: file_set, file: pdf_file, type: original_file_use, versioning: versioning)
+    end
+    subject { described_class.call(file_set: file_set, file: text_file, type: original_file_use, versioning: versioning) }
+    it 'skips creating versions' do
+      pending 'Valkyrization of versioning for files'
+      expect(subject.original_file.versions.all.count).to eq(0)
+      expect(subject.original_file.content).to eq("some updated content\n")
+    end
+  end
+end
