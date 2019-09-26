1cfabeef6ad2218f92d078c5db10448a8d6c84a5
Tom Johnson <tomjohnson@ucsb.edu>
2019-09-26 04:10:15 -0700

    Use a custom Indexer for AdminSets
    Avoid deprecated `index.as` syntax for `AdminSet` data.
    
    Admin Sets should be able to index using the AdminSetIndexer, rather than the
    old-style `index.as` syntax.

--- spec/indexers/hyrax/admin_set_indexer_spec.rb
+++ spec/indexers/hyrax/admin_set_indexer_spec.rb
@@ -1,6 +1,6 @@
 RSpec.describe Hyrax::AdminSetIndexer do
   let(:indexer) { described_class.new(admin_set) }
-  let(:admin_set) { build(:admin_set) }
+  let(:admin_set) { build(:complete_admin_set) }
   let(:doc) do
     {
       'generic_type_sim' => ['Admin Set']
@@ -10,6 +10,39 @@ RSpec.describe Hyrax::AdminSetIndexer do
   describe "#generate_solr_document" do
     it "has required fields" do
       expect(indexer.generate_solr_document).to match a_hash_including(doc)
+    end
+  end
+
+  describe 'alt_title' do
+    it 'is stored searchable' do
+      expect(indexer.generate_solr_document)
+        .to match a_hash_including('alt_title_sim' => admin_set.alt_title)
+    end
+  end
+
+  describe 'creator' do
+    it 'is indexed as a symbol' do
+      expect(indexer.generate_solr_document)
+        .to match a_hash_including('creator_ssim' => admin_set.creator)
+    end
+  end
+
+  describe 'description' do
+    it 'is stored searchable' do
+      expect(indexer.generate_solr_document)
+        .to match a_hash_including('description_sim' => admin_set.description)
+    end
+  end
+
+  describe 'title' do
+    it 'is stored searchable' do
+      expect(indexer.generate_solr_document)
+        .to match a_hash_including('title_tesim' => admin_set.title)
+    end
+
+    it 'is facetable' do
+      expect(indexer.generate_solr_document)
+        .to match a_hash_including('title_sim' => admin_set.title)
     end
   end
 end
