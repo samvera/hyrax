ff4e2a73564416c6b2e2761f1f192ae2a1087511
Tom Johnson <tomjohnson@ucsb.edu>
2019-09-26 02:51:32 -0700

    Provide a Valkyrie changeset factory
    For any given Valkyrie model, we want a factory for an appropriate `ChangeSet`,
    with attribute accessors and a `#sync` method.
    
    We provide this with a `Hyrax::ChangeSet.for(my_resource)` factory. This builds
    the changeset class on the fly, applying the fields from the resource
    schema. Then a new instance of the class is initialized to handle the `ChangeSet`
    for the specific object.
    
    As future work, we could consider caching the changeset classes. For now, it
    seems best to watch and wait--avoiding premature optimization.

---
+++ spec/models/hyrax/change_set_spec.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+RSpec.describe Hyrax::ChangeSet do
+  subject(:change_set) { described_class.for(resource) }
+  let(:resource)       { resource_class.new }
+  let(:resource_class) { Hyrax::Test::BookResource }
+  let(:titles)         { ['comet in moominland', 'finn family moomintroll'] }
+
+  it 'changes when changed' do
+    expect { change_set.title = titles }
+      .to change { change_set.changed? }
+      .from(false)
+      .to(true)
+  end
+
+  it 'sets changeset attributes' do
+    expect { change_set.title = titles }
+      .to change { change_set.title }
+      .to contain_exactly(*titles)
+  end
+
+  it 'applies changeset attributes' do
+    change_set.title = titles
+
+    expect { change_set.sync }
+      .to change { resource.title }
+      .to contain_exactly(*titles)
+  end
+
+  it 'does not expose reserved attributes' do
+    expect { change_set.id = 'fake id' }.to raise_error NoMethodError
+  end
+
+  it 'does not list attributes' do
+    expect(change_set.class.fields)
+      .not_to include(*resource_class.reserved_attributes)
+  end
+end
