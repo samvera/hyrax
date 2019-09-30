2cc9f35b73aa3e7fb953f42bc70efdb790df0974
E. Lynette Rayle <elr37@cornell.edu>
2019-09-30 08:17:40 -0400

    allow characterize_job to receive valkyrie resource
    Allows CharacterizeJob to receive a Valkyrie::Resource and immediately convert it to an AF object.  This allows callers of this job to be fully valkyrized.
    
    PR #4055 Convert file ids in resource fileset to pcdm files in AF fileset
    
    Make CharacterizationJob able to process a Valkyrie::Resource.
    
    This supports full valkyrization of new service Hyrax::Files::IngestFileService (replacing FileActor #ingest_file method).

--- app/jobs/characterize_job.rb
+++ app/jobs/characterize_job.rb
@@ -3,10 +3,14 @@ class CharacterizeJob < Hyrax::ApplicationJob
 
   # Characterizes the file at 'filepath' if available, otherwise, pulls a copy from the repository
   # and runs characterization on that file.
-  # @param [FileSet] file_set
+  # @param [FileSet, Valkyrie::Resource] file_set
   # @param [String] file_id identifier for a Hydra::PCDM::File
   # @param [String, NilClass] filepath the cached file within the Hyrax.config.working_path
   def perform(file_set, file_id, filepath = nil)
+    # STATUS: Valkyrie Transistion - currently converts a Valkyrie::Resource to a FileSet as it enters this method.
+    # TODO: Valkyrie Transistion - Next step is to make the characterization process operate on a Valkyrie::Resource.
+    file_set = ValkyrieTransitionHelper.to_active_fedora(file_set)
+
     raise "#{file_set.class.characterization_proxy} was not found for FileSet #{file_set.id}" unless file_set.characterization_proxy?
     filepath = Hyrax::WorkingDirectory.find_or_retrieve(file_id, file_set.id) unless filepath && File.exist?(filepath)
     characterize(file_set, file_id, filepath)
--- spec/jobs/characterize_job_spec.rb
+++ spec/jobs/characterize_job_spec.rb
@@ -1,26 +1,38 @@
 RSpec.describe CharacterizeJob do
+  [true, false].each do |test_valkyrie|
+    context "when test_valkyrie is #{test_valkyrie}" do
   let(:file_set_id) { 'abc12345' }
   let(:filename)    { Rails.root.join('tmp', 'uploads', 'ab', 'c1', '23', '45', 'abc12345', 'picture.png').to_s }
-  let(:file_set) do
+  # let(:af_filename)    { Rails.root.join('tmp', 'uploads', 'ab', 'c1', '23', '45', 'abc12345', 'picture.png').to_s }
+  # let(:valk_filename)    { Rails.root.join('tmp', 'uploads', 'ab', 'c1', '23', '45', 'abc12345', 'files', 'picture.png').to_s }
+  # let(:filename) { test_valkyrie ? valk_filename : af_filename }
+  let(:af_file_set) do
     FileSet.new(id: file_set_id).tap do |fs|
       allow(fs).to receive(:original_file).and_return(file)
       allow(fs).to receive(:update_index)
     end
   end
-  # let(:io)          { JobIoWrapper.new(file_set_id: file_set.id, user: create(:user), path: filename) }
+  let(:valk_file_set) { af_file_set.valkyrie_resource }
+  let(:file_set) { test_valkyrie ? valk_file_set : af_file_set }
   let(:file) do
+    # af_file_set.file_set.association(:original_file)
     Hydra::PCDM::File.new.tap do |f|
+    # Hydra::PCDM::File.new(id: "ab/c1/23/45/abc12345/files/xyp98764").tap do |f|
       f.content = 'foo'
       f.original_name = 'picture.png'
       f.save!
       allow(f).to receive(:save!)
     end
   end
+  let(:valk_file_ids) { [Valkyrie::ID.new(file.id)] }
 
   before do
-    allow(FileSet).to receive(:find).with(file_set_id).and_return(file_set)
+    allow(FileSet).to receive(:find).with(file_set_id).and_return(af_file_set)
+    allow_any_instance_of(FileSet).to receive(:new_record?).and_return(false) if test_valkyrie # rubocop:disable RSpec/AnyInstance
+byebug
+    allow_any_instance_of(valk_file_set).to receive(:original_file_ids).and_return(valk_file_ids) if test_valkyrie # rubocop:disable RSpec/AnyInstance
     allow(Hydra::Works::CharacterizationService).to receive(:run).with(file, filename)
-    allow(CreateDerivativesJob).to receive(:perform_later).with(file_set, file.id, filename)
+    allow(CreateDerivativesJob).to receive(:perform_later).with(af_file_set, file.id, filename)
   end
 
   context 'with valid filepath param' do
@@ -37,14 +49,14 @@ RSpec.describe CharacterizeJob do
     it 'runs Hydra::Works::CharacterizationService and creates a CreateDerivativesJob' do
       expect(Hydra::Works::CharacterizationService).to receive(:run).with(file, filename)
       expect(file).to receive(:save!)
-      expect(file_set).to receive(:update_index)
-      expect(CreateDerivativesJob).to receive(:perform_later).with(file_set, file.id, filename)
+      expect(af_file_set).to receive(:update_index)
+      expect(CreateDerivativesJob).to receive(:perform_later).with(af_file_set, file.id, filename)
       described_class.perform_now(file_set, file.id)
     end
   end
 
   context 'when the characterization proxy content is absent' do
-    before { allow(file_set).to receive(:characterization_proxy?).and_return(false) }
+    before { allow(af_file_set).to receive(:characterization_proxy?).and_return(false) }
     it 'raises an error' do
       expect { described_class.perform_now(file_set, file.id) }.to raise_error(StandardError, /original_file was not found/)
     end
@@ -55,12 +67,14 @@ RSpec.describe CharacterizeJob do
     let(:collection) { build(:collection_lw) }
 
     before do
-      allow(file_set).to receive(:parent).and_return(work)
+      allow(af_file_set).to receive(:parent).and_return(work)
       allow(work).to receive(:in_collections).and_return([collection])
     end
     it "reindexes the collection" do
       expect(collection).to receive(:update_index)
       described_class.perform_now(file_set, file.id)
+    end
+  end
     end
   end
 end
