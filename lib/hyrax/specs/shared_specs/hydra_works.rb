cd8d127815154e00385c431b5ae165f206d550c3
Tom Johnson <tomjohnson@ucsb.edu>
2019-10-02 17:08:30 -0700

    Support PCDM/Hydra Works domain type inspectors
    Add flags for collection/object/work/file_set/file. `Hyrax::Resource` responds
    `false` to all of these methods, but domain model objects should override with
    `true` when they represent the relevant domain objects.
    
    These checks should *always* be used in place of type checks to determine
    PCDM/Hydra Works types.

--- lib/hyrax/specs/shared_specs/hydra_works.rb
+++ lib/hyrax/specs/shared_specs/hydra_works.rb
@@ -18,9 +18,21 @@ RSpec.shared_examples 'a Hyrax::Resource' do
         .to contain_exactly id
     end
   end
+
+  it { is_expected.to respond_to :collection? }
+  it { is_expected.to respond_to :file? }
+  it { is_expected.to respond_to :file_set? }
+  it { is_expected.to respond_to :pcdm_object? }
+  it { is_expected.to respond_to :work? }
 end
 
 RSpec.shared_examples 'a Hyrax::Work' do
   it_behaves_like 'a Hyrax::Resource'
   it_behaves_like 'a model with core metadata'
+
+  it { is_expected.not_to be_collection }
+  it { is_expected.not_to be_file }
+  it { is_expected.not_to be_file_set }
+  it { is_expected.to be_pcdm_object }
+  it { is_expected.to be_work }
 end
