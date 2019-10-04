066da244f924334a0f17a44ad990036290ea1603
Tom Johnson <tomjohnson@ucsb.edu>
2019-10-04 14:05:06 -0700

    Implement a `Hyrax::Work` model
    Adds a top-level PCDM/Hydra Works model for `Work` in native Valkyrie. Shared
    examples for Hydra Works-level domain objects are added. Likewise for the
    `:core_metadata` schema.
    
    The test `SimpleWork` class is updated to inherit `Hyrax::Work`.

--- lib/hyrax/specs/shared_specs.rb
+++ lib/hyrax/specs/shared_specs.rb
@@ -1,2 +1,4 @@
 require 'hyrax/specs/shared_specs/derivative_service'
+require 'hyrax/specs/shared_specs/metadata'
+require 'hyrax/specs/shared_specs/hydra_works'
 require 'hyrax/specs/shared_specs/workflow_method'
