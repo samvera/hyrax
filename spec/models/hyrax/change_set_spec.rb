3a68125a41b63a5a17ac2b89aeb97c0d6ec9509f
Tom Johnson <tomjohnson@ucsb.edu>
2019-10-15 14:14:18 -0700

    Add a default value of `nil` for `Hyrax::ChangeSet` attributes
    This makes auto-created change sets easier to work with.

--- spec/models/hyrax/change_set_spec.rb
+++ spec/models/hyrax/change_set_spec.rb
@@ -2,37 +2,50 @@
 
 RSpec.describe Hyrax::ChangeSet do
   subject(:change_set) { described_class.for(resource) }
-  let(:resource)       { resource_class.new }
-  let(:resource_class) { Hyrax::Test::BookResource }
+  let(:resource)       { build(:hyrax_work) }
   let(:titles)         { ['comet in moominland', 'finn family moomintroll'] }
 
-  it 'changes when changed' do
-    expect { change_set.title = titles }
-      .to change { change_set.changed? }
-      .from(false)
-      .to(true)
+  describe 'properties' do
+    it 'changes when changed' do
+      expect { change_set.title = titles }
+        .to change { change_set.changed? }
+        .from(false)
+        .to(true)
+    end
+
+    it 'sets changeset attributes' do
+      expect { change_set.title = titles }
+        .to change { change_set.title }
+        .to contain_exactly(*titles)
+    end
+
+    it 'does not expose reserved attributes' do
+      expect { change_set.id = 'fake id' }.to raise_error NoMethodError
+    end
+
+    it 'does not list reserved attributes as fields' do
+      expect(change_set.class.fields)
+        .not_to include(*resource.class.reserved_attributes)
+    end
   end
 
-  it 'sets changeset attributes' do
-    expect { change_set.title = titles }
-      .to change { change_set.title }
-      .to contain_exactly(*titles)
-  end
+  describe '#sync' do
+    it 'applies changeset attributes' do
+      change_set.title = titles
 
-  it 'applies changeset attributes' do
-    change_set.title = titles
+      expect { change_set.sync }
+        .to change { resource.title }
+        .to contain_exactly(*titles)
+    end
 
-    expect { change_set.sync }
-      .to change { resource.title }
-      .to contain_exactly(*titles)
-  end
+    it 'can save resources after sync' do
+      change_set.title = titles
+      change_set.sync
 
-  it 'does not expose reserved attributes' do
-    expect { change_set.id = 'fake id' }.to raise_error NoMethodError
-  end
+      id = Hyrax.persister.save(resource: resource).id
 
-  it 'does not list attributes' do
-    expect(change_set.class.fields)
-      .not_to include(*resource_class.reserved_attributes)
+      expect(Hyrax.query_service.find_by(id: id))
+        .to have_attributes(title: contain_exactly(*titles))
+    end
   end
 end
