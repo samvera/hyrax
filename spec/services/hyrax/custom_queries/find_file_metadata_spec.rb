561ef658c72476dfb1ecda383a97322b294b64aa
E. Lynette Rayle <elr37@cornell.edu>
2019-10-16 20:59:57 -0400

    rewrote FindFiles custom query specs to match style of others
    Matching style of find_access_control_spec.rb in PR #4078

--- spec/services/hyrax/custom_queries/find_file_metadata_spec.rb
+++ spec/services/hyrax/custom_queries/find_file_metadata_spec.rb
@@ -1,118 +1,72 @@
 RSpec.describe Hyrax::CustomQueries::FindFileMetadata do
-  before do
-    query_service.custom_queries.register_query_handler(described_class)
-  end
-  let(:metadata_adapter) { Valkyrie::Persistence::Memory::MetadataAdapter.new }
-  let(:query_service) { metadata_adapter.query_service }
-  let(:persister) { metadata_adapter.persister }
+  let(:query_service) { Valkyrie::MetadataAdapter.find(:test_adapter).query_service }
+  subject(:query_handler) { described_class.new(query_service: query_service) }
 
-  let(:id1) { 'file1' }
-  let(:alt_id1) { 'file1_alt' }
-  let(:valk_id1) { ::Valkyrie::ID.new(id1) }
-  let(:valk_alt_id1) { ::Valkyrie::ID.new(alt_id1) }
-  let(:content1) { 'some text for content' }
-  let(:original_filename1) { 'some_text.txt' }
-  let(:mimetype1) { 'text/plain' }
-  let(:file_metadata) do
-    Hyrax::FileMetadata.new.tap do |fm|
-      fm.id = valk_id1
-      fm.alternate_ids = valk_alt_id1
-      fm.content = content1
-      fm.original_filename = original_filename1
-      fm.mime_type = mimetype1
-      persister.save(resource: fm)
-    end
-  end
-
-  let(:id2) { 'file2' }
-  let(:alt_id2) { 'file2_alt' }
-  let(:valk_id2) { ::Valkyrie::ID.new(id2) }
-  let(:valk_alt_id2) { ::Valkyrie::ID.new(alt_id2) }
-  let(:content2) { '<h3>other context we should not find</h3>' }
-  let(:original_filename2) { 'other_text.html' }
-  let(:mimetype2) { 'text/html' }
-  let(:file_metadata2) do
-    Hyrax::FileMetadata.new.tap do |fm|
-      fm.id = valk_id2
-      fm.alternate_ids = valk_alt_id2
-      fm.content = content2
-      fm.original_filename = original_filename2
-      fm.mime_type = mimetype2
-      persister.save(resource: fm)
+  describe '.queries' do
+    it 'lists queries' do
+      expect(described_class.queries).to eq [:find_file_metadata_by,
+                                             :find_file_metadata_by_alternate_identifier,
+                                             :find_many_file_metadata_by_ids]
     end
   end
 
   describe '.find_file_metadata_by' do
-    subject { query_service.custom_queries.find_file_metadata_by(id: valk_id) }
-    context 'when file exists' do
-      before do
-        file_metadata
-        file_metadata2
-      end
-      let(:valk_id) { valk_id1 }
+    context 'when id is for a file metadata resource' do
+      let!(:file_metadata1) { FactoryBot.create_using_test_adapter(:hyrax_file_metadata) }
+      let!(:file_metadata2) { FactoryBot.create_using_test_adapter(:hyrax_file_metadata) }
       it 'returns file metadata resource' do
-        expect(subject.id.to_s).to eq id1
-        expect(subject.alternate_ids.first.to_s).to eq alt_id1
-        expect(subject.content.first).to eq content1
-        expect(subject.original_filename.first).to eq original_filename1
-        expect(subject.mime_type.first).to eq mimetype1
+        expect(query_handler.find_file_metadata_by(id: file_metadata1.id).id.to_s).to eq file_metadata1.id.to_s
       end
     end
 
-    context 'when file does not exist' do
-      before do
-        file_metadata
-        file_metadata2
+    context 'when id for a non-file metadata resource' do
+      let!(:resource) { FactoryBot.create_using_test_adapter(:hyrax_resource) }
+      it 'raises ObjectNotFound' do
+        expect { query_handler.find_file_metadata_by(id: resource.id) }
+          .to raise_error ::Valkyrie::Persistence::ObjectNotFoundError, "Result type Hyrax::Resource for id #{resource.id} is not a `Hyrax::FileMetadata`"
       end
-      let(:valk_id) { ::Valkyrie::ID.new('BOGUS') }
-      it 'raises not found error' do
-        expect { subject }.to raise_error Valkyrie::Persistence::ObjectNotFoundError
+    end
+
+    context 'when id is invalid' do
+      it 'raises ObjectNotFound' do
+        expect { query_handler.find_file_metadata_by(id: 'BOGUS') }
+          .to raise_error Valkyrie::Persistence::ObjectNotFoundError
       end
     end
   end
 
   describe '.find_file_metadata_by_alternate_identifier' do
-    subject { query_service.custom_queries.find_file_metadata_by_alternate_identifier(alternate_identifier: valk_alt_id) }
     context 'when file exists' do
-      before do
-        file_metadata
-        file_metadata2
-      end
-      let(:valk_alt_id) { valk_alt_id2 }
+      let!(:file_metadata1) { FactoryBot.create_using_test_adapter(:hyrax_file_metadata, alternate_ids: ['fm1']) }
+      let!(:file_metadata2) { FactoryBot.create_using_test_adapter(:hyrax_file_metadata, alternate_ids: ['fm2']) }
       it 'returns file metadata resource' do
-        expect(subject.id.to_s).to eq id2
-        expect(subject.alternate_ids.first.to_s).to eq alt_id2
-        expect(subject.content.first).to eq content2
-        expect(subject.original_filename.first).to eq original_filename2
-        expect(subject.mime_type.first).to eq mimetype2
+        expect(query_handler.find_file_metadata_by_alternate_identifier(alternate_identifier: 'fm2').id.to_s).to eq file_metadata2.id.to_s
       end
     end
 
-    context 'when file does not exist' do
-      before do
-        file_metadata
-        file_metadata2
+    context 'when id for a non-file metadata resource' do
+      let!(:resource) { FactoryBot.create_using_test_adapter(:hyrax_resource, alternate_ids: ['r1']) }
+      it 'raises ObjectNotFound' do
+        expect { query_handler.find_file_metadata_by_alternate_identifier(alternate_identifier: 'r1').id.to_s }
+          .to raise_error ::Valkyrie::Persistence::ObjectNotFoundError, "Result type Hyrax::Resource for alternate_identifier #{resource.alternate_ids.first} is not a `Hyrax::FileMetadata`"
       end
-      let(:valk_alt_id) { ::Valkyrie::ID.new('BOGUS') }
-      it 'raises not found error' do
-        expect { subject }.to raise_error Valkyrie::Persistence::ObjectNotFoundError
+    end
+
+    context 'when id is invalid' do
+      it 'raises ObjectNotFound' do
+        expect { query_handler.find_file_metadata_by_alternate_identifier(alternate_identifier: 'BOGUS') }
+          .to raise_error Valkyrie::Persistence::ObjectNotFoundError
       end
     end
   end
 
   describe '.find_many_file_metadata_by_ids' do
-    let(:query_service) { Valkyrie::MetadataAdapter.find(:test_adapter).query_service }
-    subject { query_service.custom_queries.find_many_file_metadata_by_ids(ids: ids) }
     context 'when files exists' do
       let!(:file_metadata1) { FactoryBot.create_using_test_adapter(:hyrax_file_metadata) }
       let!(:file_metadata2) { FactoryBot.create_using_test_adapter(:hyrax_file_metadata) }
       let(:ids) { [file_metadata1.id, file_metadata2.id] }
-      before do
-        expect(file_metadata1.id).not_to be_empty
-        expect(file_metadata2.id).not_to be_empty
-      end
       it 'returns file metadata resources' do
-        expect(subject.map { |fm| fm.id.to_s }).to match_array(ids.map(&:to_s))
+        expect(query_handler.find_many_file_metadata_by_ids(ids: ids).map { |fm| fm.id.to_s }).to match_array(ids.map(&:to_s))
       end
     end
 
@@ -122,12 +76,8 @@ RSpec.describe Hyrax::CustomQueries::FindFileMetadata do
       let!(:non_file_metadata_resource) { FactoryBot.create_using_test_adapter(:hyrax_resource) }
       let!(:non_existent_id) { ::Valkyrie::ID.new('BOGUS') }
       let(:ids) { [file_metadata1.id, file_metadata2.id, non_file_metadata_resource.id, non_existent_id] }
-      before do
-        expect(file_metadata1.id).not_to be_empty
-        expect(file_metadata2.id).not_to be_empty
-      end
       it 'only includes file metadata resources' do
-        expect(subject.map { |fm| fm.id.to_s }).to match_array [file_metadata1.id.to_s, file_metadata2.id.to_s]
+        expect(query_handler.find_many_file_metadata_by_ids(ids: ids).map { |fm| fm.id.to_s }).to match_array [file_metadata1.id.to_s, file_metadata2.id.to_s]
       end
     end
 
@@ -135,15 +85,15 @@ RSpec.describe Hyrax::CustomQueries::FindFileMetadata do
       let!(:non_file_metadata_resource) { FactoryBot.create_using_test_adapter(:hyrax_resource) }
       let!(:non_existent_id) { ::Valkyrie::ID.new('BOGUS') }
       let(:ids) { [non_file_metadata_resource.id, non_existent_id] }
-      it 'returns empty array' do
-        expect(subject).to be_empty
+      it 'result is empty' do
+        expect(query_handler.find_many_file_metadata_by_ids(ids: ids)).to be_empty
       end
     end
 
     context 'when passed empty ids array' do
       let(:ids) { [] }
-      it 'returns empty array' do
-        expect(subject).to be_empty
+      it 'result is empty' do
+        expect(query_handler.find_many_file_metadata_by_ids(ids: ids)).to be_empty
       end
     end
   end
--- spec/wings/services/custom_queries/find_file_metadata_spec.rb
+++ spec/wings/services/custom_queries/find_file_metadata_spec.rb
@@ -4,45 +4,51 @@ require 'wings/services/custom_queries/find_file_metadata'
 
 RSpec.describe Wings::CustomQueries::FindFileMetadata, :clean_repo do
   let(:query_service) { Hyrax.query_service }
+  subject(:query_handler) { described_class.new(query_service: query_service) }
 
-  let(:pcdmfile) do
+  let(:pcdmfile1) do
     Hydra::PCDM::File.new.tap do |f|
-      f.id = af_file_id
+      f.id = af_file_id1
       f.content = 'some text for content'
       f.original_name = 'some_text.txt'
       f.mime_type = 'text/plain'
       f.save!
     end
   end
-  let(:af_file_id) { 'file1' }
-  let(:valk_id) { ::Valkyrie::ID.new(af_file_id) }
+  let(:af_file_id1) { 'file1' }
+  let(:valk_id1) { ::Valkyrie::ID.new(af_file_id1) }
 
-  let(:subject) { query_service.custom_queries.find_file_metadata_by(id: valk_id, use_valkyrie: use_valkyrie_value) }
+  describe '.queries' do
+    it 'lists queries' do
+      expect(described_class.queries).to eq [:find_file_metadata_by,
+                                             :find_file_metadata_by_alternate_identifier,
+                                             :find_many_file_metadata_by_ids]
+    end
+  end
 
   describe '.find_file_metadata_by' do
     context 'when use_valkyrie: false' do
-      before { pcdmfile }
-      let(:use_valkyrie_value) { false }
+      before { pcdmfile1 }
       it 'returns AF File' do
-        expect(subject).to be_a ActiveFedora::File
-        expect(subject.id).to eq af_file_id
+        result = query_handler.find_file_metadata_by(id: valk_id1, use_valkyrie: false)
+        expect(result).to be_a Hydra::PCDM::File
+        expect(result.id).to eq af_file_id1
       end
     end
 
     context 'when use_valkyrie: true' do
-      before { pcdmfile }
-      let(:use_valkyrie_value) { true }
+      before { pcdmfile1 }
       it 'returns ActiveFedora objects' do
-        expect(subject).to be_a Hyrax::FileMetadata
-        expect(subject.id).to eq valk_id
+        result = query_handler.find_file_metadata_by(id: valk_id1, use_valkyrie: true)
+        expect(result).to be_a Hyrax::FileMetadata
+        expect(result.id).to eq valk_id1
       end
     end
 
     context 'when invalid id' do
-      let(:use_valkyrie_value) { true }
-      let(:valk_id) { ::Valkyrie::ID.new('1212121212') }
+      let(:valk_id1) { ::Valkyrie::ID.new('BOGUS') }
       it 'returns error' do
-        expect { subject } .to raise_error
+        expect { query_handler.find_file_metadata_by(id: valk_id1) }.to raise_error Hyrax::ObjectNotFoundError
       end
     end
   end
@@ -59,58 +65,51 @@ RSpec.describe Wings::CustomQueries::FindFileMetadata, :clean_repo do
     end
     let(:af_file_id2) { 'file2' }
     let(:valk_id2)    { ::Valkyrie::ID.new(af_file_id2) }
-    let(:ids)         { [valk_id, valk_id2] }
-    let(:use_valkyrie) { true }
+    let(:ids)         { [valk_id1, valk_id2] }
 
     before do
-      pcdmfile
+      pcdmfile1
       pcdmfile2
     end
 
-    subject { query_service.custom_queries.find_many_file_metadata_by_ids(ids: ids, use_valkyrie: use_valkyrie) }
     context 'when use_valkyrie: false' do
-      let(:use_valkyrie) { false }
       it 'returns AF Files' do
-        expect(subject).to be_a Array
-        expect(subject.size).to eq 2
-        expect(subject.first).to be_a ActiveFedora::File
-        expect(subject.map { |fm| fm.id.to_s }).to match_array(ids.map(&:to_s))
+        result = query_handler.find_many_file_metadata_by_ids(ids: ids, use_valkyrie: false)
+        expect(result.first).to be_a Hydra::PCDM::File
+        expect(result.map { |fm| fm.id.to_s }).to match_array(ids.map(&:to_s))
       end
     end
 
     context 'when use_valkyrie: true' do
-      let(:use_valkyrie) { true }
       it 'returns Hyrax::FileMetadata resources' do
-        expect(subject).to be_a Array
-        expect(subject.size).to eq 2
-        expect(subject.first).to be_a Hyrax::FileMetadata
-        expect(subject.map { |fm| fm.id.to_s }).to match_array(ids.map(&:to_s))
+        result = query_handler.find_many_file_metadata_by_ids(ids: ids, use_valkyrie: true)
+        expect(result.first).to be_a Hyrax::FileMetadata
+        expect(result.map { |fm| fm.id.to_s }).to match_array(ids.map(&:to_s))
       end
     end
 
     context 'when some ids are for non-file metadata resources' do
       let!(:non_existent_id) { ::Valkyrie::ID.new('BOGUS') }
-      let(:ids) { [valk_id, valk_id2, non_existent_id] }
+      let(:ids) { [valk_id1, valk_id2, non_existent_id] }
       it 'only includes file metadata resources' do
-        expect(subject).to be_a Array
-        expect(subject.size).to eq 2
-        expect(subject.first).to be_a Hyrax::FileMetadata
-        expect(subject.map { |fm| fm.id.to_s }).to match_array [valk_id.to_s, valk_id2.to_s]
+        result = query_handler.find_many_file_metadata_by_ids(ids: ids, use_valkyrie: true)
+        expect(result.first).to be_a Hyrax::FileMetadata
+        expect(result.map { |fm| fm.id.to_s }).to match_array [valk_id1.to_s, valk_id2.to_s]
       end
     end
 
     context 'when not passed any valid ids' do
       let!(:non_existent_id) { ::Valkyrie::ID.new('BOGUS') }
       let(:ids) { [non_existent_id] }
-      it 'returns empty array' do
-        expect(subject).to eq []
+      it 'result is empty' do
+        expect(query_handler.find_many_file_metadata_by_ids(ids: ids)).to be_empty
       end
     end
 
     context 'when passed empty ids array' do
       let(:ids) { [] }
-      it 'returns empty array' do
-        expect(subject).to eq []
+      it 'result is empty' do
+        expect(query_handler.find_many_file_metadata_by_ids(ids: ids)).to be_empty
       end
     end
   end
