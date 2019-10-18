06b74754942f00f3151e9b835975d2c2e763d3fe
Tom Johnson <tomjohnson@ucsb.edu>
2019-10-17 22:29:45 -0700

    Deprecate unused `#to_sipity_entity`
    This method avoids the more robust `PowerConverter` approach, which is actually
    used throughout Hyrax. Encourage applications to use that instead.

--- spec/models/generic_work_spec.rb
+++ spec/models/generic_work_spec.rb
@@ -23,20 +23,6 @@ RSpec.describe GenericWork do
     it { is_expected.to include("has_model", "create_date", "modified_date") }
   end
 
-  describe "to_sipity_entity" do
-    let(:state) { create(:workflow_state) }
-    let(:work) { create(:work) }
-
-    before do
-      Sipity::Entity.create!(proxy_for_global_id: work.to_global_id.to_s,
-                             workflow_state: state,
-                             workflow: state.workflow)
-    end
-    subject { work.to_sipity_entity }
-
-    it { is_expected.to be_kind_of Sipity::Entity }
-  end
-
   describe '#state' do
     let(:work) { described_class.new(state: inactive) }
     let(:inactive) { ::RDF::URI('http://fedora.info/definitions/1/0/access/ObjState#inactive') }
