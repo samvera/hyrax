e8f4f94a1c76e1dff90bc44c188d27b9c29c6d37
Tom Johnson <tomjohnson@ucsb.edu>
2019-10-18 14:44:16 -0700

    Deprecate `PowerConverter` based `to_polymorphic_type`
    This was very sparsely used. Instead of porting it elsewhere, just cast the one
    caller individually.
    
    If folks are using this downstream (I'd guess they aren't) we could consider
    putting this mapper at the top `Hyrax` level, or somewhere similar.

--- app/conversions/power_converters/polymorphic_type.rb
+++ app/conversions/power_converters/polymorphic_type.rb
@@ -1,4 +1,5 @@
 PowerConverter.define_conversion_for(:polymorphic_type) do |input|
+  Deprecation.warn('PowerConverter is deprecated. Use `.base_class` instead')
   if input.respond_to?(:base_class)
     input.base_class
   elsif input.is_a?(ActiveRecord::Base)
