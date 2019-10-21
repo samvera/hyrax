cfce365319ada7b9a3cf13c42d066ff0f775a98c
Brian Keese <bkeese@indiana.edu>
2019-10-21 18:45:17 -0500

    Update app/jobs/hyrax/grant_edit_job.rb
    Co-Authored-By: Tom Johnson <tomjohnson@ucsb.edu>

--- app/jobs/hyrax/grant_edit_job.rb
+++ app/jobs/hyrax/grant_edit_job.rb
@@ -1,6 +1,6 @@
 module Hyrax
   # Grants the user's edit access on the provided FileSet
-  class GrantEditJob < Hyrax::ApplicationJob
+  class GrantEditJob < ApplicationJob
     queue_as Hyrax.config.ingest_queue_name
 
     # @param [String] file_set_id - the identifier of the object to grant access to
