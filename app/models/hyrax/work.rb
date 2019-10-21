23a40d5d8693f736fdda7af3e7722ef9b506fe2f
Tom Johnson <tomjohnson@ucsb.edu>
2019-10-20 17:25:02 -0700

    Implement Resource state in a model agnostic way
    Extract resource state/status to `Hyrax::ResourceStatus`. This service is
    agnostic to Valkyrie/ActiveFedora models, but respects Fedora "resource status"
    URIs.
    
    Additionally, add a `#state` attribute to `Hyrax::Work`. This readds an
    attribute previously handled by `Hyrax::WorkBehavior`.
    
    Though Hyrax only uses/respects Fedora's "inactive" status, the utility we
    provide supports three statuses from the Fedora Resource Status vocabulary:
    active, inactive, and deleted.

--- app/models/hyrax/work.rb
+++ app/models/hyrax/work.rb
@@ -10,6 +10,7 @@ module Hyrax
 
     attribute :admin_set_id, Valkyrie::Types::ID
     attribute :member_ids,   Valkyrie::Types::Array.of(Valkyrie::Types::ID).meta(ordered: true)
+    attribute :state,        Valkyrie::Types::URI.default(Hyrax::ResourceStatus::ACTIVE)
 
     ##
     # @return [Boolean] true
---
+++ app/services/hyrax/resource_status.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+module Hyrax
+  ##
+  # Implements the Fedora objState ontology against the given resource.
+  #
+  # Use the provided constants as values for `#status` to set state. The
+  # boolean methods provide easy checking of state.
+  #
+  # We assume that no-state, means none of the three object statuses are
+  # satisfied. Errors are raised if the `#state` attribute isn't defined.
+  #
+  # @see http://fedora.info/definitions/1/0/access/ObjState
+  #
+  # @example
+  #   status = ResourceStatus.new(resource: my_resource)
+  #   status.inactive? # => false
+  #
+  #   my_resource.state = ResourceStatus::INACTIVE
+  #   status = ResourceStatus.new(resource: my_resource)
+  #   status.inactive? # => true
+  #
+  class ResourceStatus
+    ACTIVE   = Vocab::FedoraResourceStatus.active.freeze
+    DELETED  = Vocab::FedoraResourceStatus.deleted.freeze
+    INACTIVE = Vocab::FedoraResourceStatus.inactive.freeze
+
+    ##
+    # @!attribute [rw] resource
+    #   @return [#state]
+    attr_accessor :resource
+
+    ##
+    # @param [#state] resource
+    def initialize(resource:)
+      self.resource = resource
+    end
+
+    ##
+    # @return [Boolean]
+    # @raise [NoMethodError] if the resource doesn't have a state attribute
+    def active?
+      resource.state == ACTIVE
+    end
+
+    ##
+    # @return [Boolean]
+    # @raise [NoMethodError] if the resource doesn't have a state attribute
+    def deleted?
+      resource.state == DELETED
+    end
+
+    ##
+    # @return [Boolean]
+    # @raise [NoMethodError] if the resource doesn't have a state attribute
+    def inactive?
+      resource.state == INACTIVE
+    end
+  end
+end
