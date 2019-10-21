4c6641ed25ab007556511a84f9c1604453c54ee4
Andrew Myers <andrew_myers@wgbh.org>
2019-10-21 15:17:36 -0500

    Valkyrizes jobs that revoke edit permissions
    Allows jobs that revoke edit permissions from file set objects and from file sets
    that are members of a work to use Valkyrie.
    
    Refs #4102
    Refs #4103

--- app/jobs/hyrax/revoke_edit_from_members_job.rb
+++ app/jobs/hyrax/revoke_edit_from_members_job.rb
@@ -1,14 +1,15 @@
 module Hyrax
   # Revokes edit access for the supplied user for the members attached to a work
-  class RevokeEditFromMembersJob < ApplicationJob
+  class RevokeEditFromMembersJob < Hyrax::ApplicationJob
     queue_as Hyrax.config.ingest_queue_name
 
     # @param [ActiveFedora::Base] work - the work object
     # @param [String] user_key - the user to remove
-    def perform(work, user_key)
+    # @param [Boolean] use_valkyrie - use valkyrie objects for this operation?
+    def perform(work, user_key, use_valkyrie: Hyrax.config.use_valkyrie?)
       # Iterate over ids because reifying objects is slow.
       file_set_ids(work).each do |file_set_id|
-        RevokeEditJob.perform_now(file_set_id, user_key)
+        RevokeEditJob.perform_now(file_set_id, user_key, use_valkyrie: use_valkyrie)
       end
     end
 
@@ -17,7 +18,12 @@ module Hyrax
       # Filter the member ids and return only the FileSet ids (filter out child works)
       # @return [Array<String>] the file set ids
       def file_set_ids(work)
-        ::FileSet.search_with_conditions(id: work.member_ids).map(&:id)
+        case work
+        when ActiveFedora::Base
+          ::FileSet.search_with_conditions(id: work.member_ids).map(&:id)
+        when Valkyrie::Resource
+          Hyrax.query_service.custom_queries.find_child_fileset_ids(resource: work)
+        end
       end
   end
 end
