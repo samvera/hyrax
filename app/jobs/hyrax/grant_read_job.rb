b3126462399c44e0523969b8a6f1ea734bff3568
Collin Brittle <cnbrittle@gmail.com>
2019-10-21 19:34:16 +0000

    Valkyrize grant_read_job and grant_read_to_members_job
    Also changes the jobs inheritance to use the Hyrax::ApplicationJob

--- app/jobs/hyrax/grant_read_job.rb
+++ app/jobs/hyrax/grant_read_job.rb
@@ -1,14 +1,23 @@
 module Hyrax
   # Grants the user's read access on the provided FileSet
-  class GrantReadJob < ApplicationJob
+  class GrantReadJob < Hyrax::ApplicationJob
     queue_as Hyrax.config.ingest_queue_name
 
     # @param [String] file_set_id - the identifier of the object to grant access to
     # @param [String] user_key - the user to add
-    def perform(file_set_id, user_key)
-      file_set = ::FileSet.find(file_set_id)
-      file_set.read_users += [user_key]
-      file_set.save!
+    # @param [Boolean] use_valkyrie - use valkyrie resources for this operation?
+    def perform(file_set_id, user_key, use_valkyrie: Hyrax.config.use_valkyrie?)
+      if use_valkyrie
+        file_set_id = Valkyrie::ID.new(file_set_id)
+        file_set_resource = Hyrax.query_service.find_by(id: file_set_id)
+        acl = Hyrax::AccessControlList.new(resource: file_set_resource)
+        acl.grant(:read).to(::User.find_by_user_key(user_key))
+        acl.save
+      else
+        file_set = ::FileSet.find(file_set_id)
+        file_set.read_users += [user_key]
+        file_set.save!
+      end
     end
   end
 end
--- spec/jobs/hyrax/grant_read_job_spec.rb
+++ spec/jobs/hyrax/grant_read_job_spec.rb
@@ -1,11 +1,23 @@
 RSpec.describe Hyrax::GrantReadJob do
   let(:depositor) { create(:user) }
-  let(:file_set) { build(:file_set) }
 
-  it 'grants a user read access to a FileSet' do
-    expect(FileSet).to receive(:find).with(file_set.id).and_return(file_set)
-    expect(file_set).to receive(:read_users=).with(array_including(depositor.user_key))
-    expect(file_set).to receive(:save!)
-    described_class.perform_now(file_set.id, depositor.user_key)
+  context "when use_valkyrie is false" do
+    let(:file_set) { create(:file_set, user: depositor) }
+
+    it 'grants a user read access to a FileSet' do
+      described_class.perform_now(file_set.id, depositor.user_key, use_valkyrie: false)
+      file_set.reload
+      expect(file_set.read_users).to include depositor.user_key
+    end
+  end
+
+  context "when use_valkyrie is true" do
+    let(:valk_file_set) { valkyrie_create(:hyrax_file_set) }
+
+    it 'grants a user read access to a FileSet' do
+      described_class.perform_now(valk_file_set.id.to_s, depositor.user_key, use_valkyrie: true)
+      reloaded_file_set = Hyrax.query_service.find_by(id: valk_file_set.id)
+      expect(reloaded_file_set.read_users.to_a).to include depositor.user_key
+    end
   end
 end
