1dd0d19b1c29cb4d4c4ec85ff9f5622c543bf9dc
Brian Keese <bkeese@indiana.edu>
2019-10-21 15:25:34 -0400

    Valkyrize grant_edit_job
    Update app/jobs/hyrax/grant_edit_to_members_job.rb
    
    Update app/jobs/hyrax/grant_edit_job.rb

--- app/jobs/hyrax/grant_edit_job.rb
+++ app/jobs/hyrax/grant_edit_job.rb
@@ -5,10 +5,19 @@ module Hyrax
 
     # @param [String] file_set_id - the identifier of the object to grant access to
     # @param [String] user_key - the user to add
-    def perform(file_set_id, user_key)
-      file_set = ::FileSet.find(file_set_id)
-      file_set.edit_users += [user_key]
-      file_set.save!
+    # @param [Boolean] use_valkyrie - whether to use valkyrie support
+    def perform(file_set_id, user_key, use_valkyrie: Hyrax.config.use_valkyrie?)
+      if use_valkyrie
+        id = Valkyrie::ID.new(file_set_id)
+        file_set = Hyrax.query_service.find_by(id: id)
+        acl = Hyrax::AccessControlList.new(resource: file_set)
+        acl.grant(:edit).to(::User.find_by_user_key(user_key))
+        acl.save
+      else
+        file_set = ::FileSet.find(file_set_id)
+        file_set.edit_users += [user_key]
+        file_set.save!
+      end
     end
   end
 end
--- app/jobs/hyrax/grant_edit_to_members_job.rb
+++ app/jobs/hyrax/grant_edit_to_members_job.rb
@@ -5,11 +5,12 @@ module Hyrax
 
     # @param [ActiveFedora::Base] work - the work object
     # @param [String] user_key - the user to add
+    # @param [Boolean] use_valkyrie - use valkyrie objects for this operation?
     def perform(work, user_key)
       # Iterate over ids because reifying objects is slow.
       file_set_ids(work).each do |file_set_id|
         # Call this synchronously, since we're already in a job
-        GrantEditJob.perform_now(file_set_id, user_key)
+        GrantEditJob.perform_now(file_set_id, user_key, use_valkyrie: work.is_a?(Valkyrie::Resource))
       end
     end
 
@@ -18,7 +19,12 @@ module Hyrax
       # Filter the member ids and return only the FileSet ids (filter out child works)
       # @return [Array<String>] the file set ids
       def file_set_ids(work)
-        ::FileSet.search_with_conditions(id: work.member_ids).map(&:id)
+        case work
+        when ActiveFedora::Base
+          ::FileSet.search_with_conditions(id: work.member_ids).map(&:id)
+        when Valkyrie::Resource
+          Hyrax.query_service.custom_queries.find_child_fileset_ids(resource: work)
+        end
       end
   end
 end
