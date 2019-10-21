23477c2033b29b1bff8c54d24ef18f3e2562384a
Justin Coyne <jcoyne@justincoyne.com>
2019-10-21 14:18:06 -0500

    Avoid deprecated MultiFormatter::[] method
    Use the recommended #new instead

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,10 +9,10 @@ require 'simplecov'
 require 'coveralls'
 Coveralls.wear!
 SimpleCov.root(File.expand_path('../..', __FILE__))
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
-  Coveralls::SimpleCov::Formatter,
-  SimpleCov::Formatter::HTMLFormatter
-]
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+                                                                 Coveralls::SimpleCov::Formatter,
+                                                                 SimpleCov::Formatter::HTMLFormatter
+                                                               ])
 SimpleCov.start('rails') do
   add_filter '/.internal_test_app'
   add_filter '/lib/generators'
