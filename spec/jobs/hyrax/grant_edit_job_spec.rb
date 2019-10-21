93fb80eeb2b599a045477bbf55dc4840b823fd1d
Brian Keese <bkeese@indiana.edu>
2019-10-21 15:25:34 -0400

    Valkyrize grant_edit_job
    Update app/jobs/hyrax/grant_edit_to_members_job.rb
    
    Update app/jobs/hyrax/grant_edit_job.rb

--- spec/jobs/hyrax/grant_edit_job_spec.rb
+++ spec/jobs/hyrax/grant_edit_job_spec.rb
@@ -1,11 +1,23 @@
 RSpec.describe Hyrax::GrantEditJob do
   let(:depositor) { create(:user) }
-  let(:file_set) { build(:file_set) }
 
-  it 'grants a user edit access to a FileSet' do
-    expect(FileSet).to receive(:find).with(file_set.id).and_return(file_set)
-    expect(file_set).to receive(:edit_users=).with(array_including(depositor.user_key))
-    expect(file_set).to receive(:save!)
-    described_class.perform_now(file_set.id, depositor.user_key)
+  context "when use_valkyrie is false" do
+    let(:file_set) { create(:file_set) }
+
+    it 'grants a user edit access to a FileSet' do
+      described_class.perform_now(file_set.id, depositor.user_key, use_valkyrie: false)
+      file_set.reload
+      expect(file_set.edit_users).to include depositor.user_key
+    end
+  end
+
+  context "when use_valkyrie is true" do
+    let(:file_set) { valkyrie_create(:hyrax_file_set) }
+
+    it 'grants a user edit access to a FileSet' do
+      described_class.perform_now(file_set.id.to_s, depositor.user_key, use_valkyrie: true)
+      reloaded_file_set = Hyrax.query_service.find_by(id: file_set.id)
+      expect(reloaded_file_set.edit_users).to include depositor.user_key
+    end
   end
 end
--- spec/jobs/hyrax/grant_edit_to_members_job_spec.rb
+++ spec/jobs/hyrax/grant_edit_to_members_job_spec.rb
@@ -1,16 +1,31 @@
 RSpec.describe Hyrax::GrantEditToMembersJob do
   let(:depositor) { create(:user) }
-  let(:work) { build(:work) }
-  let(:file_set_ids) { ['xyz123abc', 'abc789zyx'] }
 
-  before do
-    allow_any_instance_of(described_class).to receive(:file_set_ids).with(work).and_return(file_set_ids)
-  end
+  context "when use_valkyrie is false" do
+    let(:work) { create(:work) }
+    let(:file_set_ids) { ['xyz123abc', 'abc789zyx'] }
 
-  it 'loops over FileSet IDs, spawning a job for each' do
-    file_set_ids.each do |file_set_id|
-      expect(Hyrax::GrantEditJob).to receive(:perform_now).with(file_set_id, depositor.user_key).once
+    before do
+      allow_any_instance_of(described_class).to receive(:file_set_ids).with(work).and_return(file_set_ids)
     end
-    described_class.perform_now(work, depositor.user_key)
+
+    it 'loops over FileSet IDs, spawning a job for each' do
+      file_set_ids.each do |file_set_id|
+        expect(Hyrax::GrantEditJob).to receive(:perform_now).with(file_set_id, depositor.user_key, use_valkyrie: false).once
+      end
+      described_class.perform_now(work, depositor.user_key)
+    end
+  end
+  context "when use_valkyrie is true" do
+    let(:file_set1) { valkyrie_create(:hyrax_file_set) }
+    let(:file_set2) { valkyrie_create(:hyrax_file_set) }
+    let(:work) { valkyrie_create(:hyrax_work, member_ids: [file_set1.id, file_set2.id]) }
+
+    it 'loops over FileSet IDs, spawning a job for each' do
+      work.member_ids.each do |file_set_id|
+        expect(Hyrax::GrantEditJob).to receive(:perform_now).with(file_set_id, depositor.user_key, use_valkyrie: true).once
+      end
+      described_class.perform_now(work, depositor.user_key)
+    end
   end
 end
