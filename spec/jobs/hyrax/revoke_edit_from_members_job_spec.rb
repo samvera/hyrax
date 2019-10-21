f81515c5a90c3508457a932f4f5c5e42b3733ecb
Andrew Myers <andrew_myers@wgbh.org>
2019-10-21 15:17:36 -0500

    Valkyrizes jobs that revoke edit permissions
    Allows jobs that revoke edit permissions from file set objects and from file sets
    that are members of a work to use Valkyrie.
    
    Refs #4102
    Refs #4103

--- spec/jobs/hyrax/revoke_edit_from_members_job_spec.rb
+++ spec/jobs/hyrax/revoke_edit_from_members_job_spec.rb
@@ -1,16 +1,33 @@
 RSpec.describe Hyrax::RevokeEditFromMembersJob do
   let(:depositor) { create(:user) }
-  let(:work) { build(:work) }
-  let(:file_set_ids) { ['xyz123abc', 'abc789zyx'] }
 
-  before do
-    allow_any_instance_of(described_class).to receive(:file_set_ids).with(work).and_return(file_set_ids)
+
+  context "when use_valkyrie is false" do
+    let(:work) { build(:work) }
+    let(:file_set_ids) { ['xyz123abc', 'abc789zyx'] }
+
+    before do
+      allow_any_instance_of(described_class).to receive(:file_set_ids).with(work).and_return(file_set_ids)
+    end
+
+    it 'loops over FileSet IDs, spawning a job for each' do
+      file_set_ids.each do |file_set_id|
+        expect(Hyrax::GrantReadJob).to receive(:perform_now).with(file_set_id, depositor.user_key, use_valkyre: false).once
+      end
+      described_class.perform_now(work, depositor.user_key, use_valkyrie: false)
+    end
   end
 
-  it 'loops over FileSet IDs, spawning a job for each' do
-    file_set_ids.each do |file_set_id|
-      expect(Hyrax::RevokeEditJob).to receive(:perform_now).with(file_set_id, depositor.user_key).once
+  context "when use_valkyrie is true" do
+    let(:file_set1) { valkyrie_create(:hyrax_file_set) }
+    let(:file_set2) { valkyrie_create(:hyrax_file_set) }
+    let(:work) { valkyrie_create(:hyrax_work, member_ids: [file_set1.id, file_set2.id]) }
+
+    it 'loops over FileSet IDs, spawning a job for each' do
+      work.member_ids.each do |file_set_id|
+        expect(Hyrax::RevokeEditJob).to receive(:perform_now).with(file_set_id, depositor.user_key, use_valkyre: true).once
+      end
+      described_class.perform_now(work, depositor.user_key, use_valkyrie: true)
     end
-    described_class.perform_now(work, depositor.user_key)
   end
 end
