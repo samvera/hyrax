e076a2826aee5ee2903601621b49e56b4a531405
Fritz Freiheit <fritx@umich.edu>
2019-10-30 14:24:25 -0400

    Fix for HYRAX-#3972
    The edit works share tab displays raw html at several locations.
    
    Expected behavior:
    The edit works share tab does not display raw html.
    
    In order to test:
    * Log in as Administrator
    * Edit a work
    * Click on "Sharing" tab
    * Under header "Currently Shared With":
    ** should display "Depositor(<some email>)"
    * Under header "Add Sharing" add a group permission:
    ** by selecting from "Group" and "Choose Access" pull downs, then "Add"
    ** The message 'Permissions are not saved until the "Save" button is
       pressed at the bottom of the page.' appears and does not contain raw html.

---
+++ spec/views/hyrax/base/_form_share.erb_spec.rb
@@ -0,0 +1,40 @@
+RSpec.describe 'hyrax/base/_form_share.html.erb', type: :view do
+  let(:ability) { double }
+  let(:work) { GenericWork.new }
+  let(:form) do
+    Hyrax::GenericWorkForm.new(work, ability, controller)
+  end
+  let(:form_template) do
+    %(
+      <%= simple_form_for [main_app, @form] do |f| %>
+        <%= render "hyrax/base/form_share", f: f %>
+      <% end %>
+    )
+  end
+
+  let(:rendered_save) do
+    # explicitly save rendered, as it seems to become empty at some point during processing
+    assign(:form, form)
+    render inline: form_template
+  end
+
+  let(:page) do
+    Capybara::Node::Simple.new(rendered_save)
+  end
+
+  before do
+    user = stub_model(User)
+    allow(view).to receive(:current_ability).and_return(ability)
+    allow(view).to receive(:current_user).and_return(user)
+    allow(ability).to receive(:admin?).and_return(false)
+    allow(view).to receive(:action_name).and_return('new')
+  end
+
+  it "renders the permissions save note" do
+    expect(page).to have_selector('div#save_perm_note', visible: false)
+    expect(rendered_save).to include I18n.t("hyrax.base.form_share.permissions_save_note_html")
+  end
+  it "renders the add this group" do
+    expect(rendered_save).to include I18n.t("hyrax.base.form_share.add_this_group_html")
+  end
+end
