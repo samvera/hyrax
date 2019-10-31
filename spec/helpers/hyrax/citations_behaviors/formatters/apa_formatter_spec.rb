be4bbf9123de5887b35c025aa0bf269cb8b6224a
Julie Allinson <julie@notch8.com>
2019-10-31 17:45:31 +0000

    Return empty string if APAFormatter authors_list is empty (#4135)

---
+++ spec/helpers/hyrax/citations_behaviors/formatters/apa_formatter_spec.rb
@@ -0,0 +1,10 @@
+RSpec.describe Hyrax::CitationsBehaviors::Formatters::ApaFormatter do
+  subject(:formatter) { described_class.new(:no_context) }
+
+  let(:presenter) { Hyrax::WorkShowPresenter.new(SolrDocument.new(work.to_solr), :no_ability) }
+  let(:work)      { build(:generic_work, title: ['Title'], creator: []) }
+
+  it 'formats citations without creators' do
+    expect(formatter.format(presenter)).to eq("<i class=\"citation-title\">Title.</i> ")
+  end
+end
