7c81fa941b17145b73984b1161d9594fc9f738b9
Tom Johnson <tomjohnson@ucsb.edu>
2019-10-31 18:59:41 -0700

    Use a factory method (`.for`) for to get valkyrie indexers
    We'll want to get different indexers for different kinds of resources, so we can
    prepare them for indexing according to a customized schema. Switching to a
    factory method instead of an initializer allows us to create a new indexer
    instance based on the properties of the resource.
    
    In Hyrax, we'll likely build from the factory depending on class, but
    applications can easily override to build depending on schema/attributes,
    present values, etc..., by providing a different `ValkyrieIndexer` to their
    `IndexingAdapter`, with a custem `.for` implementation.
    
    Connected to #3800.

--- spec/indexers/hyrax/valkyrie_indexer_spec.rb
+++ spec/indexers/hyrax/valkyrie_indexer_spec.rb
@@ -4,6 +4,13 @@ RSpec.describe Hyrax::ValkyrieIndexer do
   let(:indexer) { described_class.new(resource: resource) }
   let(:resource) { FactoryBot.valkyrie_create(:hyrax_resource) }
 
+  describe '.for' do
+    it 'creates an instance of itself as the default indexer class' do
+      expect(described_class.for(resource: Valkyrie::Resource.new).class)
+        .to eq described_class
+    end
+  end
+
   describe "#to_solr" do
     it "provides id, created_at_dtsi, and updated_at_dtsi" do
       expect(indexer.to_solr).to match a_hash_including(
