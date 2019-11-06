c5f82f91fdc6fc9457700847e6e5d5cdccd46df7
Tom Johnson <tomjohnson@ucsb.edu>
2019-11-06 12:11:21 -0800

    Update spec/hyrax/transactions/steps/ensure_admin_set_spec.rb

--- spec/hyrax/transactions/steps/ensure_admin_set_spec.rb
+++ spec/hyrax/transactions/steps/ensure_admin_set_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe Hyrax::Transactions::Steps::EnsureAdminSet do
       let(:work)      { build(:generic_work, admin_set_id: admin_set.id) }
 
       it 'is a success' do
-        expect(step.call(work)).to be_successful
+        expect(step.call(work)).to be_success
       end
     end
   end
