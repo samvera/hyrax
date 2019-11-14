3e7ddf13ca9394536075a54ba7a1b590c10a2b09
Tom Johnson <tomjohnson@ucsb.edu>
2019-11-14 10:25:43 -0800

    Move wings setup to its own file
    Some of this setup is a bit fragile when called during app generation. By
    isolating it, we can handle errors more carefully. We always reraise when in
    production mode, since that means we aren't generating the app---we want to fail
    eagerly in this case to avoid rampant runtime errors.
    
    This replaces a more narrow error handling approach in `ModelRegistry`.

---
+++ lib/wings/setup.rb
@@ -0,0 +1,33 @@
+ActiveFedora::Base.include Wings::Valkyrizable
+
+Valkyrie::MetadataAdapter.register(
+  Wings::Valkyrie::MetadataAdapter.new, :wings_adapter
+)
+Valkyrie.config.metadata_adapter = :wings_adapter
+
+Valkyrie::StorageAdapter.register(
+  Wings::Storage::ActiveFedora
+    .new(connection: Ldp::Client.new(ActiveFedora.fedora.host), base_path: ActiveFedora.fedora.base_path),
+  :active_fedora
+)
+Valkyrie.config.storage_adapter = :active_fedora
+
+# TODO: Custom query registration is not Wings specific.  These custom_queries need to be registered for other adapters too.
+#       A refactor is needed to add the default implementations to hyrax.rb and only handle the wings specific overrides here.
+custom_queries = [Hyrax::CustomQueries::Navigators::ChildCollectionsNavigator,
+                  Hyrax::CustomQueries::Navigators::ChildFilesetsNavigator,
+                  Hyrax::CustomQueries::Navigators::ChildWorksNavigator,
+                  Hyrax::CustomQueries::Navigators::FindFiles,
+                  Wings::CustomQueries::FindAccessControl, # override Hyrax::CustomQueries::FindAccessControl
+                  Wings::CustomQueries::FindFileMetadata, # override Hyrax::CustomQueries::FindFileMetadata
+                  Wings::CustomQueries::FindManyByAlternateIds] # override Hyrax::CustomQueries::FindManyByAlternateIds
+custom_queries.each do |query_handler|
+  Valkyrie.config.metadata_adapter.query_service.custom_queries.register_query_handler(query_handler)
+end
+
+Wings::ModelRegistry.register(Hyrax::AccessControl,     Hydra::AccessControl)
+Wings::ModelRegistry.register(Hyrax::AdministrativeSet, AdminSet)
+Wings::ModelRegistry.register(Hyrax::PcdmCollection,    ::Collection)
+Wings::ModelRegistry.register(Hyrax::FileSet,           FileSet)
+Wings::ModelRegistry.register(Hyrax::Embargo,           Hydra::AccessControls::Embargo)
+Wings::ModelRegistry.register(Hyrax::Lease,             Hydra::AccessControls::Lease)
