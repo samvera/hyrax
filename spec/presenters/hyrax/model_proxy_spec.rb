69a7aef52c23cd2625cbc7a387a507b05289bb4a
Tom Johnson <tomjohnson@ucsb.edu>
2020-01-10 10:26:33 -0800

    Add tests for `ModelProxy`
    This mixin was previously untested and will need to change to support
    `Valkyrie`. To get ready for the changes, test it against a minimal compatible
    class.

---
+++ spec/presenters/hyrax/model_proxy_spec.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+RSpec.describe Hyrax::ModelProxy do
+  subject(:proxy)     { proxy_class.new(solr_document) }
+  let(:solr_document) { SolrDocument.new(attributes) }
+  let(:model)         { GenericWork }
+
+  let(:attributes) do
+    { "id" => '888888',
+      "has_model_ssim" => [model.to_s] }
+  end
+
+  let(:proxy_class) do
+    Class.new do
+      include Hyrax::ModelProxy
+
+      attr_accessor :solr_document
+
+      def initialize(solr_document)
+        self.solr_document = solr_document
+      end
+    end
+  end
+
+  it { is_expected.to be_persisted }
+
+  describe '#id' do
+    it 'delegates to the solr document' do
+      expect(proxy.id).to eq '888888'
+    end
+  end
+
+  describe '#model_name' do
+    it 'delegates to the has_model_ssim model' do
+      expect(proxy.model_name).to eq model.model_name
+    end
+  end
+
+  describe '#to_key' do
+    it 'delegates to the solr document' do
+      expect(proxy.to_key).to contain_exactly '888888'
+    end
+  end
+
+  describe '#to_model' do
+    it 'gives self' do
+      expect(proxy.to_model).to eql proxy
+    end
+  end
+
+  describe '#to_param' do
+    it 'delegates to the solr document' do
+      expect(proxy.to_param).to eq '888888'
+    end
+  end
+
+  describe '#valid_child_concerns' do
+    it 'delegates to the has_model_ssim model' do
+      expect(proxy.valid_child_concerns)
+        .to contain_exactly(*model.valid_child_concerns)
+    end
+  end
+end
