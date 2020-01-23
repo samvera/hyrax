b20938bcb563255e7e1e1edbe0d86f5ee5cf1d09
Tom Johnson <tomjohnson@ucsb.edu>
2020-01-22 16:29:31 -0800

    Route Valkyrie models registered with Wings to legacy models
    When a Valkyrie model exists and replaces an equivalent `ActiveFedora` model,
    use the `ActiveModel::Name` API to route to the legacy model.
    
    This allows existing apps to avoid changing their routes when introducing and
    using Valkyrie models.

--- app/models/concerns/hyrax/naming.rb
+++ app/models/concerns/hyrax/naming.rb
@@ -1,18 +1,27 @@
+# frozen_string_literal: true
+
 require_dependency 'hyrax/name'
+
 module Hyrax
   module Naming
     extend ActiveSupport::Concern
 
     module ClassMethods
       # Override of ActiveModel::Model name that allows us to use our custom name class
-      def model_name
+      def model_name(name_class: _hyrax_default_name_class)
         @_model_name ||= begin
           namespace = parents.detect do |n|
             n.respond_to?(:use_relative_model_naming?) && n.use_relative_model_naming?
           end
-          Hyrax::Name.new(self, namespace)
+          name_class.new(self, namespace)
         end
       end
+
+      private
+
+        def _hyrax_default_name_class
+          Hyrax::Name
+        end
     end
   end
 end
