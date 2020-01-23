2f037906564d288e3ae2a31b7e3f68fb26fcfcb6
Julie Allinson <julie@notch8.com>
2020-01-23 11:28:13 -0800

    add indexer to work_resource generator

--- lib/generators/hyrax/work_resource/work_resource_generator.rb
+++ lib/generators/hyrax/work_resource/work_resource_generator.rb
@@ -33,6 +33,10 @@ class Hyrax::WorkResourceGenerator < Rails::Generators::NamedBase
       rspec_installed?
   end
 
+  def create_indexer
+    template('indexer.rb.erb', File.join('app/indexers/', class_path, "#{file_name}_indexer.rb"))
+  end
+
   def create_views
     create_file File.join('app/views/hyrax', class_path, "#{plural_file_name}/_#{file_name}.html.erb") do
       "<%# This is a search result view %>\n" \
