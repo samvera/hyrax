0dcf42b3f932f9f2ebf531bc4521aa7a873090f4
aaron-collier <aaron.collier@stanford.edu>
2020-01-24 10:00:42 -0800

    Reduce method complexity

--- app/services/hyrax/resource_characterizer.rb
+++ app/services/hyrax/resource_characterizer.rb
@@ -20,15 +20,24 @@ module Hyrax
     #
     # @raise [RuntimeError] if visibility propogation fails
     def characterize
-      Hydra::Works::CharacterizationService.run(source.characterization_proxy, filepath)
-      Rails.logger.debug "Ran characterization on #{source.characterization_proxy.id} (#{source.characterization_proxy.mime_type})"
-      source.characterization_proxy.alpha_channels = channels(filepath) if source.image? && Hyrax.config.iiif_image_server?
-      Hyrax.persister.save(resource: source.characterization_proxy)
-      Hyrax.persister.save(resource: source)
+      Hydra::Works::CharacterizationService.run(characterization_proxy, filepath)
+      Rails.logger.debug "Ran characterization on #{characterization_proxy.id} (#{characterization_proxy.mime_type})"
+      characterization_proxy.alpha_channels = channels(filepath) if source.image? && Hyrax.config.iiif_image_server?
+      persist
       CreateDerivativesJob.perform_later(source, source.original_file.id, filepath)
     end
 
     private
+
+      def persist
+        Hyrax.persister.save(resource: characterization_proxy)
+        Hyrax.persister.save(resource: source)
+      end
+
+      def characterization_proxy
+        raise "#{source.class.characterization_proxy} was not found for FileSet #{source.id}" unless source.characterization_proxy?
+        source.characterization_proxy
+      end
 
       def filepath
         Hyrax::WorkingDirectory.find_or_retrieve(source.original_file.id, source.id.id)
