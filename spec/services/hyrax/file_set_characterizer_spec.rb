2d0d3f5b1b91cbba864b40410975cf6642074274
aaron-collier <aaron.collier@stanford.edu>
2020-01-24 10:44:52 -0800

    Add FileSetCharacterizer spec

---
+++ spec/services/hyrax/file_set_characterizer_spec.rb
@@ -0,0 +1,42 @@
+RSpec.describe Hyrax::FileSetCharacterizer do
+  subject(:characterizer) { described_class.new(source: file_set) }
+  let(:file_set_id) { 'abc12345' }
+  let(:filename)    { Rails.root.join('tmp', 'uploads', 'ab', 'c1', '23', '45', 'abc12345', 'picture.png').to_s }
+  let(:file_set) do
+    FileSet.new(id: file_set_id).tap do |fs|
+      allow(fs).to receive(:original_file).and_return(file)
+      allow(fs).to receive(:update_index)
+    end
+  end
+  let(:file) do
+    Hydra::PCDM::File.new.tap do |f|
+      f.content = 'foo'
+      f.original_name = 'picture.png'
+      f.save!
+      allow(f).to receive(:save!)
+    end
+  end
+
+  before do
+    allow(FileSet).to receive(:find).with(file_set_id).and_return(file_set)
+    allow(Hydra::Works::CharacterizationService).to receive(:run).with(file, filename)
+    allow(CreateDerivativesJob).to receive(:perform_later).with(file_set, file.id, filename)
+  end
+
+  context 'when the characterization proxy content is present' do
+    it 'runs Hydra::Works::CharacterizationService and creates a CreateDerivativesJob' do
+      expect(Hydra::Works::CharacterizationService).to receive(:run).with(file, filename)
+      expect(file).to receive(:save!)
+      expect(file_set).to receive(:update_index)
+      expect(CreateDerivativesJob).to receive(:perform_later).with(file_set, file.id, filename)
+      characterizer.characterize
+    end
+  end
+
+  context 'when the characterization proxy content is absent' do
+    before { allow(file_set).to receive(:characterization_proxy?).and_return(false) }
+    it 'raises an error' do
+      expect { characterizer.characterize }.to raise_error(StandardError, /original_file was not found/)
+    end
+  end
+end
