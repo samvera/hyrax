d4ca105c74a4a33a612d4ab7b6b705e2bcf5efdf
aaron-collier <aaron.collier@stanford.edu>
2020-01-24 10:32:38 -0800

    Add ResourceCharacterizer test

---
+++ spec/services/hyrax/resource_characterizer_spec.rb
@@ -0,0 +1,31 @@
+RSpec.describe Hyrax::ResourceCharacterizer do
+  subject(:characterizer) { described_class.new(source: resource) }
+  let(:resource) { FactoryBot.create(:file_set).valkyrie_resource }
+
+  before do
+    allow(Hydra::Works::CharacterizationService).to receive(:run)
+    allow(Hyrax.persister).to receive(:save)
+    allow(CreateDerivativesJob).to receive(:perform_later)
+  end
+
+  context 'with a complete Valkyrie FileSet' do
+    let(:resource) { FactoryBot.create(:file_set, content: File.open(fixture_path + '/world.png')).valkyrie_resource }
+
+    it 'runs Hydra::Works::CharacterizationService and creates a CreateDerivativesJob' do
+      expect(Hydra::Works::CharacterizationService).to receive(:run).with(resource.original_file, anything)
+      expect(CreateDerivativesJob).to receive(:perform_later).with(resource, resource.original_file.id, anything)
+      expect(Hyrax.persister).to receive(:save).twice
+      characterizer.characterize
+    end
+  end
+
+  context 'without a complete Valkyrie FileSet' do
+    let(:resource) { FactoryBot.create(:file_set).valkyrie_resource }
+
+    before { allow(resource).to receive(:characterization_proxy?).and_return(false) }
+
+    it 'runs Hydra::Works::CharacterizationService and creates a CreateDerivativesJob' do
+      expect { characterizer.characterize }.to raise_error(StandardError, /original_file was not found/)
+    end
+  end
+end
