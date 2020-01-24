88aab4732cec27f38c3a8a2f8d78ea7f0dcaf57d
aaron-collier <aaron.collier@stanford.edu>
2020-01-24 10:50:06 -0800

    Remove duplicate test

--- spec/jobs/characterize_job_spec.rb
+++ spec/jobs/characterize_job_spec.rb
@@ -8,36 +8,12 @@ RSpec.describe CharacterizeJob do
         allow(fs).to receive(:update_index)
       end
     end
-    # let(:io)          { JobIoWrapper.new(file_set_id: file_set.id, user: create(:user), path: filename) }
     let(:file) do
       Hydra::PCDM::File.new.tap do |f|
         f.content = 'foo'
         f.original_name = 'picture.png'
         f.save!
         allow(f).to receive(:save!)
-      end
-    end
-
-    before do
-      allow(FileSet).to receive(:find).with(file_set_id).and_return(file_set)
-      allow(Hydra::Works::CharacterizationService).to receive(:run).with(file, filename)
-      allow(CreateDerivativesJob).to receive(:perform_later).with(file_set, file.id, filename)
-    end
-
-    context 'when the characterization proxy content is present' do
-      it 'runs Hydra::Works::CharacterizationService and creates a CreateDerivativesJob' do
-        expect(Hydra::Works::CharacterizationService).to receive(:run).with(file, filename)
-        expect(file).to receive(:save!)
-        expect(file_set).to receive(:update_index)
-        expect(CreateDerivativesJob).to receive(:perform_later).with(file_set, file.id, filename)
-        described_class.perform_now(file_set)
-      end
-    end
-
-    context 'when the characterization proxy content is absent' do
-      before { allow(file_set).to receive(:characterization_proxy?).and_return(false) }
-      it 'raises an error' do
-        expect { described_class.perform_now(file_set) }.to raise_error(StandardError, /original_file was not found/)
       end
     end
 
