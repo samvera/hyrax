f11054b56b84bc6c4810bf97239d9d88da5081b1
Tom Johnson <tomjohnson@ucsb.edu>
2020-01-26 20:16:59 -0800

    Test `CollectionMemberSearchBuilder` with a Valkyrie collection
    Adds tests for collection member search using Valkyrie `Hyrax::PcdmCollection`.

--- spec/search_builders/hyrax/collection_member_search_builder_spec.rb
+++ spec/search_builders/hyrax/collection_member_search_builder_spec.rb
@@ -12,12 +12,51 @@ RSpec.describe Hyrax::CollectionMemberSearchBuilder do
     it { is_expected.to include :filter_models }
   end
 
+  context 'with a valkyrie collection' do
+    let(:collection) { build(:hyrax_collection, id: '12345') }
+
+    describe '#member_of_collection' do
+      it 'updates solr_parameters[:fq]' do
+        expect { builder.member_of_collection(solr_params) }
+          .to change { solr_params[:fq] }
+          .to include("#{builder.collection_membership_field}:#{collection.id}")
+      end
+    end
+
+    describe '#filter_models' do
+      it 'updates solr_parameters[:fq] to include both works and collections' do
+        expect { builder.filter_models(solr_params) }
+          .to change { solr_params[:fq].first }
+          .to include('f=has_model_ssim', 'GenericWork', 'Collection')
+      end
+
+      context 'when limiting to works' do
+        let(:include_models) { :works }
+
+        it 'updates solr_parameters[:fq] to include only works' do
+          expect { builder.filter_models(solr_params) }
+            .to change { solr_params[:fq].first }
+            .to include('f=has_model_ssim', 'GenericWork')
+        end
+      end
+
+      context 'when limiting to collections' do
+        let(:include_models) { :collections }
+
+        it 'updates solr_parameters[:fq] to include only collections' do
+          expect { builder.filter_models(solr_params) }
+            .to change { solr_params[:fq].first }
+            .to include('f=has_model_ssim', 'Collection')
+        end
+      end
+    end
+  end
+
   describe '#member_of_collection' do
     it 'updates solr_parameters[:fq]' do
       expect { builder.member_of_collection(solr_params) }
         .to change { solr_params[:fq] }
         .to include("#{builder.collection_membership_field}:#{collection.id}")
-      subject
     end
   end
 
