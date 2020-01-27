e8e10694d170aa5937d13c866316936ff6167ec0
Tom Johnson <tomjohnson@ucsb.edu>
2020-01-26 20:41:08 -0800

    Implement Valkyrie indexing for Collections
    Add an indexer for `Hyrax::PcdmCollection` and index `generic_type_sim`.

---
+++ app/indexers/hyrax/valkyrie_collection_indexer.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Hyrax
+  ##
+  # Indexes properties common to PCDM Collections
+  class ValkyrieCollectionIndexer < Hyrax::ValkyrieIndexer
+    Hyrax::ValkyrieIndexer.register self, as_indexer_for: Hyrax::PcdmCollection
+
+    include Hyrax::ResourceIndexer
+    include Hyrax::PermissionIndexer
+    include Hyrax::VisibilityIndexer
+
+    def to_solr
+      super.tap do |index_document|
+        index_document[:generic_type_sim] = ['Collection']
+      end
+    end
+  end
+end
