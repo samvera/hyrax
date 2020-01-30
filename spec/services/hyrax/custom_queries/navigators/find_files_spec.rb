838cb01c12a88151c3d35edcb1090b281d684a8d
Tom Johnson <tomjohnson@ucsb.edu>
2020-01-30 14:44:14 -0800

    Refactor file navigators specs to use test adapter by configuration
    Replaces hard-coded uses of the test adapter with configured ones. The adapter
    is still used across the board, but with more flexibility.

--- spec/services/hyrax/custom_queries/navigators/find_files_spec.rb
+++ spec/services/hyrax/custom_queries/navigators/find_files_spec.rb
@@ -1,12 +1,13 @@
-RSpec.describe Hyrax::CustomQueries::Navigators::FindFiles do
-  let(:query_service) { Valkyrie::MetadataAdapter.find(:test_adapter).query_service }
-  subject(:query_handler) { described_class.new(query_service: query_service) }
+RSpec.describe Hyrax::CustomQueries::Navigators::FindFiles, valkyrie_adapter: :test_adapter do
+  subject(:query_handler) do
+    described_class.new(query_service: Hyrax.query_service)
+  end
 
   describe '#find_files' do
     context 'when files exist' do
-      let!(:file_metadata1) { FactoryBot.create_using_test_adapter(:hyrax_file_metadata) }
-      let!(:file_metadata2) { FactoryBot.create_using_test_adapter(:hyrax_file_metadata) }
-      let!(:fileset) { FactoryBot.create_using_test_adapter(:hyrax_file_set, files: [file_metadata1, file_metadata2]) }
+      let!(:file_metadata1) { FactoryBot.valkyrie_create(:hyrax_file_metadata) }
+      let!(:file_metadata2) { FactoryBot.valkyrie_create(:hyrax_file_metadata) }
+      let!(:fileset) { FactoryBot.valkyrie_create(:hyrax_file_set, files: [file_metadata1, file_metadata2]) }
       it 'returns file metadata resource' do
         expect(query_handler.find_files(file_set: fileset).map(&:id).map(&:to_s)).to match_array [file_metadata1.id.to_s, file_metadata2.id.to_s]
       end
@@ -22,8 +23,8 @@ RSpec.describe Hyrax::CustomQueries::Navigators::FindFiles do
 
   describe '#find_original_file' do
     context 'when original file exists' do
-      let!(:original_file) { FactoryBot.create_using_test_adapter(:hyrax_file_metadata) }
-      let!(:fileset) { FactoryBot.create_using_test_adapter(:hyrax_file_set, files: [original_file], original_file: original_file) }
+      let!(:original_file) { FactoryBot.valkyrie_create(:hyrax_file_metadata) }
+      let!(:fileset) { FactoryBot.valkyrie_create(:hyrax_file_set, files: [original_file], original_file: original_file) }
       it 'returns file metadata resource' do
         expect(query_handler.find_original_file(file_set: fileset).id.to_s).to eq original_file.id.to_s
       end
@@ -48,8 +49,8 @@ RSpec.describe Hyrax::CustomQueries::Navigators::FindFiles do
 
   describe '#find_extracted_text' do
     context 'when extracted text exists' do
-      let!(:extracted_text) { FactoryBot.create_using_test_adapter(:hyrax_file_metadata) }
-      let!(:fileset) { FactoryBot.create_using_test_adapter(:hyrax_file_set, files: [extracted_text], extracted_text: extracted_text) }
+      let!(:extracted_text) { FactoryBot.valkyrie_create(:hyrax_file_metadata) }
+      let!(:fileset) { FactoryBot.valkyrie_create(:hyrax_file_set, files: [extracted_text], extracted_text: extracted_text) }
       it 'returns file metadata resource' do
         expect(query_handler.find_extracted_text(file_set: fileset).id.to_s).to eq extracted_text.id.to_s
       end
@@ -74,8 +75,8 @@ RSpec.describe Hyrax::CustomQueries::Navigators::FindFiles do
 
   describe '#find_thumbnail' do
     context 'when thumbnail exists' do
-      let!(:thumbnail) { FactoryBot.create_using_test_adapter(:hyrax_file_metadata) }
-      let!(:fileset) { FactoryBot.create_using_test_adapter(:hyrax_file_set, files: [thumbnail], thumbnail: thumbnail) }
+      let!(:thumbnail) { FactoryBot.valkyrie_create(:hyrax_file_metadata) }
+      let!(:fileset) { FactoryBot.valkyrie_create(:hyrax_file_set, files: [thumbnail], thumbnail: thumbnail) }
       it 'returns file metadata resource' do
         expect(query_handler.find_thumbnail(file_set: fileset).id.to_s).to eq thumbnail.id.to_s
       end
