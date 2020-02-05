7f51e0a0ae8c623cf4798827d688b49e9a2f2285
Tom Johnson <tomjohnson@ucsb.edu>
2020-02-04 16:18:42 -0800

    Implement member->collection directional Collection membership
    Works should be able to track their collection membership directly, without
    writing to the collection to determine membership.
    
    There are a few things to note here:
    
      - This tracks closely with the `Hydra::PCDM` implementation, which has
        advantages in the short term.
      - It may be possible to simply implement membership as parent->child with
        `#member_ids` across the board, and handle the inversion for `ActiveFedora`
        in `Wings`. Currently thinking this isn't a fruitful exercise though...
      - We already support `member_ids` in Collections; maybe we should drop this?
        It's in `Hydra::PCDM`, too.

--- app/models/hyrax/work.rb
+++ app/models/hyrax/work.rb
@@ -8,9 +8,10 @@ module Hyrax
   class Work < Hyrax::Resource
     include Hyrax::Schema(:core_metadata)
 
-    attribute :admin_set_id, Valkyrie::Types::ID
-    attribute :member_ids,   Valkyrie::Types::Array.of(Valkyrie::Types::ID).meta(ordered: true)
-    attribute :state,        Valkyrie::Types::URI.default(Hyrax::ResourceStatus::ACTIVE)
+    attribute :admin_set_id,             Valkyrie::Types::ID
+    attribute :member_ids,               Valkyrie::Types::Array.of(Valkyrie::Types::ID).meta(ordered: true)
+    attribute :member_of_collection_ids, Valkyrie::Types::Set.of(Valkyrie::Types::ID)
+    attribute :state,                    Valkyrie::Types::URI.default(Hyrax::ResourceStatus::ACTIVE)
 
     ##
     # @return [Boolean] true
