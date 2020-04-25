295c43bb5b6c0b754ab424478ea4c4d995fdb2cc
tom johnson <tomjohnson@ucsb.edu>
2020-04-24 17:43:10 -0700

    implement a solr health check
    this check fails if it can't reach Solr, and succeeds otherwise.

---
+++ spec/hyrax/health_checks/solr_check_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'hyrax/health_checks/solr_check'
+
+RSpec.describe Hyrax::HealthChecks::SolrCheck do
+  subject(:check) { described_class.new }
+
+  it 'connects to solr' do
+    expect { check.check }
+      .not_to change { check.failure_occurred }
+  end
+
+  context 'with a broken solr service' do
+    subject(:check) { described_class.new(service: solr_service) }
+    let(:solr_service) { double(Hyrax::SolrService) }
+
+    before { allow(solr_service).to receive(:get).and_raise('oh no!') }
+
+    it 'marks a failure' do
+      expect { check.check }
+        .to change { check.failure_occurred }
+        .to true
+    end
+  end
+end
