0dffbf86f31d8bb761de9825a20fb9e715cd686e
tom johnson <tomjohnson@ucsb.edu>
2020-05-05 14:08:43 -0700

    deprecate helper `curation_concern_page_title`
    we introduced a decorator in previous work, encourage users to use that
    decorator directly, instead of using a simple helper.
    
    in addition to depending less directly on model behavior (which was the initail
    goal of this work, re: Valkyrie), this should better insulate downstream
    applications from future helper changes. that is: applications can now change
    behavior by providing a new decorator class, instead of directly overriding
    helper behavior.

--- app/helpers/hyrax/title_helper.rb
+++ app/helpers/hyrax/title_helper.rb
@@ -9,13 +9,21 @@ module Hyrax::TitleHelper
     (elements.flatten.compact + [application_name]).join(' // ')
   end
 
+  ##
+  # @deprecated
   def curation_concern_page_title(curation_concern)
-    Hyrax::PageTitleDecorator.new(curation_concern).page_title
+    Deprecation.warn 'The curation_concern_page_title helper will be removed in Hyrax 4.0.' \
+                     "\n\tUse title_presenter(curation_concern).page_title instead."
+    title_presenter(curation_concern).page_title
   end
 
   def default_page_title
     text = controller_name.singularize.titleize
     text = "#{action_name.titleize} " + text if action_name
     construct_page_title(text)
+  end
+
+  def title_presenter(resource)
+    Hyrax::PageTitleDecorator.new(resource)
   end
 end
