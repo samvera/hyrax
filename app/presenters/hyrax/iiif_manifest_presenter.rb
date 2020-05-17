46f822a0ebb5b2160826ec98becc39a462d21dfe
tom johnson <tomjohnson@ucsb.edu>
2020-05-17 14:30:51 -0700

    use a specialized `IiifManifestPresenter` for manifest generation
    this complete rewrite of the manifest generation process reduces the footprint
    of database (Fedora/Solr) calls involved in the manifest generation process,
    fixes a bug in sequence/canvas generation for manifests for nested works, and
    generally separates the issue of manifest generation from the
    `WorkShowPresenter` (existing code is left in place, to be deprecated and
    removed as follow-up).
    
    [`iiif_manifest`](https://www.rubydoc.info/gems/iiif_manifest/1.0.1) expects a
    "presenter", with `#manifest_url`, `#manifest_metadata` `#description`,
    `#sequence_rendering`, and `#work_presenters`/`#file_set_presenters`. since we
    know the presenter provided will be used for the duration of a single request,
    we can make heavy use of instance variables to cache children and avoid making
    multiple requests for any particular data.
    
    the rewrite also does some work to help clarify the two peices of information
    the presenter needs in the request context: the `Ability` and the hostname to
    use for URIs within the manifest. we provide naive defaults for both of these,
    and implement the behavior more or less without regard for their
    context---i.e. the whole presenter infrastructure works without them. when
    calling from a Controller, we pass them in explicitly to augment the behavior
    for the context. this is important because we may (do) want to reuse these
    presenters in other contexts (a Job for cache pre-warming) where this data isn't
    available.
    
    the presenters themselves use `Draper::Decorator`'s `delegate_all`, so they're
    basically glorified wrappers for a model. they don't care whether the model is a
    SolrDocument, or powered by a primary database. a small amount of work is done
    to align Valkyrie, ActiveFedora, and SolrDocument-style models, but mostly
    things were already interoperable.
    
    a refactor in `DisplaysImage` rearranges some instance-level caching there, for
    easier reuse in the new presenter infrastructure.
    
    the short story is: between this and a previous optimization in
    `DisplaysImage` (#4344), manifest generation in a makeshift controlled
    benchmarking environment sped up 10-fold for works of modest size. it seems
    roughly linear between 10 and 750 child FileSets. i'm not confident enough in
    that environment (especially its behavior WRT caching) to call this "data" or to
    provide anything more solid than this statement, but there's good reason to
    believe this will be quite a bit faster/more scalable than the status-quo.

---
+++ app/presenters/hyrax/iiif_manifest_presenter.rb
@@ -0,0 +1,223 @@
+# frozen_string_literal: true
+
+module Hyrax
+  ##
+  # This presenter wraps objects in the interface required by `IIIFManifiest`.
+  # It will accept either a Work-like resource or a SolrDocument.
+  #
+  # @example with a work
+  #
+  #   monograph = Monograph.new
+  #   presenter = IiifManifestPresenter.new(monograph)
+  #   presenter.title # => []
+  #
+  #   monograph.title = ['Comet in Moominland']
+  #   presenter.title # => ['Comet in Moominland']
+  #
+  # @see https://www.rubydoc.info/gems/iiif_manifest
+  class IiifManifestPresenter < Draper::Decorator
+    delegate_all
+
+    ##
+    # @!attribute [w] ability
+    #   @return [Ability]
+    # @!attribute [w] hostname
+    #   @return [String]
+    attr_writer :ability, :hostname
+
+    class << self
+      ##
+      # @param [Hyrax::Resource, SolrDocument]
+      def for(model)
+        klass = model.file_set? ? DisplayImagePresenter : IiifManifestPresenter
+
+        klass.new(model)
+      end
+    end
+
+    ##
+    # @return [#can?]
+    def ability
+      @ability ||= NullAbility.new
+    end
+
+    ##
+    # @return [String]
+    def description
+      Array(super).first || ''
+    end
+
+    ##
+    # @return [Boolean]
+    def file_set?
+      model.try(:file_set?) || Array(model[:has_model_ssim]).include?('FileSet')
+    end
+
+    ##
+    # @return [Array<DisplayImagePresenter>]
+    def file_set_presenters
+      member_presenters.select(&:file_set?)
+    end
+
+    ##
+    # IIIF metadata for inclusion in the manifest
+    #  Called by the `iiif_manifest` gem to add metadata
+    #
+    # @todo should this use the simple_form i18n keys?! maybe the manifest
+    #   needs its own?
+    #
+    # @return [Array<Hash{String => String}>] array of metadata hashes
+    def manifest_metadata
+      metadata_fields.map do |field_name|
+        {
+          'label' => I18n.t("simple_form.labels.defaults.#{field_name}"),
+          'value' => Array(self[field_name]).map { |value| scrub(value.to_s) }
+        }
+      end
+    end
+
+    ##
+    # @return [String] the URL where the manifest can be found
+    def manifest_url
+      return '' if id.blank?
+
+      Rails.application.routes.url_helpers.polymorphic_url([:manifest, model], host: hostname)
+    end
+
+    ##
+    # @return [Array<#to_s>]
+    def member_ids
+      Array(model.try(:member_ids))
+    end
+
+    ##
+    # @note cache member presenters to avoid querying repeatedly; we expect this
+    #   presenter to live only as long as the request.
+    #
+    # @note skips presenters for objects the current `@ability` cannot read.
+    #   the default ability has all permissions.
+    #
+    # @return [Array<IiifManifestPresenter>]
+    def member_presenters
+      @member_presenters_cache ||= Factory.build_for(ids: member_ids, presenter_class: self.class).map do |presenter|
+        if ability.can?(:read, presenter.model)
+          presenter.hostname = hostname
+          presenter.ability  = ability
+          presenter
+        end
+      end.compact
+    end
+
+    ##
+    # @return [Array<Hash{String => String}>]
+    def sequence_rendering
+      Array(try(:rendering_ids)).map do |file_set_id|
+        rendering = file_set_presenters.find { |p| p.id == file_set_id}
+
+        if rendering
+          { '@id' => Hyrax::Engine.routes.url_helpers.download_url(rendering.id, host: hostname),
+            'format' => rendering.mime_type.present? ? rendering.mime_type : I18n.t("hyrax.manifest.unknown_mime_text"),
+            'label' => I18n.t("hyrax.manifest.download_text") + (rendering.label || '') }
+        end
+      end.flatten
+    end
+
+    ##
+    # @return [Boolean]
+    def work?
+      object.try(:work?) || !file_set?
+    end
+
+    ##
+    # @return [Array<IiifManifestPresenter>]
+    def work_presenters
+      member_presenters.select(&:work?)
+    end
+
+    ##
+    # An Ability-like object that gives `true` for all `can?` requests
+    class NullAbility
+      ##
+      # @return [Boolean] true
+      def can?(*)
+        true
+      end
+    end
+
+    class Factory < PresenterFactory
+      ##
+      # @return [Array]
+      def build
+        ids.map do |id|
+          solr_doc = load_docs.find { |doc| doc.id == id }
+          presenter_class.for(solr_doc) if solr_doc
+        end.compact
+      end
+
+      private
+
+        ##
+        # cache the docs in this method, rather than #build;
+        # this can probably be pushed up to the parent class
+        def load_docs
+          @cached_docs ||= super
+        end
+    end
+
+    ##
+    # a Presenter for producing `IIIFManifest::DisplayImage` objects
+    #
+    class DisplayImagePresenter < Draper::Decorator
+      delegate_all
+
+      include Hyrax::DisplaysImage
+
+      ##
+      # @!attribute [w] ability
+      #   @return [Ability]
+      # @!attribute [w] hostname
+      #   @return [String]
+      attr_writer :ability, :hostname
+
+      ##
+      # Creates a display image only where #model is an image.
+      #
+      # @return [IIIFManifest::DisplayImage] the display image required by the manifest builder.
+      def display_image
+        return nil unless model.image?
+        return nil unless latest_file_id
+
+        IIIFManifest::DisplayImage
+          .new(display_image_url(hostname),
+               format: image_format(alpha_channels),
+               width: width,
+               height: height,
+               iiif_endpoint: iiif_endpoint(latest_file_id, base_url: hostname))
+      end
+
+      def hostname
+        @hostname || 'localhost'
+      end
+
+      ##
+      # @return [Boolean] false
+      def work?
+        false
+      end
+    end
+
+    private
+
+      def hostname
+        @hostname || 'localhost'
+      end
+
+      def metadata_fields
+        Hyrax.config.iiif_metadata_fields
+      end
+
+      def scrub(value)
+        Loofah.fragment(value).scrub!(:whitewash).to_s
+      end
+  end
+end
