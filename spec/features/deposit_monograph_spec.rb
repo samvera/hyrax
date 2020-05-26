f390cca3502a7367409eda786dbb6eea69f9bca3
tom johnson <tomjohnson@ucsb.edu>
2020-05-26 16:15:02 -0700

    add a feature spec for valkyrie native models

---
+++ spec/features/deposit_monograph_spec.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+# end to end tests for creating a Valkyrie native work with an generic Valkyrie adapter
+RSpec.describe 'Creating a new Monograph work', :js, :workflow, valkyrie_adapter: :test_adapter do
+  let(:user) { create(:user) }
+
+  before { sign_in user }
+
+  it 'creates the work' do
+    click_link 'Works'
+    click_link 'Add new work'
+    choose 'payload_concern', option: 'Monograph'
+    click_button 'Create work'
+
+    click_link 'Files'
+    expect(page).to have_content 'Add files'
+    within('div#add-files') do
+      attach_file('files[]', "#{Hyrax::Engine.root}/spec/fixtures/image.jp2", visible: false)
+    end
+
+    click_link 'Descriptions'
+    fill_in('Title', with: 'Comet in Moominland')
+    fill_in('Creator', with: 'Jansson, Tove')
+    select('In Copyright', from: 'Rights statement')
+
+    choose('monograph_visibility_open')
+    check('agreement')
+    # click_on('Save')
+  end
+end
