05c5118a7b9777d6da59ef7d0c20e4a1d1a22c1f
tom johnson <tomjohnson@ucsb.edu>
2020-05-27 08:26:21 -0700

    wire the new manifests into the benchmark
    initial results show this reducing the manifest generation time by about half,
    for a work with no members; and roughly an order of magnitude (~7-8ms, down from
    50-60ms) for a work with only two constituent members.

--- spec/benchmarks/iiif_manifest_spec.rb
+++ spec/benchmarks/iiif_manifest_spec.rb
@@ -7,24 +7,20 @@ require 'rspec-benchmark'
 RSpec.describe 'IIIF Manifest generation performance', :benchmark do
   include RSpec::Benchmark::Matchers
 
-  it 'generates manifests quickly (<15ms) for simple works' do
+  it 'generates manifests quickly (<8ms) for simple works' do
     work      = FactoryBot.create(:work)
-    user      = FactoryBot.create(:admin)
-    request   = double(host: 'example.org', base_url: 'http://example.org')
-    presenter = Hyrax::WorkShowPresenter.new(work, Ability.new(user), request)
+    presenter = Hyrax::IiifManifestPresenter.new(work)
 
     expect { Hyrax::ManifestBuilderService.new.manifest_for(presenter: presenter) }
-      .to perform_under(15).ms.warmup(2).times.sample(10).times
+      .to perform_under(8).ms.warmup(2).times.sample(10).times
   end
 
-  it 'generates manifests quickly (<25ms) for works with several children' do
-    work      = FactoryBot.create(:work_with_file_and_work)
-    user      = FactoryBot.create(:admin)
-    request   = double(host: 'example.org', base_url: 'http://example.org')
-    presenter = Hyrax::WorkShowPresenter.new(work, Ability.new(user), request)
+  it 'generates manifests quickly (<10ms) for works with several children' do
+    work      = FactoryBot.create(:work_with_image_files)
+    presenter = Hyrax::IiifManifestPresenter.new(work)
 
     expect { Hyrax::ManifestBuilderService.new.manifest_for(presenter: presenter) }
-      .to perform_under(25).ms.warmup(2).times.sample(10).times
+      .to perform_under(10).ms.warmup(2).times.sample(10).times
   end
 
   it 'has constant performance for a large number of child works'
