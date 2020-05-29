8ba7cf8edba968dcd6cd3bbfd08911dd34809880
Tom Johnson <tomjohnson@ucsb.edu>
2020-05-29 16:49:04 -0700

    Update app/forms/hyrax/forms/resource_form.rb

--- app/forms/hyrax/forms/resource_form.rb
+++ app/forms/hyrax/forms/resource_form.rb
@@ -87,7 +87,7 @@ module Hyrax
       # pcdm relationships
       property :admin_set_id
       property :member_ids, default: [], type: Valkyrie::Types::Array
-      property :member_of_collection_ids, default: [],  type: Valkyrie::Types::Array
+      property :member_of_collection_ids, default: [], type: Valkyrie::Types::Array
 
       # provide a lock token for optimistic locking; we name this `version` for
       # backwards compatibility
