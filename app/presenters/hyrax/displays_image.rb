cb0812a38e2ca868d166d226294b881891462f11
tom johnson <tomjohnson@ucsb.edu>
2020-06-02 09:41:43 -0700

    Revert "Pull image info from solr when possible"
    This reverts commit 9a4abb720f507847a47c1226abddc1458370d8d9.

--- app/presenters/hyrax/displays_image.rb
+++ app/presenters/hyrax/displays_image.rb
@@ -11,10 +11,8 @@ module Hyrax
     # @return [IIIFManifest::DisplayImage] the display image required by the manifest builder.
     def display_image
       return nil unless ::FileSet.exists?(id) && solr_document.image? && current_ability.can?(:read, id)
-
-      latest_file_id = lookup_original_file_id
-
-      return nil unless latest_file_id
+      # @todo this is slow, find a better way (perhaps index iiif url):
+      original_file = ::FileSet.find(id).original_file
 
       url = Hyrax.config.iiif_image_url_builder.call(
         original_file.id,
@@ -23,8 +21,8 @@ module Hyrax
       )
       # @see https://github.com/samvera-labs/iiif_manifest
       IIIFManifest::DisplayImage.new(url,
-                                     width: solr_document[:width_is],
-                                     height: solr_document[:height_is],
+                                     width: 640,
+                                     height: 480,
                                      iiif_endpoint: iiif_endpoint(original_file.id))
     end
 
@@ -37,24 +35,5 @@ module Hyrax
           profile: Hyrax.config.iiif_image_compliance_level_uri
         )
       end
-
-      def image_format(channels)
-        channels.find { |c| c.include?('rgba') }.nil? ? 'jpg' : 'png'
-      end
-
-      def unindexed_current_file_version
-        Rails.logger.warn "Indexed current_file_version for #{id} not found, falling back to Fedora."
-        ActiveFedora::File.uri_to_id(::FileSet.find(id).current_content_version_uri)
-      end
-
-      def lookup_original_file_id
-        result = original_file_id
-        if result.blank?
-          Rails.logger.warn "original_file_id for #{id} not found, falling back to Fedora."
-          result = ActiveFedora::File.uri_to_id(::FileSet.find(id).current_content_version_uri)
-        end
-        result
-      end
-    end
   end
 end
