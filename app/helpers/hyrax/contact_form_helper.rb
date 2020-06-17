52b6b099bd37d86e1b523ddf1af78e8de936e178
tamsin johnson <tomjohnson@ucsb.edu>
2020-06-17 14:26:13 -0700

    extract hard view dependency on `Hyrax::ContactForm` to a helper
    this insulates the view contact form view from the model, with the idea of
    deprecating `ContactForm`'s class method entirely, in favor of some
    configuration elsewhere.

---
+++ app/helpers/hyrax/contact_form_helper.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module Hyrax
+  ##
+  # Helpers for displaying Hyrax's default contact form.
+  module ContactFormHelper
+    ##
+    # @api public
+    #
+    # Provides the "issue type" options for the contact form dropdown. The
+    # response should be compatible with ActionView's `options_for_select`
+    # helper.
+    #
+    # @note this helper should always provide i18nized values.
+    #
+    # @return [Array<String>]
+    #
+    # @see https://apidock.com/rails/ActionView/Helpers/FormOptionsHelper/options_for_select
+    def contact_form_issue_type_options
+      types = Hyrax::ContactForm.issue_types_for_locale.dup
+      types.unshift([t('hyrax.contact_form.select_type'), nil])
+    end
+  end
+end
