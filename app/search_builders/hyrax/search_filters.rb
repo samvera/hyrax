e890f67342d4ef73590cf9b32f786065568f47db
Blanco <blancoj@umich.edu>
2020-07-15 14:36:33 -0400

    allow approvers to approve private works. Fixes issue #4411

--- app/search_builders/hyrax/search_filters.rb
+++ app/search_builders/hyrax/search_filters.rb
@@ -13,9 +13,22 @@ module Hyrax
       self
     end
 
+    # This method will return true if the user has an appoval role, and false otherwise.
+    # This is used to for approvers to gain access to a private work.  If this check were not done, the approver
+    # would not be able to access private works.
+    def user_approver?
+      approving_role = Sipity::Role.find_by(name: Hyrax::RoleRegistry::APPROVING)
+      return false unless approving_role
+      Hyrax::Workflow::PermissionQuery.scope_processing_agents_for(user: current_ability.current_user).any? do |agent|
+        agent.workflow_responsibilities.joins(:workflow_role)
+             .where('sipity_workflow_roles.role_id' => approving_role.id).any?
+      end
+    end
+
     # Override Hydra::AccessControlsEnforcement (or Hydra::PolicyAwareAccessControlsEnforcement)
     # Allows admin users to see everything (don't apply any gated_discovery_filters for those users)
     def gated_discovery_filters(permission_types = discovery_permissions, ability = current_ability)
+      return [] if user_approver?
       return [] if ability.admin?
       super
     end
