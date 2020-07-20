dbea368071b25ade19e6674da6f13199f281a824
E. Lynette Rayle <elr37@cornell.edu>
2020-07-20 15:29:14 -0400

    allow for extensions of valkyrie indexer's to_solr that depends on resource custom metadata
    Fixes #4464
    
    Allow shared spec testing of indexers with customizations to #to_solr that depend on properties defined in the resource class that the indexer indexes.
    
    Update shared indexers spec to have a valid resource passed in to the shared spec.  The shared spec sets values it depends on in a before block.

--- lib/hyrax/specs/shared_specs/indexers.rb
+++ lib/hyrax/specs/shared_specs/indexers.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 RSpec.shared_examples 'a Hyrax::Resource indexer' do
-  subject(:indexer) { indexer_class.new(resource: resource) }
-  let(:ids)         { ['id1', 'id2'] }
-  let(:resource)    { Hyrax::Resource.new(alternate_ids: ids) }
+  subject(:indexer)  { indexer_class.new(resource: the_resource) }
+  let(:the_resource) { defined?(resource) ? resource : Hyrax::Resource.new }
+  let(:ids)          { ['id1', 'id2'] }
 
   describe '#to_solr' do
+    before { the_resource.alternate_ids = ids }
     it 'indexes alternate_ids' do
       expect(indexer.to_solr)
         .to include(alternate_ids_sm: a_collection_containing_exactly(*ids))
@@ -14,18 +15,26 @@ RSpec.shared_examples 'a Hyrax::Resource indexer' do
 end
 
 RSpec.shared_examples 'a permission indexer' do
-  subject(:indexer) { indexer_class.new(resource: resource) }
+  subject(:indexer) { indexer_class.new(resource: the_resource) }
+  let(:the_resource) do
+    if defined?(resource)
+      Hyrax::VisibilityWriter.new(resource: resource)
+          .assign_access_for(visibility: Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC)
+      resource.permission_manager.edit_groups = edit_groups
+      resource.permission_manager.edit_users  = edit_users
+      resource.permission_manager.read_users  = read_users
+      resource.permission_manager.acl.save
+      resource
+    else
+      FactoryBot.valkyrie_create(:hyrax_work, :public,
+                                 read_users: read_users,
+                                 edit_groups: edit_groups,
+                                 edit_users: edit_users)
+    end
+  end
   let(:edit_groups) { [:managers] }
   let(:edit_users)  { [FactoryBot.create(:user)] }
   let(:read_users)  { [FactoryBot.create(:user)] }
-
-  let(:resource) do
-    FactoryBot.valkyrie_create(:hyrax_work, :public,
-                               read_users: read_users,
-                               edit_groups: edit_groups,
-                               edit_users: edit_users)
-  end
-
 
   describe '#to_solr' do
     it 'indexes read permissions' do
@@ -43,8 +52,8 @@ RSpec.shared_examples 'a permission indexer' do
 end
 
 RSpec.shared_examples 'a visibility indexer' do
-  subject(:indexer) { indexer_class.new(resource: resource) }
-  let(:resource)    { FactoryBot.build(:hyrax_work) }
+  subject(:indexer)  { indexer_class.new(resource: the_resource) }
+  let(:the_resource) { defined?(resource) ? resource : FactoryBot.build(:hyrax_work) }
 
   describe '#to_solr' do
     it 'indexes visibility' do
@@ -52,7 +61,10 @@ RSpec.shared_examples 'a visibility indexer' do
     end
 
     context 'when resource is public' do
-      let(:resource) { FactoryBot.valkyrie_create(:hyrax_work, :public) }
+      before do
+        Hyrax::VisibilityWriter.new(resource: the_resource)
+          .assign_access_for(visibility: Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC)
+      end
 
       it 'indexes as open' do
         expect(indexer.to_solr).to include(visibility_ssi: 'open')
@@ -62,8 +74,8 @@ RSpec.shared_examples 'a visibility indexer' do
 end
 
 RSpec.shared_examples 'a Basic metadata indexer' do
-  subject(:indexer) { indexer_class.new(resource: resource) }
-  let(:resource)    { resource_class.new(**attributes) }
+  subject(:indexer) { indexer_class.new(resource: the_resource) }
+  let(:the_resource) { defined?(resource) ? resource : resource_class.new }
 
   let(:attributes) do
     {
@@ -79,9 +91,10 @@ RSpec.shared_examples 'a Basic metadata indexer' do
   end
 
   describe '#to_solr' do
+    before { attributes.each { |k, v| the_resource.set_value(k, v) } }
     it 'indexes basic metadata' do
       expect(indexer.to_solr)
-        .to include(keyword_sim: a_collection_containing_exactly(*attributes[:keyword]),
+        .to include(keyword_sim:   a_collection_containing_exactly(*attributes[:keyword]),
                     subject_tesim: a_collection_containing_exactly(*attributes[:subject]),
                     subject_sim:   a_collection_containing_exactly(*attributes[:subject]))
     end
@@ -89,8 +102,8 @@ RSpec.shared_examples 'a Basic metadata indexer' do
 end
 
 RSpec.shared_examples 'a Collection indexer' do
-  subject(:indexer) { indexer_class.new(resource: resource) }
-  let(:resource)    { Hyrax::PcdmCollection.new }
+  subject(:indexer) { indexer_class.new(resource: the_resource) }
+  let(:the_resource) { defined?(resource) ? resource : FactoryBot.valkyrie_create(:hyrax_collection) }
 
   it_behaves_like 'a permission indexer'
   it_behaves_like 'a visibility indexer'
@@ -109,11 +122,12 @@ RSpec.shared_examples 'a Collection indexer' do
 end
 
 RSpec.shared_examples 'a Core metadata indexer' do
-  subject(:indexer) { indexer_class.new(resource: resource) }
+  subject(:indexer) { indexer_class.new(resource: the_resource) }
   let(:titles)      { ['Comet in Moominland', 'Finn Family Moomintroll'] }
-  let(:resource)    { Hyrax::Work.new(title: titles) }
+  let(:the_resource) { defined?(resource) ? resource : Hyrax::Work.new }
 
   describe '#to_solr' do
+    before { the_resource.title = titles }
     it 'indexes title as text' do
       expect(indexer.to_solr)
         .to include(title_tesim: a_collection_containing_exactly(*titles))
--- spec/indexers/hyrax/valkyrie_collection_indexer_spec.rb
+++ spec/indexers/hyrax/valkyrie_collection_indexer_spec.rb
@@ -3,7 +3,7 @@
 require 'hyrax/specs/shared_specs'
 
 RSpec.describe Hyrax::ValkyrieCollectionIndexer do
-  subject(:indexer) { described_class.new(resource: resource) }
+  let(:resource) { FactoryBot.valkyrie_create(:hyrax_collection) }
   let(:indexer_class) { described_class }
 
   it_behaves_like 'a Hyrax::Resource indexer'
