140969702d5f7f8cd010c891c9952bbd266f73b3
Brian Keese <bkeese@indiana.edu>
2020-09-04 11:30:18 -0400

    Apply skip_to_content javascript across app not just client pages

--- app/assets/javascripts/hyrax.js
+++ app/assets/javascripts/hyrax.js
@@ -105,6 +105,7 @@
 //= require hyrax/turbolinks_events
 //= require hyrax/i18n_helper
 //= require hyrax/collapse
+//= require hyrax/skip_to_content
 
 // this needs to be after batch_select so that the form ids get setup correctly
 //= require hyrax/batch_edit
