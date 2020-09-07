7d10ea4f5117b6f0068a44dd02986e3849cbedc5
BertZZ <bertie.wooles@gmail.com>
2020-09-07 17:06:15 +0100

    Adds jobs for Expriy of Embargos

---
+++ app/jobs/embargo_expiry_job.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+class EmbargoExpiryJob < Hyrax::ApplicationJob
+  def perform
+    records_with_expired_embargos.each do |record|
+      ExpireEmbargoJob.perform_later(record)
+    end
+  end
+
+  def records_with_expired_embargos
+    ids = Hyrax::EmbargoService.assets_with_expired_embargoes.map(&:id)
+    ids.map { |id| ActiveFedora::Base.find(id) }
+  end
+end
---
+++ spec/jobs/embargo_expiry_job_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+RSpec.describe EmbargoExpiryJob do
+  subject { described_class }
+
+  let(:past_date) { 2.days.ago }
+
+  let(:work) { create(:embargoed_work) }
+
+  let!(:work_with_expired_embargo) do
+    build(:work, embargo_release_date: past_date.to_s, visibility_during_embargo: 'restricted', visibility_after_embargo: 'open').tap do |work|
+      work.save(validate: false)
+    end
+  end
+
+  let!(:file_set_with_expired_embargo) do
+    build(:file_set, embargo_release_date: past_date.to_s, visibility_during_embargo: 'restricted', visibility_after_embargo: 'open').tap do |file_set|
+      file_set.save(validate: false)
+    end
+  end
+
+  describe '#records_with_expired_embargos' do
+    it 'returns all records with expired embargos' do
+      records = described_class.new.records_with_expired_embargos
+      expect(records).to include work_with_expired_embargo
+      expect(records).to include file_set_with_expired_embargo
+    end
+  end
+
+  describe '#perform' do
+    it "Enqueues an Expire Embargo job " do
+      expect { described_class.perform_now }.to change(ActiveJob::Base.queue_adapter.enqueued_jobs, :size).by(described_class.new.records_with_expired_embargos.count)
+    end
+  end
+end
