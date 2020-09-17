05e0d2e174a47508382ef0f20e3f47bedcc87544
Bertie Wooles <bertie.wooles@gmail.com>
2020-09-17 15:03:10 +0100

    Ubiquity auto expiry jobs (#4502)
    * Adds jobs for Expriy of Embargos
    
    * Adds jobs for Expiry of Leases and cleans up specs
    
    Fixes Rubocop issues
    
    Fixes lease objects
    
    Fixes lease objects
    
    Fixes Typo
    
    * Refactors expiry into a single job
    
    Fixes Rubocop errors
    
    Fixes more Rubocop errors
    
    * move indexing adapter registration to high-precedence
    
    applications will probably want to do indexer setup in
    initializers. registration of Hyrax's shipped adapters needs to be in place
    before they do this. previously, this was the case only when the application
    does setup in a letter after `'i'` (e.g. not `hyrax.rb`).
    
    moving this up in precedence will make things more predictable for apps.
    
    * Synchronizing Spec::SpyListener and Publisher
    
    I added documentation to explain how to add a named listener, instead of
    an anonymous block listener.  I think this is useful, as our SpyListener
    used a named listener, but that mechanism was not documented in the
    Hyrax::Publisher.
    
    In addition to the documentation, I've ensured that the SpyListener's
    methods are synchronized with the Hyrax::Publisher registered events.
    This synchronization also provides a bit of a test of the implementation
    details of Dry::Events; Based on Dry's long-standing volatilitaty of
    pre-1.0 gems, I think its a good plan to test a bit of the edges of
    Dry::Events.
    
    * Update app/jobs/embargo_expiry_job.rb
    
    Co-authored-by: Tom Johnson <tomjohnson@ucsb.edu>
    
    * Update app/jobs/lease_expiry_job.rb
    
    Co-authored-by: Tom Johnson <tomjohnson@ucsb.edu>
    
    * Bumping regen to fix rubocop-ast caching issue
    
    Prior to this commit, the CI environment of Hyrax went wonky.  This, we
    hope, is addressed in our Bixby implementation (see
    https://github.com/samvera-labs/bixby/pull/47).  However, we need to
    invalidate the cache.
    
    * Adds jobs for Expriy of Embargos
    
    * Adds jobs for Expiry of Leases and cleans up specs
    
    Fixes Rubocop issues
    
    Fixes lease objects
    
    Fixes lease objects
    
    Fixes Typo
    
    * Refactors expiry into a single job
    
    Fixes Rubocop errors
    
    Fixes more Rubocop errors
    
    Co-authored-by: tamsin johnson <tomjohnson@ucsb.edu>
    Co-authored-by: Jeremy Friesen <jeremy.n.friesen@gmail.com>
    Co-authored-by: E. Lynette Rayle <elrayle@users.noreply.github.com>

---
+++ spec/jobs/embargo_expiry_job_spec.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+RSpec.describe EmbargoExpiryJob, :clean_repo do
+  subject { described_class }
+
+  let(:past_date) { 2.days.ago }
+
+  let(:embargoed_work) { create(:embargoed_work) }
+
+  let!(:work_with_expired_embargo) do
+    build(:work, embargo_release_date: past_date.to_s, visibility_during_embargo: 'restricted', visibility_after_embargo: 'open').tap do |work|
+      work.save(validate: false)
+    end
+  end
+
+  let!(:file_set_with_expired_embargo) do
+    build(:file_set, embargo_release_date: past_date.to_s, visibility_during_embargo: 'restricted', visibility_after_embargo: 'open').tap do |file_set|
+      file_set.save(validate: false)
+    end
+  end
+
+  describe '#records_with_expired_embargos' do
+    it 'returns all records with expired embargos' do
+      records = described_class.new.records_with_expired_embargos
+      expect(records).to include work_with_expired_embargo.id, file_set_with_expired_embargo.id
+      expect(records).not_to include embargoed_work.id
+    end
+  end
+
+  describe '#perform' do
+    it 'expires embargos on works with expired embargos' do
+      described_class.new.perform
+      work_with_expired_embargo.reload
+      expect(work_with_expired_embargo.visibility).to eq('open')
+    end
+
+    it 'expires embargos on file sets with expired embargos' do
+      described_class.new.perform
+      file_set_with_expired_embargo.reload
+      expect(file_set_with_expired_embargo.visibility).to eq('open')
+    end
+
+    it "Doesn't expire embargos that are still in effect" do
+      described_class.new.perform
+      embargoed_work.reload
+      expect(embargoed_work.visibility).to eq('restricted')
+    end
+  end
+end
