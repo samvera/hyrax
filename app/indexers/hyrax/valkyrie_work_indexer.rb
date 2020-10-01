801eaa2dbdb6e581a07cf08e39eb601373101465
E. Lynette Rayle <elr37@cornell.edu>
2020-10-01 15:12:35 -0400

    add suppressed_bsi to valkyrie resource work

--- app/indexers/hyrax/valkyrie_work_indexer.rb
+++ app/indexers/hyrax/valkyrie_work_indexer.rb
@@ -12,20 +12,17 @@ module Hyrax
     def to_solr # rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/MethodLength
       super.tap do |solr_doc|
         solr_doc['generic_type_sim'] = ['Work']
+        solr_doc['suppressed_bsi'] = suppressed?(resource)
         solr_doc['admin_set_id_ssim'] = [resource.admin_set_id]
         solr_doc['member_of_collection_ids_ssim'] = resource.member_of_collection_ids
         solr_doc['member_ids_ssim'] = resource.member_ids
-
-        # attributes from Hyrax::Work not defined by Hyrax::Schema includes
-        # solr_doc['on_behalf_of'] = ''
-        # solr_doc['proxy_depositor'] = ''
-        # solr_doc['state'] = ''
-
-        # This enables us to return a Work when we have a FileSet that matches
-        # the search query.  While at the same time allowing us not to return Collections
-        # when a work in the collection matches the query.
-        # solr_doc['file_set_ids_ssim'] = solr_doc['member_ids_ssim'] # TODO: Doesn't this return child works too?
       end
+    end
+
+    private
+
+    def suppressed?(resource)
+      Hyrax::ResourceStatus.new(resource: resource).inactive?
     end
   end
 end
