54a9fde32bb72a7760786890b8d0b58f3a2a3118
tamsin johnson <tomjohnson@ucsb.edu>
2020-10-06 17:23:20 -0700

    dassie: add GenericWork
    `docker-compose exec app rails g hyrax:work GenericWork`

---
+++ .dassie/app/actors/hyrax/actors/generic_work_actor.rb
@@ -0,0 +1,8 @@
+# Generated via
+#  `rails generate hyrax:work GenericWork`
+module Hyrax
+  module Actors
+    class GenericWorkActor < Hyrax::Actors::BaseActor
+    end
+  end
+end
---
+++ .dassie/app/presenters/hyrax/generic_work_presenter.rb
@@ -0,0 +1,6 @@
+# Generated via
+#  `rails generate hyrax:work GenericWork`
+module Hyrax
+  class GenericWorkPresenter < Hyrax::WorkShowPresenter
+  end
+end
