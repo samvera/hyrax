da4f2fd985d577a95cfa8f37981912e2f03d625e
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2020-10-07 11:12:41 -0400

    Restoring HTML form field name.
    In 241cbed1df4ad416b8d44763ae321e642443ed88 we introduced changes to
    address accessibility issues reported in #3966.  However, the introduced
    change broke the HTML form field names and the assumed parameters for
    the [batch processing][1].
    
    This commit reverts that change, but it does not address the
    accessibility concern raised in #3966.  That is for another commit, as I
    don't know if that other commit is the correct path forward.
    
    Closes #4516
    Closes #4515
    
    [1]:https://github.com/samvera/hyrax/blob/3c7258c95c1fe6f0fe1537078f3f5f458f211989/app/controllers/concerns/hyrax/collections/accepts_batches.rb#L20-L28

---
+++ spec/views/hyrax/batch_select/_add_button.html.erb_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+RSpec.describe 'hyrax/batch_select/_add_button.html.erb', type: :view do
+  let(:document) { double(id: 123) }
+  before do
+    render 'hyrax/batch_select/add_button.html.erb', document: document
+  end
+
+  it 'renders a checkbox named "batch_document_ids[]"' do
+    # See ./app/controllers/concerns/hyrax/collections/accepts_batches.rb
+    # for how we expect the input fields to have a name "batch_document_ids[]"
+    expect(rendered).to have_selector(%([data-behavior="batch-add-button"] .batch_document_selector#batch_document_#{document.id}[name="batch_document_ids[]"][value=#{document.id}][type=checkbox]))
+  end
+end
