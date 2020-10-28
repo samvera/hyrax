ff3926a8002757a0c81d559607c1477bc88da910
tamsin johnson <tomjohnson@ucsb.edu>
2020-10-28 16:35:16 -0700

    suppress null virus scanner warning in the test environment
    if the null virus scanner is used in test, don't overwhelm the user with
    warnings. this is a good intended usage.

--- spec/models/hyrax/virus_scanner_spec.rb
+++ spec/models/hyrax/virus_scanner_spec.rb
@@ -43,8 +43,8 @@ RSpec.describe Hyrax::VirusScanner do
   context 'when ClamAV is not defined' do
     before { Object.send(:remove_const, :ClamAV) if defined?(ClamAV) }
 
-    it 'returns false with a warning' do
-      expect(Hyrax.logger).to receive(:warn).with(kind_of(String))
+    it 'returns false' do
+      # we used to test the warning here, but we suppress it for the test env now
       is_expected.not_to be_infected
     end
   end
