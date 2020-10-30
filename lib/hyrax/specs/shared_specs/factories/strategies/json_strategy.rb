7b539a24c23d881b10fd0be41cbdfb7df12ff33f
tamsin johnson <tomjohnson@ucsb.edu>
2020-10-30 13:59:37 -0700

    move FactoryBot `JsonStrategy` to shared specs

---
+++ lib/hyrax/specs/shared_specs/factories/strategies/json_strategy.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class JsonStrategy
+  def initialize
+    @strategy = FactoryBot.strategy_by_name(:create).new
+  end
+
+  delegate :association, to: :@strategy
+
+  def result(evaluation)
+    @strategy.result(evaluation).to_json
+  end
+end
