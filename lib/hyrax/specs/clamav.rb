6bcbabcf16981941a6e9718978315ff2808a14ca
tamsin johnson <tomjohnson@ucsb.edu>
2020-10-30 13:58:11 -0700

    move rspec clamav overrides into shared space
    move this reusable code out of spec_helper for reuse.

---
+++ lib/hyrax/specs/clamav.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+if defined?(ClamAV)
+  ClamAV.instance.loaddb
+else
+  class ClamAV
+    include Singleton
+    def scanfile(_f)
+      0
+    end
+
+    def loaddb
+      nil
+    end
+  end
+end
