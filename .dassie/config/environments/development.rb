73109b0928e0344ae6b12a5a9c99689f4377cc24
Alexandra Dunn <dunn.alex@gmail.com>
2020-11-17 09:21:07 -0800

    dassie: log to stdout in development mode

--- .dassie/config/environments/development.rb
+++ .dassie/config/environments/development.rb
@@ -37,12 +37,18 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # Logging
+  #
+  config.log_level = :debug
+  config.log_formatter = ::Logger::Formatter.new
+  # log to stdout
+  logger               = ActiveSupport::Logger.new(STDOUT)
+  logger.formatter     = config.log_formatter
+  config.logger        = ActiveSupport::TaggedLogging.new(logger)
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
-
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
-
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
