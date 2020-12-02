367354e93b14f5af8d58352930ca358951e60912
tamsin johnson <tomjohnson@ucsb.edu>
2020-12-02 10:30:15 -0800

    separate af and valkyrie tests for AttachMembersActor
    these tests shouldn't overlap.

--- spec/actors/hyrax/actors/attach_members_actor_spec.rb
+++ spec/actors/hyrax/actors/attach_members_actor_spec.rb
@@ -17,77 +17,79 @@ RSpec.describe Hyrax::Actors::AttachMembersActor do
   end
 
   describe "#update" do
-    before { work.ordered_members << existing_child_work }
+    context 'when using an active_fedora work' do
+      before { work.ordered_members << existing_child_work }
 
-    context "without useful attributes" do
-      let(:attributes) { {} }
+      context "without useful attributes" do
+        let(:attributes) { {} }
 
-      it { expect(middleware.update(env)).to be true }
-    end
-
-    context "when the id already exists in the members" do
-      let(:attributes) { HashWithIndifferentAccess.new(work_members_attributes: { '0' => { id: existing_child_work.id } }) }
-
-      it "does nothing" do
-        expect { middleware.update(env) }.not_to change { env.curation_concern.ordered_members.to_a }
+        it { expect(middleware.update(env)).to be true }
       end
 
-      context "and the _destroy flag is set" do
-        let(:attributes) { HashWithIndifferentAccess.new(work_members_attributes: { '0' => { id: existing_child_work.id, _destroy: 'true' } }) }
+      context "when the id already exists in the members" do
+        let(:attributes) { HashWithIndifferentAccess.new(work_members_attributes: { '0' => { id: existing_child_work.id } }) }
 
-        it "removes from the member and the ordered members" do
-          expect { middleware.update(env) }.to change { env.curation_concern.ordered_members.to_a }
-          expect(env.curation_concern.ordered_member_ids).not_to include(existing_child_work.id)
-          expect(env.curation_concern.member_ids).not_to include(existing_child_work.id)
-        end
-      end
-    end
-
-    context "when working through Rails nested attribute scenarios" do
-      before do
-        allow(ability).to receive(:can?).with(:edit, String).and_return(true)
-        work.ordered_members << work_to_remove
-      end
-
-      let(:work_to_remove) { create(:work, title: ['Already Member and Remove']) }
-      let(:work_to_skip) { create(:work, title: ['Not a Member']) }
-      let(:work_to_add) { create(:work, title: ['Not a Member but want to add']) }
-
-      let(:attributes) do
-        HashWithIndifferentAccess.new(
-          work_members_attributes: {
-            '0' => { id: work_to_remove.id, _destroy: 'true' }, # colleciton is a member and we're removing it
-            '1' => { id: work_to_skip.id, _destroy: 'true' }, # collection is a NOT member and is marked for deletion; This is a UI introduced option
-            '2' => { id: existing_child_work.id },
-            '3' => { id: work_to_add.id }
-          }
-        )
-      end
-
-      it "handles destroy/non-destroy and keep/add behaviors" do
-        expect { middleware.update(env) }.to change { env.curation_concern.ordered_members.to_a }
-        expect(env.curation_concern.ordered_member_ids).to match_array [existing_child_work.id, work_to_add.id]
-        expect(env.curation_concern.member_ids).to match_array [existing_child_work.id, work_to_add.id]
-      end
-    end
-
-    context "when the id does not exist in the members" do
-      let(:another_work) { create(:work) }
-      let(:attributes) { HashWithIndifferentAccess.new(work_members_attributes: { '0' => { id: another_work.id } }) }
-
-      context "and I can edit that object" do
-        before do
-          allow(ability).to receive(:can?).with(:edit, String).and_return(true)
-        end
-        it "is added to the ordered members" do
-          expect { middleware.update(env) }.to change { env.curation_concern.ordered_members.to_a }
-          expect(env.curation_concern.ordered_member_ids).to include(existing_child_work.id, another_work.id)
-        end
-      end
-
-      context "and I can not edit that object" do
         it "does nothing" do
           expect { middleware.update(env) }.not_to change { env.curation_concern.ordered_members.to_a }
+        end
+
+        context "and the _destroy flag is set" do
+          let(:attributes) { HashWithIndifferentAccess.new(work_members_attributes: { '0' => { id: existing_child_work.id, _destroy: 'true' } }) }
+
+          it "removes from the member and the ordered members" do
+            expect { middleware.update(env) }.to change { env.curation_concern.ordered_members.to_a }
+            expect(env.curation_concern.ordered_member_ids).not_to include(existing_child_work.id)
+            expect(env.curation_concern.member_ids).not_to include(existing_child_work.id)
+          end
+        end
+      end
+
+      context "when working through Rails nested attribute scenarios" do
+        before do
+          allow(ability).to receive(:can?).with(:edit, String).and_return(true)
+          work.ordered_members << work_to_remove
+        end
+
+        let(:work_to_remove) { create(:work, title: ['Already Member and Remove']) }
+        let(:work_to_skip) { create(:work, title: ['Not a Member']) }
+        let(:work_to_add) { create(:work, title: ['Not a Member but want to add']) }
+
+        let(:attributes) do
+          HashWithIndifferentAccess.new(
+            work_members_attributes: {
+              '0' => { id: work_to_remove.id, _destroy: 'true' }, # colleciton is a member and we're removing it
+              '1' => { id: work_to_skip.id, _destroy: 'true' }, # collection is a NOT member and is marked for deletion; This is a UI introduced option
+              '2' => { id: existing_child_work.id },
+              '3' => { id: work_to_add.id }
+            }
+          )
+        end
+
+        it "handles destroy/non-destroy and keep/add behaviors" do
+          expect { middleware.update(env) }.to change { env.curation_concern.ordered_members.to_a }
+          expect(env.curation_concern.ordered_member_ids).to match_array [existing_child_work.id, work_to_add.id]
+          expect(env.curation_concern.member_ids).to match_array [existing_child_work.id, work_to_add.id]
+        end
+      end
+
+      context "when the id does not exist in the members" do
+        let(:another_work) { create(:work) }
+        let(:attributes) { HashWithIndifferentAccess.new(work_members_attributes: { '0' => { id: another_work.id } }) }
+
+        context "and I can edit that object" do
+          before do
+            allow(ability).to receive(:can?).with(:edit, String).and_return(true)
+          end
+          it "is added to the ordered members" do
+            expect { middleware.update(env) }.to change { env.curation_concern.ordered_members.to_a }
+            expect(env.curation_concern.ordered_member_ids).to include(existing_child_work.id, another_work.id)
+          end
+        end
+
+        context "and I can not edit that object" do
+          it "does nothing" do
+            expect { middleware.update(env) }.not_to change { env.curation_concern.ordered_members.to_a }
+          end
         end
       end
     end
