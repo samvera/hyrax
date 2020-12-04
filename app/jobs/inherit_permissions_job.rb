f9988702430324ff3d9289c178f196385a3ae13f
tamsin johnson <tomjohnson@ucsb.edu>
2020-12-03 16:07:47 -0800

    refactor InheritPermissionsJob
    avoid complicated diffing logic in favor of a wipe and replace strategy. we need
    to use `#<<` here, because it ensures the correct `access_to` is set. probably
    we can tweak `Hydra::AccessControlListpermissions=` to simplify this further.

--- app/jobs/inherit_permissions_job.rb
+++ app/jobs/inherit_permissions_job.rb
@@ -16,26 +16,11 @@ class InheritPermissionsJob < Hyrax::ApplicationJob
 
   private
 
-  # Return array of hashes representing permissions without their :access_to objects
-  # @param permissions [Permission]
-  # @return [Array<Hash>]
-  def permissions_map(permissions)
-    permissions.collect { |p| { agent: agent_object(p.agent), mode: p.mode } }
-  end
-
   # Returns a list of member file_sets for a work
   # @param work [Resource]
   # @return [Array<Hyrax::File_Set>]
   def file_sets_for(work)
-    Hyrax.query_service.custom_queries.find_child_filesets(resource: work)
-  end
-
-  # Converts string representation of Permission.agent to either User or Hyrax::Group
-  # @param agent [String]
-  # @return [User] or [Hyrax::Group]
-  def agent_object(agent)
-    return Hyrax::Group.new(agent.sub(Hyrax::Group.name_prefix, '')) if agent.starts_with?(Hyrax::Group.name_prefix)
-    User.find_by_user_key(agent)
+    Hyrax.custom_queries.find_child_filesets(resource: work)
   end
 
   # Perform the copy from the work to the contained filesets
@@ -61,14 +46,12 @@ class InheritPermissionsJob < Hyrax::ApplicationJob
   #
   # @param work containing access level and filesets
   def valkyrie_perform(work)
-    work_permissions = permissions_map(work.permission_manager.acl.permissions)
+    work_permissions = Hyrax::AccessControlList.new(resource: work).permissions
+
     file_sets_for(work).each do |file|
       file_acl = Hyrax::AccessControlList.new(resource: file)
-      file_permissions = permissions_map(file_acl.permissions)
-      # grant new work permissions to member file_sets
-      (work_permissions - file_permissions).each { |perm| file_acl.grant(perm[:mode]).to(perm[:agent]).save }
-      # remove permissions that are not on work from member file_sets
-      (file_permissions - work_permissions).each { |perm| file_acl.revoke(perm[:mode]).from(perm[:agent]).save }
+      file_acl.permissions = []
+      work_permissions.each { |permission| file_acl << permission }
       file_acl.save
     end
   end
