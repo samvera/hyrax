af6e665527a6568e7b61c1491fe73809ffcc8bf0
tamsin johnson <tomjohnson@ucsb.edu>
2020-12-07 11:09:48 -0800

    refactor workflow_factory_spec
    avoiding running operations as `subject`. `subject` should be the unit under
    test.

--- spec/services/hyrax/workflow/workflow_factory_spec.rb
+++ spec/services/hyrax/workflow/workflow_factory_spec.rb
@@ -2,25 +2,25 @@
 require 'rails_helper'
 
 RSpec.describe Hyrax::Workflow::WorkflowFactory do
+  subject(:factory) { described_class }
+
+  let(:attributes) { {} }
+  let(:deposit_action) { Sipity::WorkflowAction.create!(workflow: workflow, name: 'start') }
+  let(:permission_template) { Hyrax::PermissionTemplate.find_by!(source_id: work.admin_set_id) }
+  let(:user) { FactoryBot.create(:user) }
+  let(:work) { create(:work, with_admin_set: { with_permission_template: true }) }
+  let(:workflow) { FactoryBot.create(:workflow, active: true, permission_template: permission_template) }
+
   describe '.create' do
-    let(:work) { create(:work, with_admin_set: { with_permission_template: true }) }
-    let(:permission_template) { Hyrax::PermissionTemplate.find_by!(source_id: work.admin_set_id) }
-    let(:workflow) { create(:workflow, active: true, permission_template: permission_template) }
-    let(:attributes) { {} }
-    let(:user) { create(:user) }
-    let(:deposit_action) { Sipity::WorkflowAction.create!(workflow: workflow, name: 'start') }
-
-    subject { described_class.create(work, attributes, user) }
-
     it 'creates a Sipity::Entity, assign entity specific responsibility (but not to the full workflow) then runs the WorkflowActionService' do
-      expect(Hyrax::Workflow::WorkflowActionService).to receive(:run).with(
-        subject: kind_of(Hyrax::WorkflowActionInfo), action: deposit_action
-      )
+      expect(Hyrax::Workflow::WorkflowActionService)
+        .to receive(:run)
+        .with(subject: kind_of(Hyrax::WorkflowActionInfo), action: deposit_action)
+
       expect do
-        expect do
-          subject
-        end.to change { Sipity::Entity.count }.by(1)
-                                              .and change { Sipity::EntitySpecificResponsibility.count }.by(1)
+        expect { factory.create(work, attributes, user) }
+          .to change { Sipity::Entity.count }.by(1)
+          .and change { Sipity::EntitySpecificResponsibility.count }.by(1)
       end.not_to change { Sipity::WorkflowResponsibility.count }
     end
   end
