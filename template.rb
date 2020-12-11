2bb288d94d9bcc77cf0ebecf0fd4243abf6321be
tamsin johnson <tomjohnson@ucsb.edu>
2020-12-11 15:08:51 -0800

    prepare release 2.9.1
    include security fix in #4660

--- template.rb
+++ template.rb
@@ -1,6 +1,6 @@
 # Hack for https://github.com/rails/rails/issues/35153
 gsub_file 'Gemfile', /^gem ["']sqlite3["']$/, 'gem "sqlite3", "~> 1.3.0"'
-gem 'hyrax', '2.9.0'
+gem 'hyrax', '2.9.1'
 run 'bundle install'
 generate 'hyrax:install', '-f'
 rails_command 'db:migrate'
