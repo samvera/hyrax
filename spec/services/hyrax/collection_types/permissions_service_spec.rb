6b15e5bd5186ffa648afd054ddf53365afcead3b
tamsin johnson <tomjohnson@ucsb.edu>
2020-12-30 11:24:04 -0800

    refactor collection type permissions service spec
    these specs don't test the bulk of the code branches in the
    PermissionsService (see also: https://github.com/samvera/hyrax/issues/4686)
    
    in the interest of fixing that, and clarifying the permissions service
    contracts, clean up the tests by removing unused/misleading `:ability` variables
    and removing `subject` memos that don't represent the unit under test.

--- spec/services/hyrax/collection_types/permissions_service_spec.rb
+++ spec/services/hyrax/collection_types/permissions_service_spec.rb
@@ -1,36 +1,39 @@
 # frozen_string_literal: true
 RSpec.describe Hyrax::CollectionTypes::PermissionsService do
-  let(:user_cg) { create(:user, groups: 'create_group') }
-  let(:user_mg) { create(:user, groups: 'manage_group') }
-  let(:user_cu) { create(:user) }
-  let(:user_mu) { create(:user) }
-  let(:user_other) { create(:user) }
-  let(:user_admin) { create(:user, groups: 'admin') }
+  let(:user_cg) { FactoryBot.create(:user, groups: 'create_group') }
+  let(:user_mg) { FactoryBot.create(:user, groups: 'manage_group') }
+  let(:user_cu) { FactoryBot.create(:user) }
+  let(:user_mu) { FactoryBot.create(:user) }
+  let!(:user_collection_type) { FactoryBot.create(:user_collection_type) }
+  let!(:admin_set_collection_type) { FactoryBot.create(:admin_set_collection_type) }
 
-  let!(:user_collection_type) { create(:user_collection_type) }
-  let!(:admin_set_collection_type) { create(:admin_set_collection_type) }
-  let!(:collection_type) { create(:collection_type, creator_user: user_cu, creator_group: 'create_group', manager_user: user_mu, manager_group: 'manage_group') }
+  let!(:collection_type) do
+    FactoryBot.create(:collection_type,
+                      creator_user: user_cu,
+                      creator_group: 'create_group',
+                      manager_user: user_mu,
+                      manager_group: 'manage_group')
+  end
 
   describe '.collection_types_for_user' do # Also tests .collection_type_ids_for_user which is called by .collection_types_for_user
-    let!(:ability) { ::Ability.new(user) }
-
-    before { allow(ability).to receive(:current_user).and_return(user) }
-
-    subject { described_class.collection_types_for_user(user: user, roles: access).map(&:id) }
-
     context 'when user is a create user' do
       let(:user) { user_cu }
 
       context 'and create access is queried' do
-        let(:access) { Hyrax::CollectionTypeParticipant::CREATE_ACCESS }
+        it 'gives the collection types the user has create access for' do
+          access = Hyrax::CollectionTypeParticipant::CREATE_ACCESS
 
-        it { is_expected.to match_array [collection_type.id, user_collection_type.id] }
+          expect(described_class.collection_types_for_user(user: user, roles: access))
+            .to contain_exactly(collection_type, user_collection_type)
+        end
       end
 
       context 'and manage access is queried' do
-        let(:access) { Hyrax::CollectionTypeParticipant::MANAGE_ACCESS }
+        it 'is empty' do
+          access = Hyrax::CollectionTypeParticipant::MANAGE_ACCESS
 
-        it { is_expected.to match_array [] }
+          expect(described_class.collection_types_for_user(user: user, roles: access)).to be_empty
+        end
       end
     end
 
@@ -38,15 +41,20 @@ RSpec.describe Hyrax::CollectionTypes::PermissionsService do
       let(:user) { user_cg }
 
       context 'and create access is queried' do
-        let(:access) { Hyrax::CollectionTypeParticipant::CREATE_ACCESS }
+        it 'gives the collection types the user has create access for' do
+          access = Hyrax::CollectionTypeParticipant::CREATE_ACCESS
 
-        it { is_expected.to match_array [collection_type.id, user_collection_type.id] }
+          expect(described_class.collection_types_for_user(user: user, roles: access))
+            .to contain_exactly(collection_type, user_collection_type)
+        end
       end
 
       context 'and manage access is queried' do
-        let(:access) { Hyrax::CollectionTypeParticipant::MANAGE_ACCESS }
+        it 'is empty' do
+          access = Hyrax::CollectionTypeParticipant::MANAGE_ACCESS
 
-        it { is_expected.to match_array [] }
+          expect(described_class.collection_types_for_user(user: user, roles: access)).to be_empty
+        end
       end
     end
 
@@ -54,15 +62,21 @@ RSpec.describe Hyrax::CollectionTypes::PermissionsService do
       let(:user) { user_mu }
 
       context 'and create access is queried' do
-        let(:access) { Hyrax::CollectionTypeParticipant::CREATE_ACCESS }
+        it 'gives the collection types the user has create access for' do
+          access = Hyrax::CollectionTypeParticipant::CREATE_ACCESS
 
-        it { is_expected.to match_array [user_collection_type.id] }
+          expect(described_class.collection_types_for_user(user: user, roles: access))
+            .to contain_exactly(user_collection_type)
+        end
       end
 
       context 'and manage access is queried' do
-        let(:access) { Hyrax::CollectionTypeParticipant::MANAGE_ACCESS }
+        it 'gives the collection types the user has manage access for' do
+          access = Hyrax::CollectionTypeParticipant::MANAGE_ACCESS
 
-        it { is_expected.to match_array [collection_type.id] }
+          expect(described_class.collection_types_for_user(user: user, roles: access))
+            .to contain_exactly(collection_type)
+        end
       end
     end
 
@@ -70,229 +84,212 @@ RSpec.describe Hyrax::CollectionTypes::PermissionsService do
       let(:user) { user_mg }
 
       context 'and create access is queried' do
-        let(:access) { Hyrax::CollectionTypeParticipant::CREATE_ACCESS }
+        it 'gives the collection types the user has create access for' do
+          access = Hyrax::CollectionTypeParticipant::CREATE_ACCESS
 
-        it { is_expected.to match_array [user_collection_type.id] }
+          expect(described_class.collection_types_for_user(user: user, roles: access))
+            .to contain_exactly(user_collection_type)
+        end
       end
 
       context 'and manage access is queried' do
-        let(:access) { Hyrax::CollectionTypeParticipant::MANAGE_ACCESS }
+        it 'gives the collection types the user has manage access for' do
+          access = Hyrax::CollectionTypeParticipant::MANAGE_ACCESS
 
-        it { is_expected.to match_array [collection_type.id] }
+          expect(described_class.collection_types_for_user(user: user, roles: access))
+            .to contain_exactly(collection_type)
+        end
       end
     end
 
     context 'when user is an admin' do
-      let(:user) { user_admin }
+      let(:user) { FactoryBot.create(:user, groups: 'admin') }
 
       context 'and create access is queried' do
-        let(:access) { Hyrax::CollectionTypeParticipant::CREATE_ACCESS }
+        it 'gives all collection types' do
+          access = Hyrax::CollectionTypeParticipant::CREATE_ACCESS
 
-        it { is_expected.to match_array [collection_type.id, user_collection_type.id, admin_set_collection_type.id] }
+          expect(described_class.collection_types_for_user(user: user, roles: access))
+            .to contain_exactly(collection_type, user_collection_type, admin_set_collection_type)
+        end
       end
 
       context 'and manage access is queried' do
-        let(:access) { Hyrax::CollectionTypeParticipant::MANAGE_ACCESS }
+        it 'gives all collection types' do
+          access = Hyrax::CollectionTypeParticipant::MANAGE_ACCESS
 
-        it { is_expected.to match_array [collection_type.id, user_collection_type.id, admin_set_collection_type.id] }
+          expect(described_class.collection_types_for_user(user: user, roles: access))
+            .to contain_exactly(collection_type, user_collection_type, admin_set_collection_type)
+        end
       end
     end
 
     context 'when user with no access' do
-      let(:user) { user_other }
+      let(:user) { FactoryBot.create(:user) }
 
       context 'and create access is queried' do
-        let(:access) { Hyrax::CollectionTypeParticipant::CREATE_ACCESS }
+        it 'still grants access to the user collection types (user can create "user collections")' do
+          access = Hyrax::CollectionTypeParticipant::CREATE_ACCESS
 
-        it { is_expected.to match_array [user_collection_type.id] }
+          expect(described_class.collection_types_for_user(user: user, roles: access))
+            .to contain_exactly(user_collection_type)
+        end
       end
 
       context 'and manage access is queried' do
-        let(:access) { Hyrax::CollectionTypeParticipant::MANAGE_ACCESS }
+        it 'is empty' do
+          access = Hyrax::CollectionTypeParticipant::MANAGE_ACCESS
 
-        it { is_expected.to match_array [] }
+          expect(described_class.collection_types_for_user(user: user, roles: access)).to be_empty
+        end
       end
     end
   end
 
   describe '.can_create_any_collection_type?' do
-    let!(:ability) { ::Ability.new(user) }
-
-    before do
-      allow(ability).to receive(:current_user).and_return(user)
-    end
-
-    subject { described_class.can_create_any_collection_type?(user: user) }
-
     context 'when user is a create user' do
       let(:user) { user_cu }
 
-      it { is_expected.to be true }
+      it 'can create collection types' do
+        expect(described_class.can_create_any_collection_type?(user: user)).to be true
+      end
     end
 
     context 'when user is in create group' do
       let(:user) { user_cg }
 
-      it { is_expected.to be true }
+      it 'can create collection types' do
+        expect(described_class.can_create_any_collection_type?(user: user)).to be true
+      end
     end
 
     context 'when user is a manage user' do
       let(:user) { user_mu }
 
-      it { is_expected.to be true }
+      it 'can create collection types' do
+        expect(described_class.can_create_any_collection_type?(user: user)).to be true
+      end
     end
 
     context 'when user is in manage group' do
       let(:user) { user_mg }
 
-      it { is_expected.to be true }
+      it 'can create collection types' do
+        expect(described_class.can_create_any_collection_type?(user: user)).to be true
+      end
     end
 
     context 'when user is an admin' do
-      let(:user) { user_admin }
+      let(:user) { FactoryBot.create(:user, groups: 'admin') }
 
-      it { is_expected.to be true }
+      it 'can create collection types' do
+        expect(described_class.can_create_any_collection_type?(user: user)).to be true
+      end
     end
 
     context 'when user with no access' do
-      let(:user) { user_other }
+      let(:user) { FactoryBot.create(:user) }
 
-      # TODO: Fails for now becauser User Collection type is always created and it allows every logged in user to create one.
-      # Would require additional work to remove or change the User Collection participants so there are 0 collection types the
-      # user can create.
-      xit { is_expected.to be false }
+      it 'can create collection types' do
+        expect(described_class.can_create_any_collection_type?(user: user)).to be true
+      end
     end
   end
 
   describe '.can_create_admin_set?' do
-    let!(:ability) { ::Ability.new(user) }
-
-    before { allow(ability).to receive(:current_user).and_return(user) }
-
-    subject { described_class.can_create_admin_set_collection_type?(user: user) }
-
-    context 'when user is a create user' do
-      let(:user) { user_cu }
-
-      it { is_expected.to be false }
+    it 'create user cannot create admin sets' do
+      expect(described_class.can_create_admin_set_collection_type?(user: user_cu)).to be false
     end
 
-    context 'when user is in create group' do
-      let(:user) { user_cg }
-
-      it { is_expected.to be false }
+    it 'user in create group cannot create admin sets' do
+      expect(described_class.can_create_admin_set_collection_type?(user: user_cg)).to be false
     end
 
-    context 'when user is a manage user' do
-      let(:user) { user_mu }
-
-      it { is_expected.to be false }
+    it 'manage user cannot create admin sets' do
+      expect(described_class.can_create_admin_set_collection_type?(user: user_mu)).to be false
     end
 
-    context 'when user is in manage group' do
-      let(:user) { user_mg }
-
-      it { is_expected.to be false }
+    it 'user in manage group in cannot create admin sets' do
+      expect(described_class.can_create_admin_set_collection_type?(user: user_mg)).to be false
     end
 
-    context 'when user is an admin' do
-      let(:user) { user_admin }
-
-      it { is_expected.to be true }
+    it 'admin user can create admin sets' do
+      user = FactoryBot.create(:user, groups: 'admin')
+      expect(described_class.can_create_admin_set_collection_type?(user: user)).to be true
     end
 
-    context 'when user with no access' do
-      let(:user) { user_other }
-
-      it { is_expected.to be false }
+    it 'new user cannot create admin sets' do
+      user = FactoryBot.create(:user)
+      expect(described_class.can_create_admin_set_collection_type?(user: user)).to be false
     end
   end
 
   describe '.can_create_collection_types' do
-    let!(:ability) { ::Ability.new(user) }
-
-    before { allow(ability).to receive(:current_user).and_return(user) }
-
-    subject { described_class.can_create_collection_types(user: user).map(&:id) }
-
-    context 'when user is a create user' do
-      let(:user) { user_cu }
-
-      it { is_expected.to match_array [collection_type.id, user_collection_type.id] }
+    it 'lists collection types user can create collections for' do
+      expect(described_class.can_create_collection_types(user: user_cu))
+        .to contain_exactly(collection_type, user_collection_type)
     end
 
-    context 'when user is in create group' do
-      let(:user) { user_cg }
-
-      it { is_expected.to match_array [collection_type.id, user_collection_type.id] }
+    it 'lists collection types user groups can create collections for' do
+      expect(described_class.can_create_collection_types(user: user_cg))
+        .to contain_exactly(collection_type, user_collection_type)
     end
 
-    context 'when user is a manage user' do
-      let(:user) { user_mu }
-
-      it { is_expected.to match_array [collection_type.id, user_collection_type.id] }
+    it 'includes collection types user can manage' do
+      expect(described_class.can_create_collection_types(user: user_mu))
+        .to contain_exactly(collection_type, user_collection_type)
     end
 
-    context 'when user is in manage group' do
-      let(:user) { user_mg }
-
-      it { is_expected.to match_array [collection_type.id, user_collection_type.id] }
+    it 'includes collection types user groups can manage' do
+      expect(described_class.can_create_collection_types(user: user_mg))
+        .to contain_exactly(collection_type, user_collection_type)
     end
 
-    context 'when user is an admin' do
-      let(:user) { user_admin }
+    it 'includes all collection types for admin user' do
+      admin = FactoryBot.create(:user, groups: 'admin')
 
-      it { is_expected.to match_array [collection_type.id, user_collection_type.id, admin_set_collection_type.id] }
+      expect(described_class.can_create_collection_types(user: admin))
+        .to contain_exactly(collection_type, user_collection_type, admin_set_collection_type)
     end
 
-    context 'when user with no access' do
-      let(:user) { user_other }
-
-      it { is_expected.to match_array [user_collection_type.id] }
+    it 'includes only user collection type for new user' do
+      user = FactoryBot.create(:user)
+      expect(described_class.can_create_collection_types(user: user))
+        .to contain_exactly(user_collection_type)
     end
   end
 
   describe '.user_edit_grants_for_collection_of_type' do
-    subject { described_class.user_edit_grants_for_collection_of_type(collection_type: coltype) }
-
-    context 'for user collection type' do
-      let(:coltype) { user_collection_type }
-
-      it { is_expected.to match_array [] }
+    it 'is empty for user collection type' do
+      expect(described_class.user_edit_grants_for_collection_of_type(collection_type: user_collection_type))
+        .to be_empty
     end
 
-    context 'for admin set collection type' do
-      let(:coltype) { admin_set_collection_type }
-
-      it { is_expected.to match_array [] }
+    it 'is empty for admin set collection type' do
+      expect(described_class.user_edit_grants_for_collection_of_type(collection_type: admin_set_collection_type))
+        .to be_empty
     end
 
-    context 'for collection type' do
-      let(:coltype) { collection_type }
-
-      it { is_expected.to match_array [user_mu.user_key] }
+    it 'gives configured manage users' do
+      expect(described_class.user_edit_grants_for_collection_of_type(collection_type: collection_type))
+        .to contain_exactly(user_mu.user_key)
     end
   end
 
   describe '.group_edit_grants_for_collection_of_type' do
-    subject { described_class.group_edit_grants_for_collection_of_type(collection_type: coltype) }
-
-    context 'for user collection type' do
-      let(:coltype) { user_collection_type }
-
-      it { is_expected.to match_array ['admin'] }
+    it 'user collection type grants edit to admin group' do
+      expect(described_class.group_edit_grants_for_collection_of_type(collection_type: user_collection_type))
+        .to contain_exactly('admin')
     end
 
-    context 'for admin set collection type' do
-      let(:coltype) { admin_set_collection_type }
-
-      it { is_expected.to match_array ['admin'] }
+    it 'admin set type grants edit to admin group' do
+      expect(described_class.group_edit_grants_for_collection_of_type(collection_type: admin_set_collection_type))
+        .to contain_exactly('admin')
     end
 
-    context 'for collection type' do
-      let(:coltype) { collection_type }
-
-      it { is_expected.to match_array ['manage_group', 'admin'] }
+    it 'grants edit to provided manage and admin groups' do
+      expect(described_class.group_edit_grants_for_collection_of_type(collection_type: collection_type))
+        .to contain_exactly('manage_group', 'admin')
     end
   end
 end
