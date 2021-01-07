bc593e16247989ecea6b78ee0e09b1da6c72b24b
tamsin johnson <tomjohnson@ucsb.edu>
2021-01-07 15:21:51 -0800

    remove some duplication from the `collection_spec` feature test
    this is a bit faster now, with some unneeded test setup removed.

--- spec/features/collection_spec.rb
+++ spec/features/collection_spec.rb
@@ -1,25 +1,40 @@
 # frozen_string_literal: true
-RSpec.describe 'collection', type: :feature, clean_repo: true do
-  let(:user) { create(:user) }
+RSpec.describe 'collection', type: :feature do
+  let(:user) { FactoryBot.create(:user) }
 
-  let(:collection1) { create(:public_collection_lw, user: user) }
-  let(:collection2) { create(:public_collection_lw, user: user) }
+  let(:collection1) { FactoryBot.create(:public_collection_lw, user: user) }
+  let(:collection2) { FactoryBot.create(:public_collection_lw, user: user) }
+
+  before { sign_in(user) }
+
+  shared_context 'with many indexed members' do
+    let(:collection) { FactoryBot.create(:named_collection_lw, user: user) }
+
+    before do
+      docs = (0..12).map do |n|
+        { "has_model_ssim" => [model_name], :id => "zs25x871q#{n}",
+          "depositor_ssim" => [user.user_key],
+          "suppressed_bsi" => false,
+          "member_of_collection_ids_ssim" => [collection.id],
+          "nesting_collection__parent_ids_ssim" => [collection.id],
+          "edit_access_person_ssim" => [user.user_key] }
+      end
+      Hyrax::SolrService.add(docs, commit: true)
+    end
+  end
 
   describe 'collection show page' do
     let(:collection) do
-      create(:public_collection_lw, user: user, description: ['collection description'], collection_type_settings: :nestable)
+      FactoryBot.create(:public_collection_lw, user: user, description: ['collection description'], collection_type_settings: :nestable)
     end
-    let!(:work1) { create(:work, title: ["King Louie"], member_of_collections: [collection], user: user) }
-    let!(:work2) { create(:work, title: ["King Kong"], member_of_collections: [collection], user: user) }
-    let!(:col1) { create(:public_collection_lw, title: ["Sub-collection 1"], member_of_collections: [collection], user: user) }
-    let!(:col2) { create(:public_collection_lw, title: ["Sub-collection 2"], member_of_collections: [collection], user: user) }
-
-    before do
-      sign_in user
-      visit "/collections/#{collection.id}"
-    end
+    let!(:work1) { FactoryBot.create(:work, title: ["King Louie"], member_of_collections: [collection], user: user) }
+    let!(:work2) { FactoryBot.create(:work, title: ["King Kong"], member_of_collections: [collection], user: user) }
+    let!(:col1) { FactoryBot.create(:public_collection_lw, title: ["Sub-collection 1"], member_of_collections: [collection], user: user) }
+    let!(:col2) { FactoryBot.create(:public_collection_lw, title: ["Sub-collection 2"], member_of_collections: [collection], user: user) }
 
     it "shows a collection with a listing of Descriptive Metadata and catalog-style search results" do
+      visit "/collections/#{collection.id}"
+
       expect(page).to have_content(collection.title.first)
       expect(page).to have_content(collection.description.first)
       expect(page).to have_content("Collection Details")
@@ -39,6 +54,8 @@ RSpec.describe 'collection', type: :feature, clean_repo: true do
     end
 
     it "hides collection descriptive metadata when searching a collection" do
+      visit "/collections/#{collection.id}"
+
       expect(page).to have_content(collection.title.first)
       expect(page).to have_content(collection.description.first)
       expect(page).to have_content(work1.title.first)
@@ -57,66 +74,48 @@ RSpec.describe 'collection', type: :feature, clean_repo: true do
 
     it "returns json results with correct id and ids" do
       visit "/collections/#{collection.id}.json"
+
       expect(page).to have_http_status(:success)
       json = JSON.parse(page.body)
       expect(json['id']).to eq collection.id
       expect(json['title']).to match_array collection.title
     end
+  end
 
-    context "with a non-nestable collection type" do
-      let(:collection) do
-        build(:public_collection_lw, user: user, description: ['collection description'], collection_type_settings: :not_nestable, with_solr_document: true, with_permission_template: true)
-      end
+  context "with a non-nestable collection type" do
+    let(:collection) do
+      FactoryBot.create(:public_collection_lw, user: user, description: ['collection description'], collection_type_settings: :not_nestable, with_solr_document: true, with_permission_template: true)
+    end
 
-      it "displays basic information on its show page" do
-        expect(page).to have_content(collection.title.first)
-        expect(page).to have_content(collection.description.first)
-        expect(page).to have_content("Collection Details")
-      end
+    it "displays basic information on its show page" do
+      visit "/collections/#{collection.id}"
+
+      expect(page).to have_content(collection.title.first)
+      expect(page).to have_content(collection.description.first)
+      expect(page).to have_content("Collection Details")
     end
   end
 
-  # TODO: this is just like the block above. Merge them.
   describe 'show work pages of a collection' do
-    before do
-      docs = (0..12).map do |n|
-        { "has_model_ssim" => ["GenericWork"], :id => "zs25x871q#{n}",
-          "depositor_ssim" => [user.user_key],
-          "suppressed_bsi" => false,
-          "member_of_collection_ids_ssim" => [collection.id],
-          "nesting_collection__parent_ids_ssim" => [collection.id],
-          "edit_access_person_ssim" => [user.user_key] }
-      end
-      Hyrax::SolrService.add(docs, commit: true)
-
-      sign_in user
+    include_context 'with many indexed members' do
+      let(:model_name) { 'Monograph' }
     end
-    let(:collection) { create(:named_collection_lw, user: user) }
 
     it "shows a collection with a listing of Descriptive Metadata and catalog-style search results" do
       visit "/collections/#{collection.id}"
+
       expect(page).to have_css(".pagination")
     end
   end
 
   describe 'show subcollection pages of a collection' do
-    before do
-      docs = (0..12).map do |n|
-        { "has_model_ssim" => ["Collection"], :id => "zs25x871q#{n}",
-          "depositor_ssim" => [user.user_key],
-          "suppressed_bsi" => false,
-          "member_of_collection_ids_ssim" => [collection.id],
-          "nesting_collection__parent_ids_ssim" => [collection.id],
-          "edit_access_person_ssim" => [user.user_key] }
-      end
-      Hyrax::SolrService.add(docs, commit: true)
-
-      sign_in user
+    include_context 'with many indexed members' do
+      let(:model_name) { 'Collection' }
     end
-    let(:collection) { create(:named_collection_lw, user: user) }
 
     it "shows a collection with a listing of Descriptive Metadata and catalog-style search results" do
       visit "/collections/#{collection.id}"
+
       expect(page).to have_css(".pagination")
     end
   end
