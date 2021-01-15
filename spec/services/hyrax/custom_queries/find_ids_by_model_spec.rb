9f5f0a7bad67ada622bf9f691a59d7f58099fd68
tamsin johnson <tomjohnson@ucsb.edu>
2021-01-15 11:20:03 -0800

    implement a generic query for finding ids by model
    this makes it possible to query Valkyrie adapters to filter a set of ids down to
    those matching a model. if no ids are passed, all ids matching the model are
    returned.
    
    this default implementation follows our policy of providing an implementation of
    all custom queries that works for Hyrax using only Valkyrie's built-in
    queries. follow-up work will implement for Wings.
    
    related to #4722.

---
+++ spec/services/hyrax/custom_queries/find_ids_by_model_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+RSpec.describe Hyrax::CustomQueries::FindIdsByModel, valkyrie_adapter: :test_adapter do
+  subject(:query_handler) { described_class.new(query_service: Hyrax.query_service) }
+
+  after { Hyrax.persister.wipe! }
+
+  describe '#find_ids_by_model' do
+    let(:monographs) { [FactoryBot.valkyrie_create(:monograph), FactoryBot.valkyrie_create(:monograph), FactoryBot.valkyrie_create(:monograph)] }
+
+    before { [FactoryBot.valkyrie_create(:hyrax_work), FactoryBot.valkyrie_create(:hyrax_work)] }
+
+    it 'for a model without resources is empty' do
+      expect(query_handler.find_ids_by_model(model: Monograph)).to be_empty
+    end
+
+    it 'finds ids matching the model' do
+      monographs # create
+      expect(query_handler.find_ids_by_model(model: Monograph)).to contain_exactly(*monographs.map(&:id))
+    end
+
+    context 'with ids to filter' do
+      let(:ids) { ['first_id', 'second_id'] }
+
+      it 'for a model without resources is empty' do
+        expect(query_handler.find_ids_by_model(model: Monograph, ids: ids)).to be_empty
+      end
+
+      it 'finds ids matching the model' do
+        monograph_ids = monographs.map(&:id)
+        query_ids = monograph_ids.take(2) + ['fake_id']
+
+        expect(query_handler.find_ids_by_model(model: Monograph, ids: query_ids)).to contain_exactly(*monograph_ids.take(2))
+      end
+    end
+  end
+end
