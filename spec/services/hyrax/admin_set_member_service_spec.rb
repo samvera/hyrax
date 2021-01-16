c15e759ce4f27d9fd9cab1e28eb082cb40a14318
tamsin johnson <tomjohnson@ucsb.edu>
2021-01-15 19:23:54 -0800

    refactor AdminAdminSetMemberSearchBuilder specs
    there was a bunch going on in this test setup that wasn't really used. there
    also seems to be more going on in this builder than is really justified as well,
    especially for a class that's only used in one place.
    
    ripping up the specs then moving closer to the controller to see what's up.

--- spec/services/hyrax/admin_set_member_service_spec.rb
+++ spec/services/hyrax/admin_set_member_service_spec.rb
@@ -2,18 +2,17 @@
 RSpec.describe Hyrax::AdminSetMemberService, clean_repo: true do
   subject(:builder) { described_class.new(scope: scope, collection: admin_set, params: { "id" => admin_set.id.to_s }) }
   let(:admin_set) { FactoryBot.build(:admin_set) }
-  let(:current_ability) { instance_double(Ability, admin?: true) }
-  let(:scope) { FakeSearchBuilderScope.new(current_ability: current_ability) }
+  let(:scope) { FakeSearchBuilderScope.new(current_ability: Ability.new(user)) }
+  let(:user) { FactoryBot.create(:admin) }
 
   describe '#available_member_works' do
-    let!(:work1) { create(:generic_work, admin_set: admin_set) }
-    let!(:work2) { create(:generic_work) }
-    let!(:work3) { create(:generic_work, admin_set: admin_set) }
+    let!(:work1) { FactoryBot.create(:generic_work, admin_set: admin_set) }
+    let!(:work2) { FactoryBot.create(:generic_work) }
+    let!(:work3) { FactoryBot.create(:generic_work, admin_set: admin_set) }
 
     it 'returns the members of the admin set' do
-      ids = builder.available_member_works.response[:docs].map { |col| col[:id] }
-
-      expect(ids).to contain_exactly(work1.id, work3.id)
+      expect(builder.available_member_works.response[:docs])
+        .to contain_exactly(include(id: work1.id), include(id: work3.id))
     end
   end
 end
