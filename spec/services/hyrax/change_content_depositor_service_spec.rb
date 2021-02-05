8bcf23aee43168991960a2c9e336f4db269fd2d7
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2021-02-05 08:36:27 -0500

    Reworking spec structure (no changes otherwise)
    This is a reworking of the structure of a spec.  As written the spec
    doesn't make much space for a sibling Valkyrie spec.
    
    This commit helps a reviewer see the step and, I hope "move along".

--- spec/services/hyrax/change_content_depositor_service_spec.rb
+++ spec/services/hyrax/change_content_depositor_service_spec.rb
@@ -2,49 +2,52 @@
 RSpec.describe Hyrax::ChangeContentDepositorService do
   let!(:depositor) { create(:user) }
   let!(:receiver) { create(:user) }
-  let!(:file) do
-    create(:file_set, user: depositor)
-  end
-  let!(:work) do
-    create(:work, title: ['Test work'], user: depositor)
-  end
 
-  before do
-    work.members << file
-    described_class.call(work, receiver, reset)
-  end
-
-  context "by default, when permissions are not reset" do
-    let(:reset) { false }
-
-    it "changes the depositor and records an original depositor" do
-      work.reload
-      expect(work.depositor).to eq receiver.user_key
-      expect(work.proxy_depositor).to eq depositor.user_key
-      expect(work.edit_users).to include(receiver.user_key, depositor.user_key)
+  context "for Active Fedora objects" do
+    let!(:file) do
+      create(:file_set, user: depositor)
+    end
+    let!(:work) do
+      create(:work, title: ['Test work'], user: depositor)
     end
 
-    it "changes the depositor of the child file sets" do
-      file.reload
-      expect(file.depositor).to eq receiver.user_key
-      expect(file.edit_users).to include(receiver.user_key, depositor.user_key)
-    end
-  end
-
-  context "when permissions are reset" do
-    let(:reset) { true }
-
-    it "excludes the depositor from the edit users" do
-      work.reload
-      expect(work.depositor).to eq receiver.user_key
-      expect(work.proxy_depositor).to eq depositor.user_key
-      expect(work.edit_users).to contain_exactly(receiver.user_key)
+    before do
+      work.members << file
+      described_class.call(work, receiver, reset)
     end
 
-    it "changes the depositor of the child file sets" do
-      file.reload
-      expect(file.depositor).to eq receiver.user_key
-      expect(file.edit_users).to contain_exactly(receiver.user_key)
+    context "by default, when permissions are not reset" do
+      let(:reset) { false }
+
+      it "changes the depositor and records an original depositor" do
+        work.reload
+        expect(work.depositor).to eq receiver.user_key
+        expect(work.proxy_depositor).to eq depositor.user_key
+        expect(work.edit_users).to include(receiver.user_key, depositor.user_key)
+      end
+
+      it "changes the depositor of the child file sets" do
+        file.reload
+        expect(file.depositor).to eq receiver.user_key
+        expect(file.edit_users).to include(receiver.user_key, depositor.user_key)
+      end
+    end
+
+    context "when permissions are reset" do
+      let(:reset) { true }
+
+      it "excludes the depositor from the edit users" do
+        work.reload
+        expect(work.depositor).to eq receiver.user_key
+        expect(work.proxy_depositor).to eq depositor.user_key
+        expect(work.edit_users).to contain_exactly(receiver.user_key)
+      end
+
+      it "changes the depositor of the child file sets" do
+        file.reload
+        expect(file.depositor).to eq receiver.user_key
+        expect(file.edit_users).to contain_exactly(receiver.user_key)
+      end
     end
   end
 end
