4e89f0c9273ddae413ad65dd7f70a98d2edd5f2c
Rob Kaufman <rob@notch8.com>
2021-03-03 10:48:49 -0800

    Lock base at alpine3.12 and update fits to a url that still resolves. skip chown of paths if vars are not set

--- bin/hyrax-entrypoint.sh
+++ bin/hyrax-entrypoint.sh
@@ -5,8 +5,15 @@ set -e
 chown -fR app:app /app/samvera/hyrax-webapp/tmp
 chown -fR app:app /app/samvera/hyrax-webapp/public
 
-chown -fR app:app $HYRAX_DERIVATIVES_PATH
-chown -fR app:app $HYRAX_UPLOAD_PATH
+if [ -v HYRAX_DERIVITIVES_PATH ]; then
+  mkdir -p $HYRAX_DERIVITIVES_PATH
+  chown -f -R app:app $HYRAX_DERIVATIVES_PATH
+fi
+
+if [ -v HYRAX_UPLOAD_PATH ]; then
+  mkdir -p $HYRAX_UPLOAD_PATH
+  chown -f -R app:app $HYRAX_UPLOAD_PATH
+fi
 
 mkdir -p /app/samvera/hyrax-webapp/tmp/pids
 rm -f /app/samvera/hyrax-webapp/tmp/pids/*
