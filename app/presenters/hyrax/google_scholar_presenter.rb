f0216cd3b7d9c30b55bc472652ce14bdeb1b7144
tamsin johnson <tomjohnson@ucsb.edu>
2021-03-05 09:56:11 -0800

    move google scholar metadata display handling to gscholar presenter
    extracts view logic for google scholar metadata presentation to a new
    presenter. cleans up a bit of the logic to be more resilient to missing values
    and metadata schemas other than BasicMetadata.

--- app/presenters/hyrax/google_scholar_presenter.rb
+++ app/presenters/hyrax/google_scholar_presenter.rb
@@ -4,6 +4,12 @@ module Hyrax
   ##
   # Handles presentation for google scholar meta tags.
   #
+  # @example
+  #   my_book = Monograph.new(title: ['On Moomins'], creator: ['Tove', 'Lars'])
+  #   scholar = GoogleScholarPresenter.new(my_book)
+  #
+  #   scholar.title => 'On Moomins'
+  #
   # @see https://scholar.google.com/intl/en/scholar/inclusion.html#overview
   class GoogleScholarPresenter < Draper::Decorator
     ##
@@ -21,6 +27,60 @@ module Hyrax
       return object.scholarly? if object.respond_to?(:scholarly?)
 
       true
+    end
+
+    ##
+    # @note Google Scholar cares about author order. when possible, this should
+    #   return the othors in order. delegates to `#ordered_authors` when
+    #   available.
+    #
+    # @return [Array<String>] an ordered array of author names
+    def authors
+      return object.ordered_authors if object.respond_to?(:ordered_authors)
+
+      Array(object.creator)
+    end
+
+    ##
+    # @note falls back on {#title} if no description can be found. this
+    #   probably isn't great.
+    #
+    # @return [String] a description
+    def description
+      (Array(object.try(:description)).first || title).truncate(200)
+    end
+
+    ##
+    # @return [String] the keywords
+    def keywords
+      Array(object.try(:keyword)).join('; ')
+    end
+
+    ##
+    # @todo this should probably only return a present value if a PDF is
+    #   available!
+    #
+    # @return [#to_s]
+    def pdf_url
+      object.try(:download_url)
+    end
+
+    ##
+    # @return [String] the publication date
+    def publication_date
+      Array(object.try(:date_created)).first || ''
+    end
+
+    ##
+    # @return [String] a string representing the publisher
+    def publisher
+      Array(object.try(:publisher)).join('; ')
+    end
+
+    ##
+    # @return [String] exactly one title; the same one every time
+    def title
+      Array(object.try(:title)).sort.first || ""
     end
   end
 end
