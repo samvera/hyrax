274408db20e9bd0fa24244a5a0c18437f0aeec71
E. Lynette Rayle <elr37@cornell.edu>
2021-03-15 15:06:27 -0400

    Resources need to include the depositor to be included in search results
    This limiting part of the query that provides the list of works for Dashboard -> Works is…
    ```
    "fq"=>["{!terms f=has_model_ssim}GenericWork,NamespacedWorks::NestedWork,Monograph", "_query_:\"{!raw f=depositor_ssim}example@hotmail.com\""]
    ```
    
    So without the depositor in the solr doc, Monographs, which are Valkyrie::Resources, were never returned in the search results.  Adding in the depositor, they are now included.
    
    Remaining Work:
    
    * The thumbnail image is not displayed in the list, but is in the show page for the Monograph.

--- app/indexers/hyrax/valkyrie_work_indexer.rb
+++ app/indexers/hyrax/valkyrie_work_indexer.rb
@@ -16,6 +16,8 @@ module Hyrax
         solr_doc['admin_set_id_ssim'] = [resource.admin_set_id.to_s]
         solr_doc['member_of_collection_ids_ssim'] = resource.member_of_collection_ids.map(&:to_s)
         solr_doc['member_ids_ssim'] = resource.member_ids.map(&:to_s)
+        solr_doc['depositor_ssim'] = [resource.depositor]
+        solr_doc['depositor_tesim'] = [resource.depositor]
       end
     end
 
