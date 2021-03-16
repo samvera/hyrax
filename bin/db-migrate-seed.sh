d851ffbc09250e7852da9d9842f3b75da2284ab9
tamsin johnson <tomjohnson@ucsb.edu>
2021-03-16 14:06:32 -0700

    add a db:create step to the helm deployment process
    ensures that the configured database exists by adding a `rake db:create`
    invocation to the `db-setup` initContainer.
    
    in the event we're trying to deploy hyrax against a database engine that it has
    permissions for, but where the configured database doesn't exist, this will
    ensure it gets added before we try to run the migrations.

--- bin/db-migrate-seed.sh
+++ bin/db-migrate-seed.sh
@@ -2,8 +2,10 @@
 set -e
 
 db-wait.sh "$DB_HOST:$DB_PORT"
+bundle exec rails db:create
+bundle exec rails db:migrate
+
 db-wait.sh "$FCREPO_HOST:$FCREPO_PORT"
 db-wait.sh "$SOLR_HOST:$SOLR_PORT"
 
-bundle exec rails db:migrate
 bundle exec rails db:seed
