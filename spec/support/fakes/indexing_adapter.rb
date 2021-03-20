7864e268b534eca5575ea540282f2b4e9b008af4
tamsin johnson <tomjohnson@ucsb.edu>
2021-03-19 17:05:24 -0700

    remove deleted objects from the valkyrie powered index
    add handling for deletion to `MetadataIndexListener`.

--- spec/support/fakes/indexing_adapter.rb
+++ spec/support/fakes/indexing_adapter.rb
@@ -1,12 +1,17 @@
 # frozen_string_literal: true
 class FakeIndexingAdapter
-  attr_reader :saved_resources
+  attr_reader :deleted_resources, :saved_resources
 
   def initialize
+    @deleted_resources = []
     @saved_resources = []
   end
 
   def save(resource:)
     @saved_resources << resource
+  end
+
+  def delete(resource:)
+    @deleted_resources << resource
   end
 end
