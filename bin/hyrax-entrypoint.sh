9aca17759ea49bb86a2b132de627ed5e350cc3fe
tamsin johnson <tomjohnson@ucsb.edu>
2021-04-28 22:35:13 -0700

    remove chowns from image entrypoint
    these `chown` calls seemed useful to ensure that mounted volumes come in with
    the correct permissions. they don't really work out in practice. deployment
    platforms have their own mechanisms for setting volume permissions at mount
    time. it's best to use them.

--- bin/hyrax-entrypoint.sh
+++ bin/hyrax-entrypoint.sh
@@ -1,20 +1,6 @@
 #!/bin/sh
 set -e
 
-# Make sure common volume points have the app permission
-chown -fR app:app /app/samvera/hyrax-webapp/tmp
-chown -fR app:app /app/samvera/hyrax-webapp/public
-
-if [ -n "${HYRAX_DERIVITIVES_PATH+x}" ]; then
-  mkdir -p "${HYRAX_DERIVITIVES_PATH}"
-  chown -f -R app:app "${HYRAX_DERIVATIVES_PATH}"
-fi
-
-if [ -n "${HYRAX_UPLOAD_PATH+x}" ]; then
-  mkdir -p "${HYRAX_UPLOAD_PATH}"
-  chown -f -R app:app "${HYRAX_UPLOAD_PATH}"
-fi
-
 mkdir -p /app/samvera/hyrax-webapp/tmp/pids
 rm -f /app/samvera/hyrax-webapp/tmp/pids/*
 
