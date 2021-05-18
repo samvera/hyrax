99b4ce254933755bc7e5e2f027c327ed5496a442
straleyb <straleyb@onid.oregonstate.edu>
2021-05-18 16:30:15 -0700

    revert to initial use valkyrie

--- app/services/hyrax/workflow/pending_review_notification.rb
+++ app/services/hyrax/workflow/pending_review_notification.rb
@@ -10,7 +10,7 @@ module Hyrax
 
       def message
         I18n.t('hyrax.notifications.workflow.pending_review.message', title: title,
-                                                                      link: (link_to work_id, document_path),
+                                                                      link: (link_to work_id, document_path)
                                                                       user: user.user_key, comment: comment)
       end
 
