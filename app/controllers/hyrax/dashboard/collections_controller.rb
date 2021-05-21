f8bd25b7fd11aad4e06d3724012974277222ba3b
straleyb <straleyb@onid.oregonstate.edu>
2021-05-21 09:02:06 -0700

    update to use config for valkyrie

--- app/controllers/hyrax/dashboard/collections_controller.rb
+++ app/controllers/hyrax/dashboard/collections_controller.rb
@@ -384,7 +384,7 @@ module Hyrax
 
       def remove_members_from_collection
         batch.each do |pid|
-          work = Hyrax.query_service.find_by_alternate_identifier(alternate_identifier: pid, use_valkyrie: false)
+          work = Hyrax.query_service.find_by_alternate_identifier(alternate_identifier: pid, use_valkyrie: Hyrax.config.use_valkyrie?)
           work.member_of_collections.delete @collection
           work.save!
         end
