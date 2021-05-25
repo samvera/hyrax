2a1024c298beb8940632ba04cc1b31c28bbacfea
tamsin johnson <tomjohnson@ucsb.edu>
2021-05-25 10:10:20 -0700

    add a ChangeSet-based form for file sets
    this form might work as a drop-in replacement for the existing
    `FileSetEditForm`, even for `ActiveFedora` resources.

---
+++ app/forms/hyrax/forms/file_set_form.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+module Hyrax
+  module Forms
+    ##
+    # @api public
+    class FileSetForm < Hyrax::ChangeSet
+      class << self
+        ##
+        # @return [Array<Symbol>] list of required field names as symbols
+        def required_fields
+          definitions
+            .select { |_, definition| definition[:required] }
+            .keys.map(&:to_sym)
+        end
+      end
+
+      property :title, required: true
+      property :creator, required: true
+      property :license, required: true
+
+      property :based_near
+      property :contributor
+      property :date_created
+      property :description
+      property :identifier
+      property :keyword
+      property :language
+      property :publisher
+      property :related_url
+      property :subject
+      property :visibility, default: VisibilityIntention::PRIVATE
+
+      # virtual properties for embargo/lease;
+      property :embargo_release_date, virtual: true
+      property :visibility_after_embargo, virtual: true
+      property :visibility_during_embargo, virtual: true
+      property :lease_expiration_date, virtual: true
+      property :visibility_after_lease, virtual: true
+      property :visibility_during_lease, virtual: true
+    end
+  end
+end
