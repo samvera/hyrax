0171bc8ae54b93c43877e1bedfa059d0ca4a9a09
Chris Colvard <chris.colvard@gmail.com>
2021-06-25 15:32:44 -0400

    Rollback iiif url builder lambda signature change for Hyrax 2

--- .dassie/config/initializers/hyrax.rb
+++ .dassie/config/initializers/hyrax.rb
@@ -13,7 +13,7 @@ Hyrax.config do |config|
   config.iiif_image_server = true
 
   # Returns a URL that resolves to an image provided by a IIIF image server
-  config.iiif_image_url_builder = lambda do |file_id, base_url, size, format|
+  config.iiif_image_url_builder = lambda do |file_id, base_url, size|
     Riiif::Engine.routes.url_helpers.image_url(file_id, host: base_url, size: size)
   end
 
