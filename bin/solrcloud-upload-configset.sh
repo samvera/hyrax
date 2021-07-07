4967a87be96721b937582557680db58e3762c0f2
Matt Critchlow <mcritchlow@ucsd.edu>
2021-07-07 13:24:19 -0700

    Update solrcloud scripts variable interpolation
    The existing scripts, particularly for URLs such as
    solr_collection_create_url, solr_collection_modify_url, etc. are somehow
    getting null values of the environment variables even though they are
    defined.
    
    This changes the interpolation strategy and syntax a bit, and in local
    testing in both a raw Hyrax chart and our Comet application, I'm now
    about to get a passing `load-solr-config` `initContainer`.

--- bin/solrcloud-upload-configset.sh
+++ bin/solrcloud-upload-configset.sh
@@ -8,9 +8,11 @@ if [ "$SOLR_ADMIN_USER" ]; then
   solr_user_settings="--user $SOLR_ADMIN_USER:$SOLR_ADMIN_PASSWORD"
 fi
 
+solr_config_name="${SOLR_CONFIGSET_NAME:-solrconfig}"
+
 # Solr Cloud ConfigSet API URLs
 solr_config_list_url="http://$SOLR_HOST:$SOLR_PORT/api/cluster/configs?omitHeader=true"
-solr_config_upload_url="http://$SOLR_HOST:$SOLR_PORT/solr/admin/configs?action=UPLOAD&name=$SOLR_CONFIGSET_NAME"
+solr_config_upload_url="http://$SOLR_HOST:$SOLR_PORT/solr/admin/configs?action=UPLOAD&name=${solr_config_name}"
 
 while [ $COUNTER -lt 30 ]; do
   echo "-- Looking for Solr (${SOLR_HOST}:${SOLR_PORT})..."
@@ -19,7 +21,7 @@ while [ $COUNTER -lt 30 ]; do
     if curl --silent --user 'fake:fake' "$solr_config_list_url" | grep -q '401'; then
       # the solr pods come up and report available before they are ready to accept trusted configs
       # only try to upload the config if auth is on.
-      if curl --silent $solr_user_settings "$solr_config_list_url" | grep -q "$SOLR_CONFIGSET_NAME"; then
+      if curl --silent $solr_user_settings "$solr_config_list_url" | grep -q "$solr_config_name"; then
         echo "-- ConfigSet already exists; skipping creation ...";
       else
         echo "-- ConfigSet for ${CONFDIR} does not exist; creating ..."
