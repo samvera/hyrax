db9779b88859e3b1fd71c57dc667932b835af01a
Michael Johnson <apertome@gmail.com>
2021-07-15 20:33:23 -0400

    Implement creating basic works and collections using Valkyrie

--- .dassie/db/seeds.rb
+++ .dassie/db/seeds.rb
@@ -7,14 +7,45 @@
 # "embargo"
 # "lease"
 # "restricted"
+#
+# open
+# registered
+# restricted
 
 
-13.times do |i|
+
+
+0.times do |i|
 	title = "Work #{i}"
-	work = GenericWork.create(title:[title])
+    custom_id = "wwork_#{i}"
+    work = Monograph.new(id: custom_id)
+    work.title = title
     work.description = ["A description for Work #{i}"]
-    work.visibility = "open"
-    work.resource.creator = ["user1"]
-    work.save!
+    work.visibility = "restricted"
+    work.creator = ["user1"]
+    work.member_of_collection_ids = ["collection_2"]
+    work.rights_statement = ["http://rightsstatements.org/vocab/CNE/2.0/"]
+    Hyrax.persister.save(resource: work)
+
+
+end
+
+5.times do |i|
+    i += 100  # TODO: REMOVE
+	title = "Collection #{i}"
+    custom_id = "collection_#{i}"
+    collection = Hyrax::PcdmCollection.new(id: custom_id)
+    collection.title = title
+    #collection = Collection.new(title:[title])
+    collection.description = ["A description for Collection #{i}"]
+    collection.visibility="open"
+    # TODO: fix collection/work association
+    # collection.member_ids = ["wwork_#{i}"]
+    collection_type = "gid://dassie/Hyrax::CollectionType/9"
+    collection.collection_type_gid = collection_type
+    collection.creator = ["user1"]
+    pp collection
+    Hyrax.persister.save(resource: collection)
+
 
 end
