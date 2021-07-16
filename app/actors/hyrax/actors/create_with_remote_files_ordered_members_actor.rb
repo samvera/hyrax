3ffd335c30b40c7a77a42119b34354056a849647
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2021-07-16 13:20:25 -0400

    Extracting class as interstitial refactor
    I'm continuing to work on #4195.  This change extracts a class that can
    be called outside the ActorStack.  The goal being to leverage this
    class's logic to ingest remote URLs.
    
    This change looks to remove lots of repeated logic and instead provide a
    singular point of entry for associating a remote file with a work.

--- app/actors/hyrax/actors/create_with_remote_files_ordered_members_actor.rb
+++ app/actors/hyrax/actors/create_with_remote_files_ordered_members_actor.rb
@@ -39,54 +39,19 @@ module Hyrax
     # url property, it may have spaces, and not be a valid URI.
     class CreateWithRemoteFilesOrderedMembersActor < CreateWithRemoteFilesActor
       attr_reader :ordered_members
+      self.file_set_actor_class = Hyrax::Actors::FileSetOrderedMembersActor
 
       # @param [HashWithIndifferentAccess] remote_files
       # @return [TrueClass]
       def attach_files(env, remote_files)
-        return true unless remote_files
         @ordered_members = env.curation_concern.ordered_members.to_a
-        remote_files.each do |file_info|
-          next if file_info.blank? || file_info[:url].blank?
-          # Escape any space characters, so that this is a legal URI
-          uri = URI.parse(Addressable::URI.escape(file_info[:url]))
-          unless validate_remote_url(uri)
-            Rails.logger.error "User #{env.user.user_key} attempted to ingest file from url #{file_info[:url]}, which doesn't pass validation"
-            return false
-          end
-          auth_header = file_info.fetch(:auth_header, {})
-          create_file_from_url(env, uri, file_info[:file_name], auth_header)
-        end
-        add_ordered_members(env.user, env.curation_concern)
-        true
+        ingest_remote_files_service_class.new(user: env.user,
+                                              curation_concern: env.curation_concern,
+                                              remote_files: remote_files,
+                                              ordered_members: @ordered_members,
+                                              ordered: true,
+                                              file_set_actor_class: file_set_actor_class).attach!
       end
-
-      # Generic utility for creating FileSet from a URL
-      # Used in to import files using URLs from a file picker like browse_everything
-      def create_file_from_url(env, uri, file_name, auth_header = {})
-        ::FileSet.new(import_url: uri.to_s, label: file_name) do |fs|
-          actor = file_set_actor_class.new(fs, env.user)
-          actor.create_metadata(visibility: env.curation_concern.visibility)
-          actor.attach_to_work(env.curation_concern)
-          fs.save!
-          ordered_members << fs
-          if uri.scheme == 'file'
-            # Turn any %20 into spaces.
-            file_path = CGI.unescape(uri.path)
-            IngestLocalFileJob.perform_later(fs, file_path, env.user)
-          else
-            ImportUrlJob.perform_later(fs, operation_for(user: actor.user), auth_header)
-          end
-        end
-      end
-
-      # Add all file_sets as ordered_members in a single action
-      def add_ordered_members(user, work)
-        actor = Hyrax::Actors::OrderedMembersActor.new(ordered_members, user)
-        actor.attach_ordered_members_to_work(work)
-      end
-
-      class_attribute :file_set_actor_class
-      self.file_set_actor_class = Hyrax::Actors::FileSetOrderedMembersActor
     end
   end
 end
