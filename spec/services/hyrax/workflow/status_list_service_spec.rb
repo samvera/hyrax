3db76ee85efb0a35bc099e05699ed325ad1e726d
tamsin johnson <tomjohnson@ucsb.edu>
2021-07-22 14:12:56 -0700

    refactor status_list_service_spec to clarify setup needs
    preparing to add valkyrie tests (which we expect to fail initially). since the
    test setup is complicated, some effort was called for to clarify what's needed
    for all specs vs what supports more narrow stubs.

--- spec/services/hyrax/workflow/status_list_service_spec.rb
+++ spec/services/hyrax/workflow/status_list_service_spec.rb
@@ -1,10 +1,17 @@
 # frozen_string_literal: true
 RSpec.describe Hyrax::Workflow::StatusListService do
+  subject(:service) { described_class.new(context, "workflow_state_name_ssim:initial") }
+  let(:context) { double(current_user: user, logger: double(debug: nil)) }
+  let!(:sipity_entity) { FactoryBot.create(:sipity_entity) }
+  let(:user) { FactoryBot.create(:user) }
+
+  context 'using valkyrie models' do
+    let(:work) { FactoryBot.valkyrie_create(:hyrax_work) }
+
+    it 'without any roles is empty'
+  end
+
   describe "#each" do
-    let(:user) { create(:user) }
-    let(:context) { double(current_user: user, logger: double(debug: nil)) }
-    let(:service) { described_class.new(context, "workflow_state_name_ssim:initial") }
-    let!(:sipity_entity) { create(:sipity_entity) }
     let(:document) do
       { id: '33333',
         has_model_ssim: ['GenericWork'],
@@ -12,21 +19,21 @@ RSpec.describe Hyrax::Workflow::StatusListService do
         workflow_state_name_ssim: ["initial"],
         title_tesim: ['Hey dood!'] }
     end
-    let(:ability) do
+    let(:bad_document) do
       { id: '44444',
         has_model_ssim: ['GenericWork'],
         title_tesim: ['bad result'] }
     end
-    let(:workflow_role) { instance_double(Sipity::Role, name: 'approving') }
-    let(:workflow_roles) { [instance_double(Sipity::WorkflowRole, role: workflow_role)] }
-    let(:results) { service.each.to_a }
 
     before do
-      Hyrax::SolrService.add([document, ability], commit: true)
+      Hyrax::SolrService.add([document, bad_document], commit: true)
     end
 
     context "when user has roles" do
       let(:template) { double('template', source_id: 'foobar') }
+      let(:workflow_role) { instance_double(Sipity::Role, name: 'approving') }
+      let(:workflow_roles) { [instance_double(Sipity::WorkflowRole, role: workflow_role)] }
+
       let(:workflow) do
         instance_double(Sipity::Workflow, name: 'generic_work', permission_template: template)
       end
@@ -37,10 +44,10 @@ RSpec.describe Hyrax::Workflow::StatusListService do
       end
 
       it "returns status rows" do
-        expect(results.size).to eq 1
-        expect(results.first).to be_kind_of(SolrDocument)
-        expect(results.first.to_s).to eq 'Hey dood!'
-        expect(results.first.workflow_state).to eq 'initial'
+        expect(service.each.count).to eq 1
+        expect(service.each.first).to be_kind_of(SolrDocument)
+        expect(service.each.first.to_s).to eq 'Hey dood!'
+        expect(service.each.first.workflow_state).to eq 'initial'
       end
     end
 
@@ -48,8 +55,9 @@ RSpec.describe Hyrax::Workflow::StatusListService do
       before do
         allow(Hyrax::Workflow::PermissionQuery).to receive(:scope_processing_workflow_roles_for_user_and_workflow).and_return([])
       end
+
       it "returns nothing" do
-        expect(results).to be_empty
+        expect(service.each).to be_none
       end
     end
   end
