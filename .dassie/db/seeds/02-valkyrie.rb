e2d628967e58812f64b6e96179a6ab7746dff77d
Michael Johnson <apertome@gmail.com>
2021-07-27 16:23:22 -0400

    Set works/collections to public

--- .dassie/db/seeds/02-valkyrie.rb
+++ .dassie/db/seeds/02-valkyrie.rb
@@ -25,7 +25,12 @@ def create_resource(user, id, resource)
   resource
 end
 
-#default_admin_set_id = "admin_set%2Fdefault"
+def set_visibility(resource, visibility)
+  writer   = Hyrax::VisibilityWriter.new(resource: resource)
+  writer.assign_access_for(visibility: visibility)
+  writer.permission_manager.acl.save
+end
+
 default_admin_set_id = AdminSet.find_or_create_default_admin_set_id
 puts "Default admin set id: #{default_admin_set_id}"
 default_admin_user = "admin@example.com"
@@ -37,41 +42,39 @@ last_collection_id=nil
 	title = "Collection #{i}"
     custom_id = "collection_#{i}"
     collection = Hyrax::PcdmCollection.new(id: custom_id)
-    last_collection_id=custom_id # if last_collection_id.nil?
+    last_collection_id=custom_id
     collection.title = title
-    #collection = Collection.new(title:[title])
     collection.description = ["A description for Collection #{i}"]
-    collection.visibility="open"
-    # TODO: fix collection/work association
-    # collection.member_ids = ["work_#{i}"]
     collection_type = "gid://dassie/Hyrax::CollectionType/1"
     collection.collection_type_gid = collection_type
     collection.creator = [default_user]
-    #pp collection
-    #
+
     if i == 2 then
       collection.member_ids=["collection_1"]
-      #collection.member_of_collection_ids=["collection_1"]
     end
+
     create_resource(default_user, custom_id, collection)
+
+    # This should work, but it doesn't?
+    set_visibility( collection, "open" )
 end
 
 13.times do |i|
 	title = "Work #{i}"
     custom_id = "work_#{i}"
-    #work = Monograph.new(id: custom_id)
     afWork = GenericWork.new(title: [title], id: custom_id)
     work = afWork.valkyrie_resource
     work.title = title
     work.description = ["A description for Work #{i}"]
-    work.visibility = "restricted"
+    work.visibility = "open"
     work.creator = [default_user]
     work.depositor = [default_user]
     work.admin_set_id = default_admin_set_id
-    existing_member_ids = work.member_of_collection_ids
     work.member_of_collection_ids = [last_collection_id] if !last_collection_id.nil?
     work.rights_statement = ["http://rightsstatements.org/vocab/CNE/2.0/"]
     create_resource(default_user, custom_id, work)
+    # This should work, but it doesn't?
+    set_visibility( work, "open" )
 
 end
 
