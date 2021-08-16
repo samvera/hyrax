f118c2919317de4fadcc5dbb2f0a13df2758847b
E. Lynette Rayle <elr37@cornell.edu>
2021-08-16 17:04:07 -0400

    add parent collections navigator
    This follows the established navigator pattern.  It will be used as part of the process for replacing uses of ActiveFedora relationship `member_of_collections`.

---
+++ spec/services/hyrax/custom_queries/navigators/parent_collections_navigator_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+RSpec.describe Hyrax::CustomQueries::Navigators::ParentCollectionsNavigator, :clean_repo do
+  let(:custom_query_service) { Hyrax.custom_queries }
+
+  let!(:collection1) do
+    FactoryBot.valkyrie_create(:hyrax_collection, id: 'col1',
+                                                  title: ['Parent Collection 1'],
+                                                  members: [collection3, work1, work2])
+  end
+  let!(:collection2) do
+    FactoryBot.valkyrie_create(:hyrax_collection, id: 'col2',
+                                                  title: ['Parent Collection 1'],
+                                                  members: [collection3, work1, work2])
+  end
+  let(:collection3) { FactoryBot.valkyrie_create(:hyrax_collection, id: 'col3', title: ['Child Collection 3']) }
+  let(:work1) { FactoryBot.valkyrie_create(:hyrax_work, id: 'wk1', title: ['Child Work 1']) }
+  let(:work2) { FactoryBot.valkyrie_create(:hyrax_work, id: 'wk2', title: ['Child Work 2']) }
+
+  describe '#find_parent_collections' do
+    it 'returns parent collections as Valkyrie resources' do
+      parent_collections = custom_query_service.find_parent_collections(resource: collection3)
+      expect(parent_collections.map(&:id)).to match_array([collection1.id, collection2.id])
+    end
+  end
+
+  describe '#find_parent_collection_ids' do
+    it 'returns Valkyrie ids for parent collections' do
+      parent_collection_ids = custom_query_service.find_parent_collection_ids(resource: work1)
+      expect(parent_collection_ids).to match_array([collection1.id, collection2.id])
+    end
+  end
+end
