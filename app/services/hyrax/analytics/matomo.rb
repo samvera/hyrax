4dd0d23c059d17adbbc0bb28aa4001872e64591d
Alisha Evans <alisha@notch8.com>
2021-08-18 09:57:52 -0700

    refactored matomo.rb

--- app/services/hyrax/analytics/matomo.rb
+++ app/services/hyrax/analytics/matomo.rb
@@ -42,7 +42,7 @@ module Hyrax
           end
 
           REQUIRED_KEYS.each do |key|
-            class_eval %{ def #{key};  @config.fetch('#{key}'); end }
+            class_eval %{ def #{key}; @config.fetch('#{key}'); end }
           end
         end
 
@@ -52,27 +52,30 @@ module Hyrax
         # Example: Last 6 weeks: period: week, date: last6
 
         def downloads(ref='all', date = 'last500')
-          if ref == 'all' 
+          if ref == 'all'
             segment = 'eventCategory==Files;eventAction==Downloaded'
-            additional_params = {segment: segment}
+            additional_params = { segment: segment }
             response = api_params('Events.getName', 'day', date, additional_params)
+
             results_array(response, 'nb_events')
           else
             send(:downloads_filtered, ref, date)
-          end 
+          end
         end
 
         def downloads_filtered(ref, date)
           segment = "eventCategory==#{ref.titleize};eventAction==Downloads"
-          additional_params = {segment: segment}
+          additional_params = { segment: segment }
           response = api_params('Events.getName', 'day', date, additional_params)
+
           results_array(response, 'nb_events')
         end
 
         def top_downloads(ref='all', date = 'last500')
           if ref == 'all'
-            additional_params = {segment: "eventCategory==Files;eventAction==Downloaded"}
+            additional_params = { segment: "eventCategory==Files;eventAction==Downloaded" }
             response = api_params('Events.getName', 'range', date, additional_params)
+
             results_array_with_ids(response, 'nb_events')
           else
             send(:top_downloads_filtered, ref, date)
@@ -80,66 +83,74 @@ module Hyrax
         end
 
         def top_downloads_filtered(ref, date)
-          additional_params = {segment: "eventCategory==#{ref.titleize};eventAction==Downloads"}
+          additional_params = { segment: "eventCategory==#{ref.titleize};eventAction==Downloads" }
           response = api_params('Events.getName', 'range', date, additional_params)
+
           results_array_with_ids(response, 'nb_events')
         end
-        
+
         def downloads_for_file(file, period = 'range', date = "last12")
-          additional_params = {segment: "eventName==#{file}"}
+          additional_params = { segment: "eventName==#{file}" }
           response = api_params('Events.getName', period, date, additional_params)
+
           response.count.zero? ? 0 : response.first['nb_events'].to_i
         end
 
         def top_pages(ref="collections", date='last12')
           segment = "eventCategory==#{ref.titleize};eventAction==Views"
-          additional_params = {segment: segment}
+          additional_params = { segment: segment }
           response = api_params('Events.getName', 'range', date, additional_params)
+
           results_array_with_ids(response, 'nb_events')
         end
 
         def pageviews(ref='all', date='last500')
-          if ref == 'all' 
-            response = api_params('Actions.get', 'day', date, additional_params={})
+          if ref == 'all'
+            response = api_params('Actions.get', 'day', date)
             results_array(response, 'nb_pageviews')
           else
             send(:pageviews_filtered, ref, date)
-          end 
+          end
         end
 
-         def pageviews_filtered(ref, date)
-          additional_params = {label: ref}
+        def pageviews_filtered(ref, date)
+          additional_params = { label: ref }
           response = api_params('Actions.getPageUrls', 'day', date, additional_params)
+
           results_array(response, 'nb_hits')
         end
 
         def pageviews_for_url(url, date = 'last500')
-          response = api_params('Actions.getPageUrl', 'day', date, additional_params={}, url)
+          additional_params = { pageUrl: url }
+          response = api_params('Actions.getPageUrl', 'day', date, additional_params)
+
           results_array(response, 'nb_hits')
         end
-        
+
         def unique_visitors(date='last500')
-          response = api_params('Actions.get', 'day', date, additional_params={})
+          response = api_params('Actions.get', 'day', date)
           results_array(response, 'nb_uniq_pageviews')
         end
 
         def unique_visitors_for_url(url, date = 'last500')
-          response = api_params('Actions.getPageUrl', 'day', date, additional_params={}, url)
+          additional_params = { pageUrl: url }
+          response = api_params('Actions.getPageUrl', 'day', date, additional_params)
+
           results_array(response, 'nb_uniq_visitors')
         end
 
-        def new_visitors(period = 'month', date = 'today') 
-          response = api_params('VisitFrequency.get', period, date, additional_params={})
+        def new_visitors(period = 'month', date = 'today')
+          response = api_params('VisitFrequency.get', period, date)
           response["nb_visits_new"]
         end
 
         def returning_visitors(period = 'month', date = 'today')
-          response = api_params('VisitFrequency.get', period, date, additional_params={})
+          response = api_params('VisitFrequency.get', period, date)
           response["nb_visits_returning"]
         end
 
         def total_visitors(period = 'month', date = 'today')
-          response = api_params('VisitFrequency.get', period, date, additional_params={})
+          response = api_params('VisitFrequency.get', period, date)
           response["nb_visits_returning"].to_i + response["nb_visits_new"].to_i
         end
 
@@ -147,13 +158,11 @@ module Hyrax
           results = []
           response.each do |result|
             if result[1].empty?
-              results.push([result[0].to_date,0]) 
+              results.push([result[0].to_date, 0])
+            elsif result[1].is_a?(Array)
+              results.push([result[0].to_date, result[1].first[metric]])
             else
-              if result[1].kind_of?(Array)
-               results.push([result[0].to_date,result[1].first[metric]]) 
-              else
-               results.push([result[0].to_date,result[1][metric]]) 
-              end
+              results.push([result[0].to_date, result[1][metric]])
             end
           end
           Hyrax::Analytics::Results.new(results)
@@ -161,23 +170,23 @@ module Hyrax
 
         def results_array_with_ids(response, metric)
           results = []
-          response.each do |result| 
+          response.each do |result|
             results.push([result['label'], result[metric]])
           end
-          return results.sort_by { |el| -el[1] }
-        end 
+
+          results.sort_by { |el| -el[1] }
+        end
 
         def get(params)
           response = Faraday.get(config.base_url, params)
           return [] if response.status != 200
-          JSON.parse(response.body)
-        end    
 
-        def api_params(method, period, date, additional_params, url=nil)
-        # def api_params(method, period, date, label = nil, url = nil, segment = nil)
+          JSON.parse(response.body)
+        end
+
+        def api_params(method, period, date, additional_params = {})
           params = {
             module: "API",
-            pageUrl: url,
             idSite: config.site_id,
             method: method,
             period: period,
@@ -185,9 +194,8 @@ module Hyrax
             format: "JSON",
             token_auth: config.auth_token
           }
-          if additional_params
-            params.merge!(additional_params)
-          end
+
+          params.merge!(additional_params)
           get(params)
         end
       end
