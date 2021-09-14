198b7e232d65dd6fe04dae4cd67d755825fd12e9
tamsin johnson <tomjohnson@ucsb.edu>
2021-09-14 13:51:01 -0700

    index Hyrax::PcdmCollection with compatibility with Collection
    even though `generic_type_sim` exists, hyrax often queries `has_model_ssim`. to
    find valkyrie generated collections in this case, we need to index as
    `'Collection'`.

--- app/indexers/hyrax/pcdm_collection_indexer.rb
+++ app/indexers/hyrax/pcdm_collection_indexer.rb
@@ -13,6 +13,7 @@ module Hyrax
     def to_solr
       super.tap do |index_document|
         index_document[Hyrax.config.collection_type_index_field.to_sym] = Array(resource.try(:collection_type_gid)&.to_s)
+        index_document[:has_model_ssim] = 'Collection'
         index_document[:generic_type_sim] = ['Collection']
         index_document[:thumbnail_path_ss] = Hyrax::CollectionThumbnailPathService.call(resource)
       end
