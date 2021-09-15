caab92bfa2eb5fd21d0f7176fb6783061fe521a4
tamsin johnson <tomjohnson@ucsb.edu>
2021-09-15 15:53:19 -0700

    document WorkflowActionInfo
    just some docs to make this easier to understand for the next person.

--- app/models/hyrax/workflow_action_info.rb
+++ app/models/hyrax/workflow_action_info.rb
@@ -1,7 +1,23 @@
 # frozen_string_literal: true
 module Hyrax
+  ##
   # A simple data object for holding a user, work and their workflow proxies
+  #
+  # this is a glorified Struct that resolves +Sipity::Enitity(work)+
+  # and +Sipity::Agent(user)+ for the input given, then provides readers
+  # for its instance variables.
+  #
+  # @example
+  #   info = WorkflowActionInfo.new(my_work, current_user)
+  #
+  #   info.agent # =>  #<Sipity::Agent...>
+  #   info.entity # =>  #<Sipity::Entity...>
+  #   info.work == my_work # => true
+  #   info.user == current_user # => true
   class WorkflowActionInfo
+    ##
+    # @param work [Object]
+    # @param user [::User]
     def initialize(work, user)
       @work = work
       @user = user
