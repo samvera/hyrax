70aacc03becefff5ec24e03887891542e68fa8d0
tamsin johnson <tomjohnson@ucsb.edu>
2021-09-20 16:39:20 -0700

    Add valkyrie support to Hyrax::Workflow::RevokeEditFromDepositor
    following the pattern in GrantEditToDepositor, add valkyrie support to
    RevokeEditFromDepositor.

--- spec/services/hyrax/workflow/revoke_edit_from_depositor_spec.rb
+++ spec/services/hyrax/workflow/revoke_edit_from_depositor_spec.rb
@@ -2,47 +2,58 @@
 require 'hyrax/specs/shared_specs'
 
 RSpec.describe Hyrax::Workflow::RevokeEditFromDepositor do
-  let(:depositor) { create(:user) }
+  subject(:workflow_method) { described_class }
+  let(:depositor) { FactoryBot.create(:user) }
+  let(:editors) { [depositor.user_key] }
   let(:user) { User.new }
 
-  let(:workflow_method) { described_class }
+  let(:work) do
+    FactoryBot.valkyrie_create(:monograph,
+                               depositor: depositor.user_key,
+                               edit_users: editors)
+  end
 
   it_behaves_like 'a Hyrax workflow method'
 
   describe ".call" do
-    subject do
-      described_class.call(target: work,
-                           comment: "A pleasant read",
-                           user: user)
-    end
-
     context "with no additional editors" do
-      let(:work) { create(:work_without_access, depositor: depositor.user_key, edit_users: [depositor.user_key]) }
-
       it "removes edit access" do
-        expect { subject }.to change { work.edit_users }.from([depositor.user_key]).to([])
-        expect(work).to be_valid
+        expect { described_class.call(target: work, comment: "A pleasant read", user: user) }
+          .to change { Hyrax::PermissionManager.new(resource: work).edit_users }
+          .from(contain_exactly(depositor.user_key))
+          .to be_none
       end
     end
 
     context "with an additional editor" do
-      let(:editor) { create(:user) }
-      let(:work) { create(:work_without_access, depositor: depositor.user_key, edit_users: [depositor.user_key, editor.user_key]) }
+      let(:editor) { FactoryBot.create(:user) }
+      let(:editors) { [depositor, editor].map(&:user_key) }
 
       it "removes edit access" do
-        expect { subject }.to change { work.edit_users }.from([depositor.user_key, editor.user_key]).to([editor.user_key])
-        expect(work).to be_valid
+        expect { described_class.call(target: work, comment: "A pleasant read", user: user) }
+          .to change { Hyrax::PermissionManager.new(resource: work).edit_users }
+          .from(contain_exactly(*editors))
+          .to contain_exactly(editor.user_key)
       end
     end
 
     context "with attached FileSets", perform_enqueued: [Hyrax::RevokeEditFromMembersJob] do
-      let(:work) { create(:work_with_one_file, user: depositor) }
-      let(:file_set) { work.members.first }
+      let(:work) do
+        FactoryBot.valkyrie_create(:monograph,
+                                   members: [file_set],
+                                   depositor: depositor.user_key)
+      end
+
+      let(:file_set) do
+        FactoryBot
+          .valkyrie_create(:hyrax_file_set, edit_users: [depositor.user_key])
+      end
 
       it "removes edit access" do
-        # We need to reload, because this work happens in a background job
-        expect { subject }.to change { file_set.reload.edit_users }.from([depositor.user_key]).to([])
-        expect(work).to be_valid
+        expect { described_class.call(target: work, comment: "A pleasant read", user: user) }
+          .to change { Hyrax::PermissionManager.new(resource: file_set).edit_users }
+          .from(contain_exactly(depositor.user_key))
+          .to be_none
       end
     end
   end
