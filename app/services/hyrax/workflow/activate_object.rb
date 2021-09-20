4ab99f3bcfcc800d2af92f8bda418411462a6172
tamsin johnson <tomjohnson@ucsb.edu>
2021-09-20 12:28:42 -0700

    check in on Hyrax::Workflow::ActivateObject for valkyrie
    spec and doc updates for Valkyrie support in this workflow action module.

--- app/services/hyrax/workflow/activate_object.rb
+++ app/services/hyrax/workflow/activate_object.rb
@@ -3,14 +3,15 @@ module Hyrax
   module Workflow
     module ActivateObject
       ##
-      # This is a built in function for workflow, setting the `#state`
-      # of the target to the Fedora 'active' status URI
+      # This is a built in function for workflow, setting the +#state+
+      # of the target to the Fedora +active+ status URI
       #
-      # @param target [#state] an instance of a model that includes `Hyrax::Suppressible`
+      # @param target [#state] an instance of a model with a +#state+ property;
+      #   e.g. a {Hyrax::Work}
       #
       # @return [RDF::Vocabulary::Term] the Fedora Resource Status 'active' term
       def self.call(target:, **)
-        target.state = Vocab::FedoraResourceStatus.active
+        target.state = Hyrax::ResourceStatus::ACTIVE
       end
     end
   end
