abeb359b857d4827e1041a130cbbbcc72763ac9b
tamsin johnson <tomjohnson@ucsb.edu>
2021-09-20 12:18:07 -0700

    refactor specs for ActionTakenService to use Valkyrie

--- spec/services/hyrax/workflow/action_taken_service_spec.rb
+++ spec/services/hyrax/workflow/action_taken_service_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Hyrax::Workflow::ActionTakenService do
            order: triggered_methods,
            any?: true)
   end
-  let(:work) { FactoryBot.create(:work) }
+  let(:work) { FactoryBot.build(:monograph) }
   let(:action) { instance_double(Sipity::WorkflowAction, triggered_methods: triggered_methods_rel) }
   let(:user) { User.new }
   let(:instance) do
@@ -16,37 +16,47 @@ RSpec.describe Hyrax::Workflow::ActionTakenService do
                         user: user)
   end
 
-  context 'class methods' do
-    subject { described_class }
-
-    it { is_expected.to respond_to(:handle_action_taken) }
+  describe '.handle_action_token' do
+    it { expect(described_class).to respond_to(:handle_action_taken) }
   end
 
   describe "#call" do
-    subject { instance.call }
-
     context "when the method exists" do
       around do |example|
         class FooBar
           def self.call; end
         end
+
         example.run
+
         Object.send(:remove_const, :FooBar)
       end
 
       context "and the method succeedes" do
         it "calls the method and saves the object" do
-          expect(work).to receive(:save)
-          expect(FooBar).to receive(:call).with(target: work, user: user, comment: "A pleasant read").and_return(true)
-          subject
+          expect(FooBar)
+            .to receive(:call)
+            .with(target: work, user: user, comment: "A pleasant read")
+            .and_return(true)
+
+          expect { instance.call }
+            .to change { Hyrax.query_service.count_all_of_model(model: work.class) }
+            .by 1
         end
       end
+
       context "and the method fails" do
-        it "calls the method and saves the object" do
-          expect(work).not_to receive(:save)
-          expect(FooBar).to receive(:call).with(target: work, user: user, comment: "A pleasant read").and_return(false)
-          expect(Rails.logger).to receive(:error).with("Not all workflow methods were successful, so not saving (#{work.id})")
-          subject
+        it "calls the method and does not save the object" do
+          expect(FooBar)
+            .to receive(:call)
+            .with(target: work, user: user, comment: "A pleasant read")
+            .and_return(false)
+          expect(Rails.logger)
+            .to receive(:error)
+            .with("Not all workflow methods were successful, so not saving (#{work.id})")
+
+          expect { instance.call }
+            .not_to change { Hyrax.query_service.count_all_of_model(model: work.class) }
         end
       end
     end
@@ -57,18 +67,29 @@ RSpec.describe Hyrax::Workflow::ActionTakenService do
         example.run
         Object.send(:remove_const, :FooBar)
       end
+
       it "logs an error" do
-        expect(Rails.logger).to receive(:error).with("Expected 'FooBar' to respond to 'call', but it didn't, so not running workflow callback")
-        expect(Rails.logger).to receive(:error).with("Not all workflow methods were successful, so not saving (#{work.id})")
-        subject
+        expect(Rails.logger)
+          .to receive(:error)
+          .with("Expected 'FooBar' to respond to 'call', but it didn't, so not running workflow callback")
+        expect(Rails.logger)
+          .to receive(:error)
+          .with("Not all workflow methods were successful, so not saving (#{work.id})")
+
+        instance.call
       end
     end
 
     context "when the notification doesn't exist" do
       it "logs an error" do
-        expect(Rails.logger).to receive(:error).with("Unable to find 'FooBar', so not running workflow callback")
-        expect(Rails.logger).to receive(:error).with("Not all workflow methods were successful, so not saving (#{work.id})")
-        subject
+        expect(Rails.logger)
+          .to receive(:error)
+          .with("Unable to find 'FooBar', so not running workflow callback")
+        expect(Rails.logger)
+          .to receive(:error)
+          .with("Not all workflow methods were successful, so not saving (#{work.id})")
+
+        instance.call
       end
     end
   end
