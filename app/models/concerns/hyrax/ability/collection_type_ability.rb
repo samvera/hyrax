100e07d09f488c389d3d12a02842a08e0652f35a
E. Lynette Rayle <elr37@cornell.edu>
2021-09-24 16:06:04 -0400

    allow collection_type_ability to pass ability to PermissionsService

--- app/models/concerns/hyrax/ability/collection_type_ability.rb
+++ app/models/concerns/hyrax/ability/collection_type_ability.rb
@@ -8,7 +8,7 @@ module Hyrax
           can :create_collection_type, CollectionType
         else
           can :create_collection_of_type, CollectionType do |collection_type|
-            Hyrax::CollectionTypes::PermissionsService.can_create_collection_of_type?(user: current_user, collection_type: collection_type)
+            Hyrax::CollectionTypes::PermissionsService.can_create_collection_of_type?(ability: self, collection_type: collection_type)
           end
         end
       end
