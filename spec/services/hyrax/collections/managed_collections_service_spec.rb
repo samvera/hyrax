1c4ecad269e958cb271cb93bbad33b48479dc11f
tamsin johnson <tomjohnson@ucsb.edu>
2021-10-08 17:13:39 -0700

    change ManagedCollectionsServiceSpec to use valkyrie models in setup
    setup these tests with valkyrie.

--- spec/services/hyrax/collections/managed_collections_service_spec.rb
+++ spec/services/hyrax/collections/managed_collections_service_spec.rb
@@ -4,7 +4,9 @@ RSpec.describe Hyrax::Collections::ManagedCollectionsService, clean_repo: true d
   let(:scope) { FakeSearchBuilderScope.new(current_ability: current_ability) }
 
   describe '.managed_collections_count' do
-    let!(:collection) { FactoryBot.create(:public_collection) }
+    before do
+      FactoryBot.valkyrie_create(:hyrax_collection, :public)
+    end
 
     it 'returns number of collections that can be managed' do
       expect(described_class.managed_collections_count(scope: scope)).to eq(1)
