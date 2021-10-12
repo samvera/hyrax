50213ff30cf921b5a5e8d80b288bca097dbf8f8b
tamsin johnson <tomjohnson@ucsb.edu>
2021-10-12 03:11:12 -0700

    don't override a ChangeSet's existing data with a generic default
    if a ChangeSet has a `#collection_type_gid` value, never override it with the
    default.

--- spec/hyrax/transactions/steps/set_collection_type_gid_spec.rb
+++ spec/hyrax/transactions/steps/set_collection_type_gid_spec.rb
@@ -4,11 +4,15 @@ require 'hyrax/transactions'
 
 RSpec.describe Hyrax::Transactions::Steps::SetCollectionTypeGid do
   subject(:step)   { described_class.new }
-  let(:collection) { build(:hyrax_collection) }
+  let(:collection) { FactoryBot.build(:hyrax_collection, collection_type_gid: nil) }
   let(:change_set) { Hyrax::ChangeSet.for(collection) }
 
   describe '#call' do
-    let(:default_collection_type_gid) { Hyrax::CollectionType.find_or_create_default_collection_type.to_global_id }
+    let(:default_collection_type_gid) do
+      Hyrax::CollectionType
+        .find_or_create_default_collection_type
+        .to_global_id
+    end
 
     it 'is success' do
       expect(step.call(change_set)).to be_success
@@ -17,24 +21,32 @@ RSpec.describe Hyrax::Transactions::Steps::SetCollectionTypeGid do
     context 'when a collection type gid is NOT passed in' do
       it 'sets the default collection type gid' do
         expect { step.call(change_set) }
-          .not_to change { change_set.collection_type_gid }
-          .from(default_collection_type_gid) # The default will always be assigned if one isn't give at create time.
+          .to change { change_set.collection_type_gid }
+          .to(default_collection_type_gid)
       end
     end
 
     context 'when a collection type gid is passed in' do
-      let(:collection_type) { create(:collection_type) }
+      let(:collection_type) { FactoryBot.create(:collection_type) }
       let(:collection_type_gid) { collection_type.to_global_id.to_s }
 
       it 'is success' do
-        expect(step.call(change_set, collection_type_gid: collection_type_gid)).to be_success
+        expect(step.call(change_set, collection_type_gid: collection_type_gid))
+          .to be_success
       end
 
       it 'sets the collection type gid' do
         expect { step.call(change_set, collection_type_gid: collection_type_gid) }
           .to change { change_set.collection_type_gid }
-          .from(default_collection_type_gid)
           .to(collection_type_gid)
+      end
+
+      it 'does not override an existing collection type' do
+        change_set.collection_type_gid = 'existing'
+
+        expect { step.call(change_set, collection_type_gid: collection_type_gid) }
+          .not_to change { change_set.collection_type_gid }
+          .from('existing')
       end
     end
   end
