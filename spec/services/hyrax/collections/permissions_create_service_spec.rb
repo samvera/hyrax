8b444a0c7ec57ac7d803320c53b74a7d035bc55f
tamsin johnson <tomjohnson@ucsb.edu>
2021-10-13 12:49:25 -0700

    simplify setup and clarify expectations for PermissionsCreateService
    these specs stubbed ActiveFedora in unnecessary places, and just generally had
    convoluted setup and expectations (one test memoized an empty array, used a
    before block to populate it, and then checked its contents as the only
    expectation!).
    
    don't exercise code under test in `subject` or `before`.

--- spec/services/hyrax/collections/permissions_create_service_spec.rb
+++ spec/services/hyrax/collections/permissions_create_service_spec.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
 RSpec.describe Hyrax::Collections::PermissionsCreateService do
-  let(:user) { create(:user) }
-  let(:user2) { create(:user) }
+  let(:user) { FactoryBot.create(:user) }
+  let(:user2) { FactoryBot.create(:user) }
 
   describe ".create_default" do
-    subject { described_class.create_default(collection: collection, creating_user: user) }
+    let(:collection_type) { FactoryBot.create(:collection_type) }
 
-    let(:collection_type) { create(:collection_type) }
     let(:user_manage_attributes) do
       {
         hyrax_collection_type_id: collection_type.id,
@@ -15,6 +14,7 @@ RSpec.describe Hyrax::Collections::PermissionsCreateService do
         agent_type: 'user'
       }
     end
+
     let(:group_manage_attributes) do
       {
         hyrax_collection_type_id: collection_type.id,
@@ -23,42 +23,45 @@ RSpec.describe Hyrax::Collections::PermissionsCreateService do
         agent_type: 'group'
       }
     end
-    let!(:collection_type_participant) { create(:collection_type_participant, user_manage_attributes) }
-    let!(:collection_type_participant2) { create(:collection_type_participant, group_manage_attributes) }
-    let(:collection) { FactoryBot.build(:collection_lw, id: 'collection1', collection_type: collection_type) }
+
+    let(:collection) do
+      FactoryBot.build(:collection_lw,
+                       id: 'collection1',
+                       collection_type: collection_type)
+    end
 
     before do
-      subject
+      FactoryBot.create(:collection_type_participant, user_manage_attributes)
+      FactoryBot.create(:collection_type_participant, group_manage_attributes)
     end
 
     it "creates the default permission template for the collection" do
+      described_class.create_default(collection: collection, creating_user: user)
+
       expect(Hyrax::PermissionTemplate.find_by_source_id(collection.id)).to be_persisted
     end
 
     it "creates the default permission template access entries for the collection" do
+      described_class.create_default(collection: collection, creating_user: user)
+
       expect(Hyrax::PermissionTemplate.find_by_source_id(collection.id).access_grants.count).to eq 4
     end
   end
 
   describe ".add_access" do
-    subject { described_class.add_access(collection_id: collection.id, grants: grants) }
     let(:collection) { FactoryBot.create(:collection_lw, with_permission_template: true) }
+
     let(:grants) do
       [{ agent_type: Hyrax::PermissionTemplateAccess::GROUP,
          agent_id: 'archivist',
          access: Hyrax::PermissionTemplateAccess::DEPOSIT }]
     end
-    let(:depositor_grants) { collection.permission_template.access_grants.deposit }
-    let(:array) { [] }
-
-    before do
-      allow(ActiveFedora::Base).to receive(:find).with(collection.id).and_return(collection)
-      subject
-      depositor_grants.each { |agent| array << agent.agent_id }
-    end
 
     it 'gives deposit access to archivist group' do
-      expect(array.include?("archivist")).to eq true
+      described_class.add_access(collection_id: collection.id, grants: grants)
+
+      expect(collection.permission_template.access_grants.deposit.map(&:agent_id))
+        .to include 'archivist'
     end
   end
 end
