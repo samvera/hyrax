a3a5d890b020ca12425290c5ea0f45b656273846
E. Lynette Rayle <elr37@cornell.edu>
2021-10-16 09:13:31 -0400

    update Collection::PermissionCreateService to handle collections and admin sets
    This service is flexible enough to handle both valkyrie and active fedora collections and administrative sets.
    
    NOTE: Most of the changes in this file is are related to indentation changes for the refactor to use `class << self` since all methods are class methods.
    
    The only substantive change is the way that `collection_type` is set in `#create_default`
    
    One other change is that `#create_default` said it returns a PermissionTemplate, but it was returning the results of `#reset_access_controls_for` which returns a `Boolean`.  Updated the method to actually return the permission template.

--- app/services/hyrax/collections/permissions_create_service.rb
+++ app/services/hyrax/collections/permissions_create_service.rb
@@ -2,94 +2,110 @@
 module Hyrax
   module Collections
     class PermissionsCreateService
-      # @api public
-      #
-      # Set the default permissions for a (newly created) collection
-      #
-      # @param collection [::Collection] the collection the new permissions will act on
-      # @param creating_user [User] the user that created the collection
-      # @param grants [Array<Hash>] additional grants to apply to the new collection
-      # @return [Hyrax::PermissionTemplate]
-      def self.create_default(collection:, creating_user:, grants: [])
-        collection_type = Hyrax::CollectionType.find_by_gid!(collection.collection_type_gid)
-        access_grants = access_grants_attributes(collection_type: collection_type, creating_user: creating_user, grants: grants)
-        template = PermissionTemplate.create!(source_id: collection.id,
-                                              access_grants_attributes: access_grants.uniq)
+      class << self
+        # @api public
+        #
+        # Set the default permissions for a (newly created) collection
+        #
+        # @param collection [#collection_type_gid || Hyrax::AdministrativeSet] the collection or admin set the new permissions will act on
+        # @param creating_user [User] the user that created the collection
+        # @param grants [Array<Hash>] additional grants to apply to the new collection
+        # @return [Hyrax::PermissionTemplate]
+        def create_default(collection:, creating_user:, grants: [])
+          collection_type = collection_type(collection: collection)
+          access_grants = access_grants_attributes(collection_type: collection_type, creating_user: creating_user, grants: grants)
+          template = PermissionTemplate.create!(source_id: collection.id.to_s,
+                                                access_grants_attributes: access_grants.uniq)
 
-        template.reset_access_controls_for(collection: collection, interpret_visibility: true)
-      end
-
-      # @api public
-      #
-      # Add access grants to a collection
-      #
-      # @param collection_id [String] id of a collection
-      # @param grants [Array<Hash>] array of grants to add to the collection
-      # @example grants
-      #   [ { agent_type: Hyrax::PermissionTemplateAccess::GROUP,
-      #       agent_id: 'my_group_name',
-      #       access: Hyrax::PermissionTemplateAccess::DEPOSIT } ]
-      # @see Hyrax::PermissionTemplateAccess for valid values for agent_type and access
-      def self.add_access(collection_id:, grants:)
-        collection = Hyrax.query_service.find_by(id: collection_id)
-        template = Hyrax::PermissionTemplate.find_by!(source_id: collection_id)
-        grants.each do |grant|
-          Hyrax::PermissionTemplateAccess.find_or_create_by(permission_template_id: template.id,
-                                                            agent_type: grant[:agent_type],
-                                                            agent_id: grant[:agent_id],
-                                                            access: grant[:access])
+          template.reset_access_controls_for(collection: collection, interpret_visibility: true)
+          template
         end
 
-        template.reset_access_controls_for(collection: collection, interpret_visibility: true)
-      end
+        # @api public
+        #
+        # Add access grants to a collection
+        #
+        # @param collection_id [String] id of a collection
+        # @param grants [Array<Hash>] array of grants to add to the collection
+        # @example grants
+        #   [ { agent_type: Hyrax::PermissionTemplateAccess::GROUP,
+        #       agent_id: 'my_group_name',
+        #       access: Hyrax::PermissionTemplateAccess::DEPOSIT } ]
+        # @see Hyrax::PermissionTemplateAccess for valid values for agent_type and access
+        def add_access(collection_id:, grants:)
+          collection = Hyrax.query_service.find_by(id: collection_id)
+          template = Hyrax::PermissionTemplate.find_by!(source_id: collection_id.to_s)
+          grants.each do |grant|
+            Hyrax::PermissionTemplateAccess.find_or_create_by(permission_template_id: template.id.to_s,
+                                                              agent_type: grant[:agent_type],
+                                                              agent_id: grant[:agent_id],
+                                                              access: grant[:access])
+          end
 
-      # @api private
-      #
-      # Gather the default permissions needed for a new collection
-      #
-      # @param collection_type [CollectionType] the collection type of the new collection
-      # @param creating_user [User] the user that created the collection
-      # @param grants [Array<Hash>] additional grants to apply to the new collection
-      # @return [Hash] a hash containing permission attributes
-      def self.access_grants_attributes(collection_type:, creating_user:, grants:)
-        [
-          { agent_type: 'group', agent_id: admin_group_name, access: Hyrax::PermissionTemplateAccess::MANAGE }
-        ].tap do |attribute_list|
-          # Grant manage access to the creating_user if it exists
-          attribute_list << { agent_type: 'user', agent_id: creating_user.user_key, access: Hyrax::PermissionTemplateAccess::MANAGE } if creating_user
-        end + managers_of_collection_type(collection_type: collection_type) + grants
-      end
-      private_class_method :access_grants_attributes
-
-      # @api private
-      #
-      # Retrieve the users or groups with manage permissions for a collection type
-      #
-      # @param collection_type [CollectionType] the collection type of the new collection
-      # @return [Hash] a hash containing permission attributes
-      def self.managers_of_collection_type(collection_type:)
-        attribute_list = []
-        user_managers = Hyrax::CollectionTypes::PermissionsService.user_edit_grants_for_collection_of_type(collection_type: collection_type)
-        user_managers.each do |user|
-          attribute_list << { agent_type: 'user', agent_id: user, access: Hyrax::PermissionTemplateAccess::MANAGE }
+          template.reset_access_controls_for(collection: collection, interpret_visibility: true)
         end
-        group_managers = Hyrax::CollectionTypes::PermissionsService.group_edit_grants_for_collection_of_type(collection_type: collection_type)
-        group_managers.each do |group|
-          attribute_list << { agent_type: 'group', agent_id: group, access: Hyrax::PermissionTemplateAccess::MANAGE }
-        end
-        attribute_list
-      end
-      private_class_method :managers_of_collection_type
 
-      # @api private
-      #
-      # The value of the admin group name
-      #
-      # @return [String] a string representation of the admin group name
-      def self.admin_group_name
-        ::Ability.admin_group_name
+        private
+
+        # @api private
+        #
+        # Gather the default permissions needed for a new collection
+        #
+        # @param collection_type [CollectionType] the collection type of the new collection
+        # @param creating_user [User] the user that created the collection
+        # @param grants [Array<Hash>] additional grants to apply to the new collection
+        # @return [Array<Hash>] a hash containing permission attributes
+        def access_grants_attributes(collection_type:, creating_user:, grants:)
+          [
+            { agent_type: 'group', agent_id: admin_group_name, access: Hyrax::PermissionTemplateAccess::MANAGE }
+          ].tap do |attribute_list|
+            # Grant manage access to the creating_user if it exists
+            attribute_list << { agent_type: 'user', agent_id: creating_user.user_key, access: Hyrax::PermissionTemplateAccess::MANAGE } if creating_user
+          end + managers_of_collection_type(collection_type: collection_type) + grants
+        end
+
+        # @api private
+        #
+        # Retrieve the users or groups with manage permissions for a collection type
+        #
+        # @param collection_type [CollectionType] the collection type of the new collection
+        # @return [Array<Hash>] a hash containing permission attributes
+        def managers_of_collection_type(collection_type:)
+          attribute_list = []
+          user_managers = Hyrax::CollectionTypes::PermissionsService.user_edit_grants_for_collection_of_type(collection_type: collection_type)
+          user_managers.each do |user|
+            attribute_list << { agent_type: 'user', agent_id: user, access: Hyrax::PermissionTemplateAccess::MANAGE }
+          end
+          group_managers = Hyrax::CollectionTypes::PermissionsService.group_edit_grants_for_collection_of_type(collection_type: collection_type)
+          group_managers.each do |group|
+            attribute_list << { agent_type: 'group', agent_id: group, access: Hyrax::PermissionTemplateAccess::MANAGE }
+          end
+          attribute_list
+        end
+
+        # @api private
+        #
+        # The value of the admin group name
+        #
+        # @return [String] a string representation of the admin group name
+        def admin_group_name
+          ::Ability.admin_group_name
+        end
+
+        # @api private
+        #
+        # The collection_type for the collection
+        # @param collection [#collection_type_gid || Hyrax::AdministrativeSet] the collection or admin set the new permissions will act on
+        # @return [Hyrax::CollectionType] a string representation of the admin group name
+        def collection_type(collection:)
+          return Hyrax::CollectionType.find_or_create_admin_set_type if admin_set? collection
+          Hyrax::CollectionType.find_by_gid!(collection.collection_type_gid)
+        end
+
+        def admin_set?(collection)
+          collection.is_a?(Hyrax::AdministrativeSet) || collection.is_a?(AdminSet)
+        end
       end
-      private_class_method :admin_group_name
     end
   end
 end
--- spec/services/hyrax/collections/permissions_create_service_spec.rb
+++ spec/services/hyrax/collections/permissions_create_service_spec.rb
@@ -24,44 +24,91 @@ RSpec.describe Hyrax::Collections::PermissionsCreateService do
       }
     end
 
-    let(:collection) do
-      FactoryBot.build(:collection_lw,
-                       id: 'collection1',
-                       collection_type: collection_type)
-    end
-
     before do
       FactoryBot.create(:collection_type_participant, user_manage_attributes)
       FactoryBot.create(:collection_type_participant, group_manage_attributes)
     end
 
-    it "creates the default permission template for the collection" do
-      described_class.create_default(collection: collection, creating_user: user)
+    context "for collection" do
+      let(:collection) do
+        FactoryBot.valkyrie_create(:hyrax_collection,
+                                   collection_type_gid: collection_type.to_global_id,
+                                   with_permission_template: false)
+      end
 
-      expect(Hyrax::PermissionTemplate.find_by_source_id(collection.id)).to be_persisted
+      it "creates the default permission template and access entries" do
+        expect { described_class.create_default(collection: collection, creating_user: user) }
+          .to change { Hyrax::PermissionTemplate.count }
+          .by(1)
+          .and change { Hyrax::PermissionTemplateAccess.count }
+          .by(4)
+      end
     end
 
-    it "creates the default permission template access entries for the collection" do
-      described_class.create_default(collection: collection, creating_user: user)
+    context "for administrative set" do
+      let(:collection_type) { FactoryBot.create(:admin_set_collection_type) }
+      let(:admin_set) do
+        FactoryBot.valkyrie_create(:hyrax_admin_set,
+                                   with_permission_template: false)
+      end
 
-      expect(Hyrax::PermissionTemplate.find_by_source_id(collection.id).access_grants.count).to eq 4
+      it "creates the default permission template and access entries" do
+        expect { described_class.create_default(collection: admin_set, creating_user: user) }
+          .to change { Hyrax::PermissionTemplate.count }
+          .by(1)
+          .and change { Hyrax::PermissionTemplateAccess.count }
+          .by(4)
+      end
     end
   end
 
   describe ".add_access" do
-    let(:collection) { FactoryBot.create(:collection_lw, with_permission_template: true) }
-
     let(:grants) do
-      [{ agent_type: Hyrax::PermissionTemplateAccess::GROUP,
-         agent_id: 'archivist',
-         access: Hyrax::PermissionTemplateAccess::DEPOSIT }]
+      [
+        {
+          agent_type: Hyrax::PermissionTemplateAccess::GROUP,
+          agent_id: 'archivist',
+          access: Hyrax::PermissionTemplateAccess::DEPOSIT
+        }
+      ]
     end
 
-    it 'gives deposit access to archivist group' do
-      described_class.add_access(collection_id: collection.id, grants: grants)
+    context "for collection" do
+      let!(:collection) do
+        FactoryBot.valkyrie_create(:hyrax_collection,
+                                   with_permission_template: true)
+      end
 
-      expect(collection.permission_template.access_grants.deposit.map(&:agent_id))
-        .to include 'archivist'
+      it 'gives deposit access to archivist group' do
+        expect { described_class.add_access(collection_id: collection.id, grants: grants) }
+          .to change { Hyrax::PermissionTemplate.count }
+          .by(0)
+          .and change { Hyrax::PermissionTemplateAccess.count }
+          .by(1)
+
+        # collection depositors are granted read access to the collection
+        expect(collection.permission_manager.read_groups)
+          .to include 'archivist'
+      end
+    end
+
+    context "for administrative set" do
+      let!(:admin_set) do
+        FactoryBot.valkyrie_create(:hyrax_admin_set,
+                                   with_permission_template: true)
+      end
+
+      it 'gives deposit access to archivist group' do
+        expect { described_class.add_access(collection_id: admin_set.id, grants: grants) }
+          .to change { Hyrax::PermissionTemplate.count }
+          .by(0)
+          .and change { Hyrax::PermissionTemplateAccess.count }
+          .by(1)
+
+        # collection depositors are granted read access to the collection
+        expect(admin_set.permission_manager.read_groups)
+          .to include 'archivist'
+      end
     end
   end
 end
