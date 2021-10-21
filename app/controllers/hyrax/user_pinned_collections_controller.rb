81385cb90dc3923780b21b59d33fa58d78ce7e18
Sara G <geezy@Notch8-MacBook-Pro.local>
2021-10-20 19:21:09 -0500

    dashboard path and fix translations tests

--- app/controllers/hyrax/user_pinned_collections_controller.rb
+++ app/controllers/hyrax/user_pinned_collections_controller.rb
@@ -5,17 +5,16 @@ module Hyrax
       @pin = UserPinnedCollection.create(collection_id: params[:collection_id], user_id: current_user.id)
       respond_to do |format|
         format.json { render json: @pin, status: :created }
-        format.html { redirect_to dashboard_path }
+        format.html { redirect_to hyrax.dashboard_path }
       end
     end
 
     def destroy
       @pin = UserPinnedCollection.where(collection_id: params[:id], user_id: current_user.id).first
       @pin&.destroy
-      redirect_to dashboard_path
       respond_to do |format|
         format.json { head :no_content }
-        format.html { redirect_to dashboard_path }
+        format.html { redirect_to hyrax.dashboard_path }
       end
     end
   end
