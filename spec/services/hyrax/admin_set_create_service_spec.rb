546f16f131213e7591a988bffe26a1324b4ed1af
E. Lynette Rayle <elr37@cornell.edu>
2021-10-24 08:00:17 -0400

    add test for new #create! in AdminSetCreateService

--- spec/services/hyrax/admin_set_create_service_spec.rb
+++ spec/services/hyrax/admin_set_create_service_spec.rb
@@ -89,7 +89,28 @@ RSpec.describe Hyrax::AdminSetCreateService do
       end
     end
 
-    it "is a convenience method for .new#create" do
+    it "is a convenience method for .new#create!" do
+      service = instance_double(described_class)
+      expect(described_class).to receive(:new).and_return(service)
+      expect(service).to receive(:create!)
+      subject
+    end
+  end
+
+  describe ".call!" do
+    subject { described_class.call!(admin_set: admin_set, creating_user: user) }
+
+    let(:admin_set) { AdminSet.new(title: ['test']) }
+
+    context "when using the default admin set", :clean_repo do
+      let(:admin_set) { AdminSet.new(id: AdminSet::DEFAULT_ID) }
+
+      it 'will raise RuntimeError if you attempt to a default admin set' do
+        expect { subject }.to raise_error(RuntimeError)
+      end
+    end
+
+    it "is a convenience method for .new#create!" do
       service = instance_double(described_class)
       expect(described_class).to receive(:new).and_return(service)
       expect(service).to receive(:create!)
@@ -154,6 +175,58 @@ RSpec.describe Hyrax::AdminSetCreateService do
         it { is_expected.to be false }
         it 'will not call the workflow_importer' do
           subject
+          expect(workflow_importer).not_to have_received(:call)
+        end
+      end
+    end
+
+    describe "#create!" do
+      subject { service.create! }
+
+      context "when the admin_set is valid" do
+        let(:permission_template) { Hyrax::PermissionTemplate.find_by(source_id: admin_set.id) }
+        let(:grants) { permission_template.access_grants }
+        let(:available_workflows) { [create(:workflow), create(:workflow)] }
+
+        # rubocop:disable RSpec/AnyInstance
+        before do
+          allow_any_instance_of(Hyrax::PermissionTemplate).to receive(:available_workflows).and_return(available_workflows)
+          # Load expected Sipity roles, which were likely cleaned by DatabaseCleaner
+          Hyrax.config.persist_registered_roles!
+        end
+        # rubocop:enable RSpec/AnyInstance
+
+        it "creates an AdminSet, PermissionTemplate, Workflows, activates the default workflow, and sets access" do
+          expect(Sipity::Workflow).to receive(:activate!).with(permission_template: kind_of(Hyrax::PermissionTemplate), workflow_name: Hyrax.config.default_active_workflow_name)
+          expect do
+            expect(subject).to be_kind_of Hyrax::AdministrativeSet
+          end.to change { admin_set.persisted? }.from(false).to(true)
+                                                .and change { Sipity::WorkflowResponsibility.count }.by(12)
+          # 12 responsibilities because:
+          #  * 2 agents (user + admin group), multiplied by
+          #  * 2 available workflows, multiplied by
+          #  * 3 roles (from Hyrax::RoleRegistry), equals
+          #  * 12
+          expect(admin_set.edit_users).to match_array([user.user_key])
+          expect(admin_set.edit_groups).to match_array(['admin'])
+          expect(admin_set.read_users).to match_array([])
+          expect(admin_set.read_groups).not_to include('public')
+          expect(admin_set.creator).to eq [user.user_key]
+
+          expect(workflow_importer).to have_received(:call).with(permission_template: permission_template)
+          expect(permission_template).to be_persisted
+          expect(grants.count).to eq 2
+          expect(grants.pluck(:agent_type)).to include('group', 'user')
+          expect(grants.pluck(:agent_id)).to include('admin', user.user_key)
+          expect(grants.pluck(:access)).to include('manage')
+        end
+      end
+
+      context "when the admin_set is invalid" do
+        let(:admin_set) { AdminSet.new } # Missing title
+
+        it 'will not call the workflow_importer' do
+          expect { subject }.to raise_error(RuntimeError)
           expect(workflow_importer).not_to have_received(:call)
         end
       end
