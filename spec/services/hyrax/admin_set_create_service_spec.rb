7708b533d4b46af7cb7ce5a6def7931cc3cb1ce3
E. Lynette Rayle <elr37@cornell.edu>
2021-10-28 11:58:33 -0400

    refactor AdminSetCreateService specs
    Integrated the changes from PR #5213 over the service updates in PR #5225.

--- spec/services/hyrax/admin_set_create_service_spec.rb
+++ spec/services/hyrax/admin_set_create_service_spec.rb
@@ -5,12 +5,10 @@ RSpec.describe Hyrax::AdminSetCreateService do
   let(:query_service) { Hyrax.query_service }
 
   describe '.create_default_admin_set', :clean_repo do
-    subject(:status) { described_class.create_default_admin_set }
-
     context "when new admin set persists" do
       it "is a convenience method for .create_default_admin_set!" do
         expect(described_class).to receive(:create_default_admin_set!).and_call_original
-        expect(status).to eq true
+        expect(described_class.create_default_admin_set).to eq true
       end
     end
 
@@ -22,22 +20,19 @@ RSpec.describe Hyrax::AdminSetCreateService do
 
       it "returns false" do
         expect(described_class).to receive(:create_default_admin_set!).and_call_original
-        expect(status).to eq false
+        expect(described_class.create_default_admin_set).to eq false
       end
     end
   end
 
   describe '.find_or_create_default_admin_set', :clean_repo do
-    let(:default_admin_set) { FactoryBot.build(:default_hyrax_admin_set) }
-
-    subject(:admin_set) { described_class.find_or_create_default_admin_set }
-
     context "when default admin set doesn't exist yet" do
       it "is a convenience method for .create_default_admin_set!" do
         expect(query_service).to receive(:find_by).with(id: described_class::DEFAULT_ID)
                                                   .and_raise(Valkyrie::Persistence::ObjectNotFoundError)
         expect(described_class).to receive(:create_default_admin_set!).and_call_original
         expect(query_service).to receive(:find_by).with(id: anything).and_call_original # permission template
+        admin_set = described_class.find_or_create_default_admin_set
         expect(admin_set.title).to eq described_class::DEFAULT_TITLE
       end
     end
@@ -49,102 +44,91 @@ RSpec.describe Hyrax::AdminSetCreateService do
         expect(query_service).to receive(:find_by).with(id: described_class::DEFAULT_ID)
                                                   .and_return(default_admin_set)
         expect(described_class).not_to receive(:create_default_admin_set!)
+        admin_set = described_class.find_or_create_default_admin_set
         expect(admin_set.title).to eq described_class::DEFAULT_TITLE
       end
     end
   end
 
   describe ".default_admin_set?" do
-    let(:admin_set) { FactoryBot.build(:default_hyrax_admin_set) }
-    context "when admin_set is the default" do
-      it "returns true" do
-        expect(described_class.default_admin_set?(id: admin_set.id)).to eq true
-      end
+    it "is true for the default admin set id" do
+      expect(described_class.default_admin_set?(id: described_class::DEFAULT_ID))
+        .to eq true
     end
 
-    context "when admin_set isn't the default" do
-      let(:admin_set) { FactoryBot.build(:hyrax_admin_set, title: ['test']) }
-      it "returns false" do
-        expect(described_class.default_admin_set?(id: admin_set.id)).to eq false
-      end
+    it "is false for anything else" do
+      expect(described_class.default_admin_set?(id: 'anythingelse')).to eq false
     end
   end
 
   describe ".call" do
-    subject { described_class.call(admin_set: admin_set, creating_user: user) }
-
-    let(:admin_set) { FactoryBot.build(:hyrax_admin_set, title: ['test']) }
-
-    context "when using the default admin set", :clean_repo do
-      let(:admin_set) { FactoryBot.build(:hyrax_admin_set, id: described_class::DEFAULT_ID) }
-
-      it 'will raise RuntimeError if you attempt to a default admin set' do
-        expect { subject }.to raise_error(RuntimeError)
+    context "when passing in the default admin set", :clean_repo do
+      let(:admin_set) { FactoryBot.build(:default_hyrax_admin_set) }
+      it 'will raise RuntimeError' do
+        expect { described_class.call(admin_set: admin_set, creating_user: user) }
+          .to raise_error(RuntimeError)
       end
     end
 
-    it "is a convenience method for .new#create!" do
-      service = instance_double(described_class)
-      expect(described_class).to receive(:new).and_return(service)
-      expect(service).to receive(:create!)
-      subject
+    context "when passing a non-default admin set" do
+      let(:admin_set) { FactoryBot.build(:hyrax_admin_set) }
+      it "is a convenience method for .call!" do
+        service = instance_double(described_class)
+        expect(described_class).to receive(:new).and_return(service)
+        expect(service).to receive(:create!)
+        described_class.call(admin_set: admin_set, creating_user: user)
+      end
     end
   end
 
   describe ".call!" do
-    subject { described_class.call!(admin_set: admin_set, creating_user: user) }
-
-    let(:admin_set) { FactoryBot.build(:hyrax_admin_set, title: ['test']) }
-
-    context "when using the default admin set", :clean_repo do
-      let(:admin_set) { FactoryBot.build(:hyrax_admin_set, id: described_class::DEFAULT_ID) }
-
-      it 'will raise RuntimeError if you attempt to a default admin set' do
-        expect { subject }.to raise_error(RuntimeError)
+    context "when passing in the default admin set", :clean_repo do
+      let(:admin_set) { FactoryBot.build(:default_hyrax_admin_set) }
+      it 'will raise RuntimeError' do
+        expect { described_class.call!(admin_set: admin_set, creating_user: user) }
+          .to raise_error(RuntimeError)
       end
     end
 
-    it "is a convenience method for .new#create!" do
-      service = instance_double(described_class)
-      expect(described_class).to receive(:new).and_return(service)
-      expect(service).to receive(:create!)
-      subject
+    context "when passing a non-default admin set" do
+      let(:admin_set) { FactoryBot.build(:hyrax_admin_set) }
+      it "is a convenience method for .new#create!" do
+        service = instance_double(described_class)
+        expect(described_class).to receive(:new).and_return(service)
+        expect(service).to receive(:create!)
+        described_class.call!(admin_set: admin_set, creating_user: user)
+      end
     end
   end
 
   describe "an instance" do
     subject { service }
-
+    let(:service) { described_class.new(admin_set: admin_set, creating_user: user, workflow_importer: workflow_importer) }
     let(:workflow_importer) { double(call: true) }
     let(:admin_set) { FactoryBot.build(:hyrax_admin_set, title: ['test']) }
-    let(:service) { described_class.new(admin_set: admin_set, creating_user: user, workflow_importer: workflow_importer) }
 
     its(:default_workflow_importer) { is_expected.to respond_to(:call) }
 
     describe "#create" do
-      subject { service.create }
-
       context "when the admin_set is valid" do
         let(:updated_admin_set) { FactoryBot.valkyrie_create(:hyrax_admin_set) }
 
         it "is a convenience method for #create! that returns true" do
           expect(service).to receive(:create!).and_return(updated_admin_set)
-          expect(subject).to eq true
+          expect(service.create).to eq true
         end
       end
 
       context "when the admin_set is invalid" do
         it "is a convenience method for #create! that returns false" do
           expect(service).to receive(:create!).and_raise(RuntimeError)
-          expect(subject).to eq false
+          expect(service.create).to eq false
         end
       end
     end
 
     describe "#create!" do
       let(:admin_set) { AdminSet.new(title: ['test']) }
-
-      subject { service.create! }
 
       context "when the admin_set is valid" do
         let(:permission_template) { Hyrax::PermissionTemplate.find_by(source_id: admin_set.id) }
@@ -162,14 +146,19 @@ RSpec.describe Hyrax::AdminSetCreateService do
         it "creates an AdministrativeSet, PermissionTemplate, Workflows, activates the default workflow, and sets access" do
           expect(Sipity::Workflow).to receive(:activate!).with(permission_template: kind_of(Hyrax::PermissionTemplate), workflow_name: Hyrax.config.default_active_workflow_name)
           expect do
-            expect(subject).to be_kind_of Hyrax::AdministrativeSet
-          end.to change { admin_set.persisted? }.from(false).to(true)
-                                                .and change { Sipity::WorkflowResponsibility.count }.by(12)
+            expect(service.create!).to be_kind_of Hyrax::AdministrativeSet
+          end
+            .to change { admin_set.persisted? }
+            .from(false)
+            .to(true)
+            .and change { Sipity::WorkflowResponsibility.count }
+            .by(12)
           # 12 responsibilities because:
           #  * 2 agents (user + admin group), multiplied by
           #  * 2 available workflows, multiplied by
           #  * 3 roles (from Hyrax::RoleRegistry), equals
           #  * 12
+
           expect(admin_set.edit_users).to match_array([user.user_key])
           expect(admin_set.edit_groups).to match_array(['admin'])
           expect(admin_set.read_users).to match_array([])
@@ -189,7 +178,7 @@ RSpec.describe Hyrax::AdminSetCreateService do
         let(:admin_set) { FactoryBot.build(:invalid_hyrax_admin_set) } # Missing title
 
         it 'will not call the workflow_importer' do
-          expect { subject }.to raise_error(RuntimeError)
+          expect { service.create! }.to raise_error(RuntimeError)
           expect(workflow_importer).not_to have_received(:call)
         end
       end
