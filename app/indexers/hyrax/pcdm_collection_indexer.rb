37429a2d62c6280c70a9eee32238219eb37cad0b
E. Lynette Rayle <elr37@cornell.edu>
2021-11-02 06:50:10 -0400

    add depositor to pcdm collection indexer

--- app/indexers/hyrax/pcdm_collection_indexer.rb
+++ app/indexers/hyrax/pcdm_collection_indexer.rb
@@ -15,6 +15,8 @@ module Hyrax
         index_document[Hyrax.config.collection_type_index_field.to_sym] = Array(resource.try(:collection_type_gid)&.to_s)
         index_document[:generic_type_sim] = ['Collection']
         index_document[:thumbnail_path_ss] = Hyrax::CollectionThumbnailPathService.call(resource)
+        index_document[:depositor_ssim] = [resource.depositor]
+        index_document[:depositor_tesim] = [resource.depositor]
       end
     end
   end
