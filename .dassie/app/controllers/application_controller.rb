0c7d7e4fd47893cde9316045f09e5b88621542c6
tamsin johnson <tomjohnson@ucsb.edu>
2021-12-08 13:16:06 -0800

    remove Blacklight 6.0 skip_after_action
    this callback was removed in blacklight 7, so we don't need to skip it anymore.

--- .dassie/app/controllers/application_controller.rb
+++ .dassie/app/controllers/application_controller.rb
@@ -2,13 +2,11 @@ class ApplicationController < ActionController::Base
   helper Openseadragon::OpenseadragonHelper
   # Adds a few additional behaviors into the application controller
   include Blacklight::Controller
-  skip_after_action :discard_flash_if_xhr
   include Hydra::Controller::ControllerBehavior
 
   # Adds Hyrax behaviors into the application controller
   include Hyrax::Controller
   include Hyrax::ThemedLayoutController
+
   with_themed_layout '1_column'
-
-
 end
