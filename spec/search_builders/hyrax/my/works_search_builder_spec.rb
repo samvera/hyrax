39fc6b37178b2f38bbf45f3387bed8e2212382a7
kibigo! <kibigo@ucsb.edu>
2021-12-09 15:42:20 -0800

    Update WorksSearchBuilder specs for Blacklight 7
    Previously, these tests hardcoded the default blacklight filters, which
    have changed from Blacklight 6 to 7. This commit revises the tests to
    instead only verify that our own filters have been added to the end.

--- spec/search_builders/hyrax/my/works_search_builder_spec.rb
+++ spec/search_builders/hyrax/my/works_search_builder_spec.rb
@@ -18,24 +18,6 @@ RSpec.describe Hyrax::My::WorksSearchBuilder do
   end
 
   describe ".default_processor_chain" do
-    subject { described_class.default_processor_chain }
-
-    let(:expected_filters) do
-      [
-        :default_solr_parameters,
-        :add_query_to_solr,
-        :add_facet_fq_to_solr,
-        :add_facetting_to_solr,
-        :add_solr_fields_to_query,
-        :add_paging_to_solr,
-        :add_sorting_to_solr,
-        :add_group_config_to_solr,
-        :add_facet_paging_to_solr,
-        :filter_models,
-        :show_only_resources_deposited_by_current_user
-      ]
-    end
-
-    it { is_expected.to eq expected_filters }
+    it { expect(described_class.default_processor_chain).to end_with(:filter_models, :show_only_resources_deposited_by_current_user) }
   end
 end
