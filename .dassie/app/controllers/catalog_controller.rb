40f6ea732246b8943d983062d1846f04e4c9b029
tamsin johnson <tomjohnson@ucsb.edu>
2021-12-09 15:57:41 -0800

    shim in #repository support to `CatalogController`

--- .dassie/app/controllers/catalog_controller.rb
+++ .dassie/app/controllers/catalog_controller.rb
@@ -3,6 +3,16 @@ class CatalogController < ApplicationController
   include Hydra::Catalog
   include Hydra::Controller::ControllerBehavior
 
+  ##
+  # @deprecated provides short-term compatibility with Blacklight 6
+  # @return [Blacklight::AbstractRepository]
+  def repository
+    Deprecation.warn("Avoid direct calls to `#repository`; this method" \
+                     " provides short-term compatibility to Blacklight 6 " \
+                     " clients.")
+    blacklight_config.repository
+  end
+
   # This filter applies the hydra access controls
   before_action :enforce_show_permissions, only: :show
 
