44ee0c5cc5b444b58899c4e3a9c6a9ae84073e22
tamsin johnson <tomjohnson@ucsb.edu>
2021-12-13 15:06:05 -0800

    refactor specs for `Qa::Authorities::Collection` for readability
    remove unused setup and managed whitespace for readability on
    `Qa::Authorities::Collection`. i think this whole test setup can be refactored
    to stop using `ActiveFedora`, and that should probably happen next.
    
    this is motivated by these tests failing in Blacklight 7.

--- spec/authorities/qa/authorities/collections_spec.rb
+++ spec/authorities/qa/authorities/collections_spec.rb
@@ -1,37 +1,94 @@
 # frozen_string_literal: true
+
+# @todo FactoryBot.build seems to be used to force indexing.
+#   this can probably be refactored to just index the content we want the
+#   authority service to find and avoid DB round trips associated with
+#   `with_permission_template`,etc...
 RSpec.describe Qa::Authorities::Collections, :clean_repo do
+  subject(:service) { described_class.new }
   let(:controller) { Qa::TermsController.new }
-  let(:user1) { build(:user) }
-  let(:user2) { build(:user) }
-  let(:q) { "foo" }
-  let(:service) { described_class.new }
-  let!(:collection1) { build(:private_collection_lw, id: 'col-1-own', title: ['foo foo'], user: user1, with_permission_template: true, with_solr_document: true) }
-  let!(:collection2) { build(:private_collection_lw, id: 'col-2-own', title: ['bar'], user: user1, with_permission_template: true, with_solr_document: true) }
-  let!(:collection3) { build(:private_collection_lw, id: 'col-3-own', title: ['another foo'], user: user1, with_permission_template: true, with_solr_document: true) }
-  let!(:collection4) { build(:private_collection_lw, id: 'col-4-none', title: ['foo foo foo'], user: user2, with_permission_template: true, with_solr_document: true) }
+  let(:user1) { FactoryBot.build(:user) }
+  let(:user2) { FactoryBot.build(:user) }
+
+  let!(:collection1) do
+    FactoryBot.build(:private_collection_lw,
+                     title: ['foo foo'],
+                     user: user1,
+                     with_permission_template: true,
+                     with_solr_document: true)
+  end
+
+  let!(:collection2) do
+    FactoryBot.build(:private_collection_lw,
+                     title: ['bar'],
+                     user: user1,
+                     with_permission_template: true,
+                     with_solr_document: true)
+  end
+
+  let!(:collection3) do
+    FactoryBot.build(:private_collection_lw,
+                     title: ['another foo'],
+                     user: user1,
+                     with_permission_template: true,
+                     with_solr_document: true)
+  end
+
+  let!(:collection4) do
+    FactoryBot.build(:private_collection_lw,
+                     title: ['foo foo foo'],
+                     user: user2,
+                     with_permission_template: true,
+                     with_solr_document: true)
+  end
+
   let!(:collection5) do
-    build(:private_collection_lw, id: 'col-5-mgr', title: ['foo for you'], user: user2,
-                                  with_permission_template: { manage_users: [user1] }, with_solr_document: true)
+    FactoryBot.build(:private_collection_lw,
+                     title: ['foo for you'],
+                     user: user2,
+                     with_permission_template: { manage_users: [user1] },
+                     with_solr_document: true)
   end
+
   let!(:collection6) do
-    build(:private_collection_lw, id: 'col-6-dep', title: ['foo too'], user: user2,
-                                  with_permission_template: { deposit_users: [user1] }, with_solr_document: true)
+    FactoryBot.build(:private_collection_lw,
+                     title: ['foo too'],
+                     user: user2,
+                     with_permission_template: { deposit_users: [user1] },
+                     with_solr_document: true)
   end
+
   let!(:collection7) do
-    build(:private_collection_lw, id: 'col-7-view', title: ['foo bar baz'], user: user2,
-                                  with_permission_template: { view_users: [user1] }, with_solr_document: true)
+    FactoryBot.build(:private_collection_lw,
+                     title: ['foo bar baz'],
+                     user: user2,
+                     with_permission_template: { view_users: [user1] },
+                     with_solr_document: true)
   end
+
   let!(:collection8) do
-    build(:private_collection_lw, id: 'col-8-mgr', title: ['bar for you'], user: user2,
-                                  with_permission_template: { manage_users: [user1] }, with_solr_document: true)
+    FactoryBot.build(:private_collection_lw,
+                     id: 'col-8-mgr',
+                     title: ['bar for you'],
+                     user: user2,
+                     with_permission_template: { manage_users: [user1] },
+                     with_solr_document: true)
   end
+
   let!(:collection9) do
-    build(:private_collection_lw, id: 'col-9-dep', title: ['bar too'], user: user2,
-                                  with_permission_template: { deposit_users: [user1] }, with_solr_document: true)
+    FactoryBot.build(:private_collection_lw,
+                     title: ['bar too'],
+                     user: user2,
+                     with_permission_template: { deposit_users: [user1] },
+                     with_solr_document: true)
   end
+
   let!(:collection10) do
-    build(:private_collection_lw, id: 'col-10-view', title: ['bar bar baz'], user: user2,
-                                  with_permission_template: { view_users: [user1] }, with_solr_document: true)
+    FactoryBot.build(:private_collection_lw,
+                     title: ['bar bar baz'],
+                     user: user2,
+                     with_permission_template: { view_users: [user1] },
+                     with_solr_document: true)
   end
 
   before do
@@ -39,30 +96,40 @@ RSpec.describe Qa::Authorities::Collections, :clean_repo do
     allow(controller).to receive(:current_user).and_return(user1)
   end
 
-  subject { service.search(q, controller) }
-
   describe '#search' do
     context 'when access is read' do
-      let(:params) { ActionController::Parameters.new(q: q, access: 'read') }
+      let(:params) { ActionController::Parameters.new(q: 'foo', access: 'read') }
 
-      it 'displays a list of read collections for the current user' do
-        expect(subject.map { |result| result[:id] }).to match_array [collection1.id, collection3.id, collection5.id, collection6.id, collection7.id]
+      it 'lists collections the current user can read' do
+        expect(service.search(nil, controller))
+          .to contain_exactly(include(id: collection1.id),
+                              include(id: collection3.id),
+                              include(id: collection5.id),
+                              include(id: collection6.id),
+                              include(id: collection7.id))
       end
     end
 
     context 'when access is edit' do
-      let(:params) { ActionController::Parameters.new(q: q, access: 'edit') }
+      let(:params) { ActionController::Parameters.new(q: 'foo', access: 'edit') }
 
-      it 'displays a list of edit collections for the current user' do
-        expect(subject.map { |result| result[:id] }).to match_array [collection1.id, collection3.id, collection5.id]
+      it 'lists collections the current user can edit' do
+        expect(service.search(nil, controller))
+          .to contain_exactly(include(id: collection1.id),
+                              include(id: collection3.id),
+                              include(id: collection5.id))
       end
     end
 
     context 'when access is deposit' do
-      let(:params) { ActionController::Parameters.new(q: q, access: 'deposit') }
+      let(:params) { ActionController::Parameters.new(q: 'foo', access: 'deposit') }
 
-      it 'displays a list of edit and deposit collections for the current user' do
-        expect(subject.map { |result| result[:id] }).to match_array [collection1.id, collection3.id, collection5.id, collection6.id]
+      it 'lists collections the current user can edit or deposit' do
+        expect(service.search(nil, controller))
+          .to contain_exactly(include(id: collection1.id),
+                              include(id: collection3.id),
+                              include(id: collection5.id),
+                              include(id: collection6.id))
       end
     end
   end
