57d3b767dab51525cd3c0fc47320df71fafd42ba
kibigo! <kibigo@ucsb.edu>
2021-12-29 14:56:45 -0800

    Add window stub for Deno tests

--- app/scripts/hyrax/dev-deps.js
+++ app/scripts/hyrax/dev-deps.js
@@ -1,14 +1,15 @@
+import "../../../lib/scripts/window.js";
 export {
-  assert,               // true or false
-  assertEquals,         // deep equality
-  assertNotEquals,      // not deep equality
-  assertStrictEquals,   // strict equality
+  assert, // true or false
+  assertArrayIncludes, // array includes
+  assertEquals, // deep equality
+  assertMatch, // matches regex
+  assertNotEquals, // not deep equality
+  assertNotMatch, // does not match regex
+  assertObjectMatch, // matches subset object
+  assertStrictEquals, // strict equality
   assertStringIncludes, // string includes
-  assertMatch,          // matches regex
-  assertNotMatch,       // does not match regex
-  assertArrayIncludes,  // array includes
-  assertObjectMatch,    // matches subset object
-  assertThrows,         // function throws
-  unimplemented,        // throws if called
-  unreachable,          // throws if reached
+  assertThrows, // function throws
+  unimplemented, // throws if called
+  unreachable, // throws if reached
 } from "https://deno.land/std@0.119.0/testing/asserts.ts";
