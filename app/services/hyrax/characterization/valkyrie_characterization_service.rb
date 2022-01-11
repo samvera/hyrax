e0b3871472936d2c39110d8c48cf1f3106148c08
tamsin johnson <tomjohnson@ucsb.edu>
2022-01-10 17:48:05 -0800

    rework ValkyrieCharacterizationService
    ValkyrieCharacterizationService was initially ported in from Hydra::Works and
    lightly modified to work with Valkyrie. the ported code was a little messy, and
    in want of cleanup.
    
    the public interface on `#run` is changed here to use named parameters, and some
    of the private methods are rewritten to harden them up. references to OM, which
    was removed from the Hyrax dependency tree in 2019, are removed. unused and
    opaque behavior around "proxied terms" is removed entirely. `#send`s are changed
    to `#public_send` to avoid accidentally calling private behavior (which could be
    injected by manipulating the mapping hash).
    
    we also add a test for publishing the metadata updates on the metadata node, and
    add the implementation for that.

--- app/services/hyrax/characterization/valkyrie_characterization_service.rb
+++ app/services/hyrax/characterization/valkyrie_characterization_service.rb
@@ -1,51 +1,70 @@
 # frozen_string_literal: true
-require 'hydra-file_characterization'
-require 'nokogiri'
 
 class Hyrax::Characterization::ValkyrieCharacterizationService
   ##
-  # @param [Hyrax::FileMetadata] object which has properties to recieve characterization values
+  # @api public
+  # @param [Hyrax::FileMetadata] metadata  which has properties to recieve characterization values
   # @param [Valkyrie::StorageAdapter::StreamFile] source to run characterization against
   # @param [Hash] options the options pass to characterization
   # @option options [Hash{Symbol => Symbol}] parser_mapping
-  # @option options [Class] parser_class
-  # @option options [Object] fits
-  # @option options [Object] ch12n_tool
+  # @option options [Hydra::Works::Characterization::FitsDocument] parser
+  # @option options [Symbol] ch12n_tool
   #
-  # @return [Hash]
-  def self.run(object, source = nil, options = {})
-    new(object, source, options).characterize
-    Hyrax.persister.save(resource: object)
-  end
-
-  attr_accessor :object, :source, :mapping, :parser_class, :tools
-
-  def initialize(object, source, characterizer: Hydra::FileCharacterization, **options)
-    @characterizer = characterizer
-    @object        = object
-    @source        = source
-    @mapping       = options.fetch(:parser_mapping, Hydra::Works::Characterization.mapper)
-    @parser_class  = options.fetch(:parser_class, Hydra::Works::Characterization::FitsDocument)
-    @tools         = options.fetch(:ch12n_tool, :fits)
+  # @return [void]
+  def self.run(metadata:, file:, user: ::User.system_user, **options)
+    new(metadata: metadata, file: file, **options).characterize
+    saved = Hyrax.persister.save(resource: metadata)
+    Hyrax.publisher.publish('object.metadata.updated', object: saved, user: user)
   end
 
   ##
+  # @!attribute [rw] source
+  #   @return [Valkyrie::StorageAdapter::StreamFile]
+  # @!attribute [rw] metadata
+  #   @return [Hyrax::FileMetadata]
+  # @!attribute [rw] parser
+  #   @return [Hydra::Works::Characterization::FitsDocument]
+  # @!attribute [rw] source
+  #   @return [Valkyrie::StorageAdapter::StreamFile]
+  # @!attribute [rw] tools
+  #   @todo clarify what this is for. it gets passed to {#characterize}
+  #     on the characterizer, but it's not clear how to customize
+  #     effectively.
+  #   @return [Symbol]
+  attr_accessor :mapping, :metadata, :parser, :source, :tools
+
+  ##
+  # @api private
+  def initialize(metadata:, # rubocop:disable Metrics/ParameterLists
+                 file:,
+                 characterizer: Hydra::FileCharacterization,
+                 parser_mapping: Hydra::Works::Characterization.mapper,
+                 parser: Hydra::Works::Characterization::FitsDocument.new)
+    @characterizer = characterizer
+    @metadata      = metadata
+    @source        = file
+    @mapping       = parser_mapping
+    @parser        = parser
+    @tools         = :fits
+  end
+
+  ##
+  # @api private
+  #
   # Coerce given source into a type that can be passed to Hydra::FileCharacterization
   # Use Hydra::FileCharacterization to extract metadata (an OM XML document)
   # Get the terms (and their values) from the extracted metadata
-  # Assign the values of the terms to the properties of the object
+  # Assign the values of the terms to the properties of the metadata object
   #
-  # @return [Hash]
+  # @return [void]
   def characterize
-    content = source_to_content
-    extracted_md = extract_metadata(content)
-    terms = parse_metadata(extracted_md)
-    store_metadata(terms)
+    terms = parse_metadata(extract_metadata(content))
+    apply_metadata(terms)
   end
 
   protected
 
-  def source_to_content
+  def content
     source.rewind
     source.read
   end
@@ -57,39 +76,38 @@ class Hyrax::Characterization::ValkyrieCharacterizationService
   end
 
   def file_name
-    object.original_filename
+    metadata.original_filename
   end
 
-  # Use OM to parse metadata
   def parse_metadata(metadata)
-    omdoc = parser_class.new
-    omdoc.ng_xml = Nokogiri::XML(metadata) if metadata.present?
-    omdoc.__cleanup__ if omdoc.respond_to? :__cleanup__
-    characterization_terms(omdoc)
+    doc = parser
+    doc.ng_xml = Nokogiri::XML(metadata) if metadata.present?
+    doc.__cleanup__ if doc.respond_to? :__cleanup__
+    characterization_terms(doc)
   end
 
   # Get proxy terms and values from the parser
-  def characterization_terms(omdoc)
+  def characterization_terms(doc)
     h = {}
-    omdoc.class.terminology.terms.each_pair do |key, target|
-      # a key is a proxy if its target responds to proxied_term
-      next unless target.respond_to? :proxied_term
+
+    doc.class.terminology.terms.each_pair do |key, _target|
       begin
-        h[key] = omdoc.send(key)
+        h[key] = doc.public_send(key)
       rescue NoMethodError
         next
       end
     end
-    h.delete_if { |_k, v| v.empty? }
+
+    h.compact
   end
 
   # Assign values of the instance properties from the metadata mapping :prop => val
   # @return [Hash]
-  def store_metadata(terms)
+  def apply_metadata(terms)
     terms.each_pair do |term, value|
       property = property_for(term)
       next if property.nil?
-      # Array-ify the value to avoid a conditional here
+
       Array(value).each { |v| append_property_value(property, v) }
     end
   end
@@ -97,18 +115,20 @@ class Hyrax::Characterization::ValkyrieCharacterizationService
   # Check parser_config then self for matching term.
   # Return property symbol or nil
   def property_for(term)
-    if mapping.key?(term) && object.respond_to?(mapping[term])
+    if mapping.key?(term) && metadata.respond_to?(mapping[term])
       mapping[term]
-    elsif object.respond_to?(term)
+    elsif metadata.respond_to?(term)
       term
     end
   end
 
+  ##
+  # @todo push exceptional per-property behavior into the mapping somehow?
   def append_property_value(property, value)
     # We don't want multiple mime_types; this overwrites each time to accept last value
-    value = Array(object.public_send(property)) + [value] unless property == :mime_type
+    value = Array(metadata.public_send(property)) + [value] unless property == :mime_type
     # We don't want multiple heights / widths, pick the max
     value = value.map(&:to_i).max.to_s if property == :height || property == :width
-    object.send("#{property}=", value)
+    metadata.public_send("#{property}=", value)
   end
 end
--- spec/services/hyrax/characterization/valkyrie_characterization_service_spec.rb
+++ spec/services/hyrax/characterization/valkyrie_characterization_service_spec.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
+require 'spec_helper'
+require 'hyrax/specs/spy_listener'
 
 # rubocop:disable RSpec/AnyInstance
 RSpec.describe Hyrax::Characterization::ValkyrieCharacterizationService do
@@ -6,6 +8,7 @@ RSpec.describe Hyrax::Characterization::ValkyrieCharacterizationService do
     let(:characterizer) { double(characterize: fits_response) }
     let(:file_set)      { FactoryBot.valkyrie_create(:hyrax_file_set) }
     let(:fits_response) { IO.read('spec/fixtures/png_fits.xml') }
+    let(:listener)      { Hyrax::Specs::SpyListener.new }
     let(:metadata)      { Hyrax.custom_queries.find_file_metadata_by(id: file.id) }
     let(:upload)        { Rack::Test::UploadedFile.new('spec/fixtures/world.png', 'image/png') }
 
@@ -15,14 +18,29 @@ RSpec.describe Hyrax::Characterization::ValkyrieCharacterizationService do
                                    original_filename: 'test_world.png')
     end
 
-    it 'successfully sets the property values' do
-      described_class.run(metadata, file, characterizer: characterizer)
+    before { Hyrax.publisher.subscribe(listener) }
+    after { Hyrax.publisher.unsubscribe(listener) }
 
-      expect(metadata)
-        .to have_attributes(compression: contain_exactly("Deflate/Inflate"),
-                            format_label: contain_exactly("Portable Network Graphics"),
-                            height: contain_exactly("50"),
-                            width: contain_exactly("50"))
+    describe '#run' do
+      it 'successfully sets the property values' do
+        described_class
+          .run(metadata: metadata, file: file, characterizer: characterizer)
+
+        expect(metadata)
+          .to have_attributes(compression: contain_exactly("Deflate/Inflate"),
+                              format_label: contain_exactly("Portable Network Graphics"),
+                              height: contain_exactly("50"),
+                              width: contain_exactly("50"))
+      end
+
+      it 'publishes metadata updated for file metadata node' do
+        described_class
+          .run(metadata: metadata, file: file, characterizer: characterizer)
+
+        expect(listener.object_metadata_updated&.payload)
+          .to include(user: ::User.system_user,
+                      object: metadata)
+      end
     end
   end
 end
