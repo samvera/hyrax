86169ae455e881f61d586c614f9b6777e89123ff
tamsin johnson <tomjohnson@ucsb.edu>
2022-01-11 16:18:52 -0800

    test deposit of files via WorkUploadsHandler
    `Hyrax::WorkUploadsHandler` should always ingest files matching its
    uploads. prior to this, its tests skipped running the background job that
    handles ingest. that job has (still) no unit tests of its own.
    
    this makes inroads toward comprehensive ingest tests by wiring in one spec from
    `WorkUploadsHandler` down. we stub out the characterizer since that behavior has
    strong tests of its own, and we don't want to include the system call to
    `fits.sh` here---the background job is run by the ActiveJob test adapter
    directly in the app container, so the fits dependency isn't expected to be
    installed.

--- app/jobs/valkyrie_ingest_job.rb
+++ app/jobs/valkyrie_ingest_job.rb
@@ -14,9 +14,8 @@ class ValkyrieIngestJob < Hyrax::ApplicationJob
   # @return [void]
   def ingest(file:)
     file_set = Hyrax.query_service.find_by(id: file.file_set_uri)
-    file_metadata = Hyrax::FileMetadata.for(file: file.uploader.file)
+    updated_metadata = upload_file(file: file, file_set: file_set)
 
-    updated_metadata = upload_file(file: file, file_metadata: file_metadata, file_set: file_set)
     add_file_to_file_set(file_set: file_set, file_metadata: updated_metadata)
   end
 
@@ -33,24 +32,24 @@ class ValkyrieIngestJob < Hyrax::ApplicationJob
     Hyrax.persister.save(resource: file_set)
   end
 
+  ##
   # @param [Hyrax::UploadedFile] file
-  # @param [Hyrax::FileMetadata] file_metadata
   # @param [Hyrax::FileSet] file_set
-  # @return Hyrax::FileMetadata uploaded file
-  def upload_file(file:, file_metadata:, file_set:)
-    uploader = file.uploader
-    file_metadata.file_set_id = file.file_set_uri
+  #
+  # @return [Hyrax::FileMetadata] the metadata representing the uploaded file
+  def upload_file(file:, file_set:)
+    carrier_wave_sanitized_file = file.uploader.file
     uploaded = Hyrax.storage_adapter
                     .upload(resource: file_set,
-                            file: File.open(uploader.file.file),
-                            original_filename: file_metadata.original_filename)
+                            file: carrier_wave_sanitized_file,
+                            original_filename: carrier_wave_sanitized_file.original_filename)
+
+    file_metadata = Hyrax.custom_queries.find_file_metadata_by(id: uploaded.id)
+    file_metadata.file_set_id = file.file_set_uri
     file_metadata.file_identifier = uploaded.id
     file_metadata.size = uploaded.size
 
-    Hyrax.publisher.publish(
-      "object.file.uploaded",
-      metadata: file_metadata
-    )
+    Hyrax.publisher.publish("object.file.uploaded", metadata: file_metadata)
 
     file_metadata
   end
