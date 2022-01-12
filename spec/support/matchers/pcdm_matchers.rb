9dd80321170de9c95c7ab7d218d53736d5c9e831
tamsin johnson <tomjohnson@ucsb.edu>
2022-01-11 16:14:46 -0800

    add a matcher to check a (valkyrie) FileSet for file members
    this matcher should make it easy to check if a file set has at least one file
    member, or to check if its file members are exactly the files we expect (under
    `#==` equality).

--- spec/support/matchers/pcdm_matchers.rb
+++ spec/support/matchers/pcdm_matchers.rb
@@ -7,6 +7,25 @@ RSpec::Matchers.define :have_file_set_members do |*expected_file_sets|
   end
 end
 
+RSpec::Matchers.define :have_attached_files do |*expected_files|
+  match do |actual_file_set|
+    @actual_files = Hyrax.custom_queries.find_files(file_set: actual_file_set)
+
+    (expected_files.empty? && @actual_files.any?) ||
+      @actual_files == expected_files
+  end
+
+  failure_message_for_should do |actual_file_set|
+    if expected_files.empty?
+      "Expected #{actual_file_set} to have at least one file.\n" \
+      "Found #{@actual_files}."
+    else
+      "Expected #{actual_file_set} to have files: #{expected_files}\n" \
+      "Found #{@actual_files}."
+    end
+  end
+end
+
 RSpec::Matchers.define :be_a_resource_with_permissions do |*expected_permissions|
   match do |actual_resource|
     expect(Hyrax::AccessControlList.new(resource: actual_resource).permissions)
