297bd6e6cc95a4db59dfc5d1d223720321e6e94c
tamsin johnson <tomjohnson@ucsb.edu>
2022-01-21 14:59:00 -0800

    prepare release v3.3.0

--- template.rb
+++ template.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 # Hack for https://github.com/rails/rails/issues/35153
 gsub_file 'Gemfile', /^gem ["']sqlite3["']$/, 'gem "sqlite3", "~> 1.3.0"'
-gem 'hyrax', '3.2.0'
+gem 'hyrax', '3.3.0'
 run 'bundle install'
 generate 'hyrax:install', '-f'
