a7aa8541faecce3a92eee219d3c95f19909142a8
tamsin johnson <tomjohnson@ucsb.edu>
2022-01-25 14:46:17 -0800

    listen for membership changes on objects; index in response
    when membership changes take place, index the relevant objects.
    
    this is a pretty naive implementation of this requirement. refinements with
    fewer hard/manual solr commits are possible and should be considered as
    development continues.

--- spec/services/hyrax/listeners/metadata_index_listener_spec.rb
+++ spec/services/hyrax/listeners/metadata_index_listener_spec.rb
@@ -35,6 +35,42 @@ RSpec.describe Hyrax::Listeners::MetadataIndexListener do
     end
   end
 
+  describe '#on_object_membership_updated' do
+    let(:event_type) { :on_object_membership_updated }
+
+    it 'reindexes the object on the configured adapter' do
+      expect { listener.on_object_membership_updated(event) }
+        .to change { fake_adapter.saved_resources }
+        .to contain_exactly(resource)
+    end
+
+    it 'reindexes the object from ID on the configured adapter' do
+      data  = { object_id: resource.id }
+      event = Dry::Events::Event.new(event_type, data)
+
+      expect { listener.on_object_membership_updated(event) }
+        .to change { fake_adapter.saved_resources }
+        .to contain_exactly(resource)
+    end
+
+    context 'when the resource does not exist' do
+      let(:data) { { object_id: 'MISSING_ID_FOR_METADATA_INDEX_LISTENER_SPEC' } }
+
+      it 'does not raise' do
+        expect { listener.on_object_membership_updated(event) }
+          .not_to raise_error
+      end
+
+      it 'logs failure' do
+        expect(Hyrax.logger)
+          .to receive(:error)
+          .with(/MISSING_ID_FOR_METADATA_INDEX_LISTENER_SPEC/)
+
+        listener.on_object_membership_updated(event)
+      end
+    end
+  end
+
   describe '#on_object_metadata_updated' do
     let(:event_type) { :on_object_metadata_updated }
 
