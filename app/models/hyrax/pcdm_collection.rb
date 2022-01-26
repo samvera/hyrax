1aed2274980cef7cbdcdcb4a01a6ccdfa21d25c9
Chris Colvard <chris.colvard@gmail.com>
2022-01-26 16:29:13 -0500

    Persist visibility changes during PcdmCollection updates

--- app/models/hyrax/pcdm_collection.rb
+++ app/models/hyrax/pcdm_collection.rb
@@ -32,5 +32,27 @@ module Hyrax
     def pcdm_object?
       true
     end
+
+    def permission_manager
+      @permission_manager ||= Hyrax::PermissionManager.new(resource: self)
+    end
+
+    def visibility=(value)
+      visibility_writer.assign_access_for(visibility: value)
+    end
+
+    def visibility
+      visibility_reader.read
+    end
+
+    protected
+
+    def visibility_writer
+      Hyrax::VisibilityWriter.new(resource: self)
+    end
+
+    def visibility_reader
+      Hyrax::VisibilityReader.new(resource: self)
+    end
   end
 end
