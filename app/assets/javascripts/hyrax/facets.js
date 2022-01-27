9ef4467073405c77b21a3df973f660a236739149
Adam J. Arling <aarling@gmail.com>
2022-01-27 11:44:52 -0600

    Glyphicons replace with Font Awesome

--- app/assets/javascripts/hyrax/facets.js
+++ app/assets/javascripts/hyrax/facets.js
@@ -4,11 +4,11 @@ Blacklight.onLoad(function() {
    */
   $("li.expandable").click(function(){
     $(this).next("ul").slideToggle();
-    $(this).find('i').toggleClass("glyphicon-chevron-right glyphicon-chevron-down");
+    $(this).find('i').toggleClass("fa-chevron-right fa-chevron-down");
   });
 
   $("li.expandable_new").click(function(){
-    $(this).find('i').toggleClass("glyphicon-chevron-right glyphicon-chevron-down");
+    $(this).find('i').toggleClass("fa-chevron-right fa-chevron-down");
   });
 
 }); //end of Blacklight.onload
