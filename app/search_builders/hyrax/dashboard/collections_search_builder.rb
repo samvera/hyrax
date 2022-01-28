6792f5f1aee9456bb5ac307903dc4686a0ffb998
E. Lynette Rayle <elr37@cornell.edu>
2022-01-28 11:47:46 -0500

    add Hyrax::AdministrativeSets to the set of classes displayed on Dashboard->Collections

--- app/search_builders/hyrax/dashboard/collections_search_builder.rb
+++ app/search_builders/hyrax/dashboard/collections_search_builder.rb
@@ -9,7 +9,7 @@ module Hyrax
 
       # This overrides the models in FilterByType
       def models
-        [::AdminSet, ::Collection, Hyrax.config.collection_class].uniq.compact
+        [::AdminSet, Hyrax::AdministrativeSet, ::Collection, Hyrax.config.collection_class].uniq.compact
       end
 
       # adds a filter to exclude collections and admin sets created by the
