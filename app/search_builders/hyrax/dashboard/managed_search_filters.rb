d7d847ee9959fac6a31be065f9859eaf8dafa3ac
Fritz Freiheit <fritx@umich.edu>
2022-03-02 13:14:17 -0500

    Hyrax-5471 - Works in mediated workflow not listed as managed works f… (#5473)
    * Hyrax-5471 - Works in mediated workflow not listed as managed works for user with managing role

--- app/search_builders/hyrax/dashboard/managed_search_filters.rb
+++ app/search_builders/hyrax/dashboard/managed_search_filters.rb
@@ -11,15 +11,55 @@ module Hyrax
 
       # Override to exclude 'public' and 'registered' groups from read access.
       def apply_group_permissions(permission_types, ability = current_ability)
+        search_terms = add_managing_role_search_filter(ability: ability)
         groups = ability.user_groups
-        return [] if groups.empty?
-        permission_types.map do |type|
+        return search_terms if groups.empty?
+        permission_types.each do |type|
           field = solr_field_for(type, 'group')
-          user_groups = type == 'read' ? groups - [::Ability.public_group_name, ::Ability.registered_group_name] : groups
+          delete_groups = [::Ability.public_group_name, ::Ability.registered_group_name]
+          user_groups = type == 'read' ? groups - delete_groups : groups
           next if user_groups.empty?
-          "({!terms f=#{field}}#{user_groups.join(',')})" # parens required to properly OR the clauses together.
+          # parens required to properly OR the clauses together:
+          search_terms << "({!terms f=#{field}}#{user_groups.join(',')})"
         end
+        search_terms
       end
+
+      # Look for a user's managing role and add filters for all admin sets that have permission
+      # templates that include managing roles.
+      #
+      # rubocop:disable Metrics/MethodLength
+      # rubocop:disable Metrics/PerceivedComplexity
+      # rubocop:disable Metrics/CyclomaticComplexity
+      def add_managing_role_search_filter(ability:, search_terms: [])
+        search_terms ||= []
+        # Look for managing role assignement
+        managing_role = Sipity::Role.find_by(name: Hyrax::RoleRegistry::MANAGING)
+        return search_terms if managing_role.blank?
+        agent = ability.current_user.to_sipity_agent
+        return search_terms if agent.workflow_responsibilities.blank?
+        managing_workflow_roles = []
+        agent.workflow_responsibilities.each do |workflow_responsibility|
+          wfr = Sipity::WorkflowRole.find_by(id: workflow_responsibility.workflow_role_id)
+          managing_workflow_roles << wfr if wfr.role_id == managing_role.id
+        end
+        return search_terms if managing_workflow_roles.empty?
+        # if the user has managing responsibilties, then look up the associated admin set ids
+        admin_set_ids = managing_workflow_roles.map do |wfr|
+          wf = Sipity::Workflow.find_by(id: wfr.workflow_id)
+          pt = Hyrax::PermissionTemplate.find_by(id: wf.permission_template_id)
+          pt.source_id
+        end
+        admin_set_ids.uniq!
+        # create search terms for works that are in managed admin sets
+        admin_set_ids.each do |id|
+          search_terms << "isPartOf_ssim:#{id}"
+        end
+        search_terms
+      end
+      # rubocop:enable Metrics/MethodLength
+      # rubocop:enable Metrics/PerceivedComplexity
+      # rubocop:enable Metrics/CyclomaticComplexity
     end
   end
 end
