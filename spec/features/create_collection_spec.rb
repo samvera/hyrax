6a24b64dcff00832e26a9ed85d6c3f062c953540
Shaun Ellis <shaun@sdellis.com>
2022-03-16 11:32:15 -0400

    breaks up methods into smaller bits to satisfy rubocop complaints

--- spec/features/create_collection_spec.rb
+++ spec/features/create_collection_spec.rb
@@ -135,124 +135,87 @@ RSpec.describe 'Creating a new Admin Set', :js, :workflow, :clean_repo do
 
   def creator_create_collection(page)
     find('#add-new-collection-button').click
-    expect(page).to have_xpath("//h4", text: "User Collection")
-    expect(page).to have_xpath("//h4", text: "Other")
-    expect(page).to have_xpath("//h4", text: "Managed Collection")
-    choose "collection_type", option: "ManagedCollection"
-    click_button 'Create collection'
-    fill_in('Title', with: 'A Managed Collection')
-    fill_in('Description', with: "This collection was created by #{creator.user_key}")
-    click_on('Save')
-    expect(page).to have_content("Collection was successfully created.")
+    create_managed_collection(page)
 
-    # navigate to show page for the new collection
-    visit '/dashboard'
-    click_link('Collections', match: :first)
-    click_on('Display all details of A Managed Collection')
-
-    # confirm creating user can view and edit the new collection
-    expect(page).to have_xpath('//h2', text: 'A Managed Collection')
-    expect(page).to have_content("This collection was created by #{creator.user_key}")
-    expect(page).to have_link("Edit collection")
-
-    # confirm admin can view and edit the new collection
-    logout
-    sign_in admin
-    click_link('Collections', match: :first)
-    click_link 'All Collections'
-    click_on('Display all details of A Managed Collection')
-    expect(page).to have_xpath('//h2', text: 'A Managed Collection')
-    expect(page).to have_content("This collection was created by #{creator.user_key}")
-    expect(page).to have_link("Edit collection")
-
-    # confirm a collection type manager can view and edit the new collection
-    logout
-    sign_in manager
-    click_link('Collections', match: :first)
-    click_link 'Managed Collections'
-    click_on('Display all details of A Managed Collection')
-    expect(page).to have_xpath('//h2', text: 'A Managed Collection')
-    expect(page).to have_content("This collection was created by #{creator.user_key}")
-    expect(page).to have_link("Edit collection")
+    goto_new_collection_show_page(page)
+    confirm_user_can_view_edit(page)
+    test_manager(page)
   end
 
   def manager_create_collection(page)
     find('#add-new-collection-button').click
-    expect(page).to have_xpath("//h4", text: "User Collection")
-    expect(page).to have_xpath("//h4", text: "Other")
-    expect(page).to have_xpath("//h4", text: "Managed Collection")
-    choose "collection_type", option: "ManagedCollection"
-    click_button 'Create collection'
-    fill_in('Title', with: 'A Managed Collection')
-    fill_in('Description', with: "This collection was created by #{manager.user_key}")
-    click_on('Save')
-    expect(page).to have_content("Collection was successfully created.")
-
-    # navigate to show page for the new collection
-    visit '/dashboard'
-    click_link('Collections', match: :first)
-    click_on('Display all details of A Managed Collection')
-
-    # confirm creating user can view and edit the new collection
-    expect(page).to have_xpath('//h2', text: 'A Managed Collection')
-    expect(page).to have_content("This collection was created by #{manager.user_key}")
-    expect(page).to have_link("Edit collection")
-
-    # confirm admin can view and edit the new collection
-    logout
-    sign_in admin
-    click_link('Collections', match: :first)
-    click_link 'All Collections'
-    click_on('Display all details of A Managed Collection')
-    expect(page).to have_xpath('//h2', text: 'A Managed Collection')
-    expect(page).to have_content("This collection was created by #{manager.user_key}")
-    expect(page).to have_link("Edit collection")
-
-    # confirm a collection type creator can not view the new collection
-    logout
-    sign_in creator
-    click_link('Collections', match: :first)
-    expect(page).not_to have_content('Managed Collections')
-    expect(page).not_to have_content('A Managed Collection')
+    create_managed_collection(page)
+    goto_new_collection_show_page(page)
+    confirm_user_can_view_edit(page)
+    test_admin(page)
+    test_creator(page)
   end
 
   def admin_create_collection(page)
     find('#add-new-collection-button').click
-    expect(page).to have_xpath("//h4", text: "User Collection")
-    expect(page).to have_xpath("//h4", text: "Other")
-    expect(page).to have_xpath("//h4", text: "Managed Collection")
-    choose "collection_type", option: "ManagedCollection"
-    click_button 'Create collection'
-    fill_in('Title', with: 'A Managed Collection')
-    fill_in('Description', with: "This collection was created by #{admin.user_key}")
-    click_on('Save')
-    expect(page).to have_content("Collection was successfully created.")
-
-    # navigate to show page for the new collection
-    visit '/dashboard'
-    click_link('Collections', match: :first)
-    click_on('Display all details of A Managed Collection')
-
-    # confirm creating user can view and edit the new collection
-    expect(page).to have_xpath('//h2', text: 'A Managed Collection')
-    expect(page).to have_content("This collection was created by #{admin.user_key}")
-    expect(page).to have_link("Edit collection")
-
-    # confirm a collection type manager can view and edit the new collection
-    logout
-    sign_in manager
-    click_link('Collections', match: :first)
-    click_link 'Managed Collections'
-    click_on('Display all details of A Managed Collection')
-    expect(page).to have_xpath('//h2', text: 'A Managed Collection')
-    expect(page).to have_content("This collection was created by #{admin.user_key}")
-    expect(page).to have_link("Edit collection")
-
-    # confirm a collection type creator can not view the new collection
-    logout
-    sign_in creator
-    click_link('Collections', match: :first)
-    expect(page).not_to have_content('Managed Collections')
-    expect(page).not_to have_content('A Managed Collection')
+    create_managed_collection(page)
+    goto_new_collection_show_page(page)
+    confirm_user_can_view_edit(page)
+    test_manager(page)
+    test_creator(page)
   end
+end
+
+def test_creator(page)
+  # confirm a collection type creator can not view the new collection
+  logout
+  sign_in creator
+  click_link('Collections', match: :first)
+  expect(page).not_to have_content('Managed Collections')
+  expect(page).not_to have_content('A Managed Collection')
+end
+
+def test_manager(page)
+  # confirm a collection type manager can view and edit the new collection
+  logout
+  sign_in manager
+  click_link('Collections', match: :first)
+  click_link 'Managed Collections'
+  click_on('Display all details of A Managed Collection')
+  expect(page).to have_xpath('//h2', text: 'A Managed Collection')
+  expect(page).to have_content("This collection was created by #{admin.user_key}")
+  expect(page).to have_link("Edit collection")
+end
+
+def test_admin(page)
+  # confirm admin can view and edit the new collection
+  logout
+  sign_in admin
+  click_link('Collections', match: :first)
+  click_link 'All Collections'
+  click_on('Display all details of A Managed Collection')
+  expect(page).to have_xpath('//h2', text: 'A Managed Collection')
+  expect(page).to have_content("This collection was created by #{creator.user_key}")
+  expect(page).to have_link("Edit collection")
+end
+
+def goto_new_collection_show_page(page)
+  # navigate to show page for the new collection
+  visit '/dashboard'
+  click_link('Collections', match: :first)
+  click_on('Display all details of A Managed Collection')
+end
+
+def confirm_user_can_view_edit(page)
+  # confirm creating user can view and edit the new collection
+  expect(page).to have_xpath('//h2', text: 'A Managed Collection')
+  expect(page).to have_content("This collection was created by #{admin.user_key}")
+  expect(page).to have_link("Edit collection")
+end
+
+def create_managed_collection(page)
+  expect(page).to have_xpath("//h4", text: "User Collection")
+  expect(page).to have_xpath("//h4", text: "Other")
+  expect(page).to have_xpath("//h4", text: "Managed Collection")
+  choose "collection_type", option: "ManagedCollection"
+  click_button 'Create collection'
+  fill_in('Title', with: 'A Managed Collection')
+  fill_in('Description', with: "This collection was created by #{creator.user_key}")
+  click_on('Save')
+  expect(page).to have_content("Collection was successfully created.")
 end
