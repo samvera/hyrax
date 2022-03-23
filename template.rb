aa2f0ce7302a689bbb9d782c56274837133c85d4
Daniel Pierce <dlpierce@indiana.edu>
2022-03-23 11:20:28 -0400

    Prepare release 3.4.0

--- template.rb
+++ template.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 # Hack for https://github.com/rails/rails/issues/35153
 gsub_file 'Gemfile', /^gem ["']sqlite3["']$/, 'gem "sqlite3", "~> 1.3.0"'
-gem 'hyrax', '3.3.0'
+gem 'hyrax', '3.4.0'
 run 'bundle install'
 generate 'hyrax:install', '-f'
