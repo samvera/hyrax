be92092a09c030d8989932e29b237aa2d3cb810e
Alexandra Dunn <dunn.alex@gmail.com>
2022-04-18 12:23:37 -0700

    ValkyrieCreateDerivativesJob: fetch remote files for derivative svc

--- app/jobs/valkyrie_create_derivatives_job.rb
+++ app/jobs/valkyrie_create_derivatives_job.rb
@@ -1,14 +1,27 @@
 # frozen_string_literal: true
 class ValkyrieCreateDerivativesJob < Hyrax::ApplicationJob
   queue_as Hyrax.config.ingest_queue_name
-  def perform(_file_set_id, file_id, _filepath = nil)
+  def perform(file_set_id, file_id, _filepath = nil)
     file_metadata = Hyrax.custom_queries.find_file_metadata_by(id: file_id)
     return if file_metadata.video? && !Hyrax.config.enable_ffmpeg
     # Get file into a local path.
     file = Hyrax.storage_adapter.find_by(id: file_metadata.file_identifier)
     # Call derivatives with the file_set.
     derivative_service = Hyrax::DerivativeService.for(file_metadata)
-    derivative_service.create_derivatives(file.disk_path)
+
+    # Storage adapters that don't use the local disk (like Shrine) won't have a
+    # valid #disk_path, so we need to fetch them down first.
+    #
+    # TODO refactor the derivatives pipeline to not assume a local file path
+    diskpath = if File.exist? file.disk_path
+                 file.disk_path
+               else
+                 tmpfile = Tempfile.new(file_set_id, encoding: 'ascii-8bit')
+                 tmpfile.write file.read
+                 tmpfile
+               end
+
+    derivative_service.create_derivatives(diskpath)
     # Trigger a reindex to get the thumbnail path.
     Hyrax.publisher.publish('file.metadata.updated', metadata: file_metadata, user: nil)
   end
