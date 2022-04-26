5d097d04633d7d96c58fd921ec216b5fb285440b
Daniel Pierce <dlpierce@indiana.edu>
2022-04-26 12:16:25 -0400

    Detect correct upload path in .dassie

--- spec/jobs/characterize_job_spec.rb
+++ spec/jobs/characterize_job_spec.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 RSpec.describe CharacterizeJob, :clean_repo do
   let(:file_set_id) { 'abc12345' }
-  let(:filename)    { Rails.root.join('tmp', 'uploads', 'ab', 'c1', '23', '45', 'abc12345', 'picture.png').to_s }
+  let(:upload_root) {Pathname.new(ENV.fetch('HYRAX_UPLOAD_PATH', Rails.root.join('tmp', 'uploads')))}
+  let(:filename)    { upload_root.join('ab', 'c1', '23', '45', 'abc12345', 'picture.png').to_s }
   let(:label) { 'picture.png' }
   let(:title) { ['My User-Entered Title'] }
   let(:file_set) do
