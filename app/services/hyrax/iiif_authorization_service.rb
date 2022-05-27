f210b67cd9b31ee1a3c3e48023d0b0380508b90d
Ayoub Belemlih <13107510+abelemlih@users.noreply.github.com>
2022-05-27 13:44:08 -0400

    Upgrade to Rails 6

--- app/services/hyrax/iiif_authorization_service.rb
+++ app/services/hyrax/iiif_authorization_service.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 module Hyrax
-  class IIIFAuthorizationService
+  class IiifAuthorizationService
     attr_reader :controller
     def initialize(controller)
       @controller = controller
