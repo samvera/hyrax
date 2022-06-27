3fa47cb5b39b62c29a6788b953f78f06e5cf0280
tamsin johnson <tomjohnson@ucsb.edu>
2022-06-27 10:13:43 -0700

    remove deprecated method from InAdminSet

--- app/models/concerns/hyrax/in_admin_set.rb
+++ app/models/concerns/hyrax/in_admin_set.rb
@@ -6,12 +6,5 @@ module Hyrax
     included do
       belongs_to :admin_set, predicate: Hyrax.config.admin_set_predicate
     end
-
-    ##
-    # @deprecated Use `Sipity::Workflow.find_active_workflow_for` instead.
-    def active_workflow
-      Deprecation.warn 'Use `Sipity::Workflow.find_active_workflow_for` instead.'
-      Sipity::Workflow.find_active_workflow_for(admin_set_id: admin_set_id)
-    end
   end
 end
