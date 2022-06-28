9890add54fc58cab166e546c9f215d84621c843b
tamsin johnson <tomjohnson@ucsb.edu>
2022-06-28 16:29:17 -0700

    correctly derive paths for UploadedFileUploader
    `UploadedFileUploader`'s path generation was extremely fragile. for instance, it
    would generate an incorrect path if the configuration option didn't end with
    '/'.
    
    to generate a vaild in a system independent way, use `Pathname#/` and then cast
    back to a string.

--- app/uploaders/hyrax/uploaded_file_uploader.rb
+++ app/uploaders/hyrax/uploaded_file_uploader.rb
@@ -4,21 +4,21 @@ module Hyrax
     # Override the directory where uploaded files will be stored.
     # This is a sensible default for uploaders that are meant to be mounted:
     def store_dir
-      configured_upload_path + "#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+      (configured_upload_path / model.class.to_s.underscore / mounted_as.to_s / model.id.to_s).to_s
     end
 
     def cache_dir
-      configured_cache_path + "#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+      (configured_cache_path / model.class.to_s.underscore / mounted_as.to_s / model.id.to_s).to_s
     end
 
     private
 
     def configured_upload_path
-      Hyrax.config.upload_path.call
+      Pathname.new(Hyrax.config.upload_path.call)
     end
 
     def configured_cache_path
-      Hyrax.config.cache_path.call
+      Pathname.new(Hyrax.config.cache_path.call)
     end
   end
 end
