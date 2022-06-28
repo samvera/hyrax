f2968424342099ffe2e591af2557a37f2c833499
tamsin johnson <tomjohnson@ucsb.edu>
2022-06-28 12:36:15 -0700

    remove Blacklight 6 compatibility methods

--- app/controllers/concerns/hyrax/controller.rb
+++ app/controllers/concerns/hyrax/controller.rb
@@ -32,28 +32,6 @@ module Hyrax::Controller
     super.merge(locale: I18n.locale)
   end
 
-  ##
-  # @deprecated provides short-term compatibility with Blacklight 6
-  # @return [Class<Blacklight::SearchBuilder>]
-  def search_builder_class
-    return super if defined?(super)
-    Deprecation.warn("Avoid direct calls to `#search_builder_class`; this" \
-                     " method provides short-term compatibility to" \
-                     " Blacklight 6 clients.")
-    blacklight_config.search_builder_class
-  end
-
-  ##
-  # @deprecated provides short-term compatibility with Blacklight 6
-  # @return [Blacklight::AbstractRepository]
-  def repository
-    return super if defined?(super)
-    Deprecation.warn("Avoid direct calls to `#repository`; this method" \
-                     " provides short-term compatibility to Blacklight 6 " \
-                     " clients.")
-    blacklight_config.repository
-  end
-
   # @note for Blacklight 6/7 compatibility
   def search_results(*args)
     return super if defined?(super) # use the upstream if present (e.g. in BL 6)
