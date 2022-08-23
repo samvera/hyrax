30149e5bf1da0e53012793687f90951804ddf3c1
bwatson78 <brad.watson.orlando@gmail.com>
2022-08-23 18:12:59 -0400

    Trying again.

--- app/search_builders/hyrax/collection_member_search_builder.rb
+++ app/search_builders/hyrax/collection_member_search_builder.rb
@@ -30,7 +30,7 @@ module Hyrax
     # include filters into the query to only include the collection memebers
     def member_of_collection(solr_parameters)
       solr_parameters[:fq] ||= []
-      solr_parameters[:fq] << "#{collection_membership_field}:#{collection.id}"
+      solr_parameters[:fq] << "#{collection_membership_field}:#{@collection.id}"
     end
 
     private
