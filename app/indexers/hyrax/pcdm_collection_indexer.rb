0906567d10aded4ff6bdaa532338817843c41480
Chris Colvard <chris.colvard@gmail.com>
2022-09-09 11:33:55 -0400

    Index member_of_collection_ids

--- app/indexers/hyrax/pcdm_collection_indexer.rb
+++ app/indexers/hyrax/pcdm_collection_indexer.rb
@@ -15,6 +15,7 @@ module Hyrax
     def to_solr
       super.tap do |index_document|
         index_document[Hyrax.config.collection_type_index_field.to_sym] = Array(resource.try(:collection_type_gid)&.to_s)
+        index_document[:member_of_collection_ids_ssim] = resource.member_of_collection_ids.map(&:to_s)
         index_document[:generic_type_sim] = ['Collection']
         index_document[:depositor_ssim] = [resource.depositor]
         index_document[:depositor_tesim] = [resource.depositor]
