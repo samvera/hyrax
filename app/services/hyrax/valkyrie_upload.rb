ab879f7c394bfd15e69088b9f1b570741fd57fdc
Daniel Pierce <dlpierce@indiana.edu>
2022-09-15 14:31:08 -0400

    Fix missing file metadata preventing successful ingest

--- app/services/hyrax/valkyrie_upload.rb
+++ app/services/hyrax/valkyrie_upload.rb
@@ -37,6 +37,7 @@ class Hyrax::ValkyrieUpload
   def upload(filename:, file_set:, io:, use: Hyrax::FileMetadata::Use::ORIGINAL_FILE, user: nil)
     streamfile = storage_adapter.upload(file: io, original_filename: filename, resource: file_set)
     file_metadata = Hyrax::FileMetadata(streamfile)
+    file_metadata.file_set_id = file_set.id
     file_metadata.type << use
 
     if use == Hyrax::FileMetadata::Use::ORIGINAL_FILE
