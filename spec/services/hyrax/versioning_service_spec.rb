ad4c3200aaf1eb7feab182314150684643649a2e
kibigo! <kibigo@ucsb.edu>
2022-10-03 15:53:47 -0700

    Support VersioningService in VersionListPresenter
    Also adds tests for `#versions` in VersioningService in addition to
    the existing static method tests.

--- spec/services/hyrax/versioning_service_spec.rb
+++ spec/services/hyrax/versioning_service_spec.rb
@@ -9,7 +9,38 @@ RSpec.describe Hyrax::VersioningService do
       Hydra::Works::AddFileToFileSet.call(file, File.open(fixture_path + '/world.png'), :original_file, versioning: true)
     end
 
-    describe '#versioned_file_id' do
+    describe '#versions' do
+      subject do
+        described_class.new(resource: file.original_file).versions.map do |v|
+          Hyrax.config.translate_uri_to_id.call(v.uri)
+        end
+      end
+
+      context 'without version data' do
+        before do
+          allow(file.original_file).to receive(:has_versions?).and_return(false)
+        end
+        it { is_expected.to eq [] }
+      end
+
+      context 'with one version' do
+        it { is_expected.to eq ["#{file.original_file.id}/fcr:versions/version1"] }
+      end
+
+      context 'with two versions' do
+        before do
+          file.original_file.create_version
+        end
+        it {
+          is_expected.to eq [
+            "#{file.original_file.id}/fcr:versions/version1",
+            "#{file.original_file.id}/fcr:versions/version2"
+          ]
+        }
+      end
+    end
+
+    describe '.versioned_file_id' do
       subject { described_class.versioned_file_id file.original_file }
 
       context 'without version data' do
@@ -31,7 +62,7 @@ RSpec.describe Hyrax::VersioningService do
       end
     end
 
-    describe '#latest_version_of' do
+    describe '.latest_version_of' do
       subject { described_class.latest_version_of(file.original_file).label }
 
       context 'with one version' do
@@ -57,7 +88,43 @@ RSpec.describe Hyrax::VersioningService do
     end
     let(:file_metadata) { query_service.custom_queries.find_file_metadata_by(id: uploaded.id) }
 
-    describe '#versioned_file_id' do
+    describe '#versions' do
+      subject { described_class.new(resource: file_metadata).versions.map(&:id) }
+
+      context 'when versions are unsupported' do
+        before do
+          allow(storage_adapter).to receive(:supports?).and_return(false)
+        end
+        it { is_expected.to eq [] }
+      end
+
+      context 'without version data' do
+        before do
+          allow(storage_adapter).to receive(:supports?).and_return(true)
+          allow(storage_adapter).to receive(:find_versions).and_return([])
+        end
+        it { is_expected.to eq [] }
+      end
+
+      context 'with one version' do
+        it { is_expected.to eq ["#{uploaded.id}/fcr:versions/version1"] }
+      end
+
+      context 'with two versions' do
+        let(:another_file) { fixture_file_upload('/hyrax_generic_stub.txt') }
+        before do
+          storage_adapter.upload(resource: file_set, file: another_file, original_filename: 'filenew.txt')
+        end
+        it {
+          is_expected.to eq [
+            "#{uploaded.id}/fcr:versions/version1",
+            "#{uploaded.id}/fcr:versions/version2"
+          ]
+        }
+      end
+    end
+
+    describe '.versioned_file_id' do
       subject { described_class.versioned_file_id file_metadata }
 
       context 'when versions are unsupported' do
@@ -88,7 +155,7 @@ RSpec.describe Hyrax::VersioningService do
       end
     end
 
-    describe '#latest_version_of' do
+    describe '.latest_version_of' do
       subject { described_class.latest_version_of(file_metadata).id.to_s.split('/').last }
 
       context 'with one version' do
