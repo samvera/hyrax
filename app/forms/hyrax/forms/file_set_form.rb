cd69764bf397f07de448cc246d7eed67d7ac1d95
kibigo! <kibigo@ucsb.edu>
2022-10-13 16:30:55 -0700

    Make FileSetForm a ResourceForm
    This allows the sharing of common pieces of implementation with other
    Hyrax resource forms, like those for Hyrax::Works.

--- app/forms/hyrax/forms/file_set_form.rb
+++ app/forms/hyrax/forms/file_set_form.rb
@@ -3,8 +3,8 @@
 module Hyrax
   module Forms
     ##
-    # @api public
-    class FileSetForm < Hyrax::ChangeSet
+    # A form for +Hyrax::FileSet+s.
+    class FileSetForm < Hyrax::Forms::ResourceForm
       include Hyrax::FormFields(:core_metadata)
 
       # The fields in +:file_set_metadata+ were hardcoded into this form in a
@@ -12,26 +12,8 @@ module Hyrax
       # be configurable.
       include Hyrax::FormFields(:file_set_metadata)
 
-      class << self
-        ##
-        # @return [Array<Symbol>] list of required field names as symbols
-        def required_fields
-          definitions
-            .select { |_, definition| definition[:required] }
-            .keys.map(&:to_sym)
-        end
-      end
-
-      property :permissions, virtual: true
-      property :visibility, default: VisibilityIntention::PRIVATE
-
-      # virtual properties for embargo/lease;
-      property :embargo_release_date, virtual: true
-      property :visibility_after_embargo, virtual: true
-      property :visibility_during_embargo, virtual: true
-      property :lease_expiration_date, virtual: true
-      property :visibility_after_lease, virtual: true
-      property :visibility_during_lease, virtual: true
+      property :representative_id, type: Valkyrie::Types::String
+      property :thumbnail_id, type: Valkyrie::Types::String
     end
   end
 end
