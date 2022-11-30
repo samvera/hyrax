07d170f9562dd2a4c566f0c7c03321e7297fee27
Ben Pennell <bbpennel@email.unc.edu>
2022-11-30 11:55:49 -0500

    Add method for finding parent

--- app/controllers/hyrax/downloads_controller.rb
+++ app/controllers/hyrax/downloads_controller.rb
@@ -38,6 +38,16 @@ module Hyrax
       { type: mime_type_for(file), disposition: 'inline' }
     end
 
+    def parent(file_set: curation_concern)
+      @parent ||=
+        case file_set
+        when Hyrax::Resource
+          Hyrax.query_service.find_parents(resource: file_set).first
+        else
+          file_set.parent
+        end
+    end
+
     # Customize the :read ability in your Ability class, or override this method.
     # Hydra::Ability#download_permissions can't be used in this case because it assumes
     # that files are in a LDP basic container, and thus, included in the asset's uri.
@@ -45,7 +55,8 @@ module Hyrax
       authorize! :download, params[asset_param_key]
       # Deny access if the work containing this file is restricted by a workflow
       file_set = Hyrax.query_service.find_by_alternate_identifier(alternate_identifier: params[asset_param_key], use_valkyrie: Hyrax.config.use_valkyrie?)
-      return unless workflow_restriction?(file_set.parent, ability: current_ability)
+      file_set_parent = parent(file_set: file_set)
+      return unless workflow_restriction?(file_set_parent, ability: current_ability)
       raise Hyrax::WorkflowAuthorizationException
     rescue CanCan::AccessDenied, Hyrax::WorkflowAuthorizationException
       unauthorized_image = Rails.root.join("app", "assets", "images", "unauthorized.png")
