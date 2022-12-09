35340d5f9109d7ee35984cc37f23596f10300085
Daniel Pierce <dlpierce@indiana.edu>
2022-12-09 12:43:00 -0500

    Workaround missing human_readable_type method on Wings converted work
    curation_concern here *should* have #human_readable_type defined, but is is lost somehow. I think this is from the Wings conversion.
    
    Luckily, all that method does is call the class method, so let's do that here to avoid the problem.

--- app/controllers/hyrax/workflow_actions_controller.rb
+++ app/controllers/hyrax/workflow_actions_controller.rb
@@ -40,7 +40,7 @@ module Hyrax
 
     def after_update_response
       respond_to do |wants|
-        wants.html { redirect_to [main_app, curation_concern], notice: "The #{curation_concern.human_readable_type} has been updated." }
+        wants.html { redirect_to [main_app, curation_concern], notice: "The #{curation_concern.class.human_readable_type} has been updated." }
         wants.json { render 'hyrax/base/show', status: :ok, location: polymorphic_path([main_app, curation_concern]) }
       end
     end
