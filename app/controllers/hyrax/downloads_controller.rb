e9a8f0cc16622035deb9c94f224efcbfb2535998
Rob Kaufman <rob@notch8.com>
2023-03-22 09:19:56 -0500

    handle differences between wings adapter api and direct valkarie api in file downloads

--- app/controllers/hyrax/downloads_controller.rb
+++ app/controllers/hyrax/downloads_controller.rb
@@ -42,7 +42,11 @@ module Hyrax
     end
 
     def file_set_parent(file_set_id)
-      file_set = Hyrax.query_service.find_by_alternate_identifier(alternate_identifier: file_set_id, use_valkyrie: Hyrax.config.use_valkyrie?)
+      file_set = if defined?(Wings) && Hyrax.metadata_adapter.is_a?(Wings::Valkyrie::MetadataAdapter)
+                   Hyrax.query_service.find_by_alternate_identifier(alternate_identifier: file_set_id, use_valkyrie: Hyrax.config.use_valkyrie?)
+                 else
+                   Hyrax.query_service.find_by(id: file_set_id)
+                 end
       @parent ||=
         case file_set
         when Hyrax::Resource
