6013b67f3918a28f827dcb6e6c431c5ee9f446db
Rob Kaufman <rob@notch8.com>
2023-03-22 19:35:08 -0500

    stream data, skip if etags are valid, adjust specs

--- spec/controllers/hyrax/downloads_controller_spec.rb
+++ spec/controllers/hyrax/downloads_controller_spec.rb
@@ -6,9 +6,9 @@ RSpec.describe Hyrax::DownloadsController do
     let(:user) { create(:user) }
     let(:file_set) do
       if Hyrax.config.use_valkyrie?
-        FactoryBot.valkyrie_create
+        FactoryBot.valkyrie_create(:hyrax_file_set, :in_work, edit_users: [user], visibility_setting: Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_AUTHENTICATED)
       else
-        create(:file_with_work, user: user, content: File.open(fixture_path + '/image.png'))
+        create(:file_with_work, user: user, content: original_file)
       end
     end
 
@@ -21,7 +21,10 @@ RSpec.describe Hyrax::DownloadsController do
     context "when user doesn't have access" do
       let(:another_user) { create(:user) }
 
-      before { sign_in another_user }
+      before do
+        sign_in another_user
+        allow_any_instance_of(Hyrax::DownloadsController).to receive(:workflow_restriction?).and_return(true)
+      end
 
       it 'returns :unauthorized status with image content' do
         get :show, params: { id: file_set.to_param }
@@ -42,19 +45,35 @@ RSpec.describe Hyrax::DownloadsController do
           expect(response.content_type).to eq 'image/png'
         end
       end
-
-      it 'authorizes the resource using only the id' do
-        expect(controller).to receive(:authorize!).with(:download, file_set.id)
-        get :show, params: { id: file_set.to_param }
-      end
     end
 
     context "when the user has access" do
-      before { sign_in user }
+      before do
+        sign_in user
+        allow_any_instance_of(Hyrax::DownloadsController).to receive(:workflow_restriction?).and_return(false)
+      end
 
-      it 'sends the original file' do
-        get :show, params: { id: file_set }
-        expect(response.body).to eq file_set.original_file.content
+      context 'with original file' do
+        let(:original_file_use)  { Hyrax::FileMetadata::Use::ORIGINAL_FILE }
+        let(:thumbnail_use)      { Hyrax::FileMetadata::Use::THUMBNAIL }
+        let(:file_path) { fixture_path + '/image.png' }
+        let(:original_file_metadata) { FactoryBot.valkyrie_create(:hyrax_file_metadata, use: original_file_use, file_identifier: "disk://#{file_path}") }
+        let(:thumbnail_file_metadata) { FactoryBot.valkyrie_create(:hyrax_file_metadata, use: thumbnail_use, file_identifier: "disk://#{file_path}") }
+        let(:original_file) { File.open(file_path) }
+
+        let(:file_set) do
+          if Hyrax.config.use_valkyrie?
+            FactoryBot.valkyrie_create(:hyrax_file_set, :in_work, files: [original_file_metadata, thumbnail_file_metadata], edit_users: [user],
+                                                                  visibility_setting: Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_AUTHENTICATED)
+          else
+            create(:file_with_work, user: user, content: original_file)
+          end
+        end
+
+        it 'sends the original file' do
+          get :show, params: { id: file_set }
+          expect(response.body).to eq IO.binread(file_path)
+        end
       end
 
       context 'when restricted by workflow' do
@@ -71,6 +90,26 @@ RSpec.describe Hyrax::DownloadsController do
 
       context "with an alternative file" do
         context "that is persisted" do
+          let(:original_file_use)  { Hyrax::FileMetadata::Use::ORIGINAL_FILE }
+          let(:thumbnail_use)      { Hyrax::FileMetadata::Use::THUMBNAIL }
+          let(:file_path) { fixture_path + '/world.png' }
+          let(:original_file_metadata) do
+            FactoryBot.valkyrie_create(:hyrax_file_metadata, mime_type: 'image/png', original_filename: 'world.png', use: original_file_use, file_identifier: "disk://#{file_path}")
+          end
+          let(:thumbnail_file_metadata) do
+            FactoryBot.valkyrie_create(:hyrax_file_metadata, use: thumbnail_use, mime_type: 'image/png', original_filename: 'world.png', file_identifier: "disk://#{file_path}")
+          end
+          let(:original_file) { File.open(file_path) }
+
+          let(:file_set) do
+            if Hyrax.config.use_valkyrie?
+              FactoryBot.valkyrie_create(:hyrax_file_set, :in_work, files: [original_file_metadata, thumbnail_file_metadata], edit_users: [user],
+                                                                    visibility_setting: Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_AUTHENTICATED)
+            else
+              create(:file_with_work, user: user, content: original_file)
+            end
+          end
+
           let(:file) { File.open(fixture_path + '/world.png', 'rb') }
           let(:content) { file.read }
 
@@ -82,7 +121,7 @@ RSpec.describe Hyrax::DownloadsController do
             get :show, params: { id: file_set, file: 'thumbnail' }
             expect(response).to be_successful
             expect(response.body).to eq content
-            expect(response.headers['Content-Length']).to eq "4218"
+            expect(response.headers['Content-Length']).to eq '4218'
             expect(response.headers['Accept-Ranges']).to eq "bytes"
           end
 
@@ -99,7 +138,7 @@ RSpec.describe Hyrax::DownloadsController do
             it "head request" do
               request.env["HTTP_RANGE"] = 'bytes=0-15'
               head :show, params: { id: file_set, file: 'thumbnail' }
-              expect(response.headers['Content-Length']).to eq '4218'
+              expect(response.headers['Content-Length']).to eq '16'
               expect(response.headers['Accept-Ranges']).to eq 'bytes'
               expect(response.headers['Content-Type']).to start_with 'image/png'
             end
@@ -141,16 +180,18 @@ RSpec.describe Hyrax::DownloadsController do
         context "that isn't persisted" do
           it "raises an error if the requested file does not exist" do
             expect do
-              get :show, params: { id: file_set, file: 'thumbnail' }
+              get :show, params: { id: file_set.to_param, file: 'thumbnail' }
             end.to raise_error Hyrax::ObjectNotFoundError
           end
         end
       end
 
-      it "raises an error if the requested association does not exist" do
-        expect do
-          get :show, params: { id: file_set, file: 'non-existant' }
-        end.to raise_error Hyrax::ObjectNotFoundError
+      context 'no association' do
+        it "raises an error if the requested association does not exist" do
+          expect do
+            get :show, params: { id: file_set, file: 'non-existant' }
+          end.to raise_error Hyrax::ObjectNotFoundError
+        end
       end
     end
   end
