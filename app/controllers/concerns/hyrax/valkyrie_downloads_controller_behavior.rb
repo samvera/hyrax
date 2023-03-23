ad378b46405b30327a10ee9b50d5e597dd04cc07
Rob Kaufman <rob@notch8.com>
2023-03-22 19:35:08 -0500

    stream data, skip if etags are valid, adjust specs

--- app/controllers/concerns/hyrax/valkyrie_downloads_controller_behavior.rb
+++ app/controllers/concerns/hyrax/valkyrie_downloads_controller_behavior.rb
@@ -14,16 +14,28 @@ module Hyrax
       self.status = 200
       use = params.fetch(:file, :original_file).to_sym
       file_metadata = find_file_metadata(file_set: file_set, use: use)
+      return unless stale?(last_modified: file_metadata.updated_at, template: false)
+
       file = Hyrax.storage_adapter.find_by(id: file_metadata.file_identifier)
       prepare_file_headers_valkyrie(metadata: file_metadata, file: file)
+
       # Warning - using the range header will load the range selection in to memory
       # this can cause memory bloat
       if request.headers['Range']
         file.rewind
-        send_range_valkyrie(file: file)
+        send_data send_range_valkyrie(file: file), data_options(file_metadata)
       else
         send_file file.disk_path
       end
+    end
+
+    def data_options(file_metadata)
+      {
+        type: file_metadata.mime_type,
+        filename: file_metadata.original_filename,
+        disposition: "inline",
+        status: status
+      }
     end
 
     def send_range_valkyrie(file:)
@@ -35,14 +47,14 @@ module Hyrax
       response.headers['Content-Length'] = length.to_s
       self.status = 206
       file.read from # Seek to start of requested range
-      self.response_body = file.read length
+      file.read length
     end
 
     def prepare_file_headers_valkyrie(metadata:, file:, inline: false)
       inline_display = ActiveRecord::Type::Boolean.new.cast(params.fetch(:inline, inline))
       response.headers["Content-Disposition"] = "#{inline_display ? 'inline' : 'attachment'}; filename=#{metadata.original_filename}"
       response.headers["Content-Type"] = metadata.mime_type
-      response.headers["Content-Length"] ||= (file.try(:size) || metadata.size.first)
+      response.headers["Content-Length"] ||= (file.try(:size) || metadata.size.first).to_s
       # Prevent Rack::ETag from calculating a digest over body
       response.headers["Last-Modified"] = metadata.updated_at.utc.strftime("%a, %d %b %Y %T GMT")
       self.content_type = metadata.mime_type
@@ -50,7 +62,11 @@ module Hyrax
 
     def find_file_metadata(file_set:, use: :original_file)
       use = :thumbnail_file if use == :thumbnail
-      use = Hyrax::FileMetadata::Use.uri_for(use: use)
+      begin
+        use = Hyrax::FileMetadata::Use.uri_for(use: use)
+      rescue ArgumentError
+        raise Hyrax::ObjectNotFoundError
+      end
       results = Hyrax.custom_queries.find_many_file_metadata_by_use(resource: file_set, use: use)
       results.first || raise(Hyrax::ObjectNotFoundError)
     end
