3262b0f87028b3a241b596a7e22635fc71eb8834
Ben Pennell <bbpennel@email.unc.edu>
2023-03-31 17:01:44 -0400

    Switch workflows page over to using separate page requests for tabs. Restructure the response for workflow requests to mirror that of a blacklight response. Add method for getting total number of entities in workflow

--- app/services/hyrax/workflow/actionable_objects.rb
+++ app/services/hyrax/workflow/actionable_objects.rb
@@ -20,11 +20,27 @@ module Hyrax
       # @!attribute [rw] user
       #   @return [::User]
       attr_accessor :user
+      ##
+      # @!attribute [rw] workflow_state_filter
+      #   @return [String]
+      attr_accessor :workflow_state_filter
+      ##
+      # @!attribute [rw] page of results to return, 1 based
+      #   @return [Integer]
+      attr_accessor :page
+      ##
+      # @!attribute [rw] per_page number of results in the page
+      #   @return [Integer]
+      attr_accessor :per_page
 
       ##
       # @param [::User] user the user whose
-      def initialize(user:)
+      # @param [String] optional filter by workstate name
+      def initialize(user:, workflow_state_filter: nil)
         @user = user
+        @workflow_state_filter = workflow_state_filter
+        @page = 1
+        @per_page = 10
       end
 
       ##
@@ -46,6 +62,13 @@ module Hyrax
         end
       end
 
+      ##
+      # @return [Integer] total number of entities selected
+      def total_count
+        PermissionQuery.scope_entities_for_the_user(user: user, workflow_state_filter: workflow_state_filter)
+                       .count
+      end
+
       private
 
       ##
@@ -53,7 +76,7 @@ module Hyrax
       # @return [Array[String, Sipity::WorkflowState]]
       def id_state_pairs
         gids_and_states = PermissionQuery
-                          .scope_entities_for_the_user(user: user)
+                          .scope_entities_for_the_user(user: user, page: page, per_page: per_page, workflow_state_filter: workflow_state_filter)
                           .pluck(:proxy_for_global_id, :workflow_state_id)
 
         return [] if gids_and_states.none?
