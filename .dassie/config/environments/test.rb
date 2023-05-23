017a2b51f23b48eec737a1f186a51a045ef14d02
Chris Colvard <chris.colvard@gmail.com>
2023-05-23 14:52:45 -0400

    Allow dassie to run with rails 7

--- .dassie/config/environments/test.rb
+++ .dassie/config/environments/test.rb
@@ -44,7 +44,7 @@ Rails.application.configure do
   config.active_support.deprecation = :stderr
 
   # Raises error for missing translations
-  config.action_view.raise_on_missing_translations = true
+  #config.action_view.raise_on_missing_translations = true
 
   # disable animations with shared hyrax code
   config.middleware.use DisableAnimationsInTestEnvironment
