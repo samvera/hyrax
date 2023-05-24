fb5451f216c8a787f8e46e0d7248e11fca069660
tamsin johnson <tomjohnson@ucsb.edu>
2023-05-24 10:50:06 -0700

    remove unneeded `config[:thread_safe] = true` from redis config (#6062)
    the `Redis` gem has set `:thread_safe` to `true` by default since 2.2.0. Hyrax
    depends on `> 3`, so does not need to set this value explictly.

--- .koppie/config/initializers/redis_config.rb
+++ .koppie/config/initializers/redis_config.rb
@@ -2,7 +2,6 @@
 require 'redis'
 require 'connection_pool'
 config = YAML.safe_load(ERB.new(IO.read(Rails.root.join('config', 'redis.yml'))).result)[Rails.env].with_indifferent_access
-config[:thread_safe] = true
 
 size = ENV.fetch("HYRAX_REDIS_POOL_SIZE", 5)
 timeout = ENV.fetch("HYRAX_REDIS_TIMEOUT", 5)
