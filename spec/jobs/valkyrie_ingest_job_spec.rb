8dadfe7970c678aaeae9263f5338badf834eb859
Summer Cook <summer@scientist.com>
2023-06-29 18:54:28 -0300

    fix spacing

--- spec/jobs/valkyrie_ingest_job_spec.rb
+++ spec/jobs/valkyrie_ingest_job_spec.rb
@@ -76,7 +76,7 @@ RSpec.describe ValkyrieIngestJob do
         expect(reloaded_file_set.title).to eq ["image.png"]
         expect(reloaded_file_set.label).to eq "image.png"
         # TODO: this is currently failing on main. Needs to be debugged.
-        #e xpect(reloaded_file_set.file_ids)
+        # expect(reloaded_file_set.file_ids)
           # .to contain_exactly(reloaded_file_set.original_file_id, reloaded_file_set.thumbnail_id)
       end
     end
