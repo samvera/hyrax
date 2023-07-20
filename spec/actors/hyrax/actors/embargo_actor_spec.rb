216a695051e7f7f89d6ed01ff5c74d0640782249
Alisha Evans <alisha@scientist.com>
2023-07-20 17:44:52 -0500

    fix the specs in embargo_actor_spec.rb

--- spec/actors/hyrax/actors/embargo_actor_spec.rb
+++ spec/actors/hyrax/actors/embargo_actor_spec.rb
@@ -4,64 +4,41 @@ RSpec.describe Hyrax::Actors::EmbargoActor do
   let(:authenticated_vis) { Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_AUTHENTICATED }
   let(:public_vis) { Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC }
 
-  describe "#destroy" do
-    let(:work) do
-      FactoryBot.valkyrie_create(:hyrax_resource, embargo: embargo)
-    end
+  describe '#destroy' do
+    let(:work) { FactoryBot.valkyrie_create(:hyrax_resource, embargo: embargo) }
     let(:embargo) { FactoryBot.create(:hyrax_embargo) }
 
-    before do
-      work.visibility = authenticated_vis
-      Hyrax::AccessControlList(work).save
-    end
-
-    it "removes the embargo" do
-      expect { actor.destroy }
-        .to change { Hyrax::EmbargoManager.new(resource: work).under_embargo? }
-        .from(true)
-        .to false
-    end
-
-    it "change the visibility" do
-      expect { actor.destroy }
-        .to change { work.visibility }
-        .from(authenticated_vis)
-        .to public_vis
-    end
-
-    context "with an expired embargo" do
-      let(:work) do
-        FactoryBot.valkyrie_create(:hyrax_resource, embargo: embargo)
-      end
-
-      let(:embargo) { FactoryBot.create(:hyrax_embargo) }
+    context 'on a Valkyrie backed model', if: Hyrax.config.use_valkyrie? do
       let(:embargo_manager) { Hyrax::EmbargoManager.new(resource: work) }
-      let(:embargo_release_date) { work.embargo.embargo_release_date }
+      let(:active_embargo_release_date) { work.embargo.embargo_release_date }
 
       before do
-        allow(Hyrax::TimeService)
-          .to receive(:time_in_utc)
-          .and_return(embargo_release_date.to_datetime + 1)
-        expect(embargo_manager.under_embargo?).to eq false
+        work.visibility = authenticated_vis
+        Hyrax::AccessControlList(work).save
       end
 
-      it "removes the embargo" do
+      it 'removes the embargo' do
         expect { actor.destroy }
-          .to change { work.embargo.embargo_release_date }
-          .from(embargo_release_date)
-          .to nil
+          .to change { embargo_manager.under_embargo? }
+          .from(true)
+          .to false
       end
 
-      it "releases the embargo" do
-        expect(embargo_manager.enforced?).to eq true
+      it 'releases the embargo' do
         expect { actor.destroy }
           .to change { embargo_manager.enforced? }
           .from(true)
           .to false
       end
 
-      it "changes the visibility" do
-        expect(work.embargo.visibility_after_embargo).to eq public_vis
+      it 'changes the embargo release date' do
+        expect { actor.destroy }
+          .to change { work.embargo.embargo_release_date }
+          .from(active_embargo_release_date)
+          .to nil
+      end
+
+      it 'changes the visibility' do
         expect { actor.destroy }
           .to change { work.visibility }
           .from(authenticated_vis)
@@ -69,23 +46,23 @@ RSpec.describe Hyrax::Actors::EmbargoActor do
       end
     end
 
-    context "with a ActiveFedora model" do
+    context 'with an ActiveFedora model', unless: Hyrax.config.use_valkyrie? do
       let(:work) do
         GenericWork.new do |work|
           work.apply_depositor_metadata 'foo'
-          work.title = ["test"]
+          work.title = ['test']
           work.visibility =
             work.visibility_during_embargo = authenticated_vis
           work.visibility_after_embargo = public_vis
-          work.embargo_release_date = release_date.to_s
+          work.embargo_release_date = embargo_release_date.to_s
           work.save(validate: false)
         end
       end
 
-      context "with an active embargo" do
-        let(:release_date) { Time.zone.today + 2 }
+      context 'with an active embargo' do
+        let(:embargo_release_date) { Time.zone.today + 2 }
 
-        it "removes the embargo" do
+        it 'removes the embargo' do
           actor.destroy
           expect(work.reload.embargo_release_date).to be_nil
           expect(work.visibility).to eq authenticated_vis
@@ -93,9 +70,9 @@ RSpec.describe Hyrax::Actors::EmbargoActor do
       end
 
       context 'with an expired embargo' do
-        let(:release_date) { Time.zone.today - 2 }
+        let(:embargo_release_date) { Time.zone.today - 2 }
 
-        it "removes the embargo" do
+        it 'removes the embargo' do
           actor.destroy
           expect(work.reload.embargo_release_date).to be_nil
           expect(work.visibility).to eq public_vis
