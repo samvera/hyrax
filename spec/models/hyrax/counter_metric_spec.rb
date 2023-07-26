a6680a1cc975303aed367f1762f6e4f0cadf2617
Summer Cook <summer@scientist.com>
2023-07-26 11:24:42 -0300

    schema change and spec

--- spec/models/hyrax/counter_metric_spec.rb
+++ spec/models/hyrax/counter_metric_spec.rb
@@ -1,7 +1,32 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 module Hyrax
   RSpec.describe CounterMetric, type: :model do
-    pending "add some examples to (or delete) #{__FILE__}"
+    context 'validations' do
+      it 'is valid with valid attributes' do
+        counter_metric = build(:counter_metric)
+        expect(counter_metric).to be_valid
+      end
+    end
+
+    # TODO: figure out which fields are required,
+    # add any validations to the model, unskip these + add any other required fields
+    context 'required fields' do
+      xit 'is not valid without worktype' do
+        counter_metric = build(:counter_metric, worktype: nil)
+        expect(counter_metric).not_to be_valid
+        expect(counter_metric.errors[:worktype]).to include("can't be blank")
+      end
+
+      xit 'is not valid without resource_type' do
+        counter_metric = build(:counter_metric, resource_type: nil)
+        expect(counter_metric).not_to be_valid
+        expect(counter_metric.errors[:resource_type]).to include("can't be blank")
+      end
+
+      # Repeat similar tests for other required fields (work_id, date, total_item_investigations, total_item_requests)
+    end
   end
 end
