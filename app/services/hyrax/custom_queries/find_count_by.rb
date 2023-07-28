92ead3a248c40db2857d8709ca9968afbe1e7089
Summer <73361970+summer-cook@users.noreply.github.com>
2023-07-28 14:43:21 -0300

    5592 dashboard statistics page error (#6088)
    * set up relation and custom query
    * add in ability to count by model or property
    * get date search working
    * fix id check in active fedora converter for specs
    * use standard fedora relation if Wings is being used in all locations
    
    ---------
    
    Co-authored-by: braydonjustice <braydonjustice@gmail.com>
    Co-authored-by: Alisha Evans <alisha@scientist.com>
    Co-authored-by: Rob Kaufman <rob@notch8.com>

---
+++ app/services/hyrax/custom_queries/find_count_by.rb
@@ -0,0 +1,62 @@
+# frozen_string_literal: true
+module Hyrax
+  module CustomQueries
+    ##
+    # @see https://github.com/samvera/valkyrie/wiki/Queries#custom-queries
+    class FindCountBy
+      def self.queries
+        [:find_count_by]
+      end
+
+      def initialize(query_service:)
+        @query_service = query_service
+      end
+
+      attr_reader :query_service
+      delegate :resource_factory, to: :query_service
+      delegate :orm_class, to: :resource_factory
+
+      ##
+      # @note this is an unoptimized default implementation of this custom
+      #   query. it's Hyrax's policy to provide such implementations of custom
+      #   queries in use for cross-compatibility of Valkyrie query services.
+      #   it's advisable to provide an optimized query for the specific adapter.
+      #
+      # @param hash [Hash] the hash representation of the query
+      def find_count_by(hash = {}, models: nil)
+        return nil if models.empty? && hash.blank?
+
+        internal_array = ["{ #{hash.map { |k, v| "\"#{k}\": #{v}" }.join(', ')} }"] if hash.present?
+        if models.empty?
+          query_service.orm_class.count_by_sql(([find_count_by_properties_query] + internal_array))
+        elsif hash.blank?
+          query_service.orm_class.count_by_sql([find_count_by_models_query] + [models])
+        else
+          query_service.orm_class.count_by_sql(([find_count_by_properties_and_models_query] + internal_array + [models]))
+        end
+      end
+
+      def find_count_by_properties_and_models_query
+        <<-SQL
+          SELECT count(*) FROM orm_resources
+          WHERE metadata @> ?
+          AND internal_resource IN (?);
+        SQL
+      end
+
+      def find_count_by_models_query
+        <<-SQL
+          SELECT count(*) FROM orm_resources
+          WHERE internal_resource IN (?);
+        SQL
+      end
+
+      def find_count_by_properties_query
+        <<-SQL
+          SELECT count(*) FROM orm_resources
+          WHERE metadata @> ?;
+        SQL
+      end
+    end
+  end
+end
