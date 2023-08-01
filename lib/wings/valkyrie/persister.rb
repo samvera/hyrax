9b64c54761216dcef982e2157539638662faa90b
Alisha Evans <alisha@scientist.com>
2023-07-31 22:52:21 -0500

    disable validation on the Active Fedora object in Wings::Valkyrie::Persister#save because we have already done validation as a Valkyrie object.
    co-authored-by: rob <rob@scientist.com>

--- lib/wings/valkyrie/persister.rb
+++ lib/wings/valkyrie/persister.rb
@@ -31,7 +31,9 @@ module Wings
         # the #save! api differs between ActiveFedora::Base and ActiveFedora::File objects,
         # if we get a falsey response, we expect we have a File that has failed to save due
         # to empty content
-        af_object.save! ||
+        # we disable validation on the Active Fedora object
+        # because we've already done validation as a Valkyrie object
+        af_object.save!(validate: false) ||
           raise(FailedSaveError.new("#{af_object.class}#save! returned non-true. It might be missing required content.", obj: af_object))
 
         resource_factory.to_resource(object: af_object)
