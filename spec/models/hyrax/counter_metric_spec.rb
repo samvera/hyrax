8b3c63ecaeda06e7bb86c1295833f5e34c10a2b8
Summer Cook <summer@scientist.com>
2023-08-01 10:13:35 -0300

    update regen again, and remove skips from spec

--- spec/models/hyrax/counter_metric_spec.rb
+++ spec/models/hyrax/counter_metric_spec.rb
@@ -8,20 +8,20 @@ require 'rails_helper'
 # Once https://github.com/samvera/hyrax/issues/6125 is completed, these tests should pass in CI and can be unskipped.
 RSpec.describe Hyrax::CounterMetric, type: :model do
   context 'validations' do
-    xit 'is valid with valid attributes' do
+    it 'is valid with valid attributes' do
       counter_metric = build(:counter_metric)
       expect(counter_metric).to be_valid
     end
   end
 
   context 'required fields' do
-    xit 'is not valid without work_id' do
+    it 'is not valid without work_id' do
       counter_metric = build(:counter_metric, work_id: nil)
       expect(counter_metric).not_to be_valid
       expect(counter_metric.errors[:work_id]).to include("can't be blank")
     end
 
-    xit 'is not valid without date' do
+    it 'is not valid without date' do
       counter_metric = build(:counter_metric, date: nil)
       expect(counter_metric).not_to be_valid
       expect(counter_metric.errors[:date]).to include("can't be blank")
