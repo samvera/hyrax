ad4f2475d2eec5216bca83fff646d386046e2384
Alisha Evans <alisha@scientist.com>
2023-08-02 22:46:57 -0500

    also remove the `:id` attribute when coercing the resource lease into the af_object lease inside #apply_attributes_to_model.this avoids the "Attempting to recreate existing ldp_source" error in dozens of specs

--- lib/wings/active_fedora_converter.rb
+++ lib/wings/active_fedora_converter.rb
@@ -146,8 +146,8 @@ module Wings
           # af_object.lease.class has the same name as resource.lease.class; however, each class has a different object_id
           # so a type mismatch happens. the code below coerces the one object into the other
           # TODO(alishaevn): fix this!
-          resource_lease = af_object.reflections.fetch(:lease).klass.new(resource.lease.attributes.except(:internal_resource, :created_at, :updated_at, :new_record))
-          af_object.lease = resource_lease
+          resource_lease_dup = af_object.reflections.fetch(:lease).klass.new(resource.lease.attributes.except(:id, :internal_resource, :created_at, :updated_at, :new_record))
+          af_object.lease = resource_lease_dup
         end
         members.empty? ? af_object.try(:ordered_members)&.clear : af_object.try(:ordered_members=, members)
         af_object.try(:members)&.replace(members)
