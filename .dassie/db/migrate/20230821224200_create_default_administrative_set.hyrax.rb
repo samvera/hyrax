ab3185d3069f51160c717fbf6c6de4112f69b42a
Summer Cook <summer@scientist.com>
2023-08-21 19:45:14 -0300

    remove duplicates

--- .dassie/db/migrate/20230821224200_create_default_administrative_set.hyrax.rb
+++
@@ -1,8 +0,0 @@
-class CreateDefaultAdministrativeSet < ActiveRecord::Migration[5.2]
-  def change
-    create_table :hyrax_default_administrative_set do |t|
-      t.string :default_admin_set_id, null: false
-      t.timestamps null: false
-    end
-  end
-end
