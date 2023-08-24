14045b78c3daf4ea20b5154bea563c5fe646e026
Trey Pendragon <tpendragon@princeton.edu>
2023-08-24 14:52:10 -0700

    Use the presenter, not a form.

--- spec/features/file_manager_spec.rb
+++ spec/features/file_manager_spec.rb
@@ -26,11 +26,8 @@ RSpec.describe "file manager" do
     # has a link back to parent
     expect(page).to have_link work.to_s, href: hyrax_monograph_path(id: work.id, locale: :en)
 
-    # has thumbnails for each resource
-    # expect(page).to have_selector("img[src='/test/image/path.jpg']")
-
     # renders a form for each member
-    expect(page).to have_selector("#sortable form", count: members.length)
+    expect(page).to have_selector("#sortable form", count: work.member_ids.length)
 
     # renders an input for titles
     expect(page).to have_selector("input[name='file_set[title][]']")
@@ -39,15 +36,15 @@ RSpec.describe "file manager" do
     expect(page).to have_selector("form#resource-form")
 
     # renders a hidden field for the resource form thumbnail id
-    expect(page).to have_selector("#resource-form input[type=hidden][name='generic_work[thumbnail_id]']", visible: false)
+    expect(page).to have_selector("#resource-form input[type=hidden][name='monograph[thumbnail_id]']", visible: false)
 
     # renders a thumbnail field for each member
-    expect(page).to have_selector("input[name='thumbnail_id']", count: members.length)
+    expect(page).to have_selector("input[name='thumbnail_id']", count: work.member_ids.length)
 
     # renders a hidden field for the resource form representative id
-    expect(page).to have_selector("#resource-form input[type=hidden][name='generic_work[representative_id]']", visible: false)
+    expect(page).to have_selector("#resource-form input[type=hidden][name='monograph[representative_id]']", visible: false)
 
     # renders a representative field for each member
-    expect(page).to have_selector("input[name='representative_id']", count: members.length)
+    expect(page).to have_selector("input[name='representative_id']", count: work.member_ids.length)
   end
 end
