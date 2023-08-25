8b003fbeabecece98903a8ce561811c425124c05
Trey Pendragon <tpendragon@princeton.edu>
2023-08-25 13:53:33 -0700

    Fix member_count and member_presenters specs.
    We decided to return an array for member_presenters instead of an
    enumerable, because that's the contract that the old presenter had.
    
    Co-authored-by: Anna Headley <hackartisan@users.noreply.github.com>
    Co-authored-by: Eliot Jordan <eliotjordan@users.noreply.github.com>

--- spec/factories/hyrax_work.rb
+++ spec/factories/hyrax_work.rb
@@ -92,6 +92,17 @@ FactoryBot.define do
       end
     end
 
+    trait :with_file_and_work do
+      transient do
+        members do
+          # If you set a depositor on the containing work, propogate that into these members
+          additional_attributes = {}
+          additional_attributes[:depositor] = depositor if depositor
+          [valkyrie_create(:hyrax_file_set, additional_attributes), valkyrie_create(:hyrax_work, additional_attributes)]
+        end
+      end
+    end
+
     trait :with_one_file_set do
       transient do
         members do
