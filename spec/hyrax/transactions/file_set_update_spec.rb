a1770c3fd5cfa73bd9e4fc983ea420b0e6405b82
Longshou Situ <lsitu@ucsd.edu>
2023-08-28 16:28:33 -0700

    Add unique test and integration tests for file_set.update transaction.

---
+++ spec/hyrax/transactions/file_set_update_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+require 'spec_helper'
+require 'hyrax/transactions'
+
+RSpec.describe Hyrax::Transactions::FileSetUpdate do
+  subject(:tx)     { described_class.new }
+  let(:file_set)   { FactoryBot.valkyrie_create(:hyrax_file_set, title: 'image.jpg') }
+  let(:change_set) { Hyrax::Forms::AdministrativeSetForm.new(file_set) }
+  let(:user)       { FactoryBot.create(:user) }
+  let(:xmas)       { DateTime.parse('2022-12-25 11:30').iso8601 }
+
+  before { allow(Hyrax::TimeService).to receive(:time_in_utc).and_return(xmas) }
+
+  describe '#call' do
+    it 'is a success' do
+      expect(tx.call(change_set)).to be_success
+    end
+
+    it 'sets attributes' do
+      change_set.title = 'new file title'
+
+      expect(tx.call(change_set).value!)
+        .to have_attributes(title: contain_exactly('new file title'))
+    end
+  end
+end
