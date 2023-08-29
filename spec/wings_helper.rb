967f255bfffb5ab6bfd66b3d43a18dacc037171e
tamsin johnson <tomjohnson@ucsb.edu>
2023-08-29 15:44:16 -0700

    never set `metadata/storage_adapter` to wings or AF if disable_wings
    some test on `.koppie` seem to fail on test order(?) issues, when they decide to
    use the Wings adapter for metadata persistence. it seems like something during
    the test suite run is setting the adapter to `:wings_adapter` and then not
    unsetting it. my best guess is that something is requiring `wings` or
    `wings/setup` (maybe via `wings/helper`) during the test run.

--- spec/wings_helper.rb
+++ spec/wings_helper.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 require 'spec_helper'
-require 'wings'
+unless Hyrax.config.disable_wings
+  require 'wings'
 
-Dir[File.expand_path(File.join(File.dirname(__FILE__), 'wings', 'support', 'matchers', '**', '*.rb'))].each { |f| require f }
+  Dir[File.expand_path(File.join(File.dirname(__FILE__), 'wings', 'support', 'matchers', '**', '*.rb'))].each { |f| require f }
+end
