b3e05ae259fe732e5ddd005f85ad766f304a756a
kibigo! <kibigo@ucsb.edu>
2023-08-30 13:52:48 -0400

    Fix specs for new FileSet versioning view implementation

--- spec/views/hyrax/file_sets/_versioning.html.erb_spec.rb
+++ spec/views/hyrax/file_sets/_versioning.html.erb_spec.rb
@@ -1,19 +1,36 @@
 # frozen_string_literal: true
 RSpec.describe 'hyrax/file_sets/_versioning.html.erb', type: :view do
   let(:file_set) { stub_model(FileSet) }
+  let(:versioning_service) { Hyrax::VersioningService.new(resource: nil) }
 
   before do
-    allow(file_set).to receive(:files).and_return([])
     allow(view).to receive(:curation_concern).and_return(file_set)
-    assign(:version_list, [])
-    render
+    assign(:version_list, Hyrax::VersionListPresenter.new(versioning_service))
   end
 
-  context "without additional users" do
+  context "when versioning is supported" do
+    before do
+      allow(versioning_service).to receive(:supports_multiple_versions?).and_return(true)
+      allow(versioning_service).to receive(:versions).and_return([])
+      render
+    end
+
     it "draws the new version form without error" do
       expect(rendered).to have_content t("hyrax.file_sets.versioning.choose_file")
       expect(rendered).to have_content t("hyrax.uploads.js_templates_versioning.options.messages.max_file_size")
       expect(rendered).to have_content "maxFileSize: #{Hyrax.config.uploader[:maxFileSize]}"
+      expect(rendered).to have_content t("hyrax.file_sets.versioning.upload")
+    end
+  end
+
+  context "when versioning is unsupported" do
+    before do
+      allow(versioning_service).to receive(:supports_multiple_versions?).and_return(false)
+      render
+    end
+
+    it "does not draw the new version form" do
+      expect(rendered).not_to have_content t("hyrax.file_sets.versioning.upload")
     end
   end
 end
