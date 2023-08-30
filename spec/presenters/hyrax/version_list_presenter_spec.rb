798ad39f31a284c6782de310783acce37d186473
kibigo! <kibigo@ucsb.edu>
2023-08-30 16:18:09 -0400

    Refactor version list presenter spec for new impl

--- spec/presenters/hyrax/version_list_presenter_spec.rb
+++ spec/presenters/hyrax/version_list_presenter_spec.rb
@@ -1,63 +1,77 @@
 # frozen_string_literal: true
 RSpec.describe Hyrax::VersionListPresenter do
-  let(:resource_version) do
-    ActiveFedora::VersionsGraph::ResourceVersion.new.tap do |v|
-      v.uri = 'http://example.com/version1'
-      v.label = 'version1'
-      v.created = '2014-12-09T02:03:18.296Z'
-    end
-  end
+  let(:file_set) { FactoryBot.create(:file_set) }
+  subject(:enum) { described_class.for(file_set: file_set) }
 
-  let(:resource_version2) do
-    ActiveFedora::VersionsGraph::ResourceVersion.new.tap do |v|
-      v.uri = 'http://example.com/version2'
-      v.label = 'version2'
-      v.created = '2014-12-19T02:03:18.296Z'
-    end
-  end
-
-  subject(:enum) { described_class.new([resource_version, resource_version2]) }
-
-  describe ".for" do
-    context "with an ActiveFedora::Base" do
-      it "gives an empty enumerable" do
-        file_set = FactoryBot.create(:file_set)
-
-        expect(described_class.for(file_set: file_set)).to be_none
-      end
-
-      it "enumerates over version presenters for original_file" do
-        file_set   = FactoryBot.create(:file_set)
-        binary     = StringIO.new("hey")
-        new_binary = StringIO.new("hey2")
-
-        Hydra::Works::AddFileToFileSet
-          .call(file_set, binary, :original_file, versioning: true)
-        Hydra::Works::AddFileToFileSet
-          .call(file_set, new_binary, :original_file, versioning: true)
-
-        expect(described_class.for(file_set: file_set).count).to eq 2
+  context "when the file set has no versions" do
+    describe ".for" do
+      it "returns an enumerable with no members" do
+        expect(enum.count).to eq 0
       end
     end
 
-    context "with a bad argument" do
+    describe "#each" do
+      it "yields nothing" do
+        versions_descending = []
+        enum.each do |v|
+          versions_descending.push(v.label)
+        end
+        expect(versions_descending).to be_empty
+      end
+    end
+
+    describe "#empty?" do
+      it "is true" do
+        expect(enum).to be_empty
+      end
+    end
+  end
+
+  context "when the file set has versions" do
+    before do
+      binary     = StringIO.new("hey")
+      new_binary = StringIO.new("hey2")
+
+      Hydra::Works::AddFileToFileSet
+        .call(file_set, binary, :original_file, versioning: true)
+      Hydra::Works::AddFileToFileSet
+        .call(file_set, new_binary, :original_file, versioning: true)
+    end
+
+    describe ".for" do
+      it "returns an enumerable with members" do
+        expect(enum.count).to eq 2
+      end
+    end
+
+    describe "#each" do
+      it "yields version presenters in order" do
+        versions = Hyrax::VersioningService.new(resource: file_set.original_file).versions
+        versions_descending = []
+        enum.each do |v|
+          expect(v).to be_kind_of Hyrax::VersionPresenter
+          versions_descending.push(v.uri)
+        end
+        expect(versions_descending).to eq versions
+          .sort { |a, b| b.created <=> a.created }
+          .map(&:uri)
+      end
+    end
+
+    describe "#empty?" do
+      it "is false" do
+        expect(enum).not_to be_empty
+      end
+    end
+  end
+
+  context "when the file set is bad" do
+    let(:file_set) { nil }
+
+    describe ".for" do
       it "raises an error" do
-        expect { described_class.for(file_set: nil) }
-          .to raise_error ArgumentError
+        expect { enum }.to raise_error ArgumentError
       end
-    end
-  end
-
-  describe "#each" do
-    it "yields version_presenters" do
-      versions_descending = []
-
-      enum.each do |v|
-        expect(v).to be_kind_of Hyrax::VersionPresenter
-        versions_descending.push(v.label)
-      end
-
-      expect(versions_descending).to eq ['version2', 'version1']
     end
   end
 end
