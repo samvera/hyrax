c5758818e68d772cde51441dad127c9550214821
tamsin johnson <tomjohnson@ucsb.edu>
2023-08-31 17:24:26 -0700

    test ContentDeleteEventJob with both model ORMs

--- spec/jobs/content_delete_event_job_spec.rb
+++ spec/jobs/content_delete_event_job_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 RSpec.describe ContentDeleteEventJob do
-  let(:user) { create(:user) }
+  let(:user) { FactoryBot.create(:user) }
   let(:mock_time) { Time.zone.at(1) }
   let(:event) do
     {
@@ -14,24 +14,46 @@ RSpec.describe ContentDeleteEventJob do
   end
 
   context 'with a FileSet' do
-    let(:curation_concern) { create(:file_set, title: ['Hamlet'], user: user) }
+    let(:curation_concern) { FactoryBot.valkyrie_create(:hyrax_file_set, title: ['Hamlet'], depositor: user.user_key) }
 
     it "logs the event to the depositor's profile" do
-      expect do
-        described_class.perform_now(curation_concern.id, user)
-      end.to change { user.profile_events.length }.by(1)
+      expect { described_class.perform_now(curation_concern.id, user) }
+        .to change { user.profile_events.length }.by(1)
       expect(user.profile_events.first).to eq(event)
     end
   end
 
   context 'with a Work' do
-    let(:curation_concern) { create(:generic_work, title: ['BethsMac'], user: user) }
+    let(:curation_concern) { FactoryBot.valkyrie_create(:hyrax_work, title: ['BethsMac'], depositor: user.user_key) }
 
     it "logs the event to the depositor's profile" do
-      expect do
-        described_class.perform_now(curation_concern.id, user)
-      end.to change { user.profile_events.length }.by(1)
+      expect { described_class.perform_now(curation_concern.id, user) }
+        .to change { user.profile_events.length }.by(1)
       expect(user.profile_events.first).to eq(event)
+    end
+  end
+
+  context 'with ActiveFedora models', :active_fedora do
+    context 'with a FileSet' do
+      let(:curation_concern) { create(:file_set, title: ['Hamlet'], user: user) }
+
+      it "logs the event to the depositor's profile" do
+        expect do
+          described_class.perform_now(curation_concern.id, user)
+        end.to change { user.profile_events.length }.by(1)
+        expect(user.profile_events.first).to eq(event)
+      end
+    end
+
+    context 'with a Work' do
+      let(:curation_concern) { create(:generic_work, title: ['BethsMac'], user: user) }
+
+      it "logs the event to the depositor's profile" do
+        expect do
+          described_class.perform_now(curation_concern.id, user)
+        end.to change { user.profile_events.length }.by(1)
+        expect(user.profile_events.first).to eq(event)
+      end
     end
   end
 end
