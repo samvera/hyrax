79451ae56972a51fc2b6471dd024f40fde6348f8
Eliot Jordan <eliotj@princeton.edu>
2023-09-08 17:35:17 -0500

    Refactor and fix specs

--- app/jobs/create_work_job.rb
+++ app/jobs/create_work_job.rb
@@ -17,36 +17,48 @@ class CreateWorkJob < Hyrax::ApplicationJob
   def perform(user, model, attributes, operation)
     operation.performing!
     work = model.constantize.new
-    current_ability = Ability.new(user)
-    env = Hyrax::Actors::Environment.new(work, current_ability, attributes)
-    status = if model == ActiveFedora::Base
-               work_actor.create(env)
-             else
-               batch_create_valkyrie_work(work, attributes, user)
-             end
+    if model.constantize < ActiveFedora::Base
+      status = batch_create_af_work(work, attributes, user)
+      errors = work.errors
+    else
+      result = batch_create_valkyrie_work(work, attributes, user)
+      status = result.success?
+      errors = result.failure&.last
+    end
+
     return operation.success! if status
-    operation.fail!(work.errors.full_messages.join(' '))
+    operation.fail!(errors.full_messages.join(' '))
   end
 
   private
 
-  def work_actor
-    Hyrax::CurationConcern.actor
+  def batch_create_af_work(work, attributes, user)
+    current_ability = Ability.new(user)
+    env = Hyrax::Actors::Environment.new(work, current_ability, attributes)
+    work_actor.create(env)
   end
 
   def batch_create_valkyrie_work(work, attributes, user)
     uploaded_file_ids = attributes.delete(:uploaded_files)
     files = Hyrax::UploadedFile.find(uploaded_file_ids)
-    work.title = attributes.delete(:title)
-    work.resource_type = attributes.delete(:resource_type)
-    work.visibility = attributes.delete(:visibility)
-    work.depositor = user.user_key
-    work.creator = attributes.delete(:creator)
-    work.rights_statement = [attributes.delete(:rights_statement)]
-    permissions = work.permission_manager.acl.permissions
-    generic_work = Hyrax.persister.save(resource: work)
-    generic_work.permission_manager.acl.permissions = permissions
-    generic_work.permission_manager.acl.save
-    Hyrax::WorkUploadsHandler.new(work: generic_work).add(files: files).attach
+    permissions_params = attributes.delete(:permissions_attributes)
+    form = Hyrax::FormFactory.new.build(work, nil, nil)
+    form.validate(attributes)
+
+    transactions['change_set.create_work']
+      .with_step_args(
+        'work_resource.add_file_sets' => { uploaded_files: files },
+        'change_set.set_user_as_depositor' => { user: user },
+        'work_resource.save_acl' => { permissions_params: permissions_params }
+      )
+      .call(form)
+  end
+
+  def transactions
+    Hyrax::Transactions::Container
+  end
+
+  def work_actor
+    Hyrax::CurationConcern.actor
   end
 end
