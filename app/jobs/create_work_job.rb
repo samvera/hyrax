2d2f11ac704b9dc37ffd114b8f70943f3e15d313
VivianChu <tchu@ucsd.edu>
2023-09-12 12:40:41 -0700

    Fix batch add new works doesn't add new works (#6266)
    * Fix batch add new works doesn't add new works
    
    * Refactor and fix specs
    
    ---------
    
    Co-authored-by: Eliot Jordan <eliotj@princeton.edu>

--- app/jobs/create_work_job.rb
+++ app/jobs/create_work_job.rb
@@ -17,14 +17,46 @@ class CreateWorkJob < Hyrax::ApplicationJob
   def perform(user, model, attributes, operation)
     operation.performing!
     work = model.constantize.new
-    current_ability = Ability.new(user)
-    env = Hyrax::Actors::Environment.new(work, current_ability, attributes)
-    status = work_actor.create(env)
+    if model.constantize < ActiveFedora::Base
+      status = batch_create_af_work(work, attributes, user)
+      errors = work.errors
+    else
+      result = batch_create_valkyrie_work(work, attributes, user)
+      status = result.success?
+      errors = result.failure&.last
+    end
+
     return operation.success! if status
-    operation.fail!(work.errors.full_messages.join(' '))
+    operation.fail!(errors.full_messages.join(' '))
   end
 
   private
+
+  def batch_create_af_work(work, attributes, user)
+    current_ability = Ability.new(user)
+    env = Hyrax::Actors::Environment.new(work, current_ability, attributes)
+    work_actor.create(env)
+  end
+
+  def batch_create_valkyrie_work(work, attributes, user)
+    uploaded_file_ids = attributes.delete(:uploaded_files)
+    files = Hyrax::UploadedFile.find(uploaded_file_ids)
+    permissions_params = attributes.delete(:permissions_attributes)
+    form = Hyrax::FormFactory.new.build(work, nil, nil)
+    form.validate(attributes)
+
+    transactions['change_set.create_work']
+      .with_step_args(
+        'work_resource.add_file_sets' => { uploaded_files: files },
+        'change_set.set_user_as_depositor' => { user: user },
+        'work_resource.save_acl' => { permissions_params: permissions_params }
+      )
+      .call(form)
+  end
+
+  def transactions
+    Hyrax::Transactions::Container
+  end
 
   def work_actor
     Hyrax::CurationConcern.actor
