a75c19eb0ce830534cf25aaf8c569ca01e40bff1
bwatson78 <brad.watson.orlando@gmail.com>
2023-10-10 16:01:23 -0500

    Removes FileSet related objects when work deleted (#6334).

--- lib/hyrax/transactions/work_destroy.rb
+++ lib/hyrax/transactions/work_destroy.rb
@@ -16,6 +16,26 @@ module Hyrax
       def initialize(container: Container, steps: DEFAULT_STEPS)
         super
       end
+
+      def call(value)
+        remove_file_set_related_objects(value)
+        super
+      end
+
+      private
+
+      def retrieve_file_set_objects(work_obj)
+        work_obj.member_ids.map { |id| Hyrax.query_service.find_by(id:) }
+      end
+
+      def remove_file_set_related_objects(work_obj)
+        file_sets = retrieve_file_set_objects(work_obj)
+
+        file_sets.each do |fs|
+          Steps::DeleteAllFileMetadata.new(property: :file_ids).call(fs)
+          Steps::DeleteAccessControl.new.call(fs)
+        end
+      end
     end
   end
 end
