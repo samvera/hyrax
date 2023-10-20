0429d6116e1a78449e515fde8d270bb0a0f33a07
Daniel Pierce <dlpierce@indiana.edu>
2023-10-20 11:54:32 -0400

    Remove deprecated methods `available_child_collections` and `available_parent_collections`

--- app/forms/hyrax/forms/collection_form.rb
+++ app/forms/hyrax/forms/collection_form.rb
@@ -123,21 +123,6 @@ module Hyrax
         collection_member_service.available_member_subcollections.documents
       end
 
-      ##
-      # @deprecated this implementation requires an extra db round trip, had a
-      #   buggy cacheing mechanism, and was largely duplicative of other code.
-      #   all versions of this code are replaced by
-      #   {CollectionsHelper#available_parent_collections_data}.
-      def available_parent_collections(scope:)
-        return @available_parents if @available_parents.present?
-
-        collection = model_class.find(id)
-        colls = Hyrax::Collections::NestedCollectionQueryService.available_parent_collections(child: collection, scope: scope, limit_to_id: nil)
-        @available_parents = colls.map do |col|
-          { "id" => col.id, "title_first" => col.title.first }
-        end.to_json
-      end
-
       private
 
       def all_files_with_access
