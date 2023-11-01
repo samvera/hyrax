357b5e78988cdeb1938a722ebac52a91aa32bd97
Daniel Pierce <dlpierce@indiana.edu>
2023-11-01 17:08:40 -0400

    Fix embargo expiry job spec (#6410)
    * Add valkyrie specs for EmbargoExpiryJob
    
    Marks existing spec as AF only.
    
    Duplicates existing EmbargoExpiryJob specs but with Valkyrie resources.
    
    Spec setup is simplified by not specifying "restricted" visibility, instead expecting the factory
    default "authenticated". Additional checks to confirm the initial visibility state are also added.
    
    The embargo traits from the hyrax_work factory are copied to the hyrax_file_set factory.
    
    Saving of the expired embargo ACL is moved to inside the time machine to 10 days ago. Without this
    change the solr doc will not contain the embargo related fields, causing
    `#records_with_expired_embargoes` to not find those resources.
    
    ---------
    
    Co-authored-by: bwatson78 <brad.watson.orlando@gmail.com>

--- spec/factories/hyrax_file_set.rb
+++ spec/factories/hyrax_file_set.rb
@@ -48,6 +48,28 @@ FactoryBot.define do
       Hyrax.index_adapter.save(resource: file_set) if evaluator.with_index
     end
 
+    trait :under_embargo do
+      association :embargo, factory: :hyrax_embargo
+
+      after(:create) do |fs, _e|
+        Hyrax::EmbargoManager.new(resource: fs).apply
+        fs.permission_manager.acl.save
+      end
+    end
+
+    trait :with_expired_enforced_embargo do
+      after(:build) do |fs, _evaluator|
+        fs.embargo = FactoryBot.valkyrie_create(:hyrax_embargo, :expired)
+      end
+
+      after(:create) do |fs, _evaluator|
+        allow(Hyrax::TimeService).to receive(:time_in_utc).and_return(10.days.ago)
+        Hyrax::EmbargoManager.new(resource: fs).apply
+        fs.permission_manager.acl.save
+        allow(Hyrax::TimeService).to receive(:time_in_utc).and_call_original
+      end
+    end
+
     trait :public do
       transient do
         visibility_setting { Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC }
