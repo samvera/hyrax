c4cb0290ef60def6b585305fdea0a1b9cf7e9658
Brad Watson <brad.watson.orlando@gmail.com>
2023-11-02 10:38:44 -0500

    Attempts to fix the failing specs for shares_controller_spec. (#6411)
    * Attempts to fix the failing specs for shares_controller_spec.
    
    * Move creation of misc extant works to their own context.
    
    ---------
    
    Co-authored-by: Daniel Pierce <dlpierce@indiana.edu>

--- spec/controllers/hyrax/my/shares_controller_spec.rb
+++ spec/controllers/hyrax/my/shares_controller_spec.rb
@@ -1,22 +1,17 @@
 # frozen_string_literal: true
-RSpec.describe Hyrax::My::SharesController, type: :controller do
+RSpec.describe Hyrax::My::SharesController, :clean_repo, type: :controller do
   describe "logged in user" do
     let(:user) { create(:user) }
 
-    before do
-      sign_in user
-    end
+    before { sign_in user }
 
     describe "#index" do
       let(:other_user)   { create(:user) }
       let(:someone_else) { create(:user) }
 
-      let!(:my_work)                  { create(:work, user: user) }
-      let!(:unshared_work)            { create(:work, user: other_user) }
-      let!(:shared_with_me)           { create(:work, user: other_user, edit_users: [user, other_user]) }
-      let!(:read_shared_with_me)      { create(:work, user: other_user, read_users: [user, other_user]) }
-      let!(:shared_with_someone_else) { create(:work, user: other_user, edit_users: [someone_else, other_user]) }
-      let!(:my_collection)            { create(:public_collection_lw, user: user) }
+      let!(:shared_with_me) do
+        valkyrie_create(:monograph, depositor: other_user.user_key, edit_users: [user, other_user])
+      end
 
       it "responds with success" do
         get :index
@@ -24,18 +19,32 @@ RSpec.describe Hyrax::My::SharesController, type: :controller do
       end
 
       context "with multiple pages of results" do
-        before { 2.times { create(:work, user: other_user, edit_users: [user, other_user]) } }
+        before { 2.times { valkyrie_create(:monograph, depositor: other_user.user_key, edit_users: [user, other_user]) } }
+
         it "paginates" do
           get :index, params: { per_page: 2 }
           expect(assigns[:document_list].length).to eq 2
+
           get :index, params: { per_page: 2, page: 2 }
-          expect(assigns[:document_list].length).to be >= 1
+          expect(assigns[:document_list].length).to eq 1
         end
       end
 
-      it "shows only documents that are shared with me via edit access" do
-        get :index
-        expect(assigns[:document_list].map(&:id)).to contain_exactly(shared_with_me.id)
+      context "with other extant documents" do
+        let!(:my_work) { valkyrie_create(:monograph, depositor: user.user_key) }
+        let!(:unshared_work) { valkyrie_create(:monograph, depositor: other_user.user_key) }
+        let!(:read_shared_with_me) do
+          valkyrie_create(:monograph, depositor: other_user.user_key, read_users: [user, other_user])
+        end
+        let!(:shared_with_someone_else) do
+          valkyrie_create(:monograph, depositor: other_user.user_key, edit_users: [someone_else, other_user])
+        end
+        let!(:my_collection) { valkyrie_create(:hyrax_collection, :public, user: user) }
+
+        it "shows only documents that are shared with me via edit access" do
+          get :index
+          expect(assigns[:document_list].map(&:id)).to contain_exactly(shared_with_me.id)
+        end
       end
     end
   end
