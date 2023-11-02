a5e91e70013a04b9180446cf82552e3b01efe419
Shana Moore <shana@scientist.com>
2023-11-02 08:37:23 -0700

    Fix collection sort by drop down (#6408)
    * 🐛 Fix for Collection Sort_by functionality
    
    On the Collection show page, the sort_by dropdown button was not working. A user could select and option and a request would be made, but the sort params was not making it through to the search builder and therefore did not sort the items regardless of the selection.
    
    * 🧹 revert sort by title changes as it is not supported by hyrax (yet)
    
    * ♻️ revert changes to title

--- app/services/hyrax/collections/collection_member_search_service.rb
+++ app/services/hyrax/collections/collection_member_search_service.rb
@@ -46,8 +46,10 @@ module Hyrax
       #
       # @return [Blacklight::Solr::Response]
       def available_member_works
+        sort_field = user_params[:sort]
         response, _docs = search_results do |builder|
           builder.search_includes_models = :works
+          builder.merge(sort: sort_field)
           builder
         end
         response
