76aac0648d1f16965583948fb1906bcb64a07f76
Daniel Pierce <dlpierce@indiana.edu>
2023-11-10 03:01:20 -0500

    Adapt more record view specs for valkyrie same way as subject
    Move subject's shared examples inside the top level describe with a more distinct name.

--- spec/views/records/edit_fields/_subject.html.erb_spec.rb
+++ spec/views/records/edit_fields/_subject.html.erb_spec.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
-
-RSpec.shared_examples 'check for autocomplete url' do
-  it 'has url for autocomplete service' do
-    expect(rendered).to have_selector('input[data-autocomplete-url="/authorities/search/local/subjects"][data-autocomplete="subject"]')
-  end
-end
-
 RSpec.describe 'records/edit_fields/_subject.html.erb', type: :view do
+  RSpec.shared_examples 'check for subject autocomplete url' do
+    it 'has url for autocomplete service' do
+      expect(rendered).to have_selector('input[data-autocomplete-url="/authorities/search/local/subjects"][data-autocomplete="subject"]')
+    end
+  end
+
   let(:form_template) do
     %(
       <%= simple_form_for [main_app, @form] do |f| %>
@@ -24,13 +23,13 @@ RSpec.describe 'records/edit_fields/_subject.html.erb', type: :view do
     let(:work) { GenericWork.new }
     let(:form) { Hyrax::GenericWorkForm.new(work, nil, controller) }
 
-    include_examples 'check for autocomplete url'
+    include_examples 'check for subject autocomplete url'
   end
 
   context 'Valkyrie' do
     let(:work) { Monograph.new }
     let(:form) { Hyrax::Forms::ResourceForm.for(work) }
 
-    include_examples 'check for autocomplete url'
+    include_examples 'check for subject autocomplete url'
   end
 end
