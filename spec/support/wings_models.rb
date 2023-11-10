d452647e9cfa8c6794b62a57399e3f7924e76b1f
Daniel Pierce <dlpierce@indiana.edu>
2023-11-10 04:21:09 -0500

    Clear default wings Collection mapping and remove all uses of pcdm_collection factory

--- spec/support/wings_models.rb
+++ spec/support/wings_models.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 return if Hyrax.config.disable_wings
+
+# Clear existing Collection mapping to allow reverse lookups to resolve CollectionResource
+Wings::ModelRegistry.register(Hyrax::PcdmCollection, ActiveFedora::Base)
 Wings::ModelRegistry.register(CollectionResource, ::Collection)
