b270d43281beabb2d908f2c6b2132be2198116ef
Daniel Pierce <dlpierce@indiana.edu>
2023-11-10 14:08:04 -0500

    Allow Wings models to be unregistered; reorder Collection map during specs
    This allows CollectionResource to be used when converting from AF, and PcdmCollection to still map to Collection. Does not affect operation outside of specs.

--- spec/support/wings_models.rb
+++ spec/support/wings_models.rb
@@ -2,6 +2,8 @@
 
 return if Hyrax.config.disable_wings
 
-# Clear existing Collection mapping to allow reverse lookups to resolve CollectionResource
-Wings::ModelRegistry.register(Hyrax::PcdmCollection, ActiveFedora::Base)
+# Clear existing Collection mapping to allow reverse lookups to resolve CollectionResource.
+# Then restore PcdmCollection for any code directly using that model.
+Wings::ModelRegistry.unregister(Hyrax::PcdmCollection)
 Wings::ModelRegistry.register(CollectionResource, ::Collection)
+Wings::ModelRegistry.register(Hyrax::PcdmCollection, ::Collection)
