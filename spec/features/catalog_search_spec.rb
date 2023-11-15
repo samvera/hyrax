a72d1ec2824b5149f9df70408577c8df3f337e8f
Ayoub Belemlih <13107510+abelemlih@users.noreply.github.com>
2023-11-15 09:30:20 -0600

    Edit catalog_search_spec.rb

--- spec/features/catalog_search_spec.rb
+++ spec/features/catalog_search_spec.rb
@@ -8,27 +8,15 @@ RSpec.describe 'catalog searching', type: :feature do
 
   context 'with works and collections' do
     let!(:jills_work) do
-      if Hyrax.config.use_valkyrie?
-        FactoryBot.valkyrie_create(:monograph, :public, title: ["Jill's Research"], keyword: ['jills_keyword', 'shared_keyword'])
-      else
-        create(:public_work, title: ["Jill's Research"], keyword: ['jills_keyword', 'shared_keyword'])
-      end
+      FactoryBot.valkyrie_create(:monograph, :public, title: ["Jill's Research"], keyword: ['jills_keyword', 'shared_keyword'])
     end
 
     let!(:jacks_work) do
-      if Hyrax.config.use_valkyrie?
-        FactoryBot.valkyrie_create(:monograph, :public, title: ["Jack's Research"], keyword: ['jacks_keyword', 'shared_keyword'])
-      else
-        create(:public_work, title: ["Jack's Research"], keyword: ['jacks_keyword', 'shared_keyword'])
-      end
+      FactoryBot.valkyrie_create(:monograph, :public, title: ["Jack's Research"], keyword: ['jacks_keyword', 'shared_keyword'])
     end
 
     let!(:collection) do
-      if Hyrax.config.use_valkyrie?
-        FactoryBot.valkyrie_create(:collection_resource, :public, keyword: ['collection_keyword', 'shared_keyword'])
-      else
-        create(:public_collection_lw, keyword: ['collection_keyword', 'shared_keyword'])
-      end
+      FactoryBot.valkyrie_create(:collection_resource, :public, keyword: ['collection_keyword', 'shared_keyword'])
     end
 
     it 'performing a search' do
@@ -46,19 +34,11 @@ RSpec.describe 'catalog searching', type: :feature do
 
   context 'with public works and private collections', clean_repo: true do
     let!(:collection) do
-      if Hyrax.config.use_valkyrie?
-        FactoryBot.valkyrie_create(:collection_resource, members: [jills_work])
-      else
-        create(:private_collection_lw)
-      end
+      FactoryBot.valkyrie_create(:collection_resource, members: [jills_work])
     end
 
     let!(:jills_work) do
-      if Hyrax.config.use_valkyrie?
-        FactoryBot.valkyrie_create(:monograph, :public, title: ["Jill's Research"], keyword: ['jills_keyword'])
-      else
-        create(:public_work, title: ["Jill's Research"], keyword: ['jills_keyword'], member_of_collections: [collection])
-      end
+      FactoryBot.valkyrie_create(:monograph, :public, title: ["Jill's Research"], keyword: ['jills_keyword'])
     end
 
     it "hides collection facet values the user doesn't have access to view when performing a search" do
