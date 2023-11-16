b933237f728b61211cc51bcb347dfe4ce5003e31
bwatson78 <brad.watson.orlando@gmail.com>
2023-11-16 11:11:45 -0600

    Valkyrizes spec/authorities/qa/authorities/collections_spec.rb.

--- spec/authorities/qa/authorities/collections_spec.rb
+++ spec/authorities/qa/authorities/collections_spec.rb
@@ -7,88 +7,66 @@
 RSpec.describe Qa::Authorities::Collections, :clean_repo do
   subject(:service) { described_class.new }
   let(:controller) { Qa::TermsController.new }
-  let(:user1) { FactoryBot.build(:user) }
-  let(:user2) { FactoryBot.build(:user) }
+  let(:user1) { FactoryBot.create(:user) }
+  let(:user2) { FactoryBot.create(:user) }
 
   let!(:collection1) do
-    FactoryBot.build(:private_collection_lw,
+    FactoryBot.valkyrie_create(:hyrax_collection,
                      title: ['foo foo'],
-                     user: user1,
-                     with_permission_template: true,
-                     with_solr_document: true)
+                     user: user1)
   end
 
   let!(:collection2) do
-    FactoryBot.build(:private_collection_lw,
+    FactoryBot.valkyrie_create(:hyrax_collection,
                      title: ['bar'],
-                     user: user1,
-                     with_permission_template: true,
-                     with_solr_document: true)
+                     user: user1)
   end
 
   let!(:collection3) do
-    FactoryBot.build(:private_collection_lw,
+    FactoryBot.valkyrie_create(:hyrax_collection,
                      title: ['another foo'],
-                     user: user1,
-                     with_permission_template: true,
-                     with_solr_document: true)
+                     user: user1)
   end
 
   let!(:collection4) do
-    FactoryBot.build(:private_collection_lw,
+    FactoryBot.valkyrie_create(:hyrax_collection,
                      title: ['foo foo foo'],
-                     user: user2,
-                     with_permission_template: true,
-                     with_solr_document: true)
+                     user: user2)
   end
 
   let!(:collection5) do
-    FactoryBot.build(:private_collection_lw,
+    FactoryBot.valkyrie_create(:hyrax_collection,
                      title: ['foo for you'],
                      user: user2,
-                     with_permission_template: { manage_users: [user1] },
-                     with_solr_document: true)
+                     edit_users: [user1.user_key])
   end
 
   let!(:collection6) do
-    FactoryBot.build(:private_collection_lw,
+    FactoryBot.valkyrie_create(:hyrax_collection,
                      title: ['foo too'],
                      user: user2,
-                     with_permission_template: { deposit_users: [user1] },
-                     with_solr_document: true)
+                     edit_users: [user1.user_key])
   end
 
   let!(:collection7) do
-    FactoryBot.build(:private_collection_lw,
+    FactoryBot.valkyrie_create(:hyrax_collection,
                      title: ['foo bar baz'],
                      user: user2,
-                     with_permission_template: { view_users: [user1] },
-                     with_solr_document: true)
+                     read_users: [user1.user_key])
   end
 
   let!(:collection8) do
-    FactoryBot.build(:private_collection_lw,
-                     id: 'col-8-mgr',
-                     title: ['bar for you'],
+    FactoryBot.valkyrie_create(:hyrax_collection,
+                     title: ['bar too'],
                      user: user2,
-                     with_permission_template: { manage_users: [user1] },
-                     with_solr_document: true)
+                     edit_users: [user1.user_key])
   end
 
   let!(:collection9) do
-    FactoryBot.build(:private_collection_lw,
-                     title: ['bar too'],
-                     user: user2,
-                     with_permission_template: { deposit_users: [user1] },
-                     with_solr_document: true)
-  end
-
-  let!(:collection10) do
-    FactoryBot.build(:private_collection_lw,
+    FactoryBot.valkyrie_create(:hyrax_collection,
                      title: ['bar bar baz'],
                      user: user2,
-                     with_permission_template: { view_users: [user1] },
-                     with_solr_document: true)
+                     read_users: [user1.user_key])
   end
 
   before do
@@ -114,17 +92,6 @@ RSpec.describe Qa::Authorities::Collections, :clean_repo do
       let(:params) { ActionController::Parameters.new(q: 'foo', access: 'edit') }
 
       it 'lists collections the current user can edit' do
-        expect(service.search(nil, controller))
-          .to contain_exactly(include(id: collection1.id),
-                              include(id: collection3.id),
-                              include(id: collection5.id))
-      end
-    end
-
-    context 'when access is deposit' do
-      let(:params) { ActionController::Parameters.new(q: 'foo', access: 'deposit') }
-
-      it 'lists collections the current user can edit or deposit' do
         expect(service.search(nil, controller))
           .to contain_exactly(include(id: collection1.id),
                               include(id: collection3.id),
