b79cb5a76bcc2e199de397bf4ec31fe88cff7df2
bwatson78 <brad.watson.orlando@gmail.com>
2023-11-17 09:42:04 -0600

    uses correct PermissionTemplate structure.

--- spec/authorities/qa/authorities/collections_spec.rb
+++ spec/authorities/qa/authorities/collections_spec.rb
@@ -7,43 +7,69 @@
 RSpec.describe Qa::Authorities::Collections, :clean_repo do
   subject(:service) { described_class.new }
   let(:controller) { Qa::TermsController.new }
-  let(:user1) { FactoryBot.create(:user) }
-  let(:user2) { FactoryBot.create(:user) }
+  let(:user1) { create(:user) }
+  let(:user2) { create(:user) }
 
-  let!(:collection1) do
-    valkyrie_create(:hyrax_collection, title: ['foo foo'], user: user1)
-  end
+  let!(:collection1) { valkyrie_create(:hyrax_collection, title: ['foo foo'], user: user1) }
 
-  let!(:collection2) do
-    valkyrie_create(:hyrax_collection, title: ['bar'], user: user1)
-  end
+  let!(:collection2) { valkyrie_create(:hyrax_collection, title: ['bar'], user: user1) }
 
-  let!(:collection3) do
-    valkyrie_create(:hyrax_collection, title: ['another foo'], user: user1)
-  end
+  let!(:collection3) { valkyrie_create(:hyrax_collection, title: ['another foo'], user: user1) }
 
-  let!(:collection4) do
-    valkyrie_create(:hyrax_collection, title: ['foo foo foo'], user: user2)
-  end
+  let!(:collection4) { valkyrie_create(:hyrax_collection, title: ['foo foo foo'], user: user2) }
 
   let!(:collection5) do
-    valkyrie_create(:hyrax_collection, title: ['foo for you'], user: user2, edit_users: [user1.user_key])
+    valkyrie_create(:hyrax_collection,
+                    title: ['foo for you'],
+                    user: user2,
+                    access_grants: [{ agent_type: Hyrax::PermissionTemplateAccess::USER,
+                                      agent_id: user1.user_key,
+                                      access: Hyrax::PermissionTemplateAccess::MANAGE }])
   end
 
   let!(:collection6) do
-    valkyrie_create(:hyrax_collection, title: ['foo too'], user: user2, edit_users: [user1.user_key])
+    valkyrie_create(:hyrax_collection,
+                    title: ['foo too'],
+                    user: user2,
+                    access_grants: [{ agent_type: Hyrax::PermissionTemplateAccess::USER,
+                                      agent_id: user1.user_key,
+                                      access: Hyrax::PermissionTemplateAccess::DEPOSIT }])
   end
 
   let!(:collection7) do
-    valkyrie_create(:hyrax_collection, title: ['foo bar baz'], user: user2, read_users: [user1.user_key])
+    valkyrie_create(:hyrax_collection,
+                    title: ['foo bar baz'],
+                    user: user2,
+                    access_grants: [{ agent_type: Hyrax::PermissionTemplateAccess::USER,
+                                      agent_id: user1.user_key,
+                                      access: Hyrax::PermissionTemplateAccess::VIEW }])
   end
 
   let!(:collection8) do
-    valkyrie_create(:hyrax_collection, title: ['bar too'], user: user2, edit_users: [user1.user_key])
+    valkyrie_create(:hyrax_collection,
+                    title: ['bar for you'],
+                    user: user2,
+                    access_grants: [{ agent_type: Hyrax::PermissionTemplateAccess::USER,
+                                      agent_id: user1.user_key,
+                                      access: Hyrax::PermissionTemplateAccess::MANAGE }])
   end
 
   let!(:collection9) do
-    valkyrie_create(:hyrax_collection, title: ['bar bar baz'], user: user2, read_users: [user1.user_key])
+    valkyrie_create(:hyrax_collection,
+                    title: ['bar too'],
+                    user: user2,
+                    access_grants: [{ agent_type: Hyrax::PermissionTemplateAccess::USER,
+                                      agent_id: user1.user_key,
+                                      access: Hyrax::PermissionTemplateAccess::DEPOSIT }])
+  end
+
+  let!(:collection10) do
+    valkyrie_create(:hyrax_collection,
+                    title: ['bar bar baz'],
+                    user: user2,
+                    access_grants: [{ agent_type: Hyrax::PermissionTemplateAccess::USER,
+                                      agent_id: user1.user_key,
+                                      access: Hyrax::PermissionTemplateAccess::VIEW }])
   end
 
   before do
@@ -69,6 +95,17 @@ RSpec.describe Qa::Authorities::Collections, :clean_repo do
       let(:params) { ActionController::Parameters.new(q: 'foo', access: 'edit') }
 
       it 'lists collections the current user can edit' do
+        expect(service.search(nil, controller))
+          .to contain_exactly(include(id: collection1.id),
+                              include(id: collection3.id),
+                              include(id: collection5.id))
+      end
+    end
+
+    context 'when access is deposit' do
+      let(:params) { ActionController::Parameters.new(q: 'foo', access: 'deposit') }
+
+      it 'lists collections the current user can edit or deposit' do
         expect(service.search(nil, controller))
           .to contain_exactly(include(id: collection1.id),
                               include(id: collection3.id),
