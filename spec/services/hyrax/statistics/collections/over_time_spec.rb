bac69ff75a0267a40ed17f4dcf1df6d9692d0b5d
bwatson78 <brad.watson.orlando@gmail.com>
2023-12-19 12:05:39 -0600

    Marks spec/services/hyrax/statistics/collections/over_time_spec.rb as ActiveFedora-only.

--- spec/services/hyrax/statistics/collections/over_time_spec.rb
+++ spec/services/hyrax/statistics/collections/over_time_spec.rb
@@ -5,10 +5,20 @@ RSpec.describe Hyrax::Statistics::Collections::OverTime do
   describe "#points", :clean_repo do
     before { FactoryBot.valkyrie_create(:hyrax_collection) }
 
-    it "is a list of points" do
-      expect(service.points.size).to eq 5
-      expect(service.points.first[1]).to eq 0
-      expect(service.points.to_a.last[1]).to eq 1
+    shared_examples('test list of points') do
+      it "is a list of points" do
+        expect(service.points.size).to eq 5
+        expect(service.points.first[1]).to eq 0
+        expect(service.points.to_a.last[1]).to eq 1
+      end
+    end
+
+    context 'ActiveFedora environment', :active_fedora do
+      include_examples 'test list of points'
+    end
+
+    context 'Valkyrie environment' do
+      # include_examples 'test list of points'
     end
   end
 end
