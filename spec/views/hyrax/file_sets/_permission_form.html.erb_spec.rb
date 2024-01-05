f65b79ed647c4a7cb8d6c9a7930562232cbc85bd
bwatson78 <brad.watson.orlando@gmail.com>
2024-01-05 09:07:33 -0600

    Valkyrizes spec/views/hyrax/file_sets/_permission_form.html.erb_spec.rb.

--- spec/views/hyrax/file_sets/_permission_form.html.erb_spec.rb
+++ spec/views/hyrax/file_sets/_permission_form.html.erb_spec.rb
@@ -1,9 +1,23 @@
 # frozen_string_literal: true
 RSpec.describe 'hyrax/file_sets/_permission_form.html.erb', type: :view do
   let(:file_set) do
-    stub_model(FileSet, id: '123',
-                        depositor: 'bob',
-                        resource_type: ['Dataset'])
+    if Hyrax.config.disable_wings
+      # NOTE: stubbing the lease and embargo attributes is necessary here to avoid creating an associated embargo object that
+      #   Hyrax::LeaseabilityBehavior can process.
+      stub_model(FileSet, id: '123',
+                          depositor: 'bob',
+                          resource_type: ['Dataset'],
+                          embargo_release_date: Time.zone.today + 2,
+                          lease_expiration_date: Time.zone.today + 2,
+                          visibility_during_embargo: Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_AUTHENTICATED,
+                          visibility_after_embargo: Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC,
+                          visibility_during_lease: Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_AUTHENTICATED,
+                          visibility_after_lease: Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC)
+    else
+      stub_model(FileSet, id: '123',
+                          depositor: 'bob',
+                          resource_type: ['Dataset'])
+    end
   end
 
   let(:form) do
