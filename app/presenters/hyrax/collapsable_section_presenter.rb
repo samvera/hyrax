3e23b28fd0d4a2ec05416cd19d89b803029b500b
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2024-01-11 12:19:54 -0500

    🧹 Add title to CollapsableSectionPresenter
    This is a back-port of Hyku functionality, while preserving method
    signature compatibility.

--- app/presenters/hyrax/collapsable_section_presenter.rb
+++ app/presenters/hyrax/collapsable_section_presenter.rb
@@ -2,15 +2,16 @@
 module Hyrax
   # Draws a collapsable list widget using the Bootstrap 3 / Collapse.js plugin
   class CollapsableSectionPresenter
-    def initialize(view_context:, text:, id:, icon_class:, open:)
+    def initialize(view_context:, text:, id:, icon_class:, open:, title: nil)
       @view_context = view_context
       @text = text
       @id = id
       @icon_class = icon_class
       @open = open
+      @title = title
     end
 
-    attr_reader :view_context, :text, :id, :icon_class, :open
+    attr_reader :view_context, :text, :id, :icon_class, :open, :title
     delegate :content_tag, :safe_join, :tag, to: :view_context
 
     def render(&block)
@@ -20,13 +21,17 @@ module Hyrax
     private
 
     def button_tag
-      tag.a(role: 'button',
-            class: "#{button_class}collapse-toggle nav-link",
-            data: { toggle: 'collapse' },
-            href: "##{id}",
-            onclick: "toggleCollapse(this)",
-            'aria-expanded' => open,
-            'aria-controls' => id) do
+      attrs = {
+        role: 'button',
+        class: "#{button_class}collapse-toggle nav-link",
+        data: { toggle: 'collapse' },
+        href: "##{id}",
+        onclick: "toggleCollapse(this)",
+        'aria-expanded' => open,
+        'aria-controls' => id
+      }
+      attrs[:title] = title if title.present?
+      tag.a(attrs) do
         safe_join([tag.span('', class: icon_class, 'aria-hidden': true),
                    tag.span(text)], ' ')
       end
