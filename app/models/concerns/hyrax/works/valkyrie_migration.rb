1a15450217810fca7062d99822690fc66e757027
LaRita Robinson <larita@scientist.com>
2024-01-15 17:53:30 -0500

    Add module for migrated resources
    Adds the mapping behavior needed between a Fedora work model and a new
    Valkurie resource model.

---
+++ app/models/concerns/hyrax/works/valkyrie_migration.rb
@@ -0,0 +1,36 @@
+module Hyrax
+  module Works
+    module ValkyrieMigration
+      extend ActiveSupport::Concern
+
+      included do
+        attribute :internal_resource, Valkyrie::Types::Any.default(self.name.gsub(/Resource$/,'').freeze), internal: true
+      end
+
+      class_methods do
+        def _hyrax_default_name_class
+          Hyrax::Name
+        end
+
+        def to_rdf_representation
+          name.gsub("Resource", "")
+        end
+      end
+
+      def members
+        return @members if @members.present?
+        @members = member_ids.map do |id|
+          begin
+            Hyrax.query_service.find_by(id: id)
+          rescue Valkyrie::Persistence::ObjectNotFoundError
+            Rails.logger.warn("Could not find member #{id} for #{self.id}")
+          end
+        end
+      end
+
+      def to_solr
+        Hyrax::ValkyrieIndexer.for(resource: self).to_solr
+      end
+    end
+  end
+end
