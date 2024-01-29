35f9aca3a91114bb291a576c307af26f0540ce47
bwatson78 <brad.watson.orlando@gmail.com>
2024-01-29 14:11:38 -0600

    removes alteration of sqlite3

--- template.rb
+++ template.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 # Hack for https://github.com/rails/rails/issues/35153
-gsub_file 'Gemfile', /^gem ["']sqlite3["']$/, 'gem "sqlite3", "~> 1.3.0"'
 gem 'hyrax', git: 'git@github.com:samvera/hyrax.git', branch: 'main'
 run 'bundle install'
 generate 'hyrax:install', '-f'
