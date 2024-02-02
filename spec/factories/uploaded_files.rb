31374c8fb7b3115efba1c9374b2ac578f08dd374
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2024-02-02 14:14:03 -0500

    🧹 Favor Hyrax::Engine.root

--- spec/factories/uploaded_files.rb
+++ spec/factories/uploaded_files.rb
@@ -2,10 +2,10 @@
 FactoryBot.define do
   factory :uploaded_file, class: Hyrax::UploadedFile do
     user
-    file { File.open(Hyrax::engine.root + 'spec/fixtures/image.jp2') }
+    file { File.open(Hyrax::Engine.root.join('spec', 'fixtures', 'image.jp2').to_s) }
 
     trait :audio do
-      file { File.open(Hyrax::engine.root + 'spec/fixtures/sample_mpeg4.mp4') }
+      file { File.open(Hyrax::Engine.root.join('spec', 'fixtures', 'sample_mpeg4.mp4').to_s) }
     end
   end
 end
