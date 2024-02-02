56d155f8f1fb4d980f23e89b0637de5f3f3bbb2b
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2024-02-02 18:05:50 -0500

    Fixing merge conflict

--- spec/factories/uploaded_files.rb
+++ spec/factories/uploaded_files.rb
@@ -2,17 +2,10 @@
 FactoryBot.define do
   factory :uploaded_file, class: Hyrax::UploadedFile do
     user
-<<<<<<< HEAD
-    file { File.open(Hyrax::Engine.root + 'spec/fixtures/image.jp2') }
-
-    trait :audio do
-      file { File.open(Hyrax::Engine.root + 'spec/fixtures/sample_mpeg4.mp4') }
-=======
     file { File.open(Hyrax::Engine.root.join('spec', 'fixtures', 'image.jp2').to_s) }
 
     trait :audio do
       file { File.open(Hyrax::Engine.root.join('spec', 'fixtures', 'sample_mpeg4.mp4').to_s) }
->>>>>>> main
     end
   end
 end
