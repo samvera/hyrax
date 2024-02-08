5fbf679c7c7c17dc39d8cc144aff5757ddfa9da6
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2024-02-08 11:55:33 -0500

    Adding feature limit class attribute (#6680)
    * Adding feature limit class attribute
    
    * 🐛 Fix subject of spec

--- spec/models/featured_work_spec.rb
+++ spec/models/featured_work_spec.rb
@@ -2,17 +2,23 @@
 RSpec.describe FeaturedWork, type: :model do
   let(:feature) { described_class.create(work_id: "99") }
 
+  describe '.feature_limit' do
+    subject { described_class.feature_limit }
+
+    it { is_expected.to be_a(Integer) }
+  end
+
   it "has a file" do
     expect(feature.work_id).to eq "99"
   end
 
   it "does not allow six features" do
-    5.times do |n|
+    described_class.feature_limit.times do |n|
       expect(described_class.create(work_id: n.to_s)).not_to be_new_record
     end
     described_class.create(work_id: "6").tap do |sixth|
       expect(sixth).to be_new_record
-      expect(sixth.errors.full_messages).to eq ["Limited to 5 featured works."]
+      expect(sixth.errors.full_messages).to eq ["Limited to #{described_class.feature_limit} featured works."]
     end
     expect(described_class.count).to eq 5
   end
@@ -29,7 +35,7 @@ RSpec.describe FeaturedWork, type: :model do
     end
     context "when five exist" do
       before do
-        5.times do |n|
+        described_class.feature_limit.times do |n|
           described_class.create(work_id: n.to_s)
         end
       end
