86d903ba4a2ad8acd8dae1a1f5555fffb4ba42b7
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2024-02-14 17:58:46 -0500

    ☑️ Allow downstream apps to re-use Hyrax factories and support

--- lib/hyrax/specs/factories.rb
+++ lib/hyrax/specs/factories.rb
@@ -2,6 +2,15 @@
 # factories of the test suite to the downstream application (e.g. Hyku).
 #
 # That way, we can create extensions of those factories in the downstream application.
-Hyrax::Engine.root.glob("spec/factories").each do |path|
+Hyrax::Engine.root.join("spec/support/**/*.rb").each do |path|
   require path.to_s
+end
+
+Hyrax::Engine.root.glob("spec/factories/**/*.rb").each do |path|
+  begin
+    require path.to_s
+  rescue FactoryBot::DuplicateDefinitionError => e
+    # It's alright maybe downstream defined these
+    Rails.logger.warn(e.message)
+  end
 end
