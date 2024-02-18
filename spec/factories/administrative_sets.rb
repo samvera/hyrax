ff1786b323b953a32e8e39f87b11454c484fe910
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2024-02-18 09:35:46 -0500

    Partial revert of previous commit

--- spec/factories/administrative_sets.rb
+++ spec/factories/administrative_sets.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 FactoryBot.define do
-  factory :hyrax_admin_set, class: Hyrax.config.admin_set_class do
+  factory :hyrax_admin_set, class: 'Hyrax::AdministrativeSet' do
     title { ['My Admin Set'] }
 
     transient do
