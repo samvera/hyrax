cc86fa88456b2d5021e52f8d8842128f960215d0
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2024-02-29 14:24:42 -0500

    ♻️ Declare strategy for custom query

--- lib/goddess/custom_query_container.rb
+++ lib/goddess/custom_query_container.rb
@@ -9,12 +9,39 @@ module Goddess
     #   @return [Hash<Symbol,Symbol>]
     #   Valid strategies are :find_multiple
     #   @see Goddess::Query::MethodMissingMachinations
+    #   @todo Audit the current custom queries and assign strategies.
     class_attribute(:known_custom_queries_and_their_strategies,
                     default: {
-                      find_parents: :find_multiple,
-                      find_ids_by_model: :find_multiple,
+                       find_by_date_range: :find_multiple,
+                      find_child_collection_ids: :find_multiple,
+                      find_child_collections: :find_multiple,
+                      find_child_file_set_ids: :find_multiple,
+                      find_child_file_sets: :find_multiple,
+                      find_child_fileset_ids: :find_multiple,
+                      find_child_filesets: :find_multiple,
+                      find_child_work_ids: :find_multiple,
+                      find_child_works: :find_multiple,
                       find_collections_by_type: :find_multiple,
-                      find_parent_work: :find_single_or_nil
+                      find_collections_for: :find_multiple,
+                      find_count_by: :count_multiple,
+                      find_extracted_text: :find_single,
+                      find_file_metadata_by: :find_single,
+                      find_file_metadata_by_alternate_identifier: :find_single,
+                      find_files: :find_multiple,
+                      find_ids_by_model: :find_multiple,
+                      find_many_by_alternate_ids: :find_multiple,
+                      find_many_file_metadata_by_ids: :find_multiple,
+                      find_many_file_metadata_by_use: :find_multiple,
+                      find_members_of: :find_multiple,
+                      find_models_by_access: :find_multiple,
+                      find_original_file: :find_single,
+                      find_parent_collection_ids: :find_multiple,
+                      find_parent_collections: :find_multiple,
+                      find_parent_work: :find_single_or_nil,
+                      find_parent_work_id: :find_single_or_nil,
+                      find_parents: :find_multiple,
+                      find_thumbnail: :find_single,
+                     find_access_control_for: :find_single,
                     })
     class_attribute(:fallback_query_strategy, default: :find_single)
     # @!endgroup Class Attributes
