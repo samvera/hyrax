e525a8295ccc1d33d6554b3ebaf6a50e2cc4689d
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2024-03-07 09:41:18 -0500

    🎁 Adding auto-backup of the thumbnail

--- lib/freyja/resource_factory.rb
+++ lib/freyja/resource_factory.rb
@@ -23,23 +23,20 @@ module Freyja
         # we want a valkyrie file using the valkyrie upload 
         thumbnail = Hyrax::DerivativePath.derivative_path_for_reference(resource, 'thumbnail') # if this includes the backup directory, don't do the next bit of work
         # target should be based off of the above path
-        unless thumbnail_path_moved?(thumbnail)
+        unless thumbnail_exists?(thumbnail)
 
-          if thumbnail.present? && File.exist?(thumbnail)
-            tempfile = Tempfile.new
-            tempfile.binmode
-            tempfile.write(File.read(thumbnail))
+          tempfile = Tempfile.new
+          tempfile.binmode
+          tempfile.write(File.read(thumbnail))
 
-            Hyrax::ValkyrieUpload.file(
-                                  filename: resource.label,
-                                  file_set: resource,
-                                  io: tempfile,
-                                  use: Hyrax::FileMetadata::Use::THUMBNAIL_IMAGE,
-                                  user: User.find_or_initialize_by(User.user_key_field => resource.depositor)
-                                )
-            move_thumbnail_to_backup(thumbnail)
-          end
-
+          Hyrax::ValkyrieUpload.file(
+            filename: resource.label,
+            file_set: resource,
+            io: tempfile,
+            use: Hyrax::FileMetadata::Use::THUMBNAIL_IMAGE,
+            user: User.find_or_initialize_by(User.user_key_field => resource.depositor)
+          )
+          move_thumbnail_to_backup(thumbnail)
         end
 
         # files section
@@ -63,12 +60,33 @@ module Freyja
 
     private
 
-    def thumbnail_path_moved?(thumbnail)
-
+    ##
+    # @param path [String] path to the expected thumbnail
+    #
+    # @return [TrueClass] when the thumbnail at the given path has not been
+    #         moved to the Valkyrie storage adapter.
+    # @return [FalseClass] when the thumbnail has been moved to the Valkyrie
+    #         storage adapter.
+    # @see #move_thumbnail_to_backup
+    def thumbnail_exists?(path)
+      path.present? && File.exist?(path)
     end
 
-    def move_thumbnail_to_backup(thumbnail)
+    ##
+    # Move the given file to a backup directory, which is derived by injecting
+    # "backup-thumbnails" into the :path after the
+    # {Hyrax.config.derivatives_path} and before the other subdirectories.
+    #
+    # @param path [String]
+    def move_thumbnail_to_backup(path)
+      # Don't move what's not there.
+      return unless thumbnail_exists?(path)
 
+      base_path = Hyrax.config.derivatives_path
+      target_dirname = File.dirname(path).sub(base_path, File.join(base_path, "backup-paths"))
+      FileUtils.mkdir_p(target_dirname)
+      target = File.join(target_dirname, File.basename(path))
+      FileUtils.mv(path, target)
     end
   end
 end
