6b989a6e2d11238e16f176a68f49188628e97415
Rob Kaufman <rob@notch8.com>
2024-05-30 17:50:06 -0700

    add fallback support to find_count_by

--- app/services/hyrax/custom_queries/find_count_by.rb
+++ app/services/hyrax/custom_queries/find_count_by.rb
@@ -25,6 +25,7 @@ module Hyrax
       # @param hash [Hash] the hash representation of the query
       def find_count_by(hash = {}, models: nil)
         return nil if models.empty? && hash.blank?
+        return find_count_by_af(hash, models: models) if !query_service.respond_to?(:orm_class)
 
         internal_array = ["{ #{hash.map { |k, v| "\"#{k}\": #{v}" }.join(', ')} }"] if hash.present?
         if models.empty?
@@ -34,6 +35,12 @@ module Hyrax
         else
           query_service.orm_class.count_by_sql(([find_count_by_properties_and_models_query] + internal_array + [models]))
         end
+      end
+
+      def find_count_by_af(hash, models: nil)
+        flat_hash = hash.map { |k, v| "#{k}: \"#{v}\"" }.join(' ')
+        flat_hash += " has_model_ssim: (#{models.map { |m| "\"#{m}\"" }.join(' OR ')})" unless models.empty?
+        Hyrax::SolrService.count(flat_hash)
       end
 
       def find_count_by_properties_and_models_query
