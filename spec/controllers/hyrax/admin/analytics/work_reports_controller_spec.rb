52293b5436bd9bee7ec37474d8f9f5af4f448ba6
David Campbell <davidcam-src@proton.me>
2024-06-04 11:13:45 -0400

    rubocop

--- spec/controllers/hyrax/admin/analytics/work_reports_controller_spec.rb
+++ spec/controllers/hyrax/admin/analytics/work_reports_controller_spec.rb
@@ -6,9 +6,9 @@ RSpec.describe Hyrax::Admin::Analytics::WorkReportsController, type: :controller
   describe 'GET #index' do
     context 'when user is not logged in' do
       it 'redirects to the login page' do
-            get :index
-            expect(response).to be_redirect
-            expect(flash[:alert]).to eq("You need to sign in or sign up before continuing.")
+        get :index
+        expect(response).to be_redirect
+        expect(flash[:alert]).to eq("You need to sign in or sign up before continuing.")
       end
     end
   end
