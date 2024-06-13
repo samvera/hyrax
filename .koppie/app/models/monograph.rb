39dfc2d47651fb1a85c2f0800257c8d56f2adcc1
Kirk Wang <kirk.wang@scientist.com>
2024-06-13 13:23:58 -0700

    Update Koppie m3_profile and add ENV guards

--- .koppie/app/models/monograph.rb
+++ .koppie/app/models/monograph.rb
@@ -3,6 +3,6 @@
 # Generated via
 #  `rails generate hyrax:work_resource Monograph`
 class Monograph < Hyrax::Work
-  include Hyrax::Schema(:basic_metadata)
-  include Hyrax::Schema(:monograph)
+  include Hyrax::Schema(:basic_metadata) unless ENV.fetch('HYRAX_FLEXIBLE', false)
+  include Hyrax::Schema(:monograph) unless ENV.fetch('HYRAX_FLEXIBLE', false)
 end
