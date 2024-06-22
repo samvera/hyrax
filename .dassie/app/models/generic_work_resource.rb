1db3e8021e1b286d9146582fe822cd982a2b95d7
Daniel Pierce <dlpierce@indiana.edu>
2024-06-21 20:07:12 -0400

    Only migrate when transitioning

--- .dassie/app/models/generic_work_resource.rb
+++ .dassie/app/models/generic_work_resource.rb
@@ -6,5 +6,5 @@ class GenericWorkResource < Hyrax::Work
   include Hyrax::Schema(:basic_metadata)
   include Hyrax::Schema(:generic_work_resource)
 
-  Hyrax::ValkyrieLazyMigration.migrating(self, from: GenericWork)
+  Hyrax::ValkyrieLazyMigration.migrating(self, from: GenericWork) if Hyrax.config.valkyrie_transition?
 end
