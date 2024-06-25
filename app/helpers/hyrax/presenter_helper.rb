32685a7769a783d77257ca1b3b1d676b0d8df779
Shana Moore <shana@scientist.com>
2024-06-25 12:22:59 -0700

    🚧 WIP - handle dynamic translations

--- app/helpers/hyrax/presenter_helper.rb
+++ app/helpers/hyrax/presenter_helper.rb
@@ -9,5 +9,25 @@ module Hyrax
 
       hash.select { |_, val| val['view'].present? }
     end
+
+    def conform_options(options)
+      hash_of_locales = options['view']['label']
+      current_locale = params['locale'] || I18n.locale.to_s
+      
+      # Check if hash_of_locales is a hash and contains the current locale
+      if hash_of_locales.is_a?(Hash) && hash_of_locales[current_locale].present?
+        # Create a copy of options to avoid modifying the original hash during iteration
+        updated_options = options.deep_dup
+        
+        # Update the label based on the current locale
+        updated_options['view']['label'] = hash_of_locales[current_locale]
+        
+        # Transform keys to symbols and return the modified options
+        return updated_options['view'].transform_keys(&:to_sym)
+      end
+    
+      # If label is not a hash or does not contain the locale, transform keys to symbols as usual
+      options['view'].transform_keys(&:to_sym)
+    end
   end
 end
