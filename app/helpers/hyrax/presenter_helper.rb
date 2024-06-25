3a601f59399c6605905897ac6900f3cf132ba13d
Shana Moore <shana@scientist.com>
2024-06-25 11:20:44 -0700

    🧹 Enables dynamic display for attribute rows
    In this commit we updated code to support dynamic rendering for attribute rows

--- app/helpers/hyrax/presenter_helper.rb
+++ app/helpers/hyrax/presenter_helper.rb
@@ -3,7 +3,11 @@
 module Hyrax
   module PresenterHelper
     def view_options_for(presenter)
-      Hyrax::Schema.schema_to_hash_for(presenter.model.class).select { |_, val| val['view'].present? }
+      model_name = presenter.model.model_name.name.constantize
+      hash = Hyrax::Schema.schema_to_hash_for(model_name) ||
+               Hyrax::Schema.schema_to_hash_for((model_name.to_s + 'Resource').safe_constantize)
+
+      hash.select { |_, val| val['view'].present? }
     end
   end
 end
