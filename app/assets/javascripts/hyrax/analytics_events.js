ac2e23178217ea7ed0e305bc96fd520069571eaf
Ryan Baumann <ryan.baumann@gmail.com>
2024-08-07 17:44:14 -0400

    Use $(document).ready instead of $(document).on("ready") (deprecated in jQuery 1.8 and removed in jQuery 3.0) (#6832)

--- app/assets/javascripts/hyrax/analytics_events.js
+++ app/assets/javascripts/hyrax/analytics_events.js
@@ -69,7 +69,7 @@ if (typeof Turbolinks !== 'undefined') {
     setupTracking()
   })
 } else {
-  $(document).on('ready', function() {
+  $(document).ready(function() {
     setupTracking()
   })
 }
