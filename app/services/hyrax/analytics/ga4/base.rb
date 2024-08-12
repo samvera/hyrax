69bbfb8680558160be99fbf26e57a26b75bd6f3b
Rob Kaufman <rob@notch8.com>
2024-08-12 16:09:51 -0700

    call the cops

--- app/services/hyrax/analytics/ga4/base.rb
+++ app/services/hyrax/analytics/ga4/base.rb
@@ -6,9 +6,9 @@ module Hyrax
         attr_reader :start_date, :end_date, :dimensions, :metrics
 
         def initialize(start_date:,
-          end_date:,
-          dimensions: [],
-          metrics: [])
+                       end_date:,
+                       dimensions: [],
+                       metrics: [])
           @start_date = start_date.to_date
           @end_date = end_date.to_date
           @dimensions = dimensions
