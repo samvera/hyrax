e2876d3014f2412bf5836c9bf0c2f4aa6b319c9b
Rob Kaufman <rob@notch8.com>
2024-10-11 09:59:20 -0700

    migration fixes

--- .dassie/db/migrate/20240606205216_add_contexts_to_hyrax_flexible_schemas.rb
+++ .dassie/db/migrate/20240606205216_add_contexts_to_hyrax_flexible_schemas.rb
@@ -1,4 +1,4 @@
-class AddContextsToHyraxFlexibleSchemas < ActiveRecord::Migration<%= migration_version %>
+class AddContextsToHyraxFlexibleSchemas < ActiveRecord::Migration[6.1]
   def change
     add_column :hyrax_flexible_schemas, :contexts, :text unless column_exists?(:hyrax_flexible_schemas, :contexts)
   end
