1e379be59b0cf3f5365a508b67c6e11730d4755a
LaRita Robinson <larita@scientist.com>
2024-10-11 17:26:16 -0400

    Add failing specs to fix

--- spec/wings/attribute_transformer_spec.rb
+++ spec/wings/attribute_transformer_spec.rb
@@ -6,20 +6,27 @@ require 'wings/attribute_transformer'
 
 RSpec.describe Wings::AttributeTransformer, :active_fedora do
   let(:id)   { 'moomin123' }
+  let(:based_near) { Hyrax::ControlledVocabularies::Location.new("https://sws.geonames.org/4920808/") }
   let(:work) { GenericWork.new(id: id, **attributes) }
 
   let(:attributes) do
     {
       title: ['fake title', 'fake title 2'],
       contributor: ['user1'],
-      description: ['a description']
+      description: ['a description'],
+      based_near:[based_near]
     }
   end
 
-  it "transform the attributes" do
-    expect(described_class.run(work))
+  it "transforms the attributes" do
+    converted_work = described_class.run(work)
+    expect(converted_work)
       .to include title: work.title,
                   contributor: work.contributor.first,
                   description: work.description.first
+    # ensure that based_near is converted from RDF::URI to URI string during valkyrization
+    converted_based_near = Array.wrap(converted_work[:based_near]).first
+    expect(converted_based_near).to be_a(String)
+    expect(converted_based_near).to eq(work.based_near.first.id)
   end
 end
