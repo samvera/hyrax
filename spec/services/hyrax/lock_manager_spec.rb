fddb0646d6c142b88abb8513515c3296e59271bd
David Moles <dmoles@dmoles.net>
2024-10-30 13:40:18 -0700

    Allow LockManager#lock callers to override configuration

--- spec/services/hyrax/lock_manager_spec.rb
+++ spec/services/hyrax/lock_manager_spec.rb
@@ -13,24 +13,81 @@ RSpec.describe Hyrax::LockManager do
       expect { |probe| subject.lock('foobar', &probe) }.to yield_with_no_args
     end
 
-    it "uses the configured ttl" do
-      client = instance_double(Redlock::Client)
-      allow(Redlock::Client).to receive(:new).and_return(client)
+    describe 'ttl' do
+      it "uses the configured ttl" do
+        client = instance_double(Redlock::Client)
+        allow(Redlock::Client).to receive(:new).and_return(client)
 
-      key = 'the key'
-      expect(client).to receive(:lock).with(key, ttl).and_yield(true)
-      subject.lock(key) { |_| }
+        key = 'the key'
+        expect(client).to receive(:lock).with(key, ttl).and_yield(true)
+        subject.lock(key) { |_| }
+      end
+
+      it "accepts an optional ttl argument" do
+        new_ttl = 1000
+        expect(new_ttl).not_to eq(ttl) # just to be sure
+
+        client = instance_double(Redlock::Client)
+        allow(Redlock::Client).to receive(:new).and_return(client)
+
+        key = 'the key'
+        expect(client).to receive(:lock).with(key, new_ttl).and_yield(true)
+        subject.lock(key, ttl: new_ttl) { |_| }
+      end
     end
 
-    it "uses the configured retry_count and retry_delay" do
-      client = instance_double(Redlock::Client)
-      expect(Redlock::Client)
-        .to receive(:new)
-        .with(kind_of(Array), retry_count: retry_count, retry_delay: retry_delay)
-        .and_return(client)
+    describe 'retry_count' do
+      it "uses the configured retry_count" do
+        client = instance_double(Redlock::Client)
+        expect(Redlock::Client)
+          .to receive(:new)
+          .with(kind_of(Array), retry_count: retry_count, retry_delay: kind_of(Integer))
+          .and_return(client)
 
-      allow(client).to receive(:lock).and_yield(true)
-      subject.lock("a key") { |_| }
+        allow(client).to receive(:lock).and_yield(true)
+        subject.lock("a key") { |_| }
+      end
+
+      it "accepts an optional retry_count argument" do
+        new_retry_count = 11
+        expect(new_retry_count).not_to eq(retry_count) # just to be sure
+
+        client = instance_double(Redlock::Client)
+        expect(Redlock::Client)
+          .to receive(:new)
+          .with(kind_of(Array), retry_count: new_retry_count, retry_delay: retry_delay)
+          .and_return(client)
+
+        allow(client).to receive(:lock).and_yield(true)
+        subject.lock("a key", retry_count: new_retry_count) { |_| }
+      end
+    end
+
+    describe 'retry_delay' do
+      it "uses the configured retry_delay" do
+        client = instance_double(Redlock::Client)
+        expect(Redlock::Client)
+          .to receive(:new)
+          .with(kind_of(Array), retry_count: kind_of(Integer), retry_delay: retry_delay)
+          .and_return(client)
+
+        allow(client).to receive(:lock).and_yield(true)
+        subject.lock("a key") { |_| }
+      end
+
+      it "accepts an optional retry_delay argument" do
+        new_retry_delay = 11
+        expect(new_retry_delay).not_to eq(retry_delay) # just to be sure
+
+        client = instance_double(Redlock::Client)
+        expect(Redlock::Client)
+          .to receive(:new)
+          .with(kind_of(Array), retry_count: retry_count, retry_delay: new_retry_delay)
+          .and_return(client)
+
+        allow(client).to receive(:lock).and_yield(true)
+        subject.lock("a key", retry_delay: new_retry_delay) { |_| }
+      end
     end
   end
 end
