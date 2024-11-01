3f46a2189f307f83760374daa044851228c2e2cb
Shana Moore <shana@scientist.com>
2024-11-01 10:48:11 -0700

    ♻️ revert uploads controller
    The refactor still caused bugs. I've live tested the original implementation on the server and no longer see the retried jobs at this point.

--- app/controllers/hyrax/uploads_controller.rb
+++ app/controllers/hyrax/uploads_controller.rb
@@ -1,63 +1,34 @@
 # frozen_string_literal: true
-
 module Hyrax
   class UploadsController < ApplicationController
     load_and_authorize_resource class: Hyrax::UploadedFile
 
     def create
       if params[:id].blank?
-        handle_new_upload
+        @upload.attributes = { file: params[:files].first,
+                              user: current_user }
       else
-        handle_chunked_upload
-      end
+        @upload = Hyrax::UploadedFile.find(params[:id])
+        unpersisted_upload = Hyrax::UploadedFile.new(file: params[:files].first, user: current_user) 
 
+        # deal with chunks
+        current_size = @upload.file.size
+        content_range = request.headers['CONTENT-RANGE']
+        begin_of_chunk = content_range[/\ (.*?)-/,1].to_i # "bytes 100-999999/1973660678" will return '100'
+  
+        # Add the following chunk to the incomplete upload
+        if @upload.file.present? && begin_of_chunk == current_size
+          File.open(@upload.file.path, "ab") { |f| f.write(params[:files].first.read) } 
+        else
+          @upload.file = unpersisted_upload.file
+        end
+      end
       @upload.save!
     end
 
     def destroy
       @upload.destroy
       head :no_content
-    end
-
-    private
-
-    def handle_new_upload
-      @upload.attributes = { file: params[:files].first, user: current_user }
-    end
-
-    def chunk_valid?(upload)
-      content_range = request.headers['CONTENT-RANGE']
-      return false unless content_range
-
-      begin_of_chunk = content_range[/\ (.*?)-/, 1].to_i
-      current_size = upload.file.size
-
-      upload.file.present? && begin_of_chunk == current_size
-    end
-
-    def handle_chunked_upload
-      @upload = Hyrax::UploadedFile.find(params[:id])
-      unpersisted_upload = Hyrax::UploadedFile.new(file: params[:files].first, user: current_user)
-
-      if chunk_valid?(@upload)
-        append_chunk(@upload)
-      else
-        replace_file(@upload, unpersisted_upload)
-      end
-    end
-
-    def append_chunk(upload)
-      File.open(upload.file.path, "ab") do |f|
-        f.write(params[:files].first.read)
-      end
-
-      upload.reload
-    end
-
-    def replace_file(upload, unpersisted_upload)
-      upload.file = unpersisted_upload.file
-      upload.save!
-      upload.reload
     end
   end
 end
