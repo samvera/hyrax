3423cf85b44da9481cfdd9ebe160377cbbb9e9ca
Shana Moore <shana@scientist.com>
2024-11-01 13:18:00 -0700

    fix for failing specs

--- app/controllers/hyrax/uploads_controller.rb
+++ app/controllers/hyrax/uploads_controller.rb
@@ -23,12 +23,15 @@ module Hyrax
     def upload_with_chunking
       @upload = Hyrax::UploadedFile.find(params[:id])
       unpersisted_upload = Hyrax::UploadedFile.new(file: params[:files].first, user: current_user)
-
+    
+      # Check if CONTENT-RANGE header is present
+      content_range = request.headers['CONTENT-RANGE']
+      return @upload.file = unpersisted_upload.file if content_range.nil?
+    
       # deal with chunks
       current_size = @upload.file.size
-      content_range = request.headers['CONTENT-RANGE']
       begin_of_chunk = content_range[/\ (.*?)-/, 1].to_i # "bytes 100-999999/1973660678" will return '100'
-
+    
       # Add the following chunk to the incomplete upload
       if @upload.file.present? && begin_of_chunk == current_size
         File.open(@upload.file.path, "ab") { |f| f.write(params[:files].first.read) }
