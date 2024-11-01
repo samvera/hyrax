03819cb3c6184e43f4265d44a3722676738fff01
Shana Moore <shana@scientist.com>
2024-11-01 11:45:47 -0700

    💄 rubocop fix

--- app/controllers/hyrax/uploads_controller.rb
+++ app/controllers/hyrax/uploads_controller.rb
@@ -3,28 +3,32 @@ module Hyrax
   class UploadsController < ApplicationController
     load_and_authorize_resource class: Hyrax::UploadedFile
 
+    # rubocop:disable Metrics/AbcSize
+    # rubocop:disable Metrics/MethodLength
     def create
       if params[:id].blank?
         @upload.attributes = { file: params[:files].first,
-                              user: current_user }
+                               user: current_user }
       else
         @upload = Hyrax::UploadedFile.find(params[:id])
-        unpersisted_upload = Hyrax::UploadedFile.new(file: params[:files].first, user: current_user) 
+        unpersisted_upload = Hyrax::UploadedFile.new(file: params[:files].first, user: current_user)
 
         # deal with chunks
         current_size = @upload.file.size
         content_range = request.headers['CONTENT-RANGE']
-        begin_of_chunk = content_range[/\ (.*?)-/,1].to_i # "bytes 100-999999/1973660678" will return '100'
-  
+        begin_of_chunk = content_range[/\ (.*?)-/, 1].to_i # "bytes 100-999999/1973660678" will return '100'
+
         # Add the following chunk to the incomplete upload
         if @upload.file.present? && begin_of_chunk == current_size
-          File.open(@upload.file.path, "ab") { |f| f.write(params[:files].first.read) } 
+          File.open(@upload.file.path, "ab") { |f| f.write(params[:files].first.read) }
         else
           @upload.file = unpersisted_upload.file
         end
       end
       @upload.save!
     end
+    # rubocop:enable Metrics/AbcSize
+    # rubocop:enable Metrics/MethodLength
 
     def destroy
       @upload.destroy
