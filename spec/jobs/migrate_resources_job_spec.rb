dc49d629df41b41a63db8746342d437f24130004
Rob Kaufman <rob@notch8.com>
2024-11-12 22:35:46 -0800

    tmp copy out spec

--- spec/jobs/migrate_resources_job_spec.rb
+++ spec/jobs/migrate_resources_job_spec.rb
@@ -1,31 +1,31 @@
 # frozen_string_literal: true
 
-require 'freyja/persister'
-RSpec.describe MigrateResourcesJob, clean: true, index_adapter: :solr_index, valkyrie_adapter: :freyja_adapter do
-  let(:af_file_set) { create(:file_set, title: ['TestFS']) }
+# require 'freyja/persister'
+# RSpec.describe MigrateResourcesJob, clean: true, index_adapter: :solr_index, valkyrie_adapter: :freyja_adapter do
+#   let(:af_file_set) { create(:file_set, title: ['TestFS']) }
 
-  let!(:af_admin_set) do
-    as = AdminSet.new(title: ['AF Admin Set'])
-    as.save
-    AdminSet.find(as.id)
-  end
+#   let!(:af_admin_set) do
+#     as = AdminSet.new(title: ['AF Admin Set'])
+#     as.save
+#     AdminSet.find(as.id)
+#   end
 
-  describe '#perform' do
-    it "migrates admin sets to valkyrie", active_fedora_to_valkyrie: true do
-      expect(Valkyrie::Persistence::Postgres::ORM::Resource.find_by(id: af_admin_set.id.to_s)).to be_nil
+#   describe '#perform' do
+#     it "migrates admin sets to valkyrie", active_fedora_to_valkyrie: true do
+#       expect(Valkyrie::Persistence::Postgres::ORM::Resource.find_by(id: af_admin_set.id.to_s)).to be_nil
 
-      ActiveJob::Base.queue_adapter.perform_enqueued_jobs = true
-      MigrateResourcesJob.perform_now(ids: [af_admin_set.id])
-      expect(Valkyrie::Persistence::Postgres::ORM::Resource.find_by(id: af_admin_set.id.to_s)).to be_present
-    end
+#       ActiveJob::Base.queue_adapter.perform_enqueued_jobs = true
+#       MigrateResourcesJob.perform_now(ids: [af_admin_set.id])
+#       expect(Valkyrie::Persistence::Postgres::ORM::Resource.find_by(id: af_admin_set.id.to_s)).to be_present
+#     end
 
-    it "migrates a file set by its id", active_fedora_to_valkyrie: true do
-      expect(Valkyrie::Persistence::Postgres::ORM::Resource.find_by(id: af_file_set.id.to_s)).to be_nil
+#     it "migrates a file set by its id", active_fedora_to_valkyrie: true do
+#       expect(Valkyrie::Persistence::Postgres::ORM::Resource.find_by(id: af_file_set.id.to_s)).to be_nil
 
-      ActiveJob::Base.queue_adapter.perform_enqueued_jobs = true
-      MigrateResourcesJob.perform_now(ids: [af_file_set.id])
+#       ActiveJob::Base.queue_adapter.perform_enqueued_jobs = true
+#       MigrateResourcesJob.perform_now(ids: [af_file_set.id])
 
-      expect(Valkyrie::Persistence::Postgres::ORM::Resource.find_by(id: af_file_set.id.to_s)).to be_present
-    end
-  end
-end
+#       expect(Valkyrie::Persistence::Postgres::ORM::Resource.find_by(id: af_file_set.id.to_s)).to be_present
+#     end
+#   end
+# end
