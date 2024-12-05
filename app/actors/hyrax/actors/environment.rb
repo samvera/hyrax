f8aac41e44737c084b02a07a35012a7426cbcd1e
Randall Floyd <rdfloyd@indiana.edu>
2024-12-05 11:21:48 -0500

    Fixing entrypoint into actor stack that casts unpermitted attributes to hash

--- app/actors/hyrax/actors/environment.rb
+++ app/actors/hyrax/actors/environment.rb
@@ -8,6 +8,15 @@ module Hyrax
       def initialize(curation_concern, current_ability, attributes)
         @curation_concern = curation_concern
         @current_ability = current_ability
+
+        # TODO: how to safely permit a variable list of attributes if we don't know them in advance?
+        # It seems as though most attributes arriving here are already a hash, probably because
+        # they were handled and permitted via a form(?).  But if they are posted directly to the
+        # controller, the attributes arrive as ActionController::Parameters.
+        # So, this is no longer allowed because keys throughout structure haven't been permitted yet:
+        # @attributes = attributes.to_h.with_indifferent_access
+        #
+        # attributes.permit! if attributes.class == ActionController::Parameters
         @attributes = attributes.to_h.with_indifferent_access
       end
 
