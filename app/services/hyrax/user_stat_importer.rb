8cfc4cf1ce6e8be1a4b5c871d130f66453d8a4fb
Kirk Wang <kirk.wang@scientist.com>
2025-02-11 15:50:21 -0800

    Add fixes for GA4
    In the base, we are adding a few nil guards to we don't end up with a
    NoMethodError for NilClass. In the GA4 module we are adding a case where
    if we are getting a Hash already, don't try to JSON.parse it.  In the
    UserStatImporter we are switching a query to the Valkyrie way, which
    should already be in main.

--- app/services/hyrax/user_stat_importer.rb
+++ app/services/hyrax/user_stat_importer.rb
@@ -49,7 +49,7 @@ module Hyrax
 
     def process_files(stats, user, start_date)
       file_ids_for_user(user).each do |file_id|
-        file = ::FileSet.find(file_id)
+        file = Hyrax.query_service.find_by(id: file_id)
         view_stats = extract_stats_for(object: file, from: FileViewStat, start_date: start_date, user: user)
         stats = tally_results(view_stats, :views, stats) if view_stats.present?
         delay
