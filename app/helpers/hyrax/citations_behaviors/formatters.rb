d080551a1440bd5b8c39e620e8fcbe369b222197
Chris Colvard <chris.colvard@gmail.com>
2025-02-11 09:31:02 -0500

    Additional changes to get tests to run

--- app/helpers/hyrax/citations_behaviors/formatters.rb
+++ app/helpers/hyrax/citations_behaviors/formatters.rb
@@ -12,11 +12,6 @@ module Hyrax
           @view_context = view_context
         end
       end
-
-      autoload :ApaFormatter, 'hyrax/citations_behaviors/formatters/apa_formatter'
-      autoload :ChicagoFormatter, 'hyrax/citations_behaviors/formatters/chicago_formatter'
-      autoload :MlaFormatter, 'hyrax/citations_behaviors/formatters/mla_formatter'
-      autoload :OpenUrlFormatter, 'hyrax/citations_behaviors/formatters/open_url_formatter'
     end
   end
 end
--- app/services/hyrax/listeners.rb
+++ app/services/hyrax/listeners.rb
@@ -1,13 +1,5 @@
 # frozen_string_literal: true
 module Hyrax
-  # @note
-  #    Did you encounter an exception similar to the following:
-  #
-  #    "A copy of Hyrax::Listeners::ObjectLifecycleListener has been removed from the module tree but is still active!"
-  #
-  #    You may need to register a listener as autoload.  See
-  #    ./app/services/hyrax/listeners.rb
-  #
   # When an instance of a listener class is registered with
   # Dry::Events::Publisher#subscribe, its method(s) will be called when a event
   # is published that maps to the method name using the pattern:
