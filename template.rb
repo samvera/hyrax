bb8eda6988d82244fc6da52c66a8e85781a5d69a
Daniel Pierce <dlpierce@iu.edu>
2025-02-27 13:28:57 -0500

    Fix chars eaten by merge conflict

--- template.rb
+++ template.rb
@@ -1,4 +1,4 @@
-# frozen_string_lite
+# frozen_string_literal: true
 
 insert_into_file 'config/application.rb', after: /config\.load_defaults [0-9.]+$/ do
   "\n    config.add_autoload_paths_to_load_path = true"
