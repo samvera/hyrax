f4e2df72400e99e5d3c91e71065d77602a6b04c6
LaRita Robinson <larita@notch8.com>
2025-03-17 11:06:01 -0400

    Don't rerun sipity job 5 times
    We don't know until we try ActiveFedora whether there is might be a
    problem with the sipity entry. We should not rerun the job 5 times
    when the work was never in Fedora.

--- app/jobs/migrate_sipity_entity_job.rb
+++ app/jobs/migrate_sipity_entity_job.rb
@@ -5,11 +5,14 @@ class MigrateSipityEntityJob < ApplicationJob
   # input [String] id of a migrated resource
   def perform(id:)
     resource = Hyrax.query_service.find_by(id: id)
-    new_gid = Hyrax::GlobalID(resource).to_s
     work = resource.internal_resource.constantize.find(id)
+    new_gid = Hyrax::GlobalID(resource).to_s
     original_gid = Hyrax::GlobalID(work).to_s
     return if new_gid == original_gid
     original_entity = Sipity::Entity.find_by(proxy_for_global_id: original_gid)
     original_entity.update(proxy_for_global_id: new_gid)
+  rescue ActiveFedora::ObjectNotFoundError
+    # this happens when the resource was never in Fedora so there is nothing to migrate.
+    # We don't want to retry the job so we don't raise an error.
   end
 end
