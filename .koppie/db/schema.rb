600f8f4ac16ae964abbbcefdae8437185dace0a8
Shana Moore <shana.lavina.moore@gmail.com>
2025-03-26 11:24:49 -0700

    Update schema.rb

--- .koppie/db/schema.rb
+++ .koppie/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_06_06_205215) do
+ActiveRecord::Schema.define(version: 2024_06_06_205216) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -168,6 +168,7 @@ ActiveRecord::Schema.define(version: 2024_06_06_205215) do
     t.text "profile"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "contexts"
   end
 
   create_table "job_io_wrappers", force: :cascade do |t|
