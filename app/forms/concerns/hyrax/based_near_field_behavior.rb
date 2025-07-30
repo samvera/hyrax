de3d46fcaca954d3cdb368a051bd32587e50bdb5
Rob Kaufman <rob@notch8.com>
2025-07-29 22:24:10 -0700

    fix based near ordering issue

--- app/forms/concerns/hyrax/based_near_field_behavior.rb
+++ app/forms/concerns/hyrax/based_near_field_behavior.rb
@@ -5,12 +5,19 @@ module Hyrax
     # The form expects a ControlledVocabularies::Location object as input and produces a hash like those
     # used with accepts_nested_attributes_for.
     def self.included(descendant)
-      descendant.property :based_near_attributes, virtual: true, populator: :based_near_populator, prepopulator: :based_near_prepopulator
+      descendant.property :based_near_attributes, virtual: true, populator: :based_near_attributes_populator, prepopulator: :based_near_attributes_prepopulator
+    end
+
+    # there is a race condition during validation that leaves the based_near field in an inconsistent state.
+    # we skip the unedited based_near for validation and only handle it during attribute population
+    def deserialize(params)
+      params = deserialize!(params)
+      deserializer.new(self).from_hash(params.except('based_near'))
     end
 
     private
 
-    def based_near_populator(fragment:, **_options)
+    def based_near_attributes_populator(fragment:, **_options)
       return unless respond_to?(:based_near)
       adds = []
       deletes = []
@@ -25,7 +32,7 @@ module Hyrax
       self.based_near = ((model.based_near + adds) - deletes).uniq
     end
 
-    def based_near_prepopulator
+    def based_near_attributes_prepopulator
       return unless respond_to?(:based_near)
       self.based_near = based_near&.map do |loc|
         uri = RDF::URI.parse(loc)
