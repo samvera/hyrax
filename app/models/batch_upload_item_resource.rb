70de906112c4af703d3eec686d50fa4c087fd7fb
LaRita Robinson <larita@notch8.com>
2025-08-19 17:14:15 -0400

    Flexiblity requires a resource object

---
+++ app/models/batch_upload_item_resource.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+# This stands in for an object to be created from the BatchUploadForm.
+# It should never actually be persisted in the repository.
+# The properties on this form should be copied to a real work type.
+class BatchUploadItemResource  < Hyrax::Forms::ResourceForm
+  include Hyrax::FormFields(:basic_metadata) unless Hyrax.config.flexible?
+
+  attr_accessor :payload_concern # a Class name: what is this a batch of?
+
+  # This mocks out the behavior of Hydra::PCDM::PcdmBehavior
+  def in_collection_ids
+    []
+  end
+
+  def create_or_update
+    raise "This is a read only record"
+  end
+end
