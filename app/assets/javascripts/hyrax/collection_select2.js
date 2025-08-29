b9dd21b5ebb4a61a7c766d18c4224fc3939d7d4d
Kirk Wang <k3wang@gmail.com>
2025-08-29 16:11:59 -0700

    Fix back button breaking select2
    This commit will add logic to reinitialize select2 if the users clicks
    the back button.  Previously, the select2 element would not function and
    would require a refresh.

--- app/assets/javascripts/hyrax/collection_select2.js
+++ app/assets/javascripts/hyrax/collection_select2.js
@@ -20,3 +20,26 @@
 Blacklight.onLoad(function() {
   $('select.collection-select2').collectionSearch();
 });
+
+// Fix for if select2 breaks because of clicking the back button on the browser
+// Essentially we delete the leftover elements and reinitialize it
+window.addEventListener('popstate', function(event) {
+  Blacklight.onLoad(function() {
+    $('.select2-drop-mask').remove();
+    $('.select2-drop').remove();
+
+    $('select.collection-select2').each(function() {
+      var $select = $(this);
+
+      // Find and remove the broken Select2 container
+      $select.siblings('.select2-container').remove();
+      $select.next('.select2-container').remove();
+      $select.parent().find('.select2-container').remove();
+
+      // Make sure the select is visible and ready
+      $select.show().removeClass('select2-hidden-accessible');
+
+      $select.collectionSearch();
+    });
+  });
+});
