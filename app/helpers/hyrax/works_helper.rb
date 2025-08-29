9a9e28e13800be01f7c1139eeede27f5c9f6fe4f
Kirk Wang <k3wang@gmail.com>
2025-08-29 16:11:59 -0700

    Fix back button breaking select2
    This commit will add logic to reinitialize select2 if the users clicks
    the back button.  Previously, the select2 element would not function and
    would require a refresh.

--- app/helpers/hyrax/works_helper.rb
+++ app/helpers/hyrax/works_helper.rb
@@ -3,10 +3,12 @@
 module Hyrax
   module WorksHelper
     def available_collections(work:)
+      return [] if @current_ability.blank?
+
       all_collections = Hyrax::CollectionsService.new(self).search_results(:deposit)
       return all_collections if work.blank?
 
-      all_collections.reject { |col| col.id.in?(work.member_of_collection_ids) }
+      all_collections.reject { |col| work.member_of_collection_ids.include?(col.id) }
     end
   end
 end
