fc1a2c3f9969a817745af79281eca59dafc6a855
Daniel Pierce <dlpierce@iu.edu>
2025-09-18 15:54:59 -0400

    Update spec for clamby

--- spec/models/hyrax/virus_scanner_spec.rb
+++ spec/models/hyrax/virus_scanner_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 require 'spec_helper'
 
-RSpec.describe Hyrax::VirusScanner do
+RSpec.describe Hyrax::VirusScanner, :virus_scan do
   let(:file)   { '/tmp/path' }
   let(:logger) { Logger.new(nil) }
 
@@ -9,30 +9,16 @@ RSpec.describe Hyrax::VirusScanner do
 
   subject { described_class.new(file) }
 
-  context 'when ClamAV is defined' do
-    before do
-      class ClamAV
-        def self.instance
-          @instance ||= new
-        end
-
-        def scanfile(path)
-          puts "scanfile: #{path}"
-        end
-      end
-    end
-    after do
-      Object.send(:remove_const, :ClamAV)
-    end
+  context 'when Clamby is defined' do # Included in test app's Gemfile
     context 'with a clean file' do
-      before { allow(ClamAV.instance).to receive(:scanfile).with('/tmp/path').and_return(0) }
+      before { allow(Clamby::Command).to receive(:scan).with('/tmp/path').and_return(false) }
       it 'returns false with no warning' do
         expect(Hyrax.logger).not_to receive(:warn)
         is_expected.not_to be_infected
       end
     end
     context 'with an infected file' do
-      before { allow(ClamAV.instance).to receive(:scanfile).with('/tmp/path').and_return(1) }
+      before { allow(Clamby::Command).to receive(:scan).with('/tmp/path').and_return(true) }
       it 'returns true with a warning' do
         expect(Hyrax.logger).to receive(:warn).with(kind_of(String))
         is_expected.to be_infected
@@ -40,10 +26,9 @@ RSpec.describe Hyrax::VirusScanner do
     end
   end
 
-  context 'when ClamAV is not defined' do
-    before { Object.send(:remove_const, :ClamAV) if defined?(ClamAV) }
-
+  context 'when Clamby is not defined' do
     it 'returns false' do
+      hide_const('Clamby')
       # we used to test the warning here, but we suppress it for the test env now
       is_expected.not_to be_infected
     end
