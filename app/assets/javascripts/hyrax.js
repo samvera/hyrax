samvera/hyrax

commit 6aa82a5f1a79d24a88a592980c3a978ab5d626d0

Add to collection UI improvement - From 5.0-stable to main
This commit will favor using the select2 with the drop down with a pre-
populated list of collections over the current implementation where it
makes a query to find the collections.  Prior, the user would need to
know what the collection's name is before using this interface.  Now the
user has the option of scrolling through and also using the type
function to find the collection for deposit.

Co-authored-by: Kirk Wang <k3wang@gmail.com>

app/assets/javascripts/hyrax.js
@@ -1,6 +1,5 @@
 //= require jquery-ui
 
-
 //= require select2
 //= require fixedsticky
 
@@ -71,6 +70,7 @@
 //= require hyrax/select_work_type
 //= require hyrax/collections
 //= require hyrax/collections_v2
+//= require hyrax/collection_select2
 //= require hyrax/collection_types
 //= require hyrax/collections_utils
 //= require hyrax/select_collection_type

app/assets/javascripts/hyrax/collection_select2.js
@@ -1,0 +1,45 @@
+(function($) {
+  $.fn.collectionSearch = function() {
+    return this.each(function() {
+      var $select = $(this);
+
+      // Skip if already initialized
+      if ($select.hasClass('select2-hidden-accessible') && $select.data('select2')) {
+        return;
+      }
+
+      $select.select2({
+        placeholder: $select.data('placeholder') || 'Select',
+        allowClear: true,
+        dropdownParent: $select.closest('.modal-body')
+      });
+    });
+  };
+})(jQuery);
+
+Blacklight.onLoad(function() {
+  $('select.collection-select2').collectionSearch();
+});
+
+// Fix for if select2 breaks because of clicking the back button on the browser
+// Essentially we delete the leftover elements and reinitialize it
+window.addEventListener('popstate', function(event) {
+  Blacklight.onLoad(function() {
+    $('.select2-drop-mask').remove();
+    $('.select2-drop').remove();
+
+    $('select.collection-select2').each(function() {
+      var $select = $(this);
+
+      // Find and remove the broken Select2 container
+      $select.siblings('.select2-container').remove();
+      $select.next('.select2-container').remove();
+      $select.parent().find('.select2-container').remove();
+
+      // Make sure the select is visible and ready
+      $select.show().removeClass('select2-hidden-accessible');
+
+      $select.collectionSearch();
+    });
+  });
+});

app/assets/javascripts/hyrax/relationships/control.es6
@@ -18,7 +18,7 @@ export default class RelationshipsControl {
     this.element = $(element)
     this.members = this.element.data('members')
     this.registry = new Registry(this.element.find('tbody'), paramKey, property, templateId)
-    this.input = this.element.find(`[data-autocomplete]`)
+    this.input = this.element.find(`[data-autocomplete], .collection-select2`)
     this.warning = this.element.find(".message.has-warning")
     this.addButton = this.element.find("[data-behavior='add-relationship']")
     this.errors = null

app/helpers/hyrax/hyrax_helper_behavior.rb
@@ -20,7 +20,7 @@ module Hyrax
     include Hyrax::WorkflowsHelper
     include Hyrax::FacetsHelper
     include Hyrax::AttributesHelper
-
+    include Hyrax::WorksHelper
 
     ##
     # @return [Array<String>] the list of admin sets available for creating works for this user

app/helpers/hyrax/works_helper.rb
@@ -1,0 +1,14 @@
+# frozen_string_literal: true
+
+module Hyrax
+  module WorksHelper
+    def available_collections(work:)
+      return [] if @current_ability.blank?
+
+      all_collections = Hyrax::CollectionsService.new(self).search_results(:deposit)
+      return all_collections if work.blank?
+
+      all_collections.reject { |col| work.member_of_collection_ids.include?(col.id) }
+    end
+  end
+end

app/views/hyrax/base/_form_member_of_collections.html.erb
@@ -15,12 +15,12 @@ HTML Properties:
 
   <div class="form-inline mb-3">
       <%= f.label :member_of_collection_ids %>
-      <%= f.input_field :member_of_collection_ids,
-                  prompt: :translate,
-                  data: {
-                    autocomplete: 'collection',
-                    'autocomplete-url' => Rails.application.routes.url_helpers.qa_path + '/search/collections?access=deposit'
-                  } %>
+      <select name="member_of_collection_ids" class="ml-2 form-control collection-select2" style="width: 12rem;" data-placeholder="<%= t('hyrax.dashboard.my.action.select') %>">
+        <option value=""></option>
+        <% available_collections(work: @curation_concern).each do |coll| %>
+          <option value="<%= coll.id %>"><%= coll.title.first %></option>
+        <% end %>
+      </select>
       <a href="#" onclick="return false;" class="btn btn-secondary ml-2" data-behavior="add-relationship"><%= t('.add') %></a>
   </div>
 

app/views/hyrax/dashboard/collections/_button_for_update_collection.html.erb
@@ -3,5 +3,6 @@
 <%# label -- button label %>
 <%= button_to label, hyrax.dashboard_collection_path(collection_id),
               method: :post,
-              class: "btn btn-primary submits-batches",
+              class: "btn btn-primary submits-batches modal-submit-button",
+              disabled: @add_works_to_collection.blank?,
               data: { behavior: 'updates-collection' } %>

app/views/hyrax/dashboard/collections/_form_for_select_collection.html.erb
@@ -1,4 +1,4 @@
-<div role="dialog" class="modal collection-list-modal fade" id="collection-list-container" tabindex="-1" aria-labelledby="col_add_title">
+<div role="dialog" class="modal collection-list-modal fade disable-unless-selected" id="collection-list-container" tabindex="-1" aria-labelledby="col_add_title">
   <div class="modal-dialog text-left">
       <div class="modal-content">
         <div class="modal-header">
@@ -12,20 +12,17 @@
             <div class="collection-list">
               <div class="form-group">
                 <p><%= t("hyrax.collection.select_form.select_heading") %></p>
-              
                 <% if @add_works_to_collection.present? %>
                   <%= text_field_tag 'member_of_collection_label', @add_works_to_collection_label, disabled: true %>
                   <%= hidden_field_tag 'member_of_collection_ids', @add_works_to_collection %>
                 <% else %>
-                  <%= text_field_tag 'member_of_collection_ids', nil,
-                              prompt: :translate,
-                              data: {
-                                placeholder: t('simple_form.placeholders.defaults.member_of_collection_ids'),
-                                autocomplete: 'collection',
-                                'autocomplete-url' => Rails.application.routes.url_helpers.qa_path + '/search/collections?access=deposit'
-                              } %>
+                  <select name="member_of_collection_ids" id="member_of_collection_ids" class="form-control collection-select2" data-placeholder="<%= t('hyrax.dashboard.my.action.select') %>">
+                    <option value=""></option>
+                    <% available_collections(work: @presenter&.solr_document).each do |coll| %>
+                      <option value="<%= coll.id %>"><%= coll.title.first %></option>
+                    <% end %>
+                  </select>
                 <% end %>
-            
               </div>
             </div><!-- collection-list -->
           <% end %> <!-- else -->

commit 00fea3d5cca3855b299b717319b25a146d5c6914

Fix collection select 2 interferance
  The commit 6aa82a5f1a79d24a88a592980c3a978ab5d626d0 broke the spec at line 298 in spec/features/dashboard/collection_spec.rb because it introduced a new JavaScript file
  (collection_select2.js) that was globally initializing Select2 on elements with the class collection-select2. This global initialization was interfering with the existing Select2
  initialization for the location search field (which uses Select2 for the geonames autocomplete).

  The issue was that the popstate event handler and global initialization in collection_select2.js was too aggressive and was disrupting other Select2 instances on the page, causing the
   location field's Select2 element with ID #s2id_autogen1_search to not be created properly.

  Solution

  I fixed the issue by:

  1. Removing the global collection_select2.js file that was being loaded in the asset pipeline
  2. Integrating the Select2 initialization directly into the RelationshipsControl class (app/assets/javascripts/hyrax/relationships/control.es6) with a new initializeSelect2() method
  that:
    - Only initializes Select2 on collection-select2 elements within the relationship control context
    - Checks if the element is already initialized to avoid conflicts
    - Uses appropriate dropdown parent settings for modals

  This approach ensures that:
  - Collection select2 dropdowns work correctly when part of the relationships control
  - There's no global JavaScript that interferes with other Select2 instances like the location field
  - The test now passes as the location field's Select2 initialization works properly

  The fix maintains the functionality introduced by the commit (using Select2 for collection selection) while preventing the conflict with existing Select2 instances in the codebase.

app/assets/javascripts/hyrax.js
@@ -70,7 +70,6 @@
 //= require hyrax/select_work_type
 //= require hyrax/collections
 //= require hyrax/collections_v2
-//= require hyrax/collection_select2
 //= require hyrax/collection_types
 //= require hyrax/collections_utils
 //= require hyrax/select_collection_type

app/assets/javascripts/hyrax/collection_select2.js
@@ -1,45 +1,0 @@
-(function($) {
-  $.fn.collectionSearch = function() {
-    return this.each(function() {
-      var $select = $(this);
-
-      // Skip if already initialized
-      if ($select.hasClass('select2-hidden-accessible') && $select.data('select2')) {
-        return;
-      }
-
-      $select.select2({
-        placeholder: $select.data('placeholder') || 'Select',
-        allowClear: true,
-        dropdownParent: $select.closest('.modal-body')
-      });
-    });
-  };
-})(jQuery);
-
-Blacklight.onLoad(function() {
-  $('select.collection-select2').collectionSearch();
-});
-
-// Fix for if select2 breaks because of clicking the back button on the browser
-// Essentially we delete the leftover elements and reinitialize it
-window.addEventListener('popstate', function(event) {
-  Blacklight.onLoad(function() {
-    $('.select2-drop-mask').remove();
-    $('.select2-drop').remove();
-
-    $('select.collection-select2').each(function() {
-      var $select = $(this);
-
-      // Find and remove the broken Select2 container
-      $select.siblings('.select2-container').remove();
-      $select.next('.select2-container').remove();
-      $select.parent().find('.select2-container').remove();
-
-      // Make sure the select is visible and ready
-      $select.show().removeClass('select2-hidden-accessible');
-
-      $select.collectionSearch();
-    });
-  });
-});

app/assets/javascripts/hyrax/relationships/control.es6
@@ -25,8 +25,27 @@ export default class RelationshipsControl {
   }
 
   init() {
+    this.initializeSelect2();
     this.bindAddButton();
     this.displayMembers();
+  }
+
+  initializeSelect2() {
+    // Initialize select2 on collection-select2 dropdowns
+    let collectionSelect = this.input.filter('.collection-select2');
+    if (collectionSelect.length > 0 && !collectionSelect.hasClass('select2-hidden-accessible')) {
+      let dropdownParent = collectionSelect.closest('.modal-body');
+      let options = {
+        placeholder: collectionSelect.data('placeholder') || 'Select',
+        allowClear: true
+      };
+
+      if (dropdownParent.length > 0) {
+        options.dropdownParent = dropdownParent;
+      }
+
+      collectionSelect.select2(options);
+    }
   }
 
   validate() {