c4b8b5bdac18822d115461fb548e4a1e05274f2c
Sarah Proctor <sproctor950@gmail.com>
2025-09-19 12:05:46 -0700

    ⚙️ Temporarily disable batch_upload feature
    This commit prevents admin users from enabling batch_upload in settings.
    This addresses the issue that batch uploads and Valkyrized works are
    incompatible. The current Batch Add uses a fake Active Fedora form with
    fixed terms that ignore the selected work type, causing Valkyrie
    validations to fail (especially on 6.2).
    
    It was determined that fixing this issue is low-priority compared to
    general Bulkrax improvements. To ensure users are not enabling broken
    features, batch_upload is disabled until further decisions are made.
    
    Ref:
    - https://github.com/notch8/palni_palci_knapsack/issues/479

--- config/features.rb
+++ config/features.rb
@@ -2,6 +2,8 @@
 Flipflop.configure do
   # Strategies will be used in the order listed here.
   strategy :cookie
+  # Configuration to prevent features from being enabled
+  strategy Hyrax::Strategies::DisableFeatureStrategy
   strategy :active_record, class: Hyrax::Feature
   strategy Hyrax::Strategies::YamlStrategy, config: Hyrax.config.feature_config_path
   strategy :default
@@ -30,7 +32,7 @@ Flipflop.configure do
 
   feature :batch_upload,
           default: false,
-          description: "This functionality has been deprecated. Please use the bulk importer feature (Bulkrax) to perform this function moving forward."
+          description: "Enable uploading batches of works. <br /><strong>NOTICE:</strong> This feature has been temporarily disabled. To add or upload works in bulk, please use the <a href='https://github.com/samvera/bulkrax/wiki' target='_blank'>Bulkrax importer feature</a>.".html_safe
 
   feature :hide_private_items,
           default: false,
@@ -43,6 +45,7 @@ Flipflop.configure do
   feature :cache_work_iiif_manifest,
           default: false,
           description: "Use Rails.cache to cache the JSON document for IIIF manifests"
+
   feature :read_only,
           default: false,
           description: "Put the system into read-only mode. Deposits, edits, approvals and anything that makes a change to the data will be disabled."
