8b4aae8f9b78d821c22c26805b23738991052879
Rob Kaufman <rob@notch8.com>
2025-09-22 10:04:03 -0700

    js dep order change

--- app/assets/javascripts/hyrax.js
+++ app/assets/javascripts/hyrax.js
@@ -72,9 +72,9 @@
 //= require hyrax/collections_v2
 //= require hyrax/collection_types
 //= require hyrax/collections_utils
+//= require hyrax/collection_select
 //= require hyrax/select_collection_type
 //= require hydra-editor/hydra-editor
-//= require hyrax/collection_select
 //= require nestable
 //= require hyrax/file_manager/sorting
 //= require hyrax/file_manager/save_manager
