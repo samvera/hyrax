fcd7f58673adb8cbff72a12ff7f779dd9883ab2f
Rob Kaufman <rob@notch8.com>
2025-11-12 22:40:53 -0800

    spec fix

--- app/services/hyrax/qa_select_service.rb
+++ app/services/hyrax/qa_select_service.rb
@@ -64,7 +64,7 @@ module Hyrax
     #   is an appropriate next step?
     def include_current_value(value, _index, render_options, html_options)
       unless value.blank? || active?(value)
-        html_options[:class] += [' force-select']
+        html_options[:class] += ' force-select'
         render_options += [[label(value) { value }, value]]
       end
       [render_options, html_options]
