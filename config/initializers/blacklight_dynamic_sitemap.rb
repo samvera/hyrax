27694d511ce251c261cc2fc787dfe3a451c19213
Max Kadel <max@notch8.com>
2025-12-17 10:55:30 +0100

    Address review - use Hyrax::ModelRegistry

--- config/initializers/blacklight_dynamic_sitemap.rb
+++ config/initializers/blacklight_dynamic_sitemap.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
+require 'hyrax/model_registry'
+
+def build_model_exclusion_filter
+  classes_to_exclude = Hyrax::ModelRegistry.file_set_class_names + Hyrax::ModelRegistry.admin_set_class_names + ["Hyrax::FileMetadata"]
+  "-has_model_ssim:(\"#{classes_to_exclude.join('" OR "')}\")"
+end
 
 BlacklightDynamicSitemap::Engine.config.tap do |config|
   # New UUID-based apps can use the id field directly for efficient sitemap generation.
@@ -10,25 +16,23 @@ BlacklightDynamicSitemap::Engine.config.tap do |config|
                              # UUID IDs are hex-based - can use id field directly
                              'id'
                            end
-
-  config.hashed_id_field = 'id'
   config.unique_id_field = 'id'
 
+  model_exclusion_filter = build_model_exclusion_filter
   config.modify_show_params = lambda { |_id, default_params|
     default_params.merge(
       fl: "#{default_params[:fl]},has_model_ssim",
       fq: (default_params[:fq] || []) + [
         'read_access_group_ssim:public',
-        '-has_model_ssim:("Hyrax::FileSet" OR "Hyrax::FileMetadata" OR "Hyrax::AdministrativeSet")'
+        model_exclusion_filter
       ]
     )
   }
-
   config.modify_index_params = lambda { |default_params|
     default_params.merge(
       fq: (default_params[:fq] || []) + [
         'read_access_group_ssim:public',
-        '-has_model_ssim:("Hyrax::FileSet" OR "Hyrax::FileMetadata" OR "Hyrax::AdministrativeSet")'
+        model_exclusion_filter
       ]
     )
   }
