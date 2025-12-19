04d782b97a09030a0295ec0883d3c8f16738ebd8
Daniel Pierce <dlpierce@iu.edu>
2025-12-19 16:43:19 -0500

    rubocop

--- app/services/hyrax/custom_queries/find_by_property_value.rb
+++ app/services/hyrax/custom_queries/find_by_property_value.rb
@@ -28,15 +28,15 @@ module Hyrax
       def find_ids_by_property_pairs(pairs:, field_types: :symbol, **options)
         options = { rows: @query_rows }.merge(options)
         query = pairs.map do |k, v|
-          if field_types.is_a? Hash
-            type = field_types[k] || :symbol
-          else
-            type = field_types
-          end
+          type = if field_types.is_a? Hash
+                   field_types[k] || :symbol
+                 else
+                   field_types
+                 end
           "+#{Hyrax.config.index_field_mapper.solr_name(k, type)}:\"#{RSolr.solr_escape(v)}\""
         end
         response = Hyrax::SolrService.query_result(query.join(' '), fl: 'id', **options)
-        response['response']['docs'].map{ |doc| doc['id'] }
+        response['response']['docs'].map { |doc| doc['id'] }
       end
 
       ##
@@ -60,7 +60,7 @@ module Hyrax
       #
       # @return [Array<Valkyrie::Resource>] found resources, if any
       def find_many_by_property_value(property:, value:, field_type: :symbol, **options)
-        find_many_by_property_pairs(pairs: { property => value }, field_types: field_type, **options )
+        find_many_by_property_pairs(pairs: { property => value }, field_types: field_type, **options)
       end
 
       ##
@@ -73,7 +73,7 @@ module Hyrax
       # @return [NilClass] when no record was found
       # @return [Valkyrie::Resource] when a resource was found
       def find_by_property_value(property:, value:, field_type: :symbol, **options)
-        options.merge!(rows: 1)
+        options[:rows] = 1
         find_many_by_property_value(property: property, value: value, field_type: field_type, **options).first
       end
     end
