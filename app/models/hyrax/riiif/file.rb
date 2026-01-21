744bcec5c962d0eeaa1a61d058d17cee7cc8706c
Rob Kaufman <rob@notch8.com>
2026-01-21 14:53:45 -0800

    make riiif files dependant on riiif inclusion

--- app/models/hyrax/riiif/file.rb
+++ app/models/hyrax/riiif/file.rb
@@ -1,31 +1,32 @@
 # frozen_string_literal: true
+if defined?(::Riiif::File)
+  module Hyrax
+    module Riiif
+      class File < ::Riiif::File
+        include ActiveSupport::Benchmarkable
 
-module Hyrax
-  module Riiif
-    class File < ::Riiif::File
-      include ActiveSupport::Benchmarkable
+        attr_reader :id
 
-      attr_reader :id
+        def initialize(input_path, tempfile = nil, id:)
+          super(input_path, tempfile)
+          raise(ArgumentError, "must specify id") if id.blank?
+          @id = id
+        end
 
-      def initialize(input_path, tempfile = nil, id:)
-        super(input_path, tempfile)
-        raise(ArgumentError, "must specify id") if id.blank?
-        @id = id
-      end
-
-      # Wrap extract in a read lock and benchmark it
-      def extract(transformation, image_info = nil)
-        ::Riiif::Image.file_resolver.file_locks[id].with_read_lock do
-          benchmark "Hyrax::Riiif::File extracted #{path} with #{transformation.to_params}", level: :debug do
-            super
+        # Wrap extract in a read lock and benchmark it
+        def extract(transformation, image_info = nil)
+          ::Riiif::Image.file_resolver.file_locks[id].with_read_lock do
+            benchmark "Hyrax::Riiif::File extracted #{path} with #{transformation.to_params}", level: :debug do
+              super
+            end
           end
         end
-      end
 
-      private
+        private
 
-      def logger
-        Hyrax.logger
+        def logger
+          Hyrax.logger
+        end
       end
     end
   end
