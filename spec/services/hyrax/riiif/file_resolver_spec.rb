a78f0d1626840151989d661b020a1453e2e2ba04
Rob Kaufman <rob@notch8.com>
2026-01-22 12:38:26 -0800

    straighten out the Riiif file nameing  (#7328)
    * straighten out the Riiif file nameing so that classes are not initialized in the initalizer. solves superclass mismatch in development issue
    
    * spec fix
    
    * make riiif files dependant on riiif inclusion
    
    * zeitwork in prod mode
    
    * call the cops

--- spec/services/hyrax/riiif/file_resolver_spec.rb
+++ spec/services/hyrax/riiif/file_resolver_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 require 'spec_helper'
 
-RSpec.describe Hyrax::RiiifFileResolver do
+RSpec.describe Hyrax::Riiif::FileResolver do
   let(:resolver) { described_class.new }
 
   context 'with a file' do
@@ -11,7 +11,7 @@ RSpec.describe Hyrax::RiiifFileResolver do
     describe '#find' do
       it 'returns a locally available RiiifFile using a write lock' do
         expect(resolver.file_locks[file_set.iiif_id]).to receive(:with_write_lock).and_call_original
-        expect(resolver.find(file_set.iiif_id)).to be_instance_of Hyrax::RiiifFile
+        expect(resolver.find(file_set.iiif_id)).to be_instance_of Hyrax::Riiif::File
         expect(File.exist?(file_metadata.file.disk_path)).to eq true
       end
     end
