d62d97347f2f60b1e802707de50b6a7a0e724332
Shana Moore <shana.lavina.moore@gmail.com>
2026-02-05 10:51:11 -0800

    🧹 Adds documentation comments to workflow_indexer.rb

--- app/indexers/concerns/hyrax/workflow_indexer.rb
+++ app/indexers/concerns/hyrax/workflow_indexer.rb
@@ -2,6 +2,8 @@
 
 module Hyrax
   module WorkflowIndexer
+    # Write the suppressed status and workflow roles and state into the solr_document
+    # @param [Hash] solr_document the solr document to add the fields to
     def to_solr
       super.tap do |solr_doc|
         index_suppressed(solr_doc)
@@ -9,11 +11,15 @@ module Hyrax
       end
     end
 
+    # Write the suppressed status into the solr_document
+    # @param [Hash] solr_document the solr document to add the field to
     def index_suppressed(solr_document)
       return unless resource.respond_to?(:suppressed?)
       solr_document['suppressed_bsi'] = resource.suppressed?
     end
 
+    # Write the workflow roles and state so one can see where the document moves to next
+    # @param [Hash] solr_document the solr document to add the field to
     def index_workflow_fields(solr_document)
       entity = Sipity::Entity(resource)
       solr_document['actionable_workflow_roles_ssim'] = workflow_roles(entity).map { |role| "#{entity.workflow.permission_template.source_id}-#{entity.workflow.name}-#{role}" }
@@ -22,6 +28,9 @@ module Hyrax
       nil
     end
 
+    # Get the workflow roles for the entity
+    # @param [Sipity::Entity] entity the entity to get the workflow roles for
+    # @return [Array<String>] the workflow roles for the entity
     def workflow_roles(entity)
       Hyrax::Workflow::PermissionQuery.scope_roles_associated_with_the_given_entity(entity: entity)
     end
