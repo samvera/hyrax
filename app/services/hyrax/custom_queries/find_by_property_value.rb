6a86669db09d97e08f6183b76e9f87548924446a
Daniel Pierce <dlpierce@iu.edu>
2026-02-26 14:23:10 -0500

    Specify suffix directly; support :search_field option

--- app/services/hyrax/custom_queries/find_by_property_value.rb
+++ app/services/hyrax/custom_queries/find_by_property_value.rb
@@ -21,19 +21,19 @@ module Hyrax
       ##
       # Returns ids for resources matching all specified property/value pairs.
       # @param pairs [Hash<String, String>] properties and values to match
-      # @param field_types Hash,Symbol which solr field suffix to use; Use a hash to set per field
+      # @param field_suffix Hash,Symbol which solr field suffix to use; Use a hash to set per field pair
       # @param options Hash options to pass to solr
       #
       # @return [Array<String>] found ids, if any
-      def find_ids_by_property_pairs(pairs:, field_types: :symbol, **options)
+      def find_ids_by_property_pairs(pairs:, field_suffix: :ssim, **options)
         options = { rows: @query_rows }.merge(options)
         query = pairs.map do |k, v|
-          type = if field_types.is_a? Hash
-                   field_types[k] || :symbol
-                 else
-                   field_types
-                 end
-          "+#{Hyrax.config.index_field_mapper.solr_name(k, type)}:\"#{RSolr.solr_escape(v)}\""
+          suffix = if field_suffix.is_a? Hash
+                     field_suffix[k] || :ssim
+                   else
+                     field_suffix
+                   end
+          "+#{k}_#{suffix}:\"#{RSolr.solr_escape(v)}\""
         end
         response = Hyrax::SolrService.query_result(query.join(' '), fl: 'id', **options)
         response['response']['docs'].map { |doc| doc['id'] }
@@ -42,12 +42,12 @@ module Hyrax
       ##
       # Returns resources matching all specified property/value pairs.
       # @param pairs [Hash<String, String>] properties and values to match
-      # @param field_types Hash,Symbol which solr field suffix to use; Use a hash to set per field
+      # @param field_suffix Hash,Symbol which solr field suffix to use; Use a hash to set per field pair
       # @param options Hash options to pass to solr
       #
       # @return [Array<Valkyrie::Resource>] found resources, if any
-      def find_many_by_property_pairs(pairs:, field_types: :symbol, **options)
-        found_ids = find_ids_by_property_pairs(pairs: pairs, field_types: field_types, **options)
+      def find_many_by_property_pairs(pairs:, field_suffix: :ssim, **options)
+        found_ids = find_ids_by_property_pairs(pairs: pairs, field_suffix: field_suffix, **options)
         @query_service.find_many_by_ids(ids: found_ids)
       end
 
@@ -55,26 +55,34 @@ module Hyrax
       # Returns resources matching the specified property and value.
       # @param property [#to_s] the name of the property we're attempting to query.
       # @param value [#to_s] the property value we're trying to match.
-      # @param field_type Symbol which solr field suffix to use
+      # @param field_suffix Symbol which solr field suffix to use
+      # @param search_field String alt way to set property and suffix for compatibility
       # @param options Hash options to pass to solr
       #
       # @return [Array<Valkyrie::Resource>] found resources, if any
-      def find_many_by_property_value(property:, value:, field_type: :symbol, **options)
-        find_many_by_property_pairs(pairs: { property => value }, field_types: field_type, **options)
+      def find_many_by_property_value(property:, value:, field_suffix: :ssim, search_field: nil, **options)
+        if search_field
+          search_field_array = search_field.split('_')
+          field_suffix = search_field_array.last
+          property = search_field_array[0...-1].join('_')
+        end
+        find_many_by_property_pairs(pairs: { property => value }, field_suffix: field_suffix, **options)
       end
 
       ##
       # Returns one resource matching the specified property and value.
       # @param property [#to_s] the name of the property we're attempting to query.
       # @param value [#to_s] the property value we're trying to match.
-      # @param field_type Symbol which solr field suffix to use
+      # @param field_suffix Symbol which solr field suffix to use
+      # @param search_field String alt way to set property and suffix for compatibility
       # @param options Hash options to pass to solr
       #
       # @return [NilClass] when no record was found
       # @return [Valkyrie::Resource] when a resource was found
-      def find_by_property_value(property:, value:, field_type: :symbol, **options)
+      def find_by_property_value(property:, value:, field_suffix: :ssim, search_field: nil, **options)
         options[:rows] = 1
-        find_many_by_property_value(property: property, value: value, field_type: field_type, **options).first
+        find_many_by_property_value(property: property, value: value,
+                                    field_suffix: field_suffix, search_field: search_field, **options).first
       end
     end
   end
