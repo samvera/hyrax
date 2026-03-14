518dbd42c9a6f8f9d6fbd210ba45c018588e515c
Rob Kaufman <rob@notch8.com>
2026-03-14 16:07:55 -0700

    Fix flaky monographs_controller_spec by stabilizing child references
    - Use work.member_ids directly instead of find_child_works custom query
      (which may use Solr and have non-deterministic ordering or indexing delays)
    - Convert Valkyrie::ID to strings in params to match controller behavior
    - Force eager evaluation of all let variables before the update request
      to prevent lazy evaluation ordering issues

--- spec/controllers/hyrax/monographs_controller_spec.rb
+++ spec/controllers/hyrax/monographs_controller_spec.rb
@@ -11,16 +11,21 @@ RSpec.describe Hyrax::MonographsController do
   describe "#update" do
     context "when updating work members" do
       let(:work) { FactoryBot.valkyrie_create(:comet_in_moominland, :with_member_works) }
-      let(:children) { Hyrax.query_service.custom_queries.find_child_works(resource: work) }
-      let(:child1) { children.to_a.first }
-      let(:child2) { children.to_a.last }
+      let(:child1) { Hyrax.query_service.find_by(id: work.member_ids.first) }
+      let(:child2) { Hyrax.query_service.find_by(id: work.member_ids.last) }
       let(:child3) { FactoryBot.valkyrie_create(:monograph) }
       let(:attributes) do
-        { '0' => { id: child1.id, _destroy: 'true' },
-          '1' => { id: child3.id } }
+        { '0' => { id: child1.id.to_s, _destroy: 'true' },
+          '1' => { id: child3.id.to_s } }
       end
 
       it "can add and remove children" do
+        # Force evaluation of all children before the update request
+        expect(work.member_ids.length).to eq 2
+        child1
+        child2
+        child3
+
         patch :update, params: { id: work, monograph: { work_members_attributes: attributes } }
         reloaded = Hyrax.query_service.find_by(id: work.id)
         expect(reloaded.member_ids).to contain_exactly(child2.id, child3.id)
