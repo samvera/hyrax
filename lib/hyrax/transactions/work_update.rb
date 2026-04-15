cd979c095e7f31e318d364c84bdbbeeef448b319
LaRita Robinson <larita@notch8.com>
2026-04-15 17:54:33 -0400

    Transfer permissions along with a work
    When transferring a work to a new admin set, the
    permissions of the work should also be updated to
    match the new admin set's permission template.

--- lib/hyrax/transactions/work_update.rb
+++ lib/hyrax/transactions/work_update.rb
@@ -5,6 +5,7 @@ module Hyrax
     # @since 3.4.0
     class WorkUpdate < Transaction
       DEFAULT_STEPS = ['change_set.apply',
+                       'work_resource.apply_permission_template_on_update',
                        'work_resource.save_acl',
                        'work_resource.add_file_sets',
                        'work_resource.update_work_members'].freeze
