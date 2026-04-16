d0074b0649f599ff2c87b1637ea0f7f46dfc527f
Tiffany Chan <tjychan@uvic.ca>
2026-04-16 12:58:39 -0700

    Delete a stray, unnecessary comment

--- app/models/concerns/hyrax/file_set/transcripts.rb
+++ app/models/concerns/hyrax/file_set/transcripts.rb
@@ -4,12 +4,6 @@ module Hyrax
     module Transcripts
       extend ActiveSupport::Concern
 
-      # Add transcript_ids to the Schema
-      # class TranscriptIdsSchema < ActiveTriples::Schema
-      #   property :transcript_ids, predicate: ::RDF::URI.new('http://vocabulary.samvera.org/ns#transcriptIds'), multiple: true
-      # end
-      # ::FileSet::GeneratedResourceSchema << TranscriptIdsSchema
-
       included do
         property :transcript_ids, predicate: ::RDF::URI.new('http://vocabulary.samvera.org/ns#transcriptIds'), multiple: true do |index|
           index.as :stored_sortable
