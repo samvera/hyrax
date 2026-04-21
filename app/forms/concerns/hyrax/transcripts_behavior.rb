e1cb16ec69021575714be7ea0cfab90d1d00afb9
Tiffany Chan <tjychan@uvic.ca>
2026-04-21 15:44:25 -0700

    When searching for available transcripts. use an fq filter to capture both FileSets and Hyrax::FileSets.

--- app/forms/concerns/hyrax/transcripts_behavior.rb
+++ app/forms/concerns/hyrax/transcripts_behavior.rb
@@ -11,11 +11,16 @@ module Hyrax
       def available_transcripts(parent:, current_ability:)
         member_ids = Hyrax.custom_queries.find_child_file_set_ids(resource: parent)
         Hyrax::SolrQueryService.new
-                               .with_model(model: Hyrax.config.file_set_class.to_s)
+                               # Cast Valkyrie::IDs to strings
                                .with_ids(ids: member_ids.map(&:to_s).to_a)
                                .accessible_by(ability: current_ability, action: :edit)
                                .solr_documents(
-                                 fq: mime_type_filter_query.to_s,
+                                 # Using "has_model_ssim:*FileSet" in fq will return both FileSet
+                                 # and Hyrax::FileSet documents. In test mode, Koppie and Sirenia 
+                                 # index file sets with has_model_ssim:Hyrax::FileSet. 
+                                 # But in dev mode, they index file sets with 
+                                 # has_model_ssim:FileSet instead. This query covers both cases.
+                                 fq: [mime_type_filter_query.to_s, "has_model_ssim:*FileSet"],
                                  fl: "id,title_tesim",
                                  rows: 1000
                                )
